c82aac0efddff6e61167145b36633b6391424c5a
RoanKanninga <roankanninga@gmail.com>
2013-02-08 15:21:35 +0100

    added cbm project

---
+++ molgenis-app-omx-cbm/src/main/java/org/molgenis/cbm/omx/cbm/App.java
@@ -0,0 +1,13 @@
+package org.molgenis.app.omx.cbm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
