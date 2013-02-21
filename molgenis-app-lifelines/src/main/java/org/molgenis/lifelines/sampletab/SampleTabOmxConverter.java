c9b651d6b65e4e0deb925c7475e563af07ae324f
Chao Pang <ChaoPang229@gmail.com>
2013-02-21 20:30:55 +0100

    finish the first version of SampleTabeToOmx converter

--- molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/sampletab/SampleTabOmxConverter.java
+++ molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/sampletab/SampleTabOmxConverter.java
@@ -23,26 +23,37 @@ public class SampleTabOmxConverter
 {
 	private String submissionID;
 	private Map<String, String> unitOntologyTermsForFeatures;
+	private Map<String, String> linkForFeatureToUnits;
 
 	public SampleTabOmxConverter(String inputFilePath, String submissionID) throws IOException
 	{
 		this.submissionID = submissionID;
 		this.unitOntologyTermsForFeatures = new HashMap<String, String>();
+		this.linkForFeatureToUnits = new HashMap<String, String>();
 		ExcelReader reader = new ExcelReader(new File(inputFilePath));
 		ExcelSheetReader sheet = reader.getSheet(0);
-
 		// Collect headers as features to be imported in Omx-format
 		List<String> listOfColumns = collectColumns(sheet);
-		// Collect observableFeatures
+		// Collect observableFeatures and ontology terms, but ontology terms
+		// reference is missing from observableFeature entity
+		// at the moment, so therefore we treat all the columns as features
+		// only!
 		List<String> listOfObservableFeatures = collectObservableFeatures(listOfColumns);
 		ExcelWriter writer = new ExcelWriter(new File(inputFilePath + ".Omx.xls"));
-		addObserableFeatureTab(writer, listOfObservableFeatures);
+		// Add protocol tab
 		addProtocolTab(writer, listOfObservableFeatures);
+		// Add dataset tab
 		addDataSet(writer);
+		// Add data matrix tab
 		addSDataSetMatrix(writer, sheet, listOfObservableFeatures);
+		// Add ontology term tab
 		addOntologyTermTab(writer);
+		// Add observable feature tab
+		addObservableFeatureTab(writer, listOfObservableFeatures);
 		reader.close();
 		writer.close();
+
+		System.out.println("The sample tab data has been converted into Omx format!");
 	}
 
 	public void addOntologyTermTab(ExcelWriter writer) throws IOException
@@ -50,34 +61,33 @@ public class SampleTabOmxConverter
 		ExcelSheetWriter ontologyTermSheet = (ExcelSheetWriter) writer.createTupleWriter("ontologyTerm");
 		List<String> headers = Arrays.asList("identifier", "name");
 		ontologyTermSheet.writeColNames(headers);
-		for (Entry<String, String> entry : unitOntologyTermsForFeatures.entrySet())
+		for (Entry<String, String> entry : linkForFeatureToUnits.entrySet())
 		{
 			String ontologyTerm = entry.getValue();
 			KeyValueTuple newRow = new KeyValueTuple();
 			newRow.set("identifier", createIdentifier(ontologyTerm));
 			newRow.set("name", ontologyTerm);
 			ontologyTermSheet.write(newRow);
-			unitOntologyTermsForFeatures.put(entry.getKey(), createIdentifier(ontologyTerm));
+			linkForFeatureToUnits.put(entry.getKey(), createIdentifier(ontologyTerm));
 		}
 		ontologyTermSheet.close();
 	}
 
-	public void addObserableFeatureTab(ExcelWriter writer, List<String> listOfObservableFeatures) throws IOException
+	public void addObservableFeatureTab(ExcelWriter writer, List<String> listOfObservableFeatures) throws IOException
 	{
 		ExcelSheetWriter observableFeatureSheet = (ExcelSheetWriter) writer.createTupleWriter("observableFeature");
 		List<String> headers = Arrays.asList("identifier", "name", "unit_Identifier");
 		observableFeatureSheet.writeColNames(headers);
 		for (String eachFeature : listOfObservableFeatures)
 		{
-			eachFeature = pattenMatchExtractFeature(eachFeature);
 			KeyValueTuple newRow = new KeyValueTuple();
+			if (linkForFeatureToUnits.containsKey(eachFeature))
+			{
+				newRow.set("unit_Identifier", linkForFeatureToUnits.get(eachFeature));
+			}
+			eachFeature = pattenMatchExtractFeature(eachFeature);
 			newRow.set("identifier", createIdentifier(eachFeature));
 			newRow.set("name", eachFeature);
-			// if (unitOntologyTermsForFeatures.containsKey(eachFeature))
-			// {
-			// newRow.set("unit_Identifier",
-			// unitOntologyTermsForFeatures.get(eachFeature));
-			// }
 			observableFeatureSheet.write(newRow);
 		}
 		observableFeatureSheet.close();
@@ -127,7 +137,14 @@ public class SampleTabOmxConverter
 					String value = eachRow.getString(eachField);
 					newRow.set(headerMapper.get(eachField), value);
 				}
+				if (unitOntologyTermsForFeatures.containsKey(eachField))
+				{
+					String columnNameForUnits = unitOntologyTermsForFeatures.get(eachField);
+					String unitValue = eachRow.getString(columnNameForUnits);
+					if (unitValue != null) linkForFeatureToUnits.put(eachField, unitValue);
+				}
 			}
+
 			dataSetSheetMatrix.write(newRow);
 		}
 		dataSetSheetMatrix.close();
@@ -146,32 +163,31 @@ public class SampleTabOmxConverter
 		dataSetSheet.close();
 	}
 
-	// public List<String> collectOntologyTerms(List<String> listOfColumns)
-	// {
-	// List<String> listOfOntologyTerms = new ArrayList<String>();
-	// for (String currentColumn : listOfColumns)
-	// {
-	// if (currentColumn.toLowerCase().startsWith("characteristic"))
-	// {
-	// String nextColumn = null;
-	// int currentIndex = listOfColumns.indexOf(currentColumn);
-	// if (currentIndex + 1 < listOfColumns.size()) nextColumn =
-	// listOfColumns.get(currentIndex + 1);
-	//
-	// if (nextColumn == null || !nextColumn.toLowerCase().startsWith("unit"))
-	// {
-	// Pattern pattern = Pattern.compile(".+\\[(.+)\\]");
-	// Matcher matcher = pattern.matcher(currentColumn);
-	// if (matcher.find()) currentColumn = matcher.group(1);
-	// listOfOntologyTerms.add(currentColumn);
-	// }
-	// // In this case, the Unit should be ontologyTerm that describes
-	// // the feature
-	// else unitOntologyTermsForFeatures.put(currentColumn, nextColumn);
-	// }
-	// }
-	// return listOfOntologyTerms;
-	// }
+	public List<String> collectOntologyTerms(List<String> listOfColumns)
+	{
+		List<String> listOfOntologyTerms = new ArrayList<String>();
+		for (String currentColumn : listOfColumns)
+		{
+			if (currentColumn.toLowerCase().startsWith("characteristic"))
+			{
+				String nextColumn = null;
+				int currentIndex = listOfColumns.indexOf(currentColumn);
+				if (currentIndex + 1 < listOfColumns.size()) nextColumn = listOfColumns.get(currentIndex + 1);
+
+				if (nextColumn == null || !nextColumn.toLowerCase().startsWith("unit"))
+				{
+					Pattern pattern = Pattern.compile(".+\\[(.+)\\]");
+					Matcher matcher = pattern.matcher(currentColumn);
+					if (matcher.find()) currentColumn = matcher.group(1);
+					listOfOntologyTerms.add(currentColumn);
+				}
+				// In this case, the Unit should be ontologyTerm that describes
+				// the feature
+				else unitOntologyTermsForFeatures.put(currentColumn, nextColumn);
+			}
+		}
+		return listOfOntologyTerms;
+	}
 
 	public List<String> collectObservableFeatures(List<String> listOfColumns)
 	{
@@ -224,12 +240,7 @@ public class SampleTabOmxConverter
 
 	public static void main(String args[]) throws IOException
 	{
-		new SampleTabOmxConverter("/Users/chaopang/Desktop/sample_data.xlsx", "GCR-ada");
-		if (args.length !=2)
-		{
-		    System.err.println("Usage: <sample_data.xlsx> submission-id ");
-		    return;
-		}
-
+		if (args.length != 2) System.err.println("Usage: <sample_data.xlsx> submission-id ");
+		else new SampleTabOmxConverter(args[0], args[1]);
 	}
 }
