48890ad04f61a436268e9cb2ae23a542bf07b7b8
dennishendriksen <d.hendriksen@umcg.nl>
2013-03-14 23:32:19 +0100

    generate & autowire database/login for use in Spring controllers

--- molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppConfig.java
+++ molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppConfig.java
@@ -1,27 +1,27 @@
 package org.molgenis.lifelines;
 
-
 import java.util.List;
 
 import org.molgenis.elasticsearch.config.EmbeddedElasticSearchConfig;
-import org.molgenis.framework.db.Database;
-import org.molgenis.framework.db.DatabaseException;
-import org.molgenis.framework.security.Login;
-import org.molgenis.framework.security.SimpleLogin;
 import org.molgenis.util.GsonHttpMessageConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import app.DatabaseConfig;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("org.molgenis")
-@Import(EmbeddedElasticSearchConfig.class)
+@Import(
+{ DatabaseConfig.class, EmbeddedElasticSearchConfig.class })
 public class WebAppConfig extends WebMvcConfigurerAdapter
 {
 	@Override
@@ -29,7 +29,6 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 	{
 		converters.add(new GsonHttpMessageConverter());
 	}
-
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry)
