617c1f4b4d9929c5cfe314bd6fd517f13d6b8328
dennishendriksen <d.hendriksen@umcg.nl>
2013-03-20 14:03:40 +0100

    show catalogue by default

--- molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/plugins/home/Home.java
+++ molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/plugins/home/Home.java
@@ -7,6 +7,7 @@
 
 package org.molgenis.lifelines.plugins.home;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.molgenis.framework.db.Database;
@@ -18,12 +19,22 @@ import org.molgenis.framework.server.MolgenisSettings;
 import org.molgenis.framework.ui.PluginModel;
 import org.molgenis.framework.ui.ScreenController;
 import org.molgenis.framework.ui.ScreenMessage;
+import org.molgenis.omx.auth.MolgenisPermission;
 import org.molgenis.omx.auth.MolgenisUser;
+import org.molgenis.omx.core.MolgenisEntity;
+import org.molgenis.omx.observ.Category;
+import org.molgenis.omx.observ.DataSet;
+import org.molgenis.omx.observ.ObservableFeature;
+import org.molgenis.omx.observ.ObservationSet;
+import org.molgenis.omx.observ.ObservedValue;
+import org.molgenis.omx.observ.Protocol;
 import org.molgenis.util.ApplicationContextProvider;
 import org.molgenis.util.Entity;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 import app.FillMetadata;
+import app.ui.DataSetViewerPluginPlugin;
+import app.ui.ProtocolViewerPlugin;
 
 /**
  * Shows table of experiment information for WormQTL
@@ -50,13 +61,13 @@ public class Home extends PluginModel<Entity>
 	@Override
 	public String getViewName()
 	{
-		return "Home";
+		return Home.class.getSimpleName();
 	}
 
 	@Override
 	public String getViewTemplate()
 	{
-		return "templates/org/molgenis/lifelines/plugins/home/Home.ftl";
+		return "templates/" + Home.class.getName().replace('.', '/') + ".ftl";
 	}
 
 	@Override
@@ -76,11 +87,9 @@ public class Home extends PluginModel<Entity>
 		{
 			listUsers = db.find(MolgenisUser.class, new QueryRule(MolgenisUser.NAME, Operator.EQUALS, "admin"));
 
-			System.out.println("listUsers: " + listUsers.size());
 			if (listUsers.isEmpty())
 			{
-
-				FillMetadata.fillMetadata(db, false, "SimpleUserLoginPlugin");
+				fillMetaData(db);
 				this.setMessages(new ScreenMessage("User setup complete!", true));
 			}
 
@@ -96,6 +105,44 @@ public class Home extends PluginModel<Entity>
 			e.printStackTrace();
 		}
 
+	}
+
+	private void fillMetaData(Database db) throws Exception
+	{
+		FillMetadata.fillMetadata(db, false, "SimpleUserLoginPlugin");
+
+		List<MolgenisUser> users = db.find(MolgenisUser.class, new QueryRule(MolgenisUser.IDENTIFIER, Operator.EQUALS,
+				"anonymous"));
+		if (users != null && !users.isEmpty())
+		{
+			MolgenisUser userAnonymous = users.get(0);
+
+			List<Class<?>> visibleClasses = new ArrayList<Class<?>>();
+			visibleClasses.add(ProtocolViewerPlugin.class);
+			visibleClasses.add(DataSetViewerPluginPlugin.class);
+			// add entity dependencies for plugins
+			visibleClasses.add(DataSet.class);
+			visibleClasses.add(Protocol.class);
+			visibleClasses.add(ObservationSet.class);
+			visibleClasses.add(ObservableFeature.class);
+			visibleClasses.add(Category.class);
+			visibleClasses.add(ObservedValue.class);
+
+			for (Class<?> entityClass : visibleClasses)
+			{
+				MolgenisEntity molgenisEntity = db.find(MolgenisEntity.class,
+						new QueryRule("className", Operator.EQUALS, entityClass.getName())).get(0);
+
+				MolgenisPermission runtimePropertyPermission = new MolgenisPermission();
+				runtimePropertyPermission.setRole(userAnonymous.getId());
+				runtimePropertyPermission.setName(userAnonymous.getName());
+				runtimePropertyPermission.setIdentifier(MolgenisPermission.class.getSimpleName() + '_'
+						+ userAnonymous.getIdentifier() + '_' + entityClass.getSimpleName());
+				runtimePropertyPermission.setEntity(molgenisEntity.getId());
+				runtimePropertyPermission.setPermission("read");
+				db.add(runtimePropertyPermission);
+			}
+		}
 	}
 
 	@Override
