27d666c48591ebb66018d067b944178ab5637cce
dennishendriksen <d.hendriksen@umcg.nl>
2013-03-22 08:32:47 +0100

    Add query rules to entity collection request

--- molgenis-core/src/main/java/org/molgenis/framework/server/EntityCollectionRequest.java
+++ molgenis-core/src/main/java/org/molgenis/framework/server/EntityCollectionRequest.java
@@ -1,10 +1,15 @@
 package org.molgenis.framework.server;
 
+import java.util.List;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
+import org.molgenis.framework.db.QueryRule;
+
 public class EntityCollectionRequest
 {
+	private List<QueryRule> q;
 	@Min(0)
 	private int start = 0;
 	@Min(0)
@@ -29,5 +34,15 @@ public class EntityCollectionRequest
 	public void setNum(int num)
 	{
 		this.num = num;
+	}
+
+	public List<QueryRule> getQ()
+	{
+		return q;
+	}
+
+	public void setQ(List<QueryRule> q)
+	{
+		this.q = q;
 	}
 }
