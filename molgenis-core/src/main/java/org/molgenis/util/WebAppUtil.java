4240eca0a81e6475ec5dd1018e2494a88e2ab2bb
dennishendriksen <d.hendriksen@umcg.nl>
2013-04-14 16:13:44 +0200

    add Login and MolgenisSettings bean

--- molgenis-core/src/main/java/org/molgenis/util/WebAppUtil.java
+++ molgenis-core/src/main/java/org/molgenis/util/WebAppUtil.java
@@ -1,6 +1,8 @@
 package org.molgenis.util;
 
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.security.Login;
+import org.molgenis.framework.server.MolgenisSettings;
 import org.springframework.mail.javamail.JavaMailSender;
 
 /**
@@ -16,8 +18,18 @@ public class WebAppUtil
 		return ApplicationContextProvider.getApplicationContext().getBean("database", Database.class);
 	}
 
+	public static Login getLogin()
+	{
+		return ApplicationContextProvider.getApplicationContext().getBean("login", Login.class);
+	}
+
 	public static JavaMailSender getMailSender()
 	{
 		return ApplicationContextProvider.getApplicationContext().getBean("mailSender", JavaMailSender.class);
+	}
+
+	public static MolgenisSettings getMolgenisSettings()
+	{
+		return ApplicationContextProvider.getApplicationContext().getBean("molgenisSettings", MolgenisSettings.class);
 	}
 }
