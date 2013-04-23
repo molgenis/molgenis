766fc96ca226d9ee1e4c83aca57c07faf7ea77cf
dennishendriksen <d.hendriksen@umcg.nl>
2013-04-23 16:53:04 +0200

    add study data request modal and protocolviewer integration

---
+++ molgenis-omx-protocolviewer/src/main/java/org/molgenis/omx/controller/OrderStudyDataController.java
@@ -0,0 +1,51 @@
+package org.molgenis.omx.controller;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.Part;
+
+import org.molgenis.framework.db.DatabaseException;
+import org.molgenis.omx.beans.ShoppingCart;
+import org.molgenis.omx.service.OrderStudyDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/plugin")
+public class OrderStudyDataController
+{
+	@Autowired
+	private OrderStudyDataService orderStudyDataService;
+
+	@Autowired
+	private ShoppingCart shoppingCart;
+
+	@RequestMapping(value = "/order", method = RequestMethod.GET)
+	public String getOrderDataForm() throws DatabaseException
+	{
+		return "orderdata-modal";
+	}
+
+	// Spring's StandardServletMultipartResolver can't bind a RequestBody or ModelAttribute
+	@RequestMapping(value = "/order", method = RequestMethod.POST, headers = "Content-Type=multipart/form-data")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void orderData(@RequestParam String name, @RequestParam Part file) throws DatabaseException, IOException,
+			MessagingException
+	{
+		orderStudyDataService.orderStudyData(name, file, shoppingCart.getCart());
+	}
+
+	// TODO default exception handler?
+	@ExceptionHandler(DatabaseException.class)
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public void handleDatabaseException(DatabaseException e)
+	{
+	}
+}
