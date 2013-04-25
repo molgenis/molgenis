b08686f2821b37a8519e9996742fefb746a1a858
dennishendriksen <d.hendriksen@umcg.nl>
2013-04-25 17:26:50 +0200

    use very basic FileStore

--- molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppConfig.java
+++ molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppConfig.java
@@ -8,6 +8,7 @@ import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.omx.OmxConfig;
 import org.molgenis.util.ApplicationContextProvider;
 import org.molgenis.util.AsyncJavaMailSender;
+import org.molgenis.util.FileStore;
 import org.molgenis.util.GsonHttpMessageConverter;
 import org.molgenis.util.ShoppingCart;
 import org.springframework.beans.factory.annotation.Value;
@@ -107,6 +108,12 @@ public class WebAppConfig extends WebMvcConfigurerAdapter
 		javaMailProperties.setProperty("mail.smtp.quitwait", mailJavaQuitWait);
 		mailSender.setJavaMailProperties(javaMailProperties);
 		return mailSender;
+	}
+
+	@Bean
+	public FileStore fileStore()
+	{
+		return new FileStore(System.getProperty("user.home"));
 	}
 
 	/**
