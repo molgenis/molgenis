256eba2f341c4f3f42e4e792ae1d6ae757f5a103
erwinwinder <j.a.winder@umcg.nl>
2013-05-02 15:06:32 +0200

    Added ComputeRunDecorator

---
+++ molgenis-app-compute-db/src/main/java/org/molgenis/compute/db/decorators/ComputeRunDecorator.java
@@ -0,0 +1,44 @@
+package org.molgenis.compute.db.decorators;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.molgenis.compute.runtime.ComputeRun;
+import org.molgenis.framework.db.DatabaseException;
+import org.molgenis.framework.db.Mapper;
+import org.molgenis.framework.db.MapperDecorator;
+
+public class ComputeRunDecorator extends MapperDecorator<ComputeRun>
+{
+	public ComputeRunDecorator(Mapper<ComputeRun> generatedMapper)
+	{
+		super(generatedMapper);
+	}
+
+	@Override
+	public int add(List<ComputeRun> entities) throws DatabaseException
+	{
+		validate(entities);
+		return super.add(entities);
+	}
+
+	@Override
+	public int update(List<ComputeRun> entities) throws DatabaseException
+	{
+		validate(entities);
+		return super.update(entities);
+	}
+
+	private void validate(List<ComputeRun> entities) throws DatabaseException
+	{
+		for (ComputeRun run : entities)
+		{
+			if (StringUtils.isEmpty(run.getName()) || !StringUtils.isAlphanumeric(run.getName()))
+			{
+				throw new DatabaseException("Illegal run name [" + run.getName() + "] should be non empty alphnumeric");
+			}
+		}
+
+	}
+
+}
