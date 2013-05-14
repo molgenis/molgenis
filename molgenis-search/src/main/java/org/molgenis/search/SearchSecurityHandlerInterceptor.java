f7e987369e6dc7a1757b4a91c1304f04b30c308d
dennishendriksen <d.hendriksen@umcg.nl>
2013-05-14 16:23:02 +0200

    simplify annotations using bean scoping, fix search security interceptor

--- molgenis-search/src/main/java/org/molgenis/search/SearchSecurityHandlerInterceptor.java
+++ molgenis-search/src/main/java/org/molgenis/search/SearchSecurityHandlerInterceptor.java
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.molgenis.framework.security.Login;
+import org.molgenis.util.ApplicationContextProvider;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
- * HandlerInterceptor returns SC_UNAUTHORIZED if no Login object is present in
- * the session or the user is not authenticated and login is required.
+ * HandlerInterceptor returns SC_UNAUTHORIZED if no Login object is present in the session or the user is not
+ * authenticated and login is required.
  * 
- * Does not check entity level security or roles. Just checks if the user is
- * authenticated
+ * Does not check entity level security or roles. Just checks if the user is authenticated
  * 
  * @author erwin
  * 
@@ -21,7 +21,7 @@ public class SearchSecurityHandlerInterceptor extends HandlerInterceptorAdapter
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
 	{
-		Login login = (Login) request.getSession().getAttribute("login");
+		Login login = ApplicationContextProvider.getApplicationContext().getBean(Login.class);
 		if ((login != null) && (login.isAuthenticated() || !login.isLoginRequired()))
 		{
 			return true;
