50729dc42bad1dcc7206838279605a07e291f832
dennishendriksen <d.hendriksen@umcg.nl>
2013-05-15 15:11:14 +0200

    replace org.apache.commons.lang with org.apache.commons.lang3

--- molgenis-omx-auth/src/main/java/org/molgenis/omx/auth/service/MolgenisUserService.java
+++ molgenis-omx-auth/src/main/java/org/molgenis/omx/auth/service/MolgenisUserService.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.molgenis.framework.db.Database;
 import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.db.Query;
