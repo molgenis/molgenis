36bde16cfa389e67b2c9f76e5115f5c66f292e77
Despoina Antonakaki <antonakd@gmail.com>
2013-05-31 14:49:02 +0200

    removed Junit changed to test ng & changed chromedriver path source

--- molgenis-app-omx/src/test/java/test/DataSetViewerTest.java
+++ molgenis-app-omx/src/test/java/test/DataSetViewerTest.java
@@ -1,37 +1,53 @@
 package test;
-
-import static junit.framework.Assert.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.molgenis.MolgenisOptions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import test.DataSetViewerTest.DataSetViewerTestConfig;
 
-public class DataSetViewerTest {
-  private WebDriver driver;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = DataSetViewerTestConfig.class)
+public class DataSetViewerTest extends AbstractTestNGSpringContextTests {
   
-  @Before
+	private WebDriver driver;
+  
+  @Value("${chromedriver}")
+  private String chromedriver;
+  
+  @BeforeMethod
   public void openBrowser() {
     
-	MolgenisOptions mo = new MolgenisOptions();
-	
-	System.setProperty("webdriver.chrome.driver", mo.getChromeDriver());     driver = new ChromeDriver();
+	System.setProperty("webdriver.chrome.driver", chromedriver); 
+    driver = new ChromeDriver();
    
-    driver.get("http://localhost:8080/");
+    System.out.println(chromedriver);
+	driver.get("http://localhost:8080/");
    
   }
   
-  @After
+  @AfterMethod
   public void quit() throws IOException {
     driver.quit();
   }
-  
  
   @Test
   public void loginandclick() {
@@ -56,5 +72,21 @@ public class DataSetViewerTest {
     assertEquals("Molgenis", driver.getTitle());
 
   }
+  
+  @Configuration
+	public static class DataSetViewerTestConfig extends WebMvcConfigurationSupport
+	{
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer properties()
+		{
+			PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+			Resource[] resources = new FileSystemResource[]
+			{ new FileSystemResource(System.getProperty("user.home") + "/molgenis-server.properties") };
+			pspc.setLocations(resources);
+			pspc.setIgnoreUnresolvablePlaceholders(true);
+			pspc.setIgnoreResourceNotFound(true);
+			return pspc;
+		}
+	}
   
 }
