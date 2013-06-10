15193bc31ea78fd3d59e212a3bfee8967beeb7b6
Chao Pang <ChaoPang229@gmail.com>
2013-06-10 14:25:32 +0200

    added a new maven module for harmonization

---
+++ molgenis-omx-harmonization/src/main/java/org/molgenis/omx/harmonization/App.java
@@ -0,0 +1,13 @@
+package org.molgenis.omx.harmonization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
