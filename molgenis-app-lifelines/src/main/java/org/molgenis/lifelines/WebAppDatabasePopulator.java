2f75c481301d9b0054f1d62a54bc47e60714173c
bartcharbon <bart.charbon@gmail.com>
2013-06-11 15:08:19 +0200

    do not store admin password in source but in molgenis-server.properties

--- molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppDatabasePopulator.java
+++ molgenis-app-lifelines/src/main/java/org/molgenis/lifelines/WebAppDatabasePopulator.java
@@ -35,30 +35,36 @@ import org.molgenis.ui.StudyDefinitionLoaderPluginPlugin;
 import org.springframework.beans.factory.annotation.Value;
 
 public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
-{	
+{
 	@Value("${app.profile}")
 	private String appProfile;
+	@Value("${admin.password}")
+	private String adminPassword;
 	@Value("${researcher.password}")
 	private String dataManagerPassword;
 	@Value("${datamanager.password}")
 	private String researchPassword;
-	
+
 	@Override
 	protected void initializeApplicationDatabase(Database database) throws Exception
 	{
-		if(appProfile==null||dataManagerPassword==null||researchPassword==null){
+		if ("${app.profile}".equals(appProfile) || "${datamanager.password}".equals(dataManagerPassword)
+				|| "${researcher.password}".equals(researchPassword) || "${admin.password}".equals(adminPassword))
+		{
 			StringBuilder message = new StringBuilder("please configure: ");
-			if(appProfile == null) message.append("app.profile(possible values: workspace or website), ");
-			if(dataManagerPassword == null) message.append("default datamanager.password, ");
-			if(researchPassword == null) message.append("default researcher.password ");
+			if ("${app.profile}".equals(appProfile)) message
+					.append("app.profile(possible values: workspace or website), ");
+			if ("${datamanager.password}".equals(dataManagerPassword)) message.append("default datamanager.password, ");
+			if ("${researcher.password}".equals(researchPassword)) message.append("default researcher.password ");
+			if ("${admin.password}".equals(adminPassword)) message.append("default admin.password ");
 			message.append("in your molgenis-server.properties.");
-			throw new RuntimeException(message.toString());	
+			throw new RuntimeException(message.toString());
 		}
-					
+
 		Login login = database.getLogin();
 		database.setLogin(null);
-		login.login(database, Login.USER_ADMIN_NAME, "admin"); // FIXME hardcoded reference to admin password hash
-		
+		login.login(database, Login.USER_ADMIN_NAME, adminPassword);
+
 		// set app name
 		RuntimeProperty runtimeProperty = new RuntimeProperty();
 		runtimeProperty.setIdentifier(RuntimeProperty.class.getSimpleName() + '_' + GuiService.KEY_APP_NAME);
@@ -66,9 +72,11 @@ public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
 		runtimeProperty.setValue("LifeLines");
 		database.add(runtimeProperty);
 
-		MolgenisUser userResearcher = createUser(database, "researcher", "Researcher", "Researcher", "molgenis@gmail.com", researchPassword, false);		
-		MolgenisUser userDataManager = createUser(database, "datamanager", "DataManager", "DataManager", "molgenis@gmail.com", dataManagerPassword, false);
-		
+		MolgenisUser userResearcher = createUser(database, "researcher", "Researcher", "Researcher",
+				"molgenis@gmail.com", researchPassword, false);
+		MolgenisUser userDataManager = createUser(database, "datamanager", "DataManager", "DataManager",
+				"molgenis@gmail.com", dataManagerPassword, false);
+
 		List<MolgenisUser> users = database.find(MolgenisUser.class, new QueryRule(MolgenisUser.NAME, Operator.EQUALS,
 				Login.USER_ANONYMOUS_NAME));
 		if (users != null && !users.isEmpty())
@@ -103,7 +111,7 @@ public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
 			}
 			createPermission(database, StudyDataRequest.class, allUsersGroup, "own");
 		}
-		
+
 		createPermission(database, Characteristic.class, userDataManager, "write");
 		createPermission(database, OntologyTerm.class, userDataManager, "write");
 		createPermission(database, Ontology.class, userDataManager, "write");
@@ -113,28 +121,33 @@ public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
 		createPermission(database, ObservableFeature.class, userDataManager, "write");
 		createPermission(database, Category.class, userDataManager, "write");
 		createPermission(database, ObservedValue.class, userDataManager, "write");
-		
-		
-		if("website".equals(appProfile)){
+
+		if ("website".equals(appProfile))
+		{
 			createPermission(database, CatalogueLoaderPluginPlugin.class, userDataManager, "read");
-		}else if("workspace".equals(appProfile)){
+		}
+		else if ("workspace".equals(appProfile))
+		{
 			createPermission(database, DataSetViewerPluginPlugin.class, userDataManager, "read");
 			createPermission(database, DataSetViewerPluginPlugin.class, userResearcher, "read");
 			createPermission(database, StudyDefinitionLoaderPluginPlugin.class, userDataManager, "read");
-		}else{
-			throw new RuntimeException("please configure app.profile in your molgenis-server.properties. possible values: workspace or website");
 		}
-		
+		else
+		{
+			throw new RuntimeException(
+					"please configure app.profile in your molgenis-server.properties. possible values: workspace or website");
+		}
+
 		database.setLogin(login); // restore login
 	}
-	
+
 	private MolgenisUser createUser(Database database, String userName, String firstName, String lastName,
 			String email, String password, boolean superUser) throws NoSuchAlgorithmException, DatabaseException
 	{
 		MolgenisUser user = new MolgenisUser();
 		user.setName(userName);
 		user.setIdentifier(UUID.randomUUID().toString());
-		user.setPassword(new PasswordHasher().toMD5(password)); 
+		user.setPassword(new PasswordHasher().toMD5(password));
 		user.setEmail(email);
 		user.setFirstName(firstName);
 		user.setLastName(lastName);
@@ -143,8 +156,9 @@ public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
 		database.add(user);
 		return user;
 	}
-	
-	private void createPermission(Database database, Class<?> clazz, MolgenisRole role, String permissionString) throws DatabaseException
+
+	private void createPermission(Database database, Class<?> clazz, MolgenisRole role, String permissionString)
+			throws DatabaseException
 	{
 		MolgenisPermission permission = new MolgenisPermission();
 		permission.setEntity(MolgenisEntity.findByClassName(database, clazz.getName()));
