046abb268619f85dceece5f97c4a0cb9cb2e81fd
erwinwinder <j.a.winder@umcg.nl>
2013-06-18 12:48:12 +0200

    Index dataset after import

--- molgenis-omx-dataexplorer/src/main/java/org/molgenis/dataexplorer/search/StartUpIndexer.java
+++ molgenis-omx-dataexplorer/src/main/java/org/molgenis/dataexplorer/search/StartUpIndexer.java
@@ -2,41 +2,49 @@ package org.molgenis.dataexplorer.search;
 
 import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.tupletable.TableException;
+import org.molgenis.util.DataSetImportedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ApplicationContextEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
- * Indexes not yet indexed DataSets at application startup
+ * Indexes not yet indexed DataSets at application startup and when a dataset is imported
  * 
  * @author erwin
  * 
  */
-public class StartUpIndexer implements ApplicationListener<ApplicationContextEvent>
+public class StartUpIndexer implements ApplicationListener<ApplicationEvent>
 {
 	@Autowired
 	private DataSetsIndexer dataSetsIndexer;
 
 	// Index new datasets after the app config is loaded
 	@Override
-	public void onApplicationEvent(ApplicationContextEvent event)
+	public void onApplicationEvent(ApplicationEvent event)
 	{
-		if (event instanceof ContextRefreshedEvent)
+
+		try
 		{
-			try
+			if (event instanceof ContextRefreshedEvent)
 			{
 				dataSetsIndexer.indexNew();
 			}
-			catch (DatabaseException e)
+			else if (event instanceof DataSetImportedEvent)
 			{
-				throw new RuntimeException(e);
-			}
-			catch (TableException e)
-			{
-				throw new RuntimeException(e);
+				DataSetImportedEvent dataSetImportedEvent = (DataSetImportedEvent) event;
+				dataSetsIndexer.index(dataSetImportedEvent.getDataSetIdentifier());
 			}
 		}
+		catch (DatabaseException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch (TableException e)
+		{
+			throw new RuntimeException(e);
+		}
+
 	}
 
 }
