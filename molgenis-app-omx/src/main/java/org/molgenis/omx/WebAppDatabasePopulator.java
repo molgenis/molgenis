7a6242b512b1140d003ec55eda60c241ab981cc0
RoanKanninga <roankanninga@gmail.com>
2013-06-25 17:14:56 +0200

    fixed the logo runtimeproperty

--- molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulator.java
+++ molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulator.java
@@ -36,7 +36,7 @@ public class WebAppDatabasePopulator extends MolgenisDatabasePopulator
 
 		Map<String, String> runtimePropertyMap = new HashMap<String, String>();
 		runtimePropertyMap.put(GuiService.KEY_APP_NAME, "OMX");
-		runtimePropertyMap.put("app.logo", "img/logo_default.png");
+		runtimePropertyMap.put("app.href.logo", "img/logo_default.png");
 		runtimePropertyMap.put("app.home.html", "Welcome to Molgenis!");
 		runtimePropertyMap.put("app.background", "There is no background information");
 		runtimePropertyMap.put("app.news", "There is no news ");
