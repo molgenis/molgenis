8b84718a4d16cc818fa4cb1f30c945ec8b483c36
mdijkstra <mdijkstra@work>
2013-06-27 12:57:41 +0200

    Make job name available in generated script

--- molgenis-compute-core/src/main/java/org/molgenis/compute5/generators/TaskGenerator.java
+++ molgenis-compute-core/src/main/java/org/molgenis/compute5/generators/TaskGenerator.java
@@ -118,6 +118,8 @@ public class TaskGenerator
 				}
 
 				parameterHeader += "\n# Assign values to the parameters in this script\n";
+				parameterHeader += "\n# Set taskId, which is the jobname of this task";
+				parameterHeader += "\ntaskId=\"" + task.getName() + "\"\n";
 				
 				parameterHeader += "\n# Make compute.properties available";
 				parameterHeader += "\nrundir=\"" + computeProperties.runDir + "\"";
