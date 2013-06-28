c6efb33cf0e7d5ed67fdd34aef84ec41ef85f1db
Chao Pang <ChaoPang229@gmail.com>
2013-06-28 10:52:48 +0200

    added couple of new methods to harmonizationIndexer interface and its implementation

--- molgenis-omx-harmonization/src/main/java/org/molgenis/omx/harmonizationIndexer/plugin/AsyncHarmonizationIndexer.java
+++ molgenis-omx-harmonization/src/main/java/org/molgenis/omx/harmonizationIndexer/plugin/AsyncHarmonizationIndexer.java
@@ -1,9 +1,13 @@
 package org.molgenis.omx.harmonizationIndexer.plugin;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.commons.lang3.StringUtils;
 import org.molgenis.framework.db.Database;
 import org.molgenis.omx.harmonizationIndexer.controller.OntologyModel;
+import org.molgenis.omx.harmonizationIndexer.table.OntologyTable;
+import org.molgenis.omx.harmonizationIndexer.table.OntologyTermTable;
 import org.molgenis.search.SearchService;
 import org.molgenis.util.DatabaseUtil;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -14,6 +18,9 @@ import org.springframework.scheduling.annotation.Async;
 public class AsyncHarmonizationIndexer implements HarmonizationIndexer, InitializingBean
 {
 	private SearchService searchService;
+	private String ontologyUri = null;
+
+	private final AtomicInteger runningIndexProcesses = new AtomicInteger();
 
 	@Autowired
 	public void setSearchService(SearchService searchService)
@@ -27,21 +34,39 @@ public class AsyncHarmonizationIndexer implements HarmonizationIndexer, Initiali
 		if (searchService == null) throw new IllegalArgumentException("Missing bean of type SearchService");
 	}
 
+	@Override
+	public boolean isIndexingRunning()
+	{
+		return (runningIndexProcesses.get() > 0);
+	}
+
 	@Async
 	public void index(File ontologyFile)
 	{
+		runningIndexProcesses.incrementAndGet();
 		Database db = DatabaseUtil.createDatabase();
 
 		try
 		{
 			OntologyModel model = new OntologyModel(ontologyFile);
-			searchService.indexTupleTable("ontology", new OntologyTable(model, db));
-			searchService.indexTupleTable("ontologyTerm", new OntologyTermTable(model, db));
+			ontologyUri = model.getOntologyIRI() == null ? StringUtils.EMPTY : model.getOntologyIRI();
+			searchService.indexTupleTable("ontology" + ontologyUri, new OntologyTable(model, db));
+			searchService.indexTupleTable("ontologyTerm" + ontologyUri, new OntologyTermTable(model, db));
 		}
 		catch (OWLOntologyCreationException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally
+		{
+			DatabaseUtil.closeQuietly(db);
+			runningIndexProcesses.decrementAndGet();
+			ontologyUri = null;
+		}
+	}
+
+	public String getOntologyUri()
+	{
+		return ontologyUri;
 	}
 }
--- molgenis-omx-harmonization/src/main/java/org/molgenis/omx/harmonizationIndexer/plugin/HarmonizationIndexer.java
+++ molgenis-omx-harmonization/src/main/java/org/molgenis/omx/harmonizationIndexer/plugin/HarmonizationIndexer.java
@@ -3,8 +3,13 @@ package org.molgenis.omx.harmonizationIndexer.plugin;
 import java.io.File;
 
 import org.molgenis.framework.tupletable.TableException;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public interface HarmonizationIndexer
 {
-	void index(File file) throws TableException;
+	void index(File file) throws TableException, OWLOntologyCreationException;
+
+	boolean isIndexingRunning();
+
+	String getOntologyUri();
 }
