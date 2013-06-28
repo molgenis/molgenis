3cf14af3e7a6ada5a581ee66275fbc13aad49a2b
RoanKanninga <roankanninga@gmail.com>
2013-06-28 09:23:49 +0200

    added ViewFactory back

---
+++ molgenis-core/src/main/java/org/molgenis/framework/tupletable/view/ViewFactoryImpl.java
@@ -0,0 +1,27 @@
+package org.molgenis.framework.tupletable.view;
+
+import org.molgenis.framework.tupletable.view.renderers.Renderers.CSVRenderer;
+import org.molgenis.framework.tupletable.view.renderers.Renderers.ExcelRenderer;
+import org.molgenis.framework.tupletable.view.renderers.Renderers.Renderer;
+import org.molgenis.framework.tupletable.view.renderers.Renderers.SPSSRenderer;
+
+public class ViewFactoryImpl implements ViewFactory
+{
+	@Override
+	public Renderer createView(String viewName)
+	{
+		if (viewName.equals("EXCEL"))
+		{
+			return new ExcelRenderer();
+		}
+		else if (viewName.equals("CSV"))
+		{
+			return new CSVRenderer();
+		}
+		else if (viewName.equals("SPSS"))
+		{
+			return new SPSSRenderer();
+		}
+		throw new IllegalArgumentException(String.format("view: %s not found", viewName));
+	}
+}
