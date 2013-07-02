172255313f0e382a609cdd449260f47aaae15fe6
HV Byelas <hvbyelas@lt141171.med.rug.nl>
2013-07-02 16:35:42 +0200

    uncommented one test

--- molgenis-compute-core/src/test/java/org/molgenis/compute5/ComputeCommandLineTest.java
+++ molgenis-compute-core/src/test/java/org/molgenis/compute5/ComputeCommandLineTest.java
@@ -138,7 +138,7 @@ public class ComputeCommandLineTest
 				"--workflow", "workflow.csv",
 				"--defaults", "workflow.defaults.csv",
 				"--parameters","parameters.csv",
-				"--rundir","benchmark/run",
+				"--rundir","src/main/resources/workflows/benchmark/run",
 				"--backend","pbs",
 				"--database","none"});
 
@@ -146,10 +146,10 @@ public class ComputeCommandLineTest
 		System.out.println("--- Test Compute Properties ---");
 		String resultProperties =  getFileAsString(".compute.properties");
 
-//		if(!resultProperties.contains("rundir=src/main/resources/workflows/benchmark/run"))
-//		{
-//			Assert.fail("rundir parameter is failed");
-//		}
+		if(!resultProperties.contains("rundir=src/main/resources/workflows/benchmark/run"))
+		{
+			Assert.fail("rundir parameter is failed");
+		}
 
 		if(!resultProperties.contains("defaults=src/main/resources/workflows/benchmark/workflow.defaults.csv"))
 		{
@@ -171,46 +171,46 @@ public class ComputeCommandLineTest
 			Assert.fail("backend parameter is failed");
 		}
 
-//		System.out.println("--- Test Created Files ---");
-//
-//		File file = new File("src/main/resources/workflows/benchmark/run/step1_0.sh");
-//		if (!file.exists())
-//		{
-//			Assert.fail("step1_0.sh is not generated");
-//		}
-//
-//		file = new File("src/main/resources/workflows/benchmark/run/step1_1.sh");
-//		if (!file.exists())
-//		{
-//			Assert.fail("step1_1.sh is not generated");
-//		}
-//
-//		file = new File("src/main/resources/workflows/benchmark/run/step2_0.sh");
-//		if (!file.exists())
-//		{
-//			Assert.fail("step2_0.sh is not generated");
-//		}
-//
-//		file = new File("src/main/resources/workflows/benchmark/run/submit.sh");
-//		if (!file.exists())
-//		{
-//			Assert.fail("submit.sh is not generated");
-//		}
-//
-//		file = new File("src/main/resources/workflows/benchmark/run/user.env");
-//		if (!file.exists())
-//		{
-//			Assert.fail("user.env is not generated");
-//		}
-//
-//		System.out.println("Test correct headers insertion");
-//
-//		String script = getFileAsString("src/main/resources/workflows/benchmark/run/step1_0.sh");
-//
-//		if(!script.contains("# My own custom header"))
-//		{
-//			Assert.fail("headers/footers are not correctly inserted");
-//		}
+		System.out.println("--- Test Created Files ---");
+
+		File file = new File("src/main/resources/workflows/benchmark/run/step1_0.sh");
+		if (!file.exists())
+		{
+			Assert.fail("step1_0.sh is not generated");
+		}
+
+		file = new File("src/main/resources/workflows/benchmark/run/step1_1.sh");
+		if (!file.exists())
+		{
+			Assert.fail("step1_1.sh is not generated");
+		}
+
+		file = new File("src/main/resources/workflows/benchmark/run/step2_0.sh");
+		if (!file.exists())
+		{
+			Assert.fail("step2_0.sh is not generated");
+		}
+
+		file = new File("src/main/resources/workflows/benchmark/run/submit.sh");
+		if (!file.exists())
+		{
+			Assert.fail("submit.sh is not generated");
+		}
+
+		file = new File("src/main/resources/workflows/benchmark/run/user.env");
+		if (!file.exists())
+		{
+			Assert.fail("user.env is not generated");
+		}
+
+		System.out.println("Test correct headers insertion");
+
+		String script = getFileAsString("src/main/resources/workflows/benchmark/run/step1_0.sh");
+
+		if(!script.contains("# My own custom header"))
+		{
+			Assert.fail("headers/footers are not correctly inserted");
+		}
 	}
 
 	private final String getFileAsString(String filename) throws IOException
