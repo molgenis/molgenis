8154ec49a736b2cd052d585a5dc3f40cfbd5bb39
HV Byelas <hvbyelas@lt141171.med.rug.nl>
2013-07-04 15:38:14 +0200

    changed molgenis_compute.sh to solve issue #507

--- molgenis-compute-core/src/main/resources/molgenis_compute.sh
+++ molgenis-compute-core/src/main/resources/molgenis_compute.sh
@@ -4,20 +4,21 @@
 #
 
 MCDIR=$( cd -P "$( dirname "$0" )" && pwd )
+cd $MCDIR
 
 java -cp \
-$(dirname -- "$0")/lib/molgenis-core-0.0.1-SNAPSHOT.jar:\
-$(dirname -- "$0")/lib/molgenis-compute-core-0.0.1-SNAPSHOT.jar:\
-$(dirname -- "$0")/lib/commons-cli-1.2.jar:\
-$(dirname -- "$0")/lib/commons-io-2.4.jar:\
-$(dirname -- "$0")/lib/freemarker-2.3.18.jar:\
-$(dirname -- "$0")/lib/log4j-1.2.17.jar:\
-$(dirname -- "$0")/lib/opencsv-2.3.jar:\
-$(dirname -- "$0")/lib/httpclient-4.2.5.jar:\
-$(dirname -- "$0")/lib/httpcore-4.2.4.jar:\
-$(dirname -- "$0")/lib/guava-14.0.1.jar:\
-$(dirname -- "$0")/lib/commons-logging-1.1.1.jar:\
-$(dirname -- "$0")/lib/gson-2.2.2.jar:\
-$(dirname -- "$0")/lib/commons-lang3-3.1.jar \
+lib/molgenis-core-0.0.1-SNAPSHOT.jar:\
+lib/molgenis-compute-core-0.0.1-SNAPSHOT.jar:\
+lib/commons-cli-1.2.jar:\
+lib/commons-io-2.4.jar:\
+lib/freemarker-2.3.18.jar:\
+lib/log4j-1.2.17.jar:\
+lib/opencsv-2.3.jar:\
+lib/httpclient-4.2.5.jar:\
+lib/httpcore-4.2.4.jar:\
+lib/guava-14.0.1.jar:\
+lib/commons-logging-1.1.1.jar:\
+lib/gson-2.2.2.jar:\
+lib/commons-lang3-3.1.jar \
 org.molgenis.compute5.ComputeCommandLine \
 $*
