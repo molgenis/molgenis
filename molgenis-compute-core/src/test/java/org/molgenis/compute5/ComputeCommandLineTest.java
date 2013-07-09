95b1cfbb39b82a29a7cfc624cfeea59caf4b8711
HV Byelas <hvbyelas@MacBook-Pro-van-HV.local>
2013-07-09 18:16:25 +0200

    one more test for parameters

--- molgenis-compute-core/src/test/java/org/molgenis/compute5/ComputeCommandLineTest.java
+++ molgenis-compute-core/src/test/java/org/molgenis/compute5/ComputeCommandLineTest.java
@@ -271,6 +271,55 @@ public class ComputeCommandLineTest
 		}
 	}
 
+	@Test
+	public void testCommandLineParameters3Levels1() throws Exception
+	{
+		System.out.println("--- Start TestCommandLineParametersComputePropertiesFilesCreated ---");
+
+		File f = new File(outputDir);
+		FileUtils.deleteDirectory(f);
+		Assert.assertFalse(f.exists());
+
+		f = new File(".compute.properties");
+		FileUtils.deleteQuietly(f);
+		Assert.assertFalse(f.exists());
+
+		ComputeCommandLine.main(new String[]{
+				"--generate", "--run", "--workflow", "src/main/resources/workflows/benchmark/workflow.csv",
+				"--defaults", "src/main/resources/workflows/benchmark/workflow.defaults.csv",
+				"--parameters","src/main/resources/workflows/benchmark/parameters.3levels1.properties",
+				"--rundir",outputDir,
+				"--run",
+				"--database","none",
+				"--runid", "test1"
+		});
+
+		System.out.println("--- Test Created Files ---");
+
+		File file = new File(outputDir + "/step1_0.sh.started");
+		if (!file.exists())
+		{
+			Assert.fail("step1_0.sh.started is not generated");
+		}
+
+		file = new File(outputDir + "/step2_0.sh.started");
+		if (!file.exists())
+		{
+			Assert.fail("step2_0.sh.started is not generated");
+		}
+
+		file = new File(outputDir + "/step1_0.sh.finished");
+		if (!file.exists())
+		{
+			Assert.fail("step1_0.sh.finished is not generated");
+		}
+
+		file = new File(outputDir + "/step2_0.sh.finished");
+		if (!file.exists())
+		{
+			Assert.fail("step2_0.sh.finished is not generated");
+		}
+	}
 
 
 	@Test
