3bfcfc6299b0b4dd787e83608cfc1718006f5568
dennishendriksen <d.hendriksen@umcg.nl>
2013-08-15 17:01:41 +0200

    move catalog loader service interface to protocol viewer module

---
+++ molgenis-omx-protocolviewer/src/main/java/org/molgenis/omx/catalog/UnknownCatalogException.java
@@ -0,0 +1,27 @@
+package org.molgenis.omx.catalog;
+
+public class UnknownCatalogException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public UnknownCatalogException()
+	{
+		super();
+	}
+
+	public UnknownCatalogException(String message, Throwable t)
+	{
+		super(message, t);
+	}
+
+	public UnknownCatalogException(String message)
+	{
+		super(message);
+	}
+
+	public UnknownCatalogException(Throwable t)
+	{
+		super(t);
+	}
+
+}
