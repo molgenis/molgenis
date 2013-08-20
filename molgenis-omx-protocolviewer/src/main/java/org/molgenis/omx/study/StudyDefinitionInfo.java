c997e4812f0d3a32bd37652a236b8b64db7d27cd
dennishendriksen <d.hendriksen@umcg.nl>
2013-08-20 07:48:56 +0200

    add meta data to study definition info

--- molgenis-omx-protocolviewer/src/main/java/org/molgenis/omx/study/StudyDefinitionInfo.java
+++ molgenis-omx-protocolviewer/src/main/java/org/molgenis/omx/study/StudyDefinitionInfo.java
@@ -1,13 +1,24 @@
 package org.molgenis.omx.study;
 
+import java.util.List;
+
+/**
+ * Study definition meta data
+ * 
+ * @author erwin
+ */
 public class StudyDefinitionInfo
 {
 	private final String id;
 	private final String name;
+	private String description;
+	private String version;
+	private List<String> authors;
 
 	public StudyDefinitionInfo(String id, String name)
 	{
 		if (id == null) throw new IllegalArgumentException("Id is null");
+		if (name == null) throw new IllegalArgumentException("Name is null");
 		this.id = id;
 		this.name = name;
 	}
@@ -20,5 +31,35 @@ public class StudyDefinitionInfo
 	public String getName()
 	{
 		return name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public String getVersion()
+	{
+		return version;
+	}
+
+	public void setVersion(String version)
+	{
+		this.version = version;
+	}
+
+	public List<String> getAuthors()
+	{
+		return authors;
+	}
+
+	public void setAuthors(List<String> authors)
+	{
+		this.authors = authors;
 	}
 }
