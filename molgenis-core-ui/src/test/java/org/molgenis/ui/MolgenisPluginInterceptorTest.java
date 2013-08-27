3056e442219d8c75169c636b108c448a4dc8bf77
dennishendriksen <d.hendriksen@umcg.nl>
2013-08-27 21:57:46 +0200

    add controller security

--- molgenis-core-ui/src/test/java/org/molgenis/ui/MolgenisPluginInterceptorTest.java
+++ molgenis-core-ui/src/test/java/org/molgenis/ui/MolgenisPluginInterceptorTest.java
@@ -4,8 +4,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.molgenis.framework.db.DatabaseAccessException;
 import org.molgenis.framework.security.Login;
+import org.molgenis.framework.server.MolgenisPermissionService;
+import org.molgenis.framework.server.MolgenisPermissionService.Permission;
 import org.molgenis.framework.ui.MolgenisPlugin;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,12 +21,14 @@ import org.testng.annotations.Test;
 public class MolgenisPluginInterceptorTest
 {
 	private Login login;
+	private MolgenisPermissionService permissionService;
 	private MolgenisUi molgenisUi;
 
 	@BeforeMethod
 	public void setUp()
 	{
 		login = mock(Login.class);
+		permissionService = mock(MolgenisPermissionService.class);
 		molgenisUi = mock(MolgenisUi.class);
 	}
 
@@ -31,9 +39,42 @@ public class MolgenisPluginInterceptorTest
 	}
 
 	@Test
+	public void preHandle() throws Exception
+	{
+		String uri = MolgenisPlugin.PLUGIN_URI_PREFIX + "test";
+		MolgenisPlugin molgenisPlugin = new MolgenisPlugin(uri)
+		{
+		};
+		HandlerMethod handlerMethod = mock(HandlerMethod.class);
+		when(handlerMethod.getBean()).thenReturn(molgenisPlugin);
+		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, permissionService,
+				molgenisUi);
+
+		when(permissionService.hasPermissionOnPlugin(molgenisPlugin.getClass(), Permission.READ)).thenReturn(true);
+		assertTrue(molgenisPluginInterceptor.preHandle(mock(HttpServletRequest.class), null, handlerMethod));
+	}
+
+	@Test(expectedExceptions = DatabaseAccessException.class)
+	public void preHandle_accessDenied() throws Exception
+	{
+		String uri = MolgenisPlugin.PLUGIN_URI_PREFIX + "test";
+		MolgenisPlugin molgenisPlugin = new MolgenisPlugin(uri)
+		{
+		};
+		HandlerMethod handlerMethod = mock(HandlerMethod.class);
+		when(handlerMethod.getBean()).thenReturn(molgenisPlugin);
+		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, permissionService,
+				molgenisUi);
+
+		when(permissionService.hasPermissionOnPlugin(molgenisPlugin.getClass(), Permission.READ)).thenReturn(false);
+		molgenisPluginInterceptor.preHandle(mock(HttpServletRequest.class), null, handlerMethod);
+	}
+
+	@Test
 	public void postHandle() throws Exception
 	{
-		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, null, molgenisUi);
+		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, permissionService,
+				molgenisUi);
 		String uri = MolgenisPlugin.PLUGIN_URI_PREFIX + "test";
 		ModelAndView modelAndView = new ModelAndView();
 		HandlerMethod handlerMethod = mock(HandlerMethod.class);
@@ -50,7 +91,8 @@ public class MolgenisPluginInterceptorTest
 	public void postHandle_authenticated() throws Exception
 	{
 		when(login.isAuthenticated()).thenReturn(true);
-		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, null, molgenisUi);
+		MolgenisPluginInterceptor molgenisPluginInterceptor = new MolgenisPluginInterceptor(login, permissionService,
+				molgenisUi);
 		String uri = MolgenisPlugin.PLUGIN_URI_PREFIX + "test";
 		ModelAndView modelAndView = new ModelAndView();
 		HandlerMethod handlerMethod = mock(HandlerMethod.class);
