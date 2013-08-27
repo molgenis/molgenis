ffcd0a94caee38bc8209768aa957c2faf05c1e68
dennishendriksen <d.hendriksen@umcg.nl>
2013-08-27 21:45:33 +0200

    set permissions through permission service, use service in db populator

--- molgenis-core/src/main/java/org/molgenis/framework/server/MolgenisPermissionService.java
+++ molgenis-core/src/main/java/org/molgenis/framework/server/MolgenisPermissionService.java
@@ -1,5 +1,8 @@
 package org.molgenis.framework.server;
 
+import org.molgenis.framework.ui.MolgenisPlugin;
+import org.molgenis.util.Entity;
+
 public interface MolgenisPermissionService
 {
 	public enum Permission
@@ -9,9 +12,17 @@ public interface MolgenisPermissionService
 
 	boolean hasPermissionOnPlugin(String pluginName, Permission permission);
 
+	boolean hasPermissionOnPlugin(Class<? extends MolgenisPlugin> pluginClazz, Permission permission);
+
+	void setPermissionOnPlugin(String pluginName, Integer roleId, Permission permission);
+
+	void setPermissionOnPlugin(Class<? extends MolgenisPlugin> pluginClazz, Integer roleId, Permission permission);
+
 	boolean hasPermissionOnEntity(String entityName, Permission permission);
 
-	// void setPermissionOnPlugin(String pluginName, Permission permission, Integer roleId);
+	boolean hasPermissionOnEntity(Class<? extends Entity> entityClazz, Permission permission);
 
-	// void setPermissionOnEntity(String entityName, Permission permission, Integer roleId);
+	void setPermissionOnEntity(String entityName, Integer roleId, Permission permission);
+
+	void setPermissionOnEntity(Class<? extends Entity> entityClazz, Integer roleId, Permission permission);
 }
