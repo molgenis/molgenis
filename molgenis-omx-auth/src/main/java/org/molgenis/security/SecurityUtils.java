e9dd2aaae74d9b38b630503836e83adb0ce73ee6
dennishendriksen <d.hendriksen@umcg.nl>
2013-09-18 23:09:14 +0200

    fix bug, rename method, add docs

--- molgenis-omx-auth/src/main/java/org/molgenis/security/SecurityUtils.java
+++ molgenis-omx-auth/src/main/java/org/molgenis/security/SecurityUtils.java
@@ -6,21 +6,22 @@ import java.util.Collections;
 import org.molgenis.framework.server.MolgenisPermissionService.Permission;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtils
 {
+	public static final String ANONYMOUS_USERNAME = "anonymousUser";
+
+	public static final String AUTHORITY_SU = "ROLE_SU";
+
 	public static final String AUTHORITY_PLUGIN_PREFIX = "ROLE_PLUGIN_";
 	public static final String AUTHORITY_PLUGIN_READ_PREFIX = AUTHORITY_PLUGIN_PREFIX + Permission.READ + "_";
 	public static final String AUTHORITY_PLUGIN_WRITE_PREFIX = AUTHORITY_PLUGIN_PREFIX + Permission.WRITE + "_";
 	public static final String AUTHORITY_ENTITY_PREFIX = "ROLE_ENTITY_";
 	public static final String AUTHORITY_ENTITY_READ_PREFIX = AUTHORITY_ENTITY_PREFIX + Permission.READ + "_";;
 	public static final String AUTHORITY_ENTITY_WRITE_PREFIX = AUTHORITY_ENTITY_PREFIX + Permission.READ + "_";;
-
-	public static final GrantedAuthority GRANTED_AUTHORITY_SU = new SimpleGrantedAuthority("ROLE_SU");
 
 	public static UserDetails getCurrentUser()
 	{
@@ -46,16 +47,33 @@ public class SecurityUtils
 		else return ((UserDetails) principal).getUsername();
 	}
 
-	public static boolean isUserInRole(String... roles)
+	/**
+	 * Returns whether the current user has at least one of the given roles
+	 * 
+	 * @param roles
+	 * @return
+	 */
+	public static boolean currentUserHasRole(String... roles)
 	{
+		if (roles == null || roles.length == 0) return false;
+
 		Collection<? extends GrantedAuthority> authorities = getCurrentUser().getAuthorities();
 		for (String role : roles)
 		{
-			if (authorities.contains(new SimpleGrantedAuthority(role))) return true;
+			for (GrantedAuthority grantedAuthority : authorities)
+			{
+				if (role.equals(grantedAuthority.getAuthority())) return true;
+			}
+
 		}
 		return false;
 	}
 
+	/**
+	 * Returns whether the current user is a super user
+	 * 
+	 * @return
+	 */
 	public static boolean currentUserIsSu()
 	{
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -66,12 +84,17 @@ public class SecurityUtils
 
 		for (GrantedAuthority authority : authorities)
 		{
-			if (authority.getAuthority().equals(GRANTED_AUTHORITY_SU.getAuthority())) return true;
+			if (authority.getAuthority().equals(AUTHORITY_SU)) return true;
 		}
 
 		return false;
 	}
 
+	/**
+	 * Returns whether the current user is authenticated and not the anonymous user
+	 * 
+	 * @return
+	 */
 	public static boolean currentUserIsAuthenticated()
 	{
 		String username;
@@ -79,13 +102,19 @@ public class SecurityUtils
 		Object principal = authentication.getPrincipal();
 		if (principal instanceof UserDetails) username = ((UserDetails) principal).getUsername();
 		else username = principal.toString();
-		return authentication.isAuthenticated() && !username.equals("anonymousUser");
+		return authentication.isAuthenticated() && !username.equals(ANONYMOUS_USERNAME);
 	}
 
+	/**
+	 * Returns the default (su, read, write) roles related to a plugin
+	 * 
+	 * @param pluginId
+	 * @return
+	 */
 	public static String[] defaultPluginAuthorities(String pluginId)
 	{
 		return new String[]
-		{ GRANTED_AUTHORITY_SU.getAuthority(), getPluginReadAuthority(pluginId), getPluginWriteAuthority(pluginId) };
+		{ AUTHORITY_SU, getPluginReadAuthority(pluginId), getPluginWriteAuthority(pluginId) };
 	}
 
 	private static String getPluginReadAuthority(String pluginId)
