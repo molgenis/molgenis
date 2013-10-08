641abfeb1776d1fa8358b0537dc6f6838345ae02
dennishendriksen <d.hendriksen@umcg.nl>
2013-10-08 12:44:26 +0200

    bug fix: change user password + test

--- molgenis-security/src/main/java/org/molgenis/security/permission/Permission.java
+++ molgenis-security/src/main/java/org/molgenis/security/permission/Permission.java
@@ -24,4 +24,40 @@ public class Permission
 	{
 		this.group = group;
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((group == null) ? 0 : group.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Permission other = (Permission) obj;
+		if (group == null)
+		{
+			if (other.group != null) return false;
+		}
+		else if (!group.equals(other.group)) return false;
+		if (type == null)
+		{
+			if (other.type != null) return false;
+		}
+		else if (!type.equals(other.type)) return false;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Permission [type=" + type + ", group=" + group + "]";
+	}
 }
