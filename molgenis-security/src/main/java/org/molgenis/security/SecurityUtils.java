56a2c5df323c19d03f6079d707eb78086d9e1ffd
dennishendriksen <d.hendriksen@umcg.nl>
2013-11-20 22:06:51 +0100

    bug fix: getCurrentUsername fails if principal is not an instance of UserDetails

--- molgenis-security/src/main/java/org/molgenis/security/SecurityUtils.java
+++ molgenis-security/src/main/java/org/molgenis/security/SecurityUtils.java
@@ -26,9 +26,19 @@ public class SecurityUtils
 	public static String getCurrentUsername()
 	{
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null)
+		{
+			return null;
+		}
 		Object principal = authentication.getPrincipal();
-		if (!(principal instanceof UserDetails)) return ((UserDetails) principal).getUsername();
-		else return ((UserDetails) principal).getUsername();
+		if (principal instanceof UserDetails)
+		{
+			return ((UserDetails) principal).getUsername();
+		}
+		else
+		{
+			return principal.toString();
+		}
 	}
 
 	/**
