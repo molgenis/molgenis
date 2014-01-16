1f57b5f8e523f2e15c3f609e17ccaa304e2e2006
dennishendriksen <d.hendriksen@umcg.nl>
2014-01-16 10:27:44 +0100

    Scheduled GAF list validator/importer

--- molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/DataSetImporterService.java
+++ molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/DataSetImporterService.java
@@ -4,9 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.molgenis.data.Entity;
+import org.molgenis.data.Repository;
 import org.molgenis.omx.converters.ValueConverterException;
 
 public interface DataSetImporterService
 {
 	void importDataSet(File file, List<String> dataSetEntityNames) throws IOException, ValueConverterException;
+
+	void importSheet(Repository<? extends Entity> repo, String sheetName) throws IOException, ValueConverterException;
 }
