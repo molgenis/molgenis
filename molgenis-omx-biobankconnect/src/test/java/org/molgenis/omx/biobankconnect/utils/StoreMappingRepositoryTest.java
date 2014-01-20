8635e1d7e35b1d3f8d3e3aa05aa2f15775691a51
Chao Pang <ChaoPang229@gmail.com>
2014-01-20 13:51:22 +0100

    merged biobankconnect release.1.0 with molgenis master

--- molgenis-omx-biobankconnect/src/test/java/org/molgenis/omx/biobankconnect/utils/StoreMappingRepositoryTest.java
+++ molgenis-omx-biobankconnect/src/test/java/org/molgenis/omx/biobankconnect/utils/StoreMappingRepositoryTest.java
@@ -75,8 +75,7 @@ public class StoreMappingRepositoryTest
 
 		Query q2 = new QueryImpl();
 		q2.eq(ObservationSet.PARTOFDATASET, dataSet);
-		when(dataService.findAllAsList(ObservationSet.ENTITY_NAME, q2)).thenReturn(
-				Arrays.<Entity> asList(observationSet));
+		when(dataService.findAll(ObservationSet.ENTITY_NAME, q2)).thenReturn(Arrays.<Entity> asList(observationSet));
 
 		ObservedValue observedValue1 = mock(ObservedValue.class);
 		when(observedValue1.getObservationSet()).thenReturn(observationSet);
@@ -101,7 +100,7 @@ public class StoreMappingRepositoryTest
 
 		Query q4 = new QueryImpl();
 		q4.in(ObservedValue.OBSERVATIONSET, Arrays.asList(observationSet));
-		when(dataService.findAllAsList(ObservedValue.ENTITY_NAME, q4)).thenReturn(
+		when(dataService.findAll(ObservedValue.ENTITY_NAME, q4)).thenReturn(
 				Arrays.<Entity> asList(observedValue1, observedValue2, observedValue3));
 
 		repository = new StoreMappingRepository(dataSet, dataService);
