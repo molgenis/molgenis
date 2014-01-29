800d140200516554527f98244aec1660ff386264
dennishendriksen <d.hendriksen@umcg.nl>
2014-01-29 17:37:22 +0100

    launch index events outside of transaction

--- molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/OmxImporterServiceImpl.java
+++ molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/OmxImporterServiceImpl.java
@@ -12,10 +12,6 @@ import org.molgenis.data.EntitySource;
 import org.molgenis.framework.db.EntitiesImporter;
 import org.molgenis.framework.db.EntityImportReport;
 import org.molgenis.omx.converters.ValueConverterException;
-import org.molgenis.omx.observ.DataSet;
-import org.molgenis.omx.observ.Protocol;
-import org.molgenis.util.ApplicationContextProvider;
-import org.molgenis.util.EntityImportedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -50,18 +46,6 @@ public class OmxImporterServiceImpl implements OmxImporterService
 					dataSetSheetNames, databaseAction);
 			importReport.addEntityImportReport(dataSetImportReport);
 		}
-
-		// publish dataset imported event(s)
-		Iterable<DataSet> dataSets = dataService.findAll(DataSet.ENTITY_NAME);
-		for (DataSet dataSet : dataSets)
-			ApplicationContextProvider.getApplicationContext().publishEvent(
-					new EntityImportedEvent(this, DataSet.ENTITY_NAME, dataSet.getId()));
-
-		// publish protocol imported event(s)
-		Iterable<Protocol> protocols = dataService.findAll(Protocol.ENTITY_NAME);
-		for (Protocol protocol : protocols)
-			ApplicationContextProvider.getApplicationContext().publishEvent(
-					new EntityImportedEvent(this, Protocol.ENTITY_NAME, protocol.getId()));
 
 		return importReport;
 
