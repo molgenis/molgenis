0c3e6a774b2783eeec161fc4c820ea29f90870c1
Mark-de-Haan <markdehaan90@gmail.com>
2014-02-27 16:00:01 +0100

    Test classes for KEGG and OmimHpo annotators. Under construction. Not finished.

---
+++ molgenis-data-annotators/src/test/java/org/molgenis/data/annotation/impl/OmimHpoAnnotatorTest.java
@@ -0,0 +1,34 @@
+package org.molgenis.data.annotation.impl;
+
+import org.testng.annotations.BeforeMethod;
+
+public class OmimHpoAnnotatorTest
+{
+	@BeforeMethod
+	public void beforeMethod(){
+		
+	}
+	
+//	 public static void main(String [ ] args) throws Exception
+//	    {
+//	        //includes a gene without HGNC symbol, and a gene not related to OMIM/HPO terms
+//	        List<Locus> loci = new ArrayList<Locus>(Arrays.asList(new Locus("2", 58453844l), new Locus("2", 71892329l), new Locus("2", 73679116l), new Locus("10", 112360316l), new Locus("11", 2017661l), new Locus("1", 18151726l), new Locus("1", -1l), new Locus("11", 6637740l)));
+//
+//	        List<Entity> inputs = new ArrayList<Entity>();
+//	        for(Locus l : loci)
+//	        {
+//	            HashMap<String, Object> inputMap = new HashMap<String, Object>();
+//	            inputMap.put(CHROMOSOME, l.getChrom());
+//	            inputMap.put(POSITION, l.getPos());
+//	            inputs.add(new MapEntity(inputMap));
+//	        }
+//
+//	        Iterator<Entity> res = new OmimHpoAnnotator().annotate(inputs.iterator());
+//	        while(res.hasNext())
+//	        {
+//	            System.out.println(res.next().toString());
+//	        }
+//
+//	    }
+
+}
