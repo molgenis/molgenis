f2bec4f237425d49ffe2987881f8d63f06fd3bac
Mark-de-Haan <markdehaan90@gmail.com>
2014-03-03 15:08:43 +0100

    Made it extend from AbstractRepository, canAnnotate now checks for type as well. Made chromosome and position public for unit tests.

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/LocusAnnotator.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/LocusAnnotator.java
@@ -12,11 +12,11 @@ import org.springframework.context.event.ContextRefreshedEvent;
 /**
  * Created by jvelde on 2/13/14.
  */
-public abstract class LocusAnnotator implements RepositoryAnnotator, ApplicationListener<ContextRefreshedEvent>
+public abstract class LocusAnnotator extends AbstractRepositoryAnnotator implements RepositoryAnnotator, ApplicationListener<ContextRefreshedEvent>
 {
 
-	protected static final String CHROMOSOME = "chrom";
-	protected static final String POSITION = "pos";
+	public static final String CHROMOSOME = "chrom";
+	public static final String POSITION = "pos";
 
 	// TODO: needs genome build and possible organism !?
 
@@ -29,21 +29,5 @@ public abstract class LocusAnnotator implements RepositoryAnnotator, Application
 		metadata.addAttributeMetaData(new DefaultAttributeMetaData(POSITION, MolgenisFieldTypes.FieldTypeEnum.LONG));
 
 		return metadata;
-	}
-
-	public boolean canAnnotate(EntityMetaData sourceMetaData)
-	{
-		boolean canAnnotate = true;
-		Iterable<AttributeMetaData> inputAttributes = getInputMetaData().getAttributes();
-		for (AttributeMetaData attribute : inputAttributes)
-		{
-			if (sourceMetaData.getAttribute(attribute.getName()) == null)
-			{
-				// all attributes from the inputmetadata must be present to annotate.
-				canAnnotate = false;
-			}
-		}
-
-		return canAnnotate;
 	}
 }
