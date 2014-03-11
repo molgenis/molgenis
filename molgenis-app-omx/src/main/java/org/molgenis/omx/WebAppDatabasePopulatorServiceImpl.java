bfa01bbbcd97684d86e829d1c40a9b7b61427e68
Mark-de-Haan <markdehaan90@gmail.com>
2014-03-11 14:43:19 +0100

    Runtime properties for every annotator are set using the molgenis.home directory as a base. Every annotator checks individually whether a RTP actually leads to a file.

--- molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
+++ molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
@@ -1,10 +1,15 @@
 package org.molgenis.omx;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.molgenis.data.DataService;
+import org.molgenis.data.annotation.impl.CaddServiceAnnotator;
+import org.molgenis.data.annotation.impl.ClinicalGenomicsDatabaseServiceAnnotator;
+import org.molgenis.data.annotation.impl.DbnsfpGeneServiceAnnotator;
+import org.molgenis.data.annotation.impl.DbnsfpVariantServiceAnnotator;
 import org.molgenis.data.support.QueryImpl;
 import org.molgenis.dataexplorer.controller.DataExplorerController;
 import org.molgenis.framework.db.WebAppDatabasePopulatorService;
@@ -136,6 +141,28 @@ public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulat
 		// Charts include/exclude charts
 		runtimePropertyMap.put(DataExplorerController.KEY_APP_INCLUDE_CHARTS,
 				DataExplorerController.INCLUDE_CHARTS_MODULE + "");
+
+		// Annotators include files/tools
+		String molgenisHomeDir = System.getProperty("molgenis.home");
+		
+		if (molgenisHomeDir == null)
+		{
+			throw new IllegalArgumentException("missing required java system property 'molgenis.home'");
+		}
+
+		if (!molgenisHomeDir.endsWith("/")) molgenisHomeDir = molgenisHomeDir + '/';
+		String molgenisHomeDirAnnotationResources = molgenisHomeDir + "data/annotation_resources";
+
+		runtimePropertyMap.put(CaddServiceAnnotator.TABIX_LOCATION_PROPERTY, molgenisHomeDirAnnotationResources
+				+ "/tabix-0.2.6/tabix");
+		runtimePropertyMap.put(CaddServiceAnnotator.CADD_FILE_LOCATION_PROPERTY, molgenisHomeDirAnnotationResources
+				+ "/CADD/1000G.vcf.gz");
+		runtimePropertyMap.put(ClinicalGenomicsDatabaseServiceAnnotator.CGD_FILE_LOCATION_PROPERTY,
+				molgenisHomeDirAnnotationResources + "/CGD/CGD.txt");
+		runtimePropertyMap.put(DbnsfpGeneServiceAnnotator.GENE_FILE_LOCATION_PROPERTY,
+				molgenisHomeDirAnnotationResources + "/dbnsfp/dbNSFP2.3_gene");
+		runtimePropertyMap.put(DbnsfpVariantServiceAnnotator.CHROMOSOME_FILE_LOCATION_PROPERTY,
+				molgenisHomeDirAnnotationResources + "/dbnsfp/dbNSFP2.3_variant.chr");
 
 		for (Entry<String, String> entry : runtimePropertyMap.entrySet())
 		{
