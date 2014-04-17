fe05fc65afcd3cf549f29609ccc02e5c1922c371
jjettenn <jjettenn@gmail.com>
2014-04-17 13:07:36 +0200

    new MolgenisWebAppInitializer.java is made to reduce code redundancy WebAppInitializer extends MolgenisWebAppInitializer

---
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/MolgenisWebAppInitializer.java
@@ -0,0 +1,52 @@
+package org.molgenis.ui;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MolgenisWebAppInitializer
+{
+	private static final Logger logger = Logger.getLogger(MolgenisWebAppInitializer.class);
+
+	public void onStartup(ServletContext servletContext, Class<? extends WebApplicationInitializer> clazz)
+			throws ServletException
+	{
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(clazz);
+
+		// manage the lifecycle of the root application context
+		servletContext.addListener(new ContextLoaderListener(ctx));
+
+		// spring
+		Dynamic dispatcherServlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		if (dispatcherServlet == null)
+		{
+			logger.warn("ServletContext already contains a complete ServletRegistration for servlet 'dispatcher'");
+		}
+		else
+		{
+			final int maxSize = 32 * 1024 * 1024;
+			dispatcherServlet.setLoadOnStartup(2);
+			dispatcherServlet.addMapping("/");
+			dispatcherServlet.setMultipartConfig(new MultipartConfigElement(null, maxSize, maxSize, maxSize));
+			dispatcherServlet.setInitParameter("dispatchOptionsRequest", "true");
+		}
+
+		// add filters
+		javax.servlet.FilterRegistration.Dynamic etagFilter = servletContext.addFilter("etagFilter",
+				new ShallowEtagHeaderFilter());
+		etagFilter.addMappingForServletNames(null, true, "dispatcher");
+
+		// enable use of request scoped beans in FrontController
+		servletContext.addListener(new RequestContextListener());
+	}
+}
