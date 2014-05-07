206c0c0f6f36a59916961398c3494048711dd13f
mswertz <m.a.swertz@rug.nl>
2014-05-07 20:39:18 +0200

    mref fix?

--- molgenis-data-mysql/src/test/java/org/molgenis/data/mysql/MysqlRepositoryMrefTest.java
+++ molgenis-data-mysql/src/test/java/org/molgenis/data/mysql/MysqlRepositoryMrefTest.java
@@ -143,10 +143,14 @@ public class MysqlRepositoryMrefTest extends MysqlRepositoryAbstractDatatypeTest
         mrefRepo.update(e);
 
         e = mrefRepo.findOne("one");
-        Assert.assertEquals(e.getList("stringRef"),Arrays.asList(new String[]{"ref2","ref3"}));
+        Assert.assertEquals(e.getList("stringRef").size(),2);
+        Assert.assertTrue(e.getList("stringRef").contains("ref2"));
+        Assert.assertTrue(e.getList("stringRef").contains("ref3"));
 
 
-		// verify not null error
+
+
+        // verify not null error
 
 		// verify default
 	}
