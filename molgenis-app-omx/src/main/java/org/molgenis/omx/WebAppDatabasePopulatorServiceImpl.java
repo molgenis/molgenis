d58c0b8a4c1ba5b70e493b7eff87e409e3f98aa7
bartcharbon <bart.charbon@gmail.com>
2014-05-21 11:21:02 +0200

    update RTP genomebrowser and filter wizard

--- molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
+++ molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
@@ -27,12 +27,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulatorService
 {
-	public static final String INITLOCATION = "initLocation";
-	public static final String COORDSYSTEM = "coordSystem";
-	public static final String CHAINS = "chains";
-	public static final String SOURCES = "sources";
-	public static final String BROWSERLINKS = "browserLinks";
-
 	private final DataService dataService;
 	private final MolgenisSecurityWebAppDatabasePopulatorService molgenisSecurityWebAppDatabasePopulatorService;
 
@@ -59,20 +53,20 @@ public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulat
 		// Genomebrowser stuff
 		Map<String, String> runtimePropertyMap = new HashMap<String, String>();
 
-		runtimePropertyMap.put(INITLOCATION,
+		runtimePropertyMap.put(DataExplorerController.INITLOCATION,
 				"chr:'1',viewStart:10000000,viewEnd:10100000,cookieKey:'human',nopersist:true");
-		runtimePropertyMap.put(COORDSYSTEM,
+		runtimePropertyMap.put(DataExplorerController.COORDSYSTEM,
 				"{speciesName: 'Human',taxon: 9606,auth: 'GRCh',version: '37',ucscName: 'hg19'}");
 		runtimePropertyMap
-				.put(CHAINS,
+				.put(DataExplorerController.CHAINS,
 						"{hg18ToHg19: new Chainset('http://www.derkholm.net:8080/das/hg18ToHg19/', 'NCBI36', 'GRCh37',{speciesName: 'Human',taxon: 9606,auth: 'NCBI',version: 36,ucscName: 'hg18'})}");
 		// for use of the demo dataset add to
 		// SOURCES:",{name:'molgenis mutations',uri:'http://localhost:8080/das/molgenis/',desc:'Default from WebAppDatabasePopulatorService'}"
 		runtimePropertyMap
-				.put(SOURCES,
+				.put(DataExplorerController.SOURCES,
 						"[{name:'Genome',twoBitURI:'http://www.biodalliance.org/datasets/hg19.2bit',tier_type: 'sequence'},{name: 'Genes',desc: 'Gene structures from GENCODE 19',bwgURI: 'http://www.biodalliance.org/datasets/gencode.bb',stylesheet_uri: 'http://www.biodalliance.org/stylesheets/gencode.xml',collapseSuperGroups: true,trixURI:'http://www.biodalliance.org/datasets/geneIndex.ix'},{name: 'Repeats',desc: 'Repeat annotation from Ensembl 59',bwgURI: 'http://www.biodalliance.org/datasets/repeats.bb',stylesheet_uri: 'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},{name: 'Conservation',desc: 'Conservation',bwgURI: 'http://www.biodalliance.org/datasets/phastCons46way.bw',noDownsample: true}]");
 		runtimePropertyMap
-				.put(BROWSERLINKS,
+				.put(DataExplorerController.BROWSERLINKS,
 						"{Ensembl: 'http://www.ensembl.org/Homo_sapiens/Location/View?r=${chr}:${start}-${end}',UCSC: 'http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=chr${chr}:${start}-${end}',Sequence: 'http://www.derkholm.net:8080/das/hg19comp/sequence?segment=${chr}:${start},${end}'}");
 
 		// include/exclude dataexplorer mods
