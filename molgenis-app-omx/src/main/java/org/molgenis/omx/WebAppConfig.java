f5d064452b7b47128a4ae6995a0758bf246f4b51
erwinwinder <j.a.winder@umcg.nl>
2014-05-27 13:29:51 +0200

    R api

--- molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppConfig.java
+++ molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppConfig.java
@@ -1,5 +1,6 @@
 package org.molgenis.omx;
 
+import java.util.List;
 import java.util.Map;
 
 import org.molgenis.DatabaseConfig;
@@ -7,6 +8,7 @@ import org.molgenis.catalogmanager.CatalogManagerService;
 import org.molgenis.data.DataService;
 import org.molgenis.dataexplorer.freemarker.DataExplorerHyperlinkDirective;
 import org.molgenis.elasticsearch.config.EmbeddedElasticSearchConfig;
+import org.molgenis.messageconverter.CsvHttpMessageConverter;
 import org.molgenis.omx.catalogmanager.OmxCatalogManagerService;
 import org.molgenis.omx.config.DataExplorerConfig;
 import org.molgenis.omx.studymanager.OmxStudyManagerService;
@@ -20,6 +22,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -59,4 +62,10 @@ public class WebAppConfig extends MolgenisWebAppConfig
 				dataService));
 	}
 
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
+	{
+		super.configureMessageConverters(converters);
+		converters.add(new CsvHttpMessageConverter());
+	}
 }
