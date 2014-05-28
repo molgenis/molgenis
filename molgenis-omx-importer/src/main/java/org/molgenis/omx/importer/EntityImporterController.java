72924264c5d02a4641894cb10eb0353d07a298c0
bartcharbon <bart.charbon@gmail.com>
2014-05-28 08:15:55 +0200

    updated validator

--- molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/EntityImporterController.java
+++ molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/EntityImporterController.java
@@ -2,12 +2,14 @@ package org.molgenis.omx.importer;
 
 import org.molgenis.data.*;
 import org.molgenis.data.support.FileRepositoryCollection;
+import org.molgenis.framework.db.EntitiesValidationReport;
 import org.molgenis.framework.db.EntityImporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by charbonb on 22/05/14.
@@ -15,23 +17,24 @@ import java.io.File;
 @Component
 public class EntityImporterController
 {
-    FileRepositoryCollectionFactory fileRepositoryCollectionFactory;
-    EntityImporterFactory entityImporterFactory;
+    private FileRepositoryCollectionFactory fileRepositoryCollectionFactory;
+    private EntityImporterFactory entityImporterFactory;
+    private EntityImporterValidator validator;
 
     @Autowired
-    public EntityImporterController(EntityImporterFactory entityImporterFactory, FileRepositoryCollectionFactory fileRepositoryCollectionFactory){
+    public EntityImporterController(EntityImporterFactory entityImporterFactory, FileRepositoryCollectionFactory fileRepositoryCollectionFactory, EntityImporterValidator validator){
         this.entityImporterFactory = entityImporterFactory;
         this.fileRepositoryCollectionFactory = fileRepositoryCollectionFactory;
+        this.validator = validator;
     }
 
-    public ValidationReport handleImportRequest(File file, DatabaseAction dba)
-	{
+    public EntitiesValidationReport handleImportRequest(File file, DatabaseAction dba) throws IOException {
         FileRepositoryCollection collection = fileRepositoryCollectionFactory.createFileRepositoryCollection(file);
-        EntityImporterValidator validator = new EntityImporterValidator();
+
         EntityImporterDependencyResolver dependencyResolver = new EntityImporterDependencyResolver();
 
-		ValidationReport report = validator.validate(collection, dba);
-		if (report.isValid())
+        EntitiesValidationReport report = validator.validate(file, dba);
+		if (!report.getSheetsImportable().containsValue(false))
 		{
 			dependencyResolver.sort(collection);
 			for (String inEntityName : collection.getEntityNames())
--- molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/EntityImporterValidator.java
+++ molgenis-omx-importer/src/main/java/org/molgenis/omx/importer/EntityImporterValidator.java
@@ -1,15 +1,54 @@
 package org.molgenis.omx.importer;
 
 import org.molgenis.data.DatabaseAction;
+import org.molgenis.data.FileRepositoryCollectionFactory;
 import org.molgenis.data.Repository;
 import org.molgenis.data.RepositoryCollection;
+import org.molgenis.data.importer.EmxImportServiceImpl;
+import org.molgenis.data.importer.EmxImporterService;
+import org.molgenis.data.importer.EntitiesValidationReportImpl;
+import org.molgenis.data.vcf.VcfRepositoryCollection;
+import org.molgenis.framework.db.EntitiesValidationReport;
+import org.molgenis.framework.db.EntitiesValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by charbonb on 22/05/14.
  */
+@Component
 public class EntityImporterValidator {
 
-    ValidationReport validate(RepositoryCollection collection, DatabaseAction action){
-         return new ValidationReport();
+    @Autowired
+    EntitiesValidator validator;
+    @Autowired
+    private FileRepositoryCollectionFactory fileRepositoryCollectionFactory;
+
+    EntitiesValidationReport validate(File file, DatabaseAction action) throws IOException {
+        EntitiesValidationReport validationReport = new EntitiesValidationReportImpl();
+
+        RepositoryCollection repositoryCollection = fileRepositoryCollectionFactory
+                .createFileRepositoryCollection(file);
+        if (repositoryCollection instanceof VcfRepositoryCollection)
+        {
+            // TODO: validate VCF?
+            validationReport.getSheetsImportable().put(StringUtils.stripFilenameExtension(file.getName()), true);
+        }
+        // MySQLRepository
+        else if (repositoryCollection.getRepositoryByEntityName("attributes") != null)
+        {
+            EmxImporterService importer = new EmxImportServiceImpl();
+            validationReport = importer.validateImport(repositoryCollection);
+        }
+        // JPARepository
+        else
+        {
+            validationReport = validator.validate(file);
+        }
+        return validationReport;
     }
 }
