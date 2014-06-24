a5c6aa42bd778ad87956433d6469182d4ed77c49
erwinwinder <j.a.winder@umcg.nl>
2014-06-24 14:29:11 +0200

    Use embedded Mysql for tests

--- molgenis-data-mysql/src/test/java/org/molgenis/AppConfig.java
+++ molgenis-data-mysql/src/test/java/org/molgenis/AppConfig.java
@@ -1,10 +1,9 @@
 package org.molgenis;
 
-import java.beans.PropertyVetoException;
-
 import javax.sql.DataSource;
 
 import org.molgenis.data.DataService;
+import org.molgenis.data.mysql.EmbeddedMysqlDatabaseBuilder;
 import org.molgenis.data.mysql.MysqlRepository;
 import org.molgenis.data.mysql.MysqlRepositoryCollection;
 import org.molgenis.data.support.DataServiceImpl;
@@ -18,8 +17,6 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 /**
  * Database configuration
  */
@@ -28,38 +25,10 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @ComponentScan("org.molgenis.data")
 public class AppConfig
 {
-	private final String dbDriverClass = "com.mysql.jdbc.Driver";
-	private final String dbJdbcUri = "jdbc:mysql://localhost:3306/omx?rewriteBatchedStatements=true";
-	private final String dbUser = "molgenis";
-	private final String dbPassword = "molgenis";
-
-	@Bean
+	@Bean(destroyMethod = "shutdown")
 	public DataSource dataSource()
 	{
-		if (dbDriverClass == null) throw new IllegalArgumentException("db_driver is null");
-		if (dbJdbcUri == null) throw new IllegalArgumentException("db_uri is null");
-		if (dbUser == null) throw new IllegalArgumentException(
-				"please configure the db_user property in your molgenis-server.properties");
-		if (dbPassword == null) throw new IllegalArgumentException(
-				"please configure the db_password property in your molgenis-server.properties");
-
-		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-		try
-		{
-			dataSource.setDriverClass(dbDriverClass);
-		}
-		catch (PropertyVetoException e)
-		{
-			throw new RuntimeException(e);
-		}
-		dataSource.setJdbcUrl(dbJdbcUri);
-		dataSource.setUser(dbUser);
-		dataSource.setPassword(dbPassword);
-		dataSource.setMinPoolSize(5);
-		dataSource.setMaxPoolSize(200);
-		dataSource.setTestConnectionOnCheckin(true);
-		dataSource.setIdleConnectionTestPeriod(120);
-		return dataSource;
+		return new EmbeddedMysqlDatabaseBuilder().build();
 	}
 
 	@Bean
