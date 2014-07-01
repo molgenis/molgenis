3a336981c5e85764a9366e4c4bafc4888d12ea0b
Chao Pang <ChaoPang229@gmail.com>
2014-07-01 14:25:01 +0200

    created the algorithm the prioritize the mapping results based on the parent-child relations

--- molgenis-omx-biobankconnect/src/main/resources/js/ontology-service-result.js
+++ molgenis-omx-biobankconnect/src/main/resources/js/ontology-service-result.js
@@ -35,35 +35,98 @@
 			var layoutDiv = $('<div />').addClass('row-fluid');
 			var termDiv = $('<div />').addClass('span4').append(entity.term).appendTo(layoutDiv);
 			var ontologyTermMatchDiv= $('<div />').addClass('span8 div-expandable').appendTo(layoutDiv);
+			
+			//Collect all the scores from the candidate ontology term mappings
 			var scoreGroup = [];
 			$.each(entity.results.searchHits, function(index, hit){
-				if(index >= 20) return false;
-				var ontologyTermName = $('<div />').addClass('span5 matchterm show-popover').css('margin-bottom', '-6px').append(hit.columnValueMap.ontologyTerm);
-				var ontologyTermUrl = $('<div />').addClass('span5 termurl').css('margin-bottom', '-6px').append('<a href="' + hit.columnValueMap.ontologyTermIRI + '" target="_blank">' + hit.columnValueMap.ontologyTermIRI + '</a>');
-				var matchScore = $('<div />').addClass('span2').css('margin-bottom', '-6px').append('<center>' + hit.columnValueMap.combinedScore.toFixed(2) + '</center>');
-				$('<div />').addClass('row-fluid').append(ontologyTermName).append(ontologyTermUrl).append(matchScore).appendTo(ontologyTermMatchDiv);
 				scoreGroup.push(parseFloat(hit.columnValueMap.combinedScore.toFixed(2)));
-				ontologyTermName.popover({
+			});
+			
+			//Get the index at which two groups are separated statistically based on the scores
+			var separateLineIndex = -1;
+			if(scoreGroup.length > 2){
+				var classifications = ss.jenks(scoreGroup, 2);	
+				separateLineIndex = scoreGroup.indexOf(classifications[1]);
+			}
+			
+			var parentChildMap = {};
+			var parentChildData = {};
+			//Re-arrange the most likely ontology term mappings based on the algorithm that considers the parent-child relations
+			$.each(entity.results.searchHits, function(index, hit){
+				if(separateLineIndex < index + 1) return false;
+				parentChildData[hit.columnValueMap.nodePath] = [];
+				parentChildMap[hit.columnValueMap.nodePath] = index;
+			});
+			
+			$.each(entity.results.searchHits, function(index, hit){
+				if((index + 1) > separateLineIndex) return false;
+				var nodePath = hit.columnValueMap.nodePath;
+				var parentNodePath = hit.columnValueMap.parentNodePath;
+				if(parentChildData[parentNodePath]){
+					parentChildData[parentNodePath].push(hit);
+					delete parentChildData[nodePath]; 
+					// remember the index of the candidate mappings
+					if(parentChildMap[parentNodePath] > index) parentChildMap[parentNodePath] = index;
+					delete parentChildMap[nodePath]; 
+				}
+			});
+			//Create the html visualizations for the mappings
+			$.each(entity.results.searchHits, function(index, hit){
+				if(index >= 20) return false;
+				var ontologyTermNameDiv = $('<div />').addClass('span5 matchterm show-popover').css('margin-bottom', '-6px').append(hit.columnValueMap.ontologyTerm);
+				var ontologyTermUrlDiv = $('<div />').addClass('span5 termurl').css('margin-bottom', '-6px').append('<a href="' + hit.columnValueMap.ontologyTermIRI + '" target="_blank">' + hit.columnValueMap.ontologyTermIRI + '</a>');
+				var matchScoreDiv = $('<div />').addClass('span2').css('margin-bottom', '-6px').append('<center>' + hit.columnValueMap.combinedScore.toFixed(2) + '</center>');
+				var newLineDiv = $('<div />').addClass('row-fluid').append(ontologyTermNameDiv).append(ontologyTermUrlDiv).append(matchScoreDiv);
+				var popoverOption = {
 					'placement' : 'bottom',
 					'trigger' : 'hover',
 					'title' : 'Look up in ontology',
+					'html' : true,
 					'content' : 'Click to inspect the ontology term in the tree' 
-				}).click(function(){
+				};
+				
+				var nodePath = hit.columnValueMap.nodePath;
+				if(parentChildMap[nodePath] !== undefined && parentChildMap[nodePath] !== null && parentChildData[nodePath].length > 2){
+					if(ontologyTermMatchDiv.children('div.row-fluid:eq(' + parentChildMap[nodePath] + ')').length > 0){
+						ontologyTermMatchDiv.children('div.row-fluid:eq(' + parentChildMap[nodePath] + ')').before(newLineDiv);
+					}else{
+						ontologyTermMatchDiv.append(newLineDiv);
+					}
+					var newScore = 0;
+					$.each(parentChildData[nodePath], function(index, childHit){
+						popoverOption.content += '<u>' + childHit.columnValueMap.ontologyTerm + '</u>, ';
+						newScore = newScore + childHit.columnValueMap.combinedScore;
+					});
+					if((newScore / parentChildData[nodePath].length) > hit.columnValueMap.combinedScore){
+						popoverOption.content = 'Order has been re-arranged for this ontology term because the subclasses, ';
+						newLineDiv.find('div:last>center').html((newScore / parentChildData[nodePath].length).toFixed(2) + ' (' + hit.columnValueMap.combinedScore.toFixed(2) + ')');
+						newLineDiv.find('div:first').css('color', '#82108C');
+						popoverOption.content += ' are also selected as candidates, therefore suggests the parent class is likely to be the match!';
+						matchScoreDiv.addClass('show-popover').popover({
+							'placement' : 'bottom',
+							'trigger' : 'hover',
+							'title' : 'Score explanation',
+							'content' : 'Score is re-calculated as the average score of multiple matched child ontology terms.' 
+						});
+					}
+				}else{					
+					ontologyTermMatchDiv.append(newLineDiv);
+				}
+				
+				ontologyTermNameDiv.popover(popoverOption).click(function(){
 					ontologyTree.locateTerm(hit.columnValueMap);
 				});
-			});
-			if(scoreGroup.length > 2){
-				var classifications = ss.jenks(scoreGroup, 2);	
-				var index = scoreGroup.indexOf(classifications[1]);
-				if(index !== scoreGroup.length - 1){
+				
+				if((index + 1) == separateLineIndex){
 					var separatLine = $('<legend />').css({
 						'padding-top':'15px',
 						'padding-bottom':'5px',
 						'border-bottom-color':'#CC0025'
 					});
-					ontologyTermMatchDiv.children('div.row-fluid:eq(' + (index - 1) + ')').append(separatLine);
+					$('<div />').addClass('row-fluid').append(separatLine).appendTo(ontologyTermMatchDiv);
 				}
-			}
+			});
+			
 			var divWithColor = $('<div />').addClass('span12 well div-hover').append(layoutDiv);
 			$('<div />').addClass('row-fluid').append(divWithColor).appendTo(container);
 		});
