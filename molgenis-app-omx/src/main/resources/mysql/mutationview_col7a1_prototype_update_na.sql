9ab9b73ab23b19d8cf24b8c297f964ca5f9f7db1
tommydeboer <tommydeboer4@gmail.com>
2014-07-03 13:52:53 +0200

    removed unexplained col7a1 introduced related files

--- molgenis-app-omx/src/main/resources/mysql/mutationview_col7a1_prototype_update_na.sql
+++
@@ -1,4 +0,0 @@
-UPDATE import_mutationsview 
-SET `Other Mutation ID`='N/A', `Mutation ID`='+ N/A' 
-WHERE (`Other Mutation ID` = 'unknown' OR `Mutation ID` = '+ unknown') 
-AND Phenotype LIKE 'DDEB%';
