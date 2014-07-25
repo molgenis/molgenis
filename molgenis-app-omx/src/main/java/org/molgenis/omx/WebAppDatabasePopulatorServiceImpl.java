95b221aa0dc32f191c892561d0b296df633182ff
Mark-de-Haan <markdehaan90@gmail.com>
2014-07-25 14:58:53 +0200

    Added runtime property for regex which is applied to the searchQuery box in the data explorer

--- molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
+++ molgenis-app-omx/src/main/java/org/molgenis/omx/WebAppDatabasePopulatorServiceImpl.java
@@ -80,9 +80,14 @@ public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulat
 		// DataExplorer table editable yes/no
 		runtimePropertyMap.put(DataExplorerController.KEY_DATAEXPLORER_EDITABLE, String.valueOf(false));
 		runtimePropertyMap.put(DataExplorerController.KEY_GALAXY_ENABLED, String.valueOf(false));
-		
+
 		// DataExplorer rows clickable yes / no
 		runtimePropertyMap.put(DataExplorerController.KEY_DATAEXPLORER_ROW_CLICKABLE, String.valueOf(false));
+
+		// Regex which is applied on the searchQuery box in the data explorer. Allows the submission of chr:pos and
+		// chr:pos-pos which applies a filter to chromosome and position columns when present in data set
+		runtimePropertyMap.put(DataExplorerController.KEY_SEARCHQUERY_REGEX,
+				"^\\s*(?:chr)?([\\d]{1,2}|X|Y|MT|XY):([\\d]+)(?:-([\\d]+)+)?\\s*");
 
 		// Annotators include files/tools
 		String molgenisHomeDir = System.getProperty("molgenis.home");
