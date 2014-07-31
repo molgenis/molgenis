49666453b005d7390cee67ed6bdb8b2f4dc51d79
Mark-de-Haan <markdehaan90@gmail.com>
2014-07-31 11:07:05 +0200

    Created a method that returns a boolean on the check if the current user has permissions on a requested entity

--- molgenis-data/src/main/java/org/molgenis/util/SecurityDecoratorUtils.java
+++ molgenis-data/src/main/java/org/molgenis/util/SecurityDecoratorUtils.java
@@ -17,4 +17,18 @@ public class SecurityDecoratorUtils
 		}
 	}
 
+	public static boolean isPermissionValid(String entityName, Permission permission)
+	{
+		String role = String.format("ROLE_ENTITY_%s_%s", permission.toString(), entityName.toUpperCase());
+		if (!currentUserHasRole("ROLE_SU", "ROLE_SYSTEM", role))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+
+	}
+
 }
