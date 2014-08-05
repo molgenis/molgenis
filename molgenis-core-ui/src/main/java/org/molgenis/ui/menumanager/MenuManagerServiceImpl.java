b65aa076d7de7da30ec7f864834259e6a9b22b5a
dennishendriksen <d.hendriksen@umcg.nl>
2014-08-05 14:17:35 +0200

    Add application initializer to set default menu in case none available

--- molgenis-core-ui/src/main/java/org/molgenis/ui/menumanager/MenuManagerServiceImpl.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/menumanager/MenuManagerServiceImpl.java
@@ -1,18 +1,33 @@
 package org.molgenis.ui.menumanager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.molgenis.framework.server.MolgenisSettings;
 import org.molgenis.framework.ui.MolgenisPlugin;
 import org.molgenis.framework.ui.MolgenisPluginRegistry;
+import org.molgenis.ui.MenuType;
+import org.molgenis.ui.Molgenis;
+import org.molgenis.ui.PluginType;
+import org.molgenis.ui.XmlMolgenisUiLoader;
 import org.molgenis.ui.menu.Menu;
+import org.molgenis.ui.menu.MenuItem;
+import org.molgenis.ui.menu.MenuItemType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.google.gson.GsonBuilder;
 
-public class MenuManagerServiceImpl implements MenuManagerService
+public class MenuManagerServiceImpl implements MenuManagerService, ApplicationListener<ContextRefreshedEvent>
 {
-	private static final String KEY_MOLGENIS_MENU = "molgenis.menu";
+	private static final Logger logger = Logger.getLogger(MenuManagerServiceImpl.class);
+
+	public static final String KEY_MOLGENIS_MENU = "molgenis.menu";
 
 	private final MolgenisSettings molgenisSettings;
 	private final MolgenisPluginRegistry molgenisPluginRegistry;
@@ -54,5 +69,81 @@ public class MenuManagerServiceImpl implements MenuManagerService
 	{
 		String menuJson = new GsonBuilder().create().toJson(molgenisMenu);
 		molgenisSettings.setProperty(KEY_MOLGENIS_MENU, menuJson);
+	}
+
+	/**
+	 * Backwards compatibility: load default menu on application startup if no menu exists
+	 * 
+	 * @param event
+	 */
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event)
+	{
+		if (!molgenisSettings.propertyExists(KEY_MOLGENIS_MENU))
+		{
+			Molgenis molgenis;
+			try
+			{
+				molgenis = new XmlMolgenisUiLoader().load();
+			}
+			catch (IOException e)
+			{
+				// default menu does not exist, no op
+				return;
+			}
+
+			logger.info("Creating default menu from XML");
+			loadDefaultMenu(molgenis);
+		}
+	}
+
+	private void loadDefaultMenu(Molgenis molgenis)
+	{
+		Menu molgenisMenu = new Menu();
+		parseDefaultMenuRec(molgenisMenu, molgenis.getMenu());
+		saveMenu(molgenisMenu);
+	}
+
+	private void parseDefaultMenuRec(MenuItem menuItem, Object defaultMenuObj)
+	{
+		if (defaultMenuObj instanceof MenuType)
+		{
+			MenuType menuType = (MenuType) defaultMenuObj;
+			menuItem.setId(menuType.getName());
+			menuItem.setLabel(menuType.getLabel());
+			menuItem.setType(MenuItemType.MENU);
+
+			List<Object> defaultMenuItems = menuType.getMenuOrPlugin();
+			if (defaultMenuItems != null)
+			{
+				List<MenuItem> items = new ArrayList<MenuItem>(defaultMenuItems.size());
+				for (Object defaultSubMenuObj : defaultMenuItems)
+				{
+					MenuItem subMenuItem = new MenuItem();
+					parseDefaultMenuRec(subMenuItem, defaultSubMenuObj);
+					items.add(subMenuItem);
+				}
+				menuItem.setItems(items);
+			}
+		}
+		else if (defaultMenuObj instanceof PluginType)
+		{
+			PluginType pluginType = (PluginType) defaultMenuObj;
+			menuItem.setId(pluginType.getId());
+			menuItem.setLabel(pluginType.getName());
+			menuItem.setType(MenuItemType.PLUGIN);
+
+			String url = pluginType.getUrl();
+			int idx;
+			if ((idx = url.indexOf('?')) != -1)
+			{
+				String params = url.substring(idx + 1);
+				menuItem.setParams(params);
+			}
+		}
+		else
+		{
+			throw new RuntimeException("Unknown menu object class [" + defaultMenuObj.getClass().getName() + "]");
+		}
 	}
 }
