3ade8c7517a4f5339920f0d9ae7f5dd73606a431
Chao Pang <ChaoPang229@gmail.com>
2014-08-07 00:03:43 +0200

    fixed ElasticSearch QueryGenerator to adapt all palaga cases

---
+++ molgenis-search-elasticsearch/src/main/java/org/molgenis/elasticsearch/request/QueryGeneratorHelper.java
@@ -0,0 +1,228 @@
+package org.molgenis.elasticsearch.request;
+
+import static org.molgenis.data.QueryRule.Operator.AND;
+import static org.molgenis.data.QueryRule.Operator.NESTED;
+import static org.molgenis.data.QueryRule.Operator.OR;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.elasticsearch.index.query.BaseQueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.molgenis.MolgenisFieldTypes;
+import org.molgenis.data.EntityMetaData;
+import org.molgenis.data.QueryRule;
+import org.molgenis.data.QueryRule.Operator;
+
+public class QueryGeneratorHelper
+{
+	private final EntityMetaData entityMetaData;
+	private final LinkedHashMap<BaseQueryBuilder, Operator> baseQueryCollection;
+	private final List<QueryRule> queryRules;
+	private final List<QueryRule> mrefRules;
+
+	public QueryGeneratorHelper(List<QueryRule> rules, EntityMetaData metaData)
+	{
+		if (rules.size() == 0) throw new RuntimeException("The queryRules cannot be empty : " + rules);
+		baseQueryCollection = new LinkedHashMap<BaseQueryBuilder, Operator>(rules.size());
+		mrefRules = new ArrayList<QueryRule>();
+		queryRules = rules;
+		entityMetaData = metaData;
+	}
+
+	public BaseQueryBuilder generateQuery()
+	{
+		StringBuilder mrefField = new StringBuilder();
+
+		for (QueryRule queryRule : queryRules)
+		{
+			// Jump to next round if the queryRule is just logic operators
+			if (queryRule.getOperator().equals(AND) || queryRule.getOperator().equals(OR)) continue;
+
+			// Take special care of NESTED operator
+			if (queryRule.getOperator().equals(NESTED))
+			{
+				QueryGeneratorHelper helper = new QueryGeneratorHelper(queryRule.getNestedRules(), entityMetaData);
+				addQueryToCollection(queryRule, helper.generateQuery());
+			}
+			else if (isMref(queryRule))
+			{
+				// Initialize the mrefField the first time
+				if (mrefField.length() == 0) mrefField.append(queryRule.getField());
+
+				// Encounter a new mref queryRule add Operator between them to
+				// the rules
+				if (mrefField.toString().equals(queryRule.getField()))
+				{
+					int index = queryRules.indexOf(queryRule);
+					if (index > 0 && validateOperator(queryRules.get(index - 1)))
+					{
+						mrefRules.add(queryRules.get(index - 1));
+					}
+				}
+				else
+				{
+					// Deal with the case where the next mref does not have the
+					// same
+					// field name as current mref queryRule
+					addNestedQueryToCollection();
+				}
+				mrefField.delete(0, mrefField.length()).append(queryRule.getField());
+				mrefRules.add(queryRule);
+			}
+			else
+			{
+				// Generate nesteQuery for MREF if there are any
+				// queryRules left
+				// in mrefRules list.
+				addNestedQueryToCollection();
+				mrefField.delete(0, mrefField.length());
+
+				addQueryToCollection(queryRule,
+						QueryBuilders.queryString(LuceneQueryStringBuilder.buildQueryString(Arrays.asList(queryRule))));
+			}
+		}
+		// Generate nesteQuery for MREF if there are any
+		// queryRules left
+		// in mrefRules list.
+		addNestedQueryToCollection();
+
+		return combineQueryBuilders();
+	}
+
+	/**
+	 * A helper function to generate the ElasticSearch NestetQueryBuilder object
+	 * for the collected MREFs and add it to the queryBuilder collection
+	 */
+	private void addNestedQueryToCollection()
+	{
+		if (mrefRules.size() > 0)
+		{
+			String path = mrefRules.get(0).getField();
+			StringBuilder queryStringBuilder = new StringBuilder();
+			queryStringBuilder.append('(');
+			for (QueryRule rule : mrefRules)
+			{
+				if (rule.getField() == null)
+				{
+					queryStringBuilder.append(' ').append(rule.getOperator().toString()).append(' ');
+				}
+				else
+				{
+					EntityMetaData refEntity = entityMetaData.getAttribute(rule.getField()).getRefEntity();
+					String nestedField = rule.getField() + "." + refEntity.getLabelAttribute().getName();
+					queryStringBuilder.append(nestedField).append(':').append("\"").append(rule.getValue())
+							.append("\"");
+				}
+			}
+			queryStringBuilder.append(')');
+			addQueryToCollection(mrefRules.get(0),
+					QueryBuilders.nestedQuery(path, QueryBuilders.queryString(queryStringBuilder.toString())));
+			mrefRules.clear();
+		}
+	}
+
+	/**
+	 * A helper function to add BaseQueryBuilder to the queryBuilder collection
+	 * as well as the connected Operator (AND/OR)
+	 * 
+	 * @param queryRule
+	 * @param queryBuilder
+	 */
+	private void addQueryToCollection(QueryRule queryRule, BaseQueryBuilder queryBuilder)
+	{
+		// Initialize the Operator with 'AND' always
+		QueryRule operator = new QueryRule(AND);
+		int index = queryRules.indexOf(queryRule);
+		// if the index is 1, check the next possible operator
+		if (index == 0)
+		{
+			if (queryRules.size() > 1)
+			{
+				operator = queryRules.get(1);
+			}
+		}
+		else
+		{
+			operator = queryRules.get(index - 1);
+		}
+
+		if (validateOperator(operator))
+		{
+			baseQueryCollection.put(queryBuilder, operator.getOperator());
+		}
+	}
+
+	/**
+	 * The logic operator can only be AND/OR, otherwise a RuntimeException will
+	 * be thrown
+	 * 
+	 * @param connectedOperator
+	 * @return
+	 */
+	private boolean validateOperator(QueryRule connectedOperator)
+	{
+		if (connectedOperator.getOperator() != null
+				&& (connectedOperator.getOperator().equals(AND) || connectedOperator.getOperator().equals(OR)))
+		{
+			return true;
+		}
+		else
+		{
+			throw new RuntimeException("The query operator is not valid : " + connectedOperator.toString());
+		}
+
+	}
+
+	/**
+	 * A helper function that checks if the attribute field is a MREF field
+	 * 
+	 * @param queryRule
+	 * @return
+	 */
+	private boolean isMref(QueryRule queryRule)
+	{
+		if (entityMetaData == null || queryRule.getField() == null) return false;
+
+		return entityMetaData.getAttribute(queryRule.getField()).getDataType().getEnumType().toString()
+				.equalsIgnoreCase(MolgenisFieldTypes.MREF.toString());
+	}
+
+	/**
+	 * Helper class to generate a ElasticSearch BaseQueryBuilder Object based on
+	 * previously collected QueryBuilders. Create a BoolQueryBuilder only if
+	 * there are more than one queryBuilder in the collection, otherwise only
+	 * return the first QueryBuilder.
+	 * 
+	 * @return
+	 */
+	private BaseQueryBuilder combineQueryBuilders()
+	{
+		if (baseQueryCollection.size() == 1)
+		{
+			Iterator<BaseQueryBuilder> iterator = baseQueryCollection.keySet().iterator();
+			return iterator.next();
+		}
+		else
+		{
+			BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+			for (Entry<BaseQueryBuilder, Operator> entry : baseQueryCollection.entrySet())
+			{
+				if (entry.getValue().equals(AND))
+				{
+					boolQueryBuilder.must(entry.getKey());
+				}
+				else if (entry.getValue().equals(OR))
+				{
+					boolQueryBuilder.should(entry.getKey());
+				}
+			}
+			return boolQueryBuilder;
+		}
+	}
+}
