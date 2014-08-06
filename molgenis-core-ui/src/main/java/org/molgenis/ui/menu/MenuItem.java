213fead32579ddcee37a19cf7c0a7ade0bae4718
dennishendriksen <d.hendriksen@umcg.nl>
2014-08-06 09:16:39 +0200

    add tests

--- molgenis-core-ui/src/main/java/org/molgenis/ui/menu/MenuItem.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/menu/MenuItem.java
@@ -77,4 +77,48 @@ public class MenuItem
 	{
 		this.items = items;
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		result = prime * result + ((params == null) ? 0 : params.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		MenuItem other = (MenuItem) obj;
+		if (id == null)
+		{
+			if (other.id != null) return false;
+		}
+		else if (!id.equals(other.id)) return false;
+		if (items == null)
+		{
+			if (other.items != null) return false;
+		}
+		else if (!items.equals(other.items)) return false;
+		if (label == null)
+		{
+			if (other.label != null) return false;
+		}
+		else if (!label.equals(other.label)) return false;
+		if (params == null)
+		{
+			if (other.params != null) return false;
+		}
+		else if (!params.equals(other.params)) return false;
+		if (type != other.type) return false;
+		return true;
+	}
 }
