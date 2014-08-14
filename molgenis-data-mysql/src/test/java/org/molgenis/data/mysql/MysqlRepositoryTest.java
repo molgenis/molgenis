17c3c1c708d88ffe227eb3c34f8fa7e7ab9a0021
erwinwinder <j.a.winder@umcg.nl>
2014-08-14 12:51:39 +0200

    Added test

--- molgenis-data-mysql/src/test/java/org/molgenis/data/mysql/MysqlRepositoryTest.java
+++ molgenis-data-mysql/src/test/java/org/molgenis/data/mysql/MysqlRepositoryTest.java
@@ -1,11 +1,12 @@
 package org.molgenis.data.mysql;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.molgenis.AppConfig;
 import org.molgenis.MolgenisFieldTypes;
+import org.molgenis.data.AggregateResult;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Query;
 import org.molgenis.data.support.DefaultAttributeMetaData;
@@ -27,7 +28,34 @@ public class MysqlRepositoryTest extends AbstractTestNGSpringContextTests
 {
 	@Autowired
 	MysqlRepositoryCollection coll;
-	Logger logger = Logger.getLogger(getClass());
+
+	@Test
+	public void testAggregates()
+	{
+		coll.drop("fruit");
+
+		DefaultEntityMetaData meta = new DefaultEntityMetaData("Fruit");
+		meta.addAttribute("name").setIdAttribute(true).setNillable(false);
+		meta.addAttribute("type").setAggregateable(true);
+		MysqlRepository repo = coll.add(meta);
+
+		Entity elstar = new MapEntity("name", "Elstar");
+		elstar.set("type", "Apple");
+		repo.add(elstar);
+
+		Entity jonagold = new MapEntity("name", "Jonagold");
+		jonagold.set("type", "Apple");
+		repo.add(jonagold);
+
+		Entity conference = new MapEntity("name", "Conference");
+		conference.set("type", "Pear");
+		repo.add(conference);
+
+		AggregateResult result = repo.aggregate(meta.getAttribute("type"), null, new QueryImpl());
+		Assert.assertEquals(result.getxLabels(), Arrays.asList("Pear", "Apple", "Total"));
+		Assert.assertEquals(result.getyLabels(), Arrays.asList("Count"));
+		Assert.assertEquals(result.getMatrix(), Arrays.asList(Arrays.asList(1l), Arrays.asList(2l), Arrays.asList(3l)));
+	}
 
 	@Test
 	public void testSql() throws Exception
