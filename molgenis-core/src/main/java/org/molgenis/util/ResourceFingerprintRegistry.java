90d64b344255b8edfd5de4eba9adf3ab8fe0bbfb
dennishendriksen <d.hendriksen@umcg.nl>
2014-08-21 08:16:38 +0200

    Fix resource fingerprinting when running from war

--- molgenis-core/src/main/java/org/molgenis/util/ResourceFingerprintRegistry.java
+++ molgenis-core/src/main/java/org/molgenis/util/ResourceFingerprintRegistry.java
@@ -19,7 +19,7 @@ public class ResourceFingerprintRegistry
 
 	public String getFingerprint(String resourceName) throws IOException
 	{
-		return getFingerprint(null, resourceName);
+		return getFingerprint(this.getClass(), resourceName);
 	}
 
 	public String getFingerprint(Class<?> contextClass, String resourceName) throws IOException
