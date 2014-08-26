154fc318ba6fcebaa2b68819c486e6b6abeb7964
Fleur Kelpin <f.kelpin@umcg.nl>
2014-08-26 15:12:32 +0200

    No time picker for DATEs in the data explorer

--- molgenis-core-ui/src/main/resources/js/molgenis.js
+++ molgenis-core-ui/src/main/resources/js/molgenis.js
@@ -369,7 +369,7 @@ function createInput(attr, attrs, val, lbl) {
 		$('<span>').addClass('input-group-addon datepickerbutton')
 		    .append($('<span>').addClass('glyphicon glyp2icon-calendar'))
 		    .appendTo($div);
-		$div.datetimepicker({useSeconds : dataType === 'DATE_TIME'});
+		$div.datetimepicker(dataType === 'DATE' ? { pickTime : false } : { pickTime : true, useSeconds : true });
 		return $div;
 	case 'DECIMAL':
 		var input = createBasicInput('number', $.extend({}, attrs, {'step': 'any'}), val);
