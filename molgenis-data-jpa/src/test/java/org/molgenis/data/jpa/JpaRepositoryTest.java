f67f98c85ee01df62e2040d09c59bf374f9ddf81
dennishendriksen <d.hendriksen@umcg.nl>
2014-09-15 14:32:25 +0200

    fix test (merge with master)

--- molgenis-data-jpa/src/test/java/org/molgenis/data/jpa/JpaRepositoryTest.java
+++ molgenis-data-jpa/src/test/java/org/molgenis/data/jpa/JpaRepositoryTest.java
@@ -7,15 +7,12 @@ import static org.testng.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
-import org.molgenis.data.AggregateResult;
 import org.molgenis.data.DatabaseAction;
 import org.molgenis.data.Entity;
 import org.molgenis.data.MolgenisDataException;
 import org.molgenis.data.Query;
 import org.molgenis.data.jpa.importer.EntityImportService;
-import org.molgenis.data.support.AggregateQueryImpl;
 import org.molgenis.data.support.MapEntity;
 import org.molgenis.data.support.QueryImpl;
 import org.springframework.data.domain.Sort;
@@ -606,26 +603,5 @@ public class JpaRepositoryTest extends BaseJpaTest
 
 		assertEquals(Iterables.size(it), 1);
 		assertTrue(Iterables.contains(it, p3));
-	}
-
-	@Test
-	public void testAggregate()
-	{
-		Person p1 = new Person("Piet", "Paulusma", 30);
-		Person p2 = new Person("Piet", "de Boskabouter", 35);
-		Person p3 = new Person("Klaas", "Vaak", 35);
-		repo.add(Arrays.asList(p1, p2, p3));
-
-		AggregateResult result = repo
-				.aggregate(new AggregateQueryImpl().attrX(repo.getEntityMetaData().getAttribute("firstName")).attrY(
-						repo.getEntityMetaData().getAttribute("age")));
-		assertNotNull(result);
-		List<List<Long>> matrix = Lists.newArrayList();
-		matrix.add(Lists.newArrayList(1l, 1l, 2l));
-		matrix.add(Lists.newArrayList(1l, 0l, 1l));
-		matrix.add(Lists.newArrayList(2l, 1l, 3l));
-		assertEquals(
-				result,
-				new AggregateResult(matrix, Arrays.asList("Piet", "Klaas", "Total"), Arrays.asList("35", "30", "Total")));
 	}
 }
