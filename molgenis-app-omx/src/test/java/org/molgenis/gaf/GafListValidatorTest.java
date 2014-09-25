8c8d18b554e19798dc15fc087172d2bfa8dae38d
bartcharbon <bart.charbon@gmail.com>
2014-09-25 12:57:17 +0200

    formatting

--- molgenis-app-omx/src/test/java/org/molgenis/gaf/GafListValidatorTest.java
+++ molgenis-app-omx/src/test/java/org/molgenis/gaf/GafListValidatorTest.java
@@ -32,172 +32,166 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(classes =
-        { Config.class })
+{ Config.class })
 public class GafListValidatorTest extends AbstractTestNGSpringContextTests
 {
-    @Autowired
-    private GafListValidator gafListValidator;
+	@Autowired
+	private GafListValidator gafListValidator;
 
-    @Autowired
-    private DataService dataService;
+	@Autowired
+	private DataService dataService;
 
-    @Autowired
-    private MolgenisSettings molgenisSettings;
+	@Autowired
+	private MolgenisSettings molgenisSettings;
 
-    @Autowired
-    private GafListValidationReport report;
+	@Autowired
+	private GafListValidationReport report;
 
-    @BeforeMethod
-    public void setUp()
-    {
-        reset(dataService);
-        reset(molgenisSettings);
+	@BeforeMethod
+	public void setUp()
+	{
+		reset(dataService);
+		reset(molgenisSettings);
 
-        when(
-                molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX
-                        + GAFCol.INTERNAL_SAMPLE_ID))
-                .thenReturn("^[0-9]+$");
-        when(
-                molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX
-                        + GAFCol.EXTERNAL_SAMPLE_ID))
-                .thenReturn("^[a-zA-Z0-9_]+$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.PROJECT))
-                .thenReturn("^[a-zA-Z0-9_]+$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.SEQUENCER))
-                .thenReturn("^[a-zA-Z0-9_]+$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.CONTACT))
-                .thenReturn(
-                        "^([^<>@+0-9_,]+ <[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+>, )*[^<>@+0-9_,]+ <[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+>$");
-        when(
-                molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX
-                        + GAFCol.SEQUENCING_START_DATE))
-                .thenReturn("^[0-9]{6}$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.RUN))
-                .thenReturn("^[0-9]{4}$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.FLOWCELL))
-                .thenReturn("^(([AB][A-Z0-9]{7}XX)|(A[A-Z0-9]{4}))$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.LANE))
-                .thenReturn("^[1-8](,[1-8])*$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.INTERNAL_SAMPLE_ID))
+				.thenReturn("^[0-9]+$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.EXTERNAL_SAMPLE_ID))
+				.thenReturn("^[a-zA-Z0-9_]+$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.PROJECT)).thenReturn(
+				"^[a-zA-Z0-9_]+$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.SEQUENCER)).thenReturn(
+				"^[a-zA-Z0-9_]+$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.CONTACT))
+				.thenReturn(
+						"^([^<>@+0-9_,]+ <[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+>, )*[^<>@+0-9_,]+ <[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+>$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.SEQUENCING_START_DATE))
+				.thenReturn("^[0-9]{6}$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.RUN)).thenReturn(
+				"^[0-9]{4}$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.FLOWCELL)).thenReturn(
+				"^(([AB][A-Z0-9]{7}XX)|(A[A-Z0-9]{4}))$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.LANE)).thenReturn(
+				"^[1-8](,[1-8])*$");
 
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.BARCODE_1))
-                .thenReturn("^(None)|(((GAF)|(RPI)|(AGI)|(MON)|(RTP)|(HP8))\\s[0-9]{2}\\s([ACGT]{6})([ATCG]{2})?)$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.BARCODE_1)).thenReturn(
+				"^(None)|(((GAF)|(RPI)|(AGI)|(MON)|(RTP)|(HP8))\\s[0-9]{2}\\s([ACGT]{6})([ATCG]{2})?)$");
 
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.ARRAY_FILE))
-                .thenReturn("^.*[\\/\\\\]{1}[a-zA-Z0-9\\._]+$");
-        when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.ARRAY_ID))
-                .thenReturn("^[1-9][0-9]*$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.ARRAY_FILE)).thenReturn(
+				"^.*[\\/\\\\]{1}[a-zA-Z0-9\\._]+$");
+		when(molgenisSettings.getProperty(GafListValidator.GAF_LIST_VALIDATOR_PREFIX + GAFCol.ARRAY_ID)).thenReturn(
+				"^[1-9][0-9]*$");
 
-        ObservableFeature feature = mock(ObservableFeature.class);
-        Category category1 = mock(Category.class);
-        when(
-                dataService.findAll(Category.ENTITY_NAME, new QueryImpl().eq(Category.OBSERVABLEFEATURE, feature),
-                        Category.class)).thenReturn(Arrays.asList(category1));
+		ObservableFeature feature = mock(ObservableFeature.class);
+		Category category1 = mock(Category.class);
+		when(
+				dataService.findAll(Category.ENTITY_NAME, new QueryImpl().eq(Category.OBSERVABLEFEATURE, feature),
+						Category.class)).thenReturn(Arrays.asList(category1));
 
-    }
+	}
 
-    @Test
-    public void validate() throws IOException
-    {
-        Repository repository = this.getDefaultValidSettingRepositoryMock();
-        MapEntity entity0 = getDefaultValidMapEntityMock();
-        when(repository.iterator()).thenReturn(Arrays.<Entity> asList(entity0).iterator());
-        gafListValidator.validate(report, repository);
-        assertFalse(report.hasErrors());
-    }
+	@Test
+	public void validate() throws IOException
+	{
+		Repository repository = this.getDefaultValidSettingRepositoryMock();
+		MapEntity entity0 = getDefaultValidMapEntityMock();
+		when(repository.iterator()).thenReturn(Arrays.<Entity> asList(entity0).iterator());
+		gafListValidator.validate(report, repository);
+		assertFalse(report.hasErrors());
+	}
 
-    @Test
-    public void validate_internalSampleId_invalid() throws IOException
-    {
-        invalidTest(GAFCol.INTERNAL_SAMPLE_ID.toString(), "1+");
-    }
+	@Test
+	public void validate_internalSampleId_invalid() throws IOException
+	{
+		invalidTest(GAFCol.INTERNAL_SAMPLE_ID.toString(), "1+");
+	}
 
-    @Test
-    public void validate_externalSampleId_invalid() throws IOException
-    {
-        invalidTest(GAFCol.EXTERNAL_SAMPLE_ID.toString(), "1aA_+");
-    }
+	@Test
+	public void validate_externalSampleId_invalid() throws IOException
+	{
+		invalidTest(GAFCol.EXTERNAL_SAMPLE_ID.toString(), "1aA_+");
+	}
 
-    @Test
-    public void validate_project_invalid() throws IOException
-    {
-        invalidTest(GAFCol.PROJECT.toString(), "1aA_+");
-    }
+	@Test
+	public void validate_project_invalid() throws IOException
+	{
+		invalidTest(GAFCol.PROJECT.toString(), "1aA_+");
+	}
 
-    private Repository getDefaultValidSettingRepositoryMock()
-    {
-        Repository repository = mock(Repository.class);
-        EntityMetaData entityMetaData = mock(EntityMetaData.class);
+	private Repository getDefaultValidSettingRepositoryMock()
+	{
+		Repository repository = mock(Repository.class);
+		EntityMetaData entityMetaData = mock(EntityMetaData.class);
 
-        when(entityMetaData.getAttributes()).thenReturn(
-                Arrays.<AttributeMetaData> asList(new DefaultAttributeMetaData(GAFCol.INTERNAL_SAMPLE_ID.toString(),
-                        FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.EXTERNAL_SAMPLE_ID.toString(),
-                        FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.PROJECT.toString(),
-                        FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.BARCODE_1.toString(),
-                        FieldTypeEnum.STRING)));
+		when(entityMetaData.getAttributes()).thenReturn(
+				Arrays.<AttributeMetaData> asList(new DefaultAttributeMetaData(GAFCol.INTERNAL_SAMPLE_ID.toString(),
+						FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.EXTERNAL_SAMPLE_ID.toString(),
+						FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.PROJECT.toString(),
+						FieldTypeEnum.STRING), new DefaultAttributeMetaData(GAFCol.BARCODE_1.toString(),
+						FieldTypeEnum.STRING)));
 
-        when(repository.getEntityMetaData()).thenReturn(entityMetaData);
+		when(repository.getEntityMetaData()).thenReturn(entityMetaData);
 
-        return repository;
-    }
+		return repository;
+	}
 
-    /**
-     * returns a MapEntity with valid values
-     *
-     * @return MapEntity
-     */
-    private MapEntity getDefaultValidMapEntityMock()
-    {
-        MapEntity entity0 = new MapEntity();
-        entity0.set(GAFCol.INTERNAL_SAMPLE_ID.toString(), "1");
-        entity0.set(GAFCol.EXTERNAL_SAMPLE_ID.toString(), "1aA_");
-        entity0.set(GAFCol.PROJECT.toString(), "1aA_");
-        entity0.set(GAFCol.BARCODE_1.toString(), "RPI 12 GCTAATCA");
-        return entity0;
-    }
+	/**
+	 * returns a MapEntity with valid values
+	 * 
+	 * @return MapEntity
+	 */
+	private MapEntity getDefaultValidMapEntityMock()
+	{
+		MapEntity entity0 = new MapEntity();
+		entity0.set(GAFCol.INTERNAL_SAMPLE_ID.toString(), "1");
+		entity0.set(GAFCol.EXTERNAL_SAMPLE_ID.toString(), "1aA_");
+		entity0.set(GAFCol.PROJECT.toString(), "1aA_");
+		entity0.set(GAFCol.BARCODE_1.toString(), "RPI 12 GCTAATCA");
+		return entity0;
+	}
 
-    private void invalidTest(String nameColumn, String value) throws IOException
-    {
-        Repository repository = this.getDefaultValidSettingRepositoryMock();
-        MapEntity entity0 = getDefaultValidMapEntityMock();
+	private void invalidTest(String nameColumn, String value) throws IOException
+	{
+		Repository repository = this.getDefaultValidSettingRepositoryMock();
+		MapEntity entity0 = getDefaultValidMapEntityMock();
 
-        entity0.set(nameColumn, value);
+		entity0.set(nameColumn, value);
 
-        when(repository.iterator()).thenReturn(Arrays.<Entity> asList(entity0).iterator());
-        gafListValidator.validate(report, repository);
-        assertTrue(report.hasErrors());
-    }
+		when(repository.iterator()).thenReturn(Arrays.<Entity> asList(entity0).iterator());
+		gafListValidator.validate(report, repository);
+		assertTrue(report.hasErrors());
+	}
 
-    @Configuration
-    public static class Config
-    {
-        @Bean
-        public DataService dataService()
-        {
-            return mock(DataService.class);
-        }
+	@Configuration
+	public static class Config
+	{
+		@Bean
+		public DataService dataService()
+		{
+			return mock(DataService.class);
+		}
 
-        @Bean
-        public MolgenisSettings molgenisSettings()
-        {
-            return mock(MolgenisSettings.class);
-        }
+		@Bean
+		public MolgenisSettings molgenisSettings()
+		{
+			return mock(MolgenisSettings.class);
+		}
 
-        @Bean
-        public GafListValidator gafListValidator()
-        {
-            return new GafListValidator();
-        }
+		@Bean
+		public GafListValidator gafListValidator()
+		{
+			return new GafListValidator();
+		}
 
-        @Bean
-        public GafListValidationReport report()
-        {
-            return new GafListValidationReport();
-        }
+		@Bean
+		public GafListValidationReport report()
+		{
+			return new GafListValidationReport();
+		}
 
-        @Bean
-        public FileStore fileStore()
-        {
-            return mock(FileStore.class);
-        }
-    }
+		@Bean
+		public FileStore fileStore()
+		{
+			return mock(FileStore.class);
+		}
+	}
 }
