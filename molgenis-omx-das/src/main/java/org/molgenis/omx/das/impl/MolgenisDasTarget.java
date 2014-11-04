ac57a0edf1609439d9ee44f84edf9f0395f03947
dennishendriksen <d.hendriksen@umcg.nl>
2014-11-04 20:02:59 +0100

    Fix high priority Findbugs warning (missing hashCode), Fix bug in equals

--- molgenis-omx-das/src/main/java/org/molgenis/omx/das/impl/MolgenisDasTarget.java
+++ molgenis-omx-das/src/main/java/org/molgenis/omx/das/impl/MolgenisDasTarget.java
@@ -5,7 +5,7 @@ import uk.ac.ebi.mydas.model.DasTarget;
 
 public class MolgenisDasTarget extends DasTarget
 {
-	//class used to override  the equals method so it can be used in unittests
+	// class used to override the equals method so it can be used in unittests
 	private static final long serialVersionUID = 1L;
 
 	public MolgenisDasTarget(String targetId, int startCoordinate, int stopCoordinate, String targetName)
@@ -28,12 +28,24 @@ public class MolgenisDasTarget extends DasTarget
 			if (other.getTargetId() != null) return false;
 		}
 		else if (!getTargetId().equals(other.getTargetId())) return false;
-		if (getTargetId() == null)
+		if (getTargetName() == null)
 		{
-			if (other.getTargetId() != null) return false;
+			if (other.getTargetName() != null) return false;
 		}
 		else if (!getTargetName().equals(other.getTargetName())) return false;
 		return true;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + getStartCoordinate();
+		result = prime * result + getStopCoordinate();
+		result = prime * result + ((getTargetId() == null) ? 0 : getTargetId().hashCode());
+		result = prime * result + ((getTargetName() == null) ? 0 : getTargetName().hashCode());
+		return result;
 	}
 
 	public String getTargetId()
@@ -41,18 +53,15 @@ public class MolgenisDasTarget extends DasTarget
 		return super.getTargetId();
 	}
 
-
 	public int getStartCoordinate()
 	{
 		return super.getStartCoordinate();
 	}
 
-
 	public int getStopCoordinate()
 	{
 		return super.getStopCoordinate();
 	}
-
 
 	public String getTargetName()
 	{
