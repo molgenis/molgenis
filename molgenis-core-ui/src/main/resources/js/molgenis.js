772fe68098909d15bc247568ccfd807060e0ece4
Mark-de-Haan <markdehaan90@gmail.com>
2014-11-17 15:48:40 +0100

    Removed handlebars-block-helpers.js and moved helper blocks to molgenis.js

--- molgenis-core-ui/src/main/resources/js/molgenis.js
+++ molgenis-core-ui/src/main/resources/js/molgenis.js
@@ -641,7 +641,57 @@ function hideSpinner() {
 	}
 }
 
+/**
+ * Helper block function container
+ */
+function handleBarHelperBlocks() {
+	Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+	    if (arguments.length < 3)
+	        throw new Error("Handlebars Helper equal needs 2 parameters");
+	    if (lvalue != rvalue) {
+	        return options.inverse(this);
+	    } else {
+	        return options.fn(this);
+	    }
+	});
+	
+	Handlebars.registerHelper('notequal', function(lvalue, rvalue, options) {
+	    if (arguments.length < 3)
+	        throw new Error("Handlebars Helper equal needs 2 parameters");
+	    if (lvalue != rvalue) {
+	    	 return options.fn(this);
+	    } else {
+	    	 return options.inverse(this);
+	    }
+	});
+	
+	Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+	    switch (operator) {
+	        case '==':
+	            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+	        case '===':
+	            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+	        case '<':
+	            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+	        case '<=':
+	            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+	        case '>':
+	            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+	        case '>=':
+	            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+	        case '&&':
+	            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+	        case '||':
+	            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+	        default:
+	            return options.inverse(this);
+	    }
+	});
+}
+
 $(function() {
+	
+	handleBarHelperBlocks();
 	
 	// disable all ajax request caching
 	$.ajaxSetup({
