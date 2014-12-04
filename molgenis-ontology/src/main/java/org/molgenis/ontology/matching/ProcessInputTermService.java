47d12a5884e5f84a05ddc6248e43db6a6872322d
Chao Pang <ChaoPang229@gmail.com>
2014-12-04 12:48:09 +0100

    changed the order of code and somehow solved the blocking issue

--- molgenis-ontology/src/main/java/org/molgenis/ontology/matching/ProcessInputTermService.java
+++ molgenis-ontology/src/main/java/org/molgenis/ontology/matching/ProcessInputTermService.java
@@ -1,11 +1,11 @@
 package org.molgenis.ontology.matching;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
 import org.molgenis.auth.MolgenisUser;
 import org.molgenis.auth.UserAuthority;
 import org.molgenis.data.DataService;
@@ -36,6 +36,8 @@ import com.google.common.collect.Lists;
 
 public class ProcessInputTermService
 {
+	private static final int ADD_BATCH_SIZE = 1000;
+
 	private final EmxImportService emxImportService;
 
 	private final MysqlRepositoryCollection mysqlRepositoryCollection;
@@ -45,8 +47,6 @@ public class ProcessInputTermService
 	private final UploadProgress uploadProgress;
 
 	private final OntologyService ontologyService;
-
-	private static final Logger logger = Logger.getLogger(ProcessInputTermService.class);
 
 	@Autowired
 	public ProcessInputTermService(EmxImportService emxImportService,
@@ -73,24 +73,6 @@ public class ProcessInputTermService
 
 		emxImportService.doImport(repositoryCollection, DatabaseAction.ADD);
 
-		// FIXME : temporary work around to assign write permissions to the
-		// users who create the entities.
-		Authentication auth = securityContext.getAuthentication();
-		List<GrantedAuthority> roles = Lists.newArrayList(auth.getAuthorities());
-		for (Permission permiossion : Permission.values())
-		{
-			UserAuthority userAuthority = new UserAuthority();
-			userAuthority.setMolgenisUser(molgenisUser);
-			String role = SecurityUtils.AUTHORITY_ENTITY_PREFIX + permiossion.toString() + "_"
-					+ entityName.toUpperCase();
-			userAuthority.setRole(role);
-			roles.add(new SimpleGrantedAuthority(role));
-			dataService.add(UserAuthority.ENTITY_NAME, userAuthority);
-		}
-		dataService.getCrudRepository(UserAuthority.ENTITY_NAME).flush();
-		auth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), null, roles);
-		securityContext.setAuthentication(auth);
-
 		// Add a new entry in MatchingTask table for this new matching job
 		int threshold = uploadProgress.getThreshold(userName);
 		MapEntity mapEntity = new MapEntity();
@@ -102,11 +84,11 @@ public class ProcessInputTermService
 		dataService.add(MatchingTaskEntity.ENTITY_NAME, mapEntity);
 		dataService.getCrudRepository(MatchingTaskEntity.ENTITY_NAME).flush();
 		uploadProgress.registerUser(userName, entityName, (int) dataService.count(entityName, new QueryImpl()));
-
 		// Match input terms with code
 		Iterable<Entity> findAll = dataService.findAll(entityName);
 		try
 		{
+			List<Entity> entitiesToAdd = new ArrayList<Entity>();
 			for (Entity entity : findAll)
 			{
 				OntologyServiceResult searchEntity = ontologyService.searchEntity(ontologyIri, entity);
@@ -122,17 +104,47 @@ public class ProcessInputTermService
 							ontologyTerm.get(OntologyTermQueryRepository.ONTOLOGY_TERM_IRI));
 					matchingTaskContentEntity.set(MatchingTaskContentEntity.SCORE, score);
 					matchingTaskContentEntity.set(MatchingTaskContentEntity.VALIDATED, false);
-					dataService.add(MatchingTaskContentEntity.ENTITY_NAME, matchingTaskContentEntity);
+					entitiesToAdd.add(matchingTaskContentEntity);
 					break;
+				}
+				// Add entity in batch
+				if (entitiesToAdd.size() >= ADD_BATCH_SIZE)
+				{
+					dataService.add(MatchingTaskContentEntity.ENTITY_NAME, entitiesToAdd);
+					entitiesToAdd.clear();
 				}
 				uploadProgress.incrementProgress(userName);
 			}
-
+			// Add the rest
+			if (entitiesToAdd.size() != 0)
+			{
+				dataService.add(MatchingTaskContentEntity.ENTITY_NAME, entitiesToAdd);
+				entitiesToAdd.clear();
+			}
 			dataService.getCrudRepository(MatchingTaskContentEntity.ENTITY_NAME).flush();
+
+			// FIXME : temporary work around to assign write permissions to the
+			// users who create the entities.
+			Authentication auth = securityContext.getAuthentication();
+			List<GrantedAuthority> roles = Lists.newArrayList(auth.getAuthorities());
+			for (Permission permiossion : Permission.values())
+			{
+				UserAuthority userAuthority = new UserAuthority();
+				userAuthority.setMolgenisUser(molgenisUser);
+				String role = SecurityUtils.AUTHORITY_ENTITY_PREFIX + permiossion.toString() + "_"
+						+ entityName.toUpperCase();
+				userAuthority.setRole(role);
+				roles.add(new SimpleGrantedAuthority(role));
+				dataService.add(UserAuthority.ENTITY_NAME, userAuthority);
+				dataService.getCrudRepository(UserAuthority.ENTITY_NAME).flush();
+			}
+			auth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(), null, roles);
+			securityContext.setAuthentication(auth);
+
 		}
 		catch (Exception e)
 		{
-			logger.error(e.getMessage());
+			throw new RuntimeException(e.getMessage());
 		}
 		finally
 		{
