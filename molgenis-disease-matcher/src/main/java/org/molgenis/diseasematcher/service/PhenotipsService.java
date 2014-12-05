85e02a68e6fa0017fe8c5c3a38ba1522f2eb2d42
tommydeboer <tommydeboer4@gmail.com>
2014-12-05 14:16:45 +0100

    Removed phenotips main method

--- molgenis-disease-matcher/src/main/java/org/molgenis/diseasematcher/service/PhenotipsService.java
+++ molgenis-disease-matcher/src/main/java/org/molgenis/diseasematcher/service/PhenotipsService.java
@@ -11,8 +11,6 @@ import javax.servlet.ServletOutputStream;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
-import com.google.common.collect.Lists;
-
 @Service
 public class PhenotipsService
 {
@@ -20,33 +18,6 @@ public class PhenotipsService
 	public static final int LIMIT = 500;
 	public static final int Q = 1;
 	public static final String URL = "http://playground.phenotips.org/bin/get/PhenoTips/OmimPredictService";
-
-	public static void main(String[] args)
-	{
-		List<String> hpoTerms = Lists.newArrayList();
-		hpoTerms.add("HPO:00038313");
-		hpoTerms.add("HPO:00018234");
-		hpoTerms.add("HPO:00111338");
-
-		OutputStream out = new ByteArrayOutputStream();
-
-		PhenotipsService ps = new PhenotipsService();
-		URL phenotipsRequest;
-		try
-		{
-			// phenotipsRequest = new URL(ps.buildQueryURIString(hpoTerms));
-			phenotipsRequest = new URL(
-					"http://playground.phenotips.org/bin/get/PhenoTips/OmimPredictService?q=1&format=html&limit=30&symptom=HP%3A0009914&symptom=HP%3A0004325");
-			FileCopyUtils.copy(phenotipsRequest.openStream(), out);
-
-			out.toString();
-		}
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 	protected String buildQueryURIString(List<String> hpoTerms)
 	{
@@ -71,7 +42,6 @@ public class PhenotipsService
 		try
 		{
 			phenotipsRequest = new URL(buildQueryURIString(hpoTerms));
-			System.out.println(phenotipsRequest.toString());
 
 			OutputStream testOut = new ByteArrayOutputStream();
 			FileCopyUtils.copy(phenotipsRequest.openStream(), testOut);
