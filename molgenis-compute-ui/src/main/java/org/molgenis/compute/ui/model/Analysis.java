8719d84dd37fa12460d431427130e687f6e74e02
George <h.v.byelas@gmail.com>
2014-12-17 10:33:41 +0100

    merged

--- molgenis-compute-ui/src/main/java/org/molgenis/compute/ui/model/Analysis.java
+++ molgenis-compute-ui/src/main/java/org/molgenis/compute/ui/model/Analysis.java
@@ -122,7 +122,7 @@ public class Analysis extends MapEntity
 
 	public int getCompletedJobCount(String nodeId)
 	{
-		return getJobCount(nodeId, JobStatus.COMPLETE);
+		return getJobCount(nodeId, JobStatus.COMPLETED);
 	}
 
 	public int getFailedJobCount(String nodeId)
--- molgenis-compute-ui/src/main/java/org/molgenis/compute/ui/model/JobStatus.java
+++ molgenis-compute-ui/src/main/java/org/molgenis/compute/ui/model/JobStatus.java
@@ -2,5 +2,5 @@ package org.molgenis.compute.ui.model;
 
 public enum JobStatus
 {
-	GENERATED, SUBMITTED, STARTED, FINISHED, ERROR;
+	RUNNING, COMPLETE, FAILED;
 }
