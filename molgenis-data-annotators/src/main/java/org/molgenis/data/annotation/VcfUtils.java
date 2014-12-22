4e4b88114f8e54ca7db5ebf1109eb2c55c179775
dennishendriksen <d.hendriksen@umcg.nl>
2014-12-22 13:08:54 +0100

    Bug fix: define charset for Scanner

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/VcfUtils.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/VcfUtils.java
@@ -11,33 +11,41 @@ import org.molgenis.data.Entity;
 import org.molgenis.data.vcf.VcfRepository;
 import org.springframework.util.StringUtils;
 
-public class VcfUtils {
-	
+public class VcfUtils
+{
+
 	/**
 	 * Convert an entity to a VCF line
+	 * 
 	 * @param entity
 	 * @return
 	 */
 	public static String convertToVCF(Entity entity)
 	{
 		StringBuilder vcfRecord = new StringBuilder();
-		
-		List<String> vcfAttributes = Arrays.asList(new String[]{VariantAnnotator.CHROMOSOME,VariantAnnotator.POSITION,VcfRepository.ID,VariantAnnotator.REFERENCE,VariantAnnotator.ALTERNATIVE});
-		
-		for(String attribute : vcfAttributes){
+
+		List<String> vcfAttributes = Arrays.asList(new String[]
+		{ VariantAnnotator.CHROMOSOME, VariantAnnotator.POSITION, VcfRepository.ID, VariantAnnotator.REFERENCE,
+				VariantAnnotator.ALTERNATIVE });
+
+		for (String attribute : vcfAttributes)
+		{
 			vcfRecord.append(entity.getString(attribute) + "\t");
 		}
-		for(AttributeMetaData attributeMetaData : entity.getEntityMetaData().getAtomicAttributes()){
-			if(!vcfAttributes.contains(attributeMetaData.getName()) && attributeMetaData.isVisible() && !StringUtils.isEmpty(entity.getString(attributeMetaData.getName())))
+		for (AttributeMetaData attributeMetaData : entity.getEntityMetaData().getAtomicAttributes())
+		{
+			if (!vcfAttributes.contains(attributeMetaData.getName()) && attributeMetaData.isVisible()
+					&& !StringUtils.isEmpty(entity.getString(attributeMetaData.getName())))
 			{
-				vcfRecord.append(attributeMetaData.getName() +"=" + entity.getString(attributeMetaData.getName()) + ";");
+				vcfRecord.append(attributeMetaData.getName() + "=" + entity.getString(attributeMetaData.getName())
+						+ ";");
 			}
-				
+
 		}
-		
+
 		return vcfRecord.toString();
 	}
-	
+
 	/**
 	 * Sensitive checks for previous annotations
 	 * 
@@ -50,67 +58,71 @@ public class VcfUtils {
 	 * @return
 	 * @throws Exception
 	 */
-	public static boolean checkInput(File inputVcfFile, PrintWriter outputVCFWriter, List<String> infoFields, String checkAnnotatedBeforeValue) throws Exception
+	public static boolean checkInput(File inputVcfFile, PrintWriter outputVCFWriter, List<String> infoFields,
+			String checkAnnotatedBeforeValue) throws Exception
 	{
 		boolean annotatedBefore = false;
-	
+
 		System.out.println("Detecting VCF column header...");
-		
-		Scanner inputVcfFileScanner = new Scanner(inputVcfFile);
-        String line = inputVcfFileScanner.nextLine();
-        
-        //if first line does not start with ##, we dont trust this file as VCF
-        if(line.startsWith("##"))
-        {
-        	while(inputVcfFileScanner.hasNextLine())
-        	{
-        		//detect existing annotations of the same info field
-        		if(line.contains("##INFO=<ID="+checkAnnotatedBeforeValue) && !annotatedBefore)
-        		{
-        			System.out.println("\nThis file has already been annotated with '"+checkAnnotatedBeforeValue+"' data before it seems. Skipping any further annotation of variants that already contain this field.");
-        			annotatedBefore = true;
-        		}
-        		
-        		//read and print to output until we find the header
-        		outputVCFWriter.println(line);
-        		line = inputVcfFileScanner.nextLine();
-        		if(!line.startsWith("##"))
-        		{
-        			break;
-        		}
-        		System.out.print(".");
-        	}
-        	System.out.println("\nHeader line found:\n" + line);
-        	
-        	//check the header line
-        	if(!line.startsWith("#CHROM"))
-        	{
-        		outputVCFWriter.close();
-        		inputVcfFileScanner.close();
-        		throw new Exception("Header does not start with #CHROM, are you sure it is a VCF file?");
-        	}
-        	
-        	//print INFO lines for stuff to be annotated
-        	if(!annotatedBefore)
-        	{
-        		for(String infoField : infoFields)
-        		{
-        			outputVCFWriter.println(infoField);
-        		}
-         	}
-        	
-        	//now print header
-        	outputVCFWriter.println(line);
-        }
-        else
-        {
-        	outputVCFWriter.close();
-        	inputVcfFileScanner.close();
-        	throw new Exception("Did not find ## on the first line, are you sure it is a VCF file?");
-        }
-        
-        inputVcfFileScanner.close();
-        return annotatedBefore;
+
+		Scanner inputVcfFileScanner = new Scanner(inputVcfFile, "UTF-8");
+		String line = inputVcfFileScanner.nextLine();
+
+		// if first line does not start with ##, we dont trust this file as VCF
+		if (line.startsWith("##"))
+		{
+			while (inputVcfFileScanner.hasNextLine())
+			{
+				// detect existing annotations of the same info field
+				if (line.contains("##INFO=<ID=" + checkAnnotatedBeforeValue) && !annotatedBefore)
+				{
+					System.out
+							.println("\nThis file has already been annotated with '"
+									+ checkAnnotatedBeforeValue
+									+ "' data before it seems. Skipping any further annotation of variants that already contain this field.");
+					annotatedBefore = true;
+				}
+
+				// read and print to output until we find the header
+				outputVCFWriter.println(line);
+				line = inputVcfFileScanner.nextLine();
+				if (!line.startsWith("##"))
+				{
+					break;
+				}
+				System.out.print(".");
+			}
+			System.out.println("\nHeader line found:\n" + line);
+
+			// check the header line
+			if (!line.startsWith("#CHROM"))
+			{
+				outputVCFWriter.close();
+				inputVcfFileScanner.close();
+				throw new Exception("Header does not start with #CHROM, are you sure it is a VCF file?");
+			}
+
+			// print INFO lines for stuff to be annotated
+			if (!annotatedBefore)
+			{
+				for (String infoField : infoFields)
+				{
+					outputVCFWriter.println(infoField);
+				}
+			}
+
+			// now print header
+			outputVCFWriter.println(line);
+		}
+		else
+		{
+			outputVCFWriter.close();
+			inputVcfFileScanner.close();
+			throw new Exception("Did not find ## on the first line, are you sure it is a VCF file?");
+		}
+
+		inputVcfFileScanner.close();
+		return annotatedBefore;
 	}
 
 }
