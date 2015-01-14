cfa58badb7a44d742c64d377c6b53afb9f8a6ee0
bartcharbon <bart.charbon@gmail.com>
2015-01-14 15:05:30 +0100

    First version of Mapping entities - Work in progress - Repo's need implementation

---
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/mapping/MappingProject.java
@@ -0,0 +1,13 @@
+package org.molgenis.data.mapping;
+
+import org.molgenis.data.AttributeMetaData;
+
+/**
+ * Created by charbonb on 14/01/15.
+ */
+public class MappingProject {
+	private String id;
+	private AttributeMetaData sourceAttribureMetaData;
+	private AttributeMetaData targetAttribureMetaData;
+	private String algorithm;
+}
---
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/repository/AttributeMappingRepository.java
@@ -0,0 +1,43 @@
+package org.molgenis.data.repository;
+
+import org.molgenis.data.*;
+import org.molgenis.data.meta.EntityMappingMetaData;
+import org.molgenis.data.support.DefaultAttributeMetaData;
+import org.molgenis.data.support.MapEntity;
+
+import java.util.*;
+
+import static org.molgenis.data.meta.AttributeMetaDataMetaData.*;
+
+/**
+ * Helper class around the {@link org.molgenis.data.meta.AttributeMetaDataMetaData} repository. Internal implementation
+ * class, use {@link org.molgenis.data.meta.MetaDataServiceImpl} instead.
+ */
+public class AttributeMappingRepository
+{
+	public static final EntityMappingMetaData META_DATA = new EntityMappingMetaData();
+
+	private final CrudRepository repository;
+
+	public AttributeMappingRepository(ManageableCrudRepositoryCollection collection)
+	{
+		this.repository = collection.add(META_DATA);
+		fillAllEntityAttributes();
+	}
+
+	public void fillAllEntityAttributes()
+	{
+
+	}
+
+	private void toEntityMappingEntity(Entity entity, AttributeMetaData att, AttributeMetaData parentCompoundAtt)
+	{
+		Entity attributeMetaDataEntity = new MapEntity();
+	}
+
+	private DefaultAttributeMetaData toAttributeMetaData(Entity entity)
+	{
+		DefaultAttributeMetaData attributeMetaData = new DefaultAttributeMetaData(entity.getString(NAME));
+		return attributeMetaData;
+	}
+}
