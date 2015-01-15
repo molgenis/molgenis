f57d575365fb8f653fe9682952f4cbb3dedd7070
bartcharbon <bart.charbon@gmail.com>
2015-01-15 09:26:03 +0100

    first implementation of MappingProjectRepository

--- molgenis-data-mapper/src/main/java/org/molgenis/data/mapping/AttributeMapping.java
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/mapping/AttributeMapping.java
@@ -5,9 +5,59 @@ import org.molgenis.data.AttributeMetaData;
 /**
  * Created by charbonb on 14/01/15.
  */
-public class AttributeMapping {
+public class AttributeMapping
+{
 	private String id;
 	private AttributeMetaData sourceAttribureMetaData;
 	private AttributeMetaData targetAttribureMetaData;
 	private String algorithm;
+
+	public AttributeMapping(String id, AttributeMetaData sourceAttribureMetaData,
+			AttributeMetaData targetAttribureMetaData, String algorithm)
+	{
+		this.id = id;
+		this.sourceAttribureMetaData = sourceAttribureMetaData;
+		this.targetAttribureMetaData = targetAttribureMetaData;
+		this.algorithm = algorithm;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public AttributeMetaData getSourceAttribureMetaData()
+	{
+		return sourceAttribureMetaData;
+	}
+
+	public void setSourceAttribureMetaData(AttributeMetaData sourceAttribureMetaData)
+	{
+		this.sourceAttribureMetaData = sourceAttribureMetaData;
+	}
+
+	public AttributeMetaData getTargetAttribureMetaData()
+	{
+		return targetAttribureMetaData;
+	}
+
+	public void setTargetAttribureMetaData(AttributeMetaData targetAttribureMetaData)
+	{
+		this.targetAttribureMetaData = targetAttribureMetaData;
+	}
+
+	public String getAlgorithm()
+	{
+		return algorithm;
+	}
+
+	public void setAlgorithm(String algorithm)
+	{
+		this.algorithm = algorithm;
+	}
 }
--- molgenis-data-mapper/src/main/java/org/molgenis/data/mapping/EntityMapping.java
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/mapping/EntityMapping.java
@@ -7,9 +7,59 @@ import java.util.List;
 /**
  * Created by charbonb on 14/01/15.
  */
-public class EntityMapping {
+public class EntityMapping
+{
 	private String id;
 	private EntityMetaData sourceEntityMetaData;
 	private EntityMetaData targetEntityMetaData;
 	private List<AttributeMapping> entityMappings;
+
+	public EntityMapping(String id, EntityMetaData sourceEntityMetaData, EntityMetaData targetEntityMetaData,
+			List<AttributeMapping> entityMappings)
+	{
+		this.id = id;
+		this.sourceEntityMetaData = sourceEntityMetaData;
+		this.targetEntityMetaData = targetEntityMetaData;
+		this.entityMappings = entityMappings;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public EntityMetaData getSourceEntityMetaData()
+	{
+		return sourceEntityMetaData;
+	}
+
+	public void setSourceEntityMetaData(EntityMetaData sourceEntityMetaData)
+	{
+		this.sourceEntityMetaData = sourceEntityMetaData;
+	}
+
+	public EntityMetaData getTargetEntityMetaData()
+	{
+		return targetEntityMetaData;
+	}
+
+	public void setTargetEntityMetaData(EntityMetaData targetEntityMetaData)
+	{
+		this.targetEntityMetaData = targetEntityMetaData;
+	}
+
+	public List<AttributeMapping> getEntityMappings()
+	{
+		return entityMappings;
+	}
+
+	public void setEntityMappings(List<AttributeMapping> entityMappings)
+	{
+		this.entityMappings = entityMappings;
+	}
 }
