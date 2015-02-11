f04fdbf09496f2deb72cd8b7e056d70b2bb91962
Fleur Kelpin <f.kelpin@umcg.nl>
2015-02-11 23:13:17 +0100

    Cleanup js.

--- molgenis-data-mapper/src/main/resources/js/mapping-projects.js
+++ molgenis-data-mapper/src/main/resources/js/mapping-projects.js
@@ -2,7 +2,34 @@
 	"use strict";
 	var restApi = new molgenis.RestClient();
 	
+	/**
+	 * Updates a table's column widths.
+	 * 
+	 * @param $table
+	 *            the scrollable table whose header and body cells should be
+	 *            realigned
+	 */
+	var updateColumnWidths = function($table) {
+		var $bodyCells = $table.find('tbody tr:first').children();
+		var colWidth;
+
+		// Get the tbody columns width array
+		colWidth = $bodyCells.map(function() {
+			return $(this).width();
+		}).get();
+
+		// Set the width of thead columns
+		$table.find('thead tr').children().each(function(i, v) {
+			$(v).width(colWidth[i]);
+		});
+	};
+	
 	$(function() {
+		var $table = $('table.scroll');
+		$(window).resize(function(){
+			updateColumnWidths($table);
+		});
+		updateColumnWidths($table);
 		
 		$('form.verify').submit(function(e) {
 	        var currentForm = this;
@@ -19,22 +46,6 @@
 		});
 		
 		$('#create-new-mapping-project-form').validate();
-		
-		var $table = $('table.scroll');
-		var $bodyCells = $table.find('tbody tr:first').children();
-	    var colWidth;
-		// Adjust the width of thead cells when window resizes
-		$(window).resize(function() {
-		    // Get the tbody columns width array
-		    colWidth = $bodyCells.map(function() {
-		        return $(this).width();
-		    }).get();
-		    
-		    // Set the width of thead columns
-		    $table.find('thead tr').children().each(function(i, v) {
-		        $(v).width(colWidth[i]);
-		    });    
-		}).resize(); // Trigger resize handler
 	});
 		
 }($, window.top.molgenis = window.top.molgenis || {}));
