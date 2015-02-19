9958ef2058f4d6fd6a0ac3668cb8fdc891f740ac
erwinwinder <j.a.winder@umcg.nl>
2015-02-19 12:37:21 +0100

    Refactor

--- molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/ElasticsearchRepositoryCollection.java
+++ molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/ElasticsearchRepositoryCollection.java
@@ -1,20 +1,38 @@
 package org.molgenis.data.elasticsearch;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.DataService;
 import org.molgenis.data.EntityMetaData;
+import org.molgenis.data.ManageableRepositoryCollection;
 import org.molgenis.data.Repository;
+import org.molgenis.data.UnknownAttributeException;
+import org.molgenis.data.UnknownEntityException;
+import org.molgenis.data.support.DefaultEntityMetaData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Maps;
+
 @Component("ElasticsearchRepositoryCollection")
-public class ElasticsearchRepositoryCollection extends AbstractSearchRepositoryCollection
+public class ElasticsearchRepositoryCollection implements ManageableRepositoryCollection
 {
 	public static final String NAME = "ElasticSearch";
+	private final SearchService searchService;
+	private final DataService dataService;
+	private final Map<String, AbstractElasticsearchRepository> repositories = Maps.newLinkedHashMap();
 
 	@Autowired
 	public ElasticsearchRepositoryCollection(SearchService searchService, DataService dataService)
 	{
-		super(searchService, dataService, NAME);
+		this.searchService = searchService;
+		this.dataService = dataService;
 	}
 
 	@Override
@@ -25,6 +43,96 @@ public class ElasticsearchRepositoryCollection extends AbstractSearchRepositoryC
 		repositories.put(entityMeta.getName(), repo);
 
 		return repo;
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+
+	@Override
+	public Iterable<String> getEntityNames()
+	{
+		return repositories.keySet();
+	}
+
+	@Override
+	public Repository getRepository(String name)
+	{
+		return repositories.get(name);
+	}
+
+	@Override
+	public Iterator<Repository> iterator()
+	{
+		return Iterators.transform(repositories.values().iterator(),
+				new Function<AbstractElasticsearchRepository, Repository>()
+				{
+					@Override
+					public Repository apply(AbstractElasticsearchRepository input)
+					{
+						return input;
+					}
+				});
+	}
+
+	@Override
+	public void deleteEntityMeta(String entityName)
+	{
+		// remove the repo
+		AbstractElasticsearchRepository r = repositories.get(entityName);
+		if (r != null)
+		{
+			r.drop();
+			repositories.remove(entityName);
+		}
+	}
+
+	@Override
+	public void addAttribute(String entityName, AttributeMetaData attribute)
+	{
+		EntityMetaData entityMetaData = dataService.getEntityMetaData(entityName);
+		if (entityMetaData == null) throw new UnknownEntityException(String.format("Unknown entity '%s'", entityName));
+
+		try
+		{
+			searchService.createMappings(entityMetaData);
+		}
+		catch (IOException e)
+		{
+			throw new UncheckedIOException("Error creating mappings for [" + entityName + "]", e);
+		}
+	}
+
+	@Override
+	public void deleteAttribute(String entityName, String attributeName)
+	{
+		EntityMetaData entityMetaData = dataService.getEntityMetaData(entityName);
+		if (entityMetaData == null) throw new UnknownEntityException(String.format("Unknown entity '%s'", entityName));
+
+		DefaultEntityMetaData defaultEntityMetaData = new DefaultEntityMetaData(
+				dataService.getEntityMetaData(entityName));
+		AttributeMetaData attr = entityMetaData.getAttribute(attributeName);
+		if (attr == null) throw new UnknownAttributeException(String.format("Unknown attribute '%s' of entity '%s'",
+				attributeName, entityName));
+
+		defaultEntityMetaData.removeAttributeMetaData(attr);
+
+		try
+		{
+			searchService.createMappings(entityMetaData);
+		}
+		catch (IOException e)
+		{
+			throw new UncheckedIOException("Error creating mappings for [" + entityName + "]", e);
+		}
+	}
+
+	@Override
+	public void addAttributeSync(String entityName, AttributeMetaData attribute)
+	{
+		addAttribute(entityName, attribute);
 	}
 
 }
