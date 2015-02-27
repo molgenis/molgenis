b4e7340c9d4c146729adcaf77fee103111276a61
Mark-de-Haan <markdehaan90@gmail.com>
2015-02-27 15:40:18 +0100

    When the data explorer is called with a searchTerm in the URL, it is now applied to the datatable

--- molgenis-dataexplorer/src/main/resources/js/dataexplorer.js
+++ molgenis-dataexplorer/src/main/resources/js/dataexplorer.js
@@ -498,6 +498,13 @@ function($, molgenis, settingsXhr) {
 				// FIXME implement as part of http://www.molgenis.org/ticket/3110
 			}
 			
+			var searchTerm = $("#observationset-search").val();
+			console.log(searchTerm);
+			// restore the search term and trigger change event to filter data table
+			if(searchTerm){
+				$("#observationset-search").val(searchTerm).change();
+			}
+			
 			render();
 		}
 		
