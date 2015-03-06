b60dbe878f64e0915a10915edd6d2496c4ebf0b1
jjettenn <jjettenn@gmail.com>
2015-03-06 10:57:26 +0100

    Fix

--- molgenis-data-jpa/src/main/java/org/molgenis/data/jpa/importer/JpaImportService.java
+++ molgenis-data-jpa/src/main/java/org/molgenis/data/jpa/importer/JpaImportService.java
@@ -71,15 +71,18 @@ public class JpaImportService implements ImportService
 	{
 		String fileNameExtension = FileExtensionUtils.findExtensionFromPossibilities(file.getName(),
 				GenericImporterExtensions.getJPA());
+
+		if (!source.getEntityNames().iterator().hasNext()) return false;
+
 		if (null != fileNameExtension)
 		{
 			for (String entityName : source.getEntityNames())
 			{
-				if (targetCollection.hasRepository(entityName)) return true;
+				if (!targetCollection.hasRepository(entityName)) return false;
 			}
 		}
 
-		return false;
+		return true;
 	}
 
 	@Override
