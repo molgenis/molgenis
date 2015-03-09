46b7554d1e920c533833672892b0eb1ae9e18680
bartcharbon <bart.charbon@gmail.com>
2015-03-09 16:49:27 +0100

    code formatting

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/AbstractRepositoryAnnotator.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/AbstractRepositoryAnnotator.java
@@ -20,110 +20,135 @@ import java.util.Map;
  * File Templates.
  */
 public abstract class AbstractRepositoryAnnotator implements RepositoryAnnotator,
-        ApplicationListener<ContextRefreshedEvent> {
-    @Autowired
-    AnnotationService annotatorService;
+		ApplicationListener<ContextRefreshedEvent>
+{
+	@Autowired
+	AnnotationService annotatorService;
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        annotatorService.addAnnotator(this);
-    }
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event)
+	{
+		annotatorService.addAnnotator(this);
+	}
 
-    @Override
-    public String canAnnotate(EntityMetaData repoMetaData) {
-        Iterable<AttributeMetaData> annotatorAttributes = getInputMetaData().getAttributes();
-        for (AttributeMetaData annotatorAttribute : annotatorAttributes) {
-            // one of the needed attributes not present? we can not annotate
-            if (repoMetaData.getAttribute(annotatorAttribute.getName()) == null) {
-                return "missing required attribute";
-            }
+	@Override
+	public String canAnnotate(EntityMetaData repoMetaData)
+	{
+		Iterable<AttributeMetaData> annotatorAttributes = getInputMetaData().getAttributes();
+		for (AttributeMetaData annotatorAttribute : annotatorAttributes)
+		{
+			// one of the needed attributes not present? we can not annotate
+			if (repoMetaData.getAttribute(annotatorAttribute.getName()) == null)
+			{
+				return "missing required attribute";
+			}
 
-            // one of the needed attributes not of the correct type? we can not annotate
-            if (!repoMetaData.getAttribute(annotatorAttribute.getName()).getDataType()
-                    .equals(annotatorAttribute.getDataType())) {
-                return "a required attribute has the wrong datatype";
-            }
+			// one of the needed attributes not of the correct type? we can not annotate
+			if (!repoMetaData.getAttribute(annotatorAttribute.getName()).getDataType()
+					.equals(annotatorAttribute.getDataType()))
+			{
+				return "a required attribute has the wrong datatype";
+			}
 
-            // Are the runtime property files not available, or is a webservice down? we can not annotate
-            if (!annotationDataExists()) {
-                return "annotation datasource unreachable";
-            }
-        }
+			// Are the runtime property files not available, or is a webservice down? we can not annotate
+			if (!annotationDataExists())
+			{
+				return "annotation datasource unreachable";
+			}
+		}
 
-        return "true";
-    }
+		return "true";
+	}
 
-    /**
-     * Checks if folder and files that were set with a runtime property actually exist, or if a webservice can be
-     * reached
-     *
-     * @return boolean
-     */
-    protected abstract boolean annotationDataExists();
+	/**
+	 * Checks if folder and files that were set with a runtime property actually exist, or if a webservice can be
+	 * reached
+	 *
+	 * @return boolean
+	 */
+	protected abstract boolean annotationDataExists();
 
-    @Override
-    @Transactional
-    public Iterator<Entity> annotate(final Iterable<Entity> sourceIterable) {
-        Iterator<Entity> source = sourceIterable.iterator();
-        return new Iterator<Entity>() {
-            int current = 0;
-            int size = 0;
-            List<Entity> results;
-            Entity result;
+	@Override
+	@Transactional
+	public Iterator<Entity> annotate(final Iterable<Entity> sourceIterable)
+	{
+		Iterator<Entity> source = sourceIterable.iterator();
+		return new Iterator<Entity>()
+		{
+			int current = 0;
+			int size = 0;
+			List<Entity> results;
+			Entity result;
 
-            @Override
-            public boolean hasNext() {
-                return current < size || source.hasNext();
-            }
+			@Override
+			public boolean hasNext()
+			{
+				return current < size || source.hasNext();
+			}
 
-            @Override
-            public Entity next() {
-                Entity sourceEntity = null;
-                if (current >= size) {
-                    if (source.hasNext()) {
-                        try {
-                            sourceEntity = source.next();
-                            results = annotateEntity(sourceEntity);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
+			@Override
+			public Entity next()
+			{
+				Entity sourceEntity = null;
+				if (current >= size)
+				{
+					if (source.hasNext())
+					{
+						try
+						{
+							sourceEntity = source.next();
+							results = annotateEntity(sourceEntity);
+						}
+						catch (IOException e)
+						{
+							throw new RuntimeException(e);
+						}
+						catch (InterruptedException e)
+						{
+							throw new RuntimeException(e);
+						}
 
-                        size = results.size();
-                    }
-                    current = 0;
-                }
-                if (results.size() > 0) {
-                    result = results.get(current);
-                } else {
-                    result = sourceEntity;
-                }
-                ++current;
-                return result;
-            }
+						size = results.size();
+					}
+					current = 0;
+				}
+				if (results.size() > 0)
+				{
+					result = results.get(current);
+				}
+				else
+				{
+					result = sourceEntity;
+				}
+				++current;
+				return result;
+			}
 
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
-    }
+			@Override
+			public void remove()
+			{
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
 
-    public abstract List<Entity> annotateEntity(Entity entity) throws IOException, InterruptedException;
+	public abstract List<Entity> annotateEntity(Entity entity) throws IOException, InterruptedException;
 
-    public Entity getAnnotatedEntity(Entity entity, Map<String, Object> resultMap) {
-        DefaultEntityMetaData resultEntityMetadata = new DefaultEntityMetaData(entity.getEntityMetaData());
-        MapEntity resultEntity = new MapEntity(entity, resultEntityMetadata);
-        for (AttributeMetaData attributeMetaData : getOutputMetaData().getAtomicAttributes()) {
-            resultEntityMetadata.addAttributeMetaData(attributeMetaData);
-            resultEntity.set(attributeMetaData.getName(), resultMap.get(attributeMetaData.getName()));
-        }
-        return resultEntity;
-    }
+	public Entity getAnnotatedEntity(Entity entity, Map<String, Object> resultMap)
+	{
+		DefaultEntityMetaData resultEntityMetadata = new DefaultEntityMetaData(entity.getEntityMetaData());
+		MapEntity resultEntity = new MapEntity(entity, resultEntityMetadata);
+		for (AttributeMetaData attributeMetaData : getOutputMetaData().getAtomicAttributes())
+		{
+			resultEntityMetadata.addAttributeMetaData(attributeMetaData);
+			resultEntity.set(attributeMetaData.getName(), resultMap.get(attributeMetaData.getName()));
+		}
+		return resultEntity;
+	}
 
-    @Override
-    public String getFullName() {
-        return RepositoryAnnotator.ANNOTATOR_PREFIX + getSimpleName();
-    }
+	@Override
+	public String getFullName()
+	{
+		return RepositoryAnnotator.ANNOTATOR_PREFIX + getSimpleName();
+	}
 }
