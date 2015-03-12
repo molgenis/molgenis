237cb277fe914b286465e0097e060e03b5b1a4cd
Mark-de-Haan <markdehaan90@gmail.com>
2015-03-12 12:50:05 +0100

    Removed weird if else statement on a var that is declared right before.

--- molgenis-dataexplorer/src/main/resources/js/dataexplorer-filter-wizard.js
+++ molgenis-dataexplorer/src/main/resources/js/dataexplorer-filter-wizard.js
@@ -63,13 +63,7 @@
 			listItems.push('<li><a href="#' + tabId + '" data-toggle="tab">' + label + '</a></li>');
 			
 			var pane = $('<div class="tab-pane' + (i === 0 ? ' active"' : '"') + ' id="' + tabId + '">');
-			var paneContainer = $('<div class="well"></div>');
-			
-			if (compoundAttributes.length > 1){
-				paneContainer.addClass('filter-wizard-fixed-height');
-			} else {
-				paneContainer.removeClass('filter-wizard-fixed-height');
-			}
+			var paneContainer = $('<div class="well filter-wizard-fixed-height"></div>');
 			
 			$.each(compoundAttribute.attributes, function(i, attribute) {
 				if(attribute.fieldType !== 'COMPOUND') {
