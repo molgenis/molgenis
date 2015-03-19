18851c6e55cb83af64544fee029c10bfcc1f221e
erwinwinder <j.a.winder@umcg.nl>
2015-03-19 14:32:52 +0100

    Fix merge

--- molgenis-questionnaires/src/main/java/org/molgenis/questionnaires/QuestionnairePluginController.java
+++ molgenis-questionnaires/src/main/java/org/molgenis/questionnaires/QuestionnairePluginController.java
@@ -3,7 +3,7 @@ package org.molgenis.questionnaires;
 import static java.util.stream.StreamSupport.stream;
 import static org.molgenis.data.support.QueryImpl.EQ;
 import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_ENTITY_WRITE_PREFIX;
-import static org.molgenis.security.runas.RunAsSystemTemplate.runAsSystem;
+import static org.molgenis.security.runas.RunAsSystemProxy.runAsSystem;
 
 import java.io.IOException;
 import java.util.List;
