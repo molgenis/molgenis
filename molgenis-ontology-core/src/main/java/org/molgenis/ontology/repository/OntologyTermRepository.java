dec36f9742710f20f9b4cf3ee96899d64afcccc4
Fleur Kelpin <f.kelpin@umcg.nl>
2015-03-19 14:10:54 +0100

    Add OntologyConfig

--- molgenis-ontology-core/src/main/java/org/molgenis/ontology/repository/OntologyTermRepository.java
+++ molgenis-ontology-core/src/main/java/org/molgenis/ontology/repository/OntologyTermRepository.java
@@ -1,9 +1,25 @@
 package org.molgenis.ontology.repository;
 
+import java.util.List;
+
 import org.molgenis.data.DataService;
+import org.molgenis.data.Entity;
+import org.molgenis.ontology.repository.model.Ontology;
+import org.molgenis.ontology.repository.model.OntologyTerm;
 
 public class OntologyTermRepository
 {
 	private DataService dataService;
 
+	public List<OntologyTerm> findOntologyTerm(List<Ontology> ontologies, String search)
+	{
+		return null;
+		// return Lists.newArrayList(dataService.findAll(ENTITY_NAME,
+		// IN(ONTOLOGY, ontologies.stream().map(Ontology::getId).map(this::toOntologyTerm).collect(toList())));
+	}
+
+	private OntologyTerm toOntologyTerm(Entity entity)
+	{
+		return null;
+	}
 }
