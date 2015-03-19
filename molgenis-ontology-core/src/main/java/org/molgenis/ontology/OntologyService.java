7f6d92504921d8344e6f42d57b7cb580f376c357
Fleur Kelpin <f.kelpin@umcg.nl>
2015-03-19 12:57:37 +0100

    methods for the M3R tagwizard

--- molgenis-ontology-core/src/main/java/org/molgenis/ontology/OntologyService.java
+++ molgenis-ontology-core/src/main/java/org/molgenis/ontology/OntologyService.java
@@ -1,13 +1,19 @@
 package org.molgenis.ontology;
 
+import java.util.List;
+
 import org.molgenis.ontology.repository.model.Ontology;
 import org.molgenis.ontology.repository.model.OntologyTerm;
 
 public interface OntologyService
 {
+	List<Ontology> getOntologies();
 
 	Ontology getOntology(String name);
 
+	OntologyTerm findOntologyTerm(List<Ontology> ontologies, String search);
+
+	// voor de tag service
 	OntologyTerm getOntologyTerm(String ontology, String iri);
 
 }
--- molgenis-ontology-core/src/main/java/org/molgenis/ontology/repository/OntologyRepository.java
+++ molgenis-ontology-core/src/main/java/org/molgenis/ontology/repository/OntologyRepository.java
@@ -1,6 +1,12 @@
 package org.molgenis.ontology.repository;
 
+import org.molgenis.ontology.repository.model.Ontology;
+
 public class OntologyRepository
 {
+	public Iterable<Ontology> getOntologies()
+	{
+
+	}
 
 }
