6b59ce51c9aa05eb3676151df15d7cecfa247ec6
dennishendriksen <d.hendriksen@umcg.nl>
2015-03-19 07:55:45 +0100

    Fix #2665 HtmlField has enum type TEXT instead of HTML

--- molgenis-core/src/main/java/org/molgenis/fieldtypes/HtmlField.java
+++ molgenis-core/src/main/java/org/molgenis/fieldtypes/HtmlField.java
@@ -1,7 +1,14 @@
 package org.molgenis.fieldtypes;
 
+import org.molgenis.MolgenisFieldTypes.FieldTypeEnum;
 
 public class HtmlField extends TextField
 {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public FieldTypeEnum getEnumType()
+	{
+		return FieldTypeEnum.HTML;
+	}
 }
