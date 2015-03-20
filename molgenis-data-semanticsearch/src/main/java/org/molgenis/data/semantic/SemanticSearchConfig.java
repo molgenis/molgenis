c4c6144f970cf037128e3564373099fe6e36ad4a
Mark-de-Haan <markdehaan90@gmail.com>
2015-03-20 17:04:13 +0100

    Add IdGenerator to SemanticSearchConfig

--- molgenis-data-semanticsearch/src/main/java/org/molgenis/data/semantic/SemanticSearchConfig.java
+++ molgenis-data-semanticsearch/src/main/java/org/molgenis/data/semantic/SemanticSearchConfig.java
@@ -1,6 +1,7 @@
 package org.molgenis.data.semantic;
 
 import org.molgenis.data.DataService;
+import org.molgenis.data.IdGenerator;
 import org.molgenis.ontology.OntologyService;
 import org.molgenis.ontology.repository.OntologyRepository;
 import org.molgenis.ontology.repository.OntologyTermRepository;
@@ -19,10 +20,14 @@ public class SemanticSearchConfig
 
 	@Autowired
 	TagRepository tagRepository;
+	
+	@Autowired
+	IdGenerator idGenerator;
+	
 
 	@Bean
 	public OntologyTagService ontologyTagService()
 	{
-		return new OntologyTagService(dataService, ontologyService, tagRepository);
+		return new OntologyTagService(dataService, ontologyService, tagRepository, idGenerator);
 	}
 }
