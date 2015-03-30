4af003de3ac2a80e8ae0833fd0b51a4cdc477995
dennishendriksen <d.hendriksen@umcg.nl>
2015-03-30 15:38:30 +0200

    use base props, add focus to controls

--- molgenis-core-ui/src/main/resources/js/component/AttributeControl.js
+++ molgenis-core-ui/src/main/resources/js/component/AttributeControl.js
@@ -36,19 +36,25 @@
 			var attr = this.state.attr;
 			
 			if(attr.expression === undefined) {
+				// default props for all controls
+				var controlProps = {
+					id: props.id,
+					name: attr.name,
+					required : this._isRequired(),
+					disabled : this._isDisabled(),
+					readOnly : this._isReadOnly(),
+					focus: this.props.focus,
+					value : props.value,
+					onValueChange : this._handleValueChange,
+					onBlur : this.props.onBlur
+				};
+				
 				switch(attr.fieldType) {
 					case 'BOOL':
-						return molgenis.ui.BoolControl({
-							id: props.id,
-							name: attr.name,
+						return molgenis.ui.BoolControl(_.extend({}, controlProps, {
 							label : props.label,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
 							layout : props.layout || 'horizontal',
-							value : props.value,
-							onValueChange : this._handleValueChange
-						});
+						}));
 					case 'CATEGORICAL':
 						if(this.state.options === undefined) {
 							// options not yet available
@@ -56,17 +62,12 @@
 						}
 						
 						var CategoricalControl = props.multiple === true ? molgenis.ui.CheckboxGroup : molgenis.ui.RadioGroup;
-						return CategoricalControl({
-							id: props.id,
-							name: attr.name,
+						return CategoricalControl(_.extend({}, controlProps, {
 							options : this.state.options,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
 							layout : props.layout || 'vertical',
 							value : props.value !== undefined ? props.value[attr.refEntity.idAttribute] : undefined,
 							onValueChange : this._handleValueChange // FIXME go from id to entity
-						});
+						}));
 					case 'CATEGORICAL_MREF':
 						if(this.state.options === undefined) {
 							// options not yet available
@@ -76,25 +77,19 @@
 						var values = props.value ? _.map(props.value.items, function(item) {
 							return item[attr.refEntity.idAttribute];
 						}) : [];
-						return molgenis.ui.CheckboxGroup({
-							id: props.id,
-							name: attr.name,
+						return molgenis.ui.CheckboxGroup(_.extend({}, controlProps, {
 							options : this.state.options,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
 							layout : 'vertical', // FIXME make configurable
-							value : values,
-							onValueChange : this._handleValueChange
-						});
+							value : values
+						}));
 					case 'DATE':
-						return this._createDateControl(false, props.placeholder || 'Date');
+						return this._createDateControl(controlProps, false, props.placeholder || 'Date');
 					case 'DATE_TIME':
-						return this._createDateControl(true, props.placeholder || 'Date');
+						return this._createDateControl(controlProps, true, props.placeholder || 'Date');
 					case 'DECIMAL':
-						return this._createNumberControl('any');
+						return this._createNumberControl(controlProps, 'any');
 					case 'EMAIL':
-						return this._createStringControl('email', props.placeholder || 'Email');
+						return this._createStringControl(controlProps, 'email', props.placeholder || 'Email');
 					case 'ENUM':
 						if(this.state.options === undefined) {
 							// options not yet available
@@ -103,52 +98,33 @@
 						
 						var EnumControl = props.multiple === true ? molgenis.ui.CheckboxGroup : molgenis.ui.RadioGroup;
 						return EnumControl({
-							id: props.id,
-							name: attr.name,
 							options : this.state.options,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
 							layout : props.layout,
-							value : props.value,
-							onValueChange : this._handleValueChange
 						});
 					case 'HTML':
 						return molgenis.ui.CodeEditor({
-							id : this.props.id,
-							name: attr.name,
 							placeholder : this.props.placeholder,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
-							language: 'html',
-							value : this.props.value,
-							onValueChange : this._handleValueChange
+							language: 'html'
 						});
 					case 'HYPERLINK':
-						return this._createStringControl('url', props.placeholder || 'URL');
+						return this._createStringControl(controlProps, 'url', props.placeholder || 'URL');
 					case 'INT':
 					case 'LONG':
-						return this._createNumberControl('1');
+						return this._createNumberControl(controlProps, '1');
 					case 'XREF':
-						return this._createEntitySelectBox(props.multiple || false, props.placeholder || 'Search for a Value', props.value);
+						return this._createEntitySelectBox(controlProps, props.multiple || false, props.placeholder || 'Search for a Value', props.value);
 					case 'MREF':
-						return this._createEntitySelectBox(props.multiple || true, props.placeholder || 'Search for Values', props.value ? props.value.items : undefined);
+						return this._createEntitySelectBox(controlProps, props.multiple || true, props.placeholder || 'Search for Values', props.value ? props.value.items : undefined);
 					case 'SCRIPT':
 						return molgenis.ui.CodeEditor({
-							id : this.props.id,
-							name : attr.name,
 							placeholder : this.props.placeholder,
-							required : this._isRequired(),
-							disabled : this._isDisabled(),
-							readOnly : this._isReadOnly(),
-							value : this.props.value,
-							onValueChange : this._handleValueChange
 						});
 					case 'STRING':
-						return this._createStringControl('text', props.placeholder || '');
+						return this._createStringControl(controlProps, 'text', props.placeholder || '');
 					case 'TEXT':
-						return this._createTextAreaControl();
+						return molgenis.ui.TextArea({
+							placeholder : this.props.placeholder,
+						});
 					case 'COMPOUND' :
 					case 'FILE':
 					case 'IMAGE':
@@ -180,84 +156,38 @@
 		_isReadOnly: function() {
 			return this.props.readOnly !== undefined ? this.props.readOnly : this.state.attr.readOnly;
 		},
-		_createNumberControl: function(step) {
+		_createNumberControl: function(controlProps, step) {
 			var min = this.props.range ? this.props.range.min : undefined;
 			var max = this.props.range ? this.props.range.max : undefined;
 			var placeholder = this.props.placeholder || 'Number';
-			return molgenis.ui.Input({
+			return molgenis.ui.Input(_.extend({}, controlProps, {
 				type : 'number',
-				id : this.props.id,
-				name : this.state.attr.name,
 				placeholder : placeholder,
-				required : this._isRequired(),
-				disabled : this._isDisabled(),
-				readOnly : this._isReadOnly(),
 				step : step,
 				min : min,
 				max : max,
-				value : this.props.value,
-				onValueChange : this._handleValueChange,
-				onBlur : this.props.onBlur
-			});
+			}));
 		},
-		_createStringControl: function(type, placeholder) {
-			return molgenis.ui.Input({
+		_createStringControl: function(controlProps, type, placeholder) {
+			return molgenis.ui.Input(_.extend({}, controlProps, {
 				type : type,
-				id : this.props.id,
-				name : this.state.attr.name,
 				placeholder : placeholder,
-				required : this._isRequired(),
-				disabled : this._isDisabled(),
-				readOnly : this._isReadOnly(),
 				maxlength : '255',
-				focus: this.props.focus,
-				value : this.props.value,
-				onValueChange : this._handleValueChange,
-				onBlur : this.props.onBlur
-			});
+			}));
 		},
-		_createDateControl: function(time, placeholder) {
-			return molgenis.ui.DateControl({
-				id : this.props.id,
-				name : this.state.attr.name,
+		_createDateControl: function(controlProps, time, placeholder) {
+			return molgenis.ui.DateControl(_.extend({}, controlProps, {
 				placeholder : placeholder,
-				required : this._isRequired(),
-				disabled : this._isDisabled(),
-				readOnly : this._isReadOnly(),
 				time : time,
-				focus: this.props.focus,
-				value : this.props.value,
-				onValueChange : this._handleValueChange
-			});
+			}));
 		},
-		_createTextAreaControl: function() {
-			return molgenis.ui.TextArea({
-				id : this.props.id,
-				name : this.state.attr.name,
-				placeholder : this.props.placeholder,
-				required : this._isRequired(),
-				disabled : this._isDisabled(),
-				readOnly : this._isReadOnly(),
-				value : this.props.value,
-				onValueChange : this._handleValueChange
-			});
-		},
-		_createEntitySelectBox: function(multiple, placeholder, value) {
-			var props = this.props;
-			return molgenis.ui.EntitySelectBox({
-				id : props.id,
-				name : this.state.attr.name,
+		_createEntitySelectBox: function(controlProps, multiple, placeholder, value) {
+			return molgenis.ui.EntitySelectBox(_.extend({}, controlProps, {
 				mode: 'create',
 				placeholder : placeholder,
 				multiple : multiple,
-				required : this._isRequired(),
-				disabled : this._isDisabled(),
-				readOnly : this._isReadOnly(),
-				focus: this.props.focus,
 				entity : this.state.attr.refEntity,
-				value : value,
-				onValueChange : this._handleValueChange
-			});
+			}));
 		},
 		_onAttrInit: function() {
 			var attr = this.state.attr;
