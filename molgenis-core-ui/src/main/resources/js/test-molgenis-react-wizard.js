098c560c819ffbcb29c84f739ad681f4f8117ccc
dennishendriksen <d.hendriksen@umcg.nl>
2015-03-31 20:48:17 +0200

    remove examples (reintroduce in future)

--- molgenis-core-ui/src/main/resources/js/test-molgenis-react-wizard.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	var steps = [
-         {name: 'Step #0', content: React.DOM.h1({}, 'Content #0')},
-         {name: 'Step #1', content: React.DOM.h1({}, 'Content #1')},
-         {name: 'Step #2', content: React.DOM.h1({}, 'Content #2')},
-         {name: 'Step #3', content: React.DOM.h1({}, 'Content #3')}
-	];
-	React.render(molgenis.control.Wizard({steps: steps}), $('#wizard-container')[0]);
-});
