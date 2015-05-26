df74395cab21348035201be04bc219f880804c5e
erwinwinder <j.a.winder@umcg.nl>
2015-05-26 15:38:28 +0200

    Add OwnedDecorator if needed

--- molgenis-core-ui/src/main/java/org/molgenis/ui/MolgenisWebAppConfig.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/MolgenisWebAppConfig.java
@@ -50,6 +50,8 @@ import org.molgenis.security.CorsInterceptor;
 import org.molgenis.security.core.MolgenisPermissionService;
 import org.molgenis.security.freemarker.HasPermissionDirective;
 import org.molgenis.security.freemarker.NotHasPermissionDirective;
+import org.molgenis.security.owned.OwnedEntityMetaData;
+import org.molgenis.security.owned.OwnedEntityRepositoryDecorator;
 import org.molgenis.ui.freemarker.FormLinkDirective;
 import org.molgenis.ui.freemarker.LimitMethod;
 import org.molgenis.ui.menu.MenuMolgenisUi;
@@ -60,6 +62,7 @@ import org.molgenis.ui.menumanager.MenuManagerServiceImpl;
 import org.molgenis.ui.security.MolgenisUiPermissionDecorator;
 import org.molgenis.util.ApplicationContextProvider;
 import org.molgenis.util.DependencyResolver;
+import org.molgenis.util.EntityUtils;
 import org.molgenis.util.FileStore;
 import org.molgenis.util.GsonHttpMessageConverter;
 import org.molgenis.util.IndexedRepositoryExceptionTranslatorDecorator;
@@ -469,6 +472,10 @@ public abstract class MolgenisWebAppConfig extends WebMvcConfigurerAdapter
 				if (repository instanceof IndexedRepository)
 				{
 					IndexedRepository indexedRepos = (IndexedRepository) repository;
+					if (EntityUtils.doesExtend(repository.getEntityMetaData(), OwnedEntityMetaData.ENTITY_NAME))
+					{
+						indexedRepos = new OwnedEntityRepositoryDecorator(indexedRepos);
+					}
 
 					return new IndexedCrudRepositorySecurityDecorator(new IndexedAutoValueRepositoryDecorator(
 							new IndexedRepositoryValidationDecorator(dataService(),
