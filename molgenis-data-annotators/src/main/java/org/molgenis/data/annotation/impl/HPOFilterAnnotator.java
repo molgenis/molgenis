5be3985d4c3e5ae8d9231de05153d56d8fbdbb99
Arjan Draisma <fabulousmcdonalds@gmail.com>
2015-06-16 15:22:16 +0200

    Added hpo validation & number to HPO converter

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/impl/HPOFilterAnnotator.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/impl/HPOFilterAnnotator.java
@@ -7,6 +7,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
+import java.util.regex.Pattern;
 
 import net.sf.samtools.util.RuntimeEOFException;
 
@@ -30,6 +31,8 @@ import org.molgenis.framework.server.MolgenisSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.core.boolex.Matcher;
+
 /**
  * exclusive and common genes per phenotype / hpo term
  * 
@@ -50,7 +53,8 @@ class HPOFilterAnnotator extends LocusAnnotator
 	// time out for waiting for the HPOFilterDataProvider to complete is 10 seconds
 	Long TIME_OUT = 10000L;
 	
-	String hpo = "HP:0000769";
+	// TODO These parameters need to be set by the user!
+	String hpo = "12345";
 	boolean recursive = true;
 	
 	@Autowired
@@ -98,6 +102,7 @@ class HPOFilterAnnotator extends LocusAnnotator
 		return true;
 	}
 	
+	
 	@Override
 	public List<Entity> annotateEntity(Entity entity) throws IOException, InterruptedException 
 	{
@@ -108,6 +113,10 @@ class HPOFilterAnnotator extends LocusAnnotator
 		Locus l = new Locus(c, pos);
 		List<Entity> results = new ArrayList<>();
 		
+		if (validateHpo(hpo) == 2)
+			hpo = convertNumberToHPO(hpo);
+		
+		// Throw an error if the HPO term specified by the user does not exist
 		if (!hpoFilterData.getHPOData().containsKey(hpo))
 			throw new RuntimeException(hpo+" does not exist!");
 		
@@ -123,6 +132,15 @@ class HPOFilterAnnotator extends LocusAnnotator
 		return results;
 	}
 	
+	/**
+	 * Checks if a specified hpo contains a specified gene. If 
+	 * recursive = true, it will also check the specified HPO's 
+	 * children.
+	 * @param hpo the filter HPO term
+	 * @param gene the variants' gene
+	 * @param recursive true if searching children, false if not.
+	 * @return true if HPO contains gene, false if hpo does not contain gene
+	 */
 	private boolean HPOContainsGene(String hpo, String gene, boolean recursive) {
 		if (hpoFilterData.getAssocData().containsKey(hpo)) 
 			if (hpoFilterData.getAssocData().get(hpo).contains(gene))
@@ -132,5 +150,42 @@ class HPOFilterAnnotator extends LocusAnnotator
 					if (HPOContainsGene(child, gene, true))
 						return true;
 		return false;
+	}
+	
+	/**
+	 * validates the HPO term as input by the user
+	 * @param input HPO term to validate
+	 * @return 1 if exact term, 2 if just numbers, 0 if invalid
+	 */
+	private int validateHpo (String input) {
+		if (input.matches("(HP:[0-9]{7})")) 
+			return 1;
+		if (input.matches("([0-9]{1,7})"))
+			return 2;
+		return 0;
+	}
+	
+	/**
+	 * converts any number of 1-7 digits as a valid HPO term. Returns null if
+	 * 1 > length > 8.<br>
+	 * <br>
+	 * Example: 123 -> HP:0000123
+	 * @param num
+	 * @return a valid hpo number
+	 */
+	public String convertNumberToHPO(String num) {
+		for (String number : num.split("([^\\d]+)")) {
+			StringBuilder sb = new StringBuilder("HP:");
+			if (number.length() < 8) {
+				for (int i = number.length(); i < 7; i++) {
+					sb.append('0');
+				}
+				sb.append(number);
+				return sb.toString();
+			}else{
+				return null;
+			}
+		}
+		return null;
 	}
 }
