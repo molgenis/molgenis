68dd570c0dc1ed2e092ca05496e09cec78e25e66
erwinwinder <j.a.winder@umcg.nl>
2015-06-17 16:22:49 +0200

    Added test

---
+++ molgenis-data/src/test/java/org/molgenis/data/transaction/MolgenisTransactionManagerTest.java
@@ -0,0 +1,38 @@
+package org.molgenis.data.transaction;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MolgenisTransactionManagerTest
+{
+	private MolgenisTransactionManager molgenisTransactionManager;
+	private EntityManagerFactory mockEntityManagerFactory;
+
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		molgenisTransactionManager = new MolgenisTransactionManager();
+
+		mockEntityManagerFactory = mock(EntityManagerFactory.class);
+		molgenisTransactionManager.setEntityManagerFactory(mockEntityManagerFactory);
+	}
+
+	@Test
+	public void doGetTransaction()
+	{
+		Object trans = molgenisTransactionManager.doGetTransaction();
+		assertNotNull(trans);
+		assertTrue(trans instanceof MolgenisTransaction);
+
+		MolgenisTransaction molgenisTransaction = (MolgenisTransaction) trans;
+		assertNotNull(molgenisTransaction.getId());
+		assertNotNull(molgenisTransaction.getJpaTransaction());
+	}
+
+}
