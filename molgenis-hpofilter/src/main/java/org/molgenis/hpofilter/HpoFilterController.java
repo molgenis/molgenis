b1cd93bf2d540fa6197ea3ca9a7127f88ab2b811
ArjanDraisma <fabulousmcdonalds@gmail.com>
2015-06-19 15:59:17 +0200

    Fixed base hpofilter plugin controller file

--- molgenis-hpofilter/src/main/java/org/molgenis/hpofilter/HpoFilterController.java
+++ molgenis-hpofilter/src/main/java/org/molgenis/hpofilter/HpoFilterController.java
@@ -1,34 +1,14 @@
-package org.molgenis.catalogue;
+package org.molgenis.hpofilter;
 
-import static org.molgenis.catalogue.CatalogueController.URI;
-import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_ENTITY_READ_PREFIX;
-import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_SU;
-import static org.molgenis.security.core.utils.SecurityUtils.currentUserHasRole;
-import static org.springframework.http.HttpStatus.OK;
+import static org.molgenis.hpofilter.HpoFilterController.URI;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
-import org.apache.commons.lang3.StringUtils;
-import org.molgenis.catalogue.model.ShoppingCart;
-import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.DataService;
-import org.molgenis.data.EntityMetaData;
 import org.molgenis.framework.ui.MolgenisPluginController;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import com.google.common.collect.Lists;
 
 @Controller
 @RequestMapping(URI)
