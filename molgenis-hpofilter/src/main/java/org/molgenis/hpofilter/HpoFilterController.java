7cf248596f94b3b2e5285e3ffbbbc91ef08ab4c2
CrudeOil <adraisma@gmail.com>
2015-06-23 12:54:23 +0200

    Added filter logic

--- molgenis-hpofilter/src/main/java/org/molgenis/hpofilter/HpoFilterController.java
+++ molgenis-hpofilter/src/main/java/org/molgenis/hpofilter/HpoFilterController.java
@@ -5,17 +5,33 @@ import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_ENTITY_RE
 import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_SU;
 import static org.molgenis.security.core.utils.SecurityUtils.currentUserHasRole;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.cluster.metadata.RepositoriesMetaData;
+import org.elasticsearch.common.collect.Iterables;
+import org.molgenis.data.AggregateQuery;
+import org.molgenis.data.AggregateResult;
 import org.molgenis.data.DataService;
+import org.molgenis.data.Entity;
 import org.molgenis.data.EntityMetaData;
+import org.molgenis.data.Query;
+import org.molgenis.data.Repository;
+import org.molgenis.data.RepositoryCapability;
+import org.molgenis.data.RepositoryDecoratorFactory;
+import org.molgenis.data.importer.VcfImportService;
+import org.molgenis.data.support.DataServiceImpl;
 import org.molgenis.framework.ui.MolgenisPluginController;
-import org.molgenis.hpofilter.HpoFilterDataProvider;
+import org.molgenis.hpofilter.data.GeneMapProvider;
+import org.molgenis.hpofilter.data.HpoFilterDataProvider;
+import org.molgenis.hpofilter.data.Locus;
+import org.molgenis.hpofilter.utils.HgncLocationsUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,17 +49,24 @@ public class HpoFilterController extends MolgenisPluginController
 	public static final String ID = "hpofilter";
 	public static final String URI = MolgenisPluginController.PLUGIN_URI_PREFIX + ID;
 	private static final String VIEW_NAME = "view-hpofilter";
-	private final DataService dataService;
+	private final DataServiceImpl dataService;
 	private final HpoFilterDataProvider hpoFilterDataProvider;
+	private GeneMapProvider hgncProvider;
+	private RepositoryDecoratorFactory repositoryDecoratorFactory;
 	
 	private HashMap<String, String> autoCompletionMap;
 
 	@Autowired
-	public HpoFilterController(DataService dataService, HpoFilterDataProvider hpoFilterDataProvider)
+	public HpoFilterController(DataService dataService,
+			HpoFilterDataProvider hpoFilterDataProvider,
+			GeneMapProvider hgncProvider,
+			RepositoryDecoratorFactory repositoryDecoratorFactory)
 	{
 		super(URI);
-		this.dataService = dataService;
+		this.dataService = new DataServiceImpl();
 		this.hpoFilterDataProvider = hpoFilterDataProvider;
+		this.hgncProvider = hgncProvider;
+		this.repositoryDecoratorFactory = repositoryDecoratorFactory;
 	}
 
 	@RequestMapping
@@ -51,6 +74,7 @@ public class HpoFilterController extends MolgenisPluginController
 	{
 		boolean showEntitySelect = true;
 		List<EntityMetaData> emds = Lists.newArrayList();
+		
 		for (String entityName : dataService.getEntityNames())
 		{
 			if (currentUserHasRole(AUTHORITY_SU, AUTHORITY_ENTITY_READ_PREFIX + entityName.toUpperCase()))
@@ -104,7 +128,62 @@ public class HpoFilterController extends MolgenisPluginController
 		return response.toString();
 	}
 	
-	private void run () {
-		
+	@RequestMapping(method = RequestMethod.POST, value = "/filter")
+	private @ResponseBody Boolean filter(@RequestParam(value = "terms", required = true) String terms,
+			@RequestParam(value = "entity", required = true) String selectedEntityName,
+			@RequestParam(value = "recursive", required = false) boolean recursive,
+			Model model) {
+		try{
+			Repository repository;
+			
+			String chrom;
+			Long pos;
+			Locus locus;
+			List<String> genes;
+			if (null != selectedEntityName) {
+				repository = dataService.getManageableRepository(selectedEntityName);
+				if (null == repository.getEntityMetaData().getAttribute("#CHROM") || null == repository.getEntityMetaData().getAttribute("POS"))
+					return false;
+			}else{
+				return false;
+			}
+			
+			Iterator<Entity> e = repository.iterator();
+			while (e.hasNext()) {
+				Entity entity = e.next();
+				chrom = entity.getString("#CHROM");
+				pos = entity.getLong("POS");
+				locus =  new Locus(chrom, pos);
+				genes = HgncLocationsUtils.locationToHgcn(hgncProvider.getHgncLocations(), locus);
+				for (String gene : genes) {
+					if (HPOContainsGene(terms, gene, true))
+						System.out.println(entity.getString("POS"));
+				}
+			}
+			return true;
+		}catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Checks if a specified hpo contains a specified gene. If 
+	 * recursive = true, it will also check the specified HPO's 
+	 * children.
+	 * @param hpo the filter HPO term
+	 * @param gene the variants' gene
+	 * @param recursive true if searching children, false if not.
+	 * @return true if HPO contains gene, false if hpo does not contain gene
+	 */
+	private boolean HPOContainsGene(String hpo, String gene, boolean recursive) {
+		if (hpoFilterDataProvider.getAssocData().containsKey(hpo)) 
+			if (hpoFilterDataProvider.getAssocData().get(hpo).contains(gene))
+				return true;
+			if (recursive)
+				for (String child : hpoFilterDataProvider.getHPOData().get(hpo))
+					if (HPOContainsGene(child, gene, true))
+						return true;
+		return false;
 	}
 }
