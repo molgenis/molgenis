853eb699c551035100a895a995bd30aacb536f93
ChaoPang <ChaoPang229@gmail.com>
2015-06-23 15:42:37 +0200

    fixed the test of PudMedTermFrequency

--- molgenis-ontology-core/src/test/java/org/molgenis/ontology/ic/PubMedTermFrequencyServiceTest.java
+++ molgenis-ontology-core/src/test/java/org/molgenis/ontology/ic/PubMedTermFrequencyServiceTest.java
@@ -12,9 +12,9 @@ public class PubMedTermFrequencyServiceTest
 	public void testRegexPattern()
 	{
 		assertEquals(
-				termFrequencyService
-						.parseResponse("<eSearchResult><Count>4603</Count><RetMax>20</RetMax><RetStart>0</RetStart><IdList>"),
-				new Integer(4603));
+				termFrequencyService.parseResponse(
+						"<eSearchResult><Count>4603</Count><RetMax>20</RetMax><RetStart>0</RetStart><IdList>")
+						.getOccurrence(), 4603);
 		assertEquals(
 				termFrequencyService.parseResponse("<eSearchResult><RetMax>20</RetMax><RetStart>0</RetStart><IdList>"),
 				null);
