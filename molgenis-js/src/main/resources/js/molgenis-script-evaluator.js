b9ba4402238f63a0f443bd52f893b43238d1fe8b
Fleur Kelpin <f.kelpin@umcg.nl>
2015-06-25 12:34:47 +0200

    Add unit conversion.

--- molgenis-js/src/main/resources/js/molgenis-script-evaluator.js
+++ molgenis-js/src/main/resources/js/molgenis-script-evaluator.js
@@ -1,4 +1,31 @@
 function evalScript(script, entity) {
+	var table = {};
+	(function () {
+
+	    function addUnit (baseUnit, actualUnit, multiplier) {
+	        table[actualUnit] = { base: baseUnit, actual: actualUnit, multiplier: multiplier };
+	    };
+
+	    var prefixes = ['Y', 'Z', 'E', 'P', 'T', 'G', 'M', 'k', 'h', 'da', '', 'd', 'c', 'm', 'u', 'n', 'p', 'f', 'a', 'z', 'y'];
+	    var factors = [24, 21, 18, 15, 12, 9, 6, 3, 2, 1, 0, -1, -2, -3, -6, -9, -12, -15, -18, -21, -24];
+	    // SI units only, that follow the mg/kg/dg/cg type of format
+	    var units = ['g', 'b', 'l', 'm'];
+
+	    for (var j = 0; j < units.length; j++) {
+	        var base = units[j];
+	        for (var i = 0; i < prefixes.length; i++) {
+	            addUnit(base, prefixes[i] + base, Math.pow(10, factors[i]));
+	        }
+	    }
+
+	    // we use the SI gram unit as the base; this allows
+	    // us to convert between SI and English units
+	    addUnit('g', 'ounce', 28.3495231);
+	    addUnit('g', 'oz', 28.3495231);
+	    addUnit('g', 'pound', 453.59237);
+	    addUnit('g', 'lb', 453.59237);
+	})();
+
 	function $(attr) { 
 		var attribute = {
 			value: function() {
@@ -78,6 +105,22 @@ function evalScript(script, entity) {
 			le: function(value) {
 				this.val = _isNull(this.val) ? false : (this.val <= value);
 				return this;
+			},
+			unit: function(newUnit) {
+				if(!newUnit){
+					return this.unit;
+				}
+				this.unit = newUnit;
+				return this;
+			},
+			toUnit: function(targetUnit) {
+				var target = table[targetUnit];
+		        var current = table[this.unit];
+		        if (target.base != current.base) {
+		            throw new Error('Incompatible units; cannot convert from "' + this.unit + '" to "' + targetUnit + '"');
+		        }
+		        this.val = this.val * (current.multiplier / target.multiplier);
+		        return this;
 			}
 		}
 		
