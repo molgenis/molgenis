e1867045b7cffb1d6fa24d9aa0aaeaf6b68781dc
K. Joeri van der Velde <joeriv@gmail.com>
2015-07-01 13:38:42 +0200

    Reference fixed

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/MergeDenovoFromPhaseByTransmissionWithVCF.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/MergeDenovoFromPhaseByTransmissionWithVCF.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+import org.molgenis.data.annotator.tabix.TabixReader;
+
 public class MergeDenovoFromPhaseByTransmissionWithVCF
 {
 
