b7658fee52b1288cfeca51f972161e0af97de327
K. Joeri van der Velde <joeriv@gmail.com>
2015-07-02 15:01:32 +0200

    Fixed writing of VCF description fields

--- molgenis-data-vcf/src/main/java/org/molgenis/data/vcf/utils/VcfUtils.java
+++ molgenis-data-vcf/src/main/java/org/molgenis/data/vcf/utils/VcfUtils.java
@@ -34,7 +34,7 @@ public class VcfUtils
 	 */
 	public static String convertToVCF(Entity vcfEntity) throws MolgenisDataException
 	{
-		
+
 		StringBuilder vcfRecord = new StringBuilder();
 
 		List<String> vcfAttributes = Arrays.asList(new String[]
@@ -251,8 +251,11 @@ public class VcfUtils
 								// type and nillable
 		sb.append(",Type=");
 		sb.append(toVcfDataType(infoAttributeMetaData.getDataType().getEnumType()));
-		sb.append(",Description=");
-		sb.append(infoAttributeMetaData.getDescription());
+		sb.append(",Description=\"");
+		// http://samtools.github.io/hts-specs/VCFv4.1.pdf --> "The Description value must be surrounded by
+		// double-quotes. Double-quote character can be escaped with backslash \ and backslash as \\."
+		sb.append(infoAttributeMetaData.getDescription().replace("\\", "\\\\").replace("\"", "\\\""));
+		sb.append("\">");
 		return sb.toString();
 	}
 
