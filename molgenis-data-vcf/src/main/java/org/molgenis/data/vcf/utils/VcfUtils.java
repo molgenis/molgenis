ce083961692d6e31a991f131bd2964bcb06a983b
K. Joeri van der Velde <joeriv@gmail.com>
2015-07-02 13:10:24 +0200

    Cmd line cadd and exac annotators with new framework running again

--- molgenis-data-vcf/src/main/java/org/molgenis/data/vcf/utils/VcfUtils.java
+++ molgenis-data-vcf/src/main/java/org/molgenis/data/vcf/utils/VcfUtils.java
@@ -34,6 +34,7 @@ public class VcfUtils
 	 */
 	public static String convertToVCF(Entity vcfEntity) throws MolgenisDataException
 	{
+		
 		StringBuilder vcfRecord = new StringBuilder();
 
 		List<String> vcfAttributes = Arrays.asList(new String[]
