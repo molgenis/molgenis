52115d99e10ef8cef6ede5c50978dbe089262df6
Mark-de-Haan <markdehaan90@gmail.com>
2015-07-07 17:33:44 +0200

    Added reminder to write tests

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/entity/impl/ExacAnnotator.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/entity/impl/ExacAnnotator.java
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ExacAnnotator
 {
-
+	// TODO Write test
 	public static final String EXAC_AF = "EXAC_AF";
 	public static final String EXAC_AF_LABEL = "ExAC allele frequency";
 	public static final String EXAC_AF_ResourceAttributeName = VcfRepository.getInfoPrefix() + "AF";
