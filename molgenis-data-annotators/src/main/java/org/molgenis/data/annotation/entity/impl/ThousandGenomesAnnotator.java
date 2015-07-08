520eefaadb80ae697a28fd106a785926956c80cb
brammiedema <bram_miedema@hotmail.com>
2015-07-08 09:29:22 +0200

    merging with mark

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/entity/impl/ThousandGenomesAnnotator.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/entity/impl/ThousandGenomesAnnotator.java
@@ -10,6 +10,7 @@ import org.molgenis.data.annotation.entity.AnnotatorInfo;
 import org.molgenis.data.annotation.entity.AnnotatorInfo.Status;
 import org.molgenis.data.annotation.entity.EntityAnnotator;
 import org.molgenis.data.annotation.resources.MultiResourceConfig;
+import org.molgenis.data.annotation.resources.Resource;
 import org.molgenis.data.annotation.resources.Resources;
 import org.molgenis.data.annotation.resources.impl.MultiFileResource;
 import org.molgenis.data.annotation.resources.impl.MultiResourceConfigImpl;
@@ -24,45 +25,62 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ThousandGenomesAnnotator
 {
-
-	public static final String THOUSAND_GENOMES_AF = "THOUSAND_GENOMES_AF";
-	public static final String THOUSAND_GENOMES_AF_LABEL = "ThousandGenomes allele frequency";
-	public static final String THOUSAND_GENOMES_AF_ResourceAttributeName = VcfRepository.getInfoPrefix() + "AF";
-
-	public static final String THOUSAND_GENOMES_FILE_LOCATION_PROPERTY = "thousandgenomesc_location";
-	public static final String THOUSAND_GENOMES_TABIX_RESOURCE = "ThousandGenomesTabixResource";
+	// TODO Write test
+	public static final String THOUSAND_GENOME_AF = "1000G_AF";
+	public static final String THOUSAND_GENOME_AF_LABEL = "Thousand genome allele frequency";
+	public static final String THOUSAND_GENOME_AF_RESOURCE_ATTRIBUTE_NAME = VcfRepository.getInfoPrefix() + "AF";
+	public static final String THOUSAND_GENOME_CHROMOSOME_PROPERTY = "thousand_genome_chromosomes";
+	public static final String THOUSAND_GENOME_FILE_PATTERN_PROPERTY = "thousand_genome_file_pattern";
+	public static final String THOUSAND_GENOME_FOLDER_PROPERTY = "thousand_genome_root_directory";
+	public static final String THOUSAND_GENOME_MULTI_FILE_RESOURCE = "thousandGenomeResources";
 
 	@Autowired
 	private MolgenisSettings molgenisSettings;
+
 	@Autowired
 	private DataService dataService;
+
 	@Autowired
 	private Resources resources;
 
 	@Bean
-	public RepositoryAnnotator ThousandGenomes()
+	public RepositoryAnnotator thousandGenome()
 	{
-		// TODO: description
-		DefaultAttributeMetaData outputAttribute = new DefaultAttributeMetaData(THOUSAND_GENOMES_AF,
-				FieldTypeEnum.STRING).setDescription("TODO").setLabel(THOUSAND_GENOMES_AF_LABEL);
+		DefaultAttributeMetaData outputAttribute = new DefaultAttributeMetaData(THOUSAND_GENOME_AF,
+				FieldTypeEnum.STRING).setDescription(
+				"The allele frequency for variants seen in the population used for the thousand genomes project")
+				.setLabel(THOUSAND_GENOME_AF_LABEL);
 
-		AnnotatorInfo thousandGenomesInfo = AnnotatorInfo.create(Status.BETA, AnnotatorInfo.Type.POPULATION_REFERENCE,
-				"ThousandGenomes", "TODO", Collections.singletonList(outputAttribute));
+		AnnotatorInfo thousandGenomeInfo = AnnotatorInfo
+				.create(Status.BETA,
+						AnnotatorInfo.Type.POPULATION_REFERENCE,
+						"Thousand Genomes",
+						"The 1000 Genomes Project is an international collaboration to produce an "
+								+ "extensive public catalog of human genetic variation, including SNPs and structural variants, "
+								+ "and their haplotype contexts. This resource will support genome-wide association studies and other "
+								+ "medical research studies. "
+								+ "The genomes of about 2500 unidentified people from about 25 populations around the world will be"
+								+ "sequenced using next-generation sequencing technologies. "
+								+ "The results of the study will be freely and publicly accessible to researchers worldwide. "
+								+ "Further information about the project is available in the About tab. Information about downloading, "
+								+ "browsing or using the 1000 Genomes data is available in the Data tab.",
+						Collections.singletonList(outputAttribute));
 
-		// TODO: properly test multiAllelicResultFilter
 		LocusQueryCreator locusQueryCreator = new LocusQueryCreator();
+
+		// TODO: properly test multiAllelicFresultFilter
 		MultiAllelicResultFilter multiAllelicResultFilter = new MultiAllelicResultFilter(
-				Collections.singletonList(new DefaultAttributeMetaData(THOUSAND_GENOMES_AF_ResourceAttributeName,
+				Collections.singletonList(new DefaultAttributeMetaData(THOUSAND_GENOME_AF_RESOURCE_ATTRIBUTE_NAME,
 						FieldTypeEnum.DECIMAL)));
-		EntityAnnotator entityAnnotator = new AnnotatorImpl(THOUSAND_GENOMES_TABIX_RESOURCE, thousandGenomesInfo,
+		EntityAnnotator entityAnnotator = new AnnotatorImpl(THOUSAND_GENOME_MULTI_FILE_RESOURCE, thousandGenomeInfo,
 				locusQueryCreator, multiAllelicResultFilter, dataService, resources)
 		{
 			@Override
 			protected String getResourceAttributeName(AttributeMetaData attr)
 			{
-				if (THOUSAND_GENOMES_AF.equals(attr.getName()))
+				if (THOUSAND_GENOME_AF.equals(attr.getName()))
 				{
-					return THOUSAND_GENOMES_AF_ResourceAttributeName;
+					return THOUSAND_GENOME_AF_RESOURCE_ATTRIBUTE_NAME;
 				}
 				return attr.getName();
 			}
@@ -72,14 +90,12 @@ public class ThousandGenomesAnnotator
 	}
 
 	@Bean
-	MultiFileResource thousandGenomesResources()
+	Resource thousandGenomeSources()
 	{
-		MultiResourceConfig thousandGenomesConfig = new MultiResourceConfigImpl("1000g_chromosomes",
-				"1000g_chromosome_file_pattern", "1000g_location", molgenisSettings);
+		MultiResourceConfig thousandGenomeConfig = new MultiResourceConfigImpl(THOUSAND_GENOME_CHROMOSOME_PROPERTY,
+				THOUSAND_GENOME_FILE_PATTERN_PROPERTY, THOUSAND_GENOME_FOLDER_PROPERTY, molgenisSettings);
 
-		MultiFileResource thousandGenomesMultiResource = new MultiFileResource("1000g_annotator",
-				thousandGenomesConfig, new TabixVcfRepositoryFactory(THOUSAND_GENOMES_TABIX_RESOURCE));
-
-		return thousandGenomesMultiResource;
+		return new MultiFileResource(THOUSAND_GENOME_MULTI_FILE_RESOURCE, thousandGenomeConfig,
+				new TabixVcfRepositoryFactory(THOUSAND_GENOME_MULTI_FILE_RESOURCE));
 	}
 }
