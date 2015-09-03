df23876731828bcd2568d460da945e121010323c
Mark-de-Haan <markdehaan90@gmail.com>
2015-09-03 09:24:31 +0200

    Fix comments

--- molgenis-data-import/src/main/java/org/molgenis/data/importer/ImportedDataToBackendCompatibilityValidator.java
+++ molgenis-data-import/src/main/java/org/molgenis/data/importer/ImportedDataToBackendCompatibilityValidator.java
@@ -5,8 +5,6 @@ import org.molgenis.data.EntityMetaData;
 import org.molgenis.data.MolgenisDataException;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.collect.ImmutableCollection;
-
 /**
  * This class serves as an early validator. It can check if an entity is valid before we process parsed data into
  * repositories.
@@ -22,22 +20,15 @@ public class ImportedDataToBackendCompatibilityValidator
 		this.dataService = dataService;
 	}
 
-	public void validate(ImmutableCollection<EntityMetaData> immutableCollection)
+	public void validate(Iterable<EntityMetaData> immutableCollection)
 	{
 		immutableCollection.forEach(entity -> checkIfEntityExists(entity));
 	}
 
 	private void checkIfEntityExists(EntityMetaData entity)
 	{
-		if (dataService.hasRepository(entity.getName()))
+		if (dataService.hasRepository(entity.getName()) || dataService.hasRepository(entity.getName().toLowerCase()))
 		{
-			System.out.println("Test if get here");
-			throw new MolgenisDataException("Entity with name [" + entity.getName()
-					+ "] already exists. Casing is ignored!");
-		}
-		else if (dataService.hasRepository(entity.getName().toLowerCase()))
-		{
-			System.out.println("Test if get here");
 			throw new MolgenisDataException("Entity with name [" + entity.getName()
 					+ "] already exists. Casing is ignored!");
 		}
