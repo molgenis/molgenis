2025d7e6ed1f7d576b3143bc9ab384959317a938
jjettenn <jjettenn@gmail.com>
2015-09-10 09:55:17 +0200

    Fix migration step 17

--- molgenis-data-migrate/src/main/java/org/molgenis/data/version/v1_9/Step17AddAlgorithmStateDiscuss.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/data/version/v1_9/Step17AddAlgorithmStateDiscuss.java
@@ -10,12 +10,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.google.common.base.Preconditions;
 
 /**
- * Migration for the introduction of attributes attribute "defaultValue".
+ * Migration for the AttributeMapping table
  * <ol>
- * <li>Creates the column in the database</li>
- * <li>Iterates over the already imported JPA entities to write their defaultValue to the newly created column</li>
- * <li>Sets the defaultValue for the generateToken attribute of the Script entity</li>
- * <li>Recreates the attributes index in elasticsearch to reflect these changes</li>
+ * <li>Add a column `algorithmState` enum('CURATED','GENERATED_HIGH','GENERATED_LOW','DISCUSS') to AttributeMapping</li>
  * </ol>
  * 
  */
@@ -33,12 +30,9 @@ public class Step17AddAlgorithmStateDiscuss extends MolgenisUpgrade
 	@Override
 	public void upgrade()
 	{
-		LOG.info("Updating metadata from version 15 to 16");
-		LOG.info("Updating AttributeMapping --- add DISCUSS enum");
+		LOG.info("Update metadata of the AttributeMapping from version 1.8 to 1.9");
+		LOG.info("Alter AttributeMapping --- add column `algorithmState` enum('CURATED','GENERATED_HIGH','GENERATED_LOW','DISCUSS');");
 		jdbcTemplate
-				.execute("ALTER TABLE `AttributeMapping` CHANGE `algorithmState` `algorithmState` ENUM('CURATED','GENERATED_HIGH','GENERATED_LOW','DISCUSS');");
-		LOG.info("Updating attributes --- add DISCUSS enumOptions");
-		jdbcTemplate
-				.execute("UPDATE `attributes` SET `enumOptions` = 'CURATED,GENERATED_HIGH,GENERATED_LOW,DISCUSS' WHERE `name` = 'algorithmState' AND `dataType` = 'enum' AND `enumOptions` = 'CURATED,GENERATED_HIGH,GENERATED_LOW';");
+				.execute("Use `omx`; ALTER TABLE AttributeMapping ADD `algorithmState` enum('CURATED','GENERATED_HIGH','GENERATED_LOW','DISCUSS');");
 	}
 }
