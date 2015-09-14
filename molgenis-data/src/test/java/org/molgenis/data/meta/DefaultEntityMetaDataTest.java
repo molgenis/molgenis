952147a21bfe9eca3a0f83cd3a4d5b6c3211728e
dennishendriksen <d.hendriksen@umcg.nl>
2015-09-14 09:59:24 +0200

    Fix #3665 Entity with extends and id attribute has no id attribute

--- molgenis-data/src/test/java/org/molgenis/data/meta/DefaultEntityMetaDataTest.java
+++ molgenis-data/src/test/java/org/molgenis/data/meta/DefaultEntityMetaDataTest.java
@@ -1,12 +1,14 @@
 package org.molgenis.data.meta;
 
+import static org.testng.Assert.assertEquals;
+
+import org.molgenis.data.support.DefaultAttributeMetaData;
 import org.molgenis.data.support.DefaultEntityMetaData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DefaultEntityMetaDataTest
 {
-
 	@Test
 	public void testCopyConstructorPreservesIdAttribute()
 	{
@@ -17,4 +19,16 @@ public class DefaultEntityMetaDataTest
 		Assert.assertEquals(emdCopy.getIdAttribute().getName(), "id");
 	}
 
+	// regression test for https://github.com/molgenis/molgenis/issues/3665
+	@Test
+	public void testExtendsEntityMetaDataMissingIdAttribute()
+	{
+		DefaultEntityMetaData extendsEntityMeta = new DefaultEntityMetaData("entity");
+		extendsEntityMeta.addAttribute("attr");
+
+		DefaultEntityMetaData entityMeta = new DefaultEntityMetaData("entity");
+		entityMeta.setExtends(extendsEntityMeta);
+		DefaultAttributeMetaData idAttr = entityMeta.addAttribute("id").setIdAttribute(true);
+		assertEquals(idAttr, entityMeta.getIdAttribute());
+	}
 }
