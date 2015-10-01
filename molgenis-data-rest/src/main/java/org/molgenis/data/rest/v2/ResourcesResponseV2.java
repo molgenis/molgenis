ad32e283ca8ca4d597bab46a0a9bbadb8803e1f6
jjettenn <jjettenn@gmail.com>
2015-10-01 15:23:58 +0200

    Return only href for created resources

--- molgenis-data-rest/src/main/java/org/molgenis/data/rest/v2/ResourcesResponseV2.java
+++ molgenis-data-rest/src/main/java/org/molgenis/data/rest/v2/ResourcesResponseV2.java
@@ -4,22 +4,15 @@ import static java.util.Objects.requireNonNull;
 
 public class ResourcesResponseV2
 {
-	private final String id;
-	private final String location;
+	private final String href;
 
-	public ResourcesResponseV2(Object id, String locationHref)
+	public ResourcesResponseV2(String href)
 	{
-		this.id = requireNonNull(id).toString();
-		this.location = requireNonNull(locationHref);
+		this.href = requireNonNull(href);
 	}
 
-	public String getId()
+	public String getHref()
 	{
-		return id;
-	}
-
-	public String getLocation()
-	{
-		return location;
+		return this.href;
 	}
 }
