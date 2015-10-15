915587a8d3270b210b9af6e1838824ba5a7dd4e2
tommydeboer <tommydeboer4@gmail.com>
2015-10-15 16:59:07 +0200

    Fix #3913

--- molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/MolgenisVersionService.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/MolgenisVersionService.java
@@ -39,7 +39,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MolgenisVersionService
 {
-	public static final int CURRENT_VERSION = 20;
+	public static final int CURRENT_VERSION = 21;
 
 	private static final Logger LOG = LoggerFactory.getLogger(MolgenisVersionService.class);
 
