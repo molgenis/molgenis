66501219a1b0a37a9750688f7ac747a603b18342
Fleur Kelpin <f.kelpin@umcg.nl>
2015-10-16 17:44:44 +0200

    Revert "Fix #3878 MySqlRepository returns entities out of order"
    This reverts commit a01db64973c5799f5657eb86010616ac20775d76.

--- molgenis-data-mysql/src/main/java/org/molgenis/data/mysql/MysqlRepository.java
+++ molgenis-data-mysql/src/main/java/org/molgenis/data/mysql/MysqlRepository.java
@@ -19,7 +19,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import javax.sql.DataSource;
 
@@ -35,7 +34,6 @@ import org.molgenis.data.MolgenisDataException;
 import org.molgenis.data.MolgenisReferencedEntityException;
 import org.molgenis.data.Query;
 import org.molgenis.data.QueryRule;
-import org.molgenis.data.QueryRule.Operator;
 import org.molgenis.data.Repository;
 import org.molgenis.data.RepositoryCapability;
 import org.molgenis.data.Sort;
@@ -717,7 +715,7 @@ public class MysqlRepository extends AbstractRepository implements Manageable
 		// group by
 		if (select.indexOf("GROUP_CONCAT") != -1 && group.length() > 0) result.append(" GROUP BY ").append(group);
 		// order by
-		result.append(' ').append(getSortSql(q, parameters));
+		result.append(' ').append(getSortSql(q));
 		// limit
 		if (q.getPageSize() > 0) result.append(" LIMIT ").append(q.getPageSize());
 		if (q.getOffset() > 0) result.append(" OFFSET ").append(q.getOffset());
@@ -934,7 +932,7 @@ public class MysqlRepository extends AbstractRepository implements Manageable
 		return result.toString().trim();
 	}
 
-	protected String getSortSql(Query q, List<Object> parameters)
+	protected String getSortSql(Query q)
 	{
 		StringBuilder sortSql = new StringBuilder();
 		if (q.getSort() != null)
@@ -956,32 +954,7 @@ public class MysqlRepository extends AbstractRepository implements Manageable
 
 			if (sortSql.length() > 0) sortSql = new StringBuilder("ORDER BY ").append(sortSql.substring(2));
 		}
-		// in case of a findAll(Iterable<Object> ids) MySQL returns matching rows in a random order. use ORDER BY FIELD
-		// to guarantee the right order. This is required by the Repository interface.
-		else if (q.getRules() != null && q.getRules().size() == 1)
-		{
-			QueryRule queryRule = q.getRules().iterator().next();
-			if (queryRule.getOperator() == Operator.IN && queryRule.getValue() instanceof Iterable<?>)
-			{
-				Iterable<?> values = (Iterable<?>) queryRule.getValue();
-				if (values != null && values.iterator().hasNext())
-				{
-					// use prepared statements for the values
-					String fieldIds = StreamSupport.stream(values.spliterator(), false).map(value -> "?")
-							.collect(Collectors.joining(","));
-
-					// define order of returned rows
-					sortSql = new StringBuilder("ORDER BY FIELD(").append("this.").append('`')
-							.append(queryRule.getField()).append('`').append(",").append(fieldIds).append(")");
-
-					// use prepared statements for the values
-					parameters.addAll(StreamSupport.stream(values.spliterator(), false).map(value -> value.toString())
-							.collect(Collectors.toList()));
-				}
-			}
-		}
 		return sortSql.toString();
-
 	}
 
 	protected String getUpdateSql()
