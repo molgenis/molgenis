d35a885947bcd64d0c257dc39ae88bbf2b0385d3
dennishendriksen <d.hendriksen@umcg.nl>
2015-11-10 08:02:42 +0100

    xref/mref fixes

--- molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/index/SourceToEntityConverter.java
+++ molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/index/SourceToEntityConverter.java
@@ -1,6 +1,5 @@
 package org.molgenis.data.elasticsearch.index;
 
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Iterator;
@@ -11,6 +10,7 @@ import org.molgenis.MolgenisFieldTypes.FieldTypeEnum;
 import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.DataService;
 import org.molgenis.data.Entity;
+import org.molgenis.data.EntityManager;
 import org.molgenis.data.EntityMetaData;
 import org.molgenis.data.MolgenisDataException;
 import org.molgenis.data.UnknownAttributeException;
@@ -23,11 +23,13 @@ import org.springframework.stereotype.Component;
 public class SourceToEntityConverter
 {
 	private final DataService dataService;
+	private final EntityManager entityManager;
 
 	@Autowired
-	public SourceToEntityConverter(DataService dataService)
+	public SourceToEntityConverter(DataService dataService, EntityManager entityManager)
 	{
 		this.dataService = requireNonNull(dataService);
+		this.entityManager = requireNonNull(entityManager);
 	}
 
 	public Entity convert(Map<String, Object> source, EntityMetaData entityMeta)
@@ -42,110 +44,105 @@ public class SourceToEntityConverter
 						"Unknown attribute [" + attrName + "] of entity [" + entityMeta.getName());
 			}
 
-			Object entityValue = convert(entityMeta, attr, entry.getValue());
+			Object sourceValue = entry.getValue();
+			Object entityValue;
+			if (sourceValue != null)
+			{
+				FieldTypeEnum attrType = attr.getDataType().getEnumType();
+				switch (attr.getDataType().getEnumType())
+				{
+					case BOOL:
+					case DECIMAL:
+					case EMAIL:
+					case ENUM:
+					case HTML:
+					case HYPERLINK:
+					case INT:
+					case LONG:
+					case SCRIPT:
+					case STRING:
+					case TEXT:
+						entityValue = sourceValue;
+						break;
+					case CATEGORICAL:
+					case FILE:
+					case XREF:
+						// TODO store id for xrefs
+						if (sourceValue instanceof Map<?, ?>)
+						{
+							@SuppressWarnings("unchecked")
+							Map<String, Object> sourceRefEntity = (Map<String, Object>) sourceValue;
+							EntityMetaData refEntity = attr.getRefEntity();
+							String refIdAttrName = refEntity.getIdAttribute().getName();
+							Object sourceRefEntityId = sourceRefEntity.get(refIdAttrName);
+							entityValue = entityManager.getReference(refEntity, sourceRefEntityId);
+						}
+						else
+						{
+							throw new RuntimeException("Unexpected type [" + sourceValue.getClass().getSimpleName()
+									+ "], expected [Map<String, Object]");
+						}
+						break;
+					case CATEGORICAL_MREF:
+					case MREF:
+						if (sourceValue instanceof Iterable<?>)
+						{
+							// TODO store list of ids for mrefs
+							@SuppressWarnings("unchecked")
+							Iterable<Map<String, Object>> sourceRefEntities = (Iterable<Map<String, Object>>) sourceValue;
+							EntityMetaData refEntity = attr.getRefEntity();
+							String refIdAttrName = refEntity.getIdAttribute().getName();
+							Iterable<Object> sourceRefEntityIds = new Iterable<Object>()
+							{
+								@Override
+								public Iterator<Object> iterator()
+								{
+									return StreamSupport.stream(sourceRefEntities.spliterator(), false)
+											.map(sourceRefEntity -> sourceRefEntity.get(refIdAttrName)).iterator();
+								}
+							};
+							entityValue = entityManager.getReferences(refEntity, sourceRefEntityIds);
+						}
+						else
+						{
+							throw new RuntimeException("Unexpected type [" + sourceValue.getClass().getSimpleName()
+									+ "], expected [Iterable<Map<String, Object>>]");
+						}
+						break;
+					case COMPOUND:
+						throw new RuntimeException("Compound attribute is not an atomic attribute");
+					case DATE:
+						try
+						{
+							entityValue = MolgenisDateFormat.getDateFormat().parse((String) sourceValue);
+						}
+						catch (Exception e)
+						{
+							throw new MolgenisDataException(e);
+						}
+						break;
+					case DATE_TIME:
+						try
+						{
+							entityValue = MolgenisDateFormat.getDateTimeFormat().parse((String) sourceValue);
+						}
+						catch (Exception e)
+						{
+							throw new MolgenisDataException(e);
+						}
+						break;
+					case IMAGE:
+						throw new MolgenisDataException("Unsupported data type for indexing [" + attrType + "]");
+					default:
+						throw new RuntimeException("Unknown data type [" + attrType + "]");
+				}
+			}
+			else
+			{
+				entityValue = null;
+			}
 			entity.set(attrName, entityValue);
 		});
 		return entity;
-	}
-
-	/**
-	 * Convert a single Elasticsearch document source value to a entity attribute value
-	 * 
-	 * @param entityMeta
-	 * @param attr
-	 * @param sourceValue
-	 * @return entity attribute value
-	 */
-	private Object convert(EntityMetaData entityMeta, AttributeMetaData attr, Object sourceValue)
-	{
-		if (attr.getExpression() != null)
-		{
-			throw new MolgenisDataException(
-					format("Computed attribute [%s] not stored in source document", attr.getName()));
-		}
-
-		Object entityValue;
-		if (sourceValue != null)
-		{
-			FieldTypeEnum attrType = attr.getDataType().getEnumType();
-			switch (attrType)
-			{
-				case BOOL:
-				case EMAIL:
-				case ENUM:
-				case HTML:
-				case HYPERLINK:
-				case SCRIPT:
-				case STRING:
-				case TEXT:
-					// no conversion required
-					entityValue = sourceValue;
-					break;
-				case DECIMAL:
-					// https://groups.google.com/forum/#!msg/elasticsearch/jxIY22TmA8U/5T25x9uhBD8J
-					entityValue = ((Number) sourceValue).doubleValue();
-					break;
-				case INT:
-					// https://groups.google.com/forum/#!msg/elasticsearch/jxIY22TmA8U/5T25x9uhBD8J
-					entityValue = ((Number) sourceValue).intValue();
-					break;
-				case LONG:
-					// https://groups.google.com/forum/#!msg/elasticsearch/jxIY22TmA8U/5T25x9uhBD8J
-					entityValue = ((Number) sourceValue).longValue();
-					break;
-				case CATEGORICAL:
-				case FILE:
-				case XREF:
-					@SuppressWarnings("unchecked")
-					Map<String, Object> xrefSource = (Map<String, Object>) sourceValue;
-					entityValue = convert(xrefSource, attr.getRefEntity());
-					break;
-				case CATEGORICAL_MREF:
-				case MREF:
-					@SuppressWarnings("unchecked")
-					Iterable<Map<String, Object>> mrefSources = (Iterable<Map<String, Object>>) sourceValue;
-					entityValue = new Iterable<Entity>()
-					{
-						@Override
-						public Iterator<Entity> iterator()
-						{
-							return StreamSupport.stream(mrefSources.spliterator(), false)
-									.map(mrefSource -> convert(mrefSource, attr.getRefEntity())).iterator();
-						}
-					};
-					break;
-				case COMPOUND:
-					throw new RuntimeException("Compound attribute is not an atomic attribute");
-				case DATE:
-					try
-					{
-						entityValue = MolgenisDateFormat.getDateFormat().parse((String) sourceValue);
-					}
-					catch (Exception e)
-					{
-						throw new MolgenisDataException(e);
-					}
-					break;
-				case DATE_TIME:
-					try
-					{
-						entityValue = MolgenisDateFormat.getDateTimeFormat().parse((String) sourceValue);
-					}
-					catch (Exception e)
-					{
-						throw new MolgenisDataException(e);
-					}
-					break;
-				case IMAGE:
-					throw new MolgenisDataException(format("Unsupported data type for indexing [%s]", attrType));
-				default:
-					throw new RuntimeException(format("Unknown data type [%s]", attrType));
-			}
-		}
-		else
-		{
-			entityValue = null;
-		}
-		return entityValue;
 	}
 }
