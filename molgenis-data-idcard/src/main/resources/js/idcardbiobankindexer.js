94f94651ddd697e9c590559a99ded333888532e6
dennishendriksen <d.hendriksen@umcg.nl>
2015-11-17 07:42:02 +0000

    revert

--- molgenis-data-idcard/src/main/resources/js/idcardbiobankindexer.js
+++ molgenis-data-idcard/src/main/resources/js/idcardbiobankindexer.js
@@ -1,36 +1,58 @@
 (function($, molgenis) {
 	"use strict";
 
-	var IdcardBiobankIndexerComponent;
-
-	$(function() {
-		var onButtonClick = function(){
-    		$.post(molgenis.getContextUrl() + '/reindex').done(function(job) {
-            	updateJobStatus(job);
-            });
-        }
-
-		IdcardBiobankIndexerComponent = React.render(molgenis.ui.IdcardBiobankIndexerComponent({
+	function renderTable() {
+		React.render(molgenis.ui.Table({
 			entity: 'IdCardIndexingEvent',
-			buttonDisabled: false,
-			onButtonClick: onButtonClick
-		}), $('#idCardBiobankIndexer-container')[0]);
-	});
-
+			sort: {
+				attr: {
+					name: 'date'
+				},
+				order: 'desc',
+				path: []
+			},
+			enableAdd: false,
+			enableEdit: false,
+			enableDelete: false,
+			enableInspect: false
+		}), $('#index-event-table-container')[0]);
+	}
+	
+	function refreshTable() {
+		// refresh table
+		React.unmountComponentAtNode($('#index-event-table-container')[0]);
+		renderTable();
+	}
+	
 	function updateJobStatus(job) {
 		if(job.triggerStatus === 'NONE' || job.triggerStatus === 'SUCCESS' || job.triggerStatus === 'ERROR') {
-			IdcardBiobankIndexerComponent.setProps({buttonDisabled: false});
+			// TODO enable button
 			molgenis.createAlert([{'message': 'Reindexing ID-Card completed'}], 'success');
+			refreshTable();
 		}
 		else {
-			IdcardBiobankIndexerComponent.setProps({buttonDisabled: true});
 			molgenis.createAlert([{'message': 'Reindexing ID-Card biobanks in progress ...'}], 'info');
 			setTimeout(function() {
 				$.get(molgenis.getContextUrl() + '/status/' + job.triggerGroup + '/' + job.triggerName).done(function(job) {
-					updateJobStatus(job);
+					updateJobStatus(job);	
 				});
 			}, 1000);
 		}
 	}
+	
+	$(function() {
+		renderTable();
+		
+		React.render(molgenis.ui.Button({
+			text : 'Reindex',
+			size: 'small',
+			onClick : function() {
+				// TODO disable button
+				$.post(molgenis.getContextUrl() + '/reindex').done(function(job) {
+					updateJobStatus(job);
+				});
+			}
+		}, 'Reindex'), $('#index-btn-container')[0]);
+	});
 
 }($, window.top.molgenis = window.top.molgenis || {}));
