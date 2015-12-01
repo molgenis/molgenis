29b34e5e143a231116c8c181791b8a7747c9405a
jjettenn <jjettenn@gmail.com>
2015-12-01 15:56:49 +0100

    Change migration

--- molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_14/Step23RebuildElasticsearchIndex.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_14/Step23RebuildElasticsearchIndex.java
@@ -1,22 +1,50 @@
 package org.molgenis.migrate.version.v1_14;
 
+import org.molgenis.data.RepositoryCollection;
+import org.molgenis.data.elasticsearch.ElasticsearchRepositoryCollection;
+import org.molgenis.data.meta.MetaDataService;
 import org.molgenis.framework.MolgenisUpgrade;
+import org.molgenis.security.core.runas.RunAsSystemProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 /**
  * Rebuilds Elasticsearch index due to changes in mapping for integers fields
  */
 @Component
-public class Step23RebuildElasticsearchIndex extends MolgenisUpgrade
+public class Step23RebuildElasticsearchIndex extends MolgenisUpgrade implements
+		ApplicationListener<ContextRefreshedEvent>
 {
-	public Step23RebuildElasticsearchIndex()
+	private final MetaDataService metaDataService;
+	private boolean enabled = false;
+
+	@Autowired
+	public Step23RebuildElasticsearchIndex(MetaDataService metaDataService)
 	{
 		super(22, 23);
+		this.metaDataService = metaDataService;
 	}
 
 	@Override
 	public void upgrade()
 	{
+		enabled = true;
 		// no operation, index is rebuild after migration steps have been executed
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event)
+	{
+		if (enabled)
+		{
+			RepositoryCollection elasticSearchRepositoryCollection = metaDataService
+					.getBackend(ElasticsearchRepositoryCollection.NAME);
+			RunAsSystemProxy.runAsSystem(() -> {
+				elasticSearchRepositoryCollection.forEach(e -> e.rebuildIndex());
+			});
+		}
+
 	}
 }
