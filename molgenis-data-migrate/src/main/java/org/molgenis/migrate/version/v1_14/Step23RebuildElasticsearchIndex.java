e5ad1a47ff3c1f30aa6b6c79cfcdb40d25a13bbc
jjettenn <jjettenn@gmail.com>
2015-12-02 16:46:23 +0100

    Changed migration step to work

--- molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_14/Step23RebuildElasticsearchIndex.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_14/Step23RebuildElasticsearchIndex.java
@@ -1,14 +1,18 @@
 package org.molgenis.migrate.version.v1_14;
 
+import java.util.List;
+
+import org.molgenis.data.DataService;
 import org.molgenis.data.RepositoryCollection;
 import org.molgenis.data.elasticsearch.ElasticsearchRepositoryCollection;
-import org.molgenis.data.meta.MetaDataService;
 import org.molgenis.framework.MolgenisUpgrade;
 import org.molgenis.security.core.runas.RunAsSystemProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Lists;
 
 /**
  * Rebuilds Elasticsearch index due to changes in mapping for integers fields
@@ -17,32 +21,36 @@ import org.springframework.stereotype.Component;
 public class Step23RebuildElasticsearchIndex extends MolgenisUpgrade implements
 		ApplicationListener<ContextRefreshedEvent>
 {
-	private final MetaDataService metaDataService;
+	private final DataService dataService;
 	private boolean enabled = false;
 
 	@Autowired
-	public Step23RebuildElasticsearchIndex(MetaDataService metaDataService)
+	public Step23RebuildElasticsearchIndex(DataService dataService)
 	{
 		super(22, 23);
-		this.metaDataService = metaDataService;
+		this.dataService = dataService;
 	}
 
 	@Override
 	public void upgrade()
 	{
-		enabled = true;
-		// no operation, index is rebuild after migration steps have been executed
+		this.enabled = true;
 	}
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event)
 	{
-		if (enabled)
+		if (this.enabled)
 		{
-			RepositoryCollection elasticSearchRepositoryCollection = metaDataService
+			RepositoryCollection elasticSearchRepositoryCollection = dataService.getMeta()
 					.getBackend(ElasticsearchRepositoryCollection.NAME);
+			
+			// List of Elasticsearch entities to rebuild index
+			List<String> entityNames = Lists.newArrayList(elasticSearchRepositoryCollection.getEntityNames());
+
 			RunAsSystemProxy.runAsSystem(() -> {
-				elasticSearchRepositoryCollection.forEach(e -> e.rebuildIndex());
+				entityNames.spliterator().forEachRemaining(
+						e -> elasticSearchRepositoryCollection.getRepository(e).rebuildIndex());
 			});
 		}
 
