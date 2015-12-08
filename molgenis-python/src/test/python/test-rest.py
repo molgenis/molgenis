35e766ec40591391afe1e6d2927d8e412a8c61a1
Fleur Kelpin <f.kelpin@umcg.nl>
2015-12-08 12:49:26 +0100

    Make python3 compatible

--- molgenis-python/src/test/python/test-rest.py
+++ molgenis-python/src/test/python/test-rest.py
@@ -12,7 +12,7 @@ class TestStringMethods(unittest.TestCase):
     s = molgenis.Session(self.api_url)
     s.login('admin', 'admin')
     response = s.get('MolgenisUser', q=[{"field":"superuser", "operator":"EQUALS", "value":"true"}])
-    self.assertEquals('admin',response[0]['username'])
+    self.assertEqual('admin',response[0]['username'])
 
   def test_login_logout_and_get_MolgenisUser(self):
     s = molgenis.Session(self.api_url)
@@ -22,16 +22,16 @@ class TestStringMethods(unittest.TestCase):
     try:
       response = s.get('MolgenisUser')
     except requests.exceptions.HTTPError as e:
-      self.assertEquals(e.response.status_code, 401)
-      self.assertEquals(e.response.json()['errors'][0]['message'], 'No READ permission on entity MolgenisUser')
+      self.assertEqual(e.response.status_code, 401)
+      self.assertEqual(e.response.json()['errors'][0]['message'], 'No READ permission on entity MolgenisUser')
 
   def test_no_login_and_get_MolgenisUser(self):
     s = molgenis.Session(self.api_url)
     try:
       s.get('MolgenisUser')
     except requests.exceptions.HTTPError as e:
-      self.assertEquals(e.response.status_code, 401)
-      self.assertEquals(e.response.json()['errors'][0]['message'], 'No READ permission on entity MolgenisUser')
+      self.assertEqual(e.response.status_code, 401)
+      self.assertEqual(e.response.json()['errors'][0]['message'], 'No READ permission on entity MolgenisUser')
 
   def test_add_all(self):
     s = molgenis.Session(self.api_url)
@@ -42,9 +42,9 @@ class TestStringMethods(unittest.TestCase):
     except Exception as e:
       print(str(e))
     response = s.add_all('TypeTestRef',[{"value": "ref55","label": "label55"},{"value": "ref57","label": "label57"}])
-    self.assertEquals(['ref55','ref57'], response)
+    self.assertEqual(['ref55','ref57'], response)
     item55 = s.get('TypeTestRef', q=[{"field":"value", "operator":"EQUALS", "value":"ref55"}])[0]
-    self.assertItemsEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
+    self.assertEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
     s.delete('TypeTestRef', 'ref55')
     s.delete('TypeTestRef', 'ref57')
 
@@ -55,7 +55,7 @@ class TestStringMethods(unittest.TestCase):
       s.delete('TypeTestRef', 'ref55')
     except Exception as e:
       print(str(e))
-    self.assertEquals('ref55', s.add('TypeTestRef',{"value": "ref55","label": "label55"}))
+    self.assertEqual('ref55', s.add('TypeTestRef',{"value": "ref55","label": "label55"}))
     s.delete('TypeTestRef', 'ref55')
 
   def test_add_kwargs(self):
@@ -65,9 +65,9 @@ class TestStringMethods(unittest.TestCase):
       s.delete('TypeTestRef', 'ref55')
     except Exception as e:
       print(str(e))
-    self.assertEquals('ref55', s.add('TypeTestRef',value="ref55",label="label55"))
+    self.assertEqual('ref55', s.add('TypeTestRef',value="ref55",label="label55"))
     item55 = s.get('TypeTestRef', q=[{"field":"value", "operator":"EQUALS", "value":"ref55"}])[0]
-    self.assertItemsEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
+    self.assertEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
     s.delete('TypeTestRef', 'ref55')
 
   def test_add_merge_dict_kwargs(self):
@@ -77,22 +77,22 @@ class TestStringMethods(unittest.TestCase):
       s.delete('TypeTestRef', 'ref55')
     except Exception as e:
       print(str(e))
-    self.assertEquals('ref55', s.add('TypeTestRef',{"value":"ref55"},label="label55"))
+    self.assertEqual('ref55', s.add('TypeTestRef',{"value":"ref55"},label="label55"))
     item55 = s.get('TypeTestRef', q=[{"field":"value", "operator":"EQUALS", "value":"ref55"}])[0]
-    self.assertItemsEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
+    self.assertEqual({"value": "ref55", "label": "label55", "href": "/api/v1/TypeTestRef/ref55"}, item55)
     s.delete('TypeTestRef', 'ref55')
 
   def test_get_meta(self):
     s = molgenis.Session(self.api_url)
     s.login('admin', 'admin')
     meta = s.get_entity_meta_data('MolgenisUser')
-    self.assertEquals('username', meta['labelAttribute'])
+    self.assertEqual('username', meta['labelAttribute'])
 
   def test_get_attribute_meta(self):
     s = molgenis.Session(self.api_url)
     s.login('admin', 'admin')
     meta = s.get_attribute_meta_data('MolgenisUser', 'username')
-    self.assertItemsEqual({'aggregateable': False, 'attributes': [], 'auto': False, 'description': '',
+    self.assertEqual({'aggregateable': False, 'attributes': [], 'auto': False, 'description': '',
       'fieldType': 'STRING', 'href': '/api/v1/MolgenisUser/meta/username', 'label': 'Username', 
       'labelAttribute': True, 'lookupAttribute': True, 'maxLength': 255, 'nillable': False, 
       'readOnly': False, 'unique': True, 'visible': True, 'name': 'username'}, meta)
