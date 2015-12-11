128256564ab41f1a4f7c5f3616cd22492fa332ac
dennishendriksen <d.hendriksen@umcg.nl>
2015-12-11 08:21:15 +0100

    Fix #4268 Elasticsearch findOne with query executes query twice

--- molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/AbstractElasticsearchRepositoryTest.java
+++ molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/AbstractElasticsearchRepositoryTest.java
@@ -3,13 +3,16 @@ package org.molgenis.data.elasticsearch;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.molgenis.data.Entity;
 import org.molgenis.data.EntityMetaData;
 import org.molgenis.data.Fetch;
+import org.molgenis.data.Query;
 import org.testng.annotations.Test;
 
 public class AbstractElasticsearchRepositoryTest
@@ -40,6 +43,69 @@ public class AbstractElasticsearchRepositoryTest
 			Fetch fetch = new Fetch();
 			when(searchService.get(id, entityMeta, fetch)).thenReturn(entity);
 			assertEquals(repository.findOne(id, fetch), entity);
+		}
+		finally
+		{
+			repository.close();
+		}
+	}
+
+	@Test
+	public void findOneQuery() throws IOException
+	{
+		EntityMetaData entityMeta = mock(EntityMetaData.class);
+		SearchService searchService = mock(SearchService.class);
+		AbstractElasticsearchRepository repository = new AbstractElasticsearchRepository(searchService)
+		{
+			@Override
+			public void rebuildIndex()
+			{
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public EntityMetaData getEntityMetaData()
+			{
+				return entityMeta;
+			}
+		};
+		try
+		{
+			Entity entity = mock(Entity.class);
+			Query q = mock(Query.class);
+			when(searchService.search(q, entityMeta)).thenReturn(Arrays.asList(entity));
+			assertEquals(repository.findOne(q), entity);
+		}
+		finally
+		{
+			repository.close();
+		}
+	}
+
+	@Test
+	public void findOneQueryNoResults() throws IOException
+	{
+		EntityMetaData entityMeta = mock(EntityMetaData.class);
+		SearchService searchService = mock(SearchService.class);
+		AbstractElasticsearchRepository repository = new AbstractElasticsearchRepository(searchService)
+		{
+			@Override
+			public void rebuildIndex()
+			{
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public EntityMetaData getEntityMetaData()
+			{
+				return entityMeta;
+			}
+		};
+		try
+		{
+			Query q = mock(Query.class);
+			when(searchService.search(q, entityMeta)).thenReturn(Collections.emptyList());
+			assertNull(repository.findOne(q));
 		}
 		finally
 		{
