fccf21fb9b91eb13bc59dc9c80f76bb900180484
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-07 10:48:20 +0100

    Fix MIME type error by setting the correct baseURL in require config

--- molgenis-core-ui/src/main/resources/js/main.js
+++ molgenis-core-ui/src/main/resources/js/main.js
@@ -1,37 +1,40 @@
 'use strict';
-
-// First we’re setting a baseUrl to the scripts folder, then letting RequireJS know where our libraries are 
-// with the paths option (compulsory when using jQuery as it is a named module) and then shimming any scripts 
-// that do not have AMD support. This is common with most jQuery plugins.
+//console.log('setting configuration...');
+// First we’re setting a baseUrl to the src/main/resources/js folder, then letting RequireJS know where our 
+// libraries are with the paths option (compulsory when using jQuery as it is a named module) and 
+// then shimming any scripts that do not have AMD support. This is common with most jQuery plugins.
 require.config({
-	baseUrl : '/src/main/resources/js',
+	baseUrl : 'js/',
 	paths : {
-		jquery : '/components/jquery/jquery-2.1.1.min',
-		domready : '/components/requirejs/requirejs-domready',
-		handlebars : '/components/handlebars/handlebars',
-		bootstrap : '/components/bootstrap/bootstrap.min',
-		react : '/components/react/react-with-addons',
-		molgenis : '/modules/molgenis',
-		underscore : '/components/underscore/underscore-min',
-		react_button : '/component/Button'
+		jquery : 'components/jquery/jquery-2.1.1.min',
+		domready : 'components/requirejs/requirejs-domready',
+		handlebars : 'components/handlebars/handlebars',
+		bootstrap : 'components/bootstrap/bootstrap.min',
+		react : 'components/react/react-with-addons',
+		molgenis : 'modules/molgenis',
+		underscore : 'components/underscore/underscore-min',
+		react_button : 'component/Button'
 	},
 	shim : {
+		bootstrap : {
+			deps : 'jquery'
+		},
 		handlebars : {
 			exports : 'Handlebars'
 		},
-		'/plugins/jQEditRangeSlider-min.js' : [ 'jquery' ],
-		'/plugins/jquery-sortable-min.js' : [ 'jquery' ],
-		'/plugins/jquery.bootstrap.pager.js' : [ 'jquery' ],
-		'/plugins/jquery.bootstrap.wizard.min.js' : [ 'jquery' ],
-		'/plugins/jquery.cookie-1.4.1.min.js' : [ 'jquery' ],
-		'/plugins/jquery.fancytree.min.js' : [ 'jquery' ],
-		'/plugins/jquery.form.min.js' : [ 'jquery' ],
-		'/plugins/jquery.molgenis.attributemetadata.table.js' : [ 'jquery' ],
-		'/plugins/jquery.molgenis.table.js' : [ 'jquery' ],
-		'/plugins/jquery.molgenis.tree.js' : [ 'jquery' ],
-		'/plugins/jquery.molgenis.xrefmrefsearch.js' : [ 'jquery' ],
-		'/plugins/jquery.validate.min.js' : [ 'jquery' ],
-		'/plugins/select2-patched.js' : [ 'jquery' ],
+		'plugins/jQEditRangeSlider-min.js' : [ 'jquery' ],
+		'plugins/jquery-sortable-min.js' : [ 'jquery' ],
+		'plugins/jquery.bootstrap.pager.js' : [ 'jquery' ],
+		'plugins/jquery.bootstrap.wizard.min.js' : [ 'jquery' ],
+		'plugins/jquery.cookie-1.4.1.min.js' : [ 'jquery' ],
+		'plugins/jquery.fancytree.min.js' : [ 'jquery' ],
+		'plugins/jquery.form.min.js' : [ 'jquery' ],
+		'plugins/jquery.molgenis.attributemetadata.table.js' : [ 'jquery' ],
+		'plugins/jquery.molgenis.table.js' : [ 'jquery' ],
+		'plugins/jquery.molgenis.tree.js' : [ 'jquery' ],
+		'plugins/jquery.molgenis.xrefmrefsearch.js' : [ 'jquery' ],
+		'plugins/jquery.validate.min.js' : [ 'jquery' ],
+		'plugins/select2-patched.js' : [ 'jquery' ],
 		'plugins/bootstrap.file-input.js' : [ 'bootstrap' ],
 		'plugins/bootstrap-datetimepicker.min.js' : [ 'bootstrap' ],
 		'plugins/bootstrap-fileupload.min.js' : [ 'bootstrap' ]
@@ -52,6 +55,6 @@ define(function(require) {
 	domReady(function() {
 		// Init common module code here
 		// TODO common modules, like React components????
-		console.log('ready!');
+		console.log('All scripts loaded and DOM is ready!');
 	});
 });
