22ee65e3627eb912e26e7d781ff8003000bdc309
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 17:46:19 +0100

    Require-ified Dialog component

--- molgenis-core-ui/src/main/resources/js/component/Dialog.js
+++ molgenis-core-ui/src/main/resources/js/component/Dialog.js
@@ -1,36 +1,55 @@
-/* global _: false, React: false, molgenis: true */
-(function(_, React, molgenis) {
-	"use strict";
-	
-	var div = React.DOM.div;
-	
+define(function(require, exports, module) {
 	/**
-	 * @memberOf component
+	 * @module Dialog
+	 */
+	"use strict";
+
+	var React = require('react');
+	var _ = require('underscore');
+
+	var Button = require('component/Button');
+	var Modal = require('component/Modal');
+
+	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
+
+	var div = React.DOM.div;
+
+	/**
+	 * @memberOf Dialog
 	 */
 	var Dialog = React.createClass({
-		mixins: [molgenis.ui.mixin.DeepPureRenderMixin],
-		displayName: 'Dialog',
-		propTypes: {
-			type: React.PropTypes.oneOf(['alert', 'confirm']),
-			message: React.PropTypes.string.isRequired,
-			onCancel: React.PropTypes.func,  // confirm dialogs
-			onConfirm: React.PropTypes.func, // alert and confirm dialogs
+		mixins : [ DeepPureRenderMixin ],
+		displayName : 'Dialog',
+		propTypes : {
+			type : React.PropTypes.oneOf([ 'alert', 'confirm' ]),
+			message : React.PropTypes.string.isRequired,
+			onCancel : React.PropTypes.func, // confirm dialogs
+			onConfirm : React.PropTypes.func, // alert and confirm dialogs
 		},
-		render: function() {
-			return molgenis.ui.Modal({title: this.props.message, show: true},
-				div({className: 'row', style: {textAlign: 'right'}},
-					div({className: 'col-md-12'},
-						this.props.type === 'confirm' ? molgenis.ui.Button({text: 'Cancel', onClick: this.props.onCancel}, 'Cancel') : null,
-						molgenis.ui.Button({text: 'Ok', style: 'primary', css: {marginLeft: 5}, onClick: this.props.onConfirm}, 'Ok')
-					)
-				)
-			);
+		render : function() {
+			return Modal({
+				title : this.props.message,
+				show : true
+			}, div({
+				className : 'row',
+				style : {
+					textAlign : 'right'
+				}
+			}, div({
+				className : 'col-md-12'
+			}, this.props.type === 'confirm' ? Button({
+				text : 'Cancel',
+				onClick : this.props.onCancel
+			}, 'Cancel') : null, Button({
+				text : 'Ok',
+				style : 'primary',
+				css : {
+					marginLeft : 5
+				},
+				onClick : this.props.onConfirm
+			}, 'Ok'))));
 		}
 	});
-	
-	// export component
-	molgenis.ui = molgenis.ui || {};
-	_.extend(molgenis.ui, {
-		Dialog: React.createFactory(Dialog)
-	});
-}(_, React, molgenis));
+
+	module.exports = React.createFactory(Dialog)
+});
