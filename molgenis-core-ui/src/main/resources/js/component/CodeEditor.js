fc813759b4355fbcd73c38356f78547d8f2b0e04
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 17:46:08 +0100

    Require-ified CodeEditor component

--- molgenis-core-ui/src/main/resources/js/component/CodeEditor.js
+++ molgenis-core-ui/src/main/resources/js/component/CodeEditor.js
@@ -1,47 +1,53 @@
-/* global _: false, React: false, molgenis: true */
-(function(_, React, molgenis) {
-	"use strict";
-	
+define(function(require, exports, module) {
 	/**
-	 * @memberOf component
+	 * @module CodeEditor
+	 */
+	"use strict";
+
+	var React = require('react');
+	var _ = require('underscore');
+
+	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
+	var Ace = require('component/wrapper/Ace')
+
+	/**
+	 * @memberOf CodeEditor
 	 */
 	var CodeEditor = React.createClass({
-		mixins: [molgenis.ui.mixin.DeepPureRenderMixin],
-		displayName: 'CodeEditor',
-		propTypes: {
+		mixins : [ DeepPureRenderMixin ],
+		displayName : 'CodeEditor',
+		propTypes : {
 			id : React.PropTypes.string,
-			name: React.PropTypes.string,
+			name : React.PropTypes.string,
 			placeholder : React.PropTypes.string,
 			required : React.PropTypes.bool,
 			disabled : React.PropTypes.bool,
 			readOnly : React.PropTypes.bool,
-			maxLength: React.PropTypes.number,
-			mode: React.PropTypes.string,
+			maxLength : React.PropTypes.number,
+			mode : React.PropTypes.string,
 			value : React.PropTypes.string,
 			onValueChange : React.PropTypes.func.isRequired
 		},
-		render: function() {
-			return molgenis.ui.wrapper.Ace({
+		render : function() {
+			return Ace({
 				id : this.props.id,
-				name: this.props.name,
+				name : this.props.name,
 				placeholder : this.props.placeholder,
 				required : this.props.required,
 				disabled : this.props.disabled,
 				readOnly : this.props.readOnly,
-				maxLength: this.props.maxLength,
-				mode: this.props.language,
+				maxLength : this.props.maxLength,
+				mode : this.props.language,
 				value : this.props.value,
 				onChange : this._handleChange
 			});
 		},
-		_handleChange: function(value) {
-			this.props.onValueChange({value: value !== '' ? value : null});
+		_handleChange : function(value) {
+			this.props.onValueChange({
+				value : value !== '' ? value : null
+			});
 		}
 	});
-	
-	// export component
-	molgenis.ui = molgenis.ui || {};
-	_.extend(molgenis.ui, {
-		CodeEditor: React.createFactory(CodeEditor)
-	});
-}(_, React, molgenis));
+
+	module.exports = React.createFactory(CodeEditor)
+});
