564013b87c3e8fba7351d74aab6d0891943948b9
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 16:00:41 +0100

    Added commented code to main.js until suitable location is found

--- molgenis-core-ui/src/main/resources/js/main.js
+++ molgenis-core-ui/src/main/resources/js/main.js
@@ -12,6 +12,7 @@ require.onError = function(error) {
 
 // Configuration for RequireJS
 require.config({
+
 	// Set a baseUrl to the /js/ folder, and let RequireJS know where our
 	// libraries are with the paths option (compulsory when using jQuery as it
 	// is a named module)
@@ -24,9 +25,7 @@ require.config({
 		react : 'components/react/react-with-addons',
 		underscore : 'components/underscore/underscore-min',
 		validate : 'plugins/jquery.validate.min',
-		// Create the molgenis object in the global scope
-		molgenis : 'modules/MolgenisGlobalObject'
-
+		molgenis : 'modules/MolgenisGlobalObject',
 	},
 	// Shim any scripts that do not have AMD support
 	// This is common with most jQuery plugins
@@ -55,7 +54,10 @@ require.config({
 // might want to load and initialise on every page
 define(function(require) {
 	console.log('loading main dependencies...')
-
+	
+	// Create the molgenis object in the global scope
+	// Used for backwards compatibility
+	var molgenis = require('molgenis');
 	var $ = require('jquery');
 	var domReady = require('domready');
 	var Handlebars = require('handlebars');
@@ -63,12 +65,105 @@ define(function(require) {
 	var React = require('react');
 	var _ = require('underscore');
 	var validate = require('validate');
-	var molgenis = require('molgenis');
 
 	domReady(function() {
 		// Init common module code here
 		// TODO common modules, like React components????
 		console.log('All dependencies loaded and DOM ready!');
 
+		/*
+		 * Code that we should do something with
+		 */
+
+		// workaround for "Uncaught RangeError: Maximum call stack size
+		// exceeded"
+		// http://stackoverflow.com/a/19190216
+//		$.fn.modal.Constructor.prototype.enforceFocus = function() {
+//		};
+//
+//		/**
+//		 * Add download functionality to JQuery. data can be string of
+//		 * parameters or array/object
+//		 * 
+//		 * Default method is POST
+//		 * 
+//		 * Usage: <code>download('/localhost:8080',
+//		 'param1=value1&param2=value2')</code>
+//		 * Or: <code>download('/localhost:8080', {param1 : 'value1', param2 :
+//		 'value2'})</code>
+//		 * 
+//		 */
+//		$.download = function(url, data, method) {
+//			if (!method) {
+//				method = 'POST';
+//			}
+//
+//			data = typeof data == 'string' ? data : $.param(data);
+//
+//			// split params into form inputs
+//			var inputs = [];
+//			$.each(data.split('&'), function() {
+//				var pair = this.split('=');
+//				inputs.push('<input type="hidden" name="' + pair[0] + '" value="' + pair[1] + '" />');
+//			});
+//
+//			// send request and remove form from dom
+//			$('<form action="' + url + '" method="' + method + '">').html(inputs.join('')).appendTo('body').submit().remove();
+//		};
+//
+//		// serialize form as json object
+//		$.fn.serializeObject = function() {
+//			var o = {};
+//			var a = this.serializeArray();
+//			$.each(a, function() {
+//				if (o[this.name] !== undefined) {
+//					if (!o[this.name].push) {
+//						o[this.name] = [ o[this.name] ];
+//					}
+//					o[this.name].push(this.value || '');
+//				} else {
+//					o[this.name] = this.value || '';
+//				}
+//			});
+//			return o;
+//		};
+//
+//		$(document).ajaxError(function(event, xhr, settings, e) {
+//			if (xhr.status === 401) {
+//				document.location = "/login";
+//			}
+//			try {
+//				molgenis.createAlert(JSON.parse(xhr.responseText).errors);
+//			} catch (e) {
+//				molgenis.createAlert([ {
+//					'message' : 'An error occurred. Please contact the administrator.'
+//				} ], 'error');
+//			}
+//		});
+//
+//		window.onerror = function(msg, url, line) {
+//			molgenis.createAlert([ {
+//				'message' : 'An error occurred. Please contact the administrator.'
+//			}, {
+//				'message' : msg
+//			} ], 'error');
+//		};
+//		
+//		// use ajaxPrefilter instead of ajaxStart and ajaxStop
+//		// to work around issue http://bugs.jquery.com/ticket/13680
+//		$.ajaxPrefilter(function(options, _, jqXHR) {
+//			if (options.showSpinner !== false) {
+//				showSpinner();
+//				jqXHR.always(hideSpinner);
+//			}
+//		});
+//		
+//		/**
+//		 * Returns a promise with plugin settings
+//		 */
+//		molgenis.getPluginSettings = function() {
+//			var api = new molgenis.RestClientV2();
+//			return api.get('/api/v2/' + molgenis.getPluginSettingsId() + '/' + molgenis.getPluginId());
+//		};
 	});
 });
