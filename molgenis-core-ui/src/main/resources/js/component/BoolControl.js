e6294eb626eccc789327d8ea70b0dbacf37f1d0a
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 17:45:53 +0100

    Require-ified BoolControl component

--- molgenis-core-ui/src/main/resources/js/component/BoolControl.js
+++ molgenis-core-ui/src/main/resources/js/component/BoolControl.js
@@ -1,76 +1,100 @@
-/* global _: false, React: false, molgenis: true */
-(function(_, React, molgenis) {
+define(function(require, exports, module) {
+	/**
+	 * @module BoolControl
+	 */
 	"use strict";
-	
+
+	var React = require('react');
+	var _ = require('underscore');
+
+	var Spinner = require('component/Spinner');
+	var RadioGroup = require('component/RadioGroup');
+	var CheckboxGroup = require('component/CheckboxGroup');
+
+	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
+	var I18nStringsMixin = require('component/mixin/I18nStringsMixin');
+
 	var div = React.DOM.div, label = React.DOM.label;
-	
+
 	/**
 	 * Input control for BOOL type with checkbox or radio buttons
 	 * 
 	 * @memberOf component
 	 */
 	var BoolControl = React.createClass({
-		mixins: [molgenis.ui.mixin.DeepPureRenderMixin, molgenis.ui.mixin.I18nStringsMixin],
-		displayName: 'BoolControl',
-		propTypes: {
-			id: React.PropTypes.string,
-			name: React.PropTypes.string,
-			label: React.PropTypes.string,
-			layout: React.PropTypes.oneOf(['horizontal', 'vertical']),
-			type: React.PropTypes.oneOf(['single', 'group']),
-			multiple: React.PropTypes.bool,
-			required: React.PropTypes.bool,
-			disabled: React.PropTypes.bool,
-			readOnly: React.PropTypes.bool,
-			focus: React.PropTypes.bool,
-			value: React.PropTypes.oneOfType([React.PropTypes.bool, React.PropTypes.array]),
-			onValueChange: React.PropTypes.func.isRequired
+		mixins : [ DeepPureRenderMixin, I18nStringsMixin ],
+		displayName : 'BoolControl',
+		propTypes : {
+			id : React.PropTypes.string,
+			name : React.PropTypes.string,
+			label : React.PropTypes.string,
+			layout : React.PropTypes.oneOf([ 'horizontal', 'vertical' ]),
+			type : React.PropTypes.oneOf([ 'single', 'group' ]),
+			multiple : React.PropTypes.bool,
+			required : React.PropTypes.bool,
+			disabled : React.PropTypes.bool,
+			readOnly : React.PropTypes.bool,
+			focus : React.PropTypes.bool,
+			value : React.PropTypes.oneOfType([ React.PropTypes.bool, React.PropTypes.array ]),
+			onValueChange : React.PropTypes.func.isRequired
 		},
-		getDefaultProps: function() {
+		getDefaultProps : function() {
 			return {
-				type: 'single',
-				layout: 'horizontal',
-				required: true
+				type : 'single',
+				layout : 'horizontal',
+				required : true
 			};
 		},
-		getInitialState: function() {
+		getInitialState : function() {
 			return {
-				i18nStrings : null //loaded from server
+				i18nStrings : null
+			// loaded from server
 			};
-    	},
-		render: function() {
-			if(this.state.i18nStrings === null) {
-				return molgenis.ui.Spinner();
+		},
+		render : function() {
+			if (this.state.i18nStrings === null) {
+				return Spinner();
 			}
-			
-			var options = [{value: 'true', label: this.state.i18nStrings.form_bool_true}, {value: 'false', label: this.state.i18nStrings.form_bool_false}];
-			if(!this.props.required && !this.props.multiple) {
-				options = options.concat({value: '', label: this.state.i18nStrings.form_bool_missing});
+
+			var options = [ {
+				value : 'true',
+				label : this.state.i18nStrings.form_bool_true
+			}, {
+				value : 'false',
+				label : this.state.i18nStrings.form_bool_false
+			} ];
+			if (!this.props.required && !this.props.multiple) {
+				options = options.concat({
+					value : '',
+					label : this.state.i18nStrings.form_bool_missing
+				});
 			}
-			
-			var Element = this.props.multiple ? molgenis.ui.CheckboxGroup : molgenis.ui.RadioGroup;
+
+			var Element = this.props.multiple ? CheckboxGroup : RadioGroup;
 			return Element({
-				id: this.props.id,
-				name: this.props.name,
+				id : this.props.id,
+				name : this.props.name,
 				options : options,
 				required : this.props.required,
 				disabled : this.props.disabled,
-				readOnly: this.props.readOnly,
+				readOnly : this.props.readOnly,
 				layout : this.props.layout,
-				focus: this.props.focus,
+				focus : this.props.focus,
 				value : this._boolToString(this.props.value),
 				onValueChange : this._handleValueChange
 			});
 		},
-		_handleValueChange: function(e) {
-			this.props.onValueChange({value: this._eventToBool(e)});
+		_handleValueChange : function(e) {
+			this.props.onValueChange({
+				value : this._eventToBool(e)
+			});
 		},
-		_boolToString: function(value) {
-			if(this.props.multiple) {
+		_boolToString : function(value) {
+			if (this.props.multiple) {
 				// do not use $.map since it removes null values
-				if(value !== undefined) {
+				if (value !== undefined) {
 					value = value.slice(0);
-					for(var i = 0; i < value.length; ++i)
+					for (var i = 0; i < value.length; ++i)
 						value[i] = value[i] === true ? 'true' : (value[i] === false ? 'false' : value[i]);
 				}
 				return value;
@@ -78,26 +102,22 @@
 				return value === true ? 'true' : (value === false ? 'false' : value);
 			}
 		},
-		_eventToBool: function(e) {
-			if(this.props.multiple) {
+		_eventToBool : function(e) {
+			if (this.props.multiple) {
 				// do not use $.map since it removes null values
-				if(e.value !== undefined) {
+				if (e.value !== undefined) {
 					var value = [];
-					for(var i = 0; i < e.value.slice(0).length; ++i)
+					for (var i = 0; i < e.value.slice(0).length; ++i)
 						value[i] = value[i] === 'true' ? true : (value[i] === 'false' ? false : value[i]);
 				}
 				return value;
-			} else if(e.checked !== undefined) {
+			} else if (e.checked !== undefined) {
 				return e.checked;
 			} else {
 				return e.value === 'true' ? true : (e.value === 'false' ? false : e.value);
 			}
 		}
 	});
-	
-	// export component
-	molgenis.ui = molgenis.ui || {};
-	_.extend(molgenis.ui, {
-		BoolControl: React.createFactory(BoolControl)
-	});
-}(_, React, molgenis));
+
+	module.exports = React.createFactory(BoolControl)
+});
