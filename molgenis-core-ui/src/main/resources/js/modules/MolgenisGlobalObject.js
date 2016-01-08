944f276aeff4398ce22d836c0d2cba91e044c9c0
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 15:14:05 +0100

    Expand global MolgenisObject

--- molgenis-core-ui/src/main/resources/js/modules/MolgenisGlobalObject.js
+++ molgenis-core-ui/src/main/resources/js/modules/MolgenisGlobalObject.js
@@ -2,16 +2,20 @@ define(function(require, exports, module) {
 	/**
 	 * @module MolgenisGlobalObject
 	 */
-	
+
 	'use strict';
 	var ui = require('component/global-ui/global-ui');
-	
+	var alert = require('modules/MolgenisAlert');
+	var i18n
+
 	/*
-	 * Old style molgenis object to put in the global scope
-	 * for backwards compatibility
+	 * Old style molgenis object to put in the global scope for backwards
+	 * compatibility
 	 */
 	var molgenis = {
-		'ui' : ui
+		'ui' : ui,
+		'createAlert' : alert.createAlert,
+		'i18n' : i18n
 	};
 
 	module.exports = molgenis;
