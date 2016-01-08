9b449297ac26cedd7d8b0283dd079ad5bc6537c6
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 19:19:42 +0100

    Fix typo

--- molgenis-core-ui/src/main/resources/js/component/Popover.js
+++ molgenis-core-ui/src/main/resources/js/component/Popover.js
@@ -1,26 +1,27 @@
-define(function(require,exports,module), function(){
+define(function(require, exports, module) {
 	/**
 	 * @module Popover
 	 */
 	"use strict";
-	
+
 	var $ = require('jquery');
-	var React - require('react');
-	
+	var React
+	-require('react');
+
 	var span = React.DOM.span;
 	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
-	
+
 	/**
 	 * @memberOf Popover
 	 */
 	var Popover = React.createClass({
-		mixins: [DeepPureRenderMixin],
-		displayName: 'Popover',
-		propTypes: {
-			value: React.PropTypes.string.isRequired,
-			popoverValue: React.PropTypes.string.isRequired,
+		mixins : [ DeepPureRenderMixin ],
+		displayName : 'Popover',
+		propTypes : {
+			value : React.PropTypes.string.isRequired,
+			popoverValue : React.PropTypes.string.isRequired,
 		},
-		componentDidMount: function() {
+		componentDidMount : function() {
 			var $container = $(this.refs.popover.getDOMNode());
 			$container.popover({
 				trigger : 'hover click',
@@ -28,18 +29,18 @@ define(function(require,exports,module), function(){
 				container : 'body'
 			});
 		},
-		componentWillUnmount: function() {
+		componentWillUnmount : function() {
 			var $container = $(this.refs.popover.getDOMNode());
 			$container.popover('destroy');
 		},
-		render: function() {
+		render : function() {
 			return span({
 				'data-content' : this.props.popoverValue,
 				'data-toggle' : 'popover',
-				ref: 'popover'
+				ref : 'popover'
 			}, this.props.value);
 		}
 	});
-	
+
 	module.exports = React.createFactory(Popover);
 });
