5711fe1ced4f9a444cd6ad71449b7755d383369f
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 16:13:42 +0100

    Require-ified JQueryForm wrapper

--- molgenis-core-ui/src/main/resources/js/component/wrapper/JQueryForm.js
+++ molgenis-core-ui/src/main/resources/js/component/wrapper/JQueryForm.js
@@ -1,55 +1,56 @@
-/* global _: false, React: false, molgenis: true */
-(function(_, React, molgenis) {
-    "use strict";
-    
-    var form = React.DOM.form;
-			
-    /**
+define(function(require, exports, module) {
+	/**
+	 * @module JQueryForm
+	 */
+	"use strict";
+
+	var _ = require('underscore');
+	var $ = require('jquery');
+	var React = require('react');
+
+	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
+
+	var form = React.DOM.form;
+
+	/**
 	 * React component for jQuery Form Plugin (http://malsup.com/jquery/form/)
 	 * 
 	 * @memberOf component.wrapper
 	 */
 	var JQueryForm = React.createClass({
-		displayName: 'JQueryForm',
-		mixins: [molgenis.ui.mixin.DeepPureRenderMixin],
-		propTypes: {
-			className: React.PropTypes.string,
-			action: React.PropTypes.string.isRequired,
-			method: React.PropTypes.string,
-			noValidate: React.PropTypes.bool,
-			beforeSubmit: React.PropTypes.func,
-			success: React.PropTypes.func,
-			error: React.PropTypes.func,
+		displayName : 'JQueryForm',
+		mixins : [ DeepPureRenderMixin ],
+		propTypes : {
+			className : React.PropTypes.string,
+			action : React.PropTypes.string.isRequired,
+			method : React.PropTypes.string,
+			noValidate : React.PropTypes.bool,
+			beforeSubmit : React.PropTypes.func,
+			success : React.PropTypes.func,
+			error : React.PropTypes.func,
 		},
-		submit: function() {
+		submit : function() {
 			var $form = $(this.refs.form.getDOMNode());
 			$form.ajaxSubmit({
-				resetForm: false,
+				resetForm : false,
 				beforeSubmit : this.props.beforeSubmit,
 				success : this.props.success,
 				error : this.props.error
 			});
 		},
-		componentDidMount: function() {
+		componentDidMount : function() {
 			this.refs.form.getDOMNode().submit = this.submit;
 		},
-		componentWillUnmount: function() {
+		componentWillUnmount : function() {
 			var $form = $(this.refs.form.getDOMNode());
 			$form.ajaxFormUnbind();
 		},
-		render: function() {
-			return (
-				form(_.extend({}, this.props, {ref: 'form'}),
-					this.props.children
-				)
-			);
+		render : function() {
+			return (form(_.extend({}, this.props, {
+				ref : 'form'
+			}), this.props.children));
 		}
 	});
-	
-    // export component
-    molgenis.ui = molgenis.ui || {};
-    molgenis.ui.wrapper = molgenis.ui.wrapper || {};
-    _.extend(molgenis.ui.wrapper, {
-        JQueryForm: React.createFactory(JQueryForm)
-    });
-}(_, React, molgenis));
+
+	module.exports = React.createFactory(JQueryForm);
+});
