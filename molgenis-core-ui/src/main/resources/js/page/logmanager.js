f67251caae31c682df4a1f267f60fd6d89013b8d
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 16:01:11 +0100

    Require-ified logmanager.js

--- molgenis-core-ui/src/main/resources/js/page/logmanager.js
+++ molgenis-core-ui/src/main/resources/js/page/logmanager.js
@@ -1,41 +1,49 @@
-(function($, molgenis) {
+define(function(require, exports, module) {
+	/**
+	 * @page logmanager
+	 */
 	"use strict";
-	
+
+	var $ = require('jquery');
+	var molgenis = require('molgenis');
+
+	var contextUrl = molgenis.contextUrl;
+
 	function renderLoggerTable() {
-		window.location = molgenis.getContextUrl();
+		window.location = contextUrl;
 	}
-	
-	$(function(){
+
+	$(function() {
 		var container = $("#plugin-container");
-		
+
 		$('.log-level-select', container).select2();
-		
+
 		$(container).on('change', '.log-level-select', function(e) {
 			var logger = $(this).closest('tr').data('logger');
 			var level = $(this).val();
-			$.post(molgenis.getContextUrl() + '/logger/' + logger + '/' + level);
+			$.post(contextUrl + '/logger/' + logger + '/' + level);
 		})
-		
+
 		$(container).on('click', '#create-logger-btn', function() {
-			$.post(molgenis.getContextUrl() + '/loggers/reset', function() {
-				window.location = molgenis.getContextUrl();
+			$.post(contextUrl + '/loggers/reset', function() {
+				window.location = contextUrl;
 			});
 		});
-		
+
 		$(container).on('click', '#reset-loggers-btn', function() {
-			$.post(molgenis.getContextUrl() + '/loggers/reset').success(function() {
+			$.post(contextUrl + '/loggers/reset').success(function() {
 				renderLoggerTable();
 			});
 		});
-		
+
 		$('form[name="create-logger-form"]').submit(function(e) {
 			e.preventDefault();
-			if($(this).valid()) {
+			if ($(this).valid()) {
 				var name = $('#logger-name').val();
-				$.post(molgenis.getContextUrl() + '/logger/' + name + '/DEBUG').success(function() {
+				$.post(contextUrl + '/logger/' + name + '/DEBUG').success(function() {
 					renderLoggerTable();
-				});	
+				});
 			}
 		});
 	});
-}($, window.top.molgenis = window.top.molgenis || {}));
+});
