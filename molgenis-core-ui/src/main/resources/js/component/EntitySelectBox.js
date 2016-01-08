dc8a8d0cd8c47c74ea750ff491b850a7c9842e4f
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-08 17:46:28 +0100

    Require-ified EntitySelectBox component

--- molgenis-core-ui/src/main/resources/js/component/EntitySelectBox.js
+++ molgenis-core-ui/src/main/resources/js/component/EntitySelectBox.js
@@ -1,136 +1,168 @@
-/* global _: false, React: false, molgenis: true */
-(function(_, React, molgenis) {
-	"use strict";
-	
-	var div = React.DOM.div;
-	
-	var api = new molgenis.RestClient();
-	
+define(function(require, exports, module) {
 	/**
-	 * @memberOf component
+	 * @module EntitySelectBox
+	 */
+	"use strict";
+
+	var React = require('react');
+	var _ = require('underscore');
+
+	var Button = require('component/Button');
+	var Modal = require('component/Modal');
+
+	var DeepPureRenderMixin = require('component/mixin/DeepPureRenderMixin');
+	var EntityLoaderMixin = require('component/mixin/EntityLoaderMixin');
+	var ReactLayeredComponentMixin = require('component/mixin/ReactLayeredComponentMixin');
+
+	var Select2 = require('component/wrapper/Select2')
+
+	var div = React.DOM.div;
+
+	var api = new molgenis.RestClient();
+
+	/**
+	 * @memberOf EntitySelectBox
 	 */
 	var EntitySelectBox = React.createClass({
-		mixins: [molgenis.ui.mixin.DeepPureRenderMixin, molgenis.ui.mixin.EntityLoaderMixin, molgenis.ui.mixin.ReactLayeredComponentMixin],
-		displayName: 'EntitySelectBox',
-		propTypes: {
-			entity: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.object]).isRequired,
-			query: React.PropTypes.object,
-			mode: React.PropTypes.oneOf(['view', 'create']),
-			name: React.PropTypes.string,
-			disabled: React.PropTypes.bool,
-			readOnly: React.PropTypes.bool,
-			multiple: React.PropTypes.bool,
-			required: React.PropTypes.bool,
-			placeholder: React.PropTypes.string,
-			focus: React.PropTypes.bool,
-			value: React.PropTypes.oneOfType([React.PropTypes.object, React.PropTypes.array]),
-			onValueChange: React.PropTypes.func.isRequired
+		mixins : [ DeepPureRenderMixin, EntityLoaderMixin, ReactLayeredComponentMixin ],
+		displayName : 'EntitySelectBox',
+		propTypes : {
+			entity : React.PropTypes.oneOfType([ React.PropTypes.string, React.PropTypes.object ]).isRequired,
+			query : React.PropTypes.object,
+			mode : React.PropTypes.oneOf([ 'view', 'create' ]),
+			name : React.PropTypes.string,
+			disabled : React.PropTypes.bool,
+			readOnly : React.PropTypes.bool,
+			multiple : React.PropTypes.bool,
+			required : React.PropTypes.bool,
+			placeholder : React.PropTypes.string,
+			focus : React.PropTypes.bool,
+			value : React.PropTypes.oneOfType([ React.PropTypes.object, React.PropTypes.array ]),
+			onValueChange : React.PropTypes.func.isRequired
 		},
-		getDefaultProps: function() {
+		getDefaultProps : function() {
 			return {
-				mode: 'view'
+				mode : 'view'
 			};
 		},
-		getInitialState: function() {
+		getInitialState : function() {
 			return {
-				entity: null,
-				modal: false
+				entity : null,
+				modal : false
 			};
 		},
-		render: function() {
-			if(this.state.entity === null) {
+		render : function() {
+			if (this.state.entity === null) {
 				// entity meta data not fetched yet
 				return molgenis.ui.Spinner();
 			}
-			
+
 			var props = this.props;
 			var entity = this.state.entity;
-							
+
 			var options = {
-				enable: !this.props.disabled,
-				id: entity.idAttribute,
-				multiple: props.multiple,
+				enable : !this.props.disabled,
+				id : entity.idAttribute,
+				multiple : props.multiple,
 				allowClear : props.required ? false : true,
-				placeholder : props.placeholder || ' ', // cannot be an empty string
-				closeOnSelect: props.multiple !== true,
-			    query: this._select2Query,
-			    formatResult: this._select2FormatResult,
-			    formatSelection: this._select2FormatSelection
+				placeholder : props.placeholder || ' ', // cannot be an empty
+														// string
+				closeOnSelect : props.multiple !== true,
+				query : this._select2Query,
+				formatResult : this._select2FormatResult,
+				formatSelection : this._select2FormatSelection
 			};
-			
-			return molgenis.ui.wrapper.Select2({
+
+			return Select2({
 				options : options,
 				name : this.props.name,
 				disabled : this.props.disabled,
 				readOnly : this.props.readOnly,
-				focus: this.props.focus,
+				focus : this.props.focus,
 				value : this.props.value,
-				addonBtn: this.props.mode === 'create' && entity.writable === true,
-				addonBtnTitle: this.props.mode === 'create' && entity.writable === true ? 'Create ' + this.state.entity.label : undefined,
-				onAddonBtnClick: this._handleAddonBtnClick,
+				addonBtn : this.props.mode === 'create' && entity.writable === true,
+				addonBtnTitle : this.props.mode === 'create' && entity.writable === true ? 'Create ' + this.state.entity.label : undefined,
+				onAddonBtnClick : this._handleAddonBtnClick,
 				onChange : this._handleChange
 			});
 		},
-		renderLayer: function() {
-			if(this.state.entity === null) {
+		renderLayer : function() {
+			if (this.state.entity === null) {
 				return molgenis.ui.Spinner();
-			} else if(this.props.mode === 'create') {
+			} else if (this.props.mode === 'create') {
 				return this.state.modal ? molgenis.ui.Form({
 					entity : this.state.entity.name,
 					showHidden : true,
 					cancelBtn : true,
-					modal: true,
+					modal : true,
 					onSubmitCancel : this._onModalHide,
-					onSubmitSuccess: this._onModalHide
+					onSubmitSuccess : this._onModalHide
 				}) : div();
 			} else {
 				return div();
 			}
 		},
-		_handleChange: function(value) {
+		_handleChange : function(value) {
 			var val = this.props.multiple && value.length === 0 ? undefined : value;
-			this.props.onValueChange({value: val});
+			this.props.onValueChange({
+				value : val
+			});
 		},
-		_handleAddonBtnClick: function() {
-			this.setState({modal: true});
+		_handleAddonBtnClick : function() {
+			this.setState({
+				modal : true
+			});
 		},
-		_onModalHide: function() {
-			this.setState({modal: false});
+		_onModalHide : function() {
+			this.setState({
+				modal : false
+			});
 		},
-		_createQuery: function(term) {
+		_createQuery : function(term) {
 			var rules = [];
-			var nestedRule = null; 
-			
-			if(this.props.query) {
+			var nestedRule = null;
+
+			if (this.props.query) {
 				rules.push(this.props.query);
-				if(term.length > 0) {
-					rules.push({operator: 'AND'});
-					nestedRule = {operator: 'NESTED', nestedRules: []};
+				if (term.length > 0) {
+					rules.push({
+						operator : 'AND'
+					});
+					nestedRule = {
+						operator : 'NESTED',
+						nestedRules : []
+					};
 					rules.push(nestedRule);
 				}
 			}
-			
+
 			var likeRules = nestedRule === null ? rules : nestedRule.nestedRules;
-			if(term.length > 0) {
+			if (term.length > 0) {
 				var attrs = this._getAttrs();
-				for(var i = 0; i < attrs.length; ++i) {
+				for (var i = 0; i < attrs.length; ++i) {
 					var operator = 'LIKE';
-					switch(this.state.entity.attributes[attrs[i]].fieldType) {
-						case 'INT':
-						case 'LONG':
-						case 'BOOL':
-						case 'DATE':
-						case 'DATE_TIME':
-						case 'DECIMAL':
-							operator = 'EQUALS';
-							break;
-						case 'COMPOUND':
-							continue;
+					switch (this.state.entity.attributes[attrs[i]].fieldType) {
+					case 'INT':
+					case 'LONG':
+					case 'BOOL':
+					case 'DATE':
+					case 'DATE_TIME':
+					case 'DECIMAL':
+						operator = 'EQUALS';
+						break;
+					case 'COMPOUND':
+						continue;
 					}
-					if(i > 0) {
-						likeRules.push({operator: 'OR'});	
+					if (i > 0) {
+						likeRules.push({
+							operator : 'OR'
+						});
 					}
-					likeRules.push({field: attrs[i], operator: operator, value: term});
+					likeRules.push({
+						field : attrs[i],
+						operator : operator,
+						value : term
+					});
 				}
 			}
 			return rules;
@@ -141,8 +173,9 @@
 				var items = [];
 				items.push('<div class="row">');
 
-				var width = Math.round(12 / attrs.length); // FIXME fix in case of 5, 7 etc. lookup attributes
-				for(var i = 0; i < attrs.length; ++i) {
+				// FIXME fix in case of 5, 7 etc. lookup attributes
+				var width = Math.round(12 / attrs.length);
+				for (var i = 0; i < attrs.length; ++i) {
 					var attrName = attrs[i];
 					var attrLabel = this.state.entity.attributes[attrName].label;
 					var attrValue = item[attrName] !== undefined ? item[attrName] : '';
@@ -161,9 +194,9 @@
 		_select2FormatSelection : function(item) {
 			return item[this.state.entity.labelAttribute];
 		},
-		_select2Query : function (query) {
+		_select2Query : function(query) {
 			var num = 25;
-		    var q = {
+			var q = {
 				q : {
 					start : (query.page - 1) * num,
 					num : num,
@@ -173,34 +206,38 @@
 							property : this._getAttrs()[0]
 						} ]
 					},
-					q: this._createQuery(query.term),
-					expand: this._getAttrsWithRefEntity()
+					q : this._createQuery(query.term),
+					expand : this._getAttrsWithRefEntity()
 				}
 			};
-	    	
-	    	api.getAsync(this.state.entity.hrefCollection, q).done(function(data) {
-	    		query.callback({results: data.items, more: data.nextHref ? true : false});
-	    	});
+
+			api.getAsync(this.state.entity.hrefCollection, q).done(function(data) {
+				query.callback({
+					results : data.items,
+					more : data.nextHref ? true : false
+				});
+			});
 		},
-		_getAttrs: function() {
-			// display lookup attributes in dropdown or label attribute if no lookup attributes are defined
-			var entity = this.state.entity; 
-			if(entity.lookupAttributes && entity.lookupAttributes.length > 0) {
+		_getAttrs : function() {
+			// display lookup attributes in dropdown or label attribute if no
+			// lookup attributes are defined
+			var entity = this.state.entity;
+			if (entity.lookupAttributes && entity.lookupAttributes.length > 0) {
 				return entity.lookupAttributes;
 			} else {
-				return [entity.labelAttribute];
+				return [ entity.labelAttribute ];
 			}
 		},
-		_getAttrsWithRefEntity: function() {
+		_getAttrsWithRefEntity : function() {
 			var attrsWithRefEntity = _.filter(this.state.entity.attributes, function(attr) {
-				switch(attr.fieldType) {
-					case 'CATEGORICAL':
-					case 'CATEGORICAL_MREF':
-					case 'MREF':
-					case 'XREF':
-						return true;
-					default:
-						return false;
+				switch (attr.fieldType) {
+				case 'CATEGORICAL':
+				case 'CATEGORICAL_MREF':
+				case 'MREF':
+				case 'XREF':
+					return true;
+				default:
+					return false;
 				}
 			});
 			return _.map(attrsWithRefEntity, function(attr) {
@@ -208,10 +245,6 @@
 			});
 		}
 	});
-	
-	// export component
-	molgenis.ui = molgenis.ui || {};
-	_.extend(molgenis.ui, {
-		EntitySelectBox: React.createFactory(EntitySelectBox)
-	});
-}(_, React, molgenis));
+
+	module.exports = React.createFactory(EntitySelectBox)
+});
