cc31a37db7c8c8eceeb8c9c0336bf3ab18de549a
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-11 13:51:33 +0100

    webpack configuration file

---
+++ molgenis-core-ui/src/main/frontend/webpack.config.js
@@ -0,0 +1,36 @@
+// This file runs in node so should use commonsjs require syntax
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './main',
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/molgenis-core-ui/src/main/frontend/dist/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?presets[]=es2015,presets[]=react'],
+                exclude: /node_modules/,
+                include: __dirname
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+        ]
+    }
+}
