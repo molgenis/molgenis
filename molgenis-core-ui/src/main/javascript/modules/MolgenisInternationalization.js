7157f8bdd4b88a445ab94ea22e3b08074287289f
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-12 19:23:07 +0100

    Export directly to be able to import {method_name} from 'module_name'

--- molgenis-core-ui/src/main/javascript/modules/MolgenisInternationalization.js
+++ molgenis-core-ui/src/main/javascript/modules/MolgenisInternationalization.js
@@ -1,62 +1,59 @@
 /**
  * @module MolgenisInternationalization
  */
-'use strict';
 
 import $ from 'jquery';
 import React from 'react';
 import LanguageSelectBox from './react-components/LanguageSelectBox';
 
-var i18n = {
-    get: function(str, lang) {
-        lang = typeof lang !== 'undefined' ? lang : 'en';
-        var i18nObj;
+var i18nStrings = {};
 
-        if (str && (str.charAt(0) !== '{' || str.charAt(str.length - 1) !== '}')) {
-            i18nObj = {
-                'en': str
-            };
-        } else {
-            i18nObj = JSON.parse(str ? str : '{}');
-        }
+export function get(str, lang) {
+	lang = typeof lang !== 'undefined' ? lang : 'en';
+	var i18nObj;
 
-        return i18nObj[lang];
-    },
+	if (str && (str.charAt(0) !== '{' || str.charAt(str.length - 1) !== '}')) {
+	    i18nObj = {
+	        'en': str
+	    };
+	} else {
+	    i18nObj = JSON.parse(str ? str : '{}');
+	}
+	
+	return i18nObj[lang];
+}
 
-    getAll: function(str, lang) {
-        lang = typeof lang !== 'undefined' ? lang : 'en';
-        var i18nObj;
-        if (str && (str.charAt(0) !== '{' || str.charAt(str.length - 1) !== '}')) {
-            i18nObj = {
-                'en': str
-            };
-        } else {
-            i18nObj = JSON.parse(str ? str : '{}');
-        }
-        return i18nObj;
-    },
-
-    I18nStrings: function(callback) {
-        if (!i18n.i18nStrings) {
-            $.ajax({
-                type: 'GET',
-                url: '/api/v2/i18n',
-                contentType: 'application/json',
-                async: true,
-                success: function(data) {
-                    i18n.i18nStrings = data;
-                    callback(data);
-                }
-            });
-        } else {
-            callback(i18n.i18nStrings);
-        }
-    },
-
-    renderLanguageSelectBox: function(element) {
-        React.render(LanguageSelectBox({}), element);
+export function getAll(str, lang) {
+    lang = typeof lang !== 'undefined' ? lang : 'en';
+    var i18nObj;
+    if (str && (str.charAt(0) !== '{' || str.charAt(str.length - 1) !== '}')) {
+        i18nObj = {
+            'en': str
+        };
+    } else {
+        i18nObj = JSON.parse(str ? str : '{}');
     }
+    return i18nObj;
+}
 
-};
+export function I18nStrings(callback) {	
+	if (!i18nStrings) {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v2/i18n',
+            contentType: 'application/json',
+            async: true,
+            success: function(data) {
+                i18nStrings = data;
+                callback(data);
+            }
+        });
+    } else {
+        callback(i18nStrings);
+    }
+}
 
-export default i18n;
+export function renderLanguageSelectBox(element) {
+    React.render(LanguageSelectBox({}), element);
+}
+
