0f133dabcebabe534a9bebc80f865a4190b8ab3a
Fleur Kelpin <f.kelpin@umcg.nl>
2016-01-22 18:07:12 +0100

    Fix js bugs, the order matters

--- molgenis-core-ui/src/main/javascript/molgenis-vendor-webpack.js
+++ molgenis-core-ui/src/main/javascript/molgenis-vendor-webpack.js
@@ -1,22 +1,21 @@
 import "jquery";
-import "jq-edit-rangeslider";
-import "jquery-ui";
-import "jquery.cookie";
-import "select2";
 import "bootstrap";
-import "eonasdan-bootstrap-datetimepicker";
-import "react";
-import "react/addons"
 import "underscore";
+import "jquery-ui";
+import "react-components/css/wrapper/jquery-ui-1.9.2.custom.min.css";
+import "jquery.cookie";
 import "brace";
 import "moment";
-import "urijs";
-import "promise";
-
+import "eonasdan-bootstrap-datetimepicker";
 import "eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css";
-import "react-components/css/wrapper/select2.css";
-import "react-components/css/wrapper/jquery-ui-1.9.2.custom.min.css";
+import "jq-edit-rangeslider";
 import "react-components/css/wrapper/JQRangeSlider.css";
+import "select2";
+import "react-components/css/wrapper/select2.css";
+import "urijs";
+import "react";
+import "react/addons"
+import "promise";
 
 let mode = 'r';
 let theme = 'eclipse';
