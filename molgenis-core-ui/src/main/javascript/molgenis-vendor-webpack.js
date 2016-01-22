e5a4c98bb9f1c23fe4cd563dcf20c911d7eb79b8
Mark-de-Haan <markdehaan90@gmail.com>
2016-01-22 16:37:56 +0100

    Add inline css instead of using an alias

--- molgenis-core-ui/src/main/javascript/molgenis-vendor-webpack.js
+++ molgenis-core-ui/src/main/javascript/molgenis-vendor-webpack.js
@@ -1,21 +1,22 @@
 import "jquery";
-import "bootstrap";
-import "underscore";
-import "jquery-ui";
-import "react-components/css/wrapper/jquery-ui-1.9.2.custom.min.css";
-import "jquery.cookie";
-import "brace";
-import "moment";
-import "eonasdan-bootstrap-datetimepicker";
-import "eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css";
 import "jq-edit-rangeslider";
-import "react-components/css/wrapper/JQRangeSlider.css";
+import "jquery-ui";
+import "jquery.cookie";
 import "select2";
-import "react-components/css/wrapper/select2.css";
-import "urijs";
+import "bootstrap";
+import "eonasdan-bootstrap-datetimepicker";
 import "react";
 import "react/addons"
+import "underscore";
+import "brace";
+import "moment";
+import "urijs";
 import "promise";
+
+import "eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css";
+import "react-components/css/wrapper/select2.css";
+import "react-components/css/wrapper/jquery-ui-1.9.2.custom.min.css";
+import "react-components/css/wrapper/JQRangeSlider.css";
 
 let mode = 'r';
 let theme = 'eclipse';
