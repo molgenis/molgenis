399580ebba2f40f7f0e1c0fd30f1ff9b72051734
jjettenn <jjettenn@gmail.com>
2016-02-10 17:17:10 +0100

    ViewRepository 1. Remove search from view repository 2. Bugfix + refactor 3. Add count method

--- molgenis-data-view/src/main/java/org/molgenis/data/view/ViewRepository.java
+++ molgenis-data-view/src/main/java/org/molgenis/data/view/ViewRepository.java
@@ -19,9 +19,9 @@ import org.molgenis.data.DataService;
 import org.molgenis.data.Entity;
 import org.molgenis.data.EntityMetaData;
 import org.molgenis.data.EntityMetaData.AttributeRole;
+import org.molgenis.data.MolgenisDataException;
 import org.molgenis.data.Query;
 import org.molgenis.data.RepositoryCapability;
-import org.molgenis.data.elasticsearch.SearchService;
 import org.molgenis.data.support.AbstractRepository;
 import org.molgenis.data.support.DefaultAttributeMetaData;
 import org.molgenis.data.support.DefaultEntity;
@@ -35,16 +35,14 @@ import com.google.common.collect.Sets;
 public class ViewRepository extends AbstractRepository
 {
 	private static final Logger LOG = LoggerFactory.getLogger(ViewRepository.class);
-	private final SearchService searchService;
 	private final EntityMetaData entityMetaData;
 	private final DataService dataService;
 	public static final int BATCH_SIZE = 1000;
 
-	public ViewRepository(EntityMetaData entityMetaData, DataService dataService, SearchService searchService)
+	public ViewRepository(EntityMetaData entityMetaData, DataService dataService)
 	{
 		this.entityMetaData = requireNonNull(entityMetaData);
 		this.dataService = requireNonNull(dataService);
-		this.searchService = requireNonNull(searchService);
 	}
 
 	@Override
@@ -97,6 +95,12 @@ public class ViewRepository extends AbstractRepository
 	}
 
 	@Override
+	public long count(Query q)
+	{
+		return dataService.count(getMasterEntityName(), q);
+	}
+
+	@Override
 	public Stream<Entity> findAll(Query q)
 	{
 		//Master attributes to join on
@@ -125,11 +129,11 @@ public class ViewRepository extends AbstractRepository
 		});
 	}
 
-	private Entity getViewEntity(Entity masterEntity, EntityMetaData metaData, List<String> joinMasterMatrix,
+	private Entity getViewEntity(Entity masterEntity, EntityMetaData viewMetaData, List<String> joinMasterMatrix,
 			Map<String, List<String>> joinSlaveMatrix)
 	{
-		DefaultEntity me = new DefaultEntity(metaData, dataService);
-		me.set(masterEntity);
+		DefaultEntity me = new DefaultEntity(viewMetaData, dataService);
+		me.setOnlyAttributesWithSameMetadata(masterEntity);
 		
 		for (Entry<String, List<String>> entry : joinSlaveMatrix.entrySet())
 		{
@@ -139,12 +143,22 @@ public class ViewRepository extends AbstractRepository
 				if (i != 0) q.and();
 				q.eq(entry.getValue().get(i), masterEntity.get(joinMasterMatrix.get(i)));
 			}
-			Entity found = dataService.findOne(entry.getKey(), q);
 
-			for (String attributeName : found.getAttributeNames())
+			List<Entity> slaveEntities = dataService.findAll(entry.getKey(), q).collect(Collectors.toList());
+
+			if (slaveEntities.size() > 1)
 			{
-				me.set(entry.getKey() + "_" + attributeName, found.get(attributeName)); // We must test that we get only
-																						// one result
+				throw new MolgenisDataException("For the query: " + q + " in entity name: " + entry.getKey()
+						+ " where " + slaveEntities.size()
+						+ " results found. The VIEW backend supports only one or zero result");
+			}
+
+			if (slaveEntities.size() == 1)
+			{
+				for (String attributeName : slaveEntities.get(0).getAttributeNames())
+				{
+					me.set(entry.getKey() + "_" + attributeName, slaveEntities.get(0).get(attributeName));
+				}
 			}
 		}
 		return me;
@@ -155,7 +169,6 @@ public class ViewRepository extends AbstractRepository
 		return dataService.query(EntityViewMetaData.ENTITY_NAME)
 				.eq(EntityViewMetaData.VIEW_NAME, entityMetaData.getName()).findOne()
 				.getString(EntityViewMetaData.MASTER_ENTITY);
-
 	}
 
 	@Override
