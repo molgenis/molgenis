ddf4efc0b9f5652975ae5f2f69354b399ee33fde
Mark-de-Haan <markdehaan90@gmail.com>
2016-02-19 11:16:06 +0100

    Expand tests

--- molgenis-core-ui/src/test/javascript/JobComponentsTest.js
+++ molgenis-core-ui/src/test/javascript/JobComponentsTest.js
@@ -13,8 +13,8 @@ const job_success = {
 	status: "SUCCESS",
 	type: "TEST job",
 	entityName: "TapeTest",
-	progessMax: 1,
-	progressInt: 1,
+	progessMax: 1000,
+	progressInt: 1000,
 	progressMessage: "Test (started by Tape)",
 	submissionDate: "2100-01-01T00:00:00+0100",
 	startDate: "2100-01-01T00:15:00+0100",
@@ -26,8 +26,8 @@ const job_running = {
 	status: "RUNNING",
 	type: "TEST job",
 	entityName: "TapeTest",
-	progessMax: 1,
-	progressInt: 1,
+	progessMax: 500,
+	progressInt: 100,
 	progressMessage: "Test (started by Tape)",
 	submissionDate: "2200-01-01T00:00:00+0100",
 	startDate: "2200-01-01T00:15:00+0100",
@@ -39,8 +39,8 @@ const job_failed = {
 	status: "FAILED",
 	type: "TEST job",
 	entityName: "TapeTest",
-	progessMax: 1,
-	progressInt: 1,
+	progessMax: 200,
+	progressInt: 50,
 	progressMessage: "Test (started by Tape)",
 	submissionDate: "2300-01-01T00:00:00+0100",
 	startDate: "2300-01-01T00:15:00+0100",
@@ -57,8 +57,8 @@ test('Test if the Jobs component renders running, success and failed jobs correc
 	}));
 	
 	assert.equals(tree.toString(), 
-	'<div><div class="row"><div class="col-md-5"><div class="panel panel-primary"><div class="panel-heading">Running Jobs</div><div class="panel-body"><div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div></div></div></div><div class="col-md-7"><div><div class="panel panel-primary"><div class="panel-heading">Finished Jobs</div><div class="panel-body"><table class="table table-striped"><thead><th>Status</th><th>Runtime</th><th>Type</th><th>Message</th><th>Started</th><th>Finished</th><th>Link to entity</th></thead><tbody><tr><td>SUCCESS</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Fri, Jan 1, 2100 12:15 AM</td><td>in 84 years</td><td>URL to TapeTest</td></tr><tr><td>FAILED</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Mon, Jan 1, 2300 12:15 AM</td><td>in 284 years</td><td>URL to TapeTest</td></tr></tbody></table></div></div></div></div></div></div>', 
-	'Jobs component renders what we expect');
+	'<div class="row"><div class="col-md-5"><div class="panel panel-primary"><div class="panel-heading">Running Jobs</div><div class="panel-body"><div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div></div></div></div><div class="col-md-7"><div class="panel panel-primary"><div class="panel-heading">Finished Jobs</div><div class="panel-body"><table class="table table-striped"><thead><th>Status</th><th>Runtime</th><th>Type</th><th>Message</th><th>Started</th><th>Finished</th><th>Link to entity</th></thead><tbody><tr><td>SUCCESS</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Fri, Jan 1, 2100 12:15 AM</td><td>in 84 years</td><td>URL to TapeTest</td></tr><tr><td>FAILED</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Mon, Jan 1, 2300 12:15 AM</td><td>in 284 years</td><td>URL to TapeTest</td></tr></tbody></table></div></div></div></div>', 
+	'Jobs component rendered three types of jobs correctly');
 	
 	assert.end();
 });
@@ -70,7 +70,7 @@ test('Test if the RunningJobs component renders job progressbars', assert => {
 	
 	assert.equals(tree.toString(), 
 	'<div class="panel panel-primary"><div class="panel-heading">Running Jobs</div><div class="panel-body"><div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-success" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div><div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div><div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-danger" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div></div></div>', 
-	'RunningJobs component renders what we expect');
+	'RunningJobs component rendered progressbars for each job correctly');
 	
 	assert.end();
 });
@@ -81,8 +81,8 @@ test('Test if the JobTable renders a table with job information', assert => {
 	}));
 	
 	assert.equals(tree.toString(), 
-	'<div><div class="panel panel-primary"><div class="panel-heading">Finished Jobs</div><div class="panel-body"><table class="table table-striped"><thead><th>Status</th><th>Runtime</th><th>Type</th><th>Message</th><th>Started</th><th>Finished</th><th>Link to entity</th></thead><tbody><tr><td>SUCCESS</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Fri, Jan 1, 2100 12:15 AM</td><td>in 84 years</td><td>URL to TapeTest</td></tr><tr><td>RUNNING</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Wed, Jan 1, 2200 12:15 AM</td><td>in 184 years</td><td>URL to TapeTest</td></tr><tr><td>FAILED</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Mon, Jan 1, 2300 12:15 AM</td><td>in 284 years</td><td>URL to TapeTest</td></tr></tbody></table></div></div></div>', 
-	'JobTable component renders what we expect');
+	'<div class="panel panel-primary"><div class="panel-heading">Finished Jobs</div><div class="panel-body"><table class="table table-striped"><thead><th>Status</th><th>Runtime</th><th>Type</th><th>Message</th><th>Started</th><th>Finished</th><th>Link to entity</th></thead><tbody><tr><td>SUCCESS</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Fri, Jan 1, 2100 12:15 AM</td><td>in 84 years</td><td>URL to TapeTest</td></tr><tr><td>RUNNING</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Wed, Jan 1, 2200 12:15 AM</td><td>in 184 years</td><td>URL to TapeTest</td></tr><tr><td>FAILED</td><td>15 minutes</td><td>TEST job</td><td>Test (started by Tape)</td><td>Mon, Jan 1, 2300 12:15 AM</td><td>in 284 years</td><td>URL to TapeTest</td></tr></tbody></table></div></div>', 
+	'JobTable component rendered a table with job information correctly');
 	
 	assert.end();
 });
@@ -94,7 +94,7 @@ test('Test if the Job component renders a succeeded job correctly', assert => {
 	
 	assert.equals(tree.toString(), 
 	'<div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-success" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div>', 
-	'Job component renders what we expect');
+	'Job component rendered a succeeded job correctly');
 	
 	assert.end();
 });
@@ -106,7 +106,7 @@ test('Test if the Job component renders a running job correctly', assert => {
 	
 	assert.equals(tree.toString(), 
 	'<div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-primary progress-bar-striped active" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div>', 
-	'Job component renders what we expect');
+	'Job component rendered a running job correctly');
 	
 	assert.end();
 });
@@ -118,7 +118,7 @@ test('Test if the Job component renders a failed job correctly', assert => {
 	
 	assert.equals(tree.toString(), 
 	'<div><p>TEST job job<div class="progress background-lightgrey"><div class="progress-bar progress-bar-danger" role="progressbar" style="min-width:2em;width:100%;">Test (started by Tape)</div></div></p></div>', 
-	'Job component renders what we expect');
+	'Job component rendered a failed job correctly');
 	
 	assert.end();
 });
