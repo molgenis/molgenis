dd3006417b3d823b75acb888d094fadf6516d889
jjettenn <jjettenn@gmail.com>
2016-02-24 17:15:39 +0100

    Throw exception when the jar throws errors.

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/JarRunnerImpl.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/JarRunnerImpl.java
@@ -1,12 +1,19 @@
 package org.molgenis.data.annotation.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.molgenis.data.MolgenisDataException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JarRunnerImpl implements JarRunner
 {
+	private static final Logger LOG = LoggerFactory.getLogger(JarRunnerImpl.class);
 
 	@Override
 	public File runJar(String outputFile, List<String> params, File inputVcf) throws IOException, InterruptedException
@@ -21,10 +28,24 @@ public class JarRunnerImpl implements JarRunner
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectOutput(outputVcf);
 
-		// Error logging to standard logging.
-		pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-
 		Process p = pb.start();
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		StringBuilder msgSB = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null)
+		{
+			msgSB.append(line);
+		}
+		
+		String msg = msgSB.toString();
+		if(!msg.isEmpty()){
+			// Error logging to standard logging.
+			LOG.error(msg);
+
+			// Throw exception
+			throw new MolgenisDataException(msg);
+		}
+	
 		p.waitFor();
 		return outputVcf;
 	}
