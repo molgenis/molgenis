cdee02d3e7a5211d6ed8d82b7186bee133b32160
jjettenn <jjettenn@gmail.com>
2016-02-26 11:35:46 +0100

    Restore old functionality

--- molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/JarRunnerImpl.java
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/utils/JarRunnerImpl.java
@@ -1,19 +1,12 @@
 package org.molgenis.data.annotation.utils;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.molgenis.data.MolgenisDataException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class JarRunnerImpl implements JarRunner
 {
-	private static final Logger LOG = LoggerFactory.getLogger(JarRunnerImpl.class);
 
 	@Override
 	public File runJar(String outputFile, List<String> params, File inputVcf) throws IOException, InterruptedException
@@ -28,24 +21,10 @@ public class JarRunnerImpl implements JarRunner
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectOutput(outputVcf);
 
-		Process p = pb.start();
-		BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-		StringBuilder msgSB = new StringBuilder();
-		String line;
-		while ((line = br.readLine()) != null)
-		{
-			msgSB.append(line);
-		}
-		
-		String msg = msgSB.toString();
-		if(!msg.isEmpty()){
-			// Error logging to standard logging.
-			LOG.error(msg);
+		// Error logging to standard logging.
+		pb.redirectError(ProcessBuilder.Redirect.INHERIT);
 
-			// Throw exception
-			throw new MolgenisDataException(msg);
-		}
-	
+		Process p = pb.start();
 		p.waitFor();
 		return outputVcf;
 	}
