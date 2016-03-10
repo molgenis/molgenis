0d1ec8f7681107958c66bb45964ba53c0fbbf73f
jjettenn <jjettenn@gmail.com>
2016-03-10 10:36:06 +0100

    Fix base package problems

--- molgenis-data-rest/src/main/java/org/molgenis/data/rest/v2/RestControllerV2.java
+++ molgenis-data-rest/src/main/java/org/molgenis/data/rest/v2/RestControllerV2.java
@@ -56,6 +56,7 @@ import org.molgenis.data.meta.MetaValidationUtils;
 import org.molgenis.data.rest.EntityPager;
 import org.molgenis.data.rest.Href;
 import org.molgenis.data.rest.service.RestService;
+import org.molgenis.data.support.EntityMetaDataUtils;
 import org.molgenis.data.support.QueryImpl;
 import org.molgenis.security.core.MolgenisPermissionService;
 import org.molgenis.security.core.Permission;
@@ -346,27 +347,28 @@ class RestControllerV2
 		// No repo
 		if (!dataService.hasRepository(entityName)) throw createUnknownEntityException(entityName);
 
-		Repository repositoryToCopy = dataService.getRepository(entityName);
+		Repository repositoryToCopyFrom = dataService.getRepository(entityName);
 
 		// Check if the entity already exists
-		String newFullName = repositoryToCopy.getEntityMetaData().getPackage().getName() + "_"
-				+ request.getNewEntityName();
-		if (dataService.hasRepository(newFullName)) throw createDuplicateEntityException(request.getNewEntityName());
+		String newFullName = EntityMetaDataUtils.buildFullName(repositoryToCopyFrom.getEntityMetaData().getPackage(),
+				request.getNewEntityName());
+		if (dataService.hasRepository(newFullName)) throw createDuplicateEntityException(newFullName);
 
 		// Validate the new name
 		MetaValidationUtils.validateName(request.getNewEntityName());
 
 		// Permission
-		boolean readPermission = permissionService.hasPermissionOnEntity(repositoryToCopy.getName(), Permission.READ);
+		boolean readPermission = permissionService.hasPermissionOnEntity(repositoryToCopyFrom.getName(),
+				Permission.READ);
 		if (!readPermission) throw createNoReadPermissionOnEntityException(entityName);
 
 		// Capabilities
-		boolean writableCapabilities = dataService.getCapabilities(repositoryToCopy.getName()).contains(
+		boolean writableCapabilities = dataService.getCapabilities(repositoryToCopyFrom.getName()).contains(
 				RepositoryCapability.WRITABLE);
 		if (!writableCapabilities) throw createNoWriteCapabilitiesOnEntityException(entityName);
 
 		// Copy
-		this.copyRepositoryRunAsSystem(repositoryToCopy, request.getNewEntityName(), request.getNewEntityName());
+		this.copyRepositoryRunAsSystem(repositoryToCopyFrom, request.getNewEntityName(), request.getNewEntityName());
 
 		// Retrieve new repo
 		Repository repository = dataService.getRepository(newFullName);
@@ -379,9 +381,11 @@ class RestControllerV2
 		return repository.getName();
 	}
 
-	private void copyRepositoryRunAsSystem(Repository repository, String newRepositoryId, String newRepositoryLabel)
+	private void copyRepositoryRunAsSystem(Repository repositoryToCopyFrom, String newRepositoryId,
+			String newRepositoryLabel)
 	{
-		RunAsSystemProxy.runAsSystem(() -> dataService.copyRepository(repository, newRepositoryId, newRepositoryLabel));
+		RunAsSystemProxy.runAsSystem(() -> dataService.copyRepository(repositoryToCopyFrom, newRepositoryId,
+				newRepositoryLabel));
 	}
 
 	/**
