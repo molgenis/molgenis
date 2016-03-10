d9d3ad37c6a4a0ceb414189b9cebdf9f974234a9
Fleur Kelpin <f.kelpin@umcg.nl>
2016-03-10 10:48:47 +0100

    Remove unused import.

--- molgenis-data-import/src/main/java/org/molgenis/data/importer/ImportWizardController.java
+++ molgenis-data-import/src/main/java/org/molgenis/data/importer/ImportWizardController.java
@@ -58,7 +58,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
