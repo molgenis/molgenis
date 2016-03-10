d89a2226876d8e7398909e4732a50746bbf60a81
jjettenn <jjettenn@gmail.com>
2016-03-10 10:03:02 +0100

    Add a buildFullName method

--- molgenis-data/src/main/java/org/molgenis/data/support/EntityMetaDataUtils.java
+++ molgenis-data/src/main/java/org/molgenis/data/support/EntityMetaDataUtils.java
@@ -5,6 +5,7 @@ import static java.util.stream.StreamSupport.stream;
 import java.util.Iterator;
 
 import org.molgenis.data.AttributeMetaData;
+import org.molgenis.data.Package;
 
 public class EntityMetaDataUtils
 {
@@ -27,5 +28,28 @@ public class EntityMetaDataUtils
 				return stream(attrs.spliterator(), false).map(AttributeMetaData::getName).iterator();
 			}
 		};
+	}
+
+	/**
+	 * Builds and returns an entity full name based on a package and a simpleName
+	 * 
+	 * @param package_
+	 * @param simpleName
+	 * @return String entity full name
+	 */
+	public static String buildFullName(Package package_, String simpleName)
+	{
+		if (package_ != null && !Package.DEFAULT_PACKAGE_NAME.equals(package_.getName()))
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(package_.getName());
+			sb.append(Package.PACKAGE_SEPARATOR);
+			sb.append(simpleName);
+			return sb.toString();
+		}
+		else
+		{
+			return simpleName;
+		}
 	}
 }
