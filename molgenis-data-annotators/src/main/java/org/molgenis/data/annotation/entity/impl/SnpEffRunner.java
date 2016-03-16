027831043bbe4e50aabe84a7433fb47a1cfe8d2d
tommydeboer <tommydeboer4@gmail.com>
2016-03-16 13:22:29 +0100

    add SnpEffRunner that returns stream of effect entities

---
+++ molgenis-data-annotators/src/main/java/org/molgenis/data/annotation/entity/impl/SnpEffRunner.java
@@ -0,0 +1,304 @@
+package org.molgenis.data.annotation.entity.impl;
+
+import static java.io.File.createTempFile;
+import static java.util.stream.Collectors.toList;
+import static org.molgenis.data.support.VcfEffectsMetaData.ALT;
+import static org.molgenis.data.support.VcfEffectsMetaData.ANNOTATION;
+import static org.molgenis.data.support.VcfEffectsMetaData.CDS_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.C_DNA_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.DISTANCE_TO_FEATURE;
+import static org.molgenis.data.support.VcfEffectsMetaData.ERRORS;
+import static org.molgenis.data.support.VcfEffectsMetaData.FEATURE_ID;
+import static org.molgenis.data.support.VcfEffectsMetaData.FEATURE_TYPE;
+import static org.molgenis.data.support.VcfEffectsMetaData.GENE;
+import static org.molgenis.data.support.VcfEffectsMetaData.GENE_ID;
+import static org.molgenis.data.support.VcfEffectsMetaData.GENE_NAME;
+import static org.molgenis.data.support.VcfEffectsMetaData.HGVS_C;
+import static org.molgenis.data.support.VcfEffectsMetaData.HGVS_P;
+import static org.molgenis.data.support.VcfEffectsMetaData.PROTEIN_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.PUTATIVE_IMPACT;
+import static org.molgenis.data.support.VcfEffectsMetaData.RANK_TOTAL;
+import static org.molgenis.data.support.VcfEffectsMetaData.TRANSCRIPT_BIOTYPE;
+import static org.molgenis.data.support.VcfEffectsMetaData.VARIANT;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.elasticsearch.common.collect.Iterables;
+import org.molgenis.data.Entity;
+import org.molgenis.data.EntityMetaData;
+import org.molgenis.data.MolgenisDataException;
+import org.molgenis.data.annotation.snpEff.SnpEffResultIterator;
+import org.molgenis.data.annotation.utils.JarRunner;
+import org.molgenis.data.annotator.websettings.SnpEffAnnotatorSettings;
+import org.molgenis.data.support.DefaultEntityMetaData;
+import org.molgenis.data.support.MapEntity;
+import org.molgenis.data.support.VcfEffectsMetaData;
+import org.molgenis.data.vcf.VcfRepository;
+import org.molgenis.security.core.runas.RunAsSystemProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.beust.jcommander.internal.Lists;
+
+@Component
+public class SnpEffRunner
+{
+	private static final Logger LOG = LoggerFactory.getLogger(SnpEffAnnotator.class);
+
+	private JarRunner jarRunner;
+	private SnpEffAnnotatorSettings snpEffAnnotatorSettings;
+	private String snpEffPath;
+
+	private static final String CHARSET = "UTF-8";
+
+	public static final String NAME = "snpEff";
+	public static final String LOF = "LOF";
+	public static final String NMD = "NMD";
+	public static final String ANN = "ANN";
+
+	public enum Impact
+	{
+		MODIFIER, LOW, MODERATE, HIGH
+	}
+
+	@Autowired
+	public SnpEffRunner(JarRunner jarRunner, SnpEffAnnotatorSettings snpEffAnnotatorSettings)
+	{
+		this.jarRunner = jarRunner;
+		this.snpEffAnnotatorSettings = snpEffAnnotatorSettings;
+	}
+
+	public Stream<Entity> getSnpEffects(Iterable<Entity> source)
+	{
+		File inputVcf = null;
+		try
+		{
+			inputVcf = getInputVcfFile(source);
+		}
+		catch (IOException e)
+		{
+			throw new MolgenisDataException("Exception making temporary VCF file", e);
+
+		}
+		return getSnpEffects(source, inputVcf);
+	}
+
+	@SuppressWarnings("resource")
+	public Stream<Entity> getSnpEffects(Iterable<Entity> source, final File inputVcf)
+	{
+		try
+		{
+			EntityMetaData sourceEMD = source.iterator().next().getEntityMetaData();
+
+			if (Iterables.isEmpty(source)) return Stream.empty();
+
+			List<String> params = Arrays.asList("-Xmx2g", getSnpEffPath(), "hg19", "-noStats", "-noLog", "-lof",
+					"-canon", "-ud", "0", "-spliceSiteSize", "5");
+			File outputVcf = jarRunner.runJar(NAME, params, inputVcf);
+
+			// TODO always output to "snpEff"? won't this overwrite when you do parallel annotations?
+			File snpEffOutputWithMetaData = addVcfMetaDataToOutputVcf(outputVcf);
+			VcfRepository repo = new VcfRepository(snpEffOutputWithMetaData, "SNPEFF_OUTPUT_VCF_" + inputVcf.getName());
+
+			SnpEffResultIterator snpEffResultIterator = new SnpEffResultIterator(repo.iterator());
+
+			DefaultEntityMetaData effectsEMD = new VcfEffectsMetaData(sourceEMD.getSimpleName() + "_EFFECTS",
+					sourceEMD.getPackage(), sourceEMD);
+
+			List<Entity> effectsEntities = Lists.newArrayList();
+			for (Entity sourceEntity : source)
+			{
+				String chromosome = sourceEntity.getString(VcfRepository.CHROM);
+				Long position = sourceEntity.getLong(VcfRepository.POS);
+
+				if (chromosome != null && position != null)
+				{
+					Entity snpEffEntity = snpEffResultIterator.get(chromosome, position);
+					if (snpEffEntity != null)
+					{
+						effectsEntities.addAll(getSnpEffectsFromSnpEffEntity(sourceEntity, snpEffEntity, effectsEMD));
+					}
+				}
+			}
+
+			return effectsEntities.stream();
+		}
+		catch (IOException e)
+		{
+			throw new UncheckedIOException(e);
+		}
+		catch (InterruptedException e)
+		{
+			throw new MolgenisDataException("Exception running SnpEff", e);
+		}
+	}
+
+	// ANN=G|intron_variant|MODIFIER|LOC101926913|LOC101926913|transcript|NR_110185.1|Noncoding|5/5|n.376+9526G>C||||||,G|non_coding_exon_variant|MODIFIER|LINC01124|LINC01124|transcript|NR_027433.1|Noncoding|1/1|n.590G>C||||||;
+	private List<Entity> getSnpEffectsFromSnpEffEntity(Entity sourceEntity, Entity snpEffEntity,
+			DefaultEntityMetaData effectsEMD)
+	{
+		String[] annotations = snpEffEntity.getString(SnpEffAnnotator.ANN).split(Pattern.quote(","), -1);
+
+		// LOF and NMD fields can't be associated with a single allele-gene combination so we log them instead of
+		// including them
+		String lof = snpEffEntity.getString(SnpEffAnnotator.LOF);
+		String nmd = snpEffEntity.getString(SnpEffAnnotator.NMD);
+		if (lof != null || nmd != null)
+		{
+			LOG.info("LOF / NMD found for CHROM:{} POS:{} ANN:{} LOF:{} NMD:{} ",
+					snpEffEntity.getString(VcfRepository.CHROM), snpEffEntity.getString(VcfRepository.POS),
+					snpEffEntity.getString(SnpEffAnnotator.ANN), lof, nmd);
+		}
+
+		List<Entity> effects = Lists.newArrayList();
+		for (String annotation : annotations)
+		{
+			String[] fields = annotation.split(Pattern.quote("|"), -1);
+
+			MapEntity effect = new MapEntity(effectsEMD);
+
+			if (fields.length >= 15)
+			{
+				effect.set(ALT, fields[0]);
+				effect.set(GENE, fields[4]);
+				effect.set(VARIANT, sourceEntity);
+
+				effect.set(ANNOTATION, fields[1]);
+				effect.set(PUTATIVE_IMPACT, fields[2]);
+				effect.set(GENE_NAME, fields[3]);
+				effect.set(GENE_ID, fields[4]);
+				effect.set(FEATURE_TYPE, fields[5]);
+				effect.set(FEATURE_ID, fields[6]);
+				effect.set(TRANSCRIPT_BIOTYPE, fields[7]);
+				effect.set(RANK_TOTAL, fields[8]);
+				effect.set(HGVS_C, fields[9]);
+				effect.set(HGVS_P, fields[10]);
+				effect.set(C_DNA_POSITION, fields[11]);
+				effect.set(CDS_POSITION, fields[12]);
+				effect.set(PROTEIN_POSITION, fields[13]);
+				effect.set(DISTANCE_TO_FEATURE, fields[14]);
+				effect.set(ERRORS, fields[15]);
+			}
+			else
+			{
+				LOG.info("No results for CHROM:{} POS:{} REF:{} ALT:{} ", effect.getString(VcfRepository.CHROM),
+						effect.getString(VcfRepository.POS), effect.getString(VcfRepository.REF),
+						effect.getString(VcfRepository.ALT));
+			}
+
+			effects.add(effect);
+		}
+
+		return effects;
+	}
+
+	/**
+	 * Takes the VCF produced by SnpEff, adds metadata, and returns a file that can be used to create a VcfRepository
+	 * 
+	 * @param outputVcf
+	 * @return
+	 * @throws IOException
+	 */
+	private File addVcfMetaDataToOutputVcf(File outputVcf) throws IOException
+	{
+		File snpEffOutputWithMetaData = createTempFile(NAME + "_withMetaData", ".vcf");
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(outputVcf.getAbsolutePath()), CHARSET));
+
+		List<String> lines = reader.lines().filter(line -> !line.startsWith("##SnpEff")).collect(toList());
+		reader.close();
+
+		FileWriter writer = new FileWriter(snpEffOutputWithMetaData);
+		boolean metaDone = false;
+		for (String line : lines)
+		{
+			if (!line.startsWith(VcfRepository.PREFIX) && metaDone == false)
+			{
+				writer.write(VcfRepository.CHROM + "\t" + VcfRepository.POS + "\t" + VcfRepository.ID + "\t"
+						+ VcfRepository.REF + "\t" + VcfRepository.ALT + "\t" + VcfRepository.QUAL + "\t"
+						+ VcfRepository.FILTER + "\t" + VcfRepository.INFO + "\n");
+				metaDone = true;
+			}
+			writer.write(line + "\n");
+		}
+		writer.close();
+
+		return snpEffOutputWithMetaData;
+	}
+
+	/**
+	 * Converts entities to a VCF file that can be passed to SnpEff.
+	 * 
+	 * @param source
+	 *            the Entities to convert to VCF
+	 * @return a VCF file
+	 */
+	public File getInputVcfFile(Iterable<Entity> source) throws IOException
+	{
+		File vcf = createTempFile(NAME, ".vcf");
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(vcf), CHARSET)))
+		{
+
+			for (Entity entity : source)
+			{
+				StringBuilder builder = new StringBuilder();
+				builder.append(entity.getString(VcfRepository.CHROM));
+				builder.append("\t");
+				builder.append(entity.getString(VcfRepository.POS));
+				builder.append("\t.\t");
+				builder.append(entity.getString(VcfRepository.REF));
+				builder.append("\t");
+				builder.append(entity.getString(VcfRepository.ALT));
+				builder.append("\n");
+				bw.write(builder.toString());
+			}
+		}
+
+		return vcf;
+	}
+
+	/**
+	 * Gets the path to the SnpEff JAR. Returns null when the path is not found or snpEffAnnotatorSettings is null.
+	 * 
+	 * @return the path to the SnpEff JAR, or nul
+	 */
+	private String getSnpEffPath()
+	{
+		if ((snpEffAnnotatorSettings != null) && (snpEffPath == null))
+		{
+			snpEffPath = RunAsSystemProxy.runAsSystem(
+					() -> snpEffAnnotatorSettings.getString(SnpEffAnnotatorSettings.Meta.SNPEFF_JAR_LOCATION));
+
+			if (snpEffPath != null)
+			{
+				File snpEffFile = new File(snpEffPath);
+				if (snpEffFile.exists() && snpEffFile.isFile())
+				{
+					LOG.info("SnpEff found at: " + snpEffFile.getAbsolutePath());
+				}
+				else
+				{
+					LOG.debug("SnpEff not found at: " + snpEffFile.getAbsolutePath());
+					snpEffPath = null;
+				}
+			}
+		}
+
+		return snpEffPath;
+	}
+}
---
+++ molgenis-data-annotators/src/test/java/org/molgenis/data/annotation/entity/impl/SnpEffRunnerTest.java
@@ -0,0 +1,247 @@
+package org.molgenis.data.annotation.entity.impl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.molgenis.data.EntityMetaData.AttributeRole.ROLE_ID;
+import static org.molgenis.data.support.VcfEffectsMetaData.ANNOTATION;
+import static org.molgenis.data.support.VcfEffectsMetaData.CDS_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.C_DNA_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.DISTANCE_TO_FEATURE;
+import static org.molgenis.data.support.VcfEffectsMetaData.ERRORS;
+import static org.molgenis.data.support.VcfEffectsMetaData.FEATURE_ID;
+import static org.molgenis.data.support.VcfEffectsMetaData.FEATURE_TYPE;
+import static org.molgenis.data.support.VcfEffectsMetaData.GENE_ID;
+import static org.molgenis.data.support.VcfEffectsMetaData.GENE_NAME;
+import static org.molgenis.data.support.VcfEffectsMetaData.HGVS_C;
+import static org.molgenis.data.support.VcfEffectsMetaData.HGVS_P;
+import static org.molgenis.data.support.VcfEffectsMetaData.PROTEIN_POSITION;
+import static org.molgenis.data.support.VcfEffectsMetaData.PUTATIVE_IMPACT;
+import static org.molgenis.data.support.VcfEffectsMetaData.RANK_TOTAL;
+import static org.molgenis.data.support.VcfEffectsMetaData.TRANSCRIPT_BIOTYPE;
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.molgenis.MolgenisFieldTypes;
+import org.molgenis.data.AttributeMetaData;
+import org.molgenis.data.Entity;
+import org.molgenis.data.annotation.utils.JarRunner;
+import org.molgenis.data.annotator.websettings.SnpEffAnnotatorSettings;
+import org.molgenis.data.support.DefaultAttributeMetaData;
+import org.molgenis.data.support.DefaultEntityMetaData;
+import org.molgenis.data.support.MapEntity;
+import org.molgenis.data.support.VcfEffectsMetaData;
+import org.molgenis.data.vcf.VcfRepository;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SnpEffRunnerTest
+{
+	private final ArrayList<Entity> entities = new ArrayList<>();;
+	private DefaultEntityMetaData metaDataCanAnnotate;
+	private SnpEffRunner snpEffRunner;
+	private JarRunner jarRunner;
+	private VcfEffectsMetaData effectsEMD;
+
+	@BeforeMethod
+	public void beforeMethod() throws IOException
+	{
+		jarRunner = mock(JarRunner.class);
+		SnpEffAnnotatorSettings settings = mock(SnpEffAnnotatorSettings.class);
+
+		snpEffRunner = new SnpEffRunner(jarRunner, settings);
+
+		metaDataCanAnnotate = new DefaultEntityMetaData("test");
+		AttributeMetaData attributeMetaDataChrom = new DefaultAttributeMetaData(VcfRepository.CHROM,
+				MolgenisFieldTypes.FieldTypeEnum.STRING);
+		AttributeMetaData attributeMetaDataPos = new DefaultAttributeMetaData(VcfRepository.POS,
+				MolgenisFieldTypes.FieldTypeEnum.LONG);
+		AttributeMetaData attributeMetaDataRef = new DefaultAttributeMetaData(VcfRepository.REF,
+				MolgenisFieldTypes.FieldTypeEnum.STRING);
+		AttributeMetaData attributeMetaDataAlt = new DefaultAttributeMetaData(VcfRepository.ALT,
+				MolgenisFieldTypes.FieldTypeEnum.STRING);
+		metaDataCanAnnotate.addAttributeMetaData(attributeMetaDataChrom, ROLE_ID);
+		metaDataCanAnnotate.addAttributeMetaData(attributeMetaDataPos);
+		metaDataCanAnnotate.addAttributeMetaData(attributeMetaDataRef);
+		metaDataCanAnnotate.addAttributeMetaData(attributeMetaDataAlt);
+
+		Entity entity1 = new MapEntity(metaDataCanAnnotate);
+		entity1.set(VcfRepository.CHROM, "1");
+		entity1.set(VcfRepository.POS, 13380);
+		entity1.set(VcfRepository.REF, "C");
+		entity1.set(VcfRepository.ALT, "G");
+
+		Entity entity2 = new MapEntity(metaDataCanAnnotate);
+		entity2.set(VcfRepository.CHROM, "1");
+		entity2.set(VcfRepository.POS, 13980);
+		entity2.set(VcfRepository.REF, "T");
+		entity2.set(VcfRepository.ALT, "C");
+
+		Entity entity3 = new MapEntity(metaDataCanAnnotate);
+		entity3.set(VcfRepository.CHROM, "1");
+		entity3.set(VcfRepository.POS, 78383467);
+		entity3.set(VcfRepository.REF, "G");
+		entity3.set(VcfRepository.ALT, "A");
+
+		Entity entity4 = new MapEntity(metaDataCanAnnotate);
+		entity4.set(VcfRepository.CHROM, "1");
+		entity4.set(VcfRepository.POS, 231094050);
+		entity4.set(VcfRepository.REF, "GAA");
+		entity4.set(VcfRepository.ALT, "G,GAAA,GA");
+
+		Entity entity5 = new MapEntity(metaDataCanAnnotate);
+		entity5.set(VcfRepository.CHROM, "2");
+		entity5.set(VcfRepository.POS, 171570151);
+		entity5.set(VcfRepository.REF, "C");
+		entity5.set(VcfRepository.ALT, "T");
+
+		Entity entity6 = new MapEntity(metaDataCanAnnotate);
+		entity6.set(VcfRepository.CHROM, "4");
+		entity6.set(VcfRepository.POS, 69964234);
+		entity6.set(VcfRepository.REF, "CT");
+		entity6.set(VcfRepository.ALT, "CTT,CTTT,C");
+
+		Entity entity7 = new MapEntity(metaDataCanAnnotate);
+		entity7.set(VcfRepository.CHROM, "15");
+		entity7.set(VcfRepository.POS, 66641732);
+		entity7.set(VcfRepository.REF, "G");
+		entity7.set(VcfRepository.ALT, "A,C,T");
+
+		Entity entity8 = new MapEntity(metaDataCanAnnotate);
+		entity8.set(VcfRepository.CHROM, "21");
+		entity8.set(VcfRepository.POS, 46924425);
+		entity8.set(VcfRepository.REF, "CGGCCCCCCA");
+		entity8.set(VcfRepository.ALT, "C");
+
+		Entity entity9 = new MapEntity(metaDataCanAnnotate);
+		entity9.set(VcfRepository.CHROM, "X");
+		entity9.set(VcfRepository.POS, 79943569);
+		entity9.set(VcfRepository.REF, "T");
+		entity9.set(VcfRepository.ALT, "C");
+
+		Entity entity10 = new MapEntity(metaDataCanAnnotate);
+		entity10.set(VcfRepository.CHROM, "2");
+		entity10.set(VcfRepository.POS, 191904021);
+		entity10.set(VcfRepository.REF, "G");
+		entity10.set(VcfRepository.ALT, "T");
+
+		Entity entity11 = new MapEntity(metaDataCanAnnotate);
+		entity11.set(VcfRepository.CHROM, "3");
+		entity11.set(VcfRepository.POS, 53219680);
+		entity11.set(VcfRepository.REF, "G");
+		entity11.set(VcfRepository.ALT, "C");
+
+		Entity entity12 = new MapEntity(metaDataCanAnnotate);
+		entity12.set(VcfRepository.CHROM, "2");
+		entity12.set(VcfRepository.POS, 219142023);
+		entity12.set(VcfRepository.REF, "G");
+		entity12.set(VcfRepository.ALT, "A");
+
+		Entity entity13 = new MapEntity(metaDataCanAnnotate);
+		entity13.set(VcfRepository.CHROM, "1");
+		entity13.set(VcfRepository.POS, 1115548);
+		entity13.set(VcfRepository.REF, "G");
+		entity13.set(VcfRepository.ALT, "A");
+
+		Entity entity14 = new MapEntity(metaDataCanAnnotate);
+		entity14.set(VcfRepository.CHROM, "21");
+		entity14.set(VcfRepository.POS, 45650009);
+		entity14.set(VcfRepository.REF, "T");
+		entity14.set(VcfRepository.ALT, "TG, A, G");
+
+		entities.add(entity1);
+		entities.add(entity2);
+		entities.add(entity3);
+		entities.add(entity4);
+		entities.add(entity5);
+		entities.add(entity6);
+		entities.add(entity7);
+		entities.add(entity8);
+		entities.add(entity9);
+		entities.add(entity10);
+		entities.add(entity11);
+		entities.add(entity12);
+		entities.add(entity13);
+		entities.add(entity14);
+
+		effectsEMD = new VcfEffectsMetaData("test_EFFECTS", null, metaDataCanAnnotate);
+	}
+
+	@Test
+	public void annotateCountTest()
+	{
+		try
+		{
+			List<String> params = Arrays.asList("-Xmx2g", null, "hg19", "-noStats", "-noLog", "-lof", "-canon", "-ud",
+					"0", "-spliceSiteSize", "5");
+			when(jarRunner.runJar(SnpEffAnnotator.NAME, params, new File("src/test/resources/test-edgecases.vcf")))
+					.thenReturn(new File("src/test/resources/snpEffOutputCount.vcf"));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		Stream<Entity> results = snpEffRunner.getSnpEffects(entities,
+				new File("src/test/resources/test-edgecases.vcf"));
+		int size = results.collect(Collectors.toList()).size();
+		assertEquals(size, 24);
+	}
+
+	@Test
+	public void getSnpEffectsTest()
+	{
+		try
+		{
+			List<String> params = Arrays.asList("-Xmx2g", null, "hg19", "-noStats", "-noLog", "-lof", "-canon", "-ud",
+					"0", "-spliceSiteSize", "5");
+			when(jarRunner.runJar(SnpEffAnnotator.NAME, params, new File("src/test/resources/test-snpeff.vcf")))
+					.thenReturn(new File("src/test/resources/snpEffOutput.vcf"));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		Stream<Entity> results = snpEffRunner.getSnpEffects(Collections.singletonList(entities.get(0)),
+				new File("src/test/resources/test-snpeff.vcf"));
+
+		results.forEach(result -> {
+			Entity expected = new MapEntity(effectsEMD);
+
+			// copy id from result because it's auto generated
+			expected.set(VcfEffectsMetaData.ID, result.getIdValue());
+			expected.set(VcfEffectsMetaData.ALT, result.get(VcfEffectsMetaData.ALT));
+			expected.set(VcfEffectsMetaData.GENE, "DDX11L1");
+			expected.set(VcfEffectsMetaData.VARIANT, entities.get(0));
+
+			expected.set(ANNOTATION, "non_coding_exon_variant");
+			expected.set(PUTATIVE_IMPACT, "MODIFIER");
+			expected.set(GENE_NAME, "DDX11L1");
+			expected.set(GENE_ID, "DDX11L1");
+			expected.set(FEATURE_TYPE, "transcript");
+			expected.set(FEATURE_ID, "NR_046018.2");
+			expected.set(TRANSCRIPT_BIOTYPE, "Noncoding");
+			expected.set(RANK_TOTAL, "3/3");
+			expected.set(HGVS_C, "n.623C>G");
+			expected.set(HGVS_P, "");
+			expected.set(C_DNA_POSITION, "");
+			expected.set(CDS_POSITION, "");
+			expected.set(PROTEIN_POSITION, "");
+			expected.set(DISTANCE_TO_FEATURE, "");
+			expected.set(ERRORS, "");
+
+			for (AttributeMetaData attributeMetaData : effectsEMD.getAtomicAttributes())
+			{
+				System.out.println(attributeMetaData.getName());
+				assertEquals(result.get(attributeMetaData.getName()), expected.get(attributeMetaData.getName()));
+			}
+		});
+	}
+}
