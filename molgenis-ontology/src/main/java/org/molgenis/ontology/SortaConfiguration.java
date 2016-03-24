7bf7e7868fbf96131ff5e23d2fb53efc6bf4c058
Fleur Kelpin <f.kelpin@umcg.nl>
2016-03-24 18:32:03 +0100

    Make the metadata beans using annotations, not in java config.

--- molgenis-ontology/src/main/java/org/molgenis/ontology/SortaConfiguration.java
+++ molgenis-ontology/src/main/java/org/molgenis/ontology/SortaConfiguration.java
@@ -1,18 +1,10 @@
 package org.molgenis.ontology;
 
 import org.molgenis.data.DataService;
-import org.molgenis.ontology.core.meta.OntologyMetaData;
-import org.molgenis.ontology.core.meta.OntologyTermDynamicAnnotationMetaData;
-import org.molgenis.ontology.core.meta.OntologyTermMetaData;
-import org.molgenis.ontology.core.meta.OntologyTermNodePathMetaData;
-import org.molgenis.ontology.core.meta.OntologyTermSynonymMetaData;
 import org.molgenis.ontology.ic.OntologyTermFrequencyServiceImpl;
-import org.molgenis.ontology.ic.TermFrequencyEntityMetaData;
 import org.molgenis.ontology.ic.TermFrequencyService;
 import org.molgenis.ontology.roc.InformationContentService;
 import org.molgenis.ontology.roc.MatchQualityRocService;
-import org.molgenis.ontology.sorta.meta.MatchingTaskContentEntityMetaData;
-import org.molgenis.ontology.sorta.meta.MatchingTaskEntityMetaData;
 import org.molgenis.ontology.sorta.service.SortaService;
 import org.molgenis.ontology.sorta.service.impl.SortaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,55 +16,6 @@ public class SortaConfiguration
 {
 	@Autowired
 	private DataService dataService;
-
-	// Declaring these EntityMetaData beans makes sure their repositories are created in the default backend.
-	@Bean
-	public OntologyMetaData ontologyMetaData()
-	{
-		return OntologyMetaData.INSTANCE;
-	}
-
-	@Bean
-	public OntologyTermSynonymMetaData ontologyTermSynonymMetaData()
-	{
-		return OntologyTermSynonymMetaData.INSTANCE;
-	}
-
-	@Bean
-	public OntologyTermDynamicAnnotationMetaData ontologyTermDynamicAnnotationMetaData()
-	{
-		return OntologyTermDynamicAnnotationMetaData.INSTANCE;
-	}
-
-	@Bean
-	public OntologyTermNodePathMetaData ontologyTermNodePathMetaData()
-	{
-		return OntologyTermNodePathMetaData.INSTANCE;
-	}
-
-	@Bean
-	public OntologyTermMetaData ontologyTermMetaData()
-	{
-		return OntologyTermMetaData.INSTANCE;
-	}
-
-	@Bean
-	public MatchingTaskEntityMetaData matchingTaskEntityMetaData()
-	{
-		return MatchingTaskEntityMetaData.INSTANCE;
-	}
-
-	@Bean
-	public MatchingTaskContentEntityMetaData matchingTaskContentEntityMetaData()
-	{
-		return MatchingTaskContentEntityMetaData.INSTANCE;
-	}
-
-	@Bean
-	public TermFrequencyEntityMetaData termFrequencyEntityMetaData()
-	{
-		return TermFrequencyEntityMetaData.INSTANCE;
-	}
 
 	@Bean
 	public TermFrequencyService termFrequencyService()
