7b2a8595cb327ebe12323b51f29422fcf61dd14d
Fleur Kelpin <f.kelpin@umcg.nl>
2016-03-30 15:54:45 +0200

    Make delete URL data type HYPERLINK

--- molgenis-ontology/src/main/java/org/molgenis/ontology/sorta/meta/SortaJobExecutionMetaData.java
+++ molgenis-ontology/src/main/java/org/molgenis/ontology/sorta/meta/SortaJobExecutionMetaData.java
@@ -1,7 +1,8 @@
 package org.molgenis.ontology.sorta.meta;
 
+import static org.molgenis.MolgenisFieldTypes.DECIMAL;
+import static org.molgenis.MolgenisFieldTypes.HYPERLINK;
 import static org.molgenis.MolgenisFieldTypes.STRING;
-import static org.molgenis.MolgenisFieldTypes.FieldTypeEnum.DECIMAL;
 import static org.molgenis.ontology.sorta.job.SortaJobExecution.DELETE_URL;
 import static org.molgenis.ontology.sorta.job.SortaJobExecution.ENTITY_NAME;
 import static org.molgenis.ontology.sorta.job.SortaJobExecution.ONTOLOGY_IRI;
@@ -10,7 +11,6 @@ import static org.molgenis.ontology.sorta.job.SortaJobExecution.SOURCE_ENTITY;
 import static org.molgenis.ontology.sorta.job.SortaJobExecution.THRESHOLD;
 
 import org.molgenis.data.jobs.JobExecutionMetaData;
-import org.molgenis.data.support.DefaultAttributeMetaData;
 import org.molgenis.data.support.DefaultEntityMetaData;
 import org.molgenis.ontology.sorta.job.SortaJobExecution;
 import org.springframework.stereotype.Component;
@@ -26,10 +26,8 @@ public class SortaJobExecutionMetaData extends DefaultEntityMetaData
 		setExtends(new JobExecutionMetaData());
 		addAttribute(RESULT_ENTITY).setDataType(STRING).setNillable(false);
 		addAttribute(SOURCE_ENTITY).setDataType(STRING).setNillable(false);
-		// TODO: URL
 		addAttribute(ONTOLOGY_IRI).setDataType(STRING).setNillable(false);
-		// TODO: URL
-		addAttribute(DELETE_URL).setDataType(STRING).setNillable(false);
-		addAttributeMetaData(new DefaultAttributeMetaData(THRESHOLD, DECIMAL).setNillable(false));
+		addAttribute(DELETE_URL).setDataType(HYPERLINK).setNillable(false);
+		addAttribute(THRESHOLD).setDataType(DECIMAL).setNillable(false);
 	}
 }
