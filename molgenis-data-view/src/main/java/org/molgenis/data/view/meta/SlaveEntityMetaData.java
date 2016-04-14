ef5f88553edfbbdd5145d11327bce6bb149678cc
Mark-de-Haan <markdehaan90@gmail.com>
2016-04-14 17:30:17 +0200

    Rename JoinEntity to SlaveEntity

--- molgenis-data-view/src/main/java/org/molgenis/data/view/meta/SlaveEntityMetaData.java
+++ molgenis-data-view/src/main/java/org/molgenis/data/view/meta/SlaveEntityMetaData.java
@@ -7,19 +7,19 @@ import org.molgenis.data.support.DefaultEntityMetaData;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JoinedEntityMetaData extends DefaultEntityMetaData
+public class SlaveEntityMetaData extends DefaultEntityMetaData
 {
-	public static final String ENTITY_NAME = "JoinedEntity";
+	public static final String ENTITY_NAME = "SlaveEntity";
 	public static final String IDENTIFIER = "identifier";
-	public static final String JOIN_ENTITY = "joinEntity";
+	public static final String SLAVE_ENTITY = "slaveEntity";
 	public static final String JOINED_ATTRIBUTES = "joinedAttributes";
 
-	public JoinedEntityMetaData()
+	public SlaveEntityMetaData()
 	{
 		super(ENTITY_NAME);
 				
 		addAttribute(IDENTIFIER, AttributeRole.ROLE_ID).setAuto(true).setNillable(false).setDataType(STRING);
-		addAttribute(JOIN_ENTITY).setNillable(false).setDataType(STRING).isUnique();
+		addAttribute(SLAVE_ENTITY).setNillable(false).setDataType(STRING).isUnique();
 		addAttribute(JOINED_ATTRIBUTES).setNillable(false).setDataType(MREF)
 				.setRefEntity(new JoinedAttributeMetaData());
 	}
