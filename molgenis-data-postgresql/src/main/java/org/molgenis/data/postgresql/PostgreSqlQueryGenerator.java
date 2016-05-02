86ea8efd0ddbfade1989c3050a2f35db79512ad8
Fleur Kelpin <f.kelpin@umcg.nl>
2016-05-02 16:09:23 +0200

    Fix #4596, PostgreSQL version.

--- molgenis-data-postgresql/src/main/java/org/molgenis/data/postgresql/PostgreSqlQueryGenerator.java
+++ molgenis-data-postgresql/src/main/java/org/molgenis/data/postgresql/PostgreSqlQueryGenerator.java
@@ -191,9 +191,10 @@ class PostgreSqlQueryGenerator
 
 	public static <E extends Entity> String getSqlSelect(EntityMetaData entityMeta, Query<E> q, List<Object> parameters)
 	{
-		StringBuilder select = new StringBuilder("SELECT ");
-		StringBuilder group = new StringBuilder();
-		AtomicInteger count = new AtomicInteger();
+		final StringBuilder select = new StringBuilder("SELECT ");
+		final StringBuilder group = new StringBuilder();
+		final AtomicInteger count = new AtomicInteger();
+		final AttributeMetaData idAttribute = entityMeta.getIdAttribute();
 		getPersistedAttributes(entityMeta).forEach(attr -> {
 			if (q.getFetch() == null || q.getFetch().hasField(attr.getName()))
 			{
@@ -206,8 +207,12 @@ class PostgreSqlQueryGenerator
 				{
 					// TODO retrieve mref values in seperate queries to allow specifying limit and offset after nested
 					// MOLGENIS queries are implemented as sub-queries instead of query rules
-					select.append(MessageFormat.format("array_agg(distinct array[{0}.{1}::text,{0}.{0}::text]) AS {0}",
-							getColumnName(attr), getColumnName(JUNCTION_TABLE_ORDER_ATTR_NAME)));
+					String mrefSelect = MessageFormat
+							.format("(SELECT array_agg(DISTINCT ARRAY[{0}.{1}::TEXT,{0}.{0}::TEXT]) "
+											+ "FROM {2} AS {0} WHERE this.{3} = {0}.{3}) AS {0}", getColumnName(attr),
+									getColumnName(JUNCTION_TABLE_ORDER_ATTR_NAME),
+									getJunctionTableName(entityMeta, attr), getColumnName(idAttribute));
+					select.append(mrefSelect);
 				}
 				else
 				{
@@ -232,11 +237,6 @@ class PostgreSqlQueryGenerator
 		if (where.length() > 0)
 		{
 			result.append(" WHERE ").append(where);
-		}
-		// group by
-		if (select.indexOf("array_agg") != -1 && group.length() > 0)
-		{
-			result.append(" GROUP BY ").append(group);
 		}
 		// order by
 		result.append(' ').append(getSqlSort(entityMeta, q));
@@ -593,16 +593,6 @@ class PostgreSqlQueryGenerator
 		StringBuilder from = new StringBuilder().append(" FROM ").append(getTableName(entityMeta)).append(" AS this");
 
 		AttributeMetaData idAttribute = entityMeta.getIdAttribute();
-
-		getPersistedAttributesMref(entityMeta).forEach(mrefAttr -> {
-			if (q.getFetch() == null || q.getFetch().hasField(mrefAttr.getName()))
-			{
-				from.append(" LEFT JOIN ").append(getJunctionTableName(entityMeta, mrefAttr)).append(" AS ")
-						.append(getColumnName(mrefAttr)).append(" ON (this.").append(getColumnName(idAttribute))
-						.append(" = ").append(getColumnName(mrefAttr)).append('.').append(getColumnName(idAttribute))
-						.append(')');
-			}
-		});
 
 		List<AttributeMetaData> mrefAttrsInQuery = getMrefQueryAttrs(entityMeta, q);
 		for (int i = 0; i < mrefAttrsInQuery.size(); i++)
