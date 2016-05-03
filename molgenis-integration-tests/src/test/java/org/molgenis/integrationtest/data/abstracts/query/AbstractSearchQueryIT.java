ed9d148a7ed02c1a320f0ee3a23e9009b3110a3a
Mark-de-Haan <markdehaan90@gmail.com>
2016-05-03 15:19:53 +0200

    Added some comments for failing dateTime test

--- molgenis-integration-tests/src/test/java/org/molgenis/integrationtest/data/abstracts/query/AbstractSearchQueryIT.java
+++ molgenis-integration-tests/src/test/java/org/molgenis/integrationtest/data/abstracts/query/AbstractSearchQueryIT.java
@@ -1,16 +1,23 @@
 package org.molgenis.integrationtest.data.abstracts.query;
 
 import static java.util.stream.Collectors.toSet;
+import static org.elasticsearch.common.collect.Sets.*;
+import static org.molgenis.util.MolgenisDateFormat.getDateFormat;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import java.text.ParseException;
 import java.util.Set;
+import java.util.TimeZone;
 
 import org.elasticsearch.common.collect.Sets;
 import org.molgenis.data.Entity;
+import org.molgenis.data.MolgenisQueryException;
 import org.molgenis.data.Query;
+import org.molgenis.data.elasticsearch.request.QueryGenerator;
 import org.molgenis.data.support.QueryImpl;
+import org.molgenis.util.MolgenisDateFormat;
 
 public class AbstractSearchQueryIT extends AbstractQueryIT
 {
@@ -19,7 +26,7 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	protected void testInt()
 	{
 		Query<Entity> query = new QueryImpl<>().search(HEIGHT, String.valueOf(165));
-		Set<Entity> resultSet = Sets.newHashSet(person2);
+		Set<Entity> resultSet = newHashSet(person2);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
@@ -29,7 +36,7 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	protected void testDecimal()
 	{
 		Query<Entity> query = new QueryImpl<>().search(ACCOUNT_BALANCE, "1000 ");
-		Set<Entity> resultSet = Sets.newHashSet(person3);
+		Set<Entity> resultSet = newHashSet(person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
@@ -39,7 +46,7 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	protected void testLong()
 	{
 		Query<Entity> query = new QueryImpl<>().search(SERIAL_NUMBER, "23471900909");
-		Set<Entity> resultSet = Sets.newHashSet(person3);
+		Set<Entity> resultSet = newHashSet(person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
@@ -49,7 +56,7 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	protected void testString()
 	{
 		Query<Entity> query = new QueryImpl<>().search(QUOTE, "is bit computer");
-		Set<Entity> resultSet = Sets.newHashSet(person1, person2, person3);
+		Set<Entity> resultSet = newHashSet(person1, person2, person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
@@ -59,7 +66,7 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	protected void testDate() throws ParseException
 	{
 		Query<Entity> query = new QueryImpl<>().search(BIRTHDAY, "1950-01-31");
-		Set<Entity> resultSet = Sets.newHashSet(person3);
+		Set<Entity> resultSet = newHashSet(person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
@@ -68,12 +75,15 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	@Override
 	protected void testDateTime() throws ParseException
 	{
-		// FIXME
-		// Query<Entity> query = new QueryImpl<>().search(BIRTH_TIME, "1976-06-07 07:07:07");
-		// Set<Entity> resultSet = Sets.newHashSet(person2);
-		// assertTrue(resultSet.contains(personsRepository.findOne(query)));
-		// assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
-		// assertEquals(personsRepository.count(query), resultSet.size());
+		// FIXME Value generated just like the ElasticSearch QueryGeneratorTest.generateOneQueryRuleSearchOneFieldDateTime
+		// FIXME but still does not work
+//		String value = getDateFormat().parse("1976-06-07T07:07:07+0100").toString();
+//
+//	 	Query<Entity> query = new QueryImpl<>().search(BIRTH_TIME, value);
+//	 	Set<Entity> resultSet = Sets.newHashSet(person2);
+// 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
+//	 	assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
+//	 	assertEquals(personsRepository.count(query), resultSet.size());
 	}
 
 	@Override
@@ -119,14 +129,14 @@ public class AbstractSearchQueryIT extends AbstractQueryIT
 	{
 		Query<Entity> query = new QueryImpl<>().search("boolean");
 		System.out.println(personsRepository.findOne(query));
-		Set<Entity> resultSet = Sets.newHashSet(person3);
+		Set<Entity> resultSet = newHashSet(person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
 
 		query = new QueryImpl<>().search("1000");
 		System.out.println(personsRepository.findOne(query));
-		resultSet = Sets.newHashSet(person3);
+		resultSet = newHashSet(person3);
 		assertTrue(resultSet.contains(personsRepository.findOne(query)));
 		assertEquals(personsRepository.findAll(query).collect(toSet()), resultSet);
 		assertEquals(personsRepository.count(query), resultSet.size());
