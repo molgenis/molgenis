aec39b7b1aaa0f14ca5e4dafe5bdce0edd5303b6
jjettenn <jjettenn@gmail.com>
2016-05-03 12:27:10 +0200

    Fix log.info lines

--- molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/transaction/RebuildPartialIndex.java
+++ molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/transaction/RebuildPartialIndex.java
@@ -14,9 +14,11 @@ import org.molgenis.data.QueryRule.Operator;
 import org.molgenis.data.Sort;
 import org.molgenis.data.elasticsearch.ElasticsearchService.IndexingMode;
 import org.molgenis.data.elasticsearch.SearchService;
+import org.molgenis.data.elasticsearch.reindex.ReindexActionJobMetaData;
 import org.molgenis.data.elasticsearch.reindex.ReindexActionMetaData;
 import org.molgenis.data.elasticsearch.reindex.ReindexActionMetaData.CudType;
 import org.molgenis.data.elasticsearch.reindex.ReindexActionMetaData.DataType;
+import org.molgenis.data.elasticsearch.reindex.ReindexActionMetaData.ReindexStatus;
 import org.molgenis.data.support.QueryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,9 +44,17 @@ public class RebuildPartialIndex implements Runnable
 	public void run()
 	{
 		runAsSystem(() -> {
-			LOG.info("--- Start rebuilding index: [{}] group id: [{}]", new Date(), transactionId);
-			rebuildIndex();
-			LOG.info("--- End rebuilding index: [{}] group id: [{}]", new Date(), transactionId);
+			Entity entity = this.dataService.findOneById(ReindexActionJobMetaData.ENTITY_NAME, transactionId);
+			if (null != entity)
+			{
+				LOG.info("######## START Reindex transaction id: [{}] date: [{}] ########", transactionId, new Date());
+				rebuildIndex();
+				LOG.info("######## END Reindex transaction id: [{}] date: [{}] ########", transactionId, new Date());
+			}
+			else
+			{
+				LOG.info("No reindex action job found for transaction id: [{}]", transactionId);
+			}
 		});
 	}
 
@@ -59,56 +69,63 @@ public class RebuildPartialIndex implements Runnable
 			final String entityId = e.getString(ReindexActionMetaData.ENTITY_ID);
 			final DataType dataType = DataType.valueOf(e.getString(ReindexActionMetaData.DATA_TYPE));
 
+			this.updateActionStatus(e, ReindexStatus.STARTED);
 			if (entityId != null)
 			{
 				this.rebuildIndexOneEntity(entityFullName, entityId, cudType);
 			}
 			else if (dataType.equals(DataType.DATA))
 			{
-				this.rebuildIndexBatchEntities(entityFullName, entityMetaData);
+				this.rebuildIndexBatchEntities(entityFullName, entityMetaData, cudType);
 			}
 			else
 			{
 				this.rebuildIndexEntityMeta(entityFullName, entityMetaData, cudType);
 			}
+			this.updateActionStatus(e, ReindexStatus.FINISHED);
+
 		});
 
 		this.searchService.refreshIndex();
 	}
 
+	private void updateActionStatus(Entity e, ReindexStatus status)
+	{
+		e.set(ReindexActionMetaData.REINDEX_STATUS, status);
+		dataService.update(ReindexActionMetaData.ENTITY_NAME, e);
+	}
+
 	private void rebuildIndexOneEntity(String entityFullName, String entityId, CudType cudType)
 	{
+		LOG.info("# Reindex row id [{}] entity: [{}] cud: [{}]", entityId, entityFullName, cudType);
 		switch (cudType)
 		{
 			case ADD:
 				Entity entityA = dataService.findOneById(entityFullName, entityId);
 				this.searchService.index(entityA, entityA.getEntityMetaData(), IndexingMode.ADD);
-				LOG.info("Reindex row id [{}] entity full name: [{}] cud: [{}]", entityId, entityFullName, CudType.ADD);
 				break;
 			case UPDATE:
 				Entity entityU = dataService.findOneById(entityFullName, entityId);
 				this.searchService.index(entityU, entityU.getEntityMetaData(), IndexingMode.UPDATE);
-				LOG.info("Reindex row id [{}] entity full name: [{}] cud: [{}]", entityId, entityFullName,
-						CudType.UPDATE);
 				break;
 			case DELETE:
 				this.searchService.deleteById(entityId, dataService.getMeta().getEntityMetaData(entityFullName));
-				LOG.info("Reindex row id [{}] entity full name: [{}] cud: [{}]", entityId, entityFullName,
-						CudType.DELETE);
 				break;
 			default:
 				break;
 		}
 	}
 	
-	private void rebuildIndexBatchEntities(String entityFullName, EntityMetaData entityMetaData)
+	private void rebuildIndexBatchEntities(String entityFullName, EntityMetaData entityMetaData, CudType cudType)
 	{
+		LOG.info("# Reindex batch entities of entity: [{}] cud: [{}]", entityFullName, cudType);
 		this.searchService.rebuildIndex(dataService.getRepository(entityFullName), entityMetaData);
 	}
 
 	private void rebuildIndexEntityMeta(String entityFullName, EntityMetaData entityMetaData,
 			CudType cudType)
 	{
+		LOG.info("# Reindex data and metadata rebuild whole index of entity: [{}] cud: [{}]", entityFullName, cudType);
 		switch (cudType)
 		{
 			case UPDATE:
@@ -117,7 +134,6 @@ public class RebuildPartialIndex implements Runnable
 				break;
 			case DELETE:
 				this.searchService.delete(entityFullName);
-				break;
 			default:
 				break;
 		}
