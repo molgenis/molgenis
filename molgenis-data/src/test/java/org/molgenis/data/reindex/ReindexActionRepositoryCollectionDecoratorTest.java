fbda4f70bcf5ca64a3d940fb6dfcece91df24dfc
jjettenn <jjettenn@gmail.com>
2016-05-12 16:14:16 +0200

    Fix test

--- molgenis-data/src/test/java/org/molgenis/data/reindex/ReindexActionRepositoryCollectionDecoratorTest.java
+++ molgenis-data/src/test/java/org/molgenis/data/reindex/ReindexActionRepositoryCollectionDecoratorTest.java
@@ -31,6 +31,7 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 		Repository<Entity> repository = (Repository<Entity>) mock(Repository.class);
 		when(decoratedRepositoryCollection.getRepository(REPOSITORY_NAME)).thenReturn(repository);
 		entityMeta = mock(EntityMetaData.class);
+		when(entityMeta.getName()).thenReturn("repo");
 		when(repository.getEntityMetaData()).thenReturn(entityMeta);
 		reindexActionRegisterService = mock(ReindexActionRegisterService.class);
 		reindexActionRepositoryCollectionDecorator = new ReindexActionRepositoryCollectionDecorator(
@@ -43,7 +44,7 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 	{
 		reindexActionRepositoryCollectionDecorator.deleteEntityMeta(REPOSITORY_NAME);
 		verify(decoratedRepositoryCollection, times(1)).deleteEntityMeta(REPOSITORY_NAME);
-		verify(reindexActionRegisterService).register(entityMeta.getName(), CudType.DELETE, DataType.METADATA, null);
+		verify(reindexActionRegisterService).register("repo", CudType.DELETE, DataType.METADATA, null);
 	}
 
 	@Test
@@ -52,7 +53,7 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 		DefaultAttributeMetaData attribute = new DefaultAttributeMetaData("attribute");
 		reindexActionRepositoryCollectionDecorator.addAttribute(REPOSITORY_NAME, attribute);
 		verify(decoratedRepositoryCollection, times(1)).addAttribute(REPOSITORY_NAME, attribute);
-		verify(reindexActionRegisterService).register(entityMeta.getName(), CudType.UPDATE, DataType.METADATA, null);
+		verify(reindexActionRegisterService).register("repo", CudType.UPDATE, DataType.METADATA, null);
 	}
 
 	@Test
@@ -60,7 +61,7 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 	{
 		reindexActionRepositoryCollectionDecorator.deleteAttribute(REPOSITORY_NAME, "attribute");
 		verify(decoratedRepositoryCollection, times(1)).deleteAttribute(REPOSITORY_NAME, "attribute");
-		verify(reindexActionRegisterService).register(entityMeta.getName(), CudType.UPDATE, DataType.METADATA, null);
+		verify(reindexActionRegisterService).register("repo", CudType.UPDATE, DataType.METADATA, null);
 	}
 
 	@Test
@@ -68,8 +69,8 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 	{
 		DefaultAttributeMetaData attribute = mock(DefaultAttributeMetaData.class);
 		reindexActionRepositoryCollectionDecorator.addAttributeSync(REPOSITORY_NAME, attribute);
-		verify(decoratedRepositoryCollection, times(1)).addAttribute(REPOSITORY_NAME, attribute);
-		verify(reindexActionRegisterService).register(entityMeta.getName(), CudType.UPDATE, DataType.METADATA, null);
+		verify(decoratedRepositoryCollection, times(1)).addAttributeSync(REPOSITORY_NAME, attribute);
+		verify(reindexActionRegisterService).register("repo", CudType.UPDATE, DataType.METADATA, null);
 	}
 
 	@Test
@@ -77,6 +78,6 @@ public class ReindexActionRepositoryCollectionDecoratorTest
 	{
 		reindexActionRepositoryCollectionDecorator.addEntityMeta(entityMeta);
 		verify(decoratedRepositoryCollection, times(1)).addEntityMeta(entityMeta);
-		verify(reindexActionRegisterService).register(entityMeta.getName(), CudType.CREATE, DataType.METADATA, null);
+		verify(reindexActionRegisterService).register("repo", CudType.CREATE, DataType.METADATA, null);
 	}
 }
