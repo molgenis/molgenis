0295f9a580f50b5614a19f9c17bbb14b5f06354e
bartcharbon <bart.charbon@gmail.com>
2016-05-25 15:59:53 +0200

    remove test code

--- molgenis-app/src/main/java/org/molgenis/app/WebAppConfig.java
+++ molgenis-app/src/main/java/org/molgenis/app/WebAppConfig.java
@@ -104,7 +104,6 @@ public class WebAppConfig extends MolgenisWebAppConfig
 		upgradeService.addUpgrade(new Step30MigrateJobExecutionUser(dataSource));
 		upgradeService.addUpgrade(new Step31UpdateApplicationSettings(dataSource, idGenerator));
 		Step32AddRowLevelSecurityMetadata step32 = new Step32AddRowLevelSecurityMetadata(dataSource, idGenerator);
-		step32.setEntitiesToSecure(Arrays.asList("MolgenisUser"));
 		upgradeService.addUpgrade(step32);
 	}
 
