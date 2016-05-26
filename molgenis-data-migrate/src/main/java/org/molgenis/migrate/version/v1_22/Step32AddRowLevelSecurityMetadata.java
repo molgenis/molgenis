5e1d80624fd0f9e4e6435d1988eef8c7d771fe21
bartcharbon <bart.charbon@gmail.com>
2016-05-26 10:20:41 +0200

    clean up migration code

--- molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_22/Step32AddRowLevelSecurityMetadata.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_22/Step32AddRowLevelSecurityMetadata.java
@@ -1,7 +1,5 @@
 package org.molgenis.migrate.version.v1_22;
 
-import org.molgenis.MolgenisFieldTypes;
-import org.molgenis.auth.MolgenisUser;
 import org.molgenis.auth.MolgenisUserMetaData;
 import org.molgenis.data.IdGenerator;
 import org.molgenis.framework.MolgenisUpgrade;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -19,6 +16,7 @@ import static java.util.Objects.requireNonNull;
 
 public class Step32AddRowLevelSecurityMetadata extends MolgenisUpgrade
 {
+	public static final String UPDATE = "_UPDATE";
 	private final Logger LOG = LoggerFactory.getLogger(Step32AddRowLevelSecurityMetadata.class);
 
 	private final JdbcTemplate jdbcTemplate;
@@ -40,35 +38,34 @@ public class Step32AddRowLevelSecurityMetadata extends MolgenisUpgrade
 		LOG.info("Updating entities table ...");
 
 		// update existing settings table
-		jdbcTemplate.execute("ALTER TABLE entities ADD COLUMN `rowLevelSecured` boolean");
+		// jdbcTemplate.execute("ALTER TABLE entities ADD COLUMN `rowLevelSecured` boolean");
 
 		LOG.debug("Updated application settings");
 
 		LOG.info("Updating entities to secure with row level security...");
-		for(String fullname : entitiesToSecure){
-			LOG.info("Updating [%s] with row level security...",fullname);
+		for (String fullname : entitiesToSecure)
+		{
+			LOG.info("Updating [{}] with row level security...", fullname);
 			String rowLevelSecurityId = idGenerator.generateId();
-			//FIXME: constant
 			jdbcTemplate.update(
 					"INSERT INTO attributes (`identifier`,`name`,`dataType`,`refEntity`,`expression`,`nillable`,`auto`,`visible`,`label`,`description`,`aggregateable`,`enumOptions`,`rangeMin`,`rangeMax`,`readOnly`,`unique`,`visibleExpression`,`validationExpression`,`defaultValue`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-					rowLevelSecurityId, "_UPDATE", "xref", new MolgenisUserMetaData().getName(), null, false, false, true,
-					"name",
-					"desc",
-					false, null, null, null, false, false, null, null, "");
+					rowLevelSecurityId, UPDATE, "xref", new MolgenisUserMetaData().getName(), null, false, false, true,
+					"name", "desc", false, null, null, null, false, false, null, null, "");
 
-			jdbcTemplate.update("INSERT INTO entities_attributes (`order`, `fullName`, `attributes`) VALUES (?, ?, ?)", 0,
-					fullname, rowLevelSecurityId);
+			jdbcTemplate.update("INSERT INTO entities_attributes (`order`, `fullName`, `attributes`) VALUES (?, ?, ?)",
+					0, fullname, rowLevelSecurityId);
 
 			// update existing settings table
 			jdbcTemplate.execute("ALTER TABLE " + fullname + " ADD COLUMN `_UPDATE` varchar(255)");
-			jdbcTemplate.execute(
-					"ALTER TABLE " + fullname + " ADD CONSTRAINT `" + fullname +"`_UPDATE` FOREIGN KEY (`_UPDATE`) REFERENCES `MolgenisUser` (`ID`)");
+			jdbcTemplate.execute("ALTER TABLE " + fullname + " ADD CONSTRAINT `" + fullname + UPDATE + " FOREIGN KEY (`"
+					+ UPDATE + "`) REFERENCES `MolgenisUser` (`ID`)");
 
 		}
 		LOG.info("Updated entities to secure with row level security...");
 	}
 
-	public void setEntitiesToSecure(List<String> entitiesToSecure) {
+	public void setEntitiesToSecure(List<String> entitiesToSecure)
+	{
 		this.entitiesToSecure = entitiesToSecure;
 	}
 }
