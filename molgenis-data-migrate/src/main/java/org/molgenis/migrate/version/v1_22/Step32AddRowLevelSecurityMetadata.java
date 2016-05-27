72fecd7028a6a03abc7c77fd2cc11181d2f9fd3e
bartcharbon <bart.charbon@gmail.com>
2016-05-27 16:11:43 +0200

    change xref to mref

--- molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_22/Step32AddRowLevelSecurityMetadata.java
+++ molgenis-data-migrate/src/main/java/org/molgenis/migrate/version/v1_22/Step32AddRowLevelSecurityMetadata.java
@@ -1,7 +1,9 @@
 package org.molgenis.migrate.version.v1_22;
 
 import org.molgenis.auth.MolgenisUserMetaData;
+import org.molgenis.data.AttributeMetaData;
 import org.molgenis.data.IdGenerator;
+import org.molgenis.fieldtypes.StringField;
 import org.molgenis.framework.MolgenisUpgrade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,12 +19,15 @@ import static java.util.Objects.requireNonNull;
 public class Step32AddRowLevelSecurityMetadata extends MolgenisUpgrade
 {
 	public static final String UPDATE = "_UPDATE";
+	public static final MolgenisUserMetaData MOLGENIS_USER_META_DATA = new MolgenisUserMetaData();
 	private final Logger LOG = LoggerFactory.getLogger(Step32AddRowLevelSecurityMetadata.class);
 
 	private final JdbcTemplate jdbcTemplate;
 	private final IdGenerator idGenerator;
 
 	private List<String> entitiesToSecure = Collections.EMPTY_LIST;
+
+	private static final String VARCHAR = "VARCHAR(255)";
 
 	@Autowired
 	public Step32AddRowLevelSecurityMetadata(DataSource dataSource, IdGenerator idGenerator)
@@ -45,20 +50,49 @@ public class Step32AddRowLevelSecurityMetadata extends MolgenisUpgrade
 		LOG.info("Updating entities to secure with row level security...");
 		for (String fullname : entitiesToSecure)
 		{
-			LOG.info("Updating [{}] with row level security...", fullname);
-			String rowLevelSecurityId = idGenerator.generateId();
-			jdbcTemplate.update(
-					"INSERT INTO attributes (`identifier`,`name`,`dataType`,`refEntity`,`expression`,`nillable`,`auto`,`visible`,`label`,`description`,`aggregateable`,`enumOptions`,`rangeMin`,`rangeMax`,`readOnly`,`unique`,`visibleExpression`,`validationExpression`,`defaultValue`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-					rowLevelSecurityId, UPDATE, "xref", new MolgenisUserMetaData().getName(), null, true, false, true,
-					"name", "desc", false, null, null, null, false, false, null, null, "");
+			try
+			{
+				LOG.info("Updating [{}] with row level security...", fullname);
+				String rowLevelSecurityId = idGenerator.generateId();
+				jdbcTemplate.update(
+						"INSERT INTO attributes (`identifier`,`name`,`dataType`,`refEntity`,`expression`,`nillable`,`auto`,`visible`,`label`,`description`,`aggregateable`,`enumOptions`,`rangeMin`,`rangeMax`,`readOnly`,`unique`,`visibleExpression`,`validationExpression`,`defaultValue`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+						rowLevelSecurityId, UPDATE, "mref", new MolgenisUserMetaData().getName(), null, true, false,
+						true, "name", "desc", false, null, null, null, false, false, null, null, "");
 
-			jdbcTemplate.update("INSERT INTO entities_attributes (`order`, `fullName`, `attributes`) VALUES (?, ?, ?)",
-					0, fullname, rowLevelSecurityId);
+				jdbcTemplate.update(
+						"INSERT INTO entities_attributes (`order`, `fullName`, `attributes`) VALUES (?, ?, ?)", 0,
+						fullname, rowLevelSecurityId);
 
-			// update existing settings table
-			jdbcTemplate.execute("ALTER TABLE " + fullname + " ADD COLUMN `_UPDATE` varchar(255)");
-			jdbcTemplate.execute("ALTER TABLE " + fullname + " ADD CONSTRAINT `" + fullname + UPDATE + " FOREIGN KEY (`"
-					+ UPDATE + "`) REFERENCES `MolgenisUser` (`ID`)");
+				String foreignKeyConstraint = fullname + "_UPDATE";
+
+				AttributeMetaData idAttribute = null;// get idattribute from attributes table
+				StringBuilder sql = new StringBuilder();
+
+				// mysql keys cannot have TEXT value, so change it to VARCHAR when needed
+				String idAttrMysqlType = (idAttribute.getDataType() instanceof StringField ? VARCHAR
+						: idAttribute.getDataType().getMysqlType());
+
+				String refAttrMysqlType = (MOLGENIS_USER_META_DATA.getIdAttribute().getDataType() instanceof StringField
+						? VARCHAR : MOLGENIS_USER_META_DATA.getIdAttribute().getDataType().getMysqlType());
+
+				sql.append(" CREATE TABLE ").append('`').append(fullname).append('_').append(UPDATE).append('`')
+						.append("(`order` INT,`").append(idAttribute.getName()).append('`').append(' ')
+						.append(idAttrMysqlType).append(" NOT NULL, ").append('`').append(UPDATE).append('`')
+						.append(' ').append(refAttrMysqlType).append(" NOT NULL, FOREIGN KEY (").append('`')
+						.append(idAttribute.getName()).append('`').append(") REFERENCES ").append('`').append(fullname)
+						.append('`').append('(').append('`').append(idAttribute.getName())
+						.append("`) ON DELETE CASCADE");
+
+				sql.append(", FOREIGN KEY (").append('`').append(UPDATE).append('`').append(") REFERENCES ").append('`')
+						.append(MOLGENIS_USER_META_DATA.getName()).append('`').append('(').append('`').append("ID")
+						.append("`) ON DELETE CASCADE");
+
+				sql.append(") ENGINE=InnoDB;");
+			}
+			catch (Exception e)
+			{
+				LOG.error("", e);
+			}
 
 		}
 		LOG.info("Updated entities to secure with row level security...");
