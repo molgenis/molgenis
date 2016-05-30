a2d0e462fb2d6ce3d526d1f01a5584463e04c59b
Mark-de-Haan <markdehaan90@gmail.com>
2016-05-30 14:22:13 +0200

    Permission check upfront

--- molgenis-data/src/main/java/org/molgenis/data/RowLevelSecurityPermissionValidator.java
+++ molgenis-data/src/main/java/org/molgenis/data/RowLevelSecurityPermissionValidator.java
@@ -1,14 +1,15 @@
 package org.molgenis.data;
 
+import org.molgenis.security.core.Permission;
+import org.molgenis.security.core.runas.RunAsSystemProxy;
+
 import static autovalue.shaded.com.google.common.common.collect.Iterables.isEmpty;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.auth.MolgenisUser.USERNAME;
 import static org.molgenis.data.RowLevelSecurityRepositoryDecorator.UPDATE_ATTRIBUTE;
 import static org.molgenis.security.core.utils.SecurityUtils.currentUserIsSu;
-
-import org.molgenis.security.core.Permission;
-import org.molgenis.security.core.runas.RunAsSystemProxy;
-import org.molgenis.security.core.utils.SecurityUtils;
+import static org.molgenis.security.core.utils.SecurityUtils.getCurrentUsername;
+import static org.molgenis.security.core.utils.SecurityUtils.getEntityAuthorities;
 
 public class RowLevelSecurityPermissionValidator
 {
@@ -21,7 +22,7 @@ public class RowLevelSecurityPermissionValidator
 
 	public boolean validatePermission(Entity entity, Permission permission)
 	{
-		if (!hasPermission(entity, permission))
+		if (!userHasUpdatePermissionOnEntity(entity, permission))
 		{
 			throw new MolgenisDataAccessException(
 					"No " + permission.toString() + " permission on entity with id " + entity.getIdValue());
@@ -29,13 +30,16 @@ public class RowLevelSecurityPermissionValidator
 		return true;
 	}
 
-	public boolean hasPermission(Entity entity, Permission permission)
+	public boolean userHasUpdatePermissionOnEntity(Entity entity, Permission permission)
 	{
 		if (currentUserIsSu()) return true;
 
-		String currentUsername = SecurityUtils.getCurrentUsername();
+		String entityName = entity.getEntityMetaData().getName();
+		if(!getEntityAuthorities(entityName).contains(Permission.UPDATE)) return false;
+
+		String currentUsername = getCurrentUsername();
 		return RunAsSystemProxy.runAsSystem(() -> {
-			Iterable<Entity> users = dataService.findOne(entity.getEntityMetaData().getName(), entity.getIdValue())
+			Iterable<Entity> users = dataService.findOne(entityName, entity.getIdValue())
 					.getEntities(UPDATE_ATTRIBUTE);
 
 			if (users != null || !isEmpty(users))
