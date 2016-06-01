695eb4e9239b991d86ed376c0b6283c62528955d
Mark-de-Haan <markdehaan90@gmail.com>
2016-06-01 14:10:32 +0200

    Check if row level secured

--- molgenis-data/src/main/java/org/molgenis/data/RowLevelSecurityRepositoryDecorator.java
+++ molgenis-data/src/main/java/org/molgenis/data/RowLevelSecurityRepositoryDecorator.java
@@ -77,23 +77,28 @@ public class RowLevelSecurityRepositoryDecorator implements Repository
 	@Override
 	public Iterator<Entity> iterator()
 	{
-		final Iterator<Entity> iterator = decoratedRepository.iterator();
-		return new Iterator<Entity>()
+		if(isRowLevelSecured())
 		{
-			@Override
-			public boolean hasNext()
+			final Iterator<Entity> iterator = decoratedRepository.iterator();
+			return new Iterator<Entity>()
 			{
-				return iterator.hasNext();
-			}
+				@Override
+				public boolean hasNext()
+				{
+					return iterator.hasNext();
+				}
 
-			@Override
-			public Entity next()
-			{
-				Entity entity = iterator.next();
-				injectPermissions(entity);
-				return entity;
-			}
-		};
+				@Override
+				public Entity next()
+				{
+					return injectPermissions(iterator.next());
+				}
+			};
+		}
+		else
+		{
+			return decoratedRepository.iterator();
+		}
 	}
 
 	@Override
