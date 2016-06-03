3029e79beaf2dfd93cdab051a577d05f5d0c8b82
dennishendriksen <d.hendriksen@umcg.nl>
2016-06-03 13:43:06 +0200

    Fix: add meta data reindexing decorator

--- molgenis-data/src/main/java/org/molgenis/data/RepositoryCollectionDecoratorFactory.java
+++ molgenis-data/src/main/java/org/molgenis/data/RepositoryCollectionDecoratorFactory.java
@@ -1,8 +1,9 @@
 package org.molgenis.data;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.Spliterators.spliteratorUnknownSize;
 
+import org.molgenis.data.reindex.ReindexActionRegisterService;
+import org.molgenis.data.reindex.ReindexActionRepositoryCollectionDecorator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +14,23 @@ import org.springframework.stereotype.Component;
 public class RepositoryCollectionDecoratorFactory
 {
 	private final RepositoryDecoratorFactory repositoryDecoratorFactory;
+	private final ReindexActionRegisterService reindexActionRegisterService;
 
 	@Autowired
-	public RepositoryCollectionDecoratorFactory(RepositoryDecoratorFactory repositoryDecoratorFactory)
+	public RepositoryCollectionDecoratorFactory(RepositoryDecoratorFactory repositoryDecoratorFactory,
+			ReindexActionRegisterService reindexActionRegisterService)
 	{
 		this.repositoryDecoratorFactory = requireNonNull(repositoryDecoratorFactory);
+		this.reindexActionRegisterService = requireNonNull(reindexActionRegisterService);
 	}
 
 	public RepositoryCollection createDecoratedRepositoryCollection(RepositoryCollection repositoryCollection)
 	{
-		return new RepositoryCollectionDecorator(repositoryCollection, repositoryDecoratorFactory);
+		RepositoryCollection repoCollectionDecorator = new RepositoryCollectionDecorator(repositoryCollection,
+				repositoryDecoratorFactory);
+		repoCollectionDecorator = new ReindexActionRepositoryCollectionDecorator(repoCollectionDecorator,
+				reindexActionRegisterService);
+		return repoCollectionDecorator;
 	}
 
 }
