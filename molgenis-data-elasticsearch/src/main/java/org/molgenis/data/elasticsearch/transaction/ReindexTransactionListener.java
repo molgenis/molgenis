44e8dc32dde536f2819b5e056f9097cc1a9bda2b
Fleur Kelpin <f.kelpin@umcg.nl>
2016-06-09 10:34:17 +0200

    Try/catch around the rebuildIndex call

--- molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/transaction/ReindexTransactionListener.java
+++ molgenis-data-elasticsearch/src/main/java/org/molgenis/data/elasticsearch/transaction/ReindexTransactionListener.java
@@ -1,20 +1,37 @@
 package org.molgenis.data.elasticsearch.transaction;
 
+import static java.util.Objects.requireNonNull;
+
 import org.molgenis.data.elasticsearch.reindex.job.ReindexService;
+import org.molgenis.data.reindex.ReindexActionRegisterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ReindexTransactionListener extends DefaultMolgenisTransactionListener
 {
 	private ReindexService rebuildIndexService;
+	private ReindexActionRegisterService reindexActionRegisterService;
 
-	public ReindexTransactionListener(ReindexService rebuildIndexService)
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultMolgenisTransactionListener.class);
+
+	public ReindexTransactionListener(ReindexService rebuildIndexService,
+			ReindexActionRegisterService reindexActionRegisterService)
 	{
-		this.rebuildIndexService = rebuildIndexService;
+		this.rebuildIndexService = requireNonNull(rebuildIndexService);
+		this.reindexActionRegisterService = requireNonNull(reindexActionRegisterService);
 	}
 
 	@Override
 	public void doCleanupAfterCompletion(String transactionId)
 	{
-		// if the transaction was rolled back, so has the insert of the ReindexJob
-		rebuildIndexService.rebuildIndex(transactionId);
+		try
+		{
+			rebuildIndexService.rebuildIndex(transactionId);
+		}
+		catch (Exception ex)
+		{
+			LOG.error("Error during cleanupAfterCompletion");
+		}
+
 	}
 }
