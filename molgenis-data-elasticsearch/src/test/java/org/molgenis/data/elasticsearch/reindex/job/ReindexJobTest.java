90793b268442a2b303ba3111d794cc5ae4a9ce3e
Fleur Kelpin <f.kelpin@umcg.nl>
2016-06-17 00:05:54 +0200

    Fix test.

--- molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/reindex/job/ReindexJobTest.java
+++ molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/reindex/job/ReindexJobTest.java
@@ -210,9 +210,7 @@ public class ReindexJobTest
 		reindexJob.call(this.progress);
 		assertEquals(reindexAction.get(REINDEX_STATUS), FINISHED.name());
 
-		//TODO: verify that proper rows were fetched
-//		verify(this.searchService)
-//				.rebuildIndex(this.dataService.getRepository("any"), new DefaultEntityMetaData("test"));
+		verify(this.searchService).rebuildIndex(this.dataService.getRepository("any"));
 
 		verify(progress).status("######## START Reindex transaction id: [aabbcc] ########");
 		verify(progress).setProgressMax(1);
