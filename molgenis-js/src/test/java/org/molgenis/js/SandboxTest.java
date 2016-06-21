3e1098684ce6fd764bfa2f7cd899fa5f48fca9e8
dennishendriksen <d.hendriksen@umcg.nl>
2016-06-21 21:54:29 +0200

    Fix ScriptEvaluator test

--- molgenis-js/src/test/java/org/molgenis/js/SandboxTest.java
+++ molgenis-js/src/test/java/org/molgenis/js/SandboxTest.java
@@ -1,6 +1,6 @@
 package org.molgenis.js;
 
-public class SandboxTest extends MolgenisJsTest
+public class SandboxTest extends ScriptEvaluatorTest
 {
 	//	@Test public void testAllowed() { ScriptEvaluator.eval("1 + 1", new MapEntity("firstName", "Piet"), new EntityMetaDataImpl("person")); } @Test(expectedExceptions = EcmaError.class) public void testCallingNonVisibleClass() { EntityMetaData emd = new EntityMetaDataImpl("person"); emd.addAttribute("firstName").setDataType(MolgenisFieldTypes.SCRIPT); ScriptEvaluator.eval("new java.lang.Integer(6).toString()", new MapEntity("firstName", "Piet"), emd); } @Test public void testGlobalMethod() { EntityMetaData emd = new EntityMetaDataImpl("person"); emd.addAttribute("firstName").setDataType(MolgenisFieldTypes.SCRIPT); ScriptEvaluator.eval("$('firstName')", new MapEntity("firstName", "Piet"), emd);
 	//	}
