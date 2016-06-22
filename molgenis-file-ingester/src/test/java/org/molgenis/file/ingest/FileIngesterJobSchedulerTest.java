0227a7ef8a05db1df25ca7ba86f53c8148486fcb
dennishendriksen <d.hendriksen@umcg.nl>
2016-06-22 08:44:03 +0200

    File FileIngesterJobScheduler test

--- molgenis-file-ingester/src/test/java/org/molgenis/file/ingest/FileIngesterJobSchedulerTest.java
+++ molgenis-file-ingester/src/test/java/org/molgenis/file/ingest/FileIngesterJobSchedulerTest.java
@@ -3,58 +3,76 @@ package org.molgenis.file.ingest;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.molgenis.file.ingest.meta.FileIngestMetaData.FILE_INGEST;
 
 import org.molgenis.data.DataService;
-import org.molgenis.data.Entity;
 import org.molgenis.data.UnknownEntityException;
-import org.molgenis.data.support.DynamicEntity;
 import org.molgenis.data.validation.MolgenisValidationException;
 import org.molgenis.file.ingest.meta.FileIngest;
-import org.molgenis.file.ingest.meta.FileIngestMetaData;
+import org.molgenis.file.ingest.meta.FileIngestFactory;
+import org.molgenis.fileingester.test.AbstractMolgenisSpringTest;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FileIngesterJobSchedulerTest {
+@ContextConfiguration(classes = { FileIngesterJobSchedulerTest.Config.class })
+public class FileIngesterJobSchedulerTest extends AbstractMolgenisSpringTest
+{
+	private static DataService dataServiceMock = mock(DataService.class);
+	;
+	private static Scheduler schedulerMock = mock(Scheduler.class);
+
+	@Autowired
 	private FileIngesterJobScheduler fileIngesterJobScheduler;
-	private DataService dataServiceMock;
-	private Scheduler schedulerMock;
+
+	@Autowired
+	private DataService dataService;
+
+	@Autowired
+	private Scheduler scheduler;
+
+	@Autowired
+	private FileIngestFactory fileIngestFactory;
 
 	@BeforeMethod
-	public void setUp()
+	public void setUpBeforeMethod()
 	{
-		dataServiceMock = mock(DataService.class);
-		schedulerMock = mock(Scheduler.class);
-		fileIngesterJobScheduler = new FileIngesterJobScheduler(schedulerMock, dataServiceMock);
+		reset(dataServiceMock);
+		reset(schedulerMock);
 	}
 
 	@Test
 	public void runNow() throws SchedulerException
 	{
 		String id = "id";
-		FileIngest fileIngest = new FileIngest(null);
-		fileIngest.set(FileIngestMetaData.ID, id);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
 
-		when(dataServiceMock.findOneById(FILE_INGEST, id, FileIngest.class)).thenReturn(fileIngest);
-		when(schedulerMock.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
-		
+		when(dataService.findOneById(FILE_INGEST, id, FileIngest.class)).thenReturn(fileIngest);
+		when(scheduler.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
+
 		fileIngesterJobScheduler.runNow(id);
 
-		verify(schedulerMock).scheduleJob(any(JobDetail.class), any(Trigger.class));
+		verify(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
 	}
 
 	@Test(expectedExceptions = UnknownEntityException.class)
 	public void runNowUnknownEntity()
 	{
 		String id = "id";
-		when(dataServiceMock.findOneById(FILE_INGEST, id)).thenReturn(null);
+		when(dataService.findOneById(FILE_INGEST, id)).thenReturn(null);
 		fileIngesterJobScheduler.runNow(id);
 	}
 
@@ -62,60 +80,60 @@ public class FileIngesterJobSchedulerTest {
 	public void runNowExists() throws SchedulerException
 	{
 		String id = "id";
-		FileIngest fileIngest = new FileIngest(null);
-		fileIngest.set(FileIngestMetaData.ID, id);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
 
-		when(dataServiceMock.findOneById(FILE_INGEST, id, FileIngest.class)).thenReturn(fileIngest);
-		when(schedulerMock.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(true);
+		when(dataService.findOneById(FILE_INGEST, id, FileIngest.class)).thenReturn(fileIngest);
+		when(scheduler.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(true);
 
 		fileIngesterJobScheduler.runNow(id);
 
-		verify(schedulerMock).triggerJob(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP));
+		verify(scheduler).triggerJob(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP));
 	}
 
 	@Test
 	public void schedule() throws SchedulerException
 	{
 		String id = "id";
-		FileIngest fileIngest = new FileIngest(null);
-		fileIngest.set(FileIngestMetaData.ID, id);
-		fileIngest.set(FileIngestMetaData.CRONEXPRESSION, "	0/20 * * * * ?");
-		fileIngest.set(FileIngestMetaData.NAME, "name");
-		fileIngest.set(FileIngestMetaData.ACTIVE, true);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
+		fileIngest.setCronExpression("	0/20 * * * * ?");
+		fileIngest.setName("name");
+		fileIngest.setActive(true);
 
-		when(schedulerMock.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
+		when(scheduler.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
 
 		fileIngesterJobScheduler.schedule(fileIngest);
 
-		verify(schedulerMock).scheduleJob(any(JobDetail.class), any(Trigger.class));
+		verify(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
 	}
 
 	@Test
 	public void scheduleInactive() throws SchedulerException
 	{
 		String id = "id";
-		Entity fileIngest = new DynamicEntity(null); // // FIXME pass entity meta data instead of null
-		fileIngest.set(FileIngestMetaData.ID, id);
-		fileIngest.set(FileIngestMetaData.CRONEXPRESSION, "	0/20 * * * * ?");
-		fileIngest.set(FileIngestMetaData.NAME, "name");
-		fileIngest.set(FileIngestMetaData.ACTIVE, false);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
+		fileIngest.setCronExpression("	0/20 * * * * ?");
+		fileIngest.setName("name");
+		fileIngest.setActive(false);
 
-		when(schedulerMock.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
+		when(scheduler.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(false);
 
 		fileIngesterJobScheduler.schedule(fileIngest);
 
-		verify(schedulerMock, never()).scheduleJob(any(JobDetail.class), any(Trigger.class));
+		verify(scheduler, never()).scheduleJob(any(JobDetail.class), any(Trigger.class));
 	}
-	
+
 	@Test(expectedExceptions = MolgenisValidationException.class)
 	public void scheduleInvalidCronExpression() throws SchedulerException
 	{
 		String id = "id";
-		Entity fileIngest = new DynamicEntity(null); // // FIXME pass entity meta data instead of null
-		fileIngest.set(FileIngestMetaData.ID, id);
-		fileIngest.set(FileIngestMetaData.CRONEXPRESSION, "XXX");
-		fileIngest.set(FileIngestMetaData.NAME, "name");
-		fileIngest.set(FileIngestMetaData.ACTIVE, false);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
+		fileIngest.setCronExpression("XXX");
+		fileIngest.setName("name");
+		fileIngest.setActive(false);
 
 		fileIngesterJobScheduler.schedule(fileIngest);
 	}
@@ -124,18 +142,18 @@ public class FileIngesterJobSchedulerTest {
 	public void scheduleExisting() throws SchedulerException
 	{
 		String id = "id";
-		FileIngest fileIngest = new FileIngest(null);
-		fileIngest.set(FileIngestMetaData.ID, id);
-		fileIngest.set(FileIngestMetaData.CRONEXPRESSION, "	0/20 * * * * ?");
-		fileIngest.set(FileIngestMetaData.NAME, "name");
-		fileIngest.set(FileIngestMetaData.ACTIVE, true);
+		FileIngest fileIngest = fileIngestFactory.create();
+		fileIngest.setId(id);
+		fileIngest.setCronExpression("	0/20 * * * * ?");
+		fileIngest.setName("name");
+		fileIngest.setActive(true);
 
-		when(schedulerMock.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(true);
+		when(scheduler.checkExists(new JobKey(id, FileIngesterJobScheduler.JOB_GROUP))).thenReturn(true);
 
 		fileIngesterJobScheduler.schedule(fileIngest);
 
-		verify(schedulerMock).deleteJob((new JobKey(id, FileIngesterJobScheduler.JOB_GROUP)));
-		verify(schedulerMock).scheduleJob(any(JobDetail.class), any(Trigger.class));
+		verify(scheduler).deleteJob((new JobKey(id, FileIngesterJobScheduler.JOB_GROUP)));
+		verify(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
 	}
 
 	@Test
@@ -143,6 +161,30 @@ public class FileIngesterJobSchedulerTest {
 	{
 		String id = "id";
 		fileIngesterJobScheduler.unschedule(id);
-		verify(schedulerMock).deleteJob((new JobKey(id, FileIngesterJobScheduler.JOB_GROUP)));
+		verify(scheduler).deleteJob((new JobKey(id, FileIngesterJobScheduler.JOB_GROUP)));
+	}
+
+	@Configuration
+	@ComponentScan({ "org.molgenis.file.ingest.meta", "org.molgenis.security.owned", "org.molgenis.file.model",
+			"org.molgenis.data.jobs.model", "org.molgenis.auth" })
+	public static class Config
+	{
+		@Bean
+		public FileIngesterJobScheduler fileIngesterJobScheduler()
+		{
+			return new FileIngesterJobScheduler(scheduler(), dataService());
+		}
+
+		@Bean
+		public Scheduler scheduler()
+		{
+			return schedulerMock;
+		}
+
+		@Bean
+		public DataService dataService()
+		{
+			return dataServiceMock;
+		}
 	}
 }
