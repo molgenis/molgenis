7f9d01b1684ac9ecbec2a73aee50b5d1e9e35d7d
Mark-de-Haan <markdehaan90@gmail.com>
2016-06-23 13:25:31 +0200

    Remove modules from branch

--- molgenis-ontology/src/main/java/org/molgenis/ontology/initializer/OntologyScriptInitializer.java
+++
@@ -1,6 +0,0 @@
-package org.molgenis.ontology.initializer;
-
-public interface OntologyScriptInitializer
-{
-	void initialize();
-}
--- molgenis-security-core/src/main/java/org/molgenis/security/core/Permission.java
+++
@@ -1,6 +0,0 @@
-package org.molgenis.security.core;
-
-public enum Permission
-{
-	READ, WRITE, COUNT, NONE, WRITEMETA;
-};
