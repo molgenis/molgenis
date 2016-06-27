05059e0dacdb7525d57409799f35a157c02e7fd4
Mark-de-Haan <markdehaan90@gmail.com>
2016-06-27 17:47:04 +0200

    [WIP] CachingUtils and L1 cache unit tests

---
+++ molgenis-data/src/test/java/org/molgenis/data/cache/L1CacheTest.java
@@ -0,0 +1,20 @@
+package org.molgenis.data.cache;
+
+import org.testng.annotations.Test;
+
+public class L1CacheTest
+{
+
+
+	@Test
+	public void testHydrate()
+	{
+
+	}
+
+	@Test
+	public void deHydrate()
+	{
+
+	}
+}
