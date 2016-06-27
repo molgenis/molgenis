15f17f120676561969906b6d72dbcf85e4fd294e
dennishendriksen <d.hendriksen@umcg.nl>
2016-06-27 12:12:42 +0200

    Fix EntityUtils test

--- molgenis-data/src/test/java/org/molgenis/util/EntityUtilsTest.java
+++ molgenis-data/src/test/java/org/molgenis/util/EntityUtilsTest.java
@@ -1,70 +1,64 @@
 package org.molgenis.util;
 
+import static com.google.common.collect.ImmutableMap.of;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.molgenis.MolgenisFieldTypes.STRING;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.molgenis.data.meta.model.AttributeMetaData;
+import org.molgenis.data.meta.model.EntityMetaData;
+import org.molgenis.data.support.DynamicEntity;
+import org.testng.annotations.Test;
+
 public class EntityUtilsTest
 {
-	//	private static EntityMetaData entityMetaData;
-	//
-	//	@Test
-	//	public void isEmpty()
-	//	{
-	//		assertTrue(EntityUtils.isEmpty(new MapEntity()));
-	//		assertTrue(EntityUtils.isEmpty(new MapEntity("col", null)));
-	//		assertFalse(EntityUtils.isEmpty(new MapEntity("col", "test")));
-	//	}
-	//
-	//	@Test
-	//	public void convert()
-	//	{
-	//		entityMetaData = new EntityMetaDataImpl("User");
-	//		entityMetaData.addAttribute("name", ROLE_ID);
-	//
-	//		DataService dataService = mock(DataService.class);
-	//
-	//		User user = new User(dataService);
-	//		user.setName("Piet");
-	//		User converted = EntityUtils.convert(user, User.class, dataService);
-	//		assertEquals(converted, user);
-	//
-	//		Entity entity = new DefaultEntity(entityMetaData, dataService);
-	//		entity.set("name", "Piet");
-	//		converted = EntityUtils.convert(entity, User.class, dataService);
-	//		assertEquals(converted, entity);
-	//
-	//	}
-	//
-	//	@Test
-	//	public void doesExtend()
-	//	{
-	//		EntityMetaData grandfather = new EntityMetaDataImpl("grandfather");
-	//		assertFalse(EntityUtils.doesExtend(grandfather, "grandfather"));
-	//
-	//		EntityMetaData father = new EntityMetaDataImpl("father");
-	//		father.setExtends(grandfather);
-	//		assertTrue(EntityUtils.doesExtend(father, "grandfather"));
-	//
-	//		EntityMetaData child = new EntityMetaDataImpl("child");
-	//		child.setExtends(father);
-	//		assertTrue(EntityUtils.doesExtend(child, "grandfather"));
-	//	}
-	//
-	//	public static class User extends DefaultEntity
-	//	{
-	//		private static final long serialVersionUID = 1L;
-	//
-	//		public User(DataService dataService)
-	//		{
-	//			super(entityMetaData, dataService);
-	//		}
-	//
-	//		public String getName()
-	//		{
-	//			return getString("name");
-	//		}
-	//
-	//		public void setName(String name)
-	//		{
-	//			set("name", name);
-	//		}
-	//	}
+	@Test
+	public void isEmptyNoAttributes()
+	{
+		EntityMetaData entityMeta = when(mock(EntityMetaData.class).getName()).thenReturn("entity").getMock();
+		when(entityMeta.getAtomicAttributes()).thenReturn(emptyList());
+		assertTrue(EntityUtils.isEmpty(new DynamicEntity(entityMeta)));
+	}
 
+	@Test
+	public void isEmptyAttributeValuesNull()
+	{
+		EntityMetaData entityMeta = when(mock(EntityMetaData.class).getName()).thenReturn("entity").getMock();
+		AttributeMetaData attr = when(mock(AttributeMetaData.class).getName()).thenReturn("attr").getMock();
+		when(attr.getDataType()).thenReturn(STRING);
+		when(entityMeta.getAtomicAttributes()).thenReturn(singletonList(attr));
+		when(entityMeta.getAttribute("attr")).thenReturn(attr);
+		assertTrue(EntityUtils.isEmpty(new DynamicEntity(entityMeta, singletonMap("attr", null))));
+	}
+
+	@Test
+	public void isEmptyAttributeValuesNotNull()
+	{
+		EntityMetaData entityMeta = when(mock(EntityMetaData.class).getName()).thenReturn("entity").getMock();
+		AttributeMetaData attr = when(mock(AttributeMetaData.class).getName()).thenReturn("attr").getMock();
+		when(attr.getDataType()).thenReturn(STRING);
+		when(entityMeta.getAtomicAttributes()).thenReturn(singletonList(attr));
+		when(entityMeta.getAttribute("attr")).thenReturn(attr);
+		assertFalse(EntityUtils.isEmpty(new DynamicEntity(entityMeta, of("attr", "val"))));
+	}
+
+	@Test
+	public void doesExtend()
+	{
+		EntityMetaData grandfather = when(mock(EntityMetaData.class).getName()).thenReturn("grandfather").getMock();
+		assertFalse(EntityUtils.doesExtend(grandfather, "grandfather"));
+
+		EntityMetaData father = when(mock(EntityMetaData.class).getName()).thenReturn("father").getMock();
+		when(father.getExtends()).thenReturn(grandfather);
+		assertTrue(EntityUtils.doesExtend(father, "grandfather"));
+
+		EntityMetaData child = when(mock(EntityMetaData.class).getName()).thenReturn("child").getMock();
+		when(child.getExtends()).thenReturn(father);
+		assertTrue(EntityUtils.doesExtend(child, "grandfather"));
+	}
 }
