723cf1b6aa7f2b3679defa7e3dacbc022b4d8191
bartcharbon <bart.charbon@gmail.com>
2016-06-27 15:17:48 +0200

    Fix das unittest

--- molgenis-das/src/test/java/org/molgenis/das/impl/RepositoryRangeHandlingDataSourceTest.java
+++ molgenis-das/src/test/java/org/molgenis/das/impl/RepositoryRangeHandlingDataSourceTest.java
@@ -1,130 +1,215 @@
 package org.molgenis.das.impl;
 
-public class RepositoryRangeHandlingDataSourceTest
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.molgenis.MolgenisFieldTypes;
+import org.molgenis.data.DataService;
+import org.molgenis.data.Entity;
+import org.molgenis.data.Query;
+import org.molgenis.data.elasticsearch.util.Hit;
+import org.molgenis.data.elasticsearch.util.SearchResult;
+import org.molgenis.data.meta.model.AttributeMetaDataFactory;
+import org.molgenis.data.meta.model.EntityMetaData;
+import org.molgenis.data.meta.model.EntityMetaDataFactory;
+import org.molgenis.data.support.DynamicEntity;
+import org.molgenis.data.support.GenomicDataSettings;
+import org.molgenis.data.support.QueryImpl;
+import org.molgenis.test.data.AbstractMolgenisSpringTest;
+import org.molgenis.util.ApplicationContextProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import uk.ac.ebi.mydas.configuration.DataSourceConfiguration;
+import uk.ac.ebi.mydas.configuration.PropertyType;
+import uk.ac.ebi.mydas.exceptions.BadReferenceObjectException;
+import uk.ac.ebi.mydas.exceptions.CoordinateErrorException;
+import uk.ac.ebi.mydas.exceptions.DataSourceException;
+import uk.ac.ebi.mydas.exceptions.UnimplementedFeatureException;
+import uk.ac.ebi.mydas.model.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.molgenis.data.meta.model.EntityMetaData.AttributeRole.ROLE_ID;
+import static org.molgenis.data.meta.model.EntityMetaData.AttributeRole.ROLE_LABEL;
+import static org.molgenis.data.support.GenomicDataSettings.Meta.*;
+import static org.testng.Assert.assertEquals;
+import static uk.ac.ebi.mydas.model.DasFeatureOrientation.ORIENTATION_NOT_APPLICABLE;
+
+public class RepositoryRangeHandlingDataSourceTest extends AbstractMolgenisSpringTest
 {
-	//	RepositoryRangeHandlingDataSource source;
-	//	private DasFeature dasFeature;
-	//	private DataService dataService;
-	//	private ArrayList<Hit> resultList;
-	//	private ArrayList<DasFeature> featureList;
-	//	private GenomicDataSettings genomicDataSettings;
-	//
-	//	@BeforeMethod
-	//	public void setUp() throws DataSourceException, MalformedURLException
-	//	{
-	//		dataService = mock(DataService.class);
-	//		genomicDataSettings = mock(GenomicDataSettings.class);
-	//
-	//		ApplicationContext ctx = mock(ApplicationContext.class);
-	//		when(ctx.getBean(DataService.class)).thenReturn(dataService);
-	//		when(ctx.getBean(GenomicDataSettings.class)).thenReturn(genomicDataSettings);
-	//		new ApplicationContextProvider().setApplicationContext(ctx);
-	//
-	//		EntityMetaData metaData = new EntityMetaData("dataset");
-	//		when(dataService.getEntityMetaData("dataset")).thenReturn(metaData);
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_CHROM, metaData)).thenReturn("CHROM");
-	//
-	//		DasType type = new DasType("0", "", "", "type");
-	//		DasMethod method = new DasMethod("not_recorded", "not_recorded", "ECO:0000037");
-	//		source = new RepositoryRangeHandlingDataSource();
-	//		DataSourceConfiguration dataSourceConfig = mock(DataSourceConfiguration.class);
-	//		PropertyType propertyType = new PropertyType();
-	//		propertyType.setValue("type");
-	//		when(dataSourceConfig.getDataSourceProperties()).thenReturn(Collections.singletonMap("type", propertyType));
-	//		source.init(null, null, dataSourceConfig);
-	//		Map<URL, String> linkout = new HashMap<URL, String>();
-	//		linkout.put(new URL("http://www.molgenis.org/"), "Link");
-	//
-	//		List<DasTarget> dasTarget = new ArrayList<DasTarget>();
-	//		dasTarget.add(new MolgenisDasTarget("mutation id", 10, 1000, "description"));
-	//		List<String> notes = new ArrayList<String>();
-	//		notes.add("track:dataset");
-	//		notes.add("source:MOLGENIS");
-	//
-	//		dasFeature = new DasFeature("mutation id", "description", type, method, 10, 1000, new Double(0),
-	//				DasFeatureOrientation.ORIENTATION_NOT_APPLICABLE, DasPhase.PHASE_NOT_APPLICABLE, notes, linkout,
-	//				dasTarget, new ArrayList<String>(), null);
-	//
-	//		Query<Entity> q = new QueryImpl<Entity>().eq("CHROM", "1");
-	//		q.pageSize(100);
-	//		SearchResult result = mock(SearchResult.class);
-	//		EntityMetaData emd = new EntityMetaData("DAS");
-	//		emd.addAttribute(new AttributeMetaData("STOP"));
-	//		emd.addAttribute(new AttributeMetaData("linkout"));
-	//		emd.addAttribute(new AttributeMetaData("NAME"), ROLE_LABEL);
-	//		emd.addAttribute(new AttributeMetaData("INFO"));
-	//		emd.addAttribute(new AttributeMetaData("POS"));
-	//		emd.addAttribute(new AttributeMetaData("ID"), ROLE_ID);
-	//		emd.addAttribute(new AttributeMetaData("CHROM"));
-	//
-	//		MapEntity entity = new MapEntity(emd);
-	//
-	//		Map<String, Object> map = new HashMap<String, Object>();
-	//		map.put("STOP", 1000);
-	//		map.put("linkout", "http://www.molgenis.org/");
-	//		map.put("NAME", "mutation name");
-	//		map.put("INFO", "description");
-	//		map.put("POS", 10);
-	//		map.put("ID", "mutation id");
-	//		map.put("CHROM", "1");
-	//
-	//		entity.set(new MapEntity(map));
-	//
-	//		resultList = new ArrayList<>();
-	//		resultList.add(new Hit("", "", map));
-	//		featureList = new ArrayList<>();
-	//		featureList.add(dasFeature);
-	//		when(dataService.findAll("dataset", q)).thenAnswer(new Answer<Stream<MapEntity>>()
-	//		{
-	//			@Override
-	//			public Stream<MapEntity> answer(InvocationOnMock invocation) throws Throwable
-	//			{
-	//				return Stream.of(entity);
-	//			}
-	//		});
-	//		when(result.iterator()).thenReturn(resultList.iterator());
-	//
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_CHROM, entity.getEntityMetaData()))
-	//				.thenReturn("CHROM");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_POS, entity.getEntityMetaData()))
-	//				.thenReturn("POS");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_STOP, entity.getEntityMetaData()))
-	//				.thenReturn("STOP");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_IDENTIFIER, entity.getEntityMetaData()))
-	//				.thenReturn("ID");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_DESCRIPTION, entity.getEntityMetaData()))
-	//				.thenReturn("INFO");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_NAME, entity.getEntityMetaData()))
-	//				.thenReturn("NAME");
-	//		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_LINKOUT, entity.getEntityMetaData()))
-	//				.thenReturn("linkout");
-	//
-	//	}
-	//
-	//	@AfterMethod
-	//	public void teardown()
-	//	{
-	//		Mockito.reset(dataService);
-	//	}
-	//
-	//	@Test
-	//	public void getFeaturesRange() throws UnimplementedFeatureException, DataSourceException,
-	//			BadReferenceObjectException, CoordinateErrorException
-	//	{
-	//		assertEquals(new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getFeatures(),
-	//				source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getFeatures());
-	//		assertEquals(new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getSegmentId(),
-	//				source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getSegmentId());
-	//		assertEquals(new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getStartCoordinate(),
-	//				source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getStartCoordinate());
-	//		assertEquals(new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getStopCoordinate(),
-	//				source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getStopCoordinate());
-	//	}
-	//
-	//	@Test
-	//	public void getTypes() throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
-	//			CoordinateErrorException
-	//	{
-	//		assertEquals(Collections.singleton(new DasType("type", null, "?", "type")), source.getTypes());
-	//	}
+	@Autowired
+	private EntityMetaDataFactory entityMetaFactory;
+	@Autowired
+	private AttributeMetaDataFactory attrMetaFactory;
+
+	RepositoryRangeHandlingDataSource source;
+	private DasFeature dasFeature;
+	private DataService dataService;
+	private ArrayList<Hit> resultList;
+	private ArrayList<DasFeature> featureList;
+	private GenomicDataSettings genomicDataSettings;
+
+	@BeforeMethod
+	public void setUp() throws DataSourceException, MalformedURLException
+	{
+		dataService = mock(DataService.class);
+		genomicDataSettings = mock(GenomicDataSettings.class);
+
+		ApplicationContext ctx = mock(ApplicationContext.class);
+		when(ctx.getBean(DataService.class)).thenReturn(dataService);
+		when(ctx.getBean(GenomicDataSettings.class)).thenReturn(genomicDataSettings);
+		new ApplicationContextProvider().setApplicationContext(ctx);
+
+		EntityMetaData metaData = entityMetaFactory.create().setName("dataset");
+		when(dataService.getEntityMetaData("dataset")).thenReturn(metaData);
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_CHROM, metaData)).thenReturn("CHROM");
+
+		DasType type = new DasType("0", "", "", "type");
+		DasMethod method = new DasMethod("not_recorded", "not_recorded", "ECO:0000037");
+		source = new RepositoryRangeHandlingDataSource();
+		DataSourceConfiguration dataSourceConfig = mock(DataSourceConfiguration.class);
+		PropertyType propertyType = new PropertyType();
+		propertyType.setValue("type");
+		when(dataSourceConfig.getDataSourceProperties()).thenReturn(Collections.singletonMap("type", propertyType));
+		source.init(null, null, dataSourceConfig);
+		Map<URL, String> linkout = new HashMap<URL, String>();
+		linkout.put(new URL("http://www.molgenis.org/"), "Link");
+
+		List<DasTarget> dasTarget = new ArrayList<DasTarget>();
+		dasTarget.add(new MolgenisDasTarget("mutation id", 10, 1000, "description"));
+		List<String> notes = new ArrayList<String>();
+		notes.add("track:dataset");
+		notes.add("source:MOLGENIS");
+
+		dasFeature = new DasFeature("mutation id", "mutation name,description", type, method, 10, 1000, new Double(0),
+				ORIENTATION_NOT_APPLICABLE, DasPhase.PHASE_NOT_APPLICABLE, notes, linkout, dasTarget,
+				new ArrayList<>(), null);
+
+		Query<Entity> q = new QueryImpl<>().eq("CHROM", "1");
+		q.pageSize(100);
+		SearchResult result = mock(SearchResult.class);
+		EntityMetaData emd = entityMetaFactory.create().setName("DAS");
+		emd.addAttribute(attrMetaFactory.create().setName("STOP").setDataType(MolgenisFieldTypes.INT));
+		emd.addAttribute(attrMetaFactory.create().setName("linkout"));
+		emd.addAttribute(attrMetaFactory.create().setName("NAME"), ROLE_LABEL);
+		emd.addAttribute(attrMetaFactory.create().setName("INFO"));
+		emd.addAttribute(attrMetaFactory.create().setName("POS").setDataType(MolgenisFieldTypes.INT));
+		emd.addAttribute(attrMetaFactory.create().setName("ID"), ROLE_ID);
+		emd.addAttribute(attrMetaFactory.create().setName("CHROM"));
+
+		DynamicEntity entity = new DynamicEntity(emd);
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("STOP", 1000);
+		map.put("linkout", "http://www.molgenis.org/");
+		map.put("NAME", "mutation name");
+		map.put("INFO", "description");
+		map.put("POS", 10);
+		map.put("ID", "mutation id");
+		map.put("CHROM", "1");
+
+		for (String key : map.keySet())
+			entity.set(key, map.get(key));
+
+		resultList = new ArrayList<>();
+		resultList.add(new Hit("", "", map));
+		featureList = new ArrayList<>();
+		featureList.add(dasFeature);
+		when(dataService.findAll("dataset", q)).thenAnswer(new Answer<Stream<DynamicEntity>>()
+		{
+			@Override
+			public Stream<DynamicEntity> answer(InvocationOnMock invocation) throws Throwable
+			{
+				return Stream.of(entity);
+			}
+		});
+		when(result.iterator()).thenReturn(resultList.iterator());
+
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_CHROM, entity.getEntityMetaData()))
+				.thenReturn("CHROM");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_POS, entity.getEntityMetaData()))
+				.thenReturn("POS");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_STOP, entity.getEntityMetaData()))
+				.thenReturn("STOP");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_IDENTIFIER, entity.getEntityMetaData()))
+				.thenReturn("ID");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_DESCRIPTION, entity.getEntityMetaData()))
+				.thenReturn("INFO");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_NAME, entity.getEntityMetaData()))
+				.thenReturn("NAME");
+		when(genomicDataSettings.getAttributeNameForAttributeNameArray(ATTRS_LINKOUT, entity.getEntityMetaData()))
+				.thenReturn("linkout");
+
+	}
+
+	@AfterMethod
+	public void teardown()
+	{
+		Mockito.reset(dataService);
+	}
+
+	@Test
+	public void getFeaturesSize()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{
+		assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getFeatures().size(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getFeatures().size());
+	}
+
+	@Test
+	public void getFeaturesLabel()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{
+			assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getFeatures().iterator().next().getFeatureLabel(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getFeatures().iterator().next().getFeatureLabel());
+	}
+
+	@Test
+	public void getFeaturesId()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{	assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getFeatures().iterator().next().getFeatureId(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getFeatures().iterator().next().getFeatureId());
+	}
+
+	@Test
+	public void getFeaturesSegment()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{	assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getSegmentId(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getSegmentId());
+	}
+
+	@Test
+	public void getFeaturesStart()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{	assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getStartCoordinate(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getStartCoordinate());
+	}
+
+	@Test
+	public void getFeaturesStop()
+			throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{	assertEquals(source.getFeatures("1,dasdataset_dataset", 1, 100000, 100).getStopCoordinate(),
+				new DasAnnotatedSegment("1", 1, 100000, "1.00", "1", featureList).getStopCoordinate());
+	}
+
+	@Test
+	public void getTypes() throws UnimplementedFeatureException, DataSourceException, BadReferenceObjectException,
+			CoordinateErrorException
+	{
+		assertEquals(Collections.singleton(new DasType("type", null, "?", "type")), source.getTypes());
+	}
 }
