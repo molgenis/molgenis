755de9697a95b0563a5ae10d97fe1545b7383768
Mark-de-Haan <markdehaan90@gmail.com>
2016-08-02 09:53:27 +0200

    L3 cache WIP

--- molgenis-data-cache/src/main/java/org/molgenis/data/cache/l3/L3Cache.java
+++ molgenis-data-cache/src/main/java/org/molgenis/data/cache/l3/L3Cache.java
@@ -10,6 +10,7 @@ import org.molgenis.data.transaction.DefaultMolgenisTransactionListener;
 import org.molgenis.data.transaction.TransactionInformation;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 import java.util.Map;
@@ -20,15 +21,23 @@ import java.util.stream.Stream;
 import static com.google.common.cache.CacheBuilder.newBuilder;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newConcurrentMap;
+import static com.google.common.collect.Maps.newHashMap;
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.stream.Collectors.toList;
 import static org.slf4j.LoggerFactory.getLogger;
 
+/**
+ * In-memory Query cache containing Queries and resulting ids from cacheable repositories
+ */
 public class L3Cache extends DefaultMolgenisTransactionListener
 {
 	public static final Logger LOG = getLogger(L3Cache.class);
 	private static final int MAX_CACHE_SIZE_PER_QUERY = 1000;
 
-	private final ConcurrentMap<String, LoadingCache<Query<Entity>, List<Object>>> caches;
+	/**
+	 * maps entity name to the loading cache with Query key and Optional List of Identifiers
+	 */
+	private final ConcurrentMap<String, LoadingCache<Query<Entity>, Optional<List<Object>>>> caches;
 	private final TransactionInformation transactionInformation;
 
 	@Autowired
@@ -41,15 +50,13 @@ public class L3Cache extends DefaultMolgenisTransactionListener
 	@Override
 	public void afterCommitTransaction(String transactionId)
 	{
-		// TODO cleanup dirty repositories
-		// Is this correct?
 		transactionInformation.getDirtyRepositories().forEach(caches::remove);
 		transactionInformation.getDirtyEntities().forEach(this::evict);
 	}
 
 	private void evict(EntityKey entityKey)
 	{
-		LoadingCache<Query<Entity>, List<Object>> cache = caches.get(entityKey.getEntityName());
+		LoadingCache<Query<Entity>, Optional<List<Object>>> cache = caches.get(entityKey.getEntityName());
 		if (cache != null)
 		{
 			cache.invalidate(entityKey.getId());
@@ -63,11 +70,29 @@ public class L3Cache extends DefaultMolgenisTransactionListener
 
 	public List<Object> get(Repository<Entity> repository, Query<Entity> query)
 	{
-		LoadingCache<Query<Entity>, List<Object>> cache = getQueryCache(repository, query);
-		return cache.getUnchecked(query);
+		LoadingCache<Query<Entity>, Optional<List<Object>>> cache = getQueryCache(repository, query);
+		return cache.getUnchecked(query).orElse(null);
 	}
 
-	private LoadingCache<Query<Entity>, List<Object>> getQueryCache(Repository<Entity> repository, Query<Entity> query)
+	/**
+	 * Logs cumulative cache statistics for all known caches.
+	 */
+	@Scheduled(fixedRate = 60000)
+	public void logStatistics()
+	{
+		//TODO: do we want to log diff with last log instead?
+		if (LOG.isDebugEnabled())
+		{
+			LOG.debug("Cache stats:");
+			for (Map.Entry<String, LoadingCache<Query<Entity>, Optional<List<Object>>>> cacheEntry : caches.entrySet())
+			{
+				LOG.debug("{}:{}", cacheEntry.getKey(), cacheEntry.getValue().stats());
+			}
+		}
+	}
+
+	private LoadingCache<Query<Entity>, Optional<List<Object>>> getQueryCache(Repository<Entity> repository,
+			Query<Entity> query)
 	{
 		String name = repository.getName();
 		if (!caches.containsKey(name))
@@ -77,42 +102,19 @@ public class L3Cache extends DefaultMolgenisTransactionListener
 		return caches.get(name);
 	}
 
-	private LoadingCache<Query<Entity>, List<Object>> createQueryCache(Repository<Entity> repository,
+	private LoadingCache<Query<Entity>, Optional<List<Object>>> createQueryCache(Repository<Entity> repository,
 			Query<Entity> query)
 	{
 		return newBuilder().recordStats().maximumSize(MAX_CACHE_SIZE_PER_QUERY).expireAfterAccess(10, MINUTES)
-				.build(createCacheLoader(query));
+				.build(createCacheLoader(repository, query));
 	}
-
-	//	public Entity get(Repository<Entity> repository, Query<Entity> query)
-	//	{
-	//		LoadingCache<Query<Entity>, List<Object>> cache = getQueryCache(repository);
-	//		EntityMetaData entityMetaData = repository.getEntityMetaData();
-	//		return cache.getUnchecked(id.toString()).map(e -> entityHydration.hydrate(e, entityMetaData)).orElse(null);
-	//	}
-	//
-	//	private LoadingCache<Query<Entity>, List<Object>> getQueryCache(Repository<Entity> repository)
-	//	{
-	//		String name = repository.getName();
-	//		if (!caches.containsKey(name))
-	//		{
-	//			caches.putIfAbsent(name, createQueryCache(repository));
-	//		}
-	//		return caches.get(name);
-	//	}
-	//
-	//	private LoadingCache<Object, Optional<Map<String, Object>>> createQueryCache(Repository<Entity> repository)
-	//	{
-	//		return newBuilder().recordStats().maximumSize(MAX_CACHE_SIZE_PER_ENTITY).expireAfterAccess(10, MINUTES)
-	//				.build(createCacheLoader(repository));
-	//	}
 
 	/**
 	 * Create a cacheloader that loads entity ids from the repository and stores them together with their query
 	 *
 	 * @return
 	 */
-	private CacheLoader<Query<Entity>, List<Object>> createCacheLoader(final Repository<Entity> repository,
+	private CacheLoader<Query<Entity>, Optional<List<Object>>> createCacheLoader(final Repository<Entity> repository,
 			final Query<Entity> query)
 	{
 		return new CacheLoader<Query<Entity>, Optional<List<Object>>>()
@@ -136,10 +138,13 @@ public class L3Cache extends DefaultMolgenisTransactionListener
 			 * @throws Exception
 			 */
 			@Override
-			public Map<Query<Entity>, List<Object>> loadAll(Iterable<? extends Query<Entity>> keys) throws Exception
+			public Map<Query<Entity>, Optional<List<Object>>> loadAll(Iterable<? extends Query<Entity>> keys)
+					throws Exception
 			{
-
-				return super.loadAll(keys);
+				Map<Query<Entity>, Optional<List<Object>>> result = newHashMap();
+				result.put(query,
+						Optional.ofNullable(repository.findAll(query).map(Entity::getIdValue).collect(toList())));
+				return result;
 			}
 		};
 	}
