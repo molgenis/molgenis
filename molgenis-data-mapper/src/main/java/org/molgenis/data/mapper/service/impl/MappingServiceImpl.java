1ed2d0bada7cf4bbb4ccf241479b9ebad0ec3a52
Mark-de-Haan <markdehaan90@gmail.com>
2016-08-16 13:40:16 +0200

    Check if metadata is compatible while mapping, and return a detailed error message when not compatible + unit test

--- molgenis-data-mapper/src/main/java/org/molgenis/data/mapper/service/impl/MappingServiceImpl.java
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/mapper/service/impl/MappingServiceImpl.java
@@ -10,6 +10,7 @@ import org.molgenis.data.mapper.mapping.model.MappingTarget;
 import org.molgenis.data.mapper.repository.MappingProjectRepository;
 import org.molgenis.data.mapper.service.AlgorithmService;
 import org.molgenis.data.mapper.service.MappingService;
+import org.molgenis.data.meta.model.AttributeMetaData;
 import org.molgenis.data.meta.model.AttributeMetaDataFactory;
 import org.molgenis.data.meta.model.EntityMetaData;
 import org.molgenis.data.support.DynamicEntity;
@@ -23,12 +24,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 
+import static com.google.api.client.util.Maps.newHashMap;
+import static java.lang.String.format;
 import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.MolgenisFieldTypes.AttributeType.*;
 import static org.molgenis.data.mapper.meta.MappingProjectMetaData.NAME;
 import static org.molgenis.data.meta.model.EntityMetaData.AttributeCopyMode.DEEP_COPY_ATTRS;
+import static org.molgenis.data.support.EntityMetaDataUtils.isReferenceType;
 import static org.molgenis.security.core.runas.RunAsSystemProxy.runAsSystem;
 import static org.molgenis.util.DependencyResolver.hasSelfReferences;
 import static org.springframework.security.core.context.SecurityContextHolder.getContext;
@@ -183,6 +188,14 @@ public class MappingServiceImpl implements MappingService
 			// Get an existing repository
 			targetRepo = dataService.getRepository(entityName);
 
+			// Compare the metadata between the target repository and the mapping target
+			// Returns detailed information in case something is not compatible
+			String mappingException = compareTargetMetaDatas(targetRepo.getEntityMetaData(), targetMetaData);
+			if (mappingException != null)
+			{
+				throw new MolgenisDataException(mappingException);
+			}
+
 			// If the addSourceAttribute is true, but the existing repository does not have the SOURCE attribute yet
 			// Get the existing metadata and add the SOURCE attribute
 			EntityMetaData existingTargetMetaData = targetRepo.getEntityMetaData();
@@ -223,6 +236,64 @@ public class MappingServiceImpl implements MappingService
 		}
 	}
 
+	/**
+	 * Compares the attributes between the target repository and the mapping target.
+	 * Applied Rules:
+	 * - The mapping target can not contain attributes which are not in the target repository
+	 * - The attributes of the mapping target with the same name as attributes in the target repository should have the same type
+	 * - If there are reference attributes, the name of the reference entity should be the same in both the target repository as in the mapping target
+	 *
+	 * @param targetRepositoryMetaData
+	 * @param mappingTargetMetaData
+	 * @return A {@link String} containing details on a potential mapping exception, or null if the attributes of both the target repository and mapping target are compatible
+	 */
+	private String compareTargetMetaDatas(EntityMetaData targetRepositoryMetaData, EntityMetaData mappingTargetMetaData)
+	{
+		Map<String, AttributeMetaData> targetRepositoryAttributeMap = newHashMap();
+		targetRepositoryMetaData.getAtomicAttributes()
+				.forEach(attribute -> targetRepositoryAttributeMap.put(attribute.getName(), attribute));
+
+		for (AttributeMetaData mappingTargetAttribute : mappingTargetMetaData.getAtomicAttributes())
+		{
+			String mappingTargetAttributeName = mappingTargetAttribute.getName();
+			AttributeMetaData targetRepositoryAttribute = targetRepositoryAttributeMap.get(mappingTargetAttributeName);
+			if (targetRepositoryAttribute == null)
+			{
+				return format("Target repository does not contain the following attribute: %s",
+						mappingTargetAttributeName);
+			}
+
+			AttributeType targetRepositoryAttributeType = targetRepositoryAttribute.getDataType();
+			AttributeType mappingTargetAttributeType = mappingTargetAttribute.getDataType();
+			if (!mappingTargetAttributeType.equals(targetRepositoryAttributeType))
+			{
+				return format(
+						"attribute %s in the mapping target is type %s while attribute %s in the target repository is type %s. Please make sure the types are the same",
+						mappingTargetAttributeName, mappingTargetAttributeType, targetRepositoryAttribute.getName(),
+						targetRepositoryAttributeType);
+			}
+
+			if (isReferenceType(mappingTargetAttribute))
+			{
+				String mappingTargetRefEntityName = mappingTargetAttribute.getRefEntity().getName();
+				String targetRepositoryRefEntityName = targetRepositoryAttribute.getRefEntity().getName();
+				if (!mappingTargetRefEntityName.equals(targetRepositoryRefEntityName))
+				{
+					return format(
+							"In the mapping target, attribute %s of type %s has reference entity %s while in the target repository attribute %s of type %s has reference entity %s. "
+									+ "Please make sure the reference entities of your mapping target are pointing towards the same reference entities as your target repository",
+							mappingTargetAttributeName, mappingTargetAttributeType, mappingTargetRefEntityName,
+							targetRepositoryAttribute.getName(), targetRepositoryAttributeType,
+							targetRepositoryRefEntityName);
+				}
+			}
+
+		}
+
+		// The target repository and mapping target are compatible, return null
+		return null;
+	}
+
 	private void applyMappingsToRepositories(MappingTarget mappingTarget, Repository<Entity> targetRepo,
 			boolean addSourceAttribute)
 	{
@@ -238,8 +309,7 @@ public class MappingServiceImpl implements MappingService
 		EntityMetaData targetMetaData = targetRepo.getEntityMetaData();
 		Repository<Entity> sourceRepo = dataService.getRepository(sourceMapping.getName());
 
-		sourceRepo.iterator().forEachRemaining(sourceEntity ->
-		{
+		sourceRepo.iterator().forEachRemaining(sourceEntity -> {
 			{
 				Entity mappedEntity = applyMappingToEntity(sourceMapping, sourceEntity, targetMetaData,
 						sourceMapping.getSourceEntityMetaData(), addSourceAttribute);
