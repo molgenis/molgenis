1e708a737d8c41ceadbcb08c5eb00339904131a7
Fleur Kelpin <f.kelpin@umcg.nl>
2016-09-21 11:12:00 +0200

    Fix existing decorator tests.

--- molgenis-data-cache/src/test/java/org/molgenis/data/cache/l1/L1CacheRepositoryDecoratorTest.java
+++ molgenis-data-cache/src/test/java/org/molgenis/data/cache/l1/L1CacheRepositoryDecoratorTest.java
@@ -1,9 +1,12 @@
 package org.molgenis.data.cache.l1;
 
 import com.google.common.collect.Sets;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.molgenis.data.Entity;
+import org.molgenis.data.EntityKey;
 import org.molgenis.data.EntityManager;
 import org.molgenis.data.Repository;
 import org.molgenis.data.meta.model.AttributeMetaDataFactory;
@@ -19,8 +22,12 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static junit.framework.Assert.assertNull;
@@ -37,10 +44,12 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 {
 	private L1CacheRepositoryDecorator l1CacheRepositoryDecorator;
 	private EntityMetaData entityMetaData;
-	private Entity mockEntity;
 
-	private final String repository = "TestRepository";
+	private final String entityName = "TestRepository";
 	private final String entityID = "1";
+	private final String entityID2 = "2";
+	private Entity entity;
+	private Entity entity2;
 
 	@Autowired
 	private EntityMetaDataFactory entityMetaDataFactory;
@@ -48,32 +57,37 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 	@Autowired
 	private AttributeMetaDataFactory attributeMetaDataFactory;
 
-	@Autowired
-	private EntityManager entityManager;
-
 	@Mock
 	private L1Cache l1Cache;
 
 	@Mock
 	private Repository<Entity> decoratedRepository;
 
+	@Captor
+	private ArgumentCaptor<Stream<Entity>> entitiesCaptor;
+
+	@Captor
+	private ArgumentCaptor<Stream<Object>> entityIdsCaptor;
+
 	@BeforeClass
 	public void beforeClass()
 	{
 		initMocks(this);
 
-		entityMetaData = entityMetaDataFactory.create(repository);
+		entityMetaData = entityMetaDataFactory.create(entityName);
 		entityMetaData.addAttribute(attributeMetaDataFactory.create().setName("ID"), ROLE_ID);
 		entityMetaData.addAttribute(attributeMetaDataFactory.create().setName("ATTRIBUTE_1"));
 
-		when(entityManager.create(entityMetaData)).thenReturn(new DynamicEntity(entityMetaData));
+		entity = new DynamicEntity(entityMetaData);
+		entity.set("ID", entityID);
+		entity.set("ATTRIBUTE_1", "test_value_1");
 
-		mockEntity = entityManager.create(entityMetaData);
-		mockEntity.set("ID", entityID);
-		mockEntity.set("ATTRIBUTE_1", "test_value_1");
+		entity2 = new DynamicEntity(entityMetaData);
+		entity2.set("ID", entityID2);
+		entity2.set("ATTRIBUTE_1", "test_value_2");
 
 		when(decoratedRepository.getCapabilities()).thenReturn(Sets.newHashSet(CACHEABLE, WRITABLE));
-		when(decoratedRepository.getName()).thenReturn(repository);
+		when(decoratedRepository.getName()).thenReturn(entityName);
 		when(decoratedRepository.getEntityMetaData()).thenReturn(entityMetaData);
 
 		l1CacheRepositoryDecorator = new L1CacheRepositoryDecorator(decoratedRepository, l1Cache);
@@ -88,72 +102,85 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testAdd()
 	{
-		l1CacheRepositoryDecorator.add(mockEntity);
-		verify(l1Cache).put(repository, mockEntity);
+		l1CacheRepositoryDecorator.add(entity);
+		verify(l1Cache).put(entityName, entity);
 	}
 
-	@Test //FIXME l1Cache is used but not?
+	@Test
 	public void testAddWithStreamOfEntities()
 	{
-		l1CacheRepositoryDecorator.add(Stream.of(mockEntity));
-		// verify(l1Cache).put(repository, mockEntity);
+		l1CacheRepositoryDecorator.add(Stream.of(entity));
+
+		verify(decoratedRepository.add(entitiesCaptor.capture()));
+		entitiesCaptor.getValue().collect(Collectors.toList());
+		verify(l1Cache).put(entityName, entity);
 	}
 
 	@Test
 	public void testDelete()
 	{
-		l1CacheRepositoryDecorator.delete(mockEntity);
-		verify(l1Cache).putDeletion(create(mockEntity));
+		l1CacheRepositoryDecorator.delete(entity);
+		verify(l1Cache).putDeletion(create(entity));
 	}
 
 	@Test
 	public void testDeleteById()
 	{
 		l1CacheRepositoryDecorator.deleteById(entityID);
-		verify(l1Cache).putDeletion(create(mockEntity));
+		verify(l1Cache).putDeletion(create(entity));
 	}
 
-	@Test //FIXME l1Cache is used but not?
+	@Test
 	public void testDeleteByStreamOfEntities()
 	{
-		l1CacheRepositoryDecorator.delete(Stream.of(mockEntity));
-		// verify(l1Cache).putDeletion(EntityKey.create(mockEntity));
+		l1CacheRepositoryDecorator.delete(Stream.of(entity));
+
+		verify(decoratedRepository).delete(entitiesCaptor.capture());
+		entitiesCaptor.getValue().collect(Collectors.toList());
+		verify(l1Cache).putDeletion(EntityKey.create(entity));
 	}
 
 	@Test
 	public void testDeleteAll()
 	{
 		l1CacheRepositoryDecorator.deleteAll();
-		verify(l1Cache).evictAll(repository);
+		verify(l1Cache).evictAll(entityName);
 	}
 
-	@Test //FIXME l1Cache is used but not?
+	@Test
 	public void testDeleteAllByStreamOfIds()
 	{
 		l1CacheRepositoryDecorator.deleteAll(Stream.of(entityID));
-		// verify(l1Cache).putDeletion(EntityKey.create(repository, entityID));
+
+		verify(decoratedRepository).deleteAll(entityIdsCaptor.capture());
+		entityIdsCaptor.getValue().collect(Collectors.toList());
+		verify(l1Cache).putDeletion(EntityKey.create(entityName, entityID));
 	}
 
 	@Test
 	public void testUpdate()
 	{
-		l1CacheRepositoryDecorator.update(mockEntity);
-		verify(l1Cache).put(repository, mockEntity);
+		l1CacheRepositoryDecorator.update(entity);
+		verify(l1Cache).put(entityName, entity);
+		verify(decoratedRepository).update(entity);
 	}
 
-	@Test //FIXME l1Cache is used but not?
+	@Test
 	public void testUpdateWithStreamOfEntities()
 	{
-		l1CacheRepositoryDecorator.update(Stream.of(mockEntity));
-		// verify(l1Cache).put(repository, mockEntity);
+		l1CacheRepositoryDecorator.update(Stream.of(entity));
+
+		verify(decoratedRepository).update(entitiesCaptor.capture());
+		entitiesCaptor.getValue().collect(Collectors.toList());
+		verify(l1Cache).put(entityName, entity);
 	}
 
 	@Test
 	public void testFindOneByIdReturnsEntity()
 	{
-		when(l1Cache.get(repository, entityID, entityMetaData)).thenReturn(of(mockEntity));
+		when(l1Cache.get(entityName, entityID, entityMetaData)).thenReturn(of(entity));
 		Entity actualEntity = l1CacheRepositoryDecorator.findOneById(entityID);
-		assertEquals(actualEntity, mockEntity);
+		assertEquals(actualEntity, entity);
 
 		verify(decoratedRepository, never()).findOneById(Mockito.any());
 	}
@@ -161,7 +188,7 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testFindOneByIdReturnsEmpty()
 	{
-		when(l1Cache.get(repository, entityID, entityMetaData)).thenReturn(empty());
+		when(l1Cache.get(entityName, entityID, entityMetaData)).thenReturn(empty());
 		Entity actualEntity = l1CacheRepositoryDecorator.findOneById(entityID);
 		assertNull(actualEntity);
 
@@ -171,7 +198,7 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testFindOneByIdReturnsNull()
 	{
-		when(l1Cache.get(repository, entityID, entityMetaData)).thenReturn(null);
+		when(l1Cache.get(entityName, entityID, entityMetaData)).thenReturn(null);
 		Entity actualEntity = l1CacheRepositoryDecorator.findOneById(entityID);
 		assertNull(actualEntity);
 
@@ -180,10 +207,31 @@ public class L1CacheRepositoryDecoratorTest extends AbstractMolgenisSpringTest
 	}
 
 	@Test
-	public void testFindAllByStreamOfIds()
+	public void testFindAllByStreamOfIdsEntityNotPresentInCache()
 	{
-		l1CacheRepositoryDecorator.findAll(Stream.of(entityID));
-		// TODO Hard test
+		when(decoratedRepository.findAll(entityIdsCaptor.capture())).thenReturn(Stream.of(entity, entity2));
+
+		List<Entity> actual = l1CacheRepositoryDecorator.findAll(Stream.of(entityID, entityID2))
+				.collect(Collectors.toList());
+		assertEquals(asList(entity, entity2), actual);
+
+		List<Object> ids = entityIdsCaptor.getValue().collect(Collectors.toList());
+		assertEquals(ids, asList(entityID, entityID2));
+	}
+
+	@Test
+	public void testFindAllByStreamOfIdsOneCachedOneMissing()
+	{
+		when(l1Cache.get(entityName, entityID, entityMetaData)).thenReturn(of(entity));
+
+		when(decoratedRepository.findAll(entityIdsCaptor.capture())).thenReturn(Stream.of(entity2));
+
+		List<Entity> actual = l1CacheRepositoryDecorator.findAll(Stream.of(entityID, entityID2))
+				.collect(Collectors.toList());
+		assertEquals(asList(entity, entity2), actual);
+
+		List<Object> ids = entityIdsCaptor.getValue().collect(Collectors.toList());
+		assertEquals(ids, Collections.singletonList(entityID2));
 	}
 
 	@Configuration
