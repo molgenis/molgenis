8811880a28c66d4595d3e59acb9f5ac51b1f1aaa
ChaoPang <ChaoPang229@gmail.com>
2016-10-06 17:53:06 +0200

    replaced the invalid variable the correct one

--- molgenis-data-mapper/src/main/java/org/molgenis/data/mapper/algorithmgenerator/service/AlgorithmGeneratorService.java
+++ molgenis-data-mapper/src/main/java/org/molgenis/data/mapper/algorithmgenerator/service/AlgorithmGeneratorService.java
@@ -1,11 +1,12 @@
 package org.molgenis.data.mapper.algorithmgenerator.service;
 
-import java.util.List;
-import java.util.Map;
-
 import org.molgenis.data.mapper.algorithmgenerator.bean.GeneratedAlgorithm;
 import org.molgenis.data.meta.model.AttributeMetaData;
 import org.molgenis.data.meta.model.EntityMetaData;
+import org.molgenis.data.semanticsearch.explain.bean.ExplainedMatchCandidate;
+
+import java.util.List;
+import java.util.Map;
 
 public interface AlgorithmGeneratorService
 {
@@ -13,6 +14,6 @@ public interface AlgorithmGeneratorService
 			EntityMetaData targetEntityMetaData, EntityMetaData sourceEntityMetaData);
 
 	GeneratedAlgorithm generate(AttributeMetaData targetAttribute,
-			Map<AttributeMetaData, ExplainedAttributeMetaData> sourceAttributes, EntityMetaData targetEntityMetaData,
-			EntityMetaData sourceEntityMetaData);
+			Map<AttributeMetaData, ExplainedMatchCandidate<AttributeMetaData>> sourceAttributes,
+			EntityMetaData targetEntityMetaData, EntityMetaData sourceEntityMetaData);
 }
