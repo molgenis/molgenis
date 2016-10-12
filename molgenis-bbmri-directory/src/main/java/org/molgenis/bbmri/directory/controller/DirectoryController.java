00b4d3656b19980f17cd1d380ba73767709e64e2
Mark-de-Haan <markdehaan90@gmail.com>
2016-10-12 14:42:47 +0200

    Add initial state to directory view

--- molgenis-bbmri-directory/src/main/java/org/molgenis/bbmri/directory/controller/DirectoryController.java
+++ molgenis-bbmri-directory/src/main/java/org/molgenis/bbmri/directory/controller/DirectoryController.java
@@ -1,10 +1,10 @@
 package org.molgenis.bbmri.directory.controller;
 
 import com.google.gson.Gson;
-import org.molgenis.bbmri.directory.model.Collection;
 import org.molgenis.bbmri.directory.model.Query;
 import org.molgenis.bbmri.directory.settings.DirectorySettings;
 import org.molgenis.ui.MolgenisPluginController;
+import org.molgenis.ui.menu.MenuReaderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +12,19 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.bbmri.directory.controller.DirectoryController.URI;
-import static org.molgenis.bbmri.directory.model.Collection.createCollection;
-import static org.molgenis.bbmri.directory.model.Query.createQuery;
+import static org.molgenis.security.core.utils.SecurityUtils.getCurrentUsername;
 
 @Controller
 @RequestMapping(URI)
@@ -34,9 +35,16 @@ public class DirectoryController extends MolgenisPluginController
 	public static final String ID = "bbmridirectory";
 	public static final String URI = MolgenisPluginController.PLUGIN_URI_PREFIX + ID;
 	public static final String VIEW_DIRECTORY = "view-directory";
-	public DirectoryController()
+
+	private final MenuReaderService menuReaderService;
+
+	private static final String API_URI = "/api/";
+
+	@Autowired
+	public DirectoryController(MenuReaderService menuReaderService)
 	{
 		super(URI);
+		this.menuReaderService = menuReaderService;
 	}
 
 	@Autowired
@@ -46,28 +54,19 @@ public class DirectoryController extends MolgenisPluginController
 	DirectorySettings settings;
 
 	@RequestMapping
-	public String init(Model model)
+	public String init(HttpServletRequest request, Model model)
 	{
+		model.addAttribute("username", getCurrentUsername());
+		model.addAttribute("apiUrl", getApiUrl(request));
+		model.addAttribute("baseUrl", getBaseUrl());
 		return VIEW_DIRECTORY;
 	}
 
 	@RequestMapping("/query")
-	public java.net.URI postQuery() throws Exception
+	@ResponseBody
+	public String postQuery(@RequestBody Query query) throws Exception
 	{
-		LOG.debug("Query received, sending request");
-
-		// TODO create structured data
-
-		Collection collection = createCollection("eric:collectionID:AT_MUG:collection:all_samples",
-				"bbmri-eric:biobankID:AT_MUG");
-
-		String humanReadable = "name:Biobank Graz";
-		List<Collection> collections = singletonList(collection);
-
-		String url = "http://molgenis.todo.nl";
-
-		Query query = createQuery(url, collections, humanReadable, null);
-
+		LOG.info("Query " + query + " received, sending request");
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 
@@ -75,7 +74,10 @@ public class DirectoryController extends MolgenisPluginController
 		HttpEntity entity = new HttpEntity(query, headers);
 
 		LOG.trace("DirectorySettings.NEGOTIATOR_URL: [{}]", settings.getString(DirectorySettings.NEGOTIATOR_URL));
-		return restTemplate.postForLocation(settings.getString(DirectorySettings.NEGOTIATOR_URL), entity);
+
+		String testUri = "https://www.google.com?q=" + query.getURL();
+		return testUri;
+		// return restTemplate.postForLocation(settings.getString(DirectorySettings.NEGOTIATOR_URL), entity);
 	}
 
 	/**
@@ -83,12 +85,32 @@ public class DirectoryController extends MolgenisPluginController
 	 *
 	 * @return String
 	 */
-	private String generateBase64Authentication(){
+	private String generateBase64Authentication()
+	{
 		String username = settings.getString(DirectorySettings.USERNAME);
 		String password = settings.getString(DirectorySettings.PASSWORD);
 		requireNonNull(username, password);
 		String userPass = username + password;
 		String userPassBase64 = Base64.getEncoder().encodeToString(userPass.getBytes(StandardCharsets.UTF_8));
 		return String.format("Base %s", userPassBase64);
+	}
+
+	private static String getApiUrl(HttpServletRequest request)
+	{
+		String apiUrl;
+		if (StringUtils.isEmpty(request.getHeader("X-Forwarded-Host")))
+		{
+			apiUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getLocalPort() + API_URI;
+		}
+		else
+		{
+			apiUrl = request.getScheme() + "://" + request.getHeader("X-Forwarded-Host") + API_URI;
+		}
+		return apiUrl;
+	}
+
+	private String getBaseUrl()
+	{
+		return menuReaderService.getMenu().findMenuItemPath(DirectoryController.ID);
 	}
 }
