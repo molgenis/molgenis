e73b8d84c22573a1487eaeaca2be85fc89d60422
Fleur Kelpin <f.kelpin@umcg.nl>
2016-10-18 15:19:59 +0200

    FairController first method

--- molgenis-fair/src/main/java/org/molgenis/fair/controller/FairController.java
+++ molgenis-fair/src/main/java/org/molgenis/fair/controller/FairController.java
@@ -1,5 +1,35 @@
 package org.molgenis.fair.controller;
 
+import org.molgenis.data.DataService;
+import org.molgenis.data.Entity;
+import org.molgenis.data.support.QueryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.molgenis.fair.controller.FairController.BASE_URI;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@RequestMapping(BASE_URI)
 public class FairController
 {
+	static final String BASE_URI = "/fdp";
+
+	@Autowired
+	private final DataService dataService;
+
+	public FairController(DataService dataService)
+	{
+		this.dataService = dataService;
+	}
+
+	@RequestMapping(value = "/", method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Entity getMetadata()
+	{
+		return dataService.findOne("fdp_Metadata", new QueryImpl<>());
+	}
 }
