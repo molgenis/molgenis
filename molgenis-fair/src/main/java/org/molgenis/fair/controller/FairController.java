3cd4c3b9bec2ab57b1adc2dcda932f6a999e566c
bartcharbon <bart.charbon@gmail.com>
2016-10-19 13:20:06 +0200

    remove moved code

--- molgenis-fair/src/main/java/org/molgenis/fair/controller/FairController.java
+++ molgenis-fair/src/main/java/org/molgenis/fair/controller/FairController.java
@@ -1,23 +1,15 @@
 package org.molgenis.fair.controller;
 
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFFormat;
 import org.molgenis.data.DataService;
-import org.molgenis.data.Entity;
-import org.molgenis.data.Repository;
 import org.molgenis.data.meta.MetaDataService;
-import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.data.support.QueryImpl;
 import org.molgenis.security.core.runas.RunAsSystem;
 import org.molgenis.ui.converter.RDFMediaType;
+import org.molgenis.ui.model.SubjectEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.StringWriter;
-import java.util.Iterator;
 
 import static org.molgenis.fair.controller.FairController.BASE_URI;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -30,8 +22,9 @@ public class FairController
 
 	private final DataService dataService;
 	private final MetaDataService metaDataService;
+
 	@Autowired
-	public FairController(DataService dataService,MetaDataService metaDataService)
+	public FairController(DataService dataService, MetaDataService metaDataService)
 	{
 		this.dataService = dataService;
 		this.metaDataService = metaDataService;
@@ -40,56 +33,8 @@ public class FairController
 	@RequestMapping(method = GET, produces = RDFMediaType.TEXT_TURTLE_VALUE)
 	@ResponseBody
 	@RunAsSystem
-	public Entity getMetadata()
+	public SubjectEntity getMetadata()
 	{
-		return dataService.findOne("fdp_Metadata", new QueryImpl<>());
-	}
-
-	@RequestMapping(value = "/test", method = GET)
-	@ResponseBody
-	public String test()
-	{
-		return getRDF("sys_sec_User");
-	}
-
-	public String getRDF(String entityName)
-	{
-		StringWriter stringWriter = new StringWriter();
-
-		try
-		{
-			Model model = ModelFactory.createDefaultModel();
-			Repository repo = metaDataService.getRepository(entityName);
-			Iterator<Entity> iter = repo.iterator();
-			while (iter.hasNext())
-			{
-				Entity entity = iter.next();
-				String ns = "http://www.molgenis.org/";
-				for (Attribute metaData : entity.getEntityType().getAtomicAttributes())
-				{
-						Resource subject = model.createResource("/" + entity.getEntityType().getName().concat("/")
-								.concat(entity.getIdValue().toString()));
-						Property predicate = model
-								.createProperty("");
-						Resource object = model.createResource(entity.get(metaData.getName()) != null ? entity
-								.get(metaData.getName()).toString() : "");
-
-						connect(subject, predicate, object, model);
-
-					RDFDataMgr.write(stringWriter, model, RDFFormat.TRIG_PRETTY);
-				}
-			}
-		}
-		catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-		return stringWriter.toString();
-	}
-
-	private static Statement connect(Resource subject, Property predicate, Resource object, Model model)
-	{
-		model.add(subject, predicate, object);
-		return model.createStatement(subject, predicate, object);
+		return new SubjectEntity("http://molgenis01.gcc.rug.nl/fdp", dataService.findOne("fdp_Metadata", new QueryImpl<>()));
 	}
 }
