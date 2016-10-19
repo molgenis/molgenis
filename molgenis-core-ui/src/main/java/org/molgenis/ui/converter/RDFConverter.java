e0ccf670af2dab0176cbd41480e55262cfef62b8
bartcharbon <bart.charbon@gmail.com>
2016-10-19 16:10:33 +0200

    convert datatypes to correct jena versions (except reference types)

--- molgenis-core-ui/src/main/java/org/molgenis/ui/converter/RDFConverter.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/converter/RDFConverter.java
@@ -1,9 +1,11 @@
 package org.molgenis.ui.converter;
 
 import com.google.common.collect.Multimap;
+import org.apache.jena.datatypes.xsd.XSDDateTime;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
+import org.molgenis.AttributeType;
 import org.molgenis.data.Entity;
 import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.data.meta.model.EntityType;
@@ -21,9 +23,9 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.Calendar;
 
 import static org.molgenis.ui.converter.RDFMediaType.APPLICATION_TRIG;
 import static org.molgenis.ui.converter.RDFMediaType.TEXT_TURTLE;
@@ -64,40 +66,63 @@ public class RDFConverter extends AbstractHttpMessageConverter<SubjectEntity>
 		for (Attribute attribute : entityType.getAtomicAttributes())
 		{
 			Multimap<Relation, LabeledResource> tags = tagService.getTagsForAttribute(entityType, attribute);
+			Object value = entity.get(attribute.getName());
 			for (LabeledResource tag : tags.get(Relation.isAssociatedWith))
 			{
-				Object value = entity.get(attribute.getName());
-				if(value != null)
+				if (value != null)
 				{
-					convertToRdf(subjectEntity.getSubject() ,tag.getIri(), value.toString(), model);
+					if (attribute.getDataType().equals(AttributeType.HYPERLINK))
+					{
+						convertIRIToRdf(subjectEntity.getSubject(), tag.getIri(), entity.getString(attribute.getName()),
+								model);
+					}
+					else
+					{
+						if (attribute.getDataType().equals(AttributeType.DATE_TIME))
+						{
+							Calendar calendar = Calendar.getInstance();
+							calendar.setTime(entity.getUtilDate(attribute.getName()));
+							XSDDateTime xsdDateTime = new XSDDateTime(calendar);
+							value = xsdDateTime;
+						}
+						convertValueToRdf(subjectEntity.getSubject(), tag.getIri(), value, model);
+					}
 				}
 			}
 		}
 		RDFDataMgr.write(writer, model, RDFFormat.TURTLE);
 		writer.close();
-
 	}
 
-	public String convertToRdf(String subjectString, String tag, String attrName, Model model)
+	public void convertValueToRdf(String subjectString, String tag, Object attrValue, Model model)
 	{
-		StringWriter stringWriter = new StringWriter();
 		try
 		{
 			Resource subject = model.createResource(subjectString);
 			Property predicate = model.createProperty(tag);
-			Resource object = model.createResource(attrName);
-			connect(subject, predicate, object, model);
+			Literal object = model.createTypedLiteral(attrValue);
+			model.add(subject, predicate, object);
+			model.createStatement(subject, predicate, object);
 		}
 		catch (Exception e)
 		{
 			throw new RuntimeException(e);
 		}
-		return stringWriter.toString();
 	}
 
-	private static Statement connect(Resource subject, Property predicate, Resource object, Model model)
+	public void convertIRIToRdf(String subjectString, String tag, String attrValue, Model model)
 	{
-		model.add(subject, predicate, object);
-		return model.createStatement(subject, predicate, object);
+		try
+		{
+			Resource subject = model.createResource(subjectString);
+			Property predicate = model.createProperty(tag);
+			Resource object = model.createResource(attrValue);
+			model.add(subject, predicate, object);
+			model.createStatement(subject, predicate, object);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
