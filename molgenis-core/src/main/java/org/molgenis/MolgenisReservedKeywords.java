650664569915c5581d557e2f0a62286fa98412ce
tommydeboer <tommydeboer4@gmail.com>
2016-11-02 08:31:51 +0100

    rename keyword class

--- molgenis-core/src/main/java/org/molgenis/MolgenisReservedKeywords.java
+++ molgenis-core/src/main/java/org/molgenis/MolgenisReservedKeywords.java
@@ -5,21 +5,10 @@ import com.google.common.collect.Sets;
 import java.util.Set;
 
 /**
- * Collection of reserved keywords used by different languages.
+ * Collection of reserved keywords used by MOLGENIS.
  */
-public class ReservedKeywords
+public class MolgenisReservedKeywords
 {
-	// http://www.w3schools.com/js/js_reserved.asp
-	// Case sensitive
-	public static final Set<String> JAVASCRIPT_KEYWORDS = Sets
-			.newHashSet("abstract", "arguments", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
-					"continue", "debugger", "default", "delete", "do", "double", "else", "enum", "eval", "export",
-					"extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements",
-					"import", "in", "instanceof", "int", "interface", "let", "long", "native", "new", "null", "package",
-					"private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized",
-					"this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while",
-					"with", "yield");
-
 	// Case sensitive
 	public static final Set<String> MOLGENIS_KEYWORDS = Sets
 			.newHashSet("login", "logout", "csv", "entities", "attributes", "base", "exist", "meta");
