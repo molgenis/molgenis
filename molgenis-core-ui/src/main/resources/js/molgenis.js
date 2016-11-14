074e3b8e1a53e68a193faccfcfa4eaaf112127f5
dennishendriksen <d.hendriksen@umcg.nl>
2016-11-14 11:13:34 +0100

    Fix #5516 Categorical mref filtering displays dropdown instead of checkboxes

--- molgenis-core-ui/src/main/resources/js/molgenis.js
+++ molgenis-core-ui/src/main/resources/js/molgenis.js
@@ -294,6 +294,7 @@ function createInput(attr, attrs, val, lbl) {
 		var $input = createBasicInput('radio', attrs, val);
 		return label.append($input).append(val ? 'True' : 'False');
 	case 'CATEGORICAL':
+        case 'CATEGORICAL_MREF':
 		var label = $('<label>');
 		var $input = createBasicInput('checkbox', attrs, val);
 		return $('<div class="checkbox">').append(label.append($input).append(lbl));
@@ -342,7 +343,6 @@ function createInput(attr, attrs, val, lbl) {
 	case 'ENUM':
 	case 'SCRIPT':
 		return createBasicInput('text', attrs, val).addClass('form-control');
-	case 'CATEGORICAL_MREF':
 	case 'MREF':
 	case 'XREF':
 		return createBasicInput('hidden', attrs, val).addClass('form-control');
