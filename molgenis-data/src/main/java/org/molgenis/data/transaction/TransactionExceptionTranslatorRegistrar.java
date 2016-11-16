835abe5ab539e9c51d5a259fed35bdc1f7d119f0
dennishendriksen <d.hendriksen@umcg.nl>
2016-11-16 15:42:36 +0100

    Fix #5523 Unfriendly error message for deferred foreign key violation

---
+++ molgenis-data/src/main/java/org/molgenis/data/transaction/TransactionExceptionTranslatorRegistrar.java
@@ -0,0 +1,36 @@
+package org.molgenis.data.transaction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Discovers {@link TransactionExceptionTranslator TransactionExceptionTranslators} beans and registers them with the
+ * {@link TransactionExceptionTranslatorRegistry}.
+ */
+@Component
+public class TransactionExceptionTranslatorRegistrar
+{
+	private final TransactionExceptionTranslatorRegistry transactionExceptionTranslatorRegistry;
+
+	@Autowired
+	public TransactionExceptionTranslatorRegistrar(
+			TransactionExceptionTranslatorRegistry transactionExceptionTranslatorRegistry)
+	{
+		this.transactionExceptionTranslatorRegistry = requireNonNull(transactionExceptionTranslatorRegistry);
+	}
+
+	/**
+	 * Registers all {@link TransactionExceptionTranslator TransactionExceptionTranslators} in the given application
+	 * context with the {@link TransactionExceptionTranslatorRegistry}.
+	 *
+	 * @param applicationContext application context
+	 */
+	public void register(ApplicationContext applicationContext)
+	{
+		applicationContext.getBeansOfType(TransactionExceptionTranslator.class).values()
+				.forEach(transactionExceptionTranslatorRegistry::register);
+	}
+}
