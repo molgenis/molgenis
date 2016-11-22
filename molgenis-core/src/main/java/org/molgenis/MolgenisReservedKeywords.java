2d47b17e4cb037a5320306d785814df87eec10e4
jjettenn <jjettenn@gmail.com>
2016-11-22 12:26:27 +0100

    remove unnecessary code

--- molgenis-core/src/main/java/org/molgenis/MolgenisReservedKeywords.java
+++ molgenis-core/src/main/java/org/molgenis/MolgenisReservedKeywords.java
@@ -14,13 +14,4 @@ public class MolgenisReservedKeywords
 	 */
 	public static final Set<String> MOLGENIS_KEYWORDS = Sets
 			.newHashSet("login", "logout", "csv", "base", "exist", "meta");
-
-	/**
-	 * Elasticsearch
-	 * <p>
-	 * "type"
-	 * 1. Issue reported in MOLGENIS: Sort Data type results in error #5573
-	 * 2. Issue reported in elasticsearch: Query with a search value and a sort on field name type, results in error. #21725
-	 */
-	public static final Set<String> ELASTICSEARCH_1_7_3_KEYWORDS = Sets.newHashSet("type");
 }
