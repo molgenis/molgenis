f9ee594269e61c6838048bc8161c129050f3fd55
Fleur Kelpin <f.kelpin@umcg.nl>
2016-11-23 11:48:50 +0100

    Return 404 for the case where the file is no longer available.

--- molgenis-gavin/src/main/java/org/molgenis/gavin/controller/GavinController.java
+++ molgenis-gavin/src/main/java/org/molgenis/gavin/controller/GavinController.java
@@ -1,7 +1,6 @@
 package org.molgenis.gavin.controller;
 
 import org.molgenis.data.DataService;
-import org.molgenis.data.MolgenisDataException;
 import org.molgenis.file.FileStore;
 import org.molgenis.gavin.job.GavinJob;
 import org.molgenis.gavin.job.GavinJobExecution;
@@ -14,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -24,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -141,15 +142,15 @@ public class GavinController extends MolgenisPluginController
 	@RequestMapping(value = "/download/{jobIdentifier}", method = GET, produces = APPLICATION_OCTET_STREAM_VALUE)
 	@ResponseBody
 	public FileSystemResource download(HttpServletResponse response,
-			@PathVariable(value = "jobIdentifier") String jobIdentifier)
+			@PathVariable(value = "jobIdentifier") String jobIdentifier) throws FileNotFoundException
 	{
 		GavinJobExecution jobExecution = dataService
 				.findOneById(GAVIN_JOB_EXECUTION, jobIdentifier, GavinJobExecution.class);
 		File file = fileStore.getFile(GAVIN_APP + separator + jobIdentifier + separator + "gavin-result.vcf");
 		if (!file.exists())
 		{
-			LOG.warn(format("File {0} not found for job {1}", file.getName(), jobIdentifier));
-			throw new MolgenisDataException("No output file found for this job.");
+			LOG.warn("No result file found for job {}", jobIdentifier);
+			throw new FileNotFoundException("No result file found for this job. Results are removed every night.");
 		}
 		response.setHeader("Content-Disposition", format("inline; filename=\"{0}\"", jobExecution.getFilename()));
 		return new FileSystemResource(file);
@@ -162,22 +163,28 @@ public class GavinController extends MolgenisPluginController
 	 * @param jobIdentifier GAVIN_APP of the annotation job
 	 * @return {@link FileSystemResource} with the annotated file
 	 */
-	@RequestMapping(value = "/error/{jobIdentifier}", method = GET, produces = APPLICATION_OCTET_STREAM_VALUE)
+	@RequestMapping(value = "/error/{jobIdentifier}", method = GET)
 	@ResponseBody
-	public FileSystemResource downloadErrorReport(HttpServletResponse response,
-			@PathVariable(value = "jobIdentifier") String jobIdentifier)
+	public Resource downloadErrorReport(HttpServletResponse response,
+			@PathVariable(value = "jobIdentifier") String jobIdentifier) throws FileNotFoundException
 	{
 		GavinJobExecution jobExecution = dataService
 				.findOneById(GAVIN_JOB_EXECUTION, jobIdentifier, GavinJobExecution.class);
-		File file = fileStore.getFile(GAVIN_APP + separator + jobIdentifier + separator + "error.txt");
-		if (!file.exists())
-		{
-			LOG.warn(format("File {0} not found for job {1}", file.getName(), jobIdentifier));
-			throw new MolgenisDataException("No output file found for this job.");
-		}
 		response.setHeader("Content-Disposition",
 				format("inline; filename=\"{0}-error.txt\"", jobExecution.getFilename()));
+		final File file = fileStore.getFile(GAVIN_APP + separator + jobIdentifier + separator + "error.txt");
+		if (!file.exists())
+		{
+			LOG.warn("No error file found for job {}", jobIdentifier);
+			throw new FileNotFoundException("No error report found for this job. Results are removed every night.");
+		}
 		return new FileSystemResource(file);
+	}
+
+	@ExceptionHandler(value = FileNotFoundException.class)
+	public void handleFileNotFound(FileNotFoundException ex, HttpServletResponse res) throws IOException
+	{
+		res.sendError(404, ex.getMessage());
 	}
 
 	@ExceptionHandler(RuntimeException.class)
