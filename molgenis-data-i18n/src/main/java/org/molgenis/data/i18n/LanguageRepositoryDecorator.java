ce33c358a1452b1ed890c5d1d66c359c3df08ffe
dennishendriksen <d.hendriksen@umcg.nl>
2016-11-29 11:18:06 +0100

    Fix #5647 query() returns query with decorated repository instead of this repository

--- molgenis-data-i18n/src/main/java/org/molgenis/data/i18n/LanguageRepositoryDecorator.java
+++ molgenis-data-i18n/src/main/java/org/molgenis/data/i18n/LanguageRepositoryDecorator.java
@@ -1,24 +1,19 @@
 package org.molgenis.data.i18n;
 
-import org.molgenis.data.*;
-import org.molgenis.data.QueryRule.Operator;
-import org.molgenis.data.aggregation.AggregateQuery;
-import org.molgenis.data.aggregation.AggregateResult;
+import org.molgenis.data.AbstractRepositoryDecorator;
+import org.molgenis.data.MolgenisDataException;
+import org.molgenis.data.Query;
+import org.molgenis.data.Repository;
 import org.molgenis.data.i18n.model.Language;
-import org.molgenis.data.meta.model.EntityType;
+import org.molgenis.data.support.QueryImpl;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
-public class LanguageRepositoryDecorator implements Repository<Language>
+public class LanguageRepositoryDecorator extends AbstractRepositoryDecorator<Language>
 {
 	private final Repository<Language> decorated;
 	private final LanguageService languageService;
@@ -30,116 +25,15 @@ public class LanguageRepositoryDecorator implements Repository<Language>
 	}
 
 	@Override
-	public Iterator<Language> iterator()
+	protected Repository<Language> delegate()
 	{
-		return decorated.iterator();
-	}
-
-	@Override
-	public void forEachBatched(Fetch fetch, Consumer<List<Language>> consumer, int batchSize)
-	{
-		decorated.forEachBatched(fetch, consumer, batchSize);
-	}
-
-	@Override
-	public void close() throws IOException
-	{
-		decorated.close();
-	}
-
-	@Override
-	public Set<RepositoryCapability> getCapabilities()
-	{
-		return decorated.getCapabilities();
-	}
-
-	@Override
-	public Set<Operator> getQueryOperators()
-	{
-		return decorated.getQueryOperators();
-	}
-
-	@Override
-	public String getName()
-	{
-		return decorated.getName();
-	}
-
-	public EntityType getEntityType()
-	{
-		return decorated.getEntityType();
-	}
-
-	@Override
-	public long count()
-	{
-		return decorated.count();
+		return decorated;
 	}
 
 	@Override
 	public Query<Language> query()
 	{
-		return decorated.query();
-	}
-
-	@Override
-	public long count(Query<Language> q)
-	{
-		return decorated.count(q);
-	}
-
-	@Override
-	public Stream<Language> findAll(Query<Language> q)
-	{
-		return decorated.findAll(q);
-	}
-
-	@Override
-	public Language findOne(Query<Language> q)
-	{
-		return decorated.findOne(q);
-	}
-
-	@Override
-	public Language findOneById(Object id)
-	{
-		return decorated.findOneById(id);
-	}
-
-	@Override
-	public Language findOneById(Object id, Fetch fetch)
-	{
-		return decorated.findOneById(id, fetch);
-	}
-
-	@Override
-	public Stream<Language> findAll(Stream<Object> ids)
-	{
-		return decorated.findAll(ids);
-	}
-
-	@Override
-	public Stream<Language> findAll(Stream<Object> ids, Fetch fetch)
-	{
-		return decorated.findAll(ids, fetch);
-	}
-
-	@Override
-	public AggregateResult aggregate(AggregateQuery aggregateQuery)
-	{
-		return decorated.aggregate(aggregateQuery);
-	}
-
-	@Override
-	public void update(Language entity)
-	{
-		decorated.update(entity);
-	}
-
-	@Override
-	public void update(Stream<Language> entities)
-	{
-		decorated.update(entities);
+		return new QueryImpl<>(this);
 	}
 
 	@Override
