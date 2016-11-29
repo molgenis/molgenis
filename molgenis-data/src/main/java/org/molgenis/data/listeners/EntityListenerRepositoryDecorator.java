b518a52ac21a5734896cf100d065d437db341a0e
dennishendriksen <d.hendriksen@umcg.nl>
2016-11-29 10:54:51 +0100

    Fix #5647 query() returns query with decorated repository instead of this repository

--- molgenis-data/src/main/java/org/molgenis/data/listeners/EntityListenerRepositoryDecorator.java
+++ molgenis-data/src/main/java/org/molgenis/data/listeners/EntityListenerRepositoryDecorator.java
@@ -1,21 +1,16 @@
 package org.molgenis.data.listeners;
 
-import org.molgenis.data.*;
-import org.molgenis.data.QueryRule.Operator;
-import org.molgenis.data.aggregation.AggregateQuery;
-import org.molgenis.data.aggregation.AggregateResult;
-import org.molgenis.data.meta.model.EntityType;
+import org.molgenis.data.AbstractRepositoryDecorator;
+import org.molgenis.data.Entity;
+import org.molgenis.data.Query;
+import org.molgenis.data.Repository;
+import org.molgenis.data.support.QueryImpl;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
-public class EntityListenerRepositoryDecorator implements Repository<Entity>
+public class EntityListenerRepositoryDecorator extends AbstractRepositoryDecorator<Entity>
 {
 	private final Repository<Entity> decoratedRepository;
 	private final EntityListenersService entityListenersService;
@@ -29,104 +24,15 @@ public class EntityListenerRepositoryDecorator implements Repository<Entity>
 	}
 
 	@Override
-	public Iterator<Entity> iterator()
+	protected Repository<Entity> delegate()
 	{
-		return decoratedRepository.iterator();
-	}
-
-	@Override
-	public void forEachBatched(Fetch fetch, Consumer<List<Entity>> consumer, int batchSize)
-	{
-		decoratedRepository.forEachBatched(fetch, consumer, batchSize);
-	}
-
-	@Override
-	public void close() throws IOException
-	{
-		decoratedRepository.close();
-	}
-
-	@Override
-	public Set<RepositoryCapability> getCapabilities()
-	{
-		return decoratedRepository.getCapabilities();
-	}
-
-	@Override
-	public Set<Operator> getQueryOperators()
-	{
-		return decoratedRepository.getQueryOperators();
-	}
-
-	@Override
-	public String getName()
-	{
-		return decoratedRepository.getName();
-	}
-
-	public EntityType getEntityType()
-	{
-		return decoratedRepository.getEntityType();
-	}
-
-	@Override
-	public long count()
-	{
-		return decoratedRepository.count();
+		return decoratedRepository;
 	}
 
 	@Override
 	public Query<Entity> query()
 	{
-		return decoratedRepository.query();
-	}
-
-	@Override
-	public long count(Query<Entity> q)
-	{
-		return decoratedRepository.count(q);
-	}
-
-	@Override
-	public Stream<Entity> findAll(Query<Entity> q)
-	{
-		return decoratedRepository.findAll(q);
-	}
-
-	@Override
-	public Entity findOne(Query<Entity> q)
-	{
-		return decoratedRepository.findOne(q);
-	}
-
-	@Override
-	public Entity findOneById(Object id)
-	{
-		return decoratedRepository.findOneById(id);
-	}
-
-	@Override
-	public Entity findOneById(Object id, Fetch fetch)
-	{
-		return decoratedRepository.findOneById(id, fetch);
-	}
-
-	@Override
-	public Stream<Entity> findAll(Stream<Object> ids)
-	{
-		return decoratedRepository.findAll(ids);
-	}
-
-	@Override
-	public Stream<Entity> findAll(Stream<Object> ids, Fetch fetch)
-	{
-		return decoratedRepository.findAll(ids, fetch);
-	}
-
-	@Override
-	public AggregateResult aggregate(AggregateQuery aggregateQuery)
-	{
-		return decoratedRepository.aggregate(aggregateQuery);
+		return new QueryImpl<>(this);
 	}
 
 	@Override
@@ -141,47 +47,5 @@ public class EntityListenerRepositoryDecorator implements Repository<Entity>
 	{
 		entities = entityListenersService.updateEntities(decoratedRepository.getName(), entities);
 		decoratedRepository.update(entities);
-	}
-
-	@Override
-	public void delete(Entity entity)
-	{
-		decoratedRepository.delete(entity);
-	}
-
-	@Override
-	public void delete(Stream<Entity> entities)
-	{
-		decoratedRepository.delete(entities);
-	}
-
-	@Override
-	public void deleteById(Object id)
-	{
-		decoratedRepository.deleteById(id);
-	}
-
-	@Override
-	public void deleteAll(Stream<Object> ids)
-	{
-		decoratedRepository.deleteAll(ids);
-	}
-
-	@Override
-	public void deleteAll()
-	{
-		decoratedRepository.deleteAll();
-	}
-
-	@Override
-	public void add(Entity entity)
-	{
-		decoratedRepository.add(entity);
-	}
-
-	@Override
-	public Integer add(Stream<Entity> entities)
-	{
-		return decoratedRepository.add(entities);
 	}
 }
