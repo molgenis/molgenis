faf0964e57cd5557d0adaae6ad98c7f2b9994c4d
dennishendriksen <d.hendriksen@umcg.nl>
2016-12-01 22:05:44 +0100

    Add note why list instead of map is used

--- molgenis-data/src/main/java/org/molgenis/data/meta/system/SystemPackageRegistry.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/system/SystemPackageRegistry.java
@@ -17,6 +17,7 @@ public class SystemPackageRegistry
 {
 	private final Logger LOG = LoggerFactory.getLogger(SystemPackageRegistry.class);
 
+	// note: a list instead of map is used since system packages might not be initialized when added to the registry
 	private final List<SystemPackage> systemPackages;
 
 	public SystemPackageRegistry()
