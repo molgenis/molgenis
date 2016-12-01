2317c7bee6100dbb972da3837a26987026e34ab7
dennishendriksen <d.hendriksen@umcg.nl>
2016-12-01 22:04:27 +0100

    Fix #5683 Fix #5685 Missing package validation checks

--- molgenis-data/src/main/java/org/molgenis/data/meta/PackageRepositoryDecorator.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/PackageRepositoryDecorator.java
@@ -2,11 +2,11 @@ package org.molgenis.data.meta;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.TreeTraverser;
-import org.molgenis.data.*;
+import org.molgenis.data.AbstractRepositoryDecorator;
+import org.molgenis.data.DataService;
+import org.molgenis.data.Repository;
 import org.molgenis.data.meta.model.EntityType;
-import org.molgenis.data.meta.model.EntityTypeMetadata;
 import org.molgenis.data.meta.model.Package;
-import org.molgenis.data.meta.model.PackageMetadata;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -14,7 +14,6 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import static com.google.common.collect.Lists.reverse;
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.meta.model.EntityTypeMetadata.ENTITY_TYPE_META_DATA;
 
@@ -36,23 +35,6 @@ public class PackageRepositoryDecorator extends AbstractRepositoryDecorator<Pack
 	protected Repository<Package> delegate()
 	{
 		return decoratedRepo;
-	}
-
-	@Override
-	public void update(Package entity)
-	{
-		validateUpdateAllowed(entity);
-		decoratedRepo.update(entity);
-	}
-
-	@Override
-	public void update(Stream<Package> entities)
-	{
-		decoratedRepo.update(entities.filter(entity ->
-		{
-			validateUpdateAllowed(entity);
-			return true;
-		}));
 	}
 
 	@Override
@@ -85,45 +67,8 @@ public class PackageRepositoryDecorator extends AbstractRepositoryDecorator<Pack
 		forEach(this::deletePackage);
 	}
 
-	@Override
-	public void add(Package entity)
-	{
-		validateAddAllowed(entity);
-		decoratedRepo.add(entity);
-	}
-
-	@Override
-	public Integer add(Stream<Package> entities)
-	{
-		return decoratedRepo.add(entities.filter(entity ->
-		{
-			validateAddAllowed(entity);
-			return true;
-		}));
-	}
-
-	private void validateAddAllowed(Package package_)
-	{
-		Entity existingEntity = findOneById(package_.getIdValue(), new Fetch().field(PackageMetadata.FULL_NAME));
-		if (existingEntity != null)
-		{
-			throw new MolgenisDataException(format("Adding existing package [%s] is not allowed",
-					package_.getString(EntityTypeMetadata.FULL_NAME)));
-		}
-	}
-
-	private static void validateUpdateAllowed(Package package_)
-	{
-		if (MetaUtils.isSystemPackage(package_))
-		{
-			throw new MolgenisDataException(format("Updating system package [%s] is not allowed", package_.getName()));
-		}
-	}
-
 	private void deletePackage(Package package_)
 	{
-		validateDeleteAllowed(package_);
-
 		// recursively delete sub packages
 		getPackageTreeTraversal(package_).forEach(this::deletePackageAndContents);
 	}
@@ -137,28 +82,6 @@ public class PackageRepositoryDecorator extends AbstractRepositoryDecorator<Pack
 
 		// delete row from package table
 		decoratedRepo.delete(package_);
-	}
-
-	/**
-	 * Deleting a package is allowed if:
-	 * <ul>
-	 * <li>This package and descending packages do not contain a system package</li>
-	 * <li>This package and descending packages do not contain a system package_</li>
-	 * <li>User has {@link org.molgenis.security.core.Permission#WRITEMETA} permission on all entities in this package
-	 * and descending packages</li>
-	 * <li>There are no dependencies to entities in this package and descendant packages from entities in other packages
-	 * </li>
-	 * </ul>
-	 *
-	 * @param package_ package
-	 */
-	private static void validateDeleteAllowed(Package package_)
-	{
-		// This package and descending packages do not contain a system package
-		if (MetaUtils.isSystemPackage(package_))
-		{
-			throw new MolgenisDataException(format("Deleting system package [%s] is not allowed", package_.getName()));
-		}
 	}
 
 	private static Stream<Package> getPackageTreeTraversal(Package package_)
