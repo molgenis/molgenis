df2434982d873b51d083254a28c06a288a04b9cb
Dennis Hendriksen <d.hendriksen@umcg.nl>
2016-12-19 16:45:06 +0100

    Add add/update and add EMX import integration tests

--- molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/ImportServiceIT.java
+++ molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/ImportServiceIT.java
@@ -31,6 +31,7 @@ import static java.util.Collections.emptyMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toSet;
 import static org.molgenis.data.DatabaseAction.ADD;
+import static org.molgenis.data.DatabaseAction.ADD_UPDATE_EXISTING;
 import static org.molgenis.data.meta.DefaultPackage.PACKAGE_DEFAULT;
 import static org.molgenis.data.meta.model.Package.PACKAGE_SEPARATOR;
 import static org.molgenis.security.core.runas.RunAsSystemProxy.runAsSystem;
@@ -67,34 +68,35 @@ public class ImportServiceIT extends AbstractTestNGSpringContextTests
 	{
 		List<Object[]> data = new ArrayList<>();
 
-		data.add(createData("it_emx_datatypes.xlsx", asList("it", "emx", "datatypes"),
+		data.add(createAddData("it_emx_datatypes.xlsx", asList("it", "emx", "datatypes"),
 				ImmutableMap.<String, Integer>builder().put("TypeTestRef", 5).put("TypeTest", 38).build(),
 				ImmutableSet.of("Person", "TypeTestRef", "Location", "TypeTest")));
 
-		data.add(createData("it_emx_deep_nesting.xlsx", asList("it", "deep"),
+		data.add(createAddData("it_emx_deep_nesting.xlsx", asList("it", "deep"),
 				ImmutableMap.<String, Integer>builder().put("TestCategorical_1", 50).put("TestMref_1", 50)
 						.put("TestXref_2", 50).put("TestXref_1", 50)
 						.put("advanced" + PACKAGE_SEPARATOR + "p" + PACKAGE_SEPARATOR + "TestEntity_2", 50).build(),
 				ImmutableSet.of("TestCategorical_1", "TestMref_1", "TestXref_2", "TestXref_1", "TestEntity_0",
 						"advanced_TestEntity_1", "advanced_p_TestEntity_2")));
 
-		data.add(createData("it_emx_lookup_attribute.xlsx", asList("it", "emx", "lookupattribute"),
+		data.add(createAddData("it_emx_lookup_attribute.xlsx", asList("it", "emx", "lookupattribute"),
 				ImmutableMap.<String, Integer>builder().put("Ref1", 2).put("Ref2", 2).put("Ref3", 2).put("Ref4", 2)
 						.put("Ref5", 2).build(), ImmutableSet
 						.of("AbstractTop", "AbstractMiddle", "Ref1", "Ref2", "Ref3", "Ref4", "Ref5",
 								"TestLookupAttributes")));
 
-		data.add(createData("it_emx_autoid.xlsx", asList("it", "emx", "autoid"), ImmutableMap.of("testAutoId", 4),
+		data.add(createAddData("it_emx_autoid.xlsx", asList("it", "emx", "autoid"), ImmutableMap.of("testAutoId", 4),
 				ImmutableSet.of("testAutoId")));
 
-		data.add(createData("it_emx_onetomany.xlsx", asList("it", "emx", "onetomany"),
+		data.add(createAddData("it_emx_onetomany.xlsx", asList("it", "emx", "onetomany"),
 				ImmutableMap.<String, Integer>builder().put("book", 4).put("author", 2).put("node", 4).build(),
 				ImmutableSet.of("book", "author", "node")));
 
-		data.add(createData("it_emx_self_references.xlsx", asList("it", "emx", "selfreferences"),
+		data.add(createAddData("it_emx_self_references.xlsx", asList("it", "emx", "selfreferences"),
 				ImmutableMap.of("PersonTest", 11), ImmutableSet.of("PersonTest")));
 
-		data.add(createData("it_emx_tags.xlsx", asList("it", "emx", "tags"), emptyMap(), ImmutableSet.of("TagEntity")));
+		data.add(createAddData("it_emx_tags.xlsx", asList("it", "emx", "tags"), emptyMap(),
+				ImmutableSet.of("TagEntity")));
 
 		return data.iterator();
 	}
@@ -104,7 +106,8 @@ public class ImportServiceIT extends AbstractTestNGSpringContextTests
 	{
 		runAsSystem(() ->
 		{
-			FileRepositoryCollection repoCollection = fileRepositoryCollectionFactory.createFileRepositoryCollection(file);
+			FileRepositoryCollection repoCollection = fileRepositoryCollectionFactory
+					.createFileRepositoryCollection(file);
 			ImportService importService = importServiceFactory.getImportService(file, repoCollection);
 
 			EntityImportReport importReport = importService.doImport(repoCollection, ADD, PACKAGE_DEFAULT);
@@ -113,8 +116,66 @@ public class ImportServiceIT extends AbstractTestNGSpringContextTests
 		});
 	}
 
-	private static Object[] createData(String fileName, List<String> packageTokens, Map<String, Integer> entityCountMap,
-			Set<String> entityTypeNames)
+	@DataProvider(name = "doImportEmxAddUpdateProvider")
+	public Iterator<Object[]> doImportEmxAddUpdateProvider()
+	{
+		List<Object[]> data = new ArrayList<>();
+
+		data.add(createUpdateData("it_emx_addupdate.xlsx", "it_emx_addupdate-addupdate.xlsx",
+				asList("it", "emx", "addupdate"),
+				ImmutableMap.<String, Integer>builder().put("TestAddUpdate", 3).build(), Collections.emptySet()));
+
+		return data.iterator();
+	}
+
+	@Test(dataProvider = "doImportEmxAddUpdateProvider")
+	public void testDoImportAddUpdateEmx(File file, File addUpdateFile, Map<String, Integer> entityCountMap,
+			Set<String> addedEntityTypes)
+	{
+		executeAddUpdateOrUpdateTest(file, addUpdateFile, entityCountMap, addedEntityTypes);
+	}
+
+	private void executeAddUpdateOrUpdateTest(File file, File addUpdateFile, Map<String, Integer> entityCountMap,
+			Set<String> addedEntityTypes)
+	{
+		runAsSystem(() ->
+		{
+			FileRepositoryCollection addRepoCollection = fileRepositoryCollectionFactory
+					.createFileRepositoryCollection(file);
+			ImportService addImportService = importServiceFactory.getImportService(file, addRepoCollection);
+			addImportService.doImport(addRepoCollection, ADD, PACKAGE_DEFAULT);
+
+			FileRepositoryCollection addUpdateRepoCollection = fileRepositoryCollectionFactory
+					.createFileRepositoryCollection(addUpdateFile);
+			ImportService addUpdateImportService = importServiceFactory
+					.getImportService(addUpdateFile, addUpdateRepoCollection);
+			EntityImportReport importReport = addUpdateImportService
+					.doImport(addUpdateRepoCollection, ADD_UPDATE_EXISTING, PACKAGE_DEFAULT);
+
+			validateImportReport(importReport, entityCountMap, addedEntityTypes);
+		});
+	}
+
+	@DataProvider(name = "doImportEmxUpdateProvider")
+	public Iterator<Object[]> doImportEmxUpdateProvider()
+	{
+		List<Object[]> data = new ArrayList<>();
+
+		data.add(createUpdateData("it_emx_update.xlsx", "it_emx_update-update.xlsx", asList("it", "emx", "update"),
+				ImmutableMap.<String, Integer>builder().put("TestUpdate", 2).build(), Collections.emptySet()));
+
+		return data.iterator();
+	}
+
+	@Test(dataProvider = "doImportEmxUpdateProvider")
+	public void testDoImportUpdateEmx(File file, File updateFile, Map<String, Integer> entityCountMap,
+			Set<String> addedEntityTypes)
+	{
+		executeAddUpdateOrUpdateTest(file, updateFile, entityCountMap, addedEntityTypes);
+	}
+
+	private static Object[] createAddData(String fileName, List<String> packageTokens,
+			Map<String, Integer> entityCountMap, Set<String> entityTypeNames)
 	{
 		File file = getFile("/xls/" + fileName);
 		String packageName = String.join(PACKAGE_SEPARATOR, packageTokens);
@@ -123,6 +184,19 @@ public class ImportServiceIT extends AbstractTestNGSpringContextTests
 		Set<String> entityTypeFullyQualifiedNames = entityTypeNames.stream()
 				.map(entityName -> packageName + PACKAGE_SEPARATOR + entityName).collect(toSet());
 		return new Object[] { file, entityTypeCountMap, entityTypeFullyQualifiedNames };
+	}
+
+	private static Object[] createUpdateData(String fileName, String updateFileName, List<String> packageTokens,
+			Map<String, Integer> entityCountMap, Set<String> entityTypeNames)
+	{
+		File addFile = getFile("/xls/" + fileName);
+		File updateFile = getFile("/xls/" + updateFileName);
+		String packageName = String.join(PACKAGE_SEPARATOR, packageTokens);
+		Map<String, Object> entityTypeCountMap = entityCountMap.entrySet().stream().collect(
+				Collectors.toMap(entry -> packageName + PACKAGE_SEPARATOR + entry.getKey(), Map.Entry::getValue));
+		Set<String> entityTypeFullyQualifiedNames = entityTypeNames.stream()
+				.map(entityName -> packageName + PACKAGE_SEPARATOR + entityName).collect(toSet());
+		return new Object[] { addFile, updateFile, entityTypeCountMap, entityTypeFullyQualifiedNames };
 	}
 
 	private static void validateImportReport(EntityImportReport importReport, Map<String, Integer> entityTypeCountMap,
