78d53927c7630728aeeba7743740376d443b0d7b
Fleur Kelpin <f.kelpin@umcg.nl>
2016-12-20 13:39:08 +0100

    Move MailSenderProperty to mail package, if it's in settings package it ends up in the settings pulldown

--- molgenis-mail/src/main/java/org/molgenis/mail/MailSenderPropertyType.java
+++ molgenis-mail/src/main/java/org/molgenis/mail/MailSenderPropertyType.java
@@ -2,28 +2,27 @@ package org.molgenis.mail;
 
 import org.molgenis.data.meta.PropertyType;
 import org.molgenis.data.meta.SystemEntityType;
-import org.molgenis.data.settings.SettingsPackage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.meta.model.Package.PACKAGE_SEPARATOR;
-import static org.molgenis.data.settings.SettingsPackage.PACKAGE_SETTINGS;
+import static org.molgenis.mail.MailPackage.PACKAGE_MAIL;
 
 @Component
 public class MailSenderPropertyType extends SystemEntityType
 {
-	private static final String SIMPLE_NAME = "MailSenderProp";
-	public static final String MAIL_SENDER_PROPERTY = PACKAGE_SETTINGS + PACKAGE_SEPARATOR + SIMPLE_NAME;
+	private static final String SIMPLE_NAME = "MailSenderProperty";
+	public static final String MAIL_SENDER_PROPERTY = PACKAGE_MAIL + PACKAGE_SEPARATOR + SIMPLE_NAME;
 
-	private final SettingsPackage settingsPackage;
+	private final MailPackage mailPackage;
 	private final PropertyType propertyType;
 
 	@Autowired
-	public MailSenderPropertyType(SettingsPackage settingsPackage, PropertyType propertyType)
+	public MailSenderPropertyType(MailPackage mailPackage, PropertyType propertyType)
 	{
-		super(SIMPLE_NAME, PACKAGE_SETTINGS);
-		this.settingsPackage = requireNonNull(settingsPackage);
+		super(SIMPLE_NAME, PACKAGE_MAIL);
+		this.mailPackage = requireNonNull(mailPackage);
 		this.propertyType = requireNonNull(propertyType);
 	}
 
@@ -33,7 +32,7 @@ public class MailSenderPropertyType extends SystemEntityType
 		setLabel("Mail sender properties.");
 		setDescription(
 				"See https://javamail.java.net/nonav/docs/api/ for a description of the properties you can use.");
-		setPackage(settingsPackage);
+		setPackage(mailPackage);
 		setExtends(propertyType);
 	}
 }
