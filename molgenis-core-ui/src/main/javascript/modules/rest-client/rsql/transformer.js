7f22e43a68796455dab494e6f4412f24f04bda3a
Fleur Kelpin <f.kelpin@umcg.nl>
2017-01-05 23:41:06 +0100

    Map parsed rsql to filter model

--- molgenis-core-ui/src/main/javascript/modules/rest-client/rsql/transformer.js
+++ molgenis-core-ui/src/main/javascript/modules/rest-client/rsql/transformer.js
@@ -14,4 +14,71 @@ function findSelector(constraint) {
     return constraint.selector || findSelector(constraint.operands[0])
 }
 
-export {groupBySelector}
+function toRange(attribute, constraint) {
+    return {
+        attribute,
+        type: 'RANGE',
+        lines: constraint.operands.map(toRangeLine)
+    }
+}
+
+function toRangeLine(constraint) {
+    return (constraint.operands || [constraint]).reduce((acc, operand) => {
+        return {...acc, [operand.comparison === "=ge=" ? "from" : "to"]: operand.arguments}
+    }, {})
+}
+
+function toText(attribute, constraint) {
+    return {
+        attribute,
+        type: 'TEXT',
+        lines: constraint.operands.map(o => o.arguments)
+    }
+}
+
+function toSimpleRef(attribute, labels, constraint) {
+    return {
+        'type': 'SIMPLE_REF',
+        'attribute': attribute,
+        'args': (constraint.operands || [constraint]).map(o => {
+            const value = o.arguments
+            return {'label': labels[value], value}
+        })
+    }
+}
+
+function toComplexLine(labels, group) {
+    return {
+        'operator': group.operator,
+        'values': (group.operands || [group.arguments]).map(o => {
+            const value = o.arguments || o
+            return {'label': labels[value], value}
+        })
+    }
+}
+
+function intersperse(arr, sep) {
+    return arr.reduce((a, v) => [...a, v, sep], []).slice(0, -1)
+}
+
+function toComplexRefAnd(labels, constraint) {
+    if (constraint.operator === "OR") {
+        // this is the bottom level
+        return toComplexLine(labels, constraint)
+    }
+    return intersperse((constraint.operands||[constraint]).map(o => toComplexLine(labels, o)), "AND")
+}
+
+function toComplexRefOr(labels, constraint) {
+    return [].concat.apply([], intersperse((constraint.operands||[constraint]).map(o => toComplexRefAnd(labels, o)), "OR"))
+}
+
+function toComplexRef(attribute, labels, constraint) {
+    return {
+        'type': 'COMPLEX_REF',
+        'attribute': attribute,
+        'lines': toComplexRefOr(labels, constraint)
+    }
+}
+
+export {groupBySelector, toRangeLine, toRange, toText, toSimpleRef, toComplexRef, toComplexLine}
--- molgenis-core-ui/src/test/javascript/modules/rest-client/rsql/transformerTest.js
+++ molgenis-core-ui/src/test/javascript/modules/rest-client/rsql/transformerTest.js
@@ -1,4 +1,13 @@
-import {groupBySelector, parser} from "rest-client/rsql";
+import {parser} from "rest-client/rsql";
+import {
+    toRange,
+    toRangeLine,
+    groupBySelector,
+    toText,
+    toSimpleRef,
+    toComplexRef,
+    toComplexLine
+} from "rest-client/rsql/transformer";
 import test from "tape";
 
 test('Test that a single comparison gets mapped', assert => {
@@ -48,10 +57,163 @@ test('Test selector in xref', assert => {
             "comparison": "==",
             "arguments": "ref2"
         }
-
     }
 
     assert.deepEqual(actual, expected);
     assert.end();
 })
 
+test("Test toRangeLine for int values both specified", assert => {
+    const actual = toRangeLine(parser.parse("(count=ge=1;count=le=5)"))
+    const expected = {from: "1", to: "5"}
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toRangeLine for int values one specified", assert => {
+    const actual = toRangeLine(parser.parse("(count=ge=1)"))
+    const expected = {from: "1"}
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toRange two int ranges", assert => {
+    const attribute = {
+        name: "count",
+        label: "count label",
+        type: "INT"
+    }
+    const actual = toRange(attribute, parser.parse("(count=ge=1;count=le=5),(count=ge=8;count=le=10)"))
+    const expected = {
+        type: "RANGE",
+        attribute,
+        lines: [{from: "1", to: "5"}, {from: "8", to: "10"}]
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+
+test("Test toText two string values", assert => {
+    const attribute = {
+        name: "xstring",
+        label: "xstring label",
+        type: "STRING"
+    }
+    const actual = toText(
+        attribute,
+        parser.parse("(xstring=q=str1,xstring=q=str2)")
+    )
+
+    const expected = {
+        'type': 'TEXT',
+        'attribute': attribute,
+        'lines': ['str1', 'str2']
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toSimpleRef one value", assert => {
+    const attribute = {
+        name: "xxref",
+        label: "xxref label",
+        type: "XREF"
+    }
+    const actual = toSimpleRef(attribute, {"ref1": "label1"}, parser.parse("xxref==ref1"))
+    const expected = {
+        'type': 'SIMPLE_REF',
+        'attribute': attribute,
+        'args': [{'label': 'label1', 'value': 'ref1'}]
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toSimpleRef two values", assert => {
+    const attribute = {
+        name: "xxref",
+        label: "xxref label",
+        type: "XREF"
+    }
+    const actual = toSimpleRef(attribute, {"ref1": "label1", "ref2": "label2"},
+        parser.parse("(xxref==ref1,xxref==ref2)"))
+    const expected = {
+        'type': 'SIMPLE_REF',
+        'attribute': attribute,
+        'args': [{'label': 'label1', 'value': 'ref1'}, {'label': 'label2', 'value': 'ref2'}]
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toComplexLine one value selected", assert => {
+    const actual = toComplexLine({"ref1": "label1"}, parser.parse("xmref==ref1"))
+    const expected = {'operator': undefined, 'values': [{'label': 'label1', 'value': 'ref1'}]}
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toComplexRef five lines with random ANDs and ORs", assert => {
+    const attribute = {
+        name: "xmref",
+        label: "xmref label",
+        type: "MREF"
+    }
+
+    const actual = toComplexRef(attribute, {
+            "ref1": "label1",
+            "ref2": "label2",
+            "ref3": "label3",
+            "ref4": "label4",
+            "ref5": "label5"
+        },
+        parser.parse("(((xmref==ref1;xmref==ref2);(xmref==ref3,xmref==ref4)),(xmref==ref5,xmref==ref1),((xmref==ref2;xmref==ref3);(xmref==ref4,xmref==ref5)))"))
+    const expected = {
+        'type': 'COMPLEX_REF',
+        'attribute': attribute,
+        'lines': [
+            {
+                operator: 'AND',
+                values: [{label: 'label1', value: 'ref1'}, {label: 'label2', value: 'ref2'}]
+            },
+            'AND',
+            {
+                operator: 'OR',
+                values: [{label: 'label3', value: 'ref3'}, {label: 'label4', value: 'ref4'}]
+            },
+            'OR',
+            {
+                operator: "OR",
+                values: [{label: 'label5', value: 'ref5'}, {label: 'label1', value: 'ref1'}]
+            },
+            'OR',
+            {
+                operator: 'AND',
+                values: [{label: 'label2', value: 'ref2'}, {label: 'label3', value: 'ref3'}]
+            },
+            'AND',
+            {
+                operator: 'OR',
+                values: [{label: 'label4', value: 'ref4'}, {label: 'label5', value: 'ref5'}]
+            }]
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
+
+test("Test toComplexRef one value selected", assert => {
+    const attribute = {
+        name: "xmref",
+        label: "xmref label",
+        type: "MREF"
+    }
+    const actual = toComplexRef(attribute, {"ref1": "label1"}, parser.parse("xmref==ref1"))
+    const expected = {
+        'type': 'COMPLEX_REF',
+        'attribute': attribute,
+        'lines': [{'operator': undefined, 'values': [{'label': 'label1', 'value': 'ref1'}]}]
+    }
+    assert.deepEqual(actual, expected);
+    assert.end();
+})
