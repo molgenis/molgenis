594786b7c650481d85957087eb2ec6e811ccff34
Fleur Kelpin <f.kelpin@umcg.nl>
2017-01-26 16:16:06 +0100

    (fix) Update human readable form

--- molgenis-core-ui/src/test/javascript/modules/rest-client/rsql/createHumanReadableTest.js
+++ molgenis-core-ui/src/test/javascript/modules/rest-client/rsql/createHumanReadableTest.js
@@ -1,11 +1,10 @@
-import {getHumanReadable} from "rest-client/rsql";
-import {parser} from "rest-client/rsql";
+import {getHumanReadable, parser} from "rest-client/rsql";
 import {groupBySelector} from "rest-client/rsql/transformer";
 import test from "tape";
 
 test("Test translating a simple RSQL string into a human readable string", assert => {
     const actual = getHumanReadable("(name=q=Graz,name=q=Berlin)")
-    const expected = "name is equal to Graz or name is equal to Berlin\n"
+    const expected = "name contains Graz or name contains Berlin\n"
 
     assert.deepEqual(actual, expected);
     assert.end();
@@ -13,7 +12,7 @@ test("Test translating a simple RSQL string into a human readable string", asser
 
 test("Test translating a complex RSQL string into a human readable string", assert => {
     const actual = getHumanReadable("access_fee==false;((country==Netherlands,country==Belgium);country==France);sampleSize=ge=5")
-    const expected = "access_fee is equal to false\ncountry is equal to Netherlands or country is equal to Belgium and country is equal to France\nsampleSize is greater or equal then 5\n"
+    const expected = "access_fee equals false\ncountry equals Netherlands or country equals Belgium and country equals France\nsampleSize is greater than or equal to 5\n"
 
     assert.deepEqual(actual, expected);
     assert.end();
