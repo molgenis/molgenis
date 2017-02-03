d592f1996bb04577417c283e31f6c2a4d0db6e58
bartcharbon <bart.charbon@gmail.com>
2017-02-03 08:16:08 +0100

    Add ID field to the entityTypeFetch

--- molgenis-data/src/main/java/org/molgenis/data/meta/MetaUtils.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/MetaUtils.java
@@ -11,7 +11,7 @@ public class MetaUtils
 	public static Fetch getEntityTypeFetch()
 	{
 		// TODO simplify fetch creation (in this case *all* attributes and expand xref/mrefs)
-		return new Fetch().field(FULL_NAME).field(SIMPLE_NAME).field(PACKAGE).field(LABEL).field(DESCRIPTION)
+		return new Fetch().field(ID).field(FULL_NAME).field(SIMPLE_NAME).field(PACKAGE).field(LABEL).field(DESCRIPTION)
 				.field(ATTRIBUTES).field(IS_ABSTRACT).field(EXTENDS).field(TAGS).field(BACKEND);
 	}
 
