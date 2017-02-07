f2240206e10c272d26deeffae6532a269e1a1d5e
Fleur Kelpin <f.kelpin@umcg.nl>
2017-02-07 22:14:21 +0100

    Fix #5805 Don’t URL encode the arguments when parsing and transforming the filter values. URLencode the rsql parameter value before writing it to the URL query string, but only encode those characters that are strictly necessary to encode in query param values.

--- molgenis-core-ui/src/main/javascript/modules/rest-client/rsql/index.js
+++ molgenis-core-ui/src/main/javascript/modules/rest-client/rsql/index.js
@@ -1,8 +1,23 @@
-import parser from "./parser"
-import transformer from "./transformer"
-import { createRsqlQuery, createRsqlAggregateQuery } from "./createRsqlQuery"
-import { getHumanReadable } from "./createHumanReadable"
+import parser from "./parser";
+import transformer from "./transformer";
+import {
+    createRsqlQuery,
+    createRsqlAggregateQuery,
+    containsRsqlReservedCharacter,
+    rsqlEscape,
+    encodeRsqlValue
+} from "./createRsqlQuery";
+import {getHumanReadable} from "./createHumanReadable";
 
-export { parser, transformer, createRsqlQuery, createRsqlAggregateQuery, getHumanReadable }
+export {
+    parser,
+    transformer,
+    createRsqlQuery,
+    createRsqlAggregateQuery,
+    getHumanReadable,
+    containsRsqlReservedCharacter,
+    rsqlEscape,
+    encodeRsqlValue
+}
 
-export default { parser, transformer, createRsqlQuery, createRsqlAggregateQuery, getHumanReadable }
+export default {parser, transformer, createRsqlQuery, createRsqlAggregateQuery, getHumanReadable, encodeRsqlValue}
---
+++ molgenis-core-ui/src/test/javascript/modules/rest-client/rsql/createRsqlQueryTest.js
@@ -0,0 +1,64 @@
+import {containsRsqlReservedCharacter, rsqlEscape, createRsqlQuery, encodeRsqlValue} from "rest-client/rsql";
+import test from "tape";
+
+test('Test if containsRsqlReservedCharacter checks for reserved characters', assert => {
+    assert.ok(containsRsqlReservedCharacter("abc\"def"), "Checks for \"")
+    assert.ok(containsRsqlReservedCharacter("abc'def"), "Checks for '")
+    assert.ok(containsRsqlReservedCharacter("abc(def"), "Checks for (")
+    assert.ok(containsRsqlReservedCharacter("abc)def"), "Checks for )")
+    assert.ok(containsRsqlReservedCharacter("abc;def"), "Checks for ;")
+    assert.ok(containsRsqlReservedCharacter("abc,def"), "Checks for ,")
+    assert.ok(containsRsqlReservedCharacter("abc=def"), "Checks for =")
+    assert.ok(containsRsqlReservedCharacter("abc!def"), "Checks for !")
+    assert.ok(containsRsqlReservedCharacter("abc~def"), "Checks for ~")
+    assert.ok(containsRsqlReservedCharacter("abc<def"), "Checks for <")
+    assert.ok(containsRsqlReservedCharacter("abc>def"), "Checks for >")
+    assert.ok(!containsRsqlReservedCharacter("@#&"), "Doesn't check for other characters")
+    assert.end()
+})
+
+test('rsqlEscape escapes rsql strings properly', assert => {
+    assert.equals(rsqlEscape("abc\"def"), "'abc\"def'", "Picks ' as delimiter to escape \"")
+    assert.equals(rsqlEscape("abc'def"), "\"abc'def\"", "Picks \" as delimiter to escape '")
+    assert.equals(rsqlEscape("abc=def"), "'abc=def'", "Prefers ' as delimiter")
+    assert.equals(rsqlEscape("abc\"'def"), "'abc\"\\'def'", "Escapes delimiter if it occurs inside the string")
+    assert.end()
+})
+
+test('createRsqlQuery', assert => {
+    assert.equals(createRsqlQuery([]), "", "Empty query")
+    assert.equals(createRsqlQuery([{operator: 'EQUALS', field: 'field', value: 5}]), "field==5", "field==5")
+    assert.equals(createRsqlQuery([
+        {operator: 'EQUALS', field: 'field1', value: 5},
+        {operator: 'AND'},
+        {operator: 'IN', field: 'field2', value: [6, 7]}]), "field1==5;field2=in=(6,7)", "field1==5;field2=in=(6,7)")
+    assert.equals(createRsqlQuery([
+        {operator: 'EQUALS', field: 'fiel=d', value: 5},
+        {operator: 'AND'},
+        {operator: 'IN', field: 'field2', value: ["va(*&lue", "value\""]}
+    ]), "'fiel=d'==5;field2=in=('va(*&lue','value\"')", "Escapes field names and values")
+    assert.equals(createRsqlQuery([
+        {
+            operator: 'NESTED',
+            nestedRules: [{operator: 'EQUALS', field: 'field', value: 5}]
+        }]), "field==5", "Simplifies single nested rule")
+    assert.equals(createRsqlQuery([
+        {
+            operator: 'NESTED',
+            nestedRules: [{operator: 'EQUALS', field: 'field', value: 5},
+                {operator: 'OR'},
+                {operator: 'EQUALS', field: 'field', value: 6}]
+        },
+        {operator: 'AND'},
+        {operator: 'EQUALS', field: 'field', value: 7}]), "(field==5,field==6);field==7", "(field==5,field==6);field==7")
+    assert.end()
+})
+
+test('encodeRsqlValue', assert => {
+    assert.equals(encodeRsqlValue('=:;,-()"\'~<>'), '=:;,-()"\'~<>', 'Leaves rsql syntax =:;,-()"\'~<> readable')
+    assert.equals(encodeRsqlValue('a==b&'), 'a==b%26', 'Encodes &')
+    assert.equals(encodeRsqlValue('a==b#'), 'a==b%23', 'Encodes #')
+    assert.equals(encodeRsqlValue('a==b+'), 'a==b%2B', 'Encodes +')
+    assert.equals(encodeRsqlValue('a=="b lah"'), 'a=="b%20lah"', 'Encodes space')
+    assert.end()
+})
