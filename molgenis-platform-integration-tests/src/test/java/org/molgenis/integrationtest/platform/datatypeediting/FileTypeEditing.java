4dde975969bac83803bdf61788de704ebd441887
bartcharbon <bart.charbon@gmail.com>
2017-02-15 15:03:58 +0100

    fix IT test, replace "setFullyQualifiedName" with setName

--- molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/datatypeediting/FileTypeEditing.java
+++ molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/datatypeediting/FileTypeEditing.java
@@ -49,7 +49,7 @@ public class FileTypeEditing extends AbstractTestNGSpringContextTests
 	public void testNoConversionsAllowed()
 	{
 		EntityType entityType = entityTypeFactory.create("1");
-		//FIXME entityType.setFullyQualifiedName("FileEntity");
+		entityType.setName("FileEntity");
 		entityType.setBackend("PostgreSQL");
 
 		entityType.addAttribute(attributeFactory.create().setName("id").setIdAttribute(true), ROLE_ID);
