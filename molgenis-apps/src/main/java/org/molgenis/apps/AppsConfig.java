62d345b597fc8ecabd3551c8ee081f953e9d42c5
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-03-13 08:38:57 +0100

    Fix broken import

--- molgenis-apps/src/main/java/org/molgenis/apps/AppsConfig.java
+++ molgenis-apps/src/main/java/org/molgenis/apps/AppsConfig.java
@@ -4,36 +4,34 @@ import org.molgenis.apps.model.App;
 import org.molgenis.apps.model.AppRepositoryDecorator;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Repository;
-import org.molgenis.data.RepositoryDecoratorFactory;
-import org.molgenis.data.platform.decorators.RepositoryDecoratorRegistry;
+import org.molgenis.data.RepositoryDecoratorRegistry;
 import org.molgenis.file.FileStore;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+import static java.util.Objects.requireNonNull;
 import static org.molgenis.apps.model.AppMetaData.APP;
 
 @Configuration
 public class AppsConfig
 {
-	@Autowired
-	FileStore fileStore;
+	private final FileStore fileStore;
+	private final RepositoryDecoratorRegistry repositoryDecoratorRegistry;
 
-	@Autowired
-	RepositoryDecoratorRegistry repositoryDecoratorRegistry;
+	public AppsConfig(FileStore fileStore, RepositoryDecoratorRegistry repositoryDecoratorRegistry)
+	{
 
+		this.fileStore = requireNonNull(fileStore);
+		this.repositoryDecoratorRegistry = requireNonNull(repositoryDecoratorRegistry);
+	}
+
+	@SuppressWarnings("unchecked")
 	@PostConstruct
 	public void init()
 	{
-		repositoryDecoratorRegistry.addFactory(APP, new RepositoryDecoratorFactory()
-		{
-			@Override
-			public Repository<Entity> createDecoratedRepository(Repository<Entity> repository)
-			{
-				return (Repository<Entity>) (Repository<? extends Entity>) new AppRepositoryDecorator(
-						(Repository<App>) (Repository<? extends Entity>) repository, fileStore);
-			}
-		});
+		repositoryDecoratorRegistry.addFactory(APP,
+				repository -> (Repository<Entity>) (Repository<? extends Entity>) new AppRepositoryDecorator(
+						(Repository<App>) (Repository<? extends Entity>) repository, fileStore));
 	}
 }
