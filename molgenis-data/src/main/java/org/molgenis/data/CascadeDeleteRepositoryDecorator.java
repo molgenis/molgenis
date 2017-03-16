b7e76b4a2474db41c73ded9febe5aeb73906b43f
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-03-16 14:54:27 +0100

    M3169 Cascading delete

---
+++ molgenis-data/src/main/java/org/molgenis/data/CascadeDeleteRepositoryDecorator.java
@@ -0,0 +1,120 @@
+package org.molgenis.data;
+
+import com.google.common.collect.Iterators;
+import org.molgenis.data.meta.model.Attribute;
+import org.molgenis.data.meta.model.EntityType;
+
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static org.molgenis.data.support.EntityTypeUtils.isSingleReferenceType;
+import static org.molgenis.util.EntityUtils.asStream;
+
+public class CascadeDeleteRepositoryDecorator extends AbstractRepositoryDecorator<Entity>
+{
+	private static final int BATCH_SIZE = 1000;
+
+	private final Repository<Entity> decoratedRepository;
+	private final DataService dataService;
+
+	public CascadeDeleteRepositoryDecorator(Repository<Entity> decoratedRepository, DataService dataService)
+	{
+		this.decoratedRepository = requireNonNull(decoratedRepository);
+		this.dataService = requireNonNull(dataService);
+	}
+
+	@Override
+	protected Repository<Entity> delegate()
+	{
+		return decoratedRepository;
+	}
+
+	@Override
+	public void delete(Entity entity)
+	{
+		prepareCascadeDeletes(entity);
+		super.delete(entity);
+		handleCascadeDeletes(entity);
+	}
+
+	@Override
+	public void deleteById(Object id)
+	{
+		Entity entity = findOneById(id);
+		super.deleteById(id);
+		handleCascadeDeletes(entity);
+	}
+
+	@Override
+	public void deleteAll()
+	{
+		Iterators.partition(query().findAll().iterator(), BATCH_SIZE).forEachRemaining(entitiesBatch ->
+		{
+			super.delete(entitiesBatch.stream());
+			entitiesBatch.forEach(this::handleCascadeDeletes);
+		});
+	}
+
+	@Override
+	public void delete(Stream<Entity> entities)
+	{
+		Iterators.partition(entities.iterator(), BATCH_SIZE).forEachRemaining(entitiesBatch ->
+		{
+			entitiesBatch.forEach(this::prepareCascadeDeletes);
+			super.delete(entitiesBatch.stream());
+			entitiesBatch.forEach(this::handleCascadeDeletes);
+		});
+	}
+
+	@Override
+	public void deleteAll(Stream<Object> ids)
+	{
+		Iterators.partition(ids.iterator(), BATCH_SIZE).forEachRemaining(idsBatch ->
+		{
+			Stream<Entity> entities = findAll(idsBatch.stream());
+			super.deleteAll(idsBatch.stream());
+			entities.forEach(this::handleCascadeDeletes);
+		});
+	}
+
+	/**
+	 * Guarantee that referenced entities for attributes with cascade delete are loaded,
+	 * e.g. in case entity is a lazy or partial entity.
+	 */
+	private void prepareCascadeDeletes(Entity entity)
+	{
+		getCascadeDeleteAttributes().forEach(attr -> entity.get(attr.getName()));
+	}
+
+	private void handleCascadeDeletes(Entity entity)
+	{
+		getCascadeDeleteAttributes().forEach(attr -> handleCascadeDeletes(entity, attr));
+	}
+
+	private void handleCascadeDeletes(Entity entity, Attribute attribute)
+	{
+		EntityType refEntityType = attribute.getRefEntity();
+		if (isSingleReferenceType(attribute))
+		{
+			Entity refEntity = entity.getEntity(attribute.getName());
+			if (refEntity != null)
+			{
+				dataService.delete(refEntityType.getFullyQualifiedName(), refEntity);
+			}
+		}
+		else
+		{
+			Iterable<Entity> entities = entity.getEntities(attribute.getName());
+			if (entities.iterator().hasNext())
+			{
+				dataService.delete(refEntityType.getFullyQualifiedName(), asStream(entities));
+			}
+		}
+	}
+
+	private Stream<Attribute> getCascadeDeleteAttributes()
+	{
+		return asStream(getEntityType().getAtomicAttributes())
+				.filter(attribute -> attribute.getCascadeDelete() != null && attribute.getCascadeDelete());
+	}
+}
---
+++ molgenis-data/src/test/java/org/molgenis/data/CascadeDeleteRepositoryDecoratorTest.java
@@ -0,0 +1,188 @@
+package org.molgenis.data;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.molgenis.data.meta.model.Attribute;
+import org.molgenis.data.meta.model.EntityType;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.molgenis.data.meta.AttributeType.*;
+import static org.testng.Assert.assertEquals;
+
+public class CascadeDeleteRepositoryDecoratorTest
+{
+	private static final String ENTITY_TYPE_NAME = "entityType";
+	private static final String XREF_ATTR_NAME = "xrefAttr";
+	private static final String REF_ENTITY_TYPE_NAME = "refEntityType";
+
+	@Mock
+	private Repository<Entity> decoratedRepository;
+
+	@Mock
+	private DataService dataService;
+
+	private CascadeDeleteRepositoryDecorator cascadeDeleteRepositoryDecorator;
+
+	@Mock
+	private EntityType entityType;
+	@Mock
+	private Attribute stringAttr;
+	@Mock
+	private Attribute xrefAttr;
+	@Mock
+	private EntityType refEntityType;
+	@Mock
+	private Entity entity;
+	@Mock
+	private Entity refEntity;
+
+	@BeforeMethod
+	public void setUpBeforeMethod()
+	{
+		initMocks(this);
+		cascadeDeleteRepositoryDecorator = new CascadeDeleteRepositoryDecorator(decoratedRepository, dataService);
+
+		when(refEntityType.getFullyQualifiedName()).thenReturn(REF_ENTITY_TYPE_NAME);
+		when(refEntityType.getAtomicAttributes()).thenReturn(emptyList());
+
+		when(stringAttr.getName()).thenReturn("stringAttr");
+		when(stringAttr.getDataType()).thenReturn(STRING);
+		when(xrefAttr.getName()).thenReturn(XREF_ATTR_NAME);
+		when(xrefAttr.getDataType()).thenReturn(XREF);
+		when(xrefAttr.getRefEntity()).thenReturn(refEntityType);
+
+		when(entityType.getAtomicAttributes()).thenReturn(asList(stringAttr, xrefAttr));
+		when(decoratedRepository.getEntityType()).thenReturn(entityType);
+	}
+
+	@Test(expectedExceptions = NullPointerException.class)
+	public void testDelegate() throws Exception
+	{
+		new CascadeDeleteRepositoryDecorator(null, null);
+	}
+
+	@Test
+	public void testDeleteNoCascade() throws Exception
+	{
+		when(xrefAttr.getCascadeDelete()).thenReturn(null);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.delete(entity);
+		verify(decoratedRepository).delete(entity);
+		verify(dataService, never()).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteCascadeNotNull() throws Exception
+	{
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.delete(entity);
+		verify(decoratedRepository).delete(entity);
+		verify(dataService).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteCascadeNull() throws Exception
+	{
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(null);
+		cascadeDeleteRepositoryDecorator.delete(entity);
+		verify(decoratedRepository).delete(entity);
+		verify(dataService, never()).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteCascadeMrefEmpty() throws Exception
+	{
+		String mrefAttrName = "mrefAttrName";
+		Attribute mrefAttr = mock(Attribute.class);
+		when(mrefAttr.getName()).thenReturn(mrefAttrName);
+		when(mrefAttr.getDataType()).thenReturn(MREF);
+		when(entityType.getAtomicAttributes()).thenReturn(asList(stringAttr, mrefAttr));
+
+		when(mrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntities(mrefAttrName)).thenReturn(emptyList());
+		cascadeDeleteRepositoryDecorator.delete(entity);
+		verify(decoratedRepository).delete(entity);
+		verify(dataService, never()).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteCascadeMrefNotEmpty() throws Exception
+	{
+		String mrefAttrName = "mrefAttrName";
+		Attribute mrefAttr = mock(Attribute.class);
+		when(mrefAttr.getName()).thenReturn(mrefAttrName);
+		when(mrefAttr.getDataType()).thenReturn(MREF);
+		when(mrefAttr.getRefEntity()).thenReturn(refEntityType);
+		when(entityType.getAtomicAttributes()).thenReturn(asList(stringAttr, mrefAttr));
+
+		when(mrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntities(mrefAttrName)).thenReturn(singletonList(refEntity));
+		cascadeDeleteRepositoryDecorator.delete(entity);
+		verify(decoratedRepository).delete(entity);
+		ArgumentCaptor<Stream<Entity>> captor = ArgumentCaptor.forClass((Class) Stream.class);
+		verify(dataService).delete(eq(REF_ENTITY_TYPE_NAME), captor.capture());
+		assertEquals(captor.getValue().collect(toList()), singletonList(refEntity));
+	}
+
+	@Test
+	public void testDeleteByIdCascadeNotNull() throws Exception
+	{
+		String entityId = "id";
+		when(decoratedRepository.findOneById(entityId)).thenReturn(entity);
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.deleteById(entityId);
+		verify(decoratedRepository).deleteById(entityId);
+		verify(dataService).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteAllCascadeNotNull() throws Exception
+	{
+		when(decoratedRepository.findAll(any(Query.class))).thenReturn(Stream.of(entity));
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.deleteAll();
+		ArgumentCaptor<Stream<Entity>> captor = ArgumentCaptor.forClass((Class) Stream.class);
+		verify(decoratedRepository).delete(captor.capture());
+		assertEquals(captor.getValue().collect(toList()), singletonList(entity));
+		verify(dataService).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteStreamCascadeNotNull() throws Exception
+	{
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.delete(Stream.of(entity));
+		ArgumentCaptor<Stream<Entity>> captor = ArgumentCaptor.forClass((Class) Stream.class);
+		verify(decoratedRepository).delete(captor.capture());
+		assertEquals(captor.getValue().collect(toList()), singletonList(entity));
+		verify(dataService).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+
+	@Test
+	public void testDeleteAllStreamCascadeNotNull() throws Exception
+	{
+		String entityId = "id";
+		when(decoratedRepository.findAll(any(Stream.class))).thenReturn(Stream.of(entity));
+		when(xrefAttr.getCascadeDelete()).thenReturn(Boolean.TRUE);
+		when(entity.getEntity(XREF_ATTR_NAME)).thenReturn(refEntity);
+		cascadeDeleteRepositoryDecorator.deleteAll(Stream.of(entityId));
+		ArgumentCaptor<Stream<Object>> captor = ArgumentCaptor.forClass((Class) Stream.class);
+		verify(decoratedRepository).deleteAll(captor.capture());
+		assertEquals(captor.getValue().collect(toList()), singletonList(entityId));
+		verify(dataService).delete(REF_ENTITY_TYPE_NAME, refEntity);
+	}
+}
