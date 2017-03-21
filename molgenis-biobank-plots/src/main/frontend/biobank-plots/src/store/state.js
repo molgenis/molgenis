1968fc5ffe4af1da099ab729e0275b82ae44ad3d
Fleur Kelpin <f.kelpin@umcg.nl>
2017-03-22 00:36:37 +0100

    Initial state has no attribute charts.

--- molgenis-biobank-plots/src/main/frontend/biobank-plots/src/store/state.js
+++ molgenis-biobank-plots/src/main/frontend/biobank-plots/src/store/state.js
@@ -12,30 +12,7 @@ export const state = {
   biobank: null,
   aggs: [],
   biobanks: [],
-  attributeCharts: [{
-    title: 'Sex',
-    rows: [
-      ['sex', 194, 127, 456]
-    ],
-    columns: [
-      {type: 'string', label: 'label'},
-      {type: 'number', label: 'female'},
-      {type: 'number', label: 'male'},
-      {type: 'number', label: 'Unknown'}
-    ]
-  }, {
-    title: 'DNA',
-    rows: [
-      ['DNA', 13, 764, 0],
-      ['rnaseq', 473, 304, 0]
-    ],
-    columns: [
-      {type: 'string', label: 'label'},
-      {type: 'number', label: 'True'},
-      {type: 'number', label: 'False'},
-      {type: 'number', label: 'Unknown'}
-    ]
-  }],
+  attributeCharts: [],
   server: {
     apiUrl: 'https://molgenis09.gcc.rug.nl/api/'
   },
