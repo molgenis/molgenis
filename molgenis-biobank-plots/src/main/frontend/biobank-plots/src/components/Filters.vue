8fab59961aa0e943935a31f1918a3c2f61a752e0
Mark-de-Haan <markdehaan90@gmail.com>
2017-03-21 16:23:12 +0100

    Added reset filter button

--- molgenis-biobank-plots/src/main/frontend/biobank-plots/src/components/Filters.vue
+++ molgenis-biobank-plots/src/main/frontend/biobank-plots/src/components/Filters.vue
@@ -1,43 +1,55 @@
 <template>
-  <b-card header="Filters" header-tag="h1" show-footer>
-  <form>
-    <h4>Data types</h4>
-    <div class="custom-controls-stacked">
-      <filter-checkbox name="rnaseq" label="Transcriptome (RNAseq)"></filter-checkbox>
-      <filter-checkbox name="DNAm" label="Methylome (Illumina 450K)"></filter-checkbox>
-      <filter-checkbox name="DNA" label="DNA"></filter-checkbox>
-      <filter-checkbox name="wbcc" label="White bloodcell count"></filter-checkbox>
-      <filter-checkbox name="metabolomics" label="Metabolome (Brainshake)"></filter-checkbox>
+  <b-card show-header show-footer>
+
+    <div slot="header">
+      <div class="row">
+        <div class="col-4">
+          <h3>Filters</h3>
+        </div>
+        <div class="col-8 text-right">
+          <b-button size="sm" @click="resetFilters">Reset filters</b-button>
+        </div>
+      </div>
     </div>
 
-    <hr>
-    <h4>Gender</h4>
-    <div class="custom-controls-stacked">
-      <filter-checkbox name="male" label="Male"></filter-checkbox>
-      <filter-checkbox name="female" label="Female"></filter-checkbox>
-    </div>
+    <form>
+      <h4>Data types</h4>
+      <div class="custom-controls-stacked">
+        <filter-checkbox name="rnaseq" label="Transcriptome (RNAseq)"></filter-checkbox>
+        <filter-checkbox name="DNAm" label="Methylome (Illumina 450K)"></filter-checkbox>
+        <filter-checkbox name="DNA" label="DNA"></filter-checkbox>
+        <filter-checkbox name="wbcc" label="White bloodcell count"></filter-checkbox>
+        <filter-checkbox name="metabolomics" label="Metabolome (Brainshake)"></filter-checkbox>
+      </div>
 
-    <hr>
-    <h4>Smoking</h4>
-    <div class="custom-controls-stacked">
-      <filter-checkbox name="smoking" label="Yes"></filter-checkbox>
-      <filter-checkbox name="nonSmoking" label="No"></filter-checkbox>
-    </div>
+      <hr>
+      <h4>Gender</h4>
+      <div class="custom-controls-stacked">
+        <filter-checkbox name="male" label="Male"></filter-checkbox>
+        <filter-checkbox name="female" label="Female"></filter-checkbox>
+      </div>
 
-    <hr>
-    <h4>Age</h4>
-    From to
+      <hr>
+      <h4>Smoking</h4>
+      <div class="custom-controls-stacked">
+        <filter-checkbox name="smoking" label="Yes"></filter-checkbox>
+        <filter-checkbox name="nonSmoking" label="No"></filter-checkbox>
+      </div>
 
-    <hr>
-    <h4>Biobank</h4>
-    <select v-model="selectedBiobank" class="custom-select">
-      <option v-for="biobank in biobankOptions" :value='biobank.value'>{{biobank.text}}</option>
-    </select>
-  </form>
+      <hr>
+      <h4>Age</h4>
+      From to
 
-  <small slot="footer" class="text-muted">
-    Number of samples: {{numberOfSamples}}
-  </small>
+      <hr>
+      <h4>Biobank</h4>
+      <select v-model="selectedBiobank" class="custom-select">
+        <option v-for="biobank in biobankOptions" :value='biobank.value'>{{biobank.text}}</option>
+      </select>
+    </form>
+
+    <small slot="footer" class="text-muted">
+      Number of samples: {{numberOfSamples}}
+    </small>
 
   </b-card>
 </template>
@@ -72,6 +84,11 @@
     created () {
       this.$store.dispatch('getBiobanks')
       this.$store.commit('setFilter', {name: 'DNA', value: false})
+    },
+    methods () {
+      resetFilters: function() {
+        this.$store.commit('resetFilters')
+      }
     },
     name: 'filters'
   }
