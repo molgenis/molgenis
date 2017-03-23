f0eef2f0ab68aa0c6d471c84fc4615807d04bb34
Fleur Kelpin <f.kelpin@umcg.nl>
2017-03-23 23:09:12 +0100

    Use grey for unknown

--- molgenis-biobank-plots/src/main/frontend/biobank-plots/src/utils.js
+++ molgenis-biobank-plots/src/main/frontend/biobank-plots/src/utils.js
@@ -13,7 +13,7 @@ const resizeThrottler = () => {
     resizeTimeout = setTimeout(function () {
       resizeTimeout = null
       resizeEventBus.$emit('resize')
-    }, 30)
+    }, 200)
   }
 }
 
@@ -22,24 +22,7 @@ window.addEventListener('resize', resizeThrottler, false)
 export const chartColors = [
   '#3366CC',
   '#DC3912',
-  '#FF9900',
-  '#109618',
-  '#990099',
-  '#3B3EAC',
-  '#0099C6',
-  '#DD4477',
-  '#66AA00',
-  '#B82E2E',
-  '#316395',
-  '#994499',
-  '#22AA99',
-  '#AAAA11',
-  '#6633CC',
-  '#E67300',
-  '#8B0707',
-  '#329262',
-  '#5574A6',
-  '#3B3EAC']
+  '#cccccc']
 
 export const chartColorsGradient = [
   '#ADC9FF',
@@ -50,5 +33,5 @@ export const chartColorsGradient = [
   '#407CF2',
   '#2B6CF0',
   '#155DED',
-  '#004EEB'
+  '#cccccc'
 ]
