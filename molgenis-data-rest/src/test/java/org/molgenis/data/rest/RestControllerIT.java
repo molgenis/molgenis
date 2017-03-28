a7b788dcd8f1dd4b3e6fef26ca6c6a71c1f7d66b
Mark-de-Haan <markdehaan90@gmail.com>
2017-03-28 14:58:01 +0200

    {WIP] Added ITs for RestControllerV1

--- molgenis-data-rest/src/test/java/org/molgenis/data/rest/RestControllerIT.java
+++ molgenis-data-rest/src/test/java/org/molgenis/data/rest/RestControllerIT.java
@@ -2,16 +2,13 @@ package org.molgenis.data.rest;
 
 import com.google.common.collect.ImmutableMap;
 import io.restassured.RestAssured;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.elasticsearch.common.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import static com.google.common.collect.ImmutableMap.of;
 import static io.restassured.RestAssured.baseURI;
@@ -24,7 +21,9 @@ public class RestControllerIT
 	private static final Logger LOG = LoggerFactory.getLogger(RestControllerIT.class);
 
 	private static final String X_MOLGENIS_TOKEN = "x-molgenis-token";
+	private static final String TEXT_PLAIN = "text/plain";
 	private static final String APPLICATION_JSON = "application/json";
+	private static final String TEXT_CSV = "text/csv";
 	private static final String PATH = "api/v1/";
 
 	private static final String DEFAULT_HOST = "https://molgenis62.gcc.rug.nl/";
@@ -36,7 +35,7 @@ public class RestControllerIT
 	@Test
 	public void init()
 	{
-		LOG.info("Read enviroment variables");
+		LOG.info("Read environment variables");
 		String envHost = System.getenv("REST_TEST_HOST");
 		RestAssured.baseURI = Strings.isEmpty(envHost) ? DEFAULT_HOST : envHost;
 		LOG.info("baseURI: " + baseURI);
@@ -56,7 +55,6 @@ public class RestControllerIT
 		String testUserId = getUserId(adminToken, "test");
 		LOG.info("testUSerId: " + testUserId);
 
-
 		grantRights(adminToken, testUserId, "sys_FreemarkerTemplate");
 		grantRights(adminToken, testUserId, "sys_scr_ScriptType");
 
@@ -65,6 +63,7 @@ public class RestControllerIT
 
 	/**
 	 * Login with user name and password and return token on success
+	 *
 	 * @param userName the username to login with
 	 * @param password the password to use for login
 	 * @return the token returned from the login
@@ -75,11 +74,8 @@ public class RestControllerIT
 		loginBody.put("username", userName);
 		loginBody.put("password", password);
 
-		String token = given().log().all()
-				.contentType(APPLICATION_JSON).body(loginBody.toJSONString())
-				.when().post(PATH + "login")
-				.then().log().all()
-				.extract().path("token");
+		String token = given().log().all().contentType(APPLICATION_JSON).body(loginBody.toJSONString()).when()
+				.post(PATH + "login").then().log().all().extract().path("token");
 
 		LOG.info("Login token for user(" + userName + "): " + token);
 
@@ -96,34 +92,29 @@ public class RestControllerIT
 		createTestUserBody.put("changePassword", false);
 		createTestUserBody.put("Email", "test@example.com");
 
-		int code = given().log().all()
-				.header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
-				.body(createTestUserBody.toJSONString())
-				.when().post(PATH + "sys_sec_User")
-				.then().log().all()
-				.extract().statusCode();
+		int code = given().log().all().header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
+				.body(createTestUserBody.toJSONString()).when().post(PATH + "sys_sec_User").then().log().all().extract()
+				.statusCode();
 
 		LOG.info("Created test user code: " + Integer.toString(code));
 	}
 
 	/**
 	 * Grant user rights in list of entities
+	 *
 	 * @param adminToken the token to use for signin
-	 * @param userId the ID (not the name) of the user that needs to get the rights
-	 * @param entity a list of entity names
+	 * @param userId     the ID (not the name) of the user that needs to get the rights
+	 * @param entity     a list of entity names
 	 * @return
 	 */
 	private int grantRights(String adminToken, String userId, String entity)
 	{
-		String right =  "ROLE_ENTITY_WRITE_" + entity.toUpperCase();
+		String right = "ROLE_ENTITY_WRITE_" + entity.toUpperCase();
 		JSONObject body = new JSONObject(ImmutableMap.of("role", right, "User", userId));
 
-		return given().log().all()
-				.header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
-				.body(body.toJSONString())
-				.when().post(PATH + "sys_sec_UserAuthority")
-				.then().log().all()
-				.extract().statusCode();
+		return given().log().all().header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
+				.body(body.toJSONString()).when().post(PATH + "sys_sec_UserAuthority").then().log().all().extract()
+				.statusCode();
 	}
 
 	private String getUserId(String adminToken, String userName)
@@ -133,13 +124,9 @@ public class RestControllerIT
 				singletonList(of("field", "username", "operator", "EQUALS", "value", userName)));
 		JSONObject body = new JSONObject(query);
 
-		String id = given()
-				.header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
-				.queryParam("_method", "GET")
-				.body(body.toJSONString())
-				.when().post(PATH + "sys_sec_User")
-				.then().log().all()
-				.extract().path("items[0].id");
+		String id = given().header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
+				.queryParam("_method", "GET").body(body.toJSONString()).when().post(PATH + "sys_sec_User").then().log()
+				.all().extract().path("items[0].id");
 
 		return id;
 
@@ -158,7 +145,7 @@ public class RestControllerIT
 	}
 
 	@Test
-	public void testUserAuthorityotAllowed()
+	public void testUserAuthorityNotAllowed()
 	{
 		noPermissionRequest("sys_sec_UserAuthority");
 	}
@@ -181,21 +168,96 @@ public class RestControllerIT
 		successRequest("sys_scr_ScriptType", this.testUserToken);
 	}
 
-	private void noPermissionRequest(String requestedEntity)
+	@Test
+	public void testEntityExists()
 	{
-		given().log().all()
-				.when().get(PATH + requestedEntity)
-				.then().statusCode(401).body("errors.message[0]", equalTo("No [COUNT] permission on entity [" + requestedEntity + "]"));
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(TEXT_PLAIN).when()
+				.get(PATH + "sys_scr_ScriptType/exist").then().log().all().statusCode(200).body(equalTo("true"));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(TEXT_PLAIN).when()
+				.get(PATH + "sys_NonExistingEntity/exist").then().log().all().statusCode(200).body(equalTo("false"));
 	}
 
+	@Test
+	public void testGetEntityType()
+	{
+		String responseBody = "{\"href\":\"/api/v1/sys_scr_ScriptType/meta\",\"hrefCollection\":\"/api/v1/sys_scr_ScriptType\",\"name\":\"sys_scr_ScriptType\",\"label\":\"Script type\",\"attributes\":{\"name\":{\"href\":\"/api/v1/sys_scr_ScriptType/meta/name\"}},\"labelAttribute\":\"name\",\"idAttribute\":\"name\",\"lookupAttributes\":[],\"isAbstract\":false,\"languageCode\":\"en\",\"writable\":true}";
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/meta").then().log().all().statusCode(200).body(equalTo(responseBody));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/meta?_method=GET").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+	}
+
+	@Test
+	public void testRetrieveEntityAttributeMeta()
+	{
+		String responseBody = "{\"href\":\"/api/v1/sys_scr_ScriptType/meta/name\",\"fieldType\":\"STRING\",\"name\":\"name\",\"label\":\"name\",\"attributes\":[],\"enumOptions\":[],\"maxLength\":255,\"auto\":false,\"nillable\":false,\"readOnly\":true,\"labelAttribute\":true,\"unique\":true,\"visible\":true,\"lookupAttribute\":false,\"isAggregatable\":false}";
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/meta/name").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/meta/name?_method=GET").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+	}
+
+	@Test
+	public void testRetrieveEntity()
+	{
+		String responseBody = "{\"href\":\"/api/v1/sys_scr_ScriptType/R\",\"name\":\"R\"}";
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/R").then().log().all().statusCode(200).body(equalTo(responseBody));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType/R?_method=GET").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+	}
+
+	@Test
+	public void testRetrieveEntityAttribute()
+	{
+		String responseBody = "{\"href\":\"/api/v1/sys_md_Package/sys/children\",\"start\":0,\"num\":100,\"total\":8,\"items\":[{\"href\":\"/api/v1/sys_md_Package/sys_idx\",\"id\":\"sys_idx\",\"name\":\"idx\",\"label\":\"Index\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_idx/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_idx/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_idx/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_idx/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_mail\",\"id\":\"sys_mail\",\"name\":\"mail\",\"label\":\"Mail\",\"description\":\"Mail properties\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_mail/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_mail/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_mail/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_mail/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_map\",\"id\":\"sys_map\",\"name\":\"map\",\"label\":\"Mapper\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_map/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_map/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_map/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_map/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_md\",\"id\":\"sys_md\",\"name\":\"md\",\"label\":\"Meta\",\"description\":\"Package containing all meta data entities\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_md/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_md/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_md/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_md/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_ont\",\"id\":\"sys_ont\",\"name\":\"ont\",\"label\":\"Ontology\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_ont/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_ont/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_ont/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_ont/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_scr\",\"id\":\"sys_scr\",\"name\":\"scr\",\"label\":\"Script\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_scr/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_scr/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_scr/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_scr/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_sec\",\"id\":\"sys_sec\",\"name\":\"sec\",\"label\":\"Security\",\"description\":\"Package containing security related entities\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_sec/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_sec/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_sec/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_sec/tags\"}},{\"href\":\"/api/v1/sys_md_Package/sys_set\",\"id\":\"sys_set\",\"name\":\"set\",\"label\":\"Settings\",\"description\":\"Application and plugin settings\",\"parent\":{\"href\":\"/api/v1/sys_md_Package/sys_set/parent\"},\"children\":{\"href\":\"/api/v1/sys_md_Package/sys_set/children\"},\"entityTypes\":{\"href\":\"/api/v1/sys_md_Package/sys_set/entityTypes\"},\"tags\":{\"href\":\"/api/v1/sys_md_Package/sys_set/tags\"}}]}";
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_md_Package/sys/children").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_md_Package/sys/children?_method=GET").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+	}
+
+	@Test
+	public void testRetrieveEntityCollectionResponse()
+	{
+		String responseBody = "{\"href\":\"/api/v1/sys_scr_ScriptType\",\"meta\":{\"href\":\"/api/v1/sys_scr_ScriptType/meta\",\"hrefCollection\":\"/api/v1/sys_scr_ScriptType\",\"name\":\"sys_scr_ScriptType\",\"label\":\"Script type\",\"attributes\":{\"name\":{\"href\":\"/api/v1/sys_scr_ScriptType/meta/name\"}},\"labelAttribute\":\"name\",\"idAttribute\":\"name\",\"lookupAttributes\":[],\"isAbstract\":false,\"languageCode\":\"en\",\"writable\":true},\"start\":0,\"num\":100,\"total\":4,\"items\":[{\"href\":\"/api/v1/sys_scr_ScriptType/python\",\"name\":\"python\"},{\"href\":\"/api/v1/sys_scr_ScriptType/R\",\"name\":\"R\"},{\"href\":\"/api/v1/sys_scr_ScriptType/JavaScript%20(Magma)\",\"name\":\"JavaScript (Magma)\"},{\"href\":\"/api/v1/sys_scr_ScriptType/JavaScript\",\"name\":\"JavaScript\"}]}";
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType").then().log().all().statusCode(200).body(equalTo(responseBody));
+
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(APPLICATION_JSON).when()
+				.get(PATH + "sys_scr_ScriptType?_method=GET").then().log().all().statusCode(200)
+				.body(equalTo(responseBody));
+	}
+
+	@Test
+	public void testRetrieveEntityCollection()
+	{
+		given().log().all().header(X_MOLGENIS_TOKEN, this.testUserToken).contentType(TEXT_CSV).when()
+				.get(PATH + "csv/sys_scr_ScriptType").then().log().all().statusCode(200).body(equalTo(
+				"\"name\"\n" + "\"python\"\n" + "\"R\"\n" + "\"JavaScript (Magma)\"\n" + "\"JavaScript\"\n"));
+	}
+
+	private void noPermissionRequest(String requestedEntity)
+	{
+		given().log().all().when().get(PATH + requestedEntity).then().statusCode(401)
+				.body("errors.message[0]", equalTo("No [COUNT] permission on entity [" + requestedEntity + "]"));
+	}
 
 	private void successRequest(String requestedEntity, String token)
 	{
-		given().log().all()
-				.header(X_MOLGENIS_TOKEN, token).contentType(APPLICATION_JSON)
-				.when().get(PATH + requestedEntity)
-				.then().log().all()
-				.statusCode(200);
+		given().log().all().header(X_MOLGENIS_TOKEN, token).contentType(APPLICATION_JSON).when()
+				.get(PATH + requestedEntity).then().log().all().statusCode(200);
 	}
 
 }
