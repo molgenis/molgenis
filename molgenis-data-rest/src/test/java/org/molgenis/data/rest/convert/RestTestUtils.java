518367dba2657c2fd4627afe5b69a8f6eb13a50b
connorstroomberg <c.stroomberg@gmail.com>
2017-04-06 15:29:55 +0200

    Update the API rest tests
    - Use id instead op name when querying for entity id
    - Add package name to entity name as this is now used as entity id
    - Remove base package name from id when doing a get request as this is now done by id

--- molgenis-data-rest/src/test/java/org/molgenis/data/rest/convert/RestTestUtils.java
+++ molgenis-data-rest/src/test/java/org/molgenis/data/rest/convert/RestTestUtils.java
@@ -229,7 +229,7 @@ public class RestTestUtils
 	public static void grantRights(String adminToken, String userId, String entity,
 			Permission permission)
 	{
-		String entityTypeId = getEntityTypeId(adminToken, "name", entity, "sys_md_EntityType");
+		String entityTypeId = getEntityTypeId(adminToken, "id", entity, "sys_md_EntityType");
 		grantSystemRights(adminToken, userId, entityTypeId, permission);
 	}
 
@@ -261,10 +261,10 @@ public class RestTestUtils
 				.delete("api/v1/sys_sec_UserAuthority/" + permissionId);
 	}
 
-	public static void removeEntity(String adminToken, String entityName)
+	public static void removeEntity(String adminToken, String entityId)
 	{
 		given().header("x-molgenis-token", adminToken).contentType(APPLICATION_JSON)
-				.delete("api/v1/" + entityName + "/meta");
+				.delete("api/v1/" + entityId + "/meta");
 	}
 
 	/**
