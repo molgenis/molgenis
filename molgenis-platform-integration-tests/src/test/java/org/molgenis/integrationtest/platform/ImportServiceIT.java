1ca0f6513466dc2f414c34b9c7a5ee9c1a62a6f1
connorstroomberg <c.stroomberg@gmail.com>
2017-04-19 14:08:19 +0200

    Add verify for two rows owl import tests

--- molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/ImportServiceIT.java
+++ molgenis-platform-integration-tests/src/test/java/org/molgenis/integrationtest/platform/ImportServiceIT.java
@@ -22,6 +22,7 @@ import org.molgenis.data.vcf.model.VcfAttributes;
 import org.molgenis.framework.ui.MolgenisPluginRegistryImpl;
 import org.molgenis.ontology.OntologyDataConfig;
 import org.molgenis.ontology.core.config.OntologyTestConfig;
+import org.molgenis.ontology.core.meta.Ontology;
 import org.molgenis.ontology.importer.OntologyImportService;
 import org.molgenis.security.core.runas.RunAsSystemProxy;
 import org.molgenis.util.ResourceUtils;
@@ -40,6 +41,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.collections.Lists;
 
 import java.io.File;
 import java.util.*;
@@ -57,6 +59,7 @@ import static org.molgenis.data.DatabaseAction.ADD_UPDATE_EXISTING;
 import static org.molgenis.data.meta.DefaultPackage.PACKAGE_DEFAULT;
 import static org.molgenis.data.meta.model.Package.PACKAGE_SEPARATOR;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @ContextConfiguration(classes = { PlatformITConfig.class, ImportServiceIT.Config.class })
 @TestExecutionListeners(listeners = { WithSecurityContextTestExecutionListener.class })
@@ -254,6 +257,55 @@ public class ImportServiceIT extends AbstractTransactionalTestNGSpringContextTes
 						.of("sys_ont_OntologyTermDynamicAnnotation", 4, "sys_ont_OntologyTermSynonym", 9,
 								"sys_ont_OntologyTermNodePath", 10, "sys_ont_Ontology", 1, "sys_ont_OntologyTerm", 9),
 				emptySet());
+
+		// Verify two imported rows (organization and team, as these are interesting examples)
+		List<Entity> entities = dataService.findAll("sys_ont_OntologyTerm").collect(Collectors.toList());
+		Optional<Entity> organizationOpt = entities.stream()
+				.filter(e -> e.getString("ontologyTermName").equals("organization"))
+				.findFirst();
+		assertTrue(organizationOpt.isPresent());
+		Entity organization = organizationOpt.get();
+
+		Optional<Entity> teamOpt = entities.stream()
+				.filter(e -> e.getString("ontologyTermName").equals("team"))
+				.findFirst();
+		assertTrue(teamOpt.isPresent());
+		Entity team = teamOpt.get();
+
+		// Verify organization
+		assertEquals(organization.getString("ontologyTermIRI"), "http://www.molgenis.org#Organization");
+		assertEquals(organization.getString("ontologyTermName"), "organization");
+
+		// verify organization ontologyTermSynonym
+		Iterable<Entity> ontologyTermSynonym = organization.getEntities("ontologyTermSynonym");
+		List<Entity> termSynonymRefList = new ArrayList<>();
+		ontologyTermSynonym.forEach(termSynonymRefList::add);
+		assertEquals(termSynonymRefList.size(), 1);
+		Entity organizationOntologyTermSynonym = dataService.findOneById("sys_ont_OntologyTermSynonym", termSynonymRefList.get(0).getIdValue());
+		assertEquals(organizationOntologyTermSynonym.getString("ontologyTermSynonym"), "organization");
+
+		// verify organization ontology
+		Ontology ontology = (Ontology) organization.get("ontology");
+		assertEquals(ontology.getOntologyName(), "ontology-small");
+
+		// Verify the team row
+		assertEquals(team.getString("ontologyTermIRI"), "http://www.molgenis.org#Team");
+		assertEquals(team.getString("ontologyTermName"), "team");
+
+		// verify team dynamic annotations
+		Iterable<Entity> dynamicAnnotationItr = team.getEntities("ontologyTermDynamicAnnotation");
+		List<Entity>  dynamicAnnotations = new ArrayList<>();
+		dynamicAnnotationItr.forEach(dynamicAnnotations::add);
+		assertEquals(dynamicAnnotations.size(), 2);
+		Entity annotationOne = dataService.findOneById("sys_ont_OntologyTermDynamicAnnotation", dynamicAnnotations.get(0).getIdValue());
+		assertEquals(annotationOne.getString("label"), "friday:2412423");
+		Entity annotationTwo = dataService.findOneById("sys_ont_OntologyTermDynamicAnnotation", dynamicAnnotations.get(1).getIdValue());
+		assertEquals(annotationTwo.getString("label"), "molgenis:1231424");
+
+		// verify team ontology
+		ontology = (Ontology) team.get("ontology");
+		assertEquals(ontology.getOntologyName(), "ontology-small");
+
 	}
 
 	@WithMockUser(username = USERNAME, roles = { ROLE_READ_PACKAGE, ROLE_READ_ENTITY_TYPE, ROLE_READ_ATTRIBUTE })
