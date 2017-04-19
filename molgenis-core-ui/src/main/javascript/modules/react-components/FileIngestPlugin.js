2f16851ca1e182da727c1256ee2f17b4b9b11d5c
tommydeboer <tommydeboer4@gmail.com>
2017-04-19 14:17:17 +0200

    fix #6049

--- molgenis-core-ui/src/main/javascript/modules/react-components/FileIngestPlugin.js
+++ molgenis-core-ui/src/main/javascript/modules/react-components/FileIngestPlugin.js
@@ -33,7 +33,7 @@ class FileIngestPlugin extends Component {
                     <legend>
                         '{fileIngest.name}' import jobs
                         <span> (Target: <a
-        href = {'/menu/main/dataexplorer?entityId=' +fileIngest.entityType.id} > {fileIngest.entityType.name}</a >)</span>
+                            href={'/menu/main/dataexplorer?entityId=' + fileIngest.entityType.id}> {fileIngest.entityType.id}</a >)</span>
                     </legend>
 
                     <div className='ingest-table'>
