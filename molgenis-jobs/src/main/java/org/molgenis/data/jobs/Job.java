22c571542f251df36db0d25acfeccf8fd3759fd2
Fleur Kelpin <f.kelpin@umcg.nl>
2017-05-01 15:08:10 +0200

    Add unit tests, simplify exception handling.

--- molgenis-jobs/src/main/java/org/molgenis/data/jobs/Job.java
+++ molgenis-jobs/src/main/java/org/molgenis/data/jobs/Job.java
@@ -6,91 +6,44 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.TransactionException;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.support.TransactionOperations;
 
 import java.util.Objects;
 import java.util.concurrent.Callable;
 
 /**
- * Superclass for molgenis jobs that keeps track of their progress.
+ * Superclass for molgenis jobs.
  */
 public abstract class Job<Result> implements Callable<Result>
 {
 	private static final Logger LOG = LoggerFactory.getLogger(Job.class);
 	private final Progress progress;
-	private TransactionTemplate transactionTemplate;
+	private TransactionOperations transactionOperations;
 	private Authentication authentication;
 
-	public Job(Progress progress, TransactionTemplate transactionTemplate, Authentication authentication)
+	/**
+	 * Creates a new Job instance.
+	 *
+	 * @param progress              {@link Progress} instance to report job progress to
+	 * @param transactionOperations A {@link TransactionOperations} to execute in. If null, the job will run without a transaction.
+	 * @param authentication        The {@link Authentication} to run under
+	 */
+	public Job(Progress progress, TransactionOperations transactionOperations, Authentication authentication)
 	{
 		this.progress = Objects.requireNonNull(progress);
-		this.transactionTemplate = transactionTemplate;
+		this.transactionOperations = transactionOperations;
 		this.authentication = Objects.requireNonNull(authentication);
 	}
 
 	@Override
 	public Result call()
 	{
-		if (transactionTemplate != null)
-		{
-			return transactionTemplate.execute((status) -> doCallInTransaction());
-		}
-		else
-		{
-			return doCallInTransaction();
-		}
-	}
-
-	private Result doCallInTransaction()
-	{
-		progress.start();
-		try
-		{
-			Result result = tryRunWithAuthentication();
-			progress.success();
-			return result;
-		}
-		catch (JobExecutionException ex)
-		{
-			Exception cause = (Exception) ex.getCause();
-			LOG.warn("Error executing job", cause);
-			progress.failed(cause);
-			throw ex;
-		}
-		catch (TransactionException te)
-		{
-			LOG.error("Error rolling back transaction for failed job execution", te);
-			progress.failed(te);
-			throw te;
-		}
-		catch (Exception other)
-		{
-			LOG.error("Error logging job success", other);
-			progress.failed(other);
-			throw other;
-		}
-	}
-
-	private Result tryRunWithAuthentication()
-	{
-		try
-		{
-			return runWithAuthentication();
-		}
-		catch (Exception e)
-		{
-			throw new JobExecutionException(e);
-		}
-	}
-
-	private Result runWithAuthentication() throws Exception
-	{
-		SecurityContext originalContext = SecurityContextHolder.getContext();
+		final SecurityContext originalContext = SecurityContextHolder.getContext();
 		try
 		{
 			SecurityContextHolder.setContext(SecurityContextHolder.createEmptyContext());
 			SecurityContextHolder.getContext().setAuthentication(authentication);
-			return call(progress);
+			return authenticatedCall();
 		}
 		finally
 		{
@@ -98,11 +51,50 @@ public abstract class Job<Result> implements Callable<Result>
 		}
 	}
 
+	private Result authenticatedCall()
+	{
+		if (transactionOperations != null)
+		{
+			try
+			{
+				return transactionOperations.execute((status) -> tryCall());
+			}
+			catch (TransactionException te)
+			{
+				LOG.error("Transaction error while running job", te);
+				progress.failed(te);
+				throw te;
+			}
+
+		}
+		else
+		{
+			return tryCall();
+		}
+	}
+
+	private Result tryCall() throws JobExecutionException
+	{
+		progress.start();
+		try
+		{
+			Result result = call(progress);
+			progress.success();
+			return result;
+		}
+		catch (Exception ex)
+		{
+			LOG.warn("Error executing job", ex);
+			progress.failed(ex);
+			throw new JobExecutionException(ex);
+		}
+	}
+
 	/**
-	 * Executes this job. For concrete subclasses to implement.
+	 * Executes this Job. For concrete subclasses to implement.
 	 *
-	 * @param progress
-	 * @throws Exception
+	 * @param progress The {@link Progress} to report job progress to
+	 * @throws Exception if something goes wrong. If an exception is thrown here, the job status will be set to failed.
 	 */
 	public abstract Result call(Progress progress) throws Exception;
 }
