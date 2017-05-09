42fb0a9d23a7b0383c759b4da908e141b4def6ea
Fleur Kelpin <f.kelpin@umcg.nl>
2017-05-09 09:28:01 +0200

    Inline FileIngestJob into the config, it’s just a method call with parameters

--- molgenis-file-ingester/src/main/java/org/molgenis/file/ingest/execution/FileIngestConfig.java
+++ molgenis-file-ingester/src/main/java/org/molgenis/file/ingest/execution/FileIngestConfig.java
@@ -1,13 +1,14 @@
 package org.molgenis.file.ingest.execution;
 
 import org.molgenis.data.jobs.Job;
+import org.molgenis.data.jobs.Progress;
 import org.molgenis.data.jobs.model.JobExecution;
 import org.molgenis.file.ingest.meta.FileIngestJobExecution;
+import org.molgenis.file.model.FileMeta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
 
 import java.util.function.Function;
 
@@ -19,30 +20,33 @@ public class FileIngestConfig
 	FileIngester fileIngester;
 
 	/**
-	 * The FileIngestJob bean factory method.
-	 * Prototype scope means that a new bean is generated each time the factory method is called.
-	 *
-	 * @param jobExecution JobExecution to execute in the {@link FileIngestJob}
-	 * @return The {@link FileIngestJob} bean
-	 */
-	@Bean
-	@Scope("prototype")
-	public FileIngestJob fileIngestJob(JobExecution jobExecution)
-	{
-		FileIngestJobExecution fileIngestJobExecution = (FileIngestJobExecution) jobExecution;
-		String targetEntityId = fileIngestJobExecution.getTargetEntityId();
-		String url = fileIngestJobExecution.getUrl();
-		String loader = fileIngestJobExecution.getLoader();
-
-		return new FileIngestJob(fileIngester, targetEntityId, url, loader, fileIngestJobExecution.getIdentifier());
-	}
-
-	/**
 	 * The FileIngestJob Factory bean.
 	 */
 	@Bean
 	public Function<JobExecution, ? extends Job> fileIngestJobFactory()
 	{
-		return this::fileIngestJob;
+		return jobExecution ->
+		{
+			final FileIngestJobExecution fileIngestJobExecution = (FileIngestJobExecution) jobExecution;
+			final String targetEntityId = fileIngestJobExecution.getTargetEntityId();
+			final String url = fileIngestJobExecution.getUrl();
+			final String loader = fileIngestJobExecution.getLoader();
+
+			return new Job<FileMeta>()
+			{
+				@Override
+				public FileMeta call(Progress progress) throws Exception
+				{
+					return fileIngester
+							.ingest(targetEntityId, url, loader, fileIngestJobExecution.getIdentifier(), progress);
+				}
+
+				@Override
+				public boolean isTransactional()
+				{
+					return true;
+				}
+			};
+		};
 	}
 }
