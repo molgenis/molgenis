56a2055f17d812b140818fed80c291a12515ac2d
Fleur Kelpin <f.kelpin@umcg.nl>
2017-05-15 09:49:30 +0200

    Do the reflection only once, at construction time.

--- molgenis-jobs/src/main/java/org/molgenis/data/jobs/JobFactoryRegistry.java
+++ molgenis-jobs/src/main/java/org/molgenis/data/jobs/JobFactoryRegistry.java
@@ -2,39 +2,32 @@ package org.molgenis.data.jobs;
 
 import org.molgenis.data.jobs.model.JobExecution;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.GenericTypeResolver;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
-import static java.util.Objects.requireNonNull;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import static org.springframework.core.GenericTypeResolver.resolveTypeArgument;
 
 /**
- * Gets the JobFactory for a JobExecution. The pairs are matched by trial and error and then cached.
- * This negates the need to expose an identifier in the JobFactory beans. Ideally we'd get the generic type of
- * JobFactory at runtime but this is not possible when using lambdas (see http://stackoverflow.com/a/25612775).
+ * Gets the JobFactory for a JobExecution.
  */
 @Component
 public class JobFactoryRegistry
 {
-	private List<JobFactory> jobFactories;
+	private Map<Class<?>, JobFactory> jobFactories;
 
 	@Autowired
 	public JobFactoryRegistry(List<JobFactory> jobFactories)
 	{
-		this.jobFactories = requireNonNull(jobFactories);
+		this.jobFactories = jobFactories.stream()
+				.collect(toMap(factory -> resolveTypeArgument(factory.getClass(), JobFactory.class), identity()));
 	}
 
 	public JobFactory getJobFactory(JobExecution jobExecution)
 	{
-		for (JobFactory jobFactory : jobFactories)
-		{
-			Class<?> clazz = GenericTypeResolver.resolveTypeArgument(jobFactory.getClass(), JobFactory.class);
-			if (clazz.getName().equals(jobExecution.getClass().getName()))
-			{
-				return jobFactory;
-			}
-		}
-		return null;
+		return jobFactories.get(jobExecution.getClass());
 	}
 }
