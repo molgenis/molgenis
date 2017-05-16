50cde625b1c6b0587de202b763b4bee9bca2253c
Fleur Kelpin <f.kelpin@umcg.nl>
2017-05-16 09:08:06 +0200

    Ask JobsController for the URL where a particular JobExecution can be found.

--- molgenis-core-ui/src/main/java/org/molgenis/ui/jobs/JobsController.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/jobs/JobsController.java
@@ -4,25 +4,30 @@ import org.molgenis.auth.User;
 import org.molgenis.data.DataService;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Query;
+import org.molgenis.data.jobs.model.JobExecution;
 import org.molgenis.data.jobs.model.JobExecutionMetaData;
 import org.molgenis.data.jobs.schedule.JobScheduler;
 import org.molgenis.security.user.UserAccountService;
 import org.molgenis.ui.MolgenisPluginController;
+import org.molgenis.ui.menu.MenuReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static java.text.MessageFormat.format;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.jobs.model.JobExecutionMetaData.SUBMISSION_DATE;
 import static org.molgenis.data.jobs.model.JobExecutionMetaData.USER;
+import static org.molgenis.data.support.Href.concatEntityHref;
 import static org.molgenis.ui.jobs.JobsController.URI;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -34,22 +39,24 @@ public class JobsController extends MolgenisPluginController
 {
 	public static final String ID = "jobs";
 	public static final String URI = MolgenisPluginController.PLUGIN_URI_PREFIX + ID;
-	private static int MAX_JOBS_TO_RETURN = 20;
+	private static final int MAX_JOBS_TO_RETURN = 20;
 
-	private UserAccountService userAccountService;
-	private DataService dataService;
-	private JobExecutionMetaData jobMetaDataMetaData;
-	private JobScheduler jobScheduler;
+	private final UserAccountService userAccountService;
+	private final DataService dataService;
+	private final JobExecutionMetaData jobMetaDataMetaData;
+	private final JobScheduler jobScheduler;
+	private final MenuReaderService menuReaderService;
 
 	@Autowired
 	public JobsController(UserAccountService userAccountService, DataService dataService,
-			JobExecutionMetaData jobMetaDataMetaData, JobScheduler jobScheduler)
+			JobExecutionMetaData jobMetaDataMetaData, JobScheduler jobScheduler, MenuReaderService menuReaderService)
 	{
 		super(URI);
 		this.userAccountService = requireNonNull(userAccountService);
 		this.dataService = requireNonNull(dataService);
 		this.jobMetaDataMetaData = requireNonNull(jobMetaDataMetaData);
 		this.jobScheduler = requireNonNull(jobScheduler);
+		this.menuReaderService = requireNonNull(menuReaderService);
 	}
 
 	@RequestMapping(method = GET)
@@ -104,5 +111,13 @@ public class JobsController extends MolgenisPluginController
 	public void runNow(@PathVariable("scheduledJobId") String scheduledJobId)
 	{
 		jobScheduler.runNow(scheduledJobId);
+	}
+
+	public String createJobExecutionHref(JobExecution jobExecution, int refreshTimeoutMillis) throws IOException
+	{
+		String jobHref = concatEntityHref("/api/v2", jobExecution.getEntityType().getId(), jobExecution.getIdValue());
+		String jobControllerURL = menuReaderService.getMenu().findMenuItemPath(ID);
+		return format("{0}/viewJob/?jobHref={1}&refreshTimeoutMillis={2}", jobControllerURL, jobHref,
+				refreshTimeoutMillis);
 	}
 }
--- molgenis-scripts/src/main/java/org/molgenis/script/ScriptRunnerController.java
+++ molgenis-scripts/src/main/java/org/molgenis/script/ScriptRunnerController.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.molgenis.data.jobs.JobExecutor;
 import org.molgenis.security.user.UserAccountService;
 import org.molgenis.ui.jobs.JobsController;
-import org.molgenis.ui.menu.MenuReaderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import java.util.Map;
 
 import static java.text.MessageFormat.format;
 import static java.util.Objects.requireNonNull;
-import static org.molgenis.data.support.Href.concatEntityHref;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
@@ -36,19 +34,19 @@ public class ScriptRunnerController
 	private final JobExecutor jobExecutor;
 	private final SavedScriptRunner savedScriptRunner;
 	private final Gson gson;
-	private final MenuReaderService menuReaderService;
 	private final UserAccountService userAccountService;
+	private final JobsController jobsController;
 
 	public ScriptRunnerController(ScriptJobExecutionFactory scriptJobExecutionFactory, JobExecutor jobExecutor,
-			SavedScriptRunner savedScriptRunner, Gson gson, MenuReaderService menuReaderService,
-			UserAccountService userAccountService)
+			SavedScriptRunner savedScriptRunner, Gson gson, UserAccountService userAccountService,
+			JobsController jobsController)
 	{
 		this.scriptJobExecutionFactory = requireNonNull(scriptJobExecutionFactory);
 		this.jobExecutor = requireNonNull(jobExecutor);
 		this.savedScriptRunner = requireNonNull(savedScriptRunner);
 		this.gson = requireNonNull(gson);
-		this.menuReaderService = requireNonNull(menuReaderService);
 		this.userAccountService = requireNonNull(userAccountService);
+		this.jobsController = requireNonNull(jobsController);
 	}
 
 	/**
@@ -72,10 +70,7 @@ public class ScriptRunnerController
 
 		jobExecutor.submit(scriptJobExecution);
 
-		String jobHref = concatEntityHref("/api/v2", scriptJobExecution.getEntityType().getId(),
-				scriptJobExecution.getIdValue());
-		String jobControllerURL = menuReaderService.getMenu().findMenuItemPath(JobsController.ID);
-		response.sendRedirect(format("{0}/viewJob/?jobHref={1}&refreshTimeoutMillis=1000", jobControllerURL, jobHref));
+		response.sendRedirect(jobsController.createJobExecutionHref(scriptJobExecution, 1000));
 	}
 
 	/**
