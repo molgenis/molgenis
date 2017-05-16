6f7d8ddce90cfad9bd6a743b1a48c5d36b93a0b7
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-05-16 08:46:28 +0200

    Move org.molgenis.data.settings from molgenis-data to molgenis-settings

--- molgenis-data-index/src/test/java/org/molgenis/data/index/config/IndexTestConfig.java
+++ molgenis-data-index/src/test/java/org/molgenis/data/index/config/IndexTestConfig.java
@@ -1,5 +1,6 @@
-package org.molgenis.data.config;
+package org.molgenis.data.index.config;
 
+import org.molgenis.data.config.EntityBaseTestConfig;
 import org.molgenis.data.index.meta.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
