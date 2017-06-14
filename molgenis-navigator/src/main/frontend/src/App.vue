3e52701d09a7f4e4acc814141f5d39b89c427398
Mark-de-Haan <markdehaan90@gmail.com>
2017-06-14 15:19:08 +0200

    Port molgenis-navigator to MOLGENIS repository

---
+++ molgenis-navigator/src/main/frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <a class="btn" href="/">
+      <i class="fa fa-chevron-left" aria-hidden="true"></i> back to home
+    </a>
+    <h1 style="padding-top: 1rem; text-align: center">Navigator</h1>
+
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
+
+<script>
+
+  export default {
+    name: 'molgenis-app'
+  }
+</script>
