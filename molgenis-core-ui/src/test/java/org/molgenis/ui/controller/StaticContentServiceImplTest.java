b1d37679469f2930126ea074ea463eb828683d76
mkslofstra <mariska-karina@hotmail.com>
2017-07-06 09:01:13 +0200

    add two new tests for new functionality

--- molgenis-core-ui/src/test/java/org/molgenis/ui/controller/StaticContentServiceImplTest.java
+++ molgenis-core-ui/src/test/java/org/molgenis/ui/controller/StaticContentServiceImplTest.java
@@ -1,8 +1,10 @@
 package org.molgenis.ui.controller;
 
 import org.molgenis.data.DataService;
+import org.molgenis.data.MolgenisDataAccessException;
 import org.molgenis.data.populate.EntityPopulator;
 import org.molgenis.security.core.MolgenisPermissionService;
+import org.molgenis.security.core.Permission;
 import org.molgenis.security.core.utils.SecurityUtils;
 import org.molgenis.security.permission.MolgenisPermissionServiceImpl;
 import org.molgenis.security.user.UserDetailsService;
@@ -38,40 +40,56 @@ import static org.testng.Assert.*;
 @ContextConfiguration
 public class StaticContentServiceImplTest extends AbstractTestNGSpringContextTests
 {
+	private final String pluginId = "home";
+
 	@Autowired
 	private StaticContentService staticContentService;
 
 	@Test
 	public void getContent()
 	{
-		assertEquals(this.staticContentService.getContent("home"), "<p>Welcome to Molgenis!</p>");
+		assertEquals(this.staticContentService.getContent(pluginId), "<p>Welcome to Molgenis!</p>");
 	}
 
 	@Test
 	public void isCurrentUserCanEdit_SuperUser()
 	{
 		this.setSecurityContextSuperUser();
-		assertTrue(this.staticContentService.isCurrentUserCanEdit("home"));
+		assertTrue(this.staticContentService.isCurrentUserCanEdit(pluginId));
 	}
 
 	@Test
-	public void isCurrentUserCanEdit_NonSuperUser()
+	public void isCurrentUserCanEdit_NonSuperUserWithoutPermissions()
 	{
-		this.setSecurityContextNonSuperUser();
-		assertFalse(this.staticContentService.isCurrentUserCanEdit("home"));
+		this.setSecurityContextNonSuperUser(false);
+		assertFalse(this.staticContentService.isCurrentUserCanEdit(pluginId));
+	}
+
+	@Test
+	public void isCurrentUserCanEdit_NonSuperUserWithPermissions()
+	{
+		this.setSecurityContextNonSuperUser(true);
+		assertFalse(this.staticContentService.isCurrentUserCanEdit(pluginId));
 	}
 
 	@Test
 	public void isCurrentUserCanEdit_AnonymousUsers()
 	{
 		this.setSecurityContextAnonymousUsers();
-		assertFalse(this.staticContentService.isCurrentUserCanEdit("home"));
+		assertFalse(this.staticContentService.isCurrentUserCanEdit(pluginId));
+	}
+
+	@Test(expectedExceptions = MolgenisDataAccessException.class, expectedExceptionsMessageRegExp = "No write permissions on static content page")
+	public void checkPermissions_withoutPermissions()
+	{
+		this.setSecurityContextNonSuperUser(false);
+		this.staticContentService.checkPermissions(pluginId);
 	}
 
 	@Test
 	public void submitContent()
 	{
-		assertTrue(this.staticContentService.submitContent("home", "<p>Welcome to Molgenis!</p>"));
+		assertTrue(this.staticContentService.submitContent(pluginId, "<p>Welcome to Molgenis!</p>"));
 	}
 
 	private void setSecurityContextSuperUser()
@@ -91,19 +109,22 @@ public class StaticContentServiceImplTest extends AbstractTestNGSpringContextTes
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
-	private void setSecurityContextNonSuperUser()
+	private void setSecurityContextNonSuperUser(Boolean hasPermissions)
 	{
 		Collection<? extends GrantedAuthority> authorities = Arrays.<SimpleGrantedAuthority>asList(
 				new SimpleGrantedAuthority(SecurityUtils.AUTHORITY_PLUGIN_READ_PREFIX + "HOME"),
 				new SimpleGrantedAuthority(SecurityUtils.AUTHORITY_PLUGIN_WRITE_PREFIX + "HOME"));
 
 		Authentication authentication = mock(Authentication.class);
+		MolgenisPermissionService permissions = mock(MolgenisPermissionService.class);
 
 		doReturn(authorities).when(authentication).getAuthorities();
 
 		when(authentication.isAuthenticated()).thenReturn(true);
 		UserDetails userDetails = when(mock(UserDetails.class).getUsername()).thenReturn("user").getMock();
 		when(authentication.getPrincipal()).thenReturn(userDetails);
+
+		when(permissions.hasPermissionOnPlugin("home", Permission.WRITE)).thenReturn(hasPermissions);
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
@@ -126,7 +147,8 @@ public class StaticContentServiceImplTest extends AbstractTestNGSpringContextTes
 	public static class Config extends WebSecurityConfigurerAdapter
 	{
 		@Bean
-		public MolgenisPermissionService molgenisPermissionService(){
+		public MolgenisPermissionService molgenisPermissionService()
+		{
 			return new MolgenisPermissionServiceImpl();
 		}
 
