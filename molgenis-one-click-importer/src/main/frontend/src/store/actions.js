3b2a21af6e69ac736598a791dd286b7829785ead
connorstroomberg <c.stroomberg@gmail.com>
2017-07-10 09:43:18 +0200

    setup of one click importer WIP

--- molgenis-one-click-importer/src/main/frontend/src/store/actions.js
+++ molgenis-one-click-importer/src/main/frontend/src/store/actions.js
@@ -1,13 +1,10 @@
-import { get } from '@molgenis/molgenis-api-client'
+import { postFile } from '@molgenis/molgenis-api-client'
 
-export const GET_ENTITY_TYPES = '__GET_ENTITY_TYPES__'
+export const IMPORT = '__IMPORT__'
 
 export default {
-  /**
-   * Example action for retrieving all EntityTypes from the server
-   */
-  [GET_ENTITY_TYPES] ({commit}) {
-    get({apiUrl: '/api'}, '/v2/sys_md_EntityTypes?num=1000')
+  [IMPORT] ({commit}, file) {
+    postFile({apiUrl: '/api/v2'}, '/one-click-importer', file)
       .then(response => {
         console.log(response)
       }, error => {
