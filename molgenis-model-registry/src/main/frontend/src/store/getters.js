3b240dd8f86be0631a48c4300e1b14b6e040737b
sido <sido@bicat.com>
2017-07-12 16:18:03 +0200

    [FIX] create links between tables

--- molgenis-model-registry/src/main/frontend/src/store/getters.js
+++ molgenis-model-registry/src/main/frontend/src/store/getters.js
@@ -1,12 +1,38 @@
 import type { State } from 'utils/flow.types'
 
-// FigureTypes: Decision, Cubel, MagneticData, TriangleUp, TriangleDown
-// Colors: yellow, green, blue
+// xref 1 - 1
+// mref 1 - N
+// categorical 1 - 1
+// categorical_mref 1 - N
+// onetomany 1 - N
+const types = {
+  'xref': {
+    src: '1',
+    ref: '1'
+  },
+  'mref': {
+    src: '1',
+    ref: 'N'
+  },
+  'categorical': {
+    src: '1',
+    ref: '1'
+  },
+  'categorical_mref': {
+    src: '1',
+    ref: 'N'
+  },
+  'onetomany': {
+    src: '1',
+    ref: 'N'
+  }
+}
+
 const mapAttributeToNode = attribute => {
   let figure = 'Cubel'
   let color = 'Blue'
   let isKey = false
-  if (attribute.type === 'xref') {
+  if (isRef(attribute)) {
     figure = 'Decision'
     color = 'red'
     isKey = true
@@ -21,14 +47,30 @@ const mapNodeData = (entityTypes) => entityTypes.map(entityType => ({
   items: entityType.attributes.map(mapAttributeToNode)
 }))
 
+const isRef = (attribute) => types[attribute.type]
+
+const determineRefEntityType = (entityTypes, refEntityType) => entityTypes.find(entityType => entityType.id === refEntityType) !== null
+const determineRefAttribute = (entityTypes, refEntityType) => {
+  const referenceEntityType = determineRefEntityType(entityTypes, refEntityType)
+  if (referenceEntityType && referenceEntityType.attributes) {
+    console.log(JSON.stringify(referenceEntityType.attributes))
+    return referenceEntityType.attributes.find(attribute => {
+      if (attribute) return attribute.name === 'id'
+    })
+  }
+}
+
 const mapLinkData = (entityTypes) => {
   const links = []
   entityTypes.forEach(entityType => {
     entityType.attributes.forEach(attribute => {
-      if (attribute.type === 'xref') {
-        console.log(attribute.name)
-        const attributeDesc = attribute.name + ' | 0..N'
-        const refAttributeDesc = attribute.refEntityType.id + ' | 1'
+      if (isRef(attribute)) {
+        const refAttribute = determineRefAttribute(entityTypes, attribute.refEntityType.id)
+        const attributeDesc = types[attribute.type].src + ' | ' + attribute.name
+        let refAttributeDesc = ''
+        if (refAttribute) {
+          refAttributeDesc = refAttribute.name + ' | ' + types[attribute.type].ref
+        }
         links.push({from: entityType.id, to: attribute.refEntityType.id, text: attributeDesc, toText: refAttributeDesc})
       }
     })
