96b4969ab74f4012d51aaf12d89de8e7665283ae
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-07-18 11:28:10 +0200

    Populate initial roles and permissions

---
+++ molgenis-app/src/main/java/org/molgenis/app/PermissionPopulatorImpl.java
@@ -0,0 +1,65 @@
+package org.molgenis.app;
+
+import org.molgenis.app.controller.HomeController;
+import org.molgenis.bootstrap.populate.PermissionPopulator;
+import org.molgenis.data.security.acl.*;
+import org.molgenis.ui.admin.user.UserAccountController;
+import org.springframework.stereotype.Component;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.Objects.requireNonNull;
+import static org.molgenis.bootstrap.populate.UsersGroupsAuthoritiesPopulatorImpl.ROLE_USER_ID;
+import static org.molgenis.data.i18n.model.L10nStringMetaData.L10N_STRING;
+import static org.molgenis.data.i18n.model.LanguageMetadata.LANGUAGE;
+import static org.molgenis.data.meta.model.AttributeMetadata.ATTRIBUTE_META_DATA;
+import static org.molgenis.data.meta.model.EntityTypeMetadata.ENTITY_TYPE_META_DATA;
+import static org.molgenis.data.meta.model.PackageMetadata.PACKAGE;
+import static org.molgenis.data.meta.model.TagMetadata.TAG;
+import static org.molgenis.file.model.FileMetaMetaData.FILE_META;
+import static org.molgenis.security.core.Permission.READ;
+import static org.molgenis.security.core.Permission.WRITE;
+import static org.molgenis.security.core.utils.SecurityUtils.ANONYMOUS_USERNAME;
+import static org.molgenis.ui.PluginMetadata.PLUGIN;
+
+@Component
+public class PermissionPopulatorImpl implements PermissionPopulator
+{
+	private final EntityAclManager entityAclManager;
+
+	public PermissionPopulatorImpl(EntityAclManager entityAclManager)
+	{
+		this.entityAclManager = requireNonNull(entityAclManager);
+	}
+
+	@Override
+	public void populate()
+	{
+		SecurityId roleUserSecurityId = SecurityId.create(null, ROLE_USER_ID);
+		EntityAce roleUserReadAce = EntityAce.create(READ, roleUserSecurityId, true);
+
+		// allow anonymous user and user role to see the home plugin
+		EntityAcl homePluginAcl = entityAclManager.readAcl(EntityIdentity.create(PLUGIN, HomeController.ID));
+		EntityAce homePluginAnonymousAce = EntityAce.create(READ, SecurityId.create(ANONYMOUS_USERNAME, null), true);
+		homePluginAcl = homePluginAcl.toBuilder().setEntries(asList(homePluginAnonymousAce, roleUserReadAce)).build();
+		entityAclManager.updateAcl(homePluginAcl);
+
+		// allow user role to update profile
+		EntityAcl userAccountPluginAcl = entityAclManager.readAcl(
+				EntityIdentity.create(PLUGIN, UserAccountController.ID));
+		EntityAce userAccountPluginUserAce = EntityAce.create(WRITE, roleUserSecurityId, true);
+		userAccountPluginAcl = userAccountPluginAcl.toBuilder()
+												   .setEntries(singletonList(userAccountPluginUserAce))
+												   .build();
+		entityAclManager.updateAcl(userAccountPluginAcl);
+
+		asList(ENTITY_TYPE_META_DATA, ATTRIBUTE_META_DATA, PACKAGE, TAG, LANGUAGE, L10N_STRING, FILE_META).forEach(
+				entityTypeId ->
+				{
+					EntityAcl entityTypeAcl = entityAclManager.readAcl(
+							EntityIdentity.create(ENTITY_TYPE_META_DATA, entityTypeId));
+					entityTypeAcl = entityTypeAcl.toBuilder().setEntries(singletonList(roleUserReadAce)).build();
+					entityAclManager.updateAcl(entityTypeAcl);
+				});
+	}
+}
--- molgenis-app/src/main/java/org/molgenis/app/WebAppSystemEntityRegistry.java
+++ molgenis-app/src/main/java/org/molgenis/app/WebAppSystemEntityRegistry.java
@@ -1,19 +1,11 @@
 package org.molgenis.app;
 
-import org.molgenis.auth.*;
 import org.molgenis.bootstrap.populate.SystemEntityRegistry;
-import org.molgenis.data.DataService;
 import org.molgenis.data.Entity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-
-import static java.util.Arrays.asList;
-import static java.util.Objects.requireNonNull;
-import static org.molgenis.auth.GroupMetaData.GROUP;
-import static org.molgenis.auth.GroupMetaData.NAME;
-import static org.molgenis.security.account.AccountService.ALL_USER_GROUP;
+import java.util.Collections;
 
 /**
  * Registry of application system entities to be added to an empty database.
@@ -21,32 +13,9 @@ import static org.molgenis.security.account.AccountService.ALL_USER_GROUP;
 @Component
 public class WebAppSystemEntityRegistry implements SystemEntityRegistry
 {
-	private final DataService dataService;
-	private final GroupAuthorityFactory groupAuthorityFactory;
-	private final RoleFactory roleFactory;
-
-	@Autowired
-	public WebAppSystemEntityRegistry(DataService dataService, GroupAuthorityFactory groupAuthorityFactory,
-			RoleFactory roleFactory)
-	{
-		this.dataService = requireNonNull(dataService);
-		this.groupAuthorityFactory = requireNonNull(groupAuthorityFactory);
-		this.roleFactory = requireNonNull(roleFactory);
-	}
-
 	@Override
 	public Collection<Entity> getEntities()
 	{
-		Role userRole = roleFactory.create();
-		userRole.setLabel("User");
-
-		Group allUsersGroup = dataService.query(GROUP, Group.class).eq(NAME, ALL_USER_GROUP).findOne();
-
-		// assign 'User' role to 'All Users' group
-		GroupAuthority usersGroupUserAuthority = groupAuthorityFactory.create();
-		usersGroupUserAuthority.setGroup(allUsersGroup);
-		usersGroupUserAuthority.setRole(userRole);
-
-		return asList(userRole, usersGroupUserAuthority);
+		return Collections.emptyList();
 	}
 }
