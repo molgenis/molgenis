bf47d71c9adf6751d192d4ff97920f1083dda4f4
Mark-de-Haan <markdehaan90@gmail.com>
2017-07-19 14:37:12 +0200

    Remove store and router from app

--- molgenis-one-click-importer/src/main/frontend/src/main.js
+++ molgenis-one-click-importer/src/main/frontend/src/main.js
@@ -2,12 +2,7 @@ import 'es6-promise/auto'
 import 'babel-polyfill'
 
 import Vue from 'vue'
-import store from './store'
-import router from './router'
 import i18n from '@molgenis/molgenis-i18n-js/dist/molgenis-i18n.esm'
-
-import { sync } from 'vuex-router-sync'
-import { INITIAL_STATE } from './store/state'
 
 import App from './App'
 import VueSweetAlert from 'vue-sweetalert'
@@ -15,22 +10,18 @@ import VueSweetAlert from 'vue-sweetalert'
 import 'bootstrap/dist/css/bootstrap.css' // Bootstrap import
 import 'font-awesome/css/font-awesome.min.css' // Font awesome import
 
-sync(store, router)
-
 Vue.use(VueSweetAlert)
 
 Vue.use(i18n, {
-  lng: INITIAL_STATE.lng,
-  fallbackLng: INITIAL_STATE.fallbackLng,
+  lng: 'en',
+  fallbackLng: 'en',
   namespace: 'molgenis-one-click-importer',
   callback () {
     /* eslint-disable no-new */
     new Vue({
       el: '#molgenis-one-click-importer',
-      store,
-      router,
       template: '<App />',
-      components: { App }
+      components: {App}
     })
   }
 })
