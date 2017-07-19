57c9f5909bc2e7390273cd40380861293c492950
Fleur Kelpin <f.kelpin@umcg.nl>
2017-07-19 19:37:42 +0200

    Return NO_CONTENT status from acl PUT method

--- molgenis-security/src/main/java/org/molgenis/security/permission/MolgenisPermissionController.java
+++ molgenis-security/src/main/java/org/molgenis/security/permission/MolgenisPermissionController.java
@@ -11,6 +11,7 @@ import org.molgenis.data.security.acl.*;
 import org.molgenis.data.support.QueryImpl;
 import org.molgenis.security.core.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -74,6 +75,7 @@ public class MolgenisPermissionController
 	}
 
 	@RequestMapping(value = "/acl", method = PUT)
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void save(@RequestBody EntityAcl acl)
 	{
 		entityAclManager.updateAcl(acl);
