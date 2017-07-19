c0e4de10d75e12a64ddc8f8df510ab1411eaf8da
Mark-de-Haan <markdehaan90@gmail.com>
2017-07-19 18:22:42 +0200

    [WIP] Data table for uploaded files

--- molgenis-one-click-importer/src/main/frontend/src/components/OneClickImporter.vue
+++ molgenis-one-click-importer/src/main/frontend/src/components/OneClickImporter.vue
@@ -18,29 +18,45 @@
           :thumbnail-width=200
           :duplicate-check=true
           :use-font-awesome=true
+          :showRemoveLink=false
+          :preview-template="previewTemplate"
           v-on:vdropzone-success="onComplete"
           v-on:vdropzone-error="onError"
           v-on:duplicate-file="onDuplicate">
 
-          <div v-if="message" class="alert alert-warning" role="alert">{{message}}</div>
-          <div class="text-center">supported file types: xlsx, xls, csv, tsv</div>
-
+            <div class="text-center">supported file types: xlsx, xls, csv, tsv</div>
         </dropzone>
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <ul>
-          <li v-for="response in responses">
-            <a :href="response.url">View {{response.filename}}</a>
-          </li>
-        </ul>
+        <div id="alert-container">
+          <div v-if="message" class="alert alert-warning" role="alert">{{message}}</div>
+        </div>
+        <table class="table">
+          <thead>
+            <th>file name</th>
+            <th>View data</th>
+          </thead>
+          <tbody>
+            <tr v-for="response in responses" >
+              <td>{{response.filename}}</td>
+              <td><a :href="response.url">View...</a></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
 
   </div>
 </template>
+
+<style scoped>
+  #alert-container {
+    padding-top: 10px;
+  }
+</style>
 
 <script>
   import Dropzone from 'vue2-dropzone'
@@ -63,6 +79,9 @@
       },
       onError (file) {
         this.message = 'Something went wrong when uploading [' + file.upload.filename + ']. Please contact an administrator'
+      },
+      previewTemplate () {
+        return '<div></div>'
       }
     },
     components: {
