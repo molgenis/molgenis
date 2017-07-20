86ebb888b3fa37dca8df5a4346e8f954fea00823
Mark-de-Haan <markdehaan90@gmail.com>
2017-07-20 17:14:44 +0200

    Replace dropzone with simple file input to make it work

--- molgenis-one-click-importer/src/main/frontend/src/components/OneClickImporter.vue
+++ molgenis-one-click-importer/src/main/frontend/src/components/OneClickImporter.vue
@@ -4,46 +4,53 @@
       <div class="col">
         <h1>Import your data</h1>
         <hr>
+        <div v-if="error != undefined" class="alert alert-danger" role="alert">
+          <button @click="error=null" type="button" class="close"><span aria-hidden="true">&times;</span></button>
+          {{error}}
+        </div>
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-12">
+        <form id="upload-form" v-on:submit.prevent class="form-inline">
+          <div class="form-group">
+            <input ref="fileInput" id="file-input" type="file" @change="setFile"/>
+          </div>
+          <button class="btn btn-primary" type="submit" @click="importFile" :disabled="file === null">Import</button>
+        </form>
+        <br/>
+        <!--<dropzone FIXME-->
+        <!--id="import-dropzone"-->
+        <!--url="/plugin/one-click-importer/upload"-->
+        <!--:accepted-filetypes="['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'].join(',')"-->
+        <!--:thumbnail-height=100-->
+        <!--:thumbnail-width=200-->
+        <!--:duplicate-check=true-->
+        <!--:use-font-awesome=true-->
+        <!--:showRemoveLink=false-->
+        <!--:preview-template="previewTemplate"-->
+        <!--v-on:vdropzone-success="onComplete"-->
+        <!--v-on:vdropzone-error="onError"-->
+        <!--v-on:duplicate-file="onDuplicate">-->
 
-        <dropzone
-          id="import-dropzone"
-          url="/plugin/one-click-importer/upload"
-          :accepted-filetypes="['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'].join(',')"
-          :thumbnail-height=100
-          :thumbnail-width=200
-          :duplicate-check=true
-          :use-font-awesome=true
-          :showRemoveLink=false
-          :preview-template="previewTemplate"
-          v-on:vdropzone-success="onComplete"
-          v-on:vdropzone-error="onError"
-          v-on:duplicate-file="onDuplicate">
-
-            <div class="text-center">supported file types: xlsx, xls, csv, tsv</div>
-        </dropzone>
+        <!--<div class="text-center">supported file types: xlsx, xls, csv, tsv</div>-->
+        <!--</dropzone>-->
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <div id="alert-container">
-          <div v-if="message" class="alert alert-warning" role="alert">{{message}}</div>
-        </div>
         <table class="table">
           <thead>
-            <th>file name</th>
-            <th>View data</th>
+          <th>file name</th>
+          <th>View data</th>
           </thead>
           <tbody>
-            <tr v-for="response in responses" >
-              <td>{{response.filename}}</td>
-              <td><a :href="response.url">View...</a></td>
-            </tr>
+          <tr v-for="response in responses">
+            <td>{{response.filename}}</td>
+            <td><a target="_blank" :href="response.url">View...</a></td>
+          </tr>
           </tbody>
         </table>
       </div>
@@ -60,29 +67,67 @@
 
 <script>
   import Dropzone from 'vue2-dropzone'
+  import fetch from 'isomorphic-fetch'
 
   export default {
     name: 'one-click-importer',
     data () {
       return {
+        file: null,
         responses: [],
-        message: null
+        error: null
       }
     },
     methods: {
-      onComplete (file, response) {
-        this.responses.push({url: '/menu/main/dataexplorer?entity=' + response.entityId, filename: response.baseFileName})
-        this.message = null
+      setFile: function (event) {
+        this.file = event.target.files[0]
       },
-      onDuplicate (file) {
-        this.message = 'Can not upload duplicate file [' + file.upload.filename + ']'
-      },
-      onError (file) {
-        this.message = 'Something went wrong when uploading [' + file.upload.filename + ']. Please contact an administrator'
-      },
-      previewTemplate () {
-        return '<div></div>'
+      importFile: function () {
+        const formData = new FormData()
+        formData.append('file', this.file)
+
+        const options = {
+          body: formData,
+          method: 'POST',
+          credentials: 'same-origin'
+        }
+
+        let self = this
+
+        fetch('/plugin/one-click-importer/upload', options).then(response => {
+          if (response.headers.get('content-type') === 'application/json') {
+            response.json().then(function (json) {
+              const feedback = response.ok ? json : json.errors[0].message
+              if (response.ok) {
+                self.responses.push({
+                  url: '/menu/main/dataexplorer?entity=' + feedback.entityId,
+                  filename: feedback.baseFileName
+                })
+              } else {
+                self.error = feedback
+              }
+            })
+          }
+        })
+        this.$refs.fileInput.value = null
       }
+
+//     FIXME onComplete (file, response) {
+//        this.responses.push({
+//          url: '/menu/main/dataexplorer?entity=' + response.entityId,
+//          filename: response.baseFileName
+//        })
+//        this.message = null
+//      },
+//      onDuplicate (file) {
+//        this.message = 'Can not upload duplicate file [' + file.upload.filename + ']'
+//      },
+//      onError (file) {
+//        this.message = 'Something went wrong when uploading [' + file.upload.filename + ']. Please contact an administrator'
+//      },
+//      previewTemplate () {
+//        return '<div></div>'
+//      }
     },
     components: {
       Dropzone
