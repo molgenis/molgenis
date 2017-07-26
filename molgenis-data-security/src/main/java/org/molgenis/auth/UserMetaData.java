0d17953be1ffdd7d4d2f0886051eaf16b4676086
sido <sido@haakma.org>
2017-07-26 14:25:29 +0200

    Merge branch 'feature/2fa' of https://github.com/sidohaakma/molgenis into feature/2fa
    # Conflicts:
    #	molgenis-security/src/main/java/org/molgenis/security/MolgenisWebAppSecurityConfig.java
    #	molgenis-security/src/main/resources/templates/login-modal.ftl

--- molgenis-data-security/src/main/java/org/molgenis/auth/UserMetaData.java
+++ molgenis-data-security/src/main/java/org/molgenis/auth/UserMetaData.java
@@ -24,8 +24,6 @@ public class UserMetaData extends SystemEntityType
 	public static final String SECRET_KEY = "secretKey";
 	public static final String EMAIL = "Email";
 	public static final String GOOGLEACCOUNTID = "googleAccountId";
-	public static final String ISUSING2FA = "isUsing2fa";
-	public static final String SECRET2FA = "secret2fa";
 	public static final String ACTIVATIONCODE = "activationCode";
 	public static final String ACTIVE = "active";
 	public static final String CHANGE_PASSWORD = "changePassword";
@@ -130,9 +128,6 @@ public class UserMetaData extends SystemEntityType
 									 .setDescription(
 											 "An identifier for the user, unique among all Google accounts and never reused.")
 									 .setNillable(true);
-		addAttribute(SECRET2FA).setLabel("2FA-key")
-									 .setDescription(
-											 "Secret key for 2 factor authentication.")
-									 .setNillable(true);
+
 	}
 }
