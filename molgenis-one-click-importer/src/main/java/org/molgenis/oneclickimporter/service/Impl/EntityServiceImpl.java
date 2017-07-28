2a65835edd25415d4413fc93fbd134486cc85b8b
Mark-de-Haan <markdehaan90@gmail.com>
2017-07-28 14:17:44 +0200

    use for loop instead of while

--- molgenis-one-click-importer/src/main/java/org/molgenis/oneclickimporter/service/Impl/EntityServiceImpl.java
+++ molgenis-one-click-importer/src/main/java/org/molgenis/oneclickimporter/service/Impl/EntityServiceImpl.java
@@ -7,7 +7,6 @@ import org.molgenis.data.meta.AttributeType;
 import org.molgenis.data.meta.MetaDataService;
 import org.molgenis.data.meta.model.*;
 import org.molgenis.data.populate.IdGenerator;
-import org.molgenis.data.support.AttributeUtils;
 import org.molgenis.oneclickimporter.model.Column;
 import org.molgenis.oneclickimporter.model.DataCollection;
 import org.molgenis.oneclickimporter.service.AttributeTypeService;
@@ -18,12 +17,12 @@ import org.molgenis.security.permission.PermissionSystemService;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.EntityManager.CreationMode.NO_POPULATE;
 import static org.molgenis.data.meta.model.EntityType.AttributeRole.ROLE_ID;
+import static org.molgenis.data.support.AttributeUtils.getValidIdAttributeTypes;
 
 @Component
 public class EntityServiceImpl implements EntityService
@@ -70,7 +69,6 @@ public class EntityServiceImpl implements EntityService
 		// Create a dataTable
 		EntityType entityType = entityTypeFactory.create();
 
-
 		org.molgenis.data.meta.model.Package package_ = metaDataService.getPackage(packageName);
 		if (package_ == null)
 		{
@@ -89,17 +87,11 @@ public class EntityServiceImpl implements EntityService
 		final boolean isFirstColumnUnique = oneClickImporterService.hasUniqueValues(firstColumn);
 
 		AttributeType type = attributeTypeService.guessAttributeType(firstColumn.getDataValues());
-		final boolean isValidAttributeType = AttributeUtils.getValidIdAttributeTypes().contains(type);
+		final boolean isValidAttributeType = getValidIdAttributeTypes().contains(type);
 		final boolean useAutoId = !isFirstColumnUnique || !isValidAttributeType;
 
-		if (useAutoId)
-		{
-			entityType.addAttribute(createIdAttribute(), ROLE_ID);
-		}
-		else
-		{
-			entityType.addAttribute(createAttribute(firstColumn), ROLE_ID);
-		}
+		Attribute idAttribute = useAutoId ? createIdAttribute() : createAttribute(firstColumn);
+		entityType.addAttribute(idAttribute, ROLE_ID);
 
 		// Add all columns to the dataTable
 		columns.forEach(column ->
@@ -115,19 +107,22 @@ public class EntityServiceImpl implements EntityService
 		permissionSystemService.giveUserWriteMetaPermissions(entityType);
 		metaDataService.addEntityType(entityType);
 
-		AtomicInteger rowIndex = new AtomicInteger(0);
 		List<Entity> rows = newArrayList();
-		while (rowIndex.get() < dataCollection.getColumns().get(0).getDataValues().size())
+		int numberOfRows = dataCollection.getColumns().get(0).getDataValues().size();
+
+		for (int index = 0; index < numberOfRows; index++)
 		{
 			Entity row = entityManager.create(entityType, NO_POPULATE);
+
 			if (useAutoId)
 			{
 				row.setIdValue(idGenerator.generateId());
 			}
 
-			final int index = rowIndex.getAndIncrement();
-			columns.forEach(column -> setRowValueForAttribute(row, index, column));
-
+			for (Column column : columns)
+			{
+				setRowValueForAttribute(row, index, column);
+			}
 			rows.add(row);
 		}
 		dataService.add(entityType.getId(), rows.stream());
