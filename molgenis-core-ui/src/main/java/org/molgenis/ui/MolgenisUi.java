c7a40bab9e2c3e7869bfb879294962ff59000e14
Bart Charbon <b.charbon@umcg.nl>
2017-08-04 08:53:39 +0200

    Add functionality to get menu in json for new Vue MOLGENIS menu

--- molgenis-core-ui/src/main/java/org/molgenis/ui/MolgenisUi.java
+++ molgenis-core-ui/src/main/java/org/molgenis/ui/MolgenisUi.java
@@ -1,5 +1,9 @@
 package org.molgenis.ui;
 
+import com.google.gson.Gson;
+import org.molgenis.ui.menu.MenuUtils;
+import org.molgenis.ui.menu.json.MenuItem;
+
 public interface MolgenisUi
 {
 	/**
@@ -13,4 +17,9 @@ public interface MolgenisUi
 	 * @return menu or null if menu does not exist or is not accessible to user
 	 */
 	MolgenisUiMenu getMenu(String menuId);
+
+	default String getMenuJson()
+	{
+		return MenuUtils.getMenuJson(getMenu());
+	}
 }
