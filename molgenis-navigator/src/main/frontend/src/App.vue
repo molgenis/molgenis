29f114dafb973a01e2ff639698dc6d33ca1388c1
Bart Charbon <b.charbon@umcg.nl>
2017-08-04 13:27:41 +0200

    Add MOLGENIS header and footer to vue based plugins

--- molgenis-navigator/src/main/frontend/src/App.vue
+++ molgenis-navigator/src/main/frontend/src/App.vue
@@ -1,21 +1,7 @@
 <template>
-
-  <div class="container-fluid">
-    <div class="row temp-header">
-      <div class="col">
-        <a class="btn pull-left" href="/">
-          <i class="fa fa-chevron-left" aria-hidden="true"></i> back to home
-        </a>
-      </div>
-    </div>
-
-    <h1 style="padding-top: 1rem; text-align: center">Navigator</h1>
-
     <div id="app">
       <router-view></router-view>
     </div>
-
-  </div>
 </template>
 
 <style>
