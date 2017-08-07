074e681894967fd68a3602723a670b9a548f4e90
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-08-07 10:30:58 +0200

    RepositoryCollectionDecorator and IndexActionRepositoryCollectionDecorator extend from AbstractRepositoryCollectionDecorator

--- molgenis-data/src/main/java/org/molgenis/data/RepositoryCollectionDecorator.java
+++ molgenis-data/src/main/java/org/molgenis/data/RepositoryCollectionDecorator.java
@@ -1,10 +1,8 @@
 package org.molgenis.data;
 
-import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.data.meta.model.EntityType;
 
 import java.util.Iterator;
-import java.util.Set;
 import java.util.stream.StreamSupport;
 
 import static java.util.Objects.requireNonNull;
@@ -14,104 +12,42 @@ import static java.util.Spliterators.spliteratorUnknownSize;
 /**
  * Applies {@link Repository} decorators to all {@link RepositoryCollection} repositories.
  */
-class RepositoryCollectionDecorator implements RepositoryCollection
+class RepositoryCollectionDecorator extends AbstractRepositoryCollectionDecorator
 {
-	private final RepositoryCollection decoratedRepositoryCollection;
 	private final RepositoryDecoratorFactory repositoryDecoratorFactory;
 
-	RepositoryCollectionDecorator(RepositoryCollection decoratedRepositoryCollection,
+	RepositoryCollectionDecorator(RepositoryCollection delegateRepositoryCollection,
 			RepositoryDecoratorFactory repositoryDecoratorFactory)
 	{
-		this.decoratedRepositoryCollection = requireNonNull(decoratedRepositoryCollection);
+		super(delegateRepositoryCollection);
 		this.repositoryDecoratorFactory = requireNonNull(repositoryDecoratorFactory);
 	}
 
 	@Override
 	public Iterator<Repository<Entity>> iterator()
 	{
-		return StreamSupport.stream(spliteratorUnknownSize(decoratedRepositoryCollection.iterator(), ORDERED), false)
+		return StreamSupport.stream(spliteratorUnknownSize(delegate().iterator(), ORDERED), false)
 							.map(repositoryDecoratorFactory::createDecoratedRepository)
 							.iterator();
 	}
 
 	@Override
-	public String getName()
-	{
-		return decoratedRepositoryCollection.getName();
-	}
-
-	@Override
-	public Set<RepositoryCollectionCapability> getCapabilities()
-	{
-		return decoratedRepositoryCollection.getCapabilities();
-	}
-
-	@Override
 	public Repository<Entity> createRepository(EntityType entityType)
 	{
-		return repositoryDecoratorFactory.createDecoratedRepository(
-				decoratedRepositoryCollection.createRepository(entityType));
-	}
-
-	@Override
-	public Iterable<String> getEntityTypeIds()
-	{
-		return decoratedRepositoryCollection.getEntityTypeIds();
+		return repositoryDecoratorFactory.createDecoratedRepository(delegate().createRepository(entityType));
 	}
 
 	@Override
 	public Repository<Entity> getRepository(String id)
 	{
-		Repository<Entity> repository = decoratedRepositoryCollection.getRepository(id);
+		Repository<Entity> repository = delegate().getRepository(id);
 		return repository != null ? repositoryDecoratorFactory.createDecoratedRepository(repository) : null;
 	}
 
 	@Override
 	public Repository<Entity> getRepository(EntityType entityType)
 	{
-		Repository<Entity> repository = decoratedRepositoryCollection.getRepository(entityType);
+		Repository<Entity> repository = delegate().getRepository(entityType);
 		return repository != null ? repositoryDecoratorFactory.createDecoratedRepository(repository) : null;
-	}
-
-	@Override
-	public boolean hasRepository(String id)
-	{
-		return decoratedRepositoryCollection.hasRepository(id);
-	}
-
-	@Override
-	public boolean hasRepository(EntityType entityType)
-	{
-		return decoratedRepositoryCollection.hasRepository(entityType);
-	}
-
-	@Override
-	public void deleteRepository(EntityType entityType)
-	{
-		decoratedRepositoryCollection.deleteRepository(entityType);
-	}
-
-	@Override
-	public void updateRepository(EntityType entityType, EntityType updatedEntityType)
-	{
-		decoratedRepositoryCollection.updateRepository(entityType, updatedEntityType);
-	}
-
-	@Override
-	public void addAttribute(EntityType entityType, Attribute attribute)
-	{
-		decoratedRepositoryCollection.addAttribute(entityType, attribute);
-	}
-
-	@Override
-	public void updateAttribute(EntityType entityType, Attribute attr, Attribute updatedAttr)
-	{
-		decoratedRepositoryCollection.updateAttribute(entityType, attr, updatedAttr);
-	}
-
-	@Override
-	public void deleteAttribute(EntityType entityType, Attribute attr)
-	{
-		decoratedRepositoryCollection.deleteAttribute(entityType, attr);
 	}
 }
