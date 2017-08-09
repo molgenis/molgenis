961099d6872ef399622b4997e049e74dcf840817
Bart Charbon <b.charbon@umcg.nl>
2017-08-09 11:32:20 +0200

    Store WIP

---
+++ molgenis-searchall/src/main/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div id="app">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'molgenis-app'
+  }
+</script>
---
+++ molgenis-searchall/src/main/frontend/src/store/state.js
@@ -0,0 +1,10 @@
+// @flow
+import type { State } from './utils/flow.types'
+
+export const INITIAL_STATE = window.__INITIAL_STATE__ || {}
+
+const state: State = {
+  message: INITIAL_STATE.message
+}
+
+export default state
