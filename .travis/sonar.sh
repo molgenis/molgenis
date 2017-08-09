c59e87debb6fe65cc0b1e229d1bbff404a481f8b
Fleur Kelpin <fleur@kelpin.nl>
2017-08-09 14:07:34 +0200

    Update sonar.sh

--- .travis/sonar.sh
+++ .travis/sonar.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 if [ "${TRAVIS_REPO_SLUG}" == "molgenis/molgenis" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
 	echo "Running sonar analysis in maven..."
-    ./mvnw sonar:sonar --batch-mode --quiet
+    ./mvnw sonar:sonar \
+        -Dsonar.organization=molgenis \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login=${SONAR_TOKEN} \
+        --batch-mode --quiet
     echo "Done."
 else
     echo "Skipped Sonar analysis (only runs for committed builds on the molgenis/molgenis repo)"
