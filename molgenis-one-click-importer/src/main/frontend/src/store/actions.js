97d0430b21c516ab21f37610b26cde38161d0f48
Mark-de-Haan <markdehaan90@gmail.com>
2017-09-08 10:07:31 +0200

    Add action test dependency to one-click-importer

--- molgenis-one-click-importer/src/main/frontend/src/store/actions.js
+++ molgenis-one-click-importer/src/main/frontend/src/store/actions.js
@@ -2,24 +2,24 @@ import api from '@molgenis/molgenis-api-client'
 import { ADD_FINISHED_JOB, UPDATE_JOB } from './mutations'
 
 export const IMPORT_FILE = '__IMPORT_FILE__'
-export const POLL_JOB_URL = '__POLL_JOB_URL__'
+
+const pollJobUrl = (commit, dispatch, jobUrl) => {
+  api.get(jobUrl).then(job => {
+    commit(UPDATE_JOB, job)
+    if (job.status === 'RUNNING' || job.status === 'PENDING') {
+      setTimeout(function () {
+        pollJobUrl(commit, dispatch, jobUrl)
+      }, 1000)
+    } else {
+      commit(ADD_FINISHED_JOB, job)
+    }
+  })
+}
 
 export default {
-  [IMPORT_FILE] ({dispatch}, file) {
+  [IMPORT_FILE] ({commit, dispatch}, file) {
     api.postFile('/plugin/one-click-importer/upload', file).then(response => {
-      dispatch(POLL_JOB_URL, response)
-    })
-  },
-  [POLL_JOB_URL] ({dispatch, commit}, jobUrl) {
-    api.get(jobUrl).then(job => {
-      commit(UPDATE_JOB, job)
-      if (job.status === 'RUNNING' || job.status === 'PENDING') {
-        setTimeout(function () {
-          dispatch(POLL_JOB_URL, jobUrl)
-        }, 1000)
-      } else {
-        commit(ADD_FINISHED_JOB, job)
-      }
+      pollJobUrl(commit, dispatch, response)
     })
   }
 }
--- molgenis-one-click-importer/src/main/frontend/test/unit/specs/store/actions.spec.js
+++ molgenis-one-click-importer/src/main/frontend/test/unit/specs/store/actions.spec.js
@@ -1,110 +1,104 @@
-import testAction from '../../utils/action.utils'
+import utils from '@molgenis/molgenis-vue-test-utils'
 import td from 'testdouble'
 import api from '@molgenis/molgenis-api-client'
-import actions, { POLL_JOB_URL } from 'store/actions'
+import actions from 'store/actions'
 import { ADD_FINISHED_JOB, UPDATE_JOB } from 'store/mutations'
 
 describe('actions', () => {
   describe('Testing action IMPORT_FILE', () => {
     afterEach(() => td.reset())
 
-    it('Should send a file to the server and receive a job URL', done => {
+    it('Should send a file to the server and poll twice due to RUNNING job', done => {
       const file = 'test-file'
       const jobUrl = '/api/v2/job/test_job'
+      const job1 = {
+        id: '1',
+        status: 'RUNNING'
+      }
+
+      const job2 = {
+        id: '1',
+        status: 'FINISHED'
+      }
 
       const postFile = td.function('api.postFile')
       td.when(postFile('/plugin/one-click-importer/upload', file)).thenResolve(jobUrl)
       td.replace(api, 'postFile', postFile)
 
-      testAction(actions.__IMPORT_FILE__, file, {}, [], [{type: POLL_JOB_URL, payload: jobUrl}], done)
-    })
-  })
-
-  describe('Testing action POLL_JOB_URL', () => {
-    afterEach(() => td.reset())
-
-    it('RUNNING Job', done => {
-      const jobUrl = '/api/v2/job/test_job'
-      const job = {
-        id: '1',
-        status: 'RUNNING'
-      }
-
-      const state = {
-        job: null
-      }
-
       const get = td.function('api.get')
-      td.when(get(jobUrl)).thenResolve(job)
+      td.when(get(jobUrl)).thenResolve(job1, job2)
       td.replace(api, 'get', get)
 
-      testAction(actions.__POLL_JOB_URL__, jobUrl, state, [{type: UPDATE_JOB, payload: job}], [{
-        type: POLL_JOB_URL,
-        payload: jobUrl
-      }], done)
+      const options = {
+        payload: file,
+        expectedMutations: [
+          {type: UPDATE_JOB, payload: job1},
+          {type: UPDATE_JOB, payload: job2},
+          {type: ADD_FINISHED_JOB, payload: job2}
+        ]
+      }
+
+      utils.testAction(actions.__IMPORT_FILE__, options, done)
     })
 
-    it('PENDING Job', done => {
+    it('Should send a file to the server and poll twice due to PENDING job', done => {
+      const file = 'test-file'
       const jobUrl = '/api/v2/job/test_job'
-      const job = {
+      const job1 = {
         id: '1',
         status: 'PENDING'
       }
 
-      const state = {
-        job: null
-      }
-
-      const get = td.function('api.get')
-      td.when(get(jobUrl)).thenResolve(job)
-      td.replace(api, 'get', get)
-
-      testAction(actions.__POLL_JOB_URL__, jobUrl, state, [{type: UPDATE_JOB, payload: job}], [{
-        type: POLL_JOB_URL,
-        payload: jobUrl
-      }], done)
-    })
-
-    it('FINISHED Job', done => {
-      const jobUrl = '/api/v2/job/test_job'
-      const job = {
+      const job2 = {
         id: '1',
         status: 'FINISHED'
       }
 
-      const state = {
-        job: null
-      }
+      const postFile = td.function('api.postFile')
+      td.when(postFile('/plugin/one-click-importer/upload', file)).thenResolve(jobUrl)
+      td.replace(api, 'postFile', postFile)
 
       const get = td.function('api.get')
-      td.when(get(jobUrl)).thenResolve(job)
+      td.when(get(jobUrl)).thenResolve(job1, job2)
       td.replace(api, 'get', get)
 
-      testAction(actions.__POLL_JOB_URL__, jobUrl, state, [
-        {type: UPDATE_JOB, payload: job},
-        {type: ADD_FINISHED_JOB, payload: job}
-      ], [], done)
+      const options = {
+        payload: file,
+        expectedMutations: [
+          {type: UPDATE_JOB, payload: job1},
+          {type: UPDATE_JOB, payload: job2},
+          {type: ADD_FINISHED_JOB, payload: job2}
+        ]
+      }
+
+      utils.testAction(actions.__IMPORT_FILE__, options, done)
     })
 
-    it('FAILED Job', done => {
+    it('Should send a file to the server and fail the job instantly', done => {
+      const file = 'test-file'
       const jobUrl = '/api/v2/job/test_job'
       const job = {
         id: '1',
         status: 'FAILED'
       }
 
-      const state = {
-        job: null
-      }
+      const postFile = td.function('api.postFile')
+      td.when(postFile('/plugin/one-click-importer/upload', file)).thenResolve(jobUrl)
+      td.replace(api, 'postFile', postFile)
 
       const get = td.function('api.get')
       td.when(get(jobUrl)).thenResolve(job)
       td.replace(api, 'get', get)
 
-      testAction(actions.__POLL_JOB_URL__, jobUrl, state, [
-        {type: UPDATE_JOB, payload: job},
-        {type: ADD_FINISHED_JOB, payload: job}
-      ], [], done)
+      const options = {
+        payload: file,
+        expectedMutations: [
+          {type: UPDATE_JOB, payload: job},
+          {type: ADD_FINISHED_JOB, payload: job}
+        ]
+      }
+
+      utils.testAction(actions.__IMPORT_FILE__, options, done)
     })
   })
 })
