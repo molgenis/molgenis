4a6432971998ea25e8359ba1d3813368be9a95ff
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-09-14 08:25:34 +0200

    Fix missing import

--- molgenis-dataexplorer/src/main/java/org/molgenis/dataexplorer/controller/DirectoryController.java
+++ molgenis-dataexplorer/src/main/java/org/molgenis/dataexplorer/controller/DirectoryController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
