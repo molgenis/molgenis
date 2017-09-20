57beb3b77cc3b8167e43aff843cd90de5ad714fb
Fleur Kelpin <f.kelpin@umcg.nl>
2017-09-20 10:37:30 +0200

    Add resource links

--- molgenis-security-ui/src/main/frontend/src/components/ACLs.vue
+++ molgenis-security-ui/src/main/frontend/src/components/ACLs.vue
@@ -1,39 +1,41 @@
 <template>
   <div v-if="acls.length">
-  <table class="table table-sm table-responsive">
-    <thead>
-    <tr>
-      <th>{{'ROW' | i18n}}</th>
-      <th>{{'OWNER' | i18n}}</th>
-      <th :colspan="permissions.length + 1" class="text-center">{{'PERMISSIONS' | i18n}}</th>
-    </tr>
-    <tr>
-      <th></th>
-      <th></th>
-      <th class="text-center">{{'GRANTING' | i18n}}</th>
-      <th v-for="permission in permissions" class="text-center">{{permission | capitalizeFirstLetter}}</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(acl, rowIndex) in acls">
-      <td>{{acl.entityLabel || acl.entityId}}</td>
-      <td>{{acl.owner}}</td>
-      <td class="text-center" @click="onGrantingClick({rowIndex, aceIndex: acl.aceIndex})">
-        <i v-if="acl.granting" class="fa fa-unlock green"></i>
-        <i class="fa fa-lock red" v-else></i></td>
-      <td v-for="permission in permissions" class="text-center">
-        <div class="form-check">
-          <label class="form-check-label">
-            <input class="form-check-input"
-                   type="checkbox"
-                   :checked="acl[permission]"
-                   @click="onPermissionClick({rowIndex, aceIndex: acl.aceIndex, permission})">
-          </label>
-        </div>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+    <table class="table table-sm table-responsive">
+      <thead>
+      <tr>
+        <th>{{'ROW' | i18n}}</th>
+        <th>{{'OWNER' | i18n}}</th>
+        <th :colspan="permissions.length + 1" class="text-center">{{'PERMISSIONS' | i18n}}</th>
+      </tr>
+      <tr>
+        <th></th>
+        <th></th>
+        <th class="text-center">{{'GRANTING' | i18n}}</th>
+        <th v-for="permission in permissions" class="text-center">{{permission | capitalizeFirstLetter}}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(acl, rowIndex) in acls">
+        <td>
+          <router-link :to="linkToResource(rowIndex)">{{acl.entityLabel || acl.entityId}}</router-link>
+        </td>
+        <td>{{acl.owner}}</td>
+        <td class="text-center" @click="onGrantingClick({rowIndex, aceIndex: acl.aceIndex})">
+          <i v-if="acl.granting" class="fa fa-unlock green"></i>
+          <i class="fa fa-lock red" v-else></i></td>
+        <td v-for="permission in permissions" class="text-center">
+          <div class="form-check">
+            <label class="form-check-label">
+              <input class="form-check-input"
+                     type="checkbox"
+                     :checked="acl[permission]"
+                     @click="onPermissionClick({rowIndex, aceIndex: acl.aceIndex, permission})">
+            </label>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
   <div v-else>
     <p>{{'EMPTY_TABLE' | i18n}}</p>
@@ -41,6 +43,8 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   export default {
     name: 'acls',
     props: {
@@ -48,6 +52,17 @@
       acls: {type: Array},
       onPermissionClick: {type: Function},
       onGrantingClick: {type: Function}
+    },
+    methods: {
+      linkToResource (rowIndex) {
+        const acl = this.rows[rowIndex]
+        const entityTypeId = this.selectedEntityTypeId
+        const entityId = acl.entity
+        return `/resource/${entityTypeId}/${entityId}`
+      }
+    },
+    computed: {
+      ...mapState(['rows', 'selectedEntityTypeId'])
     },
     filters: {
       capitalizeFirstLetter (string: string): string { return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase() }
