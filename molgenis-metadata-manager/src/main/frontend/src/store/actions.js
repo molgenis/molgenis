e3fad905f28bdb13d4d4e0334ba0dafedf86fc04
Mark-de-Haan <markdehaan90@gmail.com>
2017-09-22 11:37:08 +0200

    Fix #6672 When adding attribute with ref entity to table, the complete table breaks

--- molgenis-metadata-manager/src/main/frontend/src/store/actions.js
+++ molgenis-metadata-manager/src/main/frontend/src/store/actions.js
@@ -84,7 +84,6 @@ export default {
     api.get('/plugin/metadata-manager/editorPackages').then(response => {
       commit(SET_PACKAGES, response)
     }, error => {
-      console.log(error)
       commit(CREATE_ALERT, {type: 'error', message: error})
     })
   },
@@ -103,7 +102,8 @@ export default {
    */
   [GET_ATTRIBUTE_TYPES] ({commit}: { commit: Function }) {
     api.get('/api/v2/sys_md_Attribute/meta/type').then(response => {
-      commit(SET_ATTRIBUTE_TYPES, response.enumOptions.map((type) => type.toUpperCase()))
+      // Workaround for differences between Categorical MREF enum on the server and in the REST api
+      commit(SET_ATTRIBUTE_TYPES, response.enumOptions.map(type => type === 'categoricalmref' ? 'CATEGORICAL_MREF' : type.toUpperCase()))
     }, error => {
       commit(CREATE_ALERT, {type: 'error', message: error})
     })
