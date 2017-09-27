b201203e068a747f30e977ec44c563069879e981
Sido Haakma <sido@haakma.org>
2017-09-27 16:33:56 +0200

    [FIX] Check per setting if it is present in the database

--- molgenis-bootstrap/src/main/java/org/molgenis/bootstrap/populate/GenomeBrowserAttributesPopulator.java
+++ molgenis-bootstrap/src/main/java/org/molgenis/bootstrap/populate/GenomeBrowserAttributesPopulator.java
@@ -21,15 +21,22 @@ public class GenomeBrowserAttributesPopulator
 
 	public void populate()
 	{
-		// Only apply default settings if table is empty
-		if (dataService.count(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES) == 0)
+		// Only apply default settings if table does not contain the genomebrowser setting
+		if(dataService.findOneById(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES, "simple1") == null)
 		{
 			dataService.add(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES,
 					gbaFactory.create("simple1", true, 0, "start", "chr", null, null, null));
+		}
+		if(dataService.findOneById(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES, "VCF") == null) {
 			dataService.add(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES,
 					gbaFactory.create("VCF", true, 1, "POS", "#CHROM", "REF", "ALT", null));
+		}
+		if(dataService.findOneById(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES, "full") == null)
+		{
 			dataService.add(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES,
 					gbaFactory.create("full", true, 2, "start", "chr", "ref", "alt", "stop"));
+		}
+		if(dataService.findOneById(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES, "simple2") == null) {
 			dataService.add(GenomeBrowserAttributesMetadata.GENOMEBROWSERATTRIBUTES,
 					gbaFactory.create("simple2", true, 3, "POS", "#CHROM", null, null, null));
 		}
