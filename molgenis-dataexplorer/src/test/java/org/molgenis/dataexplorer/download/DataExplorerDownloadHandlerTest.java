137a3dc444ae608fd38ca3a71c59d980e7cf98a9
Bart Charbon <b.charbon@umcg.nl>
2017-10-02 14:38:08 +0200

    add test for success path

--- molgenis-dataexplorer/src/test/java/org/molgenis/dataexplorer/download/DataExplorerDownloadHandlerTest.java
+++ molgenis-dataexplorer/src/test/java/org/molgenis/dataexplorer/download/DataExplorerDownloadHandlerTest.java
@@ -17,8 +17,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Collections;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.molgenis.dataexplorer.controller.DataRequest.ColNames.ATTRIBUTE_LABELS;
+import static org.molgenis.dataexplorer.controller.DataRequest.EntityValues.ENTITY_IDS;
 
 public class DataExplorerDownloadHandlerTest extends AbstractMockitoTest
 {
@@ -36,7 +37,7 @@ public class DataExplorerDownloadHandlerTest extends AbstractMockitoTest
 	}
 
 	@Test(expectedExceptions = MolgenisDataException.class, expectedExceptionsMessageRegExp = "Total number of cells for this download exceeds the maximum of 500000 for .xlsx downloads, please use .csv instead")
-	public void testWriteToExcel() throws Exception
+	public void testWriteToExcelFail() throws Exception
 	{
 		EntityType entityType = mock(EntityType.class);
 		Attribute attribute = mock(Attribute.class);
@@ -52,6 +53,33 @@ public class DataExplorerDownloadHandlerTest extends AbstractMockitoTest
 		when(dataRequest.getQuery()).thenReturn(new QueryImpl<>());
 
 		dataExplorerDownloadHandler.writeToExcel(dataRequest, mock(OutputStream.class));
+	}
+
+	@Test
+	public void testWriteToExcel() throws Exception
+	{
+		EntityType entityType = mock(EntityType.class);
+		Attribute attribute = mock(Attribute.class);
+		when(attribute.getName()).thenReturn("attr");
+		MetaDataService metaDataService = mock(MetaDataService.class);
+
+		when(dataService.getMeta()).thenReturn(metaDataService);
+		when(dataService.count("sys_set_thousandgenomes", new QueryImpl<>())).thenReturn(5L);
+		when(dataService.getEntityType("sys_set_thousandgenomes")).thenReturn(entityType);
+		when(metaDataService.getEntityTypeById("sys_set_thousandgenomes")).thenReturn(entityType);
+		when(entityType.getAllAttributes()).thenReturn(Collections.singletonList(attribute));
+
+		DataRequest dataRequest = mock(DataRequest.class);
+		when(dataRequest.getEntityName()).thenReturn("sys_set_thousandgenomes");
+		when(dataRequest.getQuery()).thenReturn(new QueryImpl<>());
+		when(dataRequest.getAttributeNames()).thenReturn(Collections.singletonList("attr"));
+		when(dataRequest.getColNames()).thenReturn(ATTRIBUTE_LABELS);
+		when(dataRequest.getEntityValues()).thenReturn(ENTITY_IDS);
+
+		when(entityType.getAtomicAttributes()).thenReturn(Collections.singletonList(attribute));
+		OutputStream out = mock(OutputStream.class);
+		dataExplorerDownloadHandler.writeToExcel(dataRequest, out);
+		verify(out, atLeastOnce()).flush();
 	}
 
 }
