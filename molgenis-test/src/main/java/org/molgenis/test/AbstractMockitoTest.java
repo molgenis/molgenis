6e8a9ddfec64c76ac0d8c4585c6aa9d4fd5faed1
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-11-03 11:33:43 +0100

    Use MockitoSession with default strict stubs validation

--- molgenis-test/src/main/java/org/molgenis/test/AbstractMockitoTest.java
+++ molgenis-test/src/main/java/org/molgenis/test/AbstractMockitoTest.java
@@ -1,13 +1,38 @@
 package org.molgenis.test;
 
-import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoSession;
+import org.mockito.quality.Strictness;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import static java.util.Objects.requireNonNull;
+import static org.mockito.Mockito.mockitoSession;
 
 public class AbstractMockitoTest
 {
+	private final Strictness strictness;
+
+	private MockitoSession mockitoSession;
+
+	public AbstractMockitoTest()
+	{
+		this(Strictness.STRICT_STUBS);
+	}
+
+	public AbstractMockitoTest(Strictness strictness)
+	{
+		this.strictness = requireNonNull(strictness);
+	}
+
 	@BeforeMethod
 	public void initMocks()
 	{
-		MockitoAnnotations.initMocks(this);
+		mockitoSession = mockitoSession().initMocks(this).strictness(strictness).startMocking();
+	}
+
+	@AfterMethod
+	public void tearDownAfterMethod()
+	{
+		mockitoSession.finishMocking();
 	}
 }
