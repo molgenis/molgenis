7288f8995b1896516a3516d55d3edb12b0a2a771
Bart Charbon <b.charbon@umcg.nl>
2017-11-06 13:46:07 +0100

    M5205: remove unused plugins: Model Registry

--- molgenis-model-registry/src/main/resources/js/chrome-getTransformToElement-polyfill.js
+++
@@ -1,5 +0,0 @@
-// Polyfill for Chrome 48 and up to provide method getTransformToElement used by joint-js
-// see http://jointjs.com/blog/get-transform-to-element-polyfill.html
-SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(toElement) {
-    return toElement.getScreenCTM().inverse().multiply(this.getScreenCTM());
-};
