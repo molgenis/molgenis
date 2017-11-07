deaa20a4dc274fda5cd575d17cf6b5b5e89e3561
Fleur Kelpin <f.kelpin@umcg.nl>
2017-11-07 11:14:39 +0100

    Fix sonar bugs in postgres module.

--- molgenis-data-postgresql/src/test/java/org/molgenis/data/postgresql/PostgreSqlRepositoryTest.java
+++ molgenis-data-postgresql/src/test/java/org/molgenis/data/postgresql/PostgreSqlRepositoryTest.java
@@ -1,5 +1,7 @@
 package org.molgenis.data.postgresql;
 
+import org.mockito.Mock;
+import org.mockito.MockitoSession;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Query;
 import org.molgenis.data.QueryRule;
@@ -9,7 +11,7 @@ import org.molgenis.data.validation.MolgenisValidationException;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,6 +22,7 @@ import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.mockito.Mockito.*;
+import static org.mockito.quality.Strictness.STRICT_STUBS;
 import static org.molgenis.data.QueryRule.Operator.EQUALS;
 import static org.molgenis.data.meta.AttributeType.*;
 import static org.testng.Assert.assertEquals;
@@ -27,17 +30,32 @@ import static org.testng.Assert.assertEquals;
 public class PostgreSqlRepositoryTest
 {
 	private PostgreSqlRepository postgreSqlRepo;
+	@Mock
 	private JdbcTemplate jdbcTemplate;
+	@Mock
 	private PostgreSqlEntityFactory postgreSqlEntityFactory;
+	@Mock
+	private DataSource dataSource;
+	@Mock
+	private EntityType entityType;
+	@Mock
+	private Query<Entity> query;
+	@Mock
+	private RowMapper<Entity> rowMapper;
+
+	private MockitoSession mockitoSession;
 
 	@BeforeMethod
 	public void setUpBeforeMethod() throws Exception
 	{
-		postgreSqlEntityFactory = mock(PostgreSqlEntityFactory.class);
-		jdbcTemplate = mock(JdbcTemplate.class);
-		DataSource dataSource = mock(DataSource.class);
-		PlatformTransactionManager transactionManager = mock(PlatformTransactionManager.class);
-		postgreSqlRepo = new PostgreSqlRepository(postgreSqlEntityFactory, jdbcTemplate, dataSource);
+		mockitoSession = mockitoSession().initMocks(this).strictness(STRICT_STUBS).startMocking();
+		postgreSqlRepo = new PostgreSqlRepository(postgreSqlEntityFactory, jdbcTemplate, dataSource, entityType);
+	}
+
+	@AfterMethod
+	public void afterMethod()
+	{
+		mockitoSession.finishMocking();
 	}
 
 	// TODO test all query operators for one-to-many
@@ -45,10 +63,34 @@ public class PostgreSqlRepositoryTest
 	public void countQueryOneToManyEquals() throws Exception
 	{
 		String oneToManyAttrName = "oneToManyAttr";
-		EntityType entityType = createEntityMetaOneToMany(oneToManyAttrName);
+		String refIdAttrName = "refEntityId";
+		Attribute refIdAttr = mock(Attribute.class);
+		when(refIdAttr.getName()).thenReturn(refIdAttrName);
+		when(refIdAttr.getDataType()).thenReturn(INT);
 
-		@SuppressWarnings("unchecked")
-		Query<Entity> query = mock(Query.class);
+		String xrefAttrName = "xrefAttr";
+		Attribute xrefAttr = mock(Attribute.class);
+		when(xrefAttr.getName()).thenReturn(xrefAttrName);
+
+		EntityType refEntityMeta = mock(EntityType.class);
+		when(refEntityMeta.getId()).thenReturn("refEntityId");
+		when(refEntityMeta.getIdAttribute()).thenReturn(refIdAttr);
+
+		String idAttrName = "entityId";
+		Attribute idAttr = mock(Attribute.class);
+		when(idAttr.getName()).thenReturn(idAttrName);
+
+		Attribute oneToManyAttr = mock(Attribute.class);
+		when(oneToManyAttr.getName()).thenReturn(oneToManyAttrName);
+		when(oneToManyAttr.getDataType()).thenReturn(ONE_TO_MANY);
+		when(oneToManyAttr.getRefEntity()).thenReturn(refEntityMeta);
+		when(oneToManyAttr.isMappedBy()).thenReturn(true);
+		when(oneToManyAttr.getMappedBy()).thenReturn(xrefAttr);
+
+		when(entityType.getId()).thenReturn("entityId");
+		when(entityType.getIdAttribute()).thenReturn(idAttr);
+		doReturn(oneToManyAttr).when(entityType).getAttribute(oneToManyAttrName);
+
 		int queryValue = 2;
 		QueryRule queryRule = new QueryRule(oneToManyAttrName, EQUALS, queryValue);
 		when(query.getRules()).thenReturn(singletonList(queryRule));
@@ -56,7 +98,7 @@ public class PostgreSqlRepositoryTest
 		String sql = "SELECT COUNT(DISTINCT this.\"entityId\") FROM \"entityId#fc2928f6\" AS this LEFT JOIN \"refEntityId#07f902bf\" AS \"oneToManyAttr_filter1\" ON (this.\"entityId\" = \"oneToManyAttr_filter1\".\"xrefAttr\") WHERE \"oneToManyAttr_filter1\".\"refEntityId\" = ?";
 		long count = 123L;
 		when(jdbcTemplate.queryForObject(sql, new Object[] { queryValue }, Long.class)).thenReturn(count);
-		postgreSqlRepo.setEntityType(entityType);
+
 		assertEquals(postgreSqlRepo.count(query), count);
 	}
 
@@ -64,21 +106,48 @@ public class PostgreSqlRepositoryTest
 	public void findAllQueryOneToManyEquals() throws Exception
 	{
 		String oneToManyAttrName = "oneToManyAttr";
-		EntityType entityType = createEntityMetaOneToMany(oneToManyAttrName);
+		String refIdAttrName = "refEntityId";
+		Attribute refIdAttr = mock(Attribute.class);
+		when(refIdAttr.getName()).thenReturn(refIdAttrName);
+		when(refIdAttr.getDataType()).thenReturn(INT);
 
-		@SuppressWarnings("unchecked")
-		Query<Entity> query = mock(Query.class);
+		String xrefAttrName = "xrefAttr";
+		Attribute xrefAttr = mock(Attribute.class);
+		when(xrefAttr.getName()).thenReturn(xrefAttrName);
+
+		EntityType refEntityMeta = mock(EntityType.class);
+		when(refEntityMeta.getId()).thenReturn("refEntityId");
+		when(refEntityMeta.getIdAttribute()).thenReturn(refIdAttr);
+		doReturn(refIdAttr).when(refEntityMeta).getAttribute(refIdAttrName);
+
+		String idAttrName = "entityId";
+		Attribute idAttr = mock(Attribute.class);
+		when(idAttr.getName()).thenReturn(idAttrName);
+		when(idAttr.getDataType()).thenReturn(STRING);
+
+		Attribute oneToManyAttr = mock(Attribute.class);
+		when(oneToManyAttr.getName()).thenReturn(oneToManyAttrName);
+		when(oneToManyAttr.getDataType()).thenReturn(ONE_TO_MANY);
+		when(oneToManyAttr.getRefEntity()).thenReturn(refEntityMeta);
+		when(oneToManyAttr.isMappedBy()).thenReturn(true);
+		when(oneToManyAttr.getMappedBy()).thenReturn(xrefAttr);
+
+		when(entityType.getId()).thenReturn("entityId");
+		when(entityType.getIdAttribute()).thenReturn(idAttr);
+		doReturn(oneToManyAttr).when(entityType).getAttribute(oneToManyAttrName);
+		when(entityType.getAtomicAttributes()).thenReturn(newArrayList(idAttr, oneToManyAttr));
+		EntityType entityType = this.entityType;
+		postgreSqlRepo = new PostgreSqlRepository(postgreSqlEntityFactory, jdbcTemplate, dataSource, entityType);
+
 		int queryValue = 2;
 		QueryRule queryRule = new QueryRule(oneToManyAttrName, EQUALS, queryValue);
 		when(query.getRules()).thenReturn(singletonList(queryRule));
 
 		String sql = "SELECT DISTINCT this.\"entityId\", (SELECT array_agg(\"refEntityId\" ORDER BY \"refEntityId\" ASC) FROM \"refEntityId#07f902bf\" WHERE this.\"entityId\" = \"refEntityId#07f902bf\".\"xrefAttr\") AS \"oneToManyAttr\" FROM \"entityId#fc2928f6\" AS this LEFT JOIN \"refEntityId#07f902bf\" AS \"oneToManyAttr_filter1\" ON (this.\"entityId\" = \"oneToManyAttr_filter1\".\"xrefAttr\") WHERE \"oneToManyAttr_filter1\".\"refEntityId\" = ?  LIMIT 1000";
-		@SuppressWarnings("unchecked")
-		RowMapper<Entity> rowMapper = mock(RowMapper.class);
+
 		when(postgreSqlEntityFactory.createRowMapper(entityType, null)).thenReturn(rowMapper);
 		Entity entity0 = mock(Entity.class);
 		when(jdbcTemplate.query(sql, new Object[] { queryValue }, rowMapper)).thenReturn(singletonList(entity0));
-		postgreSqlRepo.setEntityType(entityType);
 		assertEquals(postgreSqlRepo.findAll(query).collect(toList()), singletonList(entity0));
 	}
 
@@ -89,21 +158,15 @@ public class PostgreSqlRepositoryTest
 		when(idAttr.getName()).thenReturn("attr");
 		when(idAttr.getDataType()).thenReturn(STRING);
 
-		EntityType entityType = mock(EntityType.class);
 		when(entityType.getIdAttribute()).thenReturn(idAttr);
 		when(entityType.getAtomicAttributes()).thenReturn(singletonList(idAttr));
 		when(entityType.getId()).thenReturn("entity");
-		when(entityType.getIdValue()).thenReturn("entity");
-		when(entityType.getAttribute("attr")).thenReturn(idAttr);
 
 		Entity entity0 = mock(Entity.class);
-		when(entity0.getIdValue()).thenReturn("id0");
 		Entity entity1 = mock(Entity.class);
-		when(entity1.getIdValue()).thenReturn("id1");
 
 		when(jdbcTemplate.batchUpdate(any(String.class), any(BatchPreparedStatementSetter.class))).thenReturn(
 				new int[] { 2 });
-		postgreSqlRepo.setEntityType(entityType);
 
 		postgreSqlRepo.update(Stream.of(entity0, entity1));
 		verify(jdbcTemplate).batchUpdate(any(String.class), any(BatchPreparedStatementSetter.class));
@@ -118,11 +181,9 @@ public class PostgreSqlRepositoryTest
 		when(idAttr.getName()).thenReturn("attr");
 		when(idAttr.getDataType()).thenReturn(STRING);
 
-		EntityType entityType = mock(EntityType.class);
 		when(entityType.getIdAttribute()).thenReturn(idAttr);
 		when(entityType.getAtomicAttributes()).thenReturn(singletonList(idAttr));
 		when(entityType.getId()).thenReturn("entity");
-		when(entityType.getIdValue()).thenReturn("entity");
 		when(entityType.getAttribute("attr")).thenReturn(idAttr);
 
 		Entity entity0 = mock(Entity.class);
@@ -130,59 +191,12 @@ public class PostgreSqlRepositoryTest
 		Entity entity1 = mock(Entity.class);
 		when(entity1.getIdValue()).thenReturn("id1");
 
-		when(jdbcTemplate.query(any(String.class), any(Object[].class), isNull(RowMapper.class))).thenReturn(
+		when(jdbcTemplate.query(any(String.class), any(Object[].class), (RowMapper) isNull())).thenReturn(
 				singletonList(entity0));
 		when(jdbcTemplate.batchUpdate(any(String.class), any(BatchPreparedStatementSetter.class))).thenReturn(
 				new int[] { 1 });
-		postgreSqlRepo.setEntityType(entityType);
 
 		postgreSqlRepo.update(Stream.of(entity0, entity1));
 	}
 
-	private static EntityType createEntityMetaOneToMany(String oneToManyAttrName)
-	{
-		String refIdAttrName = "refEntityId";
-		Attribute refIdAttr = mock(Attribute.class);
-		when(refIdAttr.getIdentifier()).thenReturn(refIdAttrName);
-		when(refIdAttr.getName()).thenReturn(refIdAttrName);
-		when(refIdAttr.getDataType()).thenReturn(INT);
-
-		String xrefAttrName = "xrefAttr";
-		Attribute xrefAttr = mock(Attribute.class);
-		when(xrefAttr.getIdentifier()).thenReturn(xrefAttrName);
-		when(xrefAttr.getName()).thenReturn(xrefAttrName);
-		when(xrefAttr.getDataType()).thenReturn(XREF);
-
-		EntityType refEntityMeta = mock(EntityType.class);
-		String refEntityName = "RefEntity";
-		when(refEntityMeta.getId()).thenReturn("refEntityId");
-		when(refEntityMeta.getIdValue()).thenReturn("refEntityId");
-		when(refEntityMeta.getIdAttribute()).thenReturn(refIdAttr);
-		when(refEntityMeta.getAttribute(refIdAttrName)).thenReturn(refIdAttr);
-		when(refEntityMeta.getAttribute(xrefAttrName)).thenReturn(xrefAttr);
-
-		String idAttrName = "entityId";
-		Attribute idAttr = mock(Attribute.class);
-		when(idAttr.getIdentifier()).thenReturn(idAttrName);
-		when(idAttr.getName()).thenReturn(idAttrName);
-		when(idAttr.getDataType()).thenReturn(STRING);
-
-		Attribute oneToManyAttr = mock(Attribute.class);
-		when(oneToManyAttr.getIdentifier()).thenReturn(oneToManyAttrName);
-		when(oneToManyAttr.getName()).thenReturn(oneToManyAttrName);
-		when(oneToManyAttr.getDataType()).thenReturn(ONE_TO_MANY);
-		when(oneToManyAttr.getRefEntity()).thenReturn(refEntityMeta);
-		when(oneToManyAttr.isMappedBy()).thenReturn(true);
-		when(oneToManyAttr.getMappedBy()).thenReturn(xrefAttr);
-
-		EntityType entityType = mock(EntityType.class);
-		String entityTypeId = "Entity";
-		when(entityType.getId()).thenReturn("entityId");
-		when(entityType.getIdValue()).thenReturn("entityId");
-		when(entityType.getIdAttribute()).thenReturn(idAttr);
-		when(entityType.getAttribute(idAttrName)).thenReturn(idAttr);
-		when(entityType.getAttribute(oneToManyAttrName)).thenReturn(oneToManyAttr);
-		when(entityType.getAtomicAttributes()).thenReturn(newArrayList(idAttr, oneToManyAttr));
-		return entityType;
-	}
 }
