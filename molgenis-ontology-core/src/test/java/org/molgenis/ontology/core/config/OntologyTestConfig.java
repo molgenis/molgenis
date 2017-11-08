7abe400b978b798c8a015f805c0003a17f4ec8ea
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-11-08 10:26:47 +0100

    Revert integration test changes

--- molgenis-ontology-core/src/test/java/org/molgenis/ontology/core/config/OntologyTestConfig.java
+++ molgenis-ontology-core/src/test/java/org/molgenis/ontology/core/config/OntologyTestConfig.java
@@ -3,10 +3,6 @@ package org.molgenis.ontology.core.config;
 import org.molgenis.data.config.EntityBaseTestConfig;
 import org.molgenis.ontology.core.meta.*;
 import org.molgenis.ontology.core.model.OntologyPackage;
-import org.molgenis.ontology.core.repository.OntologyRepository;
-import org.molgenis.ontology.core.repository.OntologyTermRepository;
-import org.molgenis.ontology.core.service.impl.OntologyServiceImpl;
-import org.molgenis.ontology.importer.OntologyImportService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -15,9 +11,7 @@ import org.springframework.context.annotation.Import;
 		OntologyTermFactory.class, OntologyTermDynamicAnnotationMetaData.class,
 		OntologyTermDynamicAnnotationFactory.class, OntologyTermNodePathMetaData.class,
 		OntologyTermNodePathFactory.class, OntologyTermSynonymMetaData.class, OntologyTermSynonymFactory.class,
-		OntologyPackage.class, OntologyServiceImpl.class, OntologyRepository.class, OntologyTermRepository.class,
-		OntologyImportService.class, OntologyFactory.class })
+		OntologyPackage.class })
 public class OntologyTestConfig
 {
-
 }
