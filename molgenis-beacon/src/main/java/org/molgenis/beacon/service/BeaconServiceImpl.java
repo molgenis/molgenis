c49b443d647dfedc92b30c598246718f55211039
connor.stroomberg <connorstroomberg@users.noreply.github.com>
2017-11-13 13:42:31 +0100

    Use GenomeBrowserAttributesMeta for constants related to gene data

--- molgenis-beacon/src/main/java/org/molgenis/beacon/service/BeaconServiceImpl.java
+++ molgenis-beacon/src/main/java/org/molgenis/beacon/service/BeaconServiceImpl.java
@@ -2,12 +2,10 @@ package org.molgenis.beacon.service;
 
 import org.molgenis.data.DataService;
 import org.molgenis.data.support.QueryImpl;
-import org.molgenis.data.vcf.model.VcfAttributes;
+import org.molgenis.genomebrowser.meta.GenomeBrowserAttributesMetadata;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Objects.requireNonNull;
 
 @Component
@@ -23,13 +21,15 @@ public class BeaconServiceImpl implements BeaconService
 	@Override
 	public boolean query(String chromosome, Long position, String reference, String allele, String entityTypeID)
 	{
+
+
 		/* Use the count() method to determine if a variation exists */
-		return dataService.count(entityTypeID, new QueryImpl<>().eq(VcfAttributes.CHROM, chromosome)
+		return dataService.count(entityTypeID, new QueryImpl<>().eq(GenomeBrowserAttributesMetadata.CHROM, chromosome)
 																.and()
-																.eq(VcfAttributes.POS, position)
+																.eq(GenomeBrowserAttributesMetadata.POS, position)
 																.and()
-																.eq(VcfAttributes.REF, reference)
+																.eq(GenomeBrowserAttributesMetadata.REF, reference)
 																.and()
-																.eq(VcfAttributes.ALT, allele)) > 0;
+																.eq(GenomeBrowserAttributesMetadata.ALT, allele)) > 0;
 	}
 }
