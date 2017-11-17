792c5789509a065dfcc3754b73b56535e1a083f4
Dennis Hendriksen <d.hendriksen@umcg.nl>
2017-11-17 10:26:44 +0100

    Generate default value reference constraint violation messages

--- molgenis-data-validation/src/main/java/org/molgenis/data/validation/message/MessageGeneratorDefaultValueReference.java
+++ molgenis-data-validation/src/main/java/org/molgenis/data/validation/message/MessageGeneratorDefaultValueReference.java
@@ -8,8 +8,11 @@ import org.molgenis.util.UnexpectedEnumException;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static java.text.MessageFormat.format;
 import static java.util.stream.Collectors.joining;
+import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
 
 class MessageGeneratorDefaultValueReference
 {
@@ -19,6 +22,54 @@ class MessageGeneratorDefaultValueReference
 
 	static ConstraintViolationMessage createMessage(DefaultValueReferenceConstraintViolation constraintViolation)
 	{
+		ConstraintViolationMessage constraintViolationMessage;
+
+		DefaultValueReferenceConstraint defaultValueReferenceConstraint = constraintViolation.getConstraint();
+		switch (defaultValueReferenceConstraint)
+		{
+			case REFERENCE_EXISTS:
+				if (constraintViolation.getEntityId().isPresent() && constraintViolation.getAttributes().isPresent())
+				{
+					constraintViolationMessage = createMessageReferenceExists("V60a", constraintViolation);
+				}
+				else
+				{
+					constraintViolationMessage = createMessageReferenceExists("V60b", constraintViolation);
+				}
+				break;
+			default:
+				throw new UnexpectedEnumException(defaultValueReferenceConstraint);
+		}
+		return constraintViolationMessage;
+	}
+
+	private static ConstraintViolationMessage createMessageReferenceExists(String errorCode,
+			DefaultValueReferenceConstraintViolation constraintViolation)
+	{
+
+		String message = getMessage(constraintViolation);
+
+		String localizedMessage;
+		if (constraintViolation.getEntityId().isPresent() && constraintViolation.getAttributes().isPresent())
+		{
+			localizedMessage = getLocalizedMessage(errorCode, constraintViolation.getEntityType().getLabel(),
+					constraintViolation.getEntityId().get(), constraintViolation.getAttributes()
+																				.get()
+																				.stream()
+																				.map(Attribute::getLabel)
+																				.collect(Collectors.joining(
+																						","))).orElse(message);
+		}
+		else
+		{
+			localizedMessage = getLocalizedMessage(errorCode, constraintViolation.getEntityType().getLabel()).orElse(
+					message);
+		}
+		return ConstraintViolationMessage.create(errorCode, message, localizedMessage);
+	}
+
+	private static String getMessage(DefaultValueReferenceConstraintViolation constraintViolation)
+	{
 		DefaultValueReferenceConstraint constraint = constraintViolation.getConstraint();
 		EntityType entityType = constraintViolation.getEntityType();
 		Optional<Object> entityId = constraintViolation.getEntityId();
@@ -27,28 +78,18 @@ class MessageGeneratorDefaultValueReference
 		String message;
 		if (entityId.isPresent() && attributes.isPresent())
 		{
-			message = String.format("constraint:%s type:%s entity:%s attributes:[%s]", constraint.name(),
-					entityType.getId(), entityId.toString(),
+			message = String.format("constraint:%s type:%s entity:%s attributes:[%s]", constraint.name(), entityType.getId(), entityId.toString(),
 					attributes.get().stream().map(Attribute::getIdentifier).collect(joining(",")));
 		}
 		else
 		{
 			message = String.format("constraint:%s type:%s", constraint.name(), entityType.getId());
 		}
+		return message;
+	}
 
-		String errorCode;
-		String localizedMessage;
-		switch (constraint)
-		{
-			case REFERENCE_EXISTS:
-				errorCode = "V__";
-				// TODO '%s' entities are referenced as default value by attributes"
-				localizedMessage = message; // TODO implement (something like: '%s' with id '%s' is referenced as default value by attribute(s): '%s')
-				break;
-			default:
-				throw new UnexpectedEnumException(constraint);
-		}
-
-		return ConstraintViolationMessage.create(errorCode, message, localizedMessage);
+	private static Optional<String> getLocalizedMessage(String errorCode, Object... arguments)
+	{
+		return getLanguageService().map(languageService -> format(languageService.getString(errorCode), arguments));
 	}
 }
