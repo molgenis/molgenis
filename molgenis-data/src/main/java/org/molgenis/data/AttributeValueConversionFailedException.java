3e20cab86dbecd23ab9bb7fd8d7ca80de9e7f458
Bart Charbon <b.charbon@umcg.nl>
2017-11-27 13:24:33 +0100

    Add cause to AttributeValueConversionFailedException

--- molgenis-data/src/main/java/org/molgenis/data/AttributeValueConversionFailedException.java
+++ molgenis-data/src/main/java/org/molgenis/data/AttributeValueConversionFailedException.java
@@ -3,6 +3,7 @@ package org.molgenis.data;
 import org.molgenis.data.meta.model.Attribute;
 
 import java.text.MessageFormat;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
@@ -16,19 +17,30 @@ public class AttributeValueConversionFailedException extends CodedRuntimeExcepti
 
 	private final Attribute attr;
 	private final Object value;
+	private Optional<Exception> exception;
+
+	public AttributeValueConversionFailedException(Attribute attr, Object value, Exception exception)
+	{
+		super(ERROR_CODE);
+		this.attr = requireNonNull(attr);
+		this.value = requireNonNull(value);
+		this.exception = Optional.of(exception);
+	}
 
 	public AttributeValueConversionFailedException(Attribute attr, Object value)
 	{
 		super(ERROR_CODE);
 		this.attr = requireNonNull(attr);
 		this.value = requireNonNull(value);
+		this.exception = exception;
 	}
 
 	@Override
 	public String getMessage()
 	{
-		return String.format("type:%s attribute:%s expected:%s actual:%s value:%s", attr.getEntity().getId(),
-				attr.getName(), attr.getDataType().name(), value.getClass().getName(), value.toString());
+		Throwable t = exception.isPresent() ? exception.get().getCause() : null;
+		return String.format("type:%s attribute:%s expected:%s actual:%s value:%s, cause:%s", attr.getEntity().getId(),
+				attr.getName(), attr.getDataType().name(), value.getClass().getName(), value.toString(), t);
 	}
 
 	@Override
