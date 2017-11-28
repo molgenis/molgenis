b4421e42328bc7c2d7afeea0bedc5b141eca0d8b
Bart Charbon <b.charbon@umcg.nl>
2017-11-28 15:24:44 +0100

    Start refactorig language exceptions

---
+++ molgenis-data-i18n/src/main/java/org/molgenis/data/i18n/exception/DeleteLanguageException.java
@@ -0,0 +1,31 @@
+package org.molgenis.data.i18n.exception;
+
+import org.molgenis.data.CodedRuntimeException;
+
+import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
+
+public class DeleteLanguageException extends CodedRuntimeException
+{
+	private final static String ERROR_CODE = "L01";
+
+	public DeleteLanguageException()
+	{
+		super(ERROR_CODE);
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return String.format("");
+	}
+
+	@Override
+	public String getLocalizedMessage()
+	{
+		return getLanguageService().map(languageService ->
+		{
+			String format = languageService.getString(ERROR_CODE);
+			return format;
+		}).orElseGet(super::getLocalizedMessage);
+	}
+}
