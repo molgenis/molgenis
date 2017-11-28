356128975063095b32320845dfb99b703049bfd1
Bart Charbon <b.charbon@umcg.nl>
2017-11-28 13:18:13 +0100

    Start refactorig index exceptions

--- molgenis-data-index/src/main/java/org/molgenis/data/index/exception/IndexAlreadyExistsException.java
+++ molgenis-data-index/src/main/java/org/molgenis/data/index/exception/IndexAlreadyExistsException.java
@@ -1,12 +1,40 @@
 package org.molgenis.data.index.exception;
 
-@Deprecated // FIXME extend from CodedRuntimeException
-public class IndexAlreadyExistsException extends IndexException
+import org.molgenis.data.CodedRuntimeException;
+
+import java.text.MessageFormat;
+
+import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
+
+public class IndexAlreadyExistsException extends CodedRuntimeException
 {
-	private static final long serialVersionUID = 1L;
+	private final static String ERROR_CODE = "IDX01";
+	private String indexName;
 
 	public IndexAlreadyExistsException(String indexName)
 	{
-		super(String.format("Index '%s' already exists.", indexName));
+		super(ERROR_CODE);
+		this.indexName = indexName;
+	}
+
+	public String getIndexName()
+	{
+		return indexName;
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return String.format("indexName:%s", indexName);
+	}
+
+	@Override
+	public String getLocalizedMessage()
+	{
+		return getLanguageService().map(languageService ->
+		{
+			String format = languageService.getString(ERROR_CODE);
+			return MessageFormat.format(format, indexName);
+		}).orElse(super.getLocalizedMessage());
 	}
 }
