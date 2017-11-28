414affdcc4808bc39caabe88055c6f6cb3b47475
Bart Charbon <b.charbon@umcg.nl>
2017-11-28 13:17:19 +0100

    Use Attribute instead of attributeName where possible

--- molgenis-data-import/src/main/java/org/molgenis/data/importer/emx/exception/UnknownMappedByException.java
+++ molgenis-data-import/src/main/java/org/molgenis/data/importer/emx/exception/UnknownMappedByException.java
@@ -1,5 +1,7 @@
 package org.molgenis.data.importer.emx.exception;
 
+import org.molgenis.data.meta.model.Attribute;
+
 import java.text.MessageFormat;
 
 import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
@@ -7,14 +9,14 @@ import static org.molgenis.data.i18n.LanguageServiceHolder.getLanguageService;
 public class UnknownMappedByException extends EmxException
 {
 	private final static String ERROR_CODE = "E08";
-	private String attributeName;
+	private Attribute attribute;
 	private String mappedByAttributeName;
 	private int rowIndex;
 
-	public UnknownMappedByException(String attributeName, String mappedByAttributeName, int rowIndex)
+	public UnknownMappedByException(Attribute attribute, String mappedByAttributeName, int rowIndex)
 	{
 		super(ERROR_CODE);
-		this.attributeName = attributeName;
+		this.attribute = attribute;
 		this.mappedByAttributeName = mappedByAttributeName;
 		this.rowIndex = rowIndex;
 	}
@@ -22,7 +24,8 @@ public class UnknownMappedByException extends EmxException
 	@Override
 	public String getMessage()
 	{
-		return String.format("attributeName:%s referenceName:%s, rowIndex:%d", attributeName, mappedByAttributeName,
+		return String.format("attributeName:%s referenceName:%s, rowIndex:%d", attribute.getName(),
+				mappedByAttributeName,
 				rowIndex);
 	}
 
@@ -32,7 +35,7 @@ public class UnknownMappedByException extends EmxException
 		return getLanguageService().map(languageService ->
 		{
 			String format = languageService.getString(ERROR_CODE);
-			return MessageFormat.format(format, mappedByAttributeName, attributeName, rowIndex);
+			return MessageFormat.format(format, mappedByAttributeName, attribute.getName(), rowIndex);
 		}).orElse(super.getLocalizedMessage());
 	}
 }
