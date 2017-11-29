a205035891c756fd642a89c918c030c694891a49
Mark-de-Haan <markdehaan90@gmail.com>
2017-11-29 16:26:45 +0100

    Add swagger documentation

--- molgenis-beacon/src/main/java/org/molgenis/beacon/controller/BeaconController.java
+++ molgenis-beacon/src/main/java/org/molgenis/beacon/controller/BeaconController.java
@@ -1,5 +1,7 @@
 package org.molgenis.beacon.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.molgenis.beacon.controller.model.BeaconAlleleRequest;
 import org.molgenis.beacon.controller.model.BeaconAlleleResponse;
 import org.molgenis.beacon.controller.model.BeaconResponse;
@@ -14,6 +16,7 @@ import static java.util.Objects.requireNonNull;
 import static org.molgenis.beacon.controller.BeaconController.URI;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Api("Beacon")
 @Controller
 @RequestMapping(URI)
 public class BeaconController
@@ -31,6 +34,7 @@ public class BeaconController
 
 	@ResponseBody
 	@GetMapping(value = "/list", produces = APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "List all beacons", response = BeaconResponse.class, responseContainer = "List")
 	public List<BeaconResponse> getAllBeacons()
 	{
 		return beaconInfoService.getAvailableBeacons();
@@ -38,6 +42,7 @@ public class BeaconController
 
 	@ResponseBody
 	@GetMapping(value = "/{beaconId}", produces = APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Retrieve info on one beacon", response = BeaconResponse.class)
 	public BeaconResponse info(@PathVariable("beaconId") final String beaconId)
 	{
 		return beaconInfoService.info(beaconId);
@@ -45,6 +50,7 @@ public class BeaconController
 
 	@ResponseBody
 	@GetMapping(value = "/{beaconId}/query", produces = APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Query a beacon via a GET request", response = BeaconAlleleResponse.class)
 	public BeaconAlleleResponse query(@RequestParam("referenceName") String referenceName,
 			@RequestParam("start") Long start, @RequestParam("referenceBases") String referenceBases,
 			@RequestParam("alternateBases") String alternateBases, @PathVariable("beaconId") final String beaconId)
@@ -54,6 +60,7 @@ public class BeaconController
 
 	@ResponseBody
 	@PostMapping(value = "/{beaconId}/query", produces = APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Query a beacon via a POST request", response = BeaconAlleleResponse.class)
 	public BeaconAlleleResponse query(@PathVariable("beaconId") final String beaconId,
 			@RequestBody BeaconAlleleRequest request)
 	{
