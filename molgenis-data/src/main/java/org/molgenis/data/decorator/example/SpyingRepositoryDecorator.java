6a9454088f482cbbd732a8dbe2318028886b2fa8
tommydeboer <tommydeboer4@gmail.com>
2017-12-19 15:05:31 +0100

    Temporary fix for example decorator

--- molgenis-data/src/main/java/org/molgenis/data/decorator/example/SpyingRepositoryDecorator.java
+++ molgenis-data/src/main/java/org/molgenis/data/decorator/example/SpyingRepositoryDecorator.java
@@ -5,8 +5,6 @@ import org.molgenis.data.Entity;
 import org.molgenis.data.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 
 public class SpyingRepositoryDecorator extends AbstractRepositoryDecorator<Entity>
 {
@@ -27,7 +25,6 @@ public class SpyingRepositoryDecorator extends AbstractRepositoryDecorator<Entit
 
 	private void catchPerpetrator(String entityTypeId, Object id)
 	{
-		String username = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-		LOG.info("{} changed entity [{}] in entity type [{}]", username, id, entityTypeId);
+		LOG.info("TEST");
 	}
 }
