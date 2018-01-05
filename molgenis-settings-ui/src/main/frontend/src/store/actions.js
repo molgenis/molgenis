beab36b72d3c692941307728450d5b54afdcbd3c
sido <sido@haakma.org>
2018-01-05 10:04:57 +0100

    [FIX] fixed typos and removed some useless code

--- molgenis-settings-ui/src/main/frontend/src/store/actions.js
+++ molgenis-settings-ui/src/main/frontend/src/store/actions.js
@@ -9,18 +9,14 @@ export const GET_SETTINGS = '__GET_SETTINGS__'
 export default {
   [GET_SETTINGS] ({commit}: { commit: Function }) {
     const uri = '/api/v2/sys_set_app'
-    try {
-      api.get(uri).then(response => {
-        // TODO: We don't have compounds yet, so we have this workaround for now
-        response.meta.attributes = response.meta.attributes.filter(attribute => attribute.fieldType !== 'COMPOUND')
-        const formFields = EntityToStateMapper.generateFormFields(response.meta)
-        commit(SET_FORM_FIELDS, formFields)
-        commit(SET_FORM_DATA, EntityToStateMapper.generateFormData(formFields, response.items[0]))
-      }, error => {
-        commit(SET_ERROR, error)
-      })
-    } catch (error) {
-      commit(SET_ERROR, error.message)
-    }
+    api.get(uri).then(response => {
+      // TODO: We don't have compounds yet, so we have this workaround for now
+      response.meta.attributes = response.meta.attributes.filter(attribute => attribute.fieldType !== 'COMPOUND')
+      const formFields = EntityToStateMapper.generateFormFields(response.meta)
+      commit(SET_FORM_FIELDS, formFields)
+      commit(SET_FORM_DATA, EntityToStateMapper.generateFormData(formFields, response.items[0]))
+    }, error => {
+      commit(SET_ERROR, error)
+    })
   }
 }
