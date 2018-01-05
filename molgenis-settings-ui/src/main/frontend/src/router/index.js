8804e9496024bef3ed1cd756b0e3228eeae58f34
sido <sido@haakma.org>
2018-01-05 10:04:57 +0100

    [FIX] fixed typos and removed some useless code

--- molgenis-settings-ui/src/main/frontend/src/router/index.js
+++ molgenis-settings-ui/src/main/frontend/src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Settings from '../components/Settings'
+import { INITIAL_STATE } from '../store/state'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: INITIAL_STATE.baseUrl,
   routes: [
     {
       path: '/',
