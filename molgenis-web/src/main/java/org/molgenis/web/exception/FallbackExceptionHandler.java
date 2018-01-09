3fa8e293e82716ff8ba625126bc0a234ee173082
Bart Charbon <b.charbon@umcg.nl>
2018-01-09 15:49:21 +0100

    improve exception page: add httpStatus + add stacktrace when in development mode

--- molgenis-web/src/main/java/org/molgenis/web/exception/FallbackExceptionHandler.java
+++ molgenis-web/src/main/java/org/molgenis/web/exception/FallbackExceptionHandler.java
@@ -2,6 +2,7 @@ package org.molgenis.web.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,11 +17,14 @@ public class FallbackExceptionHandler
 {
 	private static final Logger LOG = LoggerFactory.getLogger(FallbackExceptionHandler.class);
 
+	@Value("${environment:production}")
+	private String environment;
+
 	@ResponseStatus(INTERNAL_SERVER_ERROR)
 	@ExceptionHandler
 	public Object handleException(Exception e, HandlerMethod handlerMethod)
 	{
 		LOG.error("", e);
-		return ExceptionHandlerUtils.handleException(e, handlerMethod, INTERNAL_SERVER_ERROR);
+		return ExceptionHandlerUtils.handleException(e, handlerMethod, INTERNAL_SERVER_ERROR, environment);
 	}
 }
