6ba3131ab33431b790406bcb68ae56a70c7cc04c
Bart Charbon <b.charbon@umcg.nl>
2018-01-11 15:41:37 +0100

    Add Spring exception handler

--- molgenis-web/src/main/java/org/molgenis/web/exception/ExceptionHandlerUtils.java
+++ molgenis-web/src/main/java/org/molgenis/web/exception/ExceptionHandlerUtils.java
@@ -3,12 +3,15 @@ package org.molgenis.web.exception;
 import org.molgenis.web.ErrorMessageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.util.matcher.ELRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,19 +24,39 @@ class ExceptionHandlerUtils
 	public static final String STACK_TRACE = "stackTrace";
 	public static final String HTTP_STATUS_CODE = "httpStatusCode";
 
+	private static final RequestMatcher requestMatcher = new ELRequestMatcher(
+			"hasHeader('X-Requested-With','XMLHttpRequest')");
+	public static final String OPTIONS = "OPTIONS";
+
 	private ExceptionHandlerUtils()
 	{
 	}
 
-	static Object handleException(Exception e, HandlerMethod handlerMethod, HttpStatus httpStatus)
-	{
-		return handleException(e, handlerMethod, httpStatus, PRODUCTION);
-	}
-
 	static Object handleException(Exception e, HandlerMethod handlerMethod, HttpStatus httpStatus, String environment)
 	{
+		return handleException(e, isHtmlRequest(handlerMethod), httpStatus, environment);
+	}
+
+	static Object handleException(Exception e, HandlerMethod handlerMethod, HttpStatus httpStatus)
+	{
+		return handleException(e, isHtmlRequest(handlerMethod), httpStatus, PRODUCTION);
+	}
+
+	static Object handleException(Exception e, HttpServletRequest httpServletRequest, HttpStatus httpStatus,
+			String environment)
+	{
+		return handleException(e, isHtmlRequest(httpServletRequest), httpStatus, environment);
+	}
+
+	static Object handleException(Exception e, HttpServletRequest httpServletRequest, HttpStatus httpStatus)
+	{
+		return handleException(e, isHtmlRequest(httpServletRequest), httpStatus, PRODUCTION);
+	}
+
+	private static Object handleException(Exception e, boolean isHtmlRequest, HttpStatus httpStatus, String environment)
+	{
 		ErrorMessageResponse errorMessageResponse = ErrorMessageResponse.create(e.getLocalizedMessage());
-		if (isHtmlRequest(handlerMethod))
+		if ((isHtmlRequest))
 		{
 			Map<String, Object> model = new HashMap<>();
 			model.put(KEY_ERROR_MESSAGE_RESPONSE, errorMessageResponse);
@@ -55,5 +78,10 @@ class ExceptionHandlerUtils
 		return !(handlerMethod.hasMethodAnnotation(ResponseBody.class) || handlerMethod.hasMethodAnnotation(
 				ResponseStatus.class) || handlerMethod.getBeanType().isAnnotationPresent(ResponseBody.class)
 				|| handlerMethod.getBeanType().isAnnotationPresent(RestController.class));
+	}
+
+	private static boolean isHtmlRequest(HttpServletRequest httpServletRequest)
+	{
+		return !(OPTIONS.equals(httpServletRequest.getMethod()) || requestMatcher.matches(httpServletRequest));
 	}
 }
---
+++ molgenis-web/src/main/java/org/molgenis/web/exception/SpringExceptionHandler.java
@@ -0,0 +1,98 @@
+package org.molgenis.web.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMethodException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.molgenis.web.exception.ExceptionHandlerUtils.handleException;
+
+@ControllerAdvice
+@Order(0)//After the Global handler but before the fallback
+public class SpringExceptionHandler
+{
+	private static final Logger LOG = LoggerFactory.getLogger(SpringExceptionHandler.class);
+
+	@ExceptionHandler({ NoSuchRequestHandlingMethodException.class, HttpRequestMethodNotSupportedException.class,
+			HttpMediaTypeNotSupportedException.class, HttpMediaTypeNotAcceptableException.class,
+			MissingPathVariableException.class, MissingServletRequestParameterException.class,
+			ServletRequestBindingException.class, ConversionNotSupportedException.class, TypeMismatchException.class,
+			HttpMessageNotReadableException.class, HttpMessageNotWritableException.class,
+			MethodArgumentNotValidException.class, MissingServletRequestPartException.class, BindException.class,
+			NoHandlerFoundException.class, AsyncRequestTimeoutException.class })
+	public final Object handleSpringException(Exception ex, HandlerMethod handlerMethod,
+			HttpServletRequest httpServletRequest)
+	{
+		HttpStatus status;
+		if (ex instanceof NoSuchRequestHandlingMethodException || ex instanceof NoHandlerFoundException)
+		{
+			status = HttpStatus.NOT_FOUND;
+			return handleException(ex, httpServletRequest, status);
+		}
+		else if (ex instanceof HttpRequestMethodNotSupportedException)
+		{
+			status = HttpStatus.METHOD_NOT_ALLOWED;
+			return handleException(ex, handlerMethod, status);
+		}
+		else if (ex instanceof HttpMediaTypeNotSupportedException)
+		{
+			status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+			return handleException(ex, handlerMethod, status);
+		}
+		else if (ex instanceof HttpMediaTypeNotAcceptableException)
+		{
+			status = HttpStatus.NOT_ACCEPTABLE;
+			return handleException(ex, handlerMethod, status);
+		}
+		else if (ex instanceof MissingServletRequestParameterException || ex instanceof ServletRequestBindingException
+				|| ex instanceof TypeMismatchException || ex instanceof HttpMessageNotReadableException
+				|| ex instanceof MethodArgumentNotValidException || ex instanceof MissingServletRequestPartException
+				|| ex instanceof BindException)
+		{
+			status = HttpStatus.BAD_REQUEST;
+			return handleException(ex, handlerMethod, status);
+		}
+		else if (ex instanceof MissingPathVariableException || ex instanceof ConversionNotSupportedException
+				|| ex instanceof HttpMessageNotWritableException)
+		{
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			return handleException(ex, handlerMethod, status);
+		}
+		else if (ex instanceof AsyncRequestTimeoutException)
+		{
+			status = HttpStatus.SERVICE_UNAVAILABLE;
+			return handleException(ex, handlerMethod, status);
+		}
+		else
+		{
+			if (LOG.isWarnEnabled())
+			{
+				LOG.warn("Unknown exception type: " + ex.getClass().getName());
+			}
+
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			return handleException(ex, handlerMethod, status);
+		}
+	}
+}
