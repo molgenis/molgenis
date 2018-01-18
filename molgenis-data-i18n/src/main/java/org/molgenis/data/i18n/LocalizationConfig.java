9b9fe3ee86ccdb19550f78983f63fb4790ef1cd7
Fleur Kelpin <fleur@kelpin.nl>
2018-01-18 09:43:24 +0100

    [Exceptions] Handle MethodArgumentNotValidException by putting the localized error messages from BindingResult in the ErrorResponse

--- molgenis-data-i18n/src/main/java/org/molgenis/data/i18n/LocalizationConfig.java
+++ molgenis-data-i18n/src/main/java/org/molgenis/data/i18n/LocalizationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 import java.util.Locale;
 
@@ -45,6 +46,9 @@ public class LocalizationConfig
 	{
 		LocalizationMessageSource localizationMessageSource = new LocalizationMessageSource(messageFormatFactory,
 				localizationRepository(), () -> new Locale(appSettings.getLanguageCode()));
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.addBasenames("org.hibernate.validator.ValidationMessages");
+		localizationMessageSource.setParentMessageSource(resourceBundleMessageSource);
 		MessageSourceHolder.setMessageSource(localizationMessageSource);
 		return localizationMessageSource;
 	}
