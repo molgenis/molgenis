91f7bc6bcb21b8db3e1470357f7cae886004aebc
sido <sido@haakma.org>
2018-01-22 15:34:36 +0100

    [FEATURE] add settings-select

--- molgenis-settings-ui/src/main/frontend/src/components/Settings.vue
+++ molgenis-settings-ui/src/main/frontend/src/components/Settings.vue
@@ -1,30 +1,84 @@
 <template>
-  <div>
-    <h1>{{ 'plugin-title' | i18n }}</h1>
-    <form-component id="settings-form" :schema="schema" :data="data"></form-component>
+  <div class="card">
+    <div class="card-header">
+      <h5>{{ 'plugin-title' | i18n }}</h5>
+    </div>
+    <div class="card-body">
+      <div id="alert-message" v-if="message" class="alert" :class="error ? 'alert-danger' : 'alert-info'"
+           role="alert">
+        <button @click="message=null" type="button" class="close"><span aria-hidden="true">&times;</span>
+        </button>
+        <span id="message-span">{{message}}</span>
+      </div>
+      <div class="card-block">
+        <entity-select-component v-model.lazy="selectedEntity" :id="'settings-select'" :label="'Select entity'"
+                                 :description="'Select setting you want to edit'"
+                                 :entities="initSettingsOptions"></entity-select-component>
+      </div>
+      <div class="card-block">
+        <form-component id="settings-form" :schema="initFormSchema" :initialFormData="initialFormData"
+                        :hooks="hooks"></form-component>
+      </div>
+    </div>
+    <div class="card-footer">
+      <button id="save-btn" class="btn btn-primary" type="submit" form="settings-form">Save</button>
+      <button id="cancel-btn" class="btn btn-secondary" type="reset" form="settings-form">Cancel</button>
+    </div>
+
   </div>
 </template>
 
 <script>
   import { FormComponent } from '@molgenis/molgenis-ui-form'
-  import { GET_SETTINGS } from '../store/actions'
+  import EntitySelectComponent from '../components/EntitySelectComponent'
+  import { GET_SETTINGS, GET_SETTINGS_BY_ID, UPDATE_SETTINGS } from '../store/actions'
+  import { SET_FORM_DATA } from '../store/mutations'
 
   export default {
     name: 'Settings',
     components: {
-      FormComponent
+      FormComponent,
+      EntitySelectComponent
     },
     created: function () {
-      this.$store.dispatch(GET_SETTINGS)
+      this.$store.dispatch(GET_SETTINGS).then(() => {
+        this.$store.dispatch(GET_SETTINGS_BY_ID, 'sys_set_app')
+      })
+    },
+    data () {
+      return {
+        hooks: {
+          onSubmit: (formData) => {
+            this.$store.commit(SET_FORM_DATA, formData)
+            this.$store.dispatch(UPDATE_SETTINGS, this.selectedEntity)
+            this.message = 'Changes saved'
+          },
+          onCancel: () => {
+            this.message = 'onCancel'
+          },
+          onValueChanged: (formData) => {
+            console.log('value changed')
+          }
+        },
+        message: null,
+        error: null,
+        selectedEntity: null
+      }
+    },
+    watch: {
+      selectedEntity (updatedValue) {
+        this.$store.dispatch(GET_SETTINGS_BY_ID, updatedValue)
+      }
     },
     computed: {
-      schema () {
-        return {
-          fields: this.$store.state.formFields
-        }
-      },
-      data () {
+      initialFormData () {
         return this.$store.state.formData
+      },
+      initFormSchema () {
+        return {fields: this.$store.state.formFields}
+      },
+      initSettingsOptions () {
+        return this.$store.state.settings
       }
     }
   }
