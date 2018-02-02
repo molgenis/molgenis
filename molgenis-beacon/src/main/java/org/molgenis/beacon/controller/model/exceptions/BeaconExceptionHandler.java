421f0b517ef316becfa269f0ad2ac77a6b5812f4
Fleur Kelpin <f.kelpin@umcg.nl>
2018-02-02 13:14:23 +0100

    Fix #7139: Beacon exceptions sometimes handled by fallback handler

--- molgenis-beacon/src/main/java/org/molgenis/beacon/controller/model/exceptions/BeaconExceptionHandler.java
+++ molgenis-beacon/src/main/java/org/molgenis/beacon/controller/model/exceptions/BeaconExceptionHandler.java
@@ -4,16 +4,21 @@ import org.molgenis.beacon.controller.model.BeaconAlleleResponse;
 import org.molgenis.beacon.controller.model.BeaconError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 /**
  * Handles only the {@link BeaconException} that has to be converted to {@link BeaconAlleleResponse} to conform to Beacon network specifications
  */
 @ControllerAdvice
+@Order(HIGHEST_PRECEDENCE) // Take precedence over other handlers, see #7139
 public class BeaconExceptionHandler
 {
 	private static final Logger LOG = LoggerFactory.getLogger(BeaconExceptionHandler.class);
