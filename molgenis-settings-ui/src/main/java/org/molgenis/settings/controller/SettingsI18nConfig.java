edaa6f53447431910b9ee6201c2af7b8cbb335da
Mark-de-Haan <markdehaan90@gmail.com>
2018-02-02 12:20:06 +0100

    Temporarily disable i18n to prevent duplicate ID error

--- molgenis-settings-ui/src/main/java/org/molgenis/settings/controller/SettingsI18nConfig.java
+++ molgenis-settings-ui/src/main/java/org/molgenis/settings/controller/SettingsI18nConfig.java
@@ -1,16 +1,14 @@
 package org.molgenis.settings.controller;
 
 import org.molgenis.i18n.PropertiesMessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class SettingsI18nConfig
 {
 
 	public static final String NAMESPACE = "settings";
 
-	@Bean
+//	@Bean
 	public PropertiesMessageSource settingsMessageSource()
 	{
 		return new PropertiesMessageSource(NAMESPACE);
