e602b743e0c4431c6a1a1b40514fba1b9c2c76f3
Fleur Kelpin <f.kelpin@umcg.nl>
2018-02-07 18:03:16 +0100

    Let PermissionSystemServiceImpl use Acls.

--- molgenis-security/src/main/java/org/molgenis/security/permission/PermissionSystemServiceImpl.java
+++ molgenis-security/src/main/java/org/molgenis/security/permission/PermissionSystemServiceImpl.java
@@ -1,55 +1,34 @@
 package org.molgenis.security.permission;
 
-import org.molgenis.data.DataService;
 import org.molgenis.data.meta.model.EntityType;
-import org.molgenis.data.security.auth.User;
-import org.molgenis.data.security.auth.UserAuthority;
-import org.molgenis.data.security.auth.UserAuthorityFactory;
+import org.molgenis.data.security.EntityTypeIdentity;
+import org.molgenis.data.security.EntityTypePermission;
 import org.molgenis.data.security.permission.PermissionSystemService;
-import org.molgenis.data.security.user.UserService;
 import org.molgenis.security.core.utils.SecurityUtils;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.MutableAcl;
+import org.springframework.security.acls.model.MutableAclService;
+import org.springframework.security.acls.model.ObjectIdentity;
+import org.springframework.security.acls.model.Sid;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
 
 import static java.util.Collections.singleton;
+import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
-import static org.molgenis.data.security.auth.UserAuthorityMetaData.USER_AUTHORITY;
-import static org.molgenis.security.core.Permission.WRITEMETA;
 import static org.molgenis.security.core.runas.RunAsSystemAspect.runAsSystem;
-import static org.molgenis.security.core.utils.SecurityUtils.AUTHORITY_ENTITY_PREFIX;
 
 @Component
 public class PermissionSystemServiceImpl implements PermissionSystemService
 {
-	private final UserService userService;
-	private final UserAuthorityFactory userAuthorityFactory;
-	private final RoleHierarchy roleHierarchy;
-	private final DataService dataService;
-	private final PrincipalSecurityContextRegistry principalSecurityContextRegistry;
-	private final AuthenticationAuthoritiesUpdater authenticationAuthoritiesUpdater;
+	private final MutableAclService aclService;
 
-	public PermissionSystemServiceImpl(UserService userService, UserAuthorityFactory userAuthorityFactory,
-			RoleHierarchy roleHierarchy, DataService dataService,
-			PrincipalSecurityContextRegistry principalSecurityContextRegistry,
-			AuthenticationAuthoritiesUpdater authenticationAuthoritiesUpdater)
+	public PermissionSystemServiceImpl(MutableAclService aclService)
 	{
-		this.userService = requireNonNull(userService);
-		this.userAuthorityFactory = requireNonNull(userAuthorityFactory);
-		this.roleHierarchy = requireNonNull(roleHierarchy);
-		this.dataService = requireNonNull(dataService);
-		this.principalSecurityContextRegistry = requireNonNull(principalSecurityContextRegistry);
-		this.authenticationAuthoritiesUpdater = requireNonNull(authenticationAuthoritiesUpdater);
+		this.aclService = requireNonNull(aclService);
 	}
 
 	@Override
@@ -67,58 +46,19 @@ public class PermissionSystemServiceImpl implements PermissionSystemService
 			return;
 		}
 
-		Collection<GrantedAuthority> grantedAuthorities = getGrantedAuthorities(entityTypes);
-		giveUserEntityPermissionsAsSystem(grantedAuthorities);
+		Sid sid = new PrincipalSid(SecurityContextHolder.getContext().getAuthentication());
+		runAsSystem(() -> insertAces(sid, entityTypes.stream().map(EntityTypeIdentity::new).collect(toList()),
+				EntityTypePermission.WRITEMETA));
 	}
 
-	private void giveUserEntityPermissionsAsSystem(Collection<GrantedAuthority> authorities)
+	private void insertAces(Sid sid, Collection<ObjectIdentity> objectIdentities,
+			org.springframework.security.acls.model.Permission permission)
 	{
-		String currentUsername = SecurityUtils.getCurrentUsername();
-		runAsSystem(() -> updatePersistedUserAuthorities(currentUsername, authorities));
-
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Stream<SecurityContext> securityContexts = principalSecurityContextRegistry.getSecurityContexts(principal);
-
-		Collection<? extends GrantedAuthority> reachableAuthorities = roleHierarchy.getReachableGrantedAuthorities(
-				authorities);
-		securityContexts.forEach(
-				securityContext -> updateSecurityContextAuthorities(securityContext, reachableAuthorities));
-	}
-
-	private void updateSecurityContextAuthorities(SecurityContext securityContext,
-			Collection<? extends GrantedAuthority> newAuthorities)
-	{
-		Collection<? extends GrantedAuthority> authorities = securityContext.getAuthentication().getAuthorities();
-		List<GrantedAuthority> updatedAuthorities = new ArrayList<>(authorities);
-		updatedAuthorities.addAll(newAuthorities);
-
-		Authentication updatedAuthentication = authenticationAuthoritiesUpdater.updateAuthentication(
-				securityContext.getAuthentication(), updatedAuthorities);
-		securityContext.setAuthentication(updatedAuthentication);
-	}
-
-	private Collection<GrantedAuthority> getGrantedAuthorities(Collection<EntityType> entityTypeStream)
-	{
-		return entityTypeStream.stream().map(this::toGrantedAuthority).collect(toList());
-	}
-
-	private GrantedAuthority toGrantedAuthority(EntityType entityType)
-	{
-		@SuppressWarnings("StringConcatenationMissingWhitespace")
-		String role = AUTHORITY_ENTITY_PREFIX + WRITEMETA.toString() + '_' + entityType.getId();
-		return new SimpleGrantedAuthority(role);
-	}
-
-	private void updatePersistedUserAuthorities(String currentUsername, Collection<GrantedAuthority> grantedAuthorities)
-	{
-		User user = userService.getUser(currentUsername);
-		Stream<UserAuthority> userAuthorityStream = grantedAuthorities.stream().map(grantedAuthority ->
+		for (ObjectIdentity id : objectIdentities)
 		{
-			UserAuthority userAuthority = userAuthorityFactory.create();
-			userAuthority.setUser(user);
-			userAuthority.setRole(grantedAuthority.getAuthority());
-			return userAuthority;
-		});
-		dataService.add(USER_AUTHORITY, userAuthorityStream);
+			MutableAcl acl = (MutableAcl) aclService.readAclById(id, singletonList(sid));
+			acl.insertAce(0, permission, sid, true);
+			aclService.updateAcl(acl);
+		}
 	}
 }
--- molgenis-security/src/test/java/org/molgenis/security/permission/PermissionSystemServiceImplTest.java
+++ molgenis-security/src/test/java/org/molgenis/security/permission/PermissionSystemServiceImplTest.java
@@ -1,26 +1,16 @@
 package org.molgenis.security.permission;
 
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.quality.Strictness;
-import org.molgenis.data.DataService;
 import org.molgenis.data.meta.model.EntityType;
-import org.molgenis.data.security.auth.User;
-import org.molgenis.data.security.auth.UserAuthority;
-import org.molgenis.data.security.auth.UserAuthorityFactory;
+import org.molgenis.data.security.EntityTypeIdentity;
+import org.molgenis.data.security.EntityTypePermission;
 import org.molgenis.data.security.permission.PermissionSystemService;
-import org.molgenis.data.security.user.UserService;
 import org.molgenis.test.AbstractMockitoTestNGSpringContextTests;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.acls.domain.AclImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.MutableAclService;
+import org.springframework.security.acls.model.Sid;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
 import org.springframework.test.context.ContextConfiguration;
@@ -28,205 +18,81 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
 import static org.mockito.Mockito.*;
-import static org.molgenis.data.security.auth.UserAuthorityMetaData.USER_AUTHORITY;
 import static org.testng.Assert.assertEquals;
 
 @ContextConfiguration(classes = { PermissionSystemServiceImplTest.Config.class })
 @TestExecutionListeners(listeners = WithSecurityContextTestExecutionListener.class)
 public class PermissionSystemServiceImplTest extends AbstractMockitoTestNGSpringContextTests
 {
-	@Captor
-	private ArgumentCaptor<Stream<UserAuthority>> userAuthorityStreamCaptor;
+	@Mock
+	private MutableAclService mutableAclService;
+	@Mock
+	private EntityType entityType0;
+	@Mock
+	private EntityType entityType1;
+	@Mock
+	private AclImpl acl0;
+	@Mock
+	private AclImpl acl1;
 
-	@Autowired
-	private Config config;
-
-	@Autowired
-	private UserService userService;
-
-	@Autowired
-	private UserAuthorityFactory userAuthorityFactory;
-
-	@Autowired
-	private RoleHierarchy roleHierarchy;
-
-	@Autowired
-	private DataService dataService;
-
-	@Autowired
-	private PrincipalSecurityContextRegistry principalSecurityContextRegistry;
-
-	@Autowired
 	private PermissionSystemService permissionSystemService;
-
-	@Autowired
-	private AuthenticationAuthoritiesUpdater authenticationAuthoritiesUpdater;
-
-	public PermissionSystemServiceImplTest()
-	{
-		super(Strictness.WARN);
-	}
 
 	@BeforeMethod
 	public void setUpBeforeMethod()
 	{
-		config.resetMocks();
-		permissionSystemService = new PermissionSystemServiceImpl(userService, userAuthorityFactory, roleHierarchy,
-				dataService, principalSecurityContextRegistry, authenticationAuthoritiesUpdater);
+		permissionSystemService = new PermissionSystemServiceImpl(mutableAclService);
 	}
 
 	@Test(expectedExceptions = NullPointerException.class)
 	public void testPermissionSystemService()
 	{
-		new PermissionSystemServiceImpl(null, null, null, null, null, null);
+		new PermissionSystemServiceImpl(null);
 	}
 
 	@Test
 	@WithMockUser(username = "user", authorities = { "existingAuthority" })
 	public void giveUserEntityPermissions()
 	{
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		Authentication updatedAuthication = mock(Authentication.class);
-		List<GrantedAuthority> updatedAuthorities = Arrays.asList(new SimpleGrantedAuthority("existingAuthority"),
-				new SimpleGrantedAuthority("newAuthority0"), new SimpleGrantedAuthority("newAuthority1"));
-		when(authenticationAuthoritiesUpdater.updateAuthentication(authentication, updatedAuthorities)).thenReturn(
-				updatedAuthication);
+		when(entityType0.getId()).thenReturn("entityTypeId0");
+		when(entityType1.getId()).thenReturn("entityTypeId1");
 
-		String id0 = "entityTypeId0";
-		EntityType entityType0 = when(mock(EntityType.class).getId()).thenReturn(id0).getMock();
-		String id1 = "entityTypeId1";
-		EntityType entityType1 = when(mock(EntityType.class).getId()).thenReturn(id1).getMock();
+		PrincipalSid userSid = new PrincipalSid("user");
+		List<Sid> userSidList = Collections.singletonList(userSid);
+		doReturn(acl0).when(mutableAclService).readAclById(new EntityTypeIdentity("entityTypeId0"), userSidList);
+		doReturn(acl1).when(mutableAclService).readAclById(new EntityTypeIdentity("entityTypeId1"), userSidList);
 
-		User user = mock(User.class);
-		when(userService.getUser("user")).thenReturn(user);
-		when(userAuthorityFactory.create()).thenAnswer(invocation -> mock(UserAuthority.class));
-
-		Collection<? extends GrantedAuthority> authorities = asList(
-				new SimpleGrantedAuthority("ROLE_ENTITY_WRITEMETA_" + id0),
-				new SimpleGrantedAuthority("ROLE_ENTITY_WRITEMETA_" + id1));
-		when(roleHierarchy.getReachableGrantedAuthorities(authorities)).thenAnswer(
-				invocation -> asList(new SimpleGrantedAuthority("newAuthority0"),
-						new SimpleGrantedAuthority("newAuthority1")));
-
-		when(principalSecurityContextRegistry.getSecurityContexts(authentication.getPrincipal())).thenReturn(
-				Stream.of(SecurityContextHolder.getContext()));
 		permissionSystemService.giveUserWriteMetaPermissions(asList(entityType0, entityType1));
 
-		String prefix = "ROLE_ENTITY";
-		verify(dataService).add(eq(USER_AUTHORITY), userAuthorityStreamCaptor.capture());
-		List<UserAuthority> userAuthorities = userAuthorityStreamCaptor.getValue().collect(toList());
-		assertEquals(userAuthorities.size(), 2);
-		verify(userAuthorities.get(0)).setUser(user);
-		verify(userAuthorities.get(0)).setRole(prefix + "_WRITEMETA_" + id0);
-		verify(userAuthorities.get(1)).setUser(user);
-		verify(userAuthorities.get(1)).setRole(prefix + "_WRITEMETA_" + id1);
+		verify(acl0).insertAce(0, EntityTypePermission.WRITEMETA, userSid, true);
+		verify(acl1).insertAce(0, EntityTypePermission.WRITEMETA, userSid, true);
 
-		assertEquals(SecurityContextHolder.getContext().getAuthentication(), updatedAuthication);
+		verify(mutableAclService).updateAcl(acl0);
+		verify(mutableAclService).updateAcl(acl1);
 	}
 
 	@Test
 	@WithMockUser(username = "SYSTEM", authorities = { "ROLE_SYSTEM" })
-	public void giveUserEntityPermissionsUserSystem()
+	public void giveUserEntityPermissionsSystemUser()
 	{
-		giveUserEntityPermissionsUserSystemOrAdmin();
+		permissionSystemService.giveUserWriteMetaPermissions(Collections.singleton(entityType0));
+		verifyZeroInteractions(mutableAclService);
 	}
 
 	@Test
 	@WithMockUser(username = "user", authorities = { "ROLE_SU" })
-	public void giveUserEntityPermissionsUserAdmin()
+	public void giveUserEntityPermissionsSuperuser()
 	{
-		giveUserEntityPermissionsUserSystemOrAdmin();
-	}
-
-	private void giveUserEntityPermissionsUserSystemOrAdmin()
-	{
-		EntityType entityType = when(mock(EntityType.class).getId()).thenReturn("entityTypeId").getMock();
-		permissionSystemService.giveUserWriteMetaPermissions(Collections.singleton(entityType));
-		verifyZeroInteractions(dataService);
+		permissionSystemService.giveUserWriteMetaPermissions(Collections.singleton(entityType0));
+		verifyZeroInteractions(mutableAclService);
 	}
 
 	@Configuration
 	public static class Config
 	{
-		@Mock
-		private UserService userService;
-
-		@Mock
-		private UserAuthorityFactory userAuthorityFactory;
-
-		@Mock
-		private RoleHierarchy roleHierarchy;
-
-		@Mock
-		private DataService dataService;
-
-		@Mock
-		private PrincipalSecurityContextRegistry principalSecurityContextRegistry;
-
-		@Mock
-		private AuthenticationAuthoritiesUpdater authenticationAuthoritiesUpdater;
-
-		public Config()
-		{
-			MockitoAnnotations.initMocks(this);
-		}
-
-		@Bean
-		public UserService userService()
-		{
-			return userService;
-		}
-
-		@Bean
-		public UserAuthorityFactory userAuthorityFactory()
-		{
-			return userAuthorityFactory;
-		}
-
-		@Bean
-		public RoleHierarchy roleHierarchy()
-		{
-			return roleHierarchy;
-		}
-
-		@Bean
-		public DataService dataService()
-		{
-			return dataService;
-		}
-
-		@Bean
-		public PrincipalSecurityContextRegistry principalSecurityContextRegistry()
-		{
-			return principalSecurityContextRegistry;
-		}
-
-		@Bean
-		public AuthenticationAuthoritiesUpdater authenticationAuthoritiesUpdater()
-		{
-			return authenticationAuthoritiesUpdater;
-		}
-
-		@Bean
-		public PermissionSystemService permissionSystemService()
-		{
-			return new PermissionSystemServiceImpl(userService(), userAuthorityFactory(), roleHierarchy(),
-					dataService(), principalSecurityContextRegistry(), authenticationAuthoritiesUpdater());
-		}
-
-		void resetMocks()
-		{
-			reset(userService, userAuthorityFactory, roleHierarchy, dataService, principalSecurityContextRegistry,
-					authenticationAuthoritiesUpdater);
-		}
 	}
 }
