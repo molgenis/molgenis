f765877c12e77702f9849ab7ce3d5f7e3c3459fd
Dennis Hendriksen <d.hendriksen@umcg.nl>
2018-02-15 11:02:44 +0100

    Refactor ImportWizardController test to resolve molgenis-report unstable test issue

--- molgenis-core-ui/src/test/java/org/molgenis/core/ui/data/importer/wizard/ImportWizardControllerTest.java
+++ molgenis-core-ui/src/test/java/org/molgenis/core/ui/data/importer/wizard/ImportWizardControllerTest.java
@@ -2,270 +2,175 @@ package org.molgenis.core.ui.data.importer.wizard;
 
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.mockito.ArgumentCaptor;
-import org.mockito.quality.Strictness;
-import org.molgenis.core.ui.data.importer.wizard.ImportWizardControllerTest.Config;
-import org.molgenis.data.AbstractMolgenisSpringTest;
+import org.mockito.Mock;
 import org.molgenis.data.DataService;
-import org.molgenis.data.SystemRepositoryDecoratorRegistry;
+import org.molgenis.data.DatabaseAction;
 import org.molgenis.data.file.FileRepositoryCollectionFactory;
 import org.molgenis.data.file.FileStore;
-import org.molgenis.data.file.support.FileRepositoryCollection;
 import org.molgenis.data.importer.*;
-import org.molgenis.data.importer.config.ImportTestConfig;
-import org.molgenis.data.meta.EntityTypeDependencyResolver;
-import org.molgenis.data.meta.MetaDataService;
 import org.molgenis.data.meta.model.EntityType;
-import org.molgenis.data.security.config.GroupAuthorityTestConfig;
-import org.molgenis.data.security.config.GroupTestConfig;
-import org.molgenis.data.security.user.UserService;
-import org.molgenis.security.core.utils.SecurityUtils;
-import org.molgenis.security.permission.PermissionManagerServiceImpl;
-import org.molgenis.security.user.UserAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
+import org.molgenis.test.AbstractMockitoTestNGSpringContextTests;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailSender;
-import org.springframework.security.acls.model.MutableAclService;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.time.Instant;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 
+import static java.util.Arrays.asList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.testng.Assert.assertEquals;
 
-@ContextConfiguration(classes = { Config.class })
-public class ImportWizardControllerTest extends AbstractMolgenisSpringTest
+@ContextConfiguration(classes = { ImportWizardControllerTest.Config.class })
+@TestExecutionListeners(listeners = WithSecurityContextTestExecutionListener.class)
+public class ImportWizardControllerTest extends AbstractMockitoTestNGSpringContextTests
 {
-	private ImportWizardController controller;
+	private static final String USERNAME = "user";
 
-	@Autowired
+	@Mock
+	private UploadWizardPage uploadWizardPage;
+	@Mock
+	private OptionsWizardPage optionsWizardPage;
+	@Mock
+	private PackageWizardPage packageWizardPage;
+	@Mock
+	private ValidationResultWizardPage validationResultWizardPage;
+	@Mock
+	private ImportResultsWizardPage importResultsWizardPage;
+	@Mock
 	private DataService dataService;
-
-	@Autowired
-	private ImportRunFactory importRunFactory;
-
+	@Mock
 	private ImportServiceFactory importServiceFactory;
+	@Mock
 	private FileStore fileStore;
+	@Mock
 	private FileRepositoryCollectionFactory fileRepositoryCollectionFactory;
+	@Mock
 	private ImportRunService importRunService;
+	@Mock
 	private ExecutorService executorService;
-	private FileRepositoryCollection repositoryCollection;
-	private ImportService importService;
-	private Instant date;
 
-	public ImportWizardControllerTest()
-	{
-		super(Strictness.WARN);
-	}
+	private ImportWizardController importWizardController;
 
-	@SuppressWarnings("unchecked")
 	@BeforeMethod
-	public void setUp()
+	private void setUpBeforeMethod()
 	{
-		reset(dataService);
-		UploadWizardPage uploadWizardPage = mock(UploadWizardPage.class);
-		OptionsWizardPage optionsWizardPage = mock(OptionsWizardPage.class);
-		ValidationResultWizardPage validationResultWizardPage = mock(ValidationResultWizardPage.class);
-		ImportResultsWizardPage importResultsWizardPage = mock(ImportResultsWizardPage.class);
-		PackageWizardPage packageWizardPage = mock(PackageWizardPage.class);
-		importServiceFactory = mock(ImportServiceFactory.class);
-		fileStore = mock(FileStore.class);
-		fileRepositoryCollectionFactory = mock(FileRepositoryCollectionFactory.class);
-		importRunService = mock(ImportRunService.class);
-		executorService = mock(ExecutorService.class);
-		dataService = mock(DataService.class);
-		repositoryCollection = mock(FileRepositoryCollection.class);
-		importService = mock(ImportService.class);
-		EntityType entityType = mock(EntityType.class);
-
-		controller = new ImportWizardController(uploadWizardPage, optionsWizardPage, packageWizardPage,
+		importWizardController = new ImportWizardController(uploadWizardPage, optionsWizardPage, packageWizardPage,
 				validationResultWizardPage, importResultsWizardPage, dataService, importServiceFactory, fileStore,
-				fileRepositoryCollectionFactory, importRunService, executorService);
+				fileRepositoryCollectionFactory, importRunService);
+		importWizardController.setExecutorService(executorService);
+	}
 
-		date = Instant.parse("2016-01-01T12:34:28.123Z");
+	@DataProvider(name = "testImportFileProvider")
+	public static Iterator<Object[]> testInitProvider()
+	{
+		return asList(new Object[] { "add", DatabaseAction.ADD },
+				new Object[] { "update", DatabaseAction.UPDATE }).iterator();
+	}
 
-		when(entityType.getId()).thenReturn("entityTypeId");
-		when(entityType.getId()).thenReturn("entityTypeName");
+	@WithMockUser(username = USERNAME)
+	@Test(dataProvider = "testImportFileProvider")
+	public void testImportFile(String action, DatabaseAction databaseAction) throws IOException, URISyntaxException
+	{
+		HttpSession httpSession = mock(HttpSession.class);
+		HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+		when(httpServletRequest.getSession()).thenReturn(httpSession);
 
-		reset(executorService);
+		String filename = "example.xlsx";
+		MultipartFile multipartFile = createMultipartFile(filename);
+		String entityTypeId = null;
+		String packageId = null;
+		Boolean notify = null;
+
+		String tmpFileName = "tmp-example.xlsx";
+		File tmpFile = mock(File.class);
+		when(tmpFile.getName()).thenReturn(tmpFileName);
+		when(fileStore.store(any(), eq("example.xlsx"))).thenReturn(tmpFile);
+
+		ImportService importService = mock(ImportService.class);
+		when(importServiceFactory.getImportService(tmpFileName)).thenReturn(importService);
+
+		String importRunEntityTypeId = "entityTypeId";
+		EntityType entityType = when(mock(EntityType.class).getId()).thenReturn(importRunEntityTypeId).getMock();
+
+		String importRunIdValue = "importRunId";
+		ImportRun importRun = mock(ImportRun.class);
+		when(importRun.getId()).thenReturn(importRunIdValue);
+		when(importRun.getIdValue()).thenReturn(importRunIdValue);
+		when(importRun.getEntityType()).thenReturn(entityType);
+		when(importRunService.addImportRun(USERNAME, false)).thenReturn(importRun);
+
+		@SuppressWarnings("ConstantConditions")
+		ResponseEntity<String> responseEntity = importWizardController.importFile(httpServletRequest, multipartFile,
+				entityTypeId, packageId, action, notify);
+		assertEquals(responseEntity, ResponseEntity.created(new java.net.URI("/api/v2/entityTypeId/importRunId"))
+												   .contentType(TEXT_PLAIN)
+												   .body("/api/v2/entityTypeId/importRunId"));
+
+		verify(fileStore).store(any(), eq(filename));
+		ArgumentCaptor<ImportJob> importJobArgumentCaptor = ArgumentCaptor.forClass(ImportJob.class);
+		verify(executorService).execute(importJobArgumentCaptor.capture());
+		assertEquals(importJobArgumentCaptor.getValue(),
+				new ImportJob(importService, SecurityContextHolder.getContext(), null, databaseAction, importRunIdValue,
+						importRunService, httpSession, "base"));
 	}
 
 	@Test
-	public void testImportFile() throws IOException, URISyntaxException
+	public void testImportFileActionUnknown() throws IOException, URISyntaxException
 	{
-		// set up the test
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		File file = new File("/src/test/resources/example.xlsx");
+		HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+
+		String filename = "example.xlsx";
+		MultipartFile multipartFile = createMultipartFile(filename);
+		String entityTypeId = null;
+		String packageId = null;
+		String action = "unknownAction";
+		Boolean notify = null;
+
+		String tmpFileName = "tmp-example.xlsx";
+		File tmpFile = mock(File.class);
+		when(tmpFile.getName()).thenReturn(tmpFileName);
+		when(fileStore.store(any(), eq("example.xlsx"))).thenReturn(tmpFile);
+
+		@SuppressWarnings("ConstantConditions")
+		ResponseEntity<String> responseEntity = importWizardController.importFile(httpServletRequest, multipartFile,
+				entityTypeId, packageId, action, notify);
+		assertEquals(responseEntity, ResponseEntity.badRequest()
+												   .contentType(TEXT_PLAIN)
+												   .body("Invalid action:[UNKNOWNACTION] valid values: [ADD, ADD_UPDATE_EXISTING, UPDATE, ADD_IGNORE_EXISTING]"));
+
+		verify(fileStore).store(any(), eq(filename));
+		verifyZeroInteractions(executorService);
+	}
+
+	private MultipartFile createMultipartFile(String filename) throws IOException
+	{
+		File file = new File("/src/test/resources/" + filename);
 
 		DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, file.getName(), (int) file.length(),
 				file.getParentFile());
 		fileItem.getOutputStream();
-		MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-		ArgumentCaptor<InputStream> streamCaptor = ArgumentCaptor.forClass(InputStream.class);
-		when(fileStore.store(streamCaptor.capture(), eq("example.xlsx"))).thenReturn(file);
-		when(fileRepositoryCollectionFactory.createFileRepositoryCollection(file)).thenReturn(repositoryCollection);
-		when(importServiceFactory.getImportService(file.getName())).thenReturn(importService);
-		ImportRun importRun = importRunFactory.create();
-		importRun.setStartDate(date);
-		importRun.setProgress(0);
-		importRun.setStatus(ImportStatus.RUNNING.toString());
-		importRun.setOwner("Harry");
-		importRun.setNotify(false);
-		when(importRunService.addImportRun(SecurityUtils.getCurrentUsername(), false)).thenReturn(importRun);
-
-		// the actual test
-		ResponseEntity<String> response = controller.importFile(request, multipartFile, null, null, "add", null);
-		assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-
-		ArgumentCaptor<ImportJob> captor = ArgumentCaptor.forClass(ImportJob.class);
-		verify(executorService, times(1)).execute(captor.capture());
+		return new CommonsMultipartFile(fileItem);
 	}
 
-	@Test
-	public void testImportUpdateFile() throws IOException, URISyntaxException
-	{
-		// set up the test
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		File file = new File("/src/test/resources/example.xlsx");
-
-		DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, file.getName(), (int) file.length(),
-				file.getParentFile());
-		fileItem.getOutputStream();
-		MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-		ArgumentCaptor<InputStream> streamCaptor = ArgumentCaptor.forClass(InputStream.class);
-		when(fileStore.store(streamCaptor.capture(), eq("example.xlsx"))).thenReturn(file);
-		when(fileRepositoryCollectionFactory.createFileRepositoryCollection(file)).thenReturn(repositoryCollection);
-		when(importServiceFactory.getImportService(file.getName())).thenReturn(importService);
-		ImportRun importRun = importRunFactory.create();
-		importRun.setStartDate(date);
-		importRun.setProgress(0);
-		importRun.setStatus(ImportStatus.RUNNING.toString());
-		importRun.setOwner("Harry");
-		importRun.setNotify(false);
-		when(importRunService.addImportRun(SecurityUtils.getCurrentUsername(), false)).thenReturn(importRun);
-
-		// the actual test
-		ResponseEntity<String> response = controller.importFile(request, multipartFile, null, null, "update", null);
-		assertEquals(response.getStatusCode(), HttpStatus.CREATED);
-		assertEquals(response.getHeaders().getContentType(), TEXT_PLAIN);
-
-		ArgumentCaptor<ImportJob> captor = ArgumentCaptor.forClass(ImportJob.class);
-		verify(executorService, times(1)).execute(captor.capture());
-	}
-
-	@Test
-	public void testImportIllegalUpdateModeFile() throws IOException, URISyntaxException
-	{
-		// set up the test
-		HttpServletRequest request = mock(HttpServletRequest.class);
-		File file = new File("/src/test/resources/example.xlsx");
-
-		DiskFileItem fileItem = new DiskFileItem("file", "text/plain", false, file.getName(), (int) file.length(),
-				file.getParentFile());
-		fileItem.getOutputStream();
-		MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-		ArgumentCaptor<InputStream> streamCaptor = ArgumentCaptor.forClass(InputStream.class);
-		when(fileStore.store(streamCaptor.capture(), eq("example.xlsx"))).thenReturn(file);
-		when(fileRepositoryCollectionFactory.createFileRepositoryCollection(file)).thenReturn(repositoryCollection);
-		when(importServiceFactory.getImportService(file.getName())).thenReturn(importService);
-		ImportRun importRun = importRunFactory.create();
-		importRun.setStartDate(date);
-		importRun.setProgress(0);
-		importRun.setStatus(ImportStatus.RUNNING.toString());
-		importRun.setOwner("Harry");
-		importRun.setNotify(false);
-		when(importRunService.addImportRun(SecurityUtils.getCurrentUsername(), false)).thenReturn(importRun);
-
-		// the actual test
-		ResponseEntity<String> response = controller.importFile(request, multipartFile, null, null, "addsss", null);
-		assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
-		assertEquals(response.getHeaders().getContentType(), TEXT_PLAIN);
-
-		ArgumentCaptor<ImportJob> captor = ArgumentCaptor.forClass(ImportJob.class);
-		verify(executorService, times(0)).execute(captor.capture());
-	}
-
-	@Configuration
-	@Import({ ImportTestConfig.class, GroupTestConfig.class, GroupAuthorityTestConfig.class })
 	static class Config
 	{
-		@Autowired
-		private DataService dataService;
 
-		@Bean
-		public PasswordEncoder passwordEncoder()
-		{
-			return mock(PasswordEncoder.class);
-		}
-
-		@Bean
-		public SystemRepositoryDecoratorRegistry repositoryDecoratorRegistry()
-		{
-			return mock(SystemRepositoryDecoratorRegistry.class);
-		}
-
-		@Bean
-		public UserService userService()
-		{
-			return mock(UserService.class);
-		}
-
-		@Bean
-		public MutableAclService mutableAclService()
-		{
-			return mock(MutableAclService.class);
-		}
-
-		@Bean
-		public PermissionManagerServiceImpl pluginPermissionManagerServiceImpl()
-		{
-			return new PermissionManagerServiceImpl(dataService, mutableAclService());
-		}
-
-		@Bean
-		public MetaDataService metaDataService()
-		{
-			return mock(MetaDataService.class);
-		}
-
-		@Bean
-		public GrantedAuthoritiesMapper grantedAuthoritiesMapper()
-		{
-			return mock(GrantedAuthoritiesMapper.class);
-		}
-
-		@Bean
-		public UserAccountService userAccountService()
-		{
-			return mock(UserAccountService.class);
-		}
-
-		@Bean
-		public MailSender mailSender()
-		{
-			return mock(MailSender.class);
-		}
-
-		@Bean
-		public EntityTypeDependencyResolver entityTypeDependencyResolver()
-		{
-			return mock(EntityTypeDependencyResolver.class);
-		}
 	}
 }
