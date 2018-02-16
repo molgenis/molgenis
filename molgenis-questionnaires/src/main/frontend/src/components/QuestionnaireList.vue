02c7ac57745eaae84f6ac8c7220c3c78ba07ab88
Mark-de-Haan <markdehaan90@gmail.com>
2018-02-16 12:06:11 +0100

    Show status in questionnaire list + link to overview page when done

--- molgenis-questionnaires/src/main/frontend/src/components/QuestionnaireList.vue
+++ molgenis-questionnaires/src/main/frontend/src/components/QuestionnaireList.vue
@@ -20,6 +20,7 @@
             <thead>
             <tr>
               <th>{{ 'questionnaires_table_questionnaire_header' | i18n }}</th>
+              <th>{{ 'questionnaires_table_status_header' | i18n }}</th>
               <th></th>
             </tr>
             </thead>
@@ -28,8 +29,26 @@
 
             <tr v-for="questionnaire in questionnaireList">
               <td>{{ questionnaire.label }}</td>
+
+              <td v-if="questionnaire.status === 'NOT_STARTED'">
+                {{ 'questionnaires_table_status_not_started' | i18n }}
+              </td>
+
+              <td v-else-if="questionnaire.status === 'OPEN'">
+                {{ 'questionnaires_table_status_open' | i18n }}
+              </td>
+
+              <td v-else-if="questionnaire.status === 'SUBMITTED'">
+                {{ 'questionnaires_table_status_submitted' | i18n}}
+              </td>
+
               <td>
-                <router-link :to="'/' + questionnaire.id" class="btn btn-primary">
+                <router-link v-if="questionnaire.status === 'SUBMITTED'" target="_blank" :to="'/' + questionnaire.id + '/overview'"
+                             class="btn btn-primary">
+                  {{ 'questionnaires_view_questionnaire' | i18n }}
+                </router-link>
+
+                <router-link v-else :to="'/' + questionnaire.id" class="btn btn-primary">
                   {{ 'questionnaires_view_questionnaire' | i18n }}
                 </router-link>
               </td>
