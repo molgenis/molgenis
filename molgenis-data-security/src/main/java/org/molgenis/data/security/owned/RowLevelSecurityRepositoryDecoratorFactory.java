871b7f5c1c388bbbde44a8e58357391f10357d9f
Dennis Hendriksen <d.hendriksen@umcg.nl>
2018-02-27 11:30:48 +0100

    Introduce and use MutableAclClassService instead of RowLevelSecuredConfiguration entity type

--- molgenis-data-security/src/main/java/org/molgenis/data/security/owned/RowLevelSecurityRepositoryDecoratorFactory.java
+++ molgenis-data-security/src/main/java/org/molgenis/data/security/owned/RowLevelSecurityRepositoryDecoratorFactory.java
@@ -1,9 +1,9 @@
 package org.molgenis.data.security.owned;
 
-import org.molgenis.data.DataService;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Repository;
 import org.molgenis.data.security.user.UserService;
+import org.molgenis.security.acl.MutableAclClassService;
 import org.molgenis.security.core.UserPermissionEvaluator;
 import org.springframework.security.acls.model.MutableAclService;
 import org.springframework.stereotype.Component;
@@ -16,23 +16,23 @@ import static java.util.Objects.requireNonNull;
 @Component
 public class RowLevelSecurityRepositoryDecoratorFactory
 {
-	private final DataService dataService;
 	private final UserPermissionEvaluator userPermissionEvaluator;
 	private final MutableAclService mutableAclService;
+	private final MutableAclClassService mutableAclClassService;
 	private final UserService userService;
 
-	RowLevelSecurityRepositoryDecoratorFactory(DataService dataService, UserPermissionEvaluator userPermissionEvaluator,
-			MutableAclService mutableAclService, UserService userService)
+	RowLevelSecurityRepositoryDecoratorFactory(UserPermissionEvaluator userPermissionEvaluator,
+			MutableAclService mutableAclService, MutableAclClassService mutableAclClassService, UserService userService)
 	{
-		this.dataService = requireNonNull(dataService);
 		this.userPermissionEvaluator = requireNonNull(userPermissionEvaluator);
 		this.mutableAclService = requireNonNull(mutableAclService);
+		this.mutableAclClassService = requireNonNull(mutableAclClassService);
 		this.userService = requireNonNull(userService);
 	}
 
 	public Repository<Entity> createDecoratedRepository(Repository<Entity> repository)
 	{
-		return new RowLevelSecurityRepositoryDecorator(repository, dataService, userPermissionEvaluator,
-				mutableAclService, userService);
+		return new RowLevelSecurityRepositoryDecorator(repository, userPermissionEvaluator, mutableAclService,
+				mutableAclClassService, userService);
 	}
 }
