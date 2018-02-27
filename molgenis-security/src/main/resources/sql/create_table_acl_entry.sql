e8c79fd9a77d552685be9cf11d806340abe3c04b
tommydeboer <tommydeboer4@gmail.com>
2018-02-27 12:09:40 +0100

    make deleting acl class cascading

--- molgenis-security/src/main/resources/sql/create_table_acl_entry.sql
+++ molgenis-security/src/main/resources/sql/create_table_acl_entry.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS acl_entry (
   audit_success       BOOLEAN NOT NULL,
   audit_failure       BOOLEAN NOT NULL,
   CONSTRAINT unique_uk_4 UNIQUE (acl_object_identity, ace_order),
-  CONSTRAINT foreign_fk_4 FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id),
+  CONSTRAINT foreign_fk_4 FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id) ON DELETE CASCADE,
   CONSTRAINT foreign_fk_5 FOREIGN KEY (sid) REFERENCES acl_sid (id)
 );
--- molgenis-security/src/main/resources/sql/create_table_acl_object_identity.sql
+++ molgenis-security/src/main/resources/sql/create_table_acl_object_identity.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS acl_object_identity (
   entries_inheriting BOOLEAN NOT NULL,
   CONSTRAINT unique_uk_3 UNIQUE (object_id_class, object_id_identity),
   CONSTRAINT foreign_fk_1 FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id),
-  CONSTRAINT foreign_fk_2 FOREIGN KEY (object_id_class) REFERENCES acl_class (id),
+  CONSTRAINT foreign_fk_2 FOREIGN KEY (object_id_class) REFERENCES acl_class (id) ON DELETE CASCADE,
   CONSTRAINT foreign_fk_3 FOREIGN KEY (owner_sid) REFERENCES acl_sid (id)
 );
