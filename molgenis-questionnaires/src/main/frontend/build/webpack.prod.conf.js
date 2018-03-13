a495e346655100b1f38c87890e28dd93773231b4
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-13 17:09:14 +0100

    Fix target directory for production build

--- molgenis-questionnaires/src/main/frontend/build/webpack.prod.conf.js
+++ molgenis-questionnaires/src/main/frontend/build/webpack.prod.conf.js
@@ -26,8 +26,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/questionnaire/[name].js'),
-    chunkFilename: utils.assetsPath('js/questionnaire/[id].js')
+    filename: utils.assetsPath('js/questionnaires/[name].js'),
+    chunkFilename: utils.assetsPath('js/questionnaires/[id].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
@@ -45,7 +45,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/questionnaire/[name].css'),
+      filename: utils.assetsPath('css/questionnaires/[name].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
       // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
