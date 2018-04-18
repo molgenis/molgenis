3ffc34b67dfb4b6eee0e1ed1f799097956947d97
tommydeboer <tommydeboer4@gmail.com>
2018-04-18 11:23:12 +0200

    Add test

--- molgenis-security/src/test/java/org/molgenis/security/token/RunAsUserTokenFactoryTest.java
+++ molgenis-security/src/test/java/org/molgenis/security/token/RunAsUserTokenFactoryTest.java
@@ -1,20 +1,30 @@
 package org.molgenis.security.token;
 
-import org.molgenis.data.MolgenisDataAccessException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 public class RunAsUserTokenFactoryTest
 {
+	private RunAsUserTokenFactory runAsUserTokenFactory;
+	private UserDetailsChecker userDetailsChecker;
 
-	@Test(expectedExceptions = MolgenisDataAccessException.class)
+	@BeforeMethod
+	public void setupBeforeMethod()
+	{
+		userDetailsChecker = mock(UserDetailsChecker.class);
+		runAsUserTokenFactory = new RunAsUserTokenFactory(userDetailsChecker);
+	}
+
+	@Test
 	public void testTokenForDisabledUser()
 	{
 		UserDetails userDetails = mock(UserDetails.class);
-		when(userDetails.isEnabled()).thenReturn(false);
-//		TokenFactory.getRestAuthenticationToken(userDetails, "test");
+		runAsUserTokenFactory.create("test", userDetails, null);
+		verify(userDetailsChecker).check(userDetails);
 	}
 }
