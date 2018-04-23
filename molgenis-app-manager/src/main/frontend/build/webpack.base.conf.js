3258a34f6603082dda1bd3c9d5b5521b6a3bb638
Mark-de-Haan <markdehaan90@gmail.com>
2018-04-23 13:34:04 +0200

    Get test framework working

--- molgenis-app-manager/src/main/frontend/build/webpack.base.conf.js
+++ molgenis-app-manager/src/main/frontend/build/webpack.base.conf.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      'src': resolve('src'),
     }
   },
   module: {
--- molgenis-app-manager/src/main/frontend/test/unit/karma.conf.js
+++ molgenis-app-manager/src/main/frontend/test/unit/karma.conf.js
@@ -1,20 +1,15 @@
-// This is a karma config file. For more details see
-//   http://karma-runner.github.io/0.13/config/configuration-file.html
-// we are also using it with karma-webpack
-//   https://github.com/webpack/karma-webpack
-
 var webpackConfig = require('../../build/webpack.test.conf')
 
-module.exports = function karmaConfig (config) {
+module.exports = function (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    browsers: ['PhantomJS'], //ChromeCanary
+    frameworks: ['mocha', 'sinon-chai'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: [
+      '../../node_modules/babel-polyfill/dist/polyfill.js',
+      '../../node_modules/es6-promise/dist/es6-promise.auto.js',
+      './index.js'
+    ],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
@@ -23,11 +18,15 @@ module.exports = function karmaConfig (config) {
       noInfo: true
     },
     coverageReporter: {
-      dir: './coverage',
+      dir: '../../../../../target/generated-sources/coverage',
       reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
+        {type: 'cobertura', subdir: 'cobertura'}
       ]
+    },
+    junitReporter: {
+      outputDir: '../../../../../target/surefire-reports',
+      outputFile: 'TEST-results.xml',
+      useBrowserName: false
     }
   })
 }
