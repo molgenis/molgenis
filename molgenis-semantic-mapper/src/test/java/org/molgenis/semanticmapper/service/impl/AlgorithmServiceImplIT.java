60a5505685d58e29e10334418926330f13767177
Fleur Kelpin <fleur@kelpin.nl>
2018-05-03 16:05:57 +0200

    Speed up javascript and magmascript evaluation (#7323)
    * Add performance test
    
    * Cache compiled expressions, bind entity map to $ function on the java side
    
    * Let script-evaluator.js only expose evalScript function and new MagmaScript class.
    
    Put magmascript instance functions on the class's prototype and static functions on MagmaScript class.
    Copy the static functions to nashorn engine scope for server side script evaluation
    Copy the static functions to local scope inside evalScript function.
    
    * Move magma bindings creation to JSMagmaScriptEvaluator.
    
    * Move magmascript-specific Bindings from NashornScriptEngine to JSMagmaScriptEvaluator.
    Create constants for the expected bindings keys.
    
    * Weigh compiled expressions cache based on expression length.
    
    * Split off performance test to separate test case.
    
    * Hide Bindings implementation details from callers.
    
    * Suppress unused variable warnings about variables put in scope for eval-ed script to use.
    
    * Fix JsMagmaScriptEvaluatorTest
    
    * Unwrap javax.script.ScriptException
    
    * Remove unused option to specify fetching depth for evalList.
    
    * Process review comments.
    
    * Add ExpressionValidatorTest
    
    (cherry picked from commit b3f2e0e)

--- molgenis-semantic-mapper/src/test/java/org/molgenis/semanticmapper/service/impl/AlgorithmServiceImplIT.java
+++ molgenis-semantic-mapper/src/test/java/org/molgenis/semanticmapper/service/impl/AlgorithmServiceImplIT.java
@@ -43,6 +43,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import javax.script.ScriptException;
+import java.io.IOException;
 import java.text.ParseException;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -540,13 +542,13 @@ public class AlgorithmServiceImplIT extends AbstractMolgenisSpringTest
 		}
 
 		@Bean
-		public JsMagmaScriptEvaluator jsScriptEvaluator()
+		public JsMagmaScriptEvaluator jsScriptEvaluator() throws ScriptException, IOException
 		{
 			return new JsMagmaScriptEvaluator(new NashornScriptEngine());
 		}
 
 		@Bean
-		public AlgorithmService algorithmService()
+		public AlgorithmService algorithmService() throws ScriptException, IOException
 		{
 			return new AlgorithmServiceImpl(ontologyTagService(), semanticSearchService(), algorithmGeneratorService(),
 					entityManager(), jsScriptEvaluator());
