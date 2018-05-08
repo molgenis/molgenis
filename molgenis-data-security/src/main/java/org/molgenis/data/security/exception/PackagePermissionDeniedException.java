dd486283572987286deadfc843aae34660249940
Bart Charbon <b.charbon@umcg.nl>
2018-05-08 13:35:19 +0200

    Fix formatting

--- molgenis-data-security/src/main/java/org/molgenis/data/security/exception/PackagePermissionDeniedException.java
+++ molgenis-data-security/src/main/java/org/molgenis/data/security/exception/PackagePermissionDeniedException.java
@@ -6,25 +6,28 @@ import org.molgenis.data.security.PackagePermission;
 import static java.util.Objects.requireNonNull;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class PackagePermissionDeniedException extends PermissionDeniedException {
-  private static final String ERROR_CODE = "DS01";
-  private final PackagePermission permission;
-  private final transient Package pack;
+public class PackagePermissionDeniedException extends PermissionDeniedException
+{
+	private static final String ERROR_CODE = "DS01";
+	private final PackagePermission permission;
+	private final transient Package pack;
 
-  public PackagePermissionDeniedException(PackagePermission permission, Package pack) {
-    super(ERROR_CODE);
-    this.permission = requireNonNull(permission);
-    this.pack = requireNonNull(pack);
-  }
+	public PackagePermissionDeniedException(PackagePermission permission, Package pack)
+	{
+		super(ERROR_CODE);
+		this.permission = requireNonNull(permission);
+		this.pack = requireNonNull(pack);
+	}
 
-  @Override
-  public String getMessage()
-  {
-    return String.format("permission:%s package:%s", permission.getName(), pack.getId());
-  }
+	@Override
+	public String getMessage()
+	{
+		return String.format("permission:%s package:%s", permission.getName(), pack.getId());
+	}
 
-  @Override
-  protected Object[] getLocalizedMessageArguments() {
-    return new Object[] {permission.getName(), pack.getLabel()};
-  }
+	@Override
+	protected Object[] getLocalizedMessageArguments()
+	{
+		return new Object[] { permission.getName(), pack.getLabel() };
+	}
 }
