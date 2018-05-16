b41014f6b671376f30f38be5b5eac5d92d2905eb
sido <sido@haakma.org>
2018-05-16 09:50:43 +0200

    fix(index.js): removed proxytable because of failing e2e tests

--- molgenis-questionnaires/src/main/frontend/config/index.js
+++ molgenis-questionnaires/src/main/frontend/config/index.js
@@ -15,14 +15,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/api': {
-        target: 'http://localhost:8080'
-      },
-      '/menu/plugins': {
-        target: 'http://localhost:8080'
-      }
-    },
+    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
