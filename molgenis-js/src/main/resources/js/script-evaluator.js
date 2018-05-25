5deb2c2ac9ad0f6dce8dba7f72d7eac27cb118c4
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-05-25 16:31:37 +0200

    Revert "Merge pull request #7380 from sidohaakma/6.1"
    This reverts commit 64c1724a8bc467d6fe04985e0bf468cd62fc1961, reversing
    changes made to d705df0a9f4f4c32d33ce99b66a7ae8487154513.

--- molgenis-js/src/main/resources/js/script-evaluator.js
+++ molgenis-js/src/main/resources/js/script-evaluator.js
@@ -35,6 +35,18 @@ function MagmaScript (val) {
  *
  */
 MagmaScript.prototype.value = function () {
+    if (this.val !== null && typeof this.val === 'object') {
+        if (this.val['_idValue'] !== undefined) {
+
+            // Map a list of entities to a list of ID's
+            if (Array.isArray(this.val)) {
+                return this.val.map(function (value) {
+                    return new MagmaScript.newValue(value).value()
+                })
+            }
+            return this.val['_idValue']
+        }
+    }
     return this.val
 }
 
@@ -160,20 +172,12 @@ MagmaScript.prototype.age = function () {
  * @method map
  */
 MagmaScript.prototype.map = function (categoryMapping, defaultValue, nullValue) {
-    function toIdValue (value) {
-        if (value === null) return null
-        if (value === undefined) return undefined
-        if (Array.isArray(value)) return value.map(toIdValue)
-        if (typeof value === 'object' && value['_idValue'] !== undefined) return value._idValue
-        return value
-    }
-
     if (typeof categoryMapping === 'function') {
         this.val = this.val.map(MagmaScript.newValue).map(categoryMapping)
     } else {
-        var idValue = toIdValue(this.val)
-        if (idValue in categoryMapping) {
-            this.val = categoryMapping[idValue]
+        this.val = this.value()
+        if (this.val in categoryMapping) {
+            this.val = categoryMapping[this.val]
         } else {
             if (nullValue !== undefined && ((this.val === undefined) || (this.val === null))) {
                 this.val = nullValue
