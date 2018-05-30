1cacdf185eebdf4b211f3db948a3eaf3474d9d40
tommydeboer <tommydeboer4@gmail.com>
2018-05-30 09:32:28 +0200

    Fix use of UnknownEntityException

--- molgenis-data-security/src/main/java/org/molgenis/data/security/permission/RoleMembershipServiceImpl.java
+++ molgenis-data-security/src/main/java/org/molgenis/data/security/permission/RoleMembershipServiceImpl.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
-import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static org.molgenis.data.security.auth.RoleMembershipMetadata.ROLE_MEMBERSHIP;
+import static org.molgenis.data.security.auth.RoleMetadata.NAME;
+import static org.molgenis.data.security.auth.UserMetaData.USERNAME;
 
 @Component
 public class RoleMembershipServiceImpl implements RoleMembershipService
@@ -18,13 +19,17 @@ public class RoleMembershipServiceImpl implements RoleMembershipService
 	private final UserService userService;
 	private final RoleMembershipFactory roleMembershipFactory;
 	private final DataService dataService;
+	private final UserMetaData userMetaData;
+	private final RoleMetadata roleMetadata;
 
 	RoleMembershipServiceImpl(UserService userService, RoleMembershipFactory roleMembershipFactory,
-			DataService dataService)
+			DataService dataService, UserMetaData userMetaData, RoleMetadata roleMetadata)
 	{
 		this.userService = requireNonNull(userService);
 		this.roleMembershipFactory = requireNonNull(roleMembershipFactory);
 		this.dataService = requireNonNull(dataService);
+		this.userMetaData = requireNonNull(userMetaData);
+		this.roleMetadata = requireNonNull(roleMetadata);
 	}
 
 	@Override
@@ -33,13 +38,13 @@ public class RoleMembershipServiceImpl implements RoleMembershipService
 		User groupCreator = userService.getUser(username);
 		if (groupCreator == null)
 		{
-			throw new UnknownEntityException(format("User with name [%s] not found", username));
+			throw new UnknownEntityException(userMetaData, userMetaData.getAttribute(USERNAME), username);
 		}
 
-		Role role = dataService.query(RoleMetadata.ROLE, Role.class).eq(RoleMetadata.NAME, rolename).findOne();
+		Role role = dataService.query(RoleMetadata.ROLE, Role.class).eq(NAME, rolename).findOne();
 		if (role == null)
 		{
-			throw new UnknownEntityException(format("Role with name [%s] not found", rolename));
+			throw new UnknownEntityException(roleMetadata, roleMetadata.getAttribute(NAME), rolename);
 		}
 
 		RoleMembership roleMembership = roleMembershipFactory.create();
