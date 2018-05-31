ab2af57daf0a2c3b6874778584a7e29633d09943
Fleur Kelpin <f.kelpin@umcg.nl>
2018-05-31 15:29:02 +0200

    Validate group name pattern and document response codes for the addGroup method.

--- molgenis-security/src/main/java/org/molgenis/security/group/GroupRestController.java
+++ molgenis-security/src/main/java/org/molgenis/security/group/GroupRestController.java
@@ -6,7 +6,9 @@ import org.molgenis.data.security.permission.RoleMembershipService;
 import org.molgenis.security.core.GroupValueFactory;
 import org.molgenis.security.core.model.GroupValue;
 import org.molgenis.security.core.model.RoleValue;
+import org.molgenis.web.ErrorMessageResponse;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,7 @@ import static org.molgenis.data.security.auth.GroupService.MANAGER;
 import static org.molgenis.security.core.utils.SecurityUtils.getCurrentUsername;
 
 @RestController
+@Validated
 @Api("Group")
 public class GroupRestController
 {
@@ -39,12 +42,15 @@ public class GroupRestController
 	@ApiOperation(value = "Create a new Group", response = String.class)
 	@Transactional
 	@ApiResponses({
+			@ApiResponse(code = 400, message = "When the request is incorrect", response = ErrorMessageResponse.class),
+			@ApiResponse(code = 401, message = "When authentication information is missing", response = ErrorMessageResponse.class),
+			@ApiResponse(code = 403, message = "When the authenticated user has insufficient permissions", response = ErrorMessageResponse.class),
 			@ApiResponse(code = 200, message = "The name of the newly created group", response = String.class) })
 	public String createGroup(
-			@ApiParam("Alphanumeric name for the group") @Pattern(regexp = "^[a-z][a-z0-9]*(-[a-z0-9]+)*$") @RequestParam(value = "name") @Nullable String name,
+			@ApiParam("Alphanumeric name for the group") @Pattern(regexp = "^[a-z][a-z0-9]*(-[a-z0-9]+)*$") @RequestParam(name = "name", value = "name") String name,
 			@ApiParam("Label for the group") @RequestParam("label") String label,
 			@ApiParam("Description for the group") @RequestParam(value = "description", required = false) @Nullable String description,
-			@ApiParam("Indication if this group should be publicly visible (not yet implemented!)") @RequestParam(value = "public", required = false, defaultValue = "true") boolean publiclyVisible)
+			@ApiParam(value = "Indication if this group should be publicly visible (not yet implemented!)", defaultValue = "true") @RequestParam(value = "public", required = false, defaultValue = "true") boolean publiclyVisible)
 	{
 		GroupValue groupValue = groupValueFactory.createGroup(name, label, description, publiclyVisible,
 				DEFAULT_ROLES.keySet());
