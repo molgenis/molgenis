89bb78f60d1d73fc2e915052607fe1f788dc5826
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-06-15 09:51:43 +0200

    code cleanup, fix lint warnings

--- molgenis-security-ui/src/main/java/org/molgenis/securityui/controller/SecurityUiController.java
+++ molgenis-security-ui/src/main/java/org/molgenis/securityui/controller/SecurityUiController.java
@@ -5,7 +5,6 @@ import org.molgenis.core.ui.menu.MenuReaderService;
 import org.molgenis.security.user.UserAccountService;
 import org.molgenis.settings.AppSettings;
 import org.molgenis.web.PluginController;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,15 +19,12 @@ public class SecurityUiController extends VuePluginController
 	public static final String ID = "security-ui";
 	public static final String URI = PluginController.PLUGIN_URI_PREFIX + ID;
 
-	public static final String VIEW_TEMPLATE = "view-security-ui";
-
-	private AppSettings appSettings;
+	static final String VIEW_TEMPLATE = "view-security-ui";
 
 	SecurityUiController(MenuReaderService menuReaderService, AppSettings appSettings,
 			UserAccountService userAccountService)
 	{
 		super(URI, menuReaderService, appSettings, userAccountService);
-		this.appSettings = appSettings;
 	}
 
 	@GetMapping("/**")
