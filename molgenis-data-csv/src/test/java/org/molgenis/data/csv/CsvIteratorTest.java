e6f2ae231425017b00af5c819a61e2eac3bacf5c
Dennis Hendriksen <d.hendriksen@umcg.nl>
2018-06-22 11:29:37 +0200

    Fix #7434 Test resource concurrency issues

--- molgenis-data-csv/src/test/java/org/molgenis/data/csv/CsvIteratorTest.java
+++ molgenis-data-csv/src/test/java/org/molgenis/data/csv/CsvIteratorTest.java
@@ -2,7 +2,6 @@ package org.molgenis.data.csv;
 
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Sets;
-import org.apache.commons.io.FileUtils;
 import org.molgenis.data.AbstractMolgenisSpringTest;
 import org.molgenis.data.Entity;
 import org.molgenis.data.MolgenisDataException;
@@ -10,11 +9,12 @@ import org.molgenis.data.meta.model.AttributeFactory;
 import org.molgenis.data.meta.model.EntityType;
 import org.molgenis.data.meta.model.EntityTypeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
@@ -40,8 +40,7 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testIteratorFromCsvFile() throws IOException
 	{
-		File csvFile = createTmpFileForResource("testdata.csv");
-
+		File csvFile = new ClassPathResource("testdata.csv").getFile();
 		CsvIterator it = new CsvIterator(csvFile, "testdata", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
@@ -56,49 +55,22 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test(expectedExceptions = MolgenisDataException.class, expectedExceptionsMessageRegExp = "Number of values \\(1\\) doesn't match the number of headers \\(2\\): \\[val1\\]")
 	public void testIteratorValueHeaderMismatchOneNonEmptyValue() throws IOException
 	{
-		File csvFile = File.createTempFile("testdata", ".csv");
-		try
-		{
-			try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(csvFile)))
-			{
-				outputStreamWriter.write("col1,col2\n");
-				outputStreamWriter.write("val1\n");
-			}
-			new CsvIterator(csvFile, "testdata", null, ',', entityType).next();
-		}
-		finally
-		{
-			//noinspection ResultOfMethodCallIgnored
-			csvFile.delete();
-		}
+		File csvFile = new ClassPathResource("testdatamissingvalue.csv").getFile();
+		new CsvIterator(csvFile, "testdatamissingvalue", null, ',', entityType).next();
 	}
 
 	@SuppressWarnings("deprecation")
 	@Test(expectedExceptions = MolgenisDataException.class, expectedExceptionsMessageRegExp = "Number of values \\(2\\) doesn't match the number of headers \\(3\\): \\[val1,val2\\]")
 	public void testIteratorValueHeaderMismatch() throws IOException
 	{
-		File csvFile = File.createTempFile("testdata", ".csv");
-		try
-		{
-			try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(csvFile)))
-			{
-				outputStreamWriter.write("col1,col2,col3\n");
-				outputStreamWriter.write("val1,val2\n");
-			}
-			new CsvIterator(csvFile, "testdata", null, ',', entityType).next();
-		}
-		finally
-		{
-			//noinspection ResultOfMethodCallIgnored
-			csvFile.delete();
-		}
+		File csvFile = new ClassPathResource("testdatamissingvalue2.csv").getFile();
+		new CsvIterator(csvFile, "testdatamissingvalue", null, ',', entityType).next();
 	}
 
 	@Test
 	public void testIteratorFromZipFile() throws IOException
 	{
-		File zipFile = createTmpFileForResource("zipFile.zip");
-
+		File zipFile = new ClassPathResource("zipFile.zip").getFile();
 		CsvIterator it = new CsvIterator(zipFile, "testdata", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
@@ -107,8 +79,7 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testIteratorFromZipFileWithFolder() throws IOException
 	{
-		File zipFile = createTmpFileForResource("zipFileWithFolder.zip");
-
+		File zipFile = new ClassPathResource("zipFileWithFolder.zip").getFile();
 		CsvIterator it = new CsvIterator(zipFile, "testdata", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
@@ -117,8 +88,7 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testIteratorFromCsvFileWithBom() throws IOException
 	{
-		File csvFile = createTmpFileForResource("testDataWithBom.csv");
-
+		File csvFile = new ClassPathResource("testDataWithBom.csv").getFile();
 		CsvIterator it = new CsvIterator(csvFile, "testdata", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
@@ -132,8 +102,7 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testIteratorFromZipFileWithBom() throws IOException
 	{
-		File zipFile = createTmpFileForResource("zipFileWithBom.zip");
-
+		File zipFile = new ClassPathResource("zipFileWithBom.zip").getFile();
 		CsvIterator it = new CsvIterator(zipFile, "testDataWithBom", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
@@ -142,33 +111,17 @@ public class CsvIteratorTest extends AbstractMolgenisSpringTest
 	@Test
 	public void testIteratorFromZipFileWithFolderWithBom() throws IOException
 	{
-		File zipFile = createTmpFileForResource("zipFileWithFolderWithBom.zip");
-
+		File zipFile = new ClassPathResource("zipFileWithFolderWithBom.zip").getFile();
 		CsvIterator it = new CsvIterator(zipFile, "testDataWithBom", null, null, entityType);
 		assertEquals(it.getColNamesMap().keySet(), Sets.newLinkedHashSet(Arrays.asList("col1", "col2")));
 		assertEquals(Iterators.size(it), 5);
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test(expectedExceptions = MolgenisDataException.class, expectedExceptionsMessageRegExp = "Duplicate column header 'col1' not allowed")
 	public void testCsvIteratorDuplicateColumnHeader() throws IOException
 	{
-		File tmpFile = createTmpFileForResource("duplicate-sheet-header.csv");
-		try
-		{
-			new CsvIterator(tmpFile, "duplicate-sheet-header", null, ',', entityType);
-		}
-		finally
-		{
-			//noinspection ResultOfMethodCallIgnored
-			tmpFile.delete();
-		}
-	}
-
-	private File createTmpFileForResource(String fileName) throws IOException
-	{
-		InputStream in = getClass().getResourceAsStream("/" + fileName);
-		File csvFile = new File(FileUtils.getTempDirectory(), fileName);
-		FileCopyUtils.copy(in, new FileOutputStream(csvFile));
-		return csvFile;
+		File tmpFile = new ClassPathResource("duplicate-sheet-header.csv").getFile();
+		new CsvIterator(tmpFile, "duplicate-sheet-header", null, ',', entityType);
 	}
 }
