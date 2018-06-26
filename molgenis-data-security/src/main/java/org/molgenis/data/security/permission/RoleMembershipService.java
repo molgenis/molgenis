32a1c600529361385d8f2ffa89fe242356eeb27b
connor.stroomberg <connorstroomberg@users.noreply.github.com>
2018-06-26 22:32:20 +0200

    WIP setup for add member to group

--- molgenis-data-security/src/main/java/org/molgenis/data/security/permission/RoleMembershipService.java
+++ molgenis-data-security/src/main/java/org/molgenis/data/security/permission/RoleMembershipService.java
@@ -2,12 +2,15 @@ package org.molgenis.data.security.permission;
 
 import org.molgenis.data.security.auth.Role;
 import org.molgenis.data.security.auth.RoleMembership;
+import org.molgenis.data.security.auth.User;
 
 import java.util.Collection;
 
 public interface RoleMembershipService
 {
 	void addUserToRole(String username, String roleName);
+
+	void addUserToRole(final User user, final Role role);
 
 	Collection<RoleMembership> getMemberships(Collection<Role> roles);
 }
