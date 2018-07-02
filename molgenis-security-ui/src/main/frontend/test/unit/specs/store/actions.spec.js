0ef45b785f80e2c3a179ac0a88b570d7ab4cf57c
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-07-02 12:59:07 +0200

    Add test for actions; fetchGroupRoles, tempFetchUsers and fetchGroupMembers.

--- molgenis-security-ui/src/main/frontend/test/unit/specs/store/actions.spec.js
+++ molgenis-security-ui/src/main/frontend/test/unit/specs/store/actions.spec.js
@@ -43,6 +43,143 @@ describe('actions', () => {
       testUtils.testAction(actions.fetchGroups, options, done)
     })
   })
+
+  describe('fetchGroupRoles', () => {
+
+    const groupName = 'my-group'
+
+    it('should fetch a list of groups roles for a given group and commit them to the store', done => {
+      const roles = [
+        { roleName: 'VIEWER', roleLabel: 'Viewer' },
+        { roleName: 'ADMIN', roleLabel: 'Admin' }
+      ]
+
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/group/my-group/role')).thenResolve(roles)
+      td.replace(api, 'get', get)
+
+      const options = {
+        payload: groupName,
+        expectedMutations: [
+          { type: 'setGroupRoles', payload: { groupName, groupRoles: roles } }
+        ]
+      }
+
+      testUtils.testAction(actions.fetchGroupRoles, options, done)
+    })
+
+    it('should commit any errors to the store', done => {
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/group/error-group/role')).thenReject()
+      td.replace(api, 'get', get)
+
+      const options = {
+        payload: 'error-group',
+        expectedMutations: [
+          {type: 'setToast', payload: { type: 'danger', message: 'Error when calling backend' }}
+        ]
+      }
+
+      testUtils.testAction(actions.fetchGroupRoles, options, done)
+    })
+  })
+
+  describe('tempFetchUsers', () => {
+
+    it('should fetch a list of all users ( until we have a invite system)', done => {
+      const users = [
+        { id: 'dsds-34324-2', username: 'user1' },
+        { id: 'dsds-34324-3', username: 'user2' }
+      ]
+
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/user')).thenResolve(users)
+      td.replace(api, 'get', get)
+
+      const options = {
+        expectedMutations: [
+          { type: 'setUsers', payload: users }
+        ]
+      }
+
+      testUtils.testAction(actions.tempFetchUsers, options, done)
+    })
+
+    it('should commit any errors to the store', done => {
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/user')).thenReject()
+      td.replace(api, 'get', get)
+
+      const options = {
+        expectedMutations: [
+          {type: 'setToast', payload: { type: 'danger', message: 'Error when calling backend' }}
+        ]
+      }
+
+      testUtils.testAction(actions.tempFetchUsers, options, done)
+    })
+  })
+
+  describe('fetchGroupMembers', () => {
+
+    const groupName = 'my-group'
+
+    it('should fetch a list of groups roles for a given group and commit them to the store', done => {
+      const members = [
+        {
+          user: {id: '123-abc', username: 'user1'},
+          role: {roleName: 'ADMIN', roleLabel: 'Admin'}
+        },
+        {
+          user: {id: '456-dfg', username: 'user2'},
+          role: {roleName: 'VIEWER', roleLabel: 'Viewer'}
+        }
+      ]
+
+      const groupMembers = [
+        {
+          userId: members[0].user.id,
+          username: members[0].user.username,
+          roleName: members[0].role.roleName,
+          roleLabel: members[0].role.roleLabel
+        }, {
+          userId: members[1].user.id,
+          username: members[1].user.username,
+          roleName: members[1].role.roleName,
+          roleLabel: members[1].role.roleLabel
+        }
+      ]
+
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/group/my-group/member')).thenResolve(members)
+      td.replace(api, 'get', get)
+
+      const options = {
+        payload: groupName,
+        expectedMutations: [
+          { type: 'setGroupMembers', payload: { groupName, groupMembers } }
+        ]
+      }
+
+      testUtils.testAction(actions.fetchGroupMembers, options, done)
+    })
+
+    it('should commit any errors to the store', done => {
+      const get = td.function('api.get')
+      td.when(get('/api/plugin/security/group/error-group/member')).thenReject()
+      td.replace(api, 'get', get)
+
+      const options = {
+        payload: 'error-group',
+        expectedMutations: [
+          {type: 'setToast', payload: { type: 'danger', message: 'Error when calling backend' }}
+        ]
+      }
+
+      testUtils.testAction(actions.fetchGroupMembers, options, done)
+    })
+  })
+
   describe('createGroup', () => {
     it('should create a group and displays toast', done => {
       const createGroupCommand = {
