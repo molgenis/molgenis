60bbd162e1e87fcf8968f1fd2b7437e45f94e14a
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-07-02 15:25:22 +0200

    test for groupDetail component

---
+++ molgenis-security-ui/src/main/frontend/test/unit/specs/components/GroupDetail.spec.js
@@ -0,0 +1,95 @@
+import GroupDetail from '../../../../src/components/GroupDetail'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import td from 'testdouble'
+import Vuex from 'vuex'
+
+const $t = (key) => {
+  const translations = {}
+  return translations[key]
+}
+
+describe('GroupDetail component', () => {
+  let getters
+  let mutations
+  let actions
+  let localVue
+  let state
+  let store
+
+  let pushedRoute = {}
+  const $router = {
+    push: function (pushed) {
+      pushedRoute = pushed
+    }
+  }
+  const $route = {
+    path: '/group'
+  }
+
+  const groupMembers = {
+    group1: [
+      {
+        userId: 'user-1',
+        username: 'bert',
+        roleName: 'VIEWER',
+        roleLabel: 'Viewer'
+      },
+      {
+        userId: 'user-2',
+        username: 'ans',
+        roleName: 'MANAGER',
+        roleLabel: 'Manager'
+      }
+    ]
+  }
+
+  const loginUser = {
+    name: 'admin',
+    isSuperUser: true
+  }
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Vuex)
+    localVue.filter('i18n', $t)
+
+    state = {
+      loginUser: loginUser
+    }
+
+    actions = {
+      fetchGroupMembers: () => td.function()
+    }
+
+    getters = {
+      groupMembers: () => groupMembers,
+    }
+
+    mutations = {
+      clearToast: td.function()
+    }
+
+    store = new Vuex.Store({state, actions, getters, mutations})
+  })
+
+  const stubs = ['router-link', 'router-view']
+
+  it('should return the groupMembers via a getter', () => {
+    const wrapper = shallowMount(GroupDetail, { store, stubs, localVue })
+    expect(wrapper.vm.groupMembers).to.deep.equal(groupMembers)
+  })
+  it('should navigate to the group add page', () => {
+    const wrapper = shallowMount(GroupDetail, {
+      propsData: {
+        name: 'group1'
+      },
+      mocks: { $router, $route },
+      store,
+      stubs,
+      localVue
+    })
+    wrapper.find('#add-member-btn').trigger('click')
+    expect(pushedRoute).to.deep.equal({name: 'addMember', params: { groupName: 'group1' }})
+    td.verify(mutations.clearToast(state, undefined))
+  })
+})
