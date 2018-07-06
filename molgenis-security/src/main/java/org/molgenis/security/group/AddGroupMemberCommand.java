1e63e25145ddad6e76574e78fb9024bb4ac30ba2
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-07-06 13:36:54 +0200

    Add test for GroupRestController + use auto_value instead of pojo

--- molgenis-security/src/main/java/org/molgenis/security/group/AddGroupMemberCommand.java
+++ molgenis-security/src/main/java/org/molgenis/security/group/AddGroupMemberCommand.java
@@ -8,6 +8,10 @@ import org.molgenis.util.AutoGson;
 @SuppressWarnings("squid:S1610")
 public abstract class AddGroupMemberCommand
 {
+	static AddGroupMemberCommand create(String username, String roleName) {
+		return new AutoValue_AddGroupMemberCommand(username, roleName);
+	}
+
 	public abstract String getUsername();
 	public abstract String getRoleName();
 }
