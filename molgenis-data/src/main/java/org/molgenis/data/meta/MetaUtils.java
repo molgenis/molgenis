471853a79c0cb9e568e5333d5998ad008b279e61
mkslofstra <mariska-karina@hotmail.com>
2018-07-13 14:15:10 +0200

    Add questionnaire report docs

--- molgenis-data/src/main/java/org/molgenis/data/meta/MetaUtils.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/MetaUtils.java
@@ -6,7 +6,6 @@ import org.molgenis.data.meta.model.Package;
 import static org.molgenis.data.meta.model.EntityTypeMetadata.*;
 import static org.molgenis.data.meta.model.Package.PACKAGE_SEPARATOR;
 import static org.molgenis.data.system.model.RootSystemPackage.PACKAGE_SYSTEM;
-import static org.molgenis.security.core.runas.RunAsSystemAspect.runAsSystem;
 
 public class MetaUtils
 {
@@ -37,11 +36,8 @@ public class MetaUtils
 	 */
 	public static boolean isSystemPackage(Package aPackage)
 	{
-		//Needs to run as system since you might not have permission to read the sys package
-		//Once we have READ_META permission checks on the packages this runAsSystem can be removed
-		//https://github.com/molgenis/molgenis/issues/7528
-		return runAsSystem(() -> aPackage.getId().equals(PACKAGE_SYSTEM) || (aPackage.getRootPackage() != null
-				&& aPackage.getRootPackage().getId().equals(PACKAGE_SYSTEM)));
+		return aPackage.getId().equals(PACKAGE_SYSTEM) || (aPackage.getRootPackage() != null
+				&& aPackage.getRootPackage().getId().equals(PACKAGE_SYSTEM));
 	}
 
 	public static String getFullyQualyfiedName(Package aPackage)
