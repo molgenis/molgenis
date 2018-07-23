6b7e7841b45a4b4b3e71f57eb10f710e0a9993db
sido <sido@haakma.org>
2018-07-23 16:58:23 +0200

    placed preauth annotation on top of controller and service

--- molgenis-ontology/src/main/java/org/molgenis/ontology/sorta/controller/SortaController.java
+++ molgenis-ontology/src/main/java/org/molgenis/ontology/sorta/controller/SortaController.java
@@ -43,6 +43,7 @@ import org.molgenis.web.PluginController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -78,6 +79,7 @@ import static org.molgenis.ontology.utils.SortaServiceUtil.getEntityAsMap;
 
 @Controller
 @RequestMapping(URI)
+@PreAuthorize("hasAnyRole('ROLE_SU')")
 public class SortaController extends PluginController
 {
 	private static final Logger LOG = LoggerFactory.getLogger(SortaController.class);
