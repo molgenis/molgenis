45b35b272e89652808e5df4b047dd1ed7e93067c
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-08-24 11:15:40 +0200

    cleanup imports

--- molgenis-jobs/src/main/java/org/molgenis/jobs/JobExecutionConfig.java
+++ molgenis-jobs/src/main/java/org/molgenis/jobs/JobExecutionConfig.java
@@ -3,8 +3,10 @@ package org.molgenis.jobs;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.molgenis.data.DataService;
 import org.molgenis.data.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.mail.MailSender;
 
 import java.util.concurrent.ExecutorService;
