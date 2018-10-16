871a1f3be6de9dc54027885856dbe50d99174c60
Fleur Kelpin <f.kelpin@umcg.nl>
2018-10-16 15:35:25 +0200

    feat (semantic-mapper): Make depth configurable in mapping project.

--- molgenis-semantic-mapper/src/main/java/org/molgenis/semanticmapper/service/MappingService.java
+++ molgenis-semantic-mapper/src/main/java/org/molgenis/semanticmapper/service/MappingService.java
@@ -16,7 +16,7 @@ public interface MappingService {
    *
    * @param target name of the first target entity
    */
-  MappingProject addMappingProject(String name, String target);
+  MappingProject addMappingProject(String name, String target, int depth);
 
   /**
    * Retrieves all {@link MappingProject}s.
