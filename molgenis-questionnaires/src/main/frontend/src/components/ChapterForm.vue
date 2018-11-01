988539793c515e7e87ec1e42a24415df2578dded
mkslofstra <mariska-karina@hotmail.com>
2018-11-01 14:06:03 +0100

    [FIX] Styling of questionnaire

--- molgenis-questionnaires/src/main/frontend/src/components/ChapterForm.vue
+++ molgenis-questionnaires/src/main/frontend/src/components/ChapterForm.vue
@@ -9,6 +9,20 @@
   </form-component>
 </template>
 
+<style>
+  .questionnaire-chapter .pl-2 > fieldset > small {
+    font-size: 100%;
+  }
+
+  .questionnaire-chapter [class^="pl-"] > fieldset > legend {
+    padding-top: 1rem;
+  }
+
+  .questionnaire-chapter .pl-4 > fieldset > legend {
+    font-size: 100%;
+  }
+</style>
+
 <script>
   import { FormComponent } from '@molgenis/molgenis-ui-form'
   import 'flatpickr/dist/flatpickr.css'
