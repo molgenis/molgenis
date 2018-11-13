1b05595afb20fcbb1ea7628b39951ad4b627ab60
tommydeboer <tommydeboer4@gmail.com>
2018-11-13 13:59:35 +0100

    Add ResourceCollector for easy retrieval of actual resources

--- molgenis-navigator/src/main/java/org/molgenis/navigator/model/util/ResourceIdentifierUtil.java
+++ molgenis-navigator/src/main/java/org/molgenis/navigator/model/util/ResourceIdentifierUtil.java
@@ -9,9 +9,14 @@ import org.molgenis.navigator.model.ResourceIdentifier;
 import org.molgenis.navigator.model.ResourceType;
 
 public class ResourceIdentifierUtil {
+
+  private ResourceIdentifierUtil() {}
+
   public static List<ResourceIdentifier> getResourcesFromJson(String resourceJson) {
     Gson gson = new Gson();
     List<ResourceIdentifier> resources = newArrayList();
+
+    @SuppressWarnings("unchecked")
     List<Map<String, String>> jsonList = gson.fromJson(resourceJson, List.class);
     for (Map<String, String> jsonResource : jsonList) {
       resources.add(
