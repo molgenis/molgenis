d0bc214e350f3d1f09fe74ef068145f73987b420
Dennis Hendriksen <d.hendriksen@umcg.nl>
2018-11-26 13:19:16 +0100

    Fix #7621 Plugin delete doesn't remove remove menu entries

--- molgenis-data-plugin/src/main/java/org/molgenis/data/plugin/model/PluginRepositoryDecoratorFactory.java
+++ molgenis-data-plugin/src/main/java/org/molgenis/data/plugin/model/PluginRepositoryDecoratorFactory.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import org.molgenis.data.AbstractSystemRepositoryDecoratorFactory;
 import org.molgenis.data.Repository;
+import org.molgenis.settings.AppSettings;
 import org.springframework.security.acls.model.MutableAclService;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +12,18 @@ import org.springframework.stereotype.Component;
 public class PluginRepositoryDecoratorFactory
     extends AbstractSystemRepositoryDecoratorFactory<Plugin, PluginMetadata> {
   private final MutableAclService mutableAclService;
+  private final AppSettings appSettings;
 
   public PluginRepositoryDecoratorFactory(
-      PluginMetadata pluginMetadata, MutableAclService mutableAclService) {
+      PluginMetadata pluginMetadata, MutableAclService mutableAclService, AppSettings appSettings) {
     super(pluginMetadata);
     this.mutableAclService = requireNonNull(mutableAclService);
+    this.appSettings = requireNonNull(appSettings);
   }
 
   @Override
   public Repository<Plugin> createDecoratedRepository(Repository<Plugin> repository) {
-    return new PluginSecurityRepositoryDecorator(repository, mutableAclService);
+    return new PluginSecurityRepositoryDecorator(
+        new PluginRepositoryDecorator(repository, appSettings), mutableAclService);
   }
 }
