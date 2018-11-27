2c2502383053ff76c651e34e68c2cc2bd38d7eac
Bart Charbon <b.charbon@umcg.nl>
2018-11-27 13:19:15 +0100

    Add tests for factory, entity and metadata for group and user

---
+++ molgenis-data-security/src/test/java/org/molgenis/data/security/auth/GroupTest.java
@@ -0,0 +1,38 @@
+package org.molgenis.data.security.auth;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.molgenis.data.config.EntityBaseTestConfig;
+import org.molgenis.data.meta.AbstractSystemEntityTest;
+import org.molgenis.data.security.config.GroupTestConfig;
+import org.molgenis.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(
+    classes = {
+        EntityBaseTestConfig.class,
+        GroupMetadata.class,
+        GroupFactory.class,
+        SecurityPackage.class
+        , GroupTestConfig.class})
+public class GroupTest extends AbstractSystemEntityTest {
+
+  @Autowired
+  GroupMetadata metadata;
+  @Autowired
+  GroupFactory factory;
+
+  private Map<String, Pair<Class, Object>> getOverriddenAttributes() {
+    //Add attributes with 'smart' getters and setters that covert back anf forth to correct values for MOLGENIS datatypes
+    //Provide the attribute name as key, and a pair of returntype (Class) and a Object to be used as test value
+    Map<String, Pair<Class, Object>> map = new HashMap<>();
+    return map;
+  }
+
+  @Test
+  public void testSystemEntity() {
+    internalTestAttributes(metadata, Group.class, factory, getOverriddenAttributes());
+  }
+}
---
+++ molgenis-data-security/src/test/java/org/molgenis/data/security/auth/UserTest.java
@@ -0,0 +1,37 @@
+package org.molgenis.data.security.auth;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.molgenis.data.config.EntityBaseTestConfig;
+import org.molgenis.data.meta.AbstractSystemEntityTest;
+import org.molgenis.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(
+    classes = {
+        EntityBaseTestConfig.class,
+        UserMetaData.class,
+        UserFactory.class,
+        SecurityPackage.class
+    })
+public class UserTest extends AbstractSystemEntityTest {
+
+  @Autowired
+  UserMetaData metadata;
+  @Autowired
+  UserFactory factory;
+
+  private Map<String, Pair<Class, Object>> getOverriddenAttributes() {
+    //Add attributes with 'smart' getters and setters that covert back anf forth to correct values for MOLGENIS datatypes
+    //Provide the attribute name as key, and a pair of returntype (Class) and a Object to be used as test value
+    Map<String, Pair<Class, Object>> map = new HashMap<>();
+    return map;
+  }
+
+  @Test
+  public void testSystemEntity() {
+    internalTestAttributes(metadata, User.class, factory, getOverriddenAttributes());
+  }
+}
