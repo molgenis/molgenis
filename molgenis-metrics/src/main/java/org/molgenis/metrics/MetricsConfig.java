4535b4a2283306dac189e00babf991845eb98c7a
tommydeboer <tommydeboer4@gmail.com>
2018-12-20 15:54:32 +0100

    Remove meter bindings

--- molgenis-metrics/src/main/java/org/molgenis/metrics/MetricsConfig.java
+++ molgenis-metrics/src/main/java/org/molgenis/metrics/MetricsConfig.java
@@ -3,30 +3,16 @@ package org.molgenis.metrics;
 import static io.micrometer.prometheus.PrometheusConfig.DEFAULT;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.MeterBinder;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
 
-  private final Collection<MeterBinder> binders;
-
-  @Autowired
-  public MetricsConfig(List<MeterBinder> binders) {
-    this.binders = (binders != null ? binders : Collections.emptyList());
-  }
-
   @Bean
   public MeterRegistry meterRegistry() {
-    MeterRegistry result = new PrometheusMeterRegistry(DEFAULT);
-    binders.forEach(binder -> binder.bindTo(result));
-    return result;
+    return new PrometheusMeterRegistry(DEFAULT);
   }
 
   @Bean
