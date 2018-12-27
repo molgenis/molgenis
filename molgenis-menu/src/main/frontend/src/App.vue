37ae1c427bee5ae106d32a51002a8b1399753137
connor <c.stroomberg@gmail.com>
2018-12-27 13:26:11 +0100

    Link data to component via window object
    This is a placeholder until we replace it with a api call

--- molgenis-menu/src/main/frontend/src/App.vue
+++ molgenis-menu/src/main/frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="molgenis-site-menu">
-    <NavBar></NavBar>
+    <NavBar :molgenis-menu="molgenisMenu"></NavBar>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 import NavBar from '../node_modules/molgenis-ui-context/src/components/NavBar.vue'
 
@@ -12,6 +12,11 @@ export default Vue.extend({
   name: 'molgenis-site-menu',
   components: {
     NavBar
+  },
+  data () {
+    return {
+      molgenisMenu: window.molgenisMenu
+    }
   }
 })
 
