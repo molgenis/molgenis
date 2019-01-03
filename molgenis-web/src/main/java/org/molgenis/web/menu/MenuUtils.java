b76b4302660331be1f9b70c4571563a947b9ebff
Fleur Kelpin <f.kelpin@umcg.nl>
2019-01-03 14:29:54 +0100

    doc(web): Extract constants for the magic strings.

--- molgenis-web/src/main/java/org/molgenis/web/menu/MenuUtils.java
+++ molgenis-web/src/main/java/org/molgenis/web/menu/MenuUtils.java
@@ -3,11 +3,18 @@ package org.molgenis.web.menu;
 import java.util.Scanner;
 
 public class MenuUtils {
+
+  /* The start of string delimiter pattern */
+  private static final String START_OF_STRING = "\\A";
+  /* The name of the default menu resource on the class path. */
+  private static final String DEFAULT_MENU_RESOURCE_NAME = "/molgenis_ui.json";
+
   private MenuUtils() {}
 
+  /** Reads the default menu contents from the classpath. */
   public static String readDefaultMenuValueFromClasspath() {
-    return new Scanner(MenuUtils.class.getResourceAsStream("/molgenis_ui.json"))
-        .useDelimiter("\\A")
+    return new Scanner(MenuUtils.class.getResourceAsStream(DEFAULT_MENU_RESOURCE_NAME))
+        .useDelimiter(START_OF_STRING)
         .next();
   }
 }
