33ae730d453860e593dc25db79f3b674b0017388
tommydeboer <tommydeboer4@gmail.com>
2019-01-10 09:26:31 +0100

    Undo test removal + remove new line

--- molgenis-data-security/src/test/java/org/molgenis/data/security/auth/TokenTest.java
+++ molgenis-data-security/src/test/java/org/molgenis/data/security/auth/TokenTest.java
@@ -1,9 +1,16 @@
 package org.molgenis.data.security.auth;
 
+import static java.time.Instant.now;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
 import org.molgenis.data.meta.AbstractSystemEntityTest;
 import org.molgenis.data.security.config.SecurityTestConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(classes = {SecurityTestConfig.class})
@@ -16,5 +23,29 @@ public class TokenTest extends AbstractSystemEntityTest {
   public void testSystemEntity() {
     internalTestAttributes(
         metadata, Token.class, factory, getOverriddenReturnTypes(), getExcludedAttrs());
+  }
+
+  private Token token;
+
+  @BeforeMethod
+  public void setup() {
+    token = factory.create();
+  }
+
+  @Test
+  public void testIsExpiredNoExpirationDate() {
+    assertFalse(token.isExpired());
+  }
+
+  @Test
+  public void testIsExpiredExpirationDateInFuture() {
+    token.setExpirationDate(now().plus(1, MINUTES));
+    assertFalse(token.isExpired());
+  }
+
+  @Test
+  public void testIsExpiredExpirationDateInPast() {
+    token.setExpirationDate(now().minus(1, SECONDS));
+    assertTrue(token.isExpired());
   }
 }
--- molgenis-security/src/main/java/org/molgenis/security/twofactor/model/UserSecretFactory.java
+++ molgenis-security/src/main/java/org/molgenis/security/twofactor/model/UserSecretFactory.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserSecretFactory
     extends AbstractSystemEntityFactory<UserSecret, UserSecretMetadata, String> {
-
   UserSecretFactory(UserSecretMetadata userSecretMetaData, EntityPopulator entityPopulator) {
     super(UserSecret.class, userSecretMetaData, entityPopulator);
   }
