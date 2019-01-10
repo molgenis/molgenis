358b999c7bae51f8b28905c46d8b752632b3228f
Bart Charbon <b.charbon@umcg.nl>
2019-01-10 16:43:12 +0100

    Add StyleSheet metadata tests

---
+++ molgenis-core-ui/src/test/java/org/molgenis/core/ui/style/StyleSheetTest.java
@@ -0,0 +1,29 @@
+package org.molgenis.core.ui.style;
+
+import org.molgenis.data.config.EntityBaseTestConfig;
+import org.molgenis.data.file.model.FileMetaMetaData;
+import org.molgenis.data.meta.AbstractSystemEntityTest;
+import org.molgenis.settings.SettingsPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(
+    classes = {
+      EntityBaseTestConfig.class,
+      StyleSheetMetadata.class,
+      StyleSheetFactory.class,
+      FileMetaMetaData.class,
+      SettingsPackage.class
+    })
+public class StyleSheetTest extends AbstractSystemEntityTest {
+
+  @Autowired StyleSheetMetadata metadata;
+  @Autowired StyleSheetFactory factory;
+
+  @Test
+  public void testSystemEntity() {
+    internalTestAttributes(
+        metadata, StyleSheet.class, factory, getOverriddenReturnTypes(), getExcludedAttrs());
+  }
+}
