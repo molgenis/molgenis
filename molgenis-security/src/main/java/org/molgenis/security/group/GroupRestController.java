b2e3564b2bb79a6618e5054c156cc06265dc6b7b
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-01-22 07:08:01 +0100

    Suppress squid:S1075: URIs should not be hardcoded

--- molgenis-security/src/main/java/org/molgenis/security/group/GroupRestController.java
+++ molgenis-security/src/main/java/org/molgenis/security/group/GroupRestController.java
@@ -59,7 +59,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class GroupRestController {
   public static final String USER = "/user";
 
+  @SuppressWarnings("squid:S1075") // URIs should not be hardcoded
   private static final String SECURITY_API_PATH = "/api/plugin/security";
+
   static final String GROUP_END_POINT = SECURITY_API_PATH + "/group";
   private static final String GROUP_MEMBER_END_POINT = GROUP_END_POINT + "/{groupName}/member";
   private static final String GROUP_PERMISSION_END_POINT =
@@ -141,9 +143,7 @@ public class GroupRestController {
   })
   @ResponseBody
   public List<GroupResponse> getGroups() {
-    return groupService
-        .getGroups()
-        .stream()
+    return groupService.getGroups().stream()
         .filter(group -> userPermissionEvaluator.hasPermission(new GroupIdentity(group), VIEW))
         .map(GroupResponse::fromEntity)
         .collect(Collectors.toList());
@@ -156,9 +156,7 @@ public class GroupRestController {
       @PathVariable(value = "groupName") String groupName) {
     checkGroupPermission(groupName, VIEW_MEMBERSHIP);
     Iterable<Role> roles = groupService.getGroup(groupName).getRoles();
-    return roleMembershipService
-        .getMemberships(Lists.newArrayList(roles))
-        .stream()
+    return roleMembershipService.getMemberships(Lists.newArrayList(roles)).stream()
         .map(GroupMemberResponse::fromEntity)
         .collect(Collectors.toList());
   }
@@ -246,9 +244,7 @@ public class GroupRestController {
   @ResponseBody
   @PreAuthorize("hasAnyRole('SU', 'MANAGER')")
   public Collection<UserResponse> getUsers() {
-    return userService
-        .getUsers()
-        .stream()
+    return userService.getUsers().stream()
         .filter(u -> !u.getUsername().equals("anonymous"))
         .map(UserResponse::fromEntity)
         .collect(Collectors.toList());
@@ -270,9 +266,7 @@ public class GroupRestController {
   }
 
   private String getManagerRoleName(GroupValue groupValue) {
-    return groupValue
-        .getRoles()
-        .stream()
+    return groupValue.getRoles().stream()
         .filter(role -> role.getLabel().equals(MANAGER))
         .map(RoleValue::getName)
         .findFirst()
