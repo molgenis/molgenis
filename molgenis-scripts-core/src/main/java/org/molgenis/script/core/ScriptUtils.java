83d0fc95f3d2efad653d54b4e89364c4cca78aa7
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-01-29 16:01:17 +0100

    Fix #2898 Script without results extension 400 error (#8198)

--- molgenis-scripts-core/src/main/java/org/molgenis/script/core/ScriptUtils.java
+++ molgenis-scripts-core/src/main/java/org/molgenis/script/core/ScriptUtils.java
@@ -1,14 +1,22 @@
 package org.molgenis.script.core;
 
+import static java.util.Collections.emptyMap;
+
+import freemarker.core.InvalidReferenceException;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import freemarker.template.Version;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ScriptUtils {
+  private static final Version VERSION = Configuration.VERSION_2_3_21;
+
   private ScriptUtils() {}
 
   /** Render a script using the given parameter values */
@@ -16,15 +24,40 @@ public class ScriptUtils {
     StringWriter stringWriter = new StringWriter();
     try {
       Template template =
-          new Template(
-              null,
-              new StringReader(script.getContent()),
-              new Configuration(Configuration.VERSION_2_3_21));
+          new Template(null, new StringReader(script.getContent()), new Configuration(VERSION));
       template.process(parameterValues, stringWriter);
     } catch (TemplateException | IOException e) {
       throw new GenerateScriptException(
           "Error processing parameters for script [" + script.getName() + "]. " + e.getMessage());
     }
     return stringWriter.toString();
+  }
+
+  public static boolean hasScriptVariable(Script script, String variable) {
+    return getScriptVariables(script).contains(variable);
+  }
+
+  /** @return script variables */
+  public static Set<String> getScriptVariables(Script script) {
+    // based on https://stackoverflow.com/a/48024379
+    Set<String> scriptExpressions = new LinkedHashSet<>();
+    Configuration configuration = new Configuration(VERSION);
+    configuration.setTemplateExceptionHandler(
+        (te, env, out) -> {
+          if (te instanceof InvalidReferenceException) {
+            scriptExpressions.add(te.getBlamedExpressionString());
+            return;
+          }
+          throw te;
+        });
+
+    try {
+      Template template = new Template(null, new StringReader(script.getContent()), configuration);
+      template.process(emptyMap(), new StringWriter());
+    } catch (TemplateException | IOException e) {
+      throw new GenerateScriptException(
+          "Error processing parameters for script [" + script.getName() + "]. " + e.getMessage());
+    }
+    return scriptExpressions;
   }
 }
