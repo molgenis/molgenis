8c3646720027024c5e0efb854672f1f2f030b807
Fleur Kelpin <f.kelpin@umcg.nl>
2019-01-31 20:18:20 +0100

    chore: Move core-ui frontend code to molgenis-frontend

--- molgenis-core-ui/src/main/frontend/src/modules/rest-client/index.js
+++
@@ -1,7 +0,0 @@
-import RestClientV1 from "./RestClientV1";
-import RestClientV2 from "./RestClientV2";
-import rsql from "./rsql"
-
-const packageSeparator = '_';
-
-export { RestClientV1, RestClientV2, rsql, packageSeparator }
