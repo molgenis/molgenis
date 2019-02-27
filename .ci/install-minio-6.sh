21ae18332ceb54e0bd79fbad3e21f696f96c5f82
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-02-27 12:31:43 +0100

    Fix environment vars

--- .ci/install-minio-6.sh
+++ .ci/install-minio-6.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 sudo chmod +x minio
-sudo export MINIO_ACCESS_KEY=molgenis
-sudo export MINIO_SECRET_KEY=molgenis
+sudo env MINIO_ACCESS_KEY=molgenis
+sudo env MINIO_SECRET_KEY=molgenis
 sudo ./minio server /data
