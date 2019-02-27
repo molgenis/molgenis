ef3103499500666533a7120ce15ba28a38ff2cd4
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-02-27 13:00:38 +0100

    Start Minio in the background

--- .ci/install-minio-6.sh
+++ .ci/install-minio-6.sh
@@ -3,4 +3,4 @@ sudo wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 sudo chmod +x minio
 export MINIO_ACCESS_KEY=molgenis
 export MINIO_SECRET_KEY=molgenis
-sudo ./minio server /data
+sudo ./minio server /data &
