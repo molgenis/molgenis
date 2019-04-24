294a8b6b97669a0a88720e065cf061c47565eebd
connor <c.stroomberg@gmail.com>
2019-04-24 09:44:20 +0200

    use basic typed exception instead of coded runtime

--- molgenis-security/src/main/java/org/molgenis/security/exception/WebAppSecurityConfigException.java
+++ molgenis-security/src/main/java/org/molgenis/security/exception/WebAppSecurityConfigException.java
@@ -1,21 +1,8 @@
 package org.molgenis.security.exception;
 
-import static java.util.Objects.requireNonNull;
-
-import org.molgenis.i18n.CodedRuntimeException;
-
-public class WebAppSecurityConfigException extends CodedRuntimeException {
-
-  private static final String ERROR_CODE = "SEC05";
-  private final Throwable cause;
+public class WebAppSecurityConfigException extends RuntimeException {
 
   public WebAppSecurityConfigException(Throwable cause) {
-    super(ERROR_CODE, cause);
-    this.cause = requireNonNull(cause);
-  }
-
-  @Override
-  protected Object[] getLocalizedMessageArguments() {
-    return new Object[] {cause.getLocalizedMessage()};
+    super(cause);
   }
 }
