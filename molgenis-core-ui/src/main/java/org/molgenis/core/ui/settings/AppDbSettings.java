dde675b6992b6306b31cdd3b5e9341c760d40e8d
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-05-06 15:23:58 +0200

    Fix #8419 Recaptcha settings should have nullable expressions

--- molgenis-core-ui/src/main/java/org/molgenis/core/ui/settings/AppDbSettings.java
+++ molgenis-core-ui/src/main/java/org/molgenis/core/ui/settings/AppDbSettings.java
@@ -201,14 +201,14 @@ public class AppDbSettings extends DefaultSettingsEntity implements AppSettings 
       addAttribute(RECAPTCHA_PRIVATE_KEY)
           .setParent(recaptchaAttr)
           .setDataType(STRING)
-          .setNillable(true)
+          .setNullableExpression("$('" + RECAPTCHA_IS_ENABLED + "').eq(false).value()")
           .setLabel("Recaptcha secret")
           .setDescription(
               "The secret needed by the server to authenticate with the google servers");
       addAttribute(RECAPTCHA_PUBLIC_KEY)
           .setParent(recaptchaAttr)
           .setDataType(STRING)
-          .setNillable(true)
+          .setNullableExpression("$('" + RECAPTCHA_IS_ENABLED + "').eq(false).value()")
           .setLabel("Recaptcha site key")
           .setDescription(
               "The site key needed by the clients to authenticate with the google servers");
