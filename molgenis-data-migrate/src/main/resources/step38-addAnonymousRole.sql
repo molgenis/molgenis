a2f0ea8a8ce94d7da4bcbdfef58a9249dbb764f0
Bart Charbon <b.charbon@umcg.nl>
2019-05-17 10:53:21 +0200

    trigger build

--- molgenis-data-migrate/src/main/resources/step38-addAnonymousRole.sql
+++ molgenis-data-migrate/src/main/resources/step38-addAnonymousRole.sql
@@ -1,4 +1,5 @@
 INSERT INTO public."sys_sec_Role#b6639604" (name, label, "labelEn", "labelNl", id) VALUES ( 'ANONYMOUS', 'Anonymous','Anonymous', 'Anoniem','Anonymous');
+
 INSERT INTO public."sys_sec_Role#b6639604_includes" (id, includes, "order")(SELECT "role".id,(SELECT id from public."sys_sec_Role#b6639604" where name = 'ANONYMOUS'), COALESCE(MAX("order")+1,0) from public."sys_sec_Role#b6639604" role
 LEFT JOIN public."sys_sec_Role#b6639604_includes" ON "role".id = public."sys_sec_Role#b6639604_includes".id
 where name = 'USER' GROUP BY "role".id);
