7f10075e4a46b7f4ad333bccbde7aa5ba0a4fe2b
Bart Charbon <b.charbon@umcg.nl>
2019-06-24 08:05:14 +0200

    sortconverter

---
+++ molgenis-api-data/src/main/java/org/molgenis/api/data/v3/SortConverter.java
@@ -0,0 +1,28 @@
+package org.molgenis.api.data.v3;
+
+import org.molgenis.data.Sort;
+import org.springframework.core.convert.converter.Converter;
+
+public class SortConverter implements Converter<String, Sort> {
+  private static final String ORDER_ASC_PREFIX = "+";
+  private static final String ORDER_DESC_PREFIX = "-";
+
+  @Override
+  public Sort convert(String source) {
+    Sort sort = new Sort();
+    for (String attr : source.split(",")) {
+      Sort.Direction direction;
+      if (attr.startsWith(ORDER_DESC_PREFIX)) {
+        direction = Sort.Direction.DESC;
+        attr = attr.substring(ORDER_DESC_PREFIX.length());
+      } else if (attr.startsWith(ORDER_ASC_PREFIX)) {
+        direction = Sort.Direction.ASC;
+        attr = attr.substring(ORDER_ASC_PREFIX.length());
+      } else {
+        direction = Sort.Direction.ASC;
+      }
+      sort.on(attr, direction);
+    }
+    return sort;
+  }
+}
