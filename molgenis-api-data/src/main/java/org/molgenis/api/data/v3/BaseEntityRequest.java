d4bfc251d5c782708ce4a39fecafe9a2f94e96db
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-06-27 10:15:55 +0200

    Selection parser, converter and v3 application

--- molgenis-api-data/src/main/java/org/molgenis/api/data/v3/BaseEntityRequest.java
+++ molgenis-api-data/src/main/java/org/molgenis/api/data/v3/BaseEntityRequest.java
@@ -1,14 +1,11 @@
 package org.molgenis.api.data.v3;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import javax.validation.constraints.NotNull;
 
 public abstract class BaseEntityRequest {
   @NotNull private String entityTypeId;
-  private List<String> filter;
-  private List<String> expand;
+  private Selection filter;
+  private Selection expand;
 
   public String getEntityTypeId() {
     return entityTypeId;
@@ -19,34 +16,18 @@ public abstract class BaseEntityRequest {
   }
 
   public Selection getFilter() {
-    // TODO move to convertor / remove code duplication
-    Map<String, Selection> filterMap;
-    if (filter != null && !filter.isEmpty()) {
-      filterMap = new HashMap<>();
-      filter.forEach(key -> filterMap.put(key, Selection.FULL_SELECTION));
-    } else {
-      filterMap = null;
-    }
-    return filterMap != null ? new Selection(filterMap) : Selection.FULL_SELECTION;
+    return filter != null ? filter : Selection.FULL_SELECTION;
   }
 
-  public void setFilter(List<String> filter) {
+  public void setFilter(Selection filter) {
     this.filter = filter;
   }
 
   public Selection getExpand() {
-    // TODO move to convertor / remove code duplication
-    Map<String, Selection> expandMap;
-    if (expand != null && !expand.isEmpty()) {
-      expandMap = new HashMap<>();
-      expand.forEach(key -> expandMap.put(key, Selection.EMPTY_SELECTION));
-    } else {
-      expandMap = null;
-    }
-    return expandMap != null ? new Selection(expandMap) : Selection.EMPTY_SELECTION;
+    return expand != null ? expand : Selection.EMPTY_SELECTION;
   }
 
-  public void setExpand(List<String> expand) {
+  public void setExpand(Selection expand) {
     this.expand = expand;
   }
 }
---
+++ molgenis-api-data/src/main/java/org/molgenis/api/data/v3/DataApiV3Config.java
@@ -0,0 +1,19 @@
+package org.molgenis.api.data.v3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DataApiV3Config implements WebMvcConfigurer {
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(selectionConverter());
+  }
+
+  @Bean
+  public SelectionConverter selectionConverter() {
+    return new SelectionConverter();
+  }
+}
