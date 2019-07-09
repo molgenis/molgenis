38ad472ba8e5edae4e7f189f43dadf733495dd0a
sidohaakma <sido@haakma.org>
2019-07-09 14:40:35 +0200

    fix(frontend): added /ui to path

--- molgenis-core-ui/src/main/java/org/molgenis/core/ui/MolgenisWebAppConfig.java
+++ molgenis-core-ui/src/main/java/org/molgenis/core/ui/MolgenisWebAppConfig.java
@@ -134,7 +134,7 @@ public abstract class MolgenisWebAppConfig implements WebMvcConfigurer {
         .setCachePeriod(cachePeriod);
     registry
         .addResourceHandler(PATTERN_MOLGENIS_UI)
-        .addResourceLocations("/ui/", "classpath:/@molgenis-ui/")
+        .addResourceLocations("/ui/@molgenis-ui/", "classpath:/ui/@molgenis-ui/")
         .setCachePeriod(3600);
     registry
         .addResourceHandler(PATTERN_FONTS)
