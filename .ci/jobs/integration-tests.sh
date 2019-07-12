babd06f88f0b0049d3eae003411cc97816e9c477
Bart Charbon <b.charbon@umcg.nl>
2019-07-12 16:00:46 +0200

    remove quit and redirect test output

--- .ci/jobs/integration-tests.sh
+++ .ci/jobs/integration-tests.sh
@@ -3,8 +3,7 @@
 travis_wait
 
 # run the integration tests
-./mvnw verify -pl molgenis-platform-integration-tests --batch-mode --quiet \
-  -Dmaven.test.redirectTestOutputToFile=true \
+./mvnw verify -pl molgenis-platform-integration-tests --batch-mode \
   -Dit_db_user=postgres \
   -Dit_db_password=molgenis \
   -Dit_db_port=5433
