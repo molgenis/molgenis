e4c5000a4512045301aae1bdfab160144eba04ab
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-07-12 07:43:07 +0200

    Fix tests

--- molgenis-api/src/test/java/org/molgenis/api/convert/SortConverterTest.java
+++ molgenis-api/src/test/java/org/molgenis/api/convert/SortConverterTest.java
@@ -1,58 +1,25 @@
 package org.molgenis.api.convert;
 
-import static java.util.Arrays.asList;
-import static org.testng.Assert.assertEquals;
-
 import org.molgenis.api.model.Sort;
-import org.molgenis.api.model.Sort.Order;
-import org.molgenis.api.model.Sort.Order.Direction;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SortConverterTest {
-  @Test
-  public void convertSingleAttrDefault() {
-    Assert.assertEquals(new SortConverter().convert("item"), Sort.create("item"));
+  private SortConverter sortConverter;
+
+  @BeforeMethod
+  public void setUpBeforeMethod() {
+    sortConverter = new SortConverter();
   }
 
   @Test
-  public void convertSingleAttrAsc() {
-    assertEquals(new SortConverter().convert("+item"), Sort.create("item", Direction.ASC));
+  public void testConvert() {
+    Assert.assertEquals(sortConverter.convert("item"), Sort.create("item"));
   }
 
-  @Test
-  public void convertSingleAttrDesc() {
-    assertEquals(new SortConverter().convert("-item"), Sort.create("item", Direction.DESC));
-  }
-
-  @Test
-  public void convertMultiAttrDefault() {
-    assertEquals(
-        new SortConverter().convert("item0,item1"),
-        Sort.create(asList(Order.create("item0"), Order.create("item1"))));
-  }
-
-  @Test
-  public void convertMultiAttrAsc() {
-    assertEquals(
-        new SortConverter().convert("+item0,+item1"),
-        Sort.create(
-            asList(Order.create("item0", Direction.ASC), Order.create("item1", Direction.ASC))));
-  }
-
-  @Test
-  public void convertMultiAttrDesc() {
-    assertEquals(
-        new SortConverter().convert("-item0,-item1"),
-        Sort.create(
-            asList(Order.create("item0", Direction.DESC), Order.create("item1", Direction.DESC))));
-  }
-
-  @Test
-  public void convertMultiAttrAscAndDesc() {
-    assertEquals(
-        new SortConverter().convert("+item0,-item1"),
-        Sort.create(
-            asList(Order.create("item0", Direction.ASC), Order.create("item1", Direction.DESC))));
+  @Test(expectedExceptions = RuntimeException.class)
+  public void testConvertParseException() {
+    sortConverter.convert("+");
   }
 }
