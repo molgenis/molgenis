78fbd3e772e001fb5e6506ee68b870287ef072a6
Bart Charbon <b.charbon@umcg.nl>
2019-07-15 11:27:55 +0200

    fix travis file

--- .ci/jobs/integration-tests.sh
+++ .ci/jobs/integration-tests.sh
@@ -3,7 +3,7 @@
 travis_wait
 
 # run the integration tests
-./mvnw test -pl molgenis-platform-integration-tests --batch-mode \
+./mvnw verify -pl molgenis-platform-integration-tests --batch-mode \
   -Dit_db_user=postgres \
   -Dit_db_password=molgenis \
   -Dit_db_port=5433
