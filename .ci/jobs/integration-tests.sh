dcb34b78cd10991110c30a6e1514f5874e0a4b44
Bart Charbon <b.charbon@umcg.nl>
2019-07-15 09:21:54 +0200

    only run copyIT to see if that works on travis

--- .ci/jobs/integration-tests.sh
+++ .ci/jobs/integration-tests.sh
@@ -3,7 +3,8 @@
 travis_wait
 
 # run the integration tests
-./mvnw verify -pl molgenis-platform-integration-tests --batch-mode \
+./mvnw test -pl molgenis-platform-integration-tests --batch-mode \
   -Dit_db_user=postgres \
   -Dit_db_password=molgenis \
-  -Dit_db_port=5433
+  -Dit_db_port=5433 \
+  -Dtest=CopyServiceIT
