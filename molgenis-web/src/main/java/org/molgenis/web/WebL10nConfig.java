b849c97758184f762ee50639c16fa8edf73b0166
Bart Charbon <b.charbon@umcg.nl>
2019-07-16 14:09:01 +0200

    move coded exception code to molgenis-util module so it can be used in molgenis-api (part II)

--- molgenis-web/src/main/java/org/molgenis/web/WebL10nConfig.java
+++ molgenis-web/src/main/java/org/molgenis/web/WebL10nConfig.java
@@ -1,6 +1,6 @@
 package org.molgenis.web;
 
-import org.molgenis.i18n.PropertiesMessageSource;
+import org.molgenis.util.i18n.PropertiesMessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
