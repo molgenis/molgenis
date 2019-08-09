b597e0db131ac5712474e7c729968f1440ffe348
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-08-09 13:34:38 +0200

    Fix Collections import Test class extends AbstractMockitoTest

--- molgenis-api-data/src/test/java/org/molgenis/api/data/v3/FetchMapperTest.java
+++ molgenis-api-data/src/test/java/org/molgenis/api/data/v3/FetchMapperTest.java
@@ -1,9 +1,9 @@
 package org.molgenis.api.data.v3;
 
-import static freemarker.template.utility.Collections12.singletonList;
+import static java.util.Collections.singletonList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,26 +12,27 @@ import org.molgenis.data.Fetch;
 import org.molgenis.data.meta.AttributeType;
 import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.data.meta.model.EntityType;
+import org.molgenis.test.AbstractMockitoTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FetchMapperTest {
+public class FetchMapperTest extends AbstractMockitoTest {
+  private FetchMapper fetchMapper;
+
+  @BeforeMethod
+  public void setUpBeforeMethod() {
+    fetchMapper = new FetchMapper();
+  }
 
   @Test
   public void testToFetch() {
-    FetchMapper fetchMapper = new FetchMapper();
-
     EntityType entityType = mock(EntityType.class);
     Attribute attribute = mock(Attribute.class);
     when(attribute.getName()).thenReturn("test");
     Attribute attributeXref = mock(Attribute.class);
     when(attributeXref.getName()).thenReturn("xref");
     when(attributeXref.getDataType()).thenReturn(AttributeType.XREF);
-    Attribute refAttribute = mock(Attribute.class);
-    when(refAttribute.getName()).thenReturn("refId");
-    EntityType refEntityType = mock(EntityType.class);
-    when(refAttribute.getRefEntity()).thenReturn(refEntityType);
     when(entityType.getAtomicAttributes()).thenReturn(Arrays.asList(attribute, attributeXref));
-    when(refEntityType.getAtomicAttributes()).thenReturn(singletonList(refAttribute));
 
     Selection expand = new Selection(Collections.singletonMap("xref", Selection.FULL_SELECTION));
     Selection filter = Selection.FULL_SELECTION;
@@ -41,11 +42,10 @@ public class FetchMapperTest {
 
   @Test
   public void testToFetchEmpty() {
-    FetchMapper fetchMapper = new FetchMapper();
     EntityType entityType = mock(EntityType.class);
     Attribute attribute = mock(Attribute.class);
     when(attribute.getName()).thenReturn("test");
-    when(entityType.getAtomicAttributes()).thenReturn(Collections.singletonList(attribute));
+    when(entityType.getAtomicAttributes()).thenReturn(singletonList(attribute));
     Selection expand = Selection.EMPTY_SELECTION;
     Selection filter = Selection.FULL_SELECTION;
     Fetch expected = new Fetch().field("test");
