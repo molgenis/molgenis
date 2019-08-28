e7566614dcda0de36060600c494c2e8745f723bf
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-08-28 09:29:48 +0200

    Fix code smells (#8634)

--- molgenis-api-data/src/main/java/org/molgenis/api/data/v3/UnsupportedAttributeTypeException.java
+++ molgenis-api-data/src/main/java/org/molgenis/api/data/v3/UnsupportedAttributeTypeException.java
@@ -1,20 +1,29 @@
 package org.molgenis.api.data.v3;
 
+import org.molgenis.data.meta.AttributeType;
 import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.util.exception.BadRequestException;
 
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class UnsupportedAttributeTypeException extends BadRequestException {
-
   private static final String ERROR_CODE = "DAPI01";
-  private Attribute attribute;
 
-  protected UnsupportedAttributeTypeException(Attribute attribute) {
+  private final String attributeName;
+  private final AttributeType attributeType;
+
+  UnsupportedAttributeTypeException(Attribute attribute) {
     super(ERROR_CODE);
-    this.attribute = attribute;
+    this.attributeName = attribute.getName();
+    this.attributeType = attribute.getDataType();
+  }
+
+  @Override
+  public String getMessage() {
+    return String.format("name:%s type:%s", attributeName, attributeType.toString());
   }
 
   @Override
   protected Object[] getLocalizedMessageArguments() {
-    return new Object[] {attribute.getName(), attribute.getDataType()};
+    return new Object[] {attributeName, attributeType};
   }
 }
