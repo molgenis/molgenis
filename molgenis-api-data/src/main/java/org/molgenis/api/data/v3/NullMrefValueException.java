03a325cf555096b4a3e51ebff5c2ef7177af31ac
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-08-28 07:28:08 +0200

    Fix code smells

--- molgenis-api-data/src/main/java/org/molgenis/api/data/v3/NullMrefValueException.java
+++ molgenis-api-data/src/main/java/org/molgenis/api/data/v3/NullMrefValueException.java
@@ -1,27 +1,25 @@
 package org.molgenis.api.data.v3;
 
-import static java.util.Objects.requireNonNull;
-
 import org.molgenis.data.meta.model.Attribute;
 import org.molgenis.util.exception.CodedRuntimeException;
 
 public class NullMrefValueException extends CodedRuntimeException {
-
   private static final String ERROR_CODE = "DAPI02";
-  private final Attribute attribute;
 
-  protected NullMrefValueException(Attribute attribute) {
+  private final String attributeName;
+
+  NullMrefValueException(Attribute attribute) {
     super(ERROR_CODE);
-    this.attribute = requireNonNull(attribute);
+    this.attributeName = attribute.getName();
   }
 
   @Override
   public String getMessage() {
-    return String.format("attribute:%s", attribute.getName());
+    return String.format("attribute:%s", attributeName);
   }
 
   @Override
   protected Object[] getLocalizedMessageArguments() {
-    return new Object[] {attribute.getName()};
+    return new Object[] {attributeName};
   }
 }
