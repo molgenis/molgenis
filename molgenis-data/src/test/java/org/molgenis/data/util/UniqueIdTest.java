acacfd653240ef07f53331be18602f385e5ff0ca
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-08-30 11:09:23 +0200

    Update UniqueId documentation and add unit test

--- molgenis-data/src/test/java/org/molgenis/data/util/UniqueIdTest.java
+++ molgenis-data/src/test/java/org/molgenis/data/util/UniqueIdTest.java
@@ -7,13 +7,6 @@ import org.testng.annotations.Test;
 public class UniqueIdTest {
   @Test
   public void testGetId() {
-    byte[] id0 = new UniqueId().getId();
-    byte[] id1 = new UniqueId().getId();
-    assertNotEquals(id0, id1);
-  }
-
-  @Test
-  public void testGetIdReuse() {
     UniqueId uniqueId = new UniqueId();
     assertNotEquals(uniqueId.getId(), uniqueId.getId());
   }
