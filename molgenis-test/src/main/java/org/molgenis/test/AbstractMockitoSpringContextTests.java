2673eeb0f26af926234e27510b0487adb1ecf133
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-09-23 14:53:52 +0200

    Fix abstract test class

--- molgenis-test/src/main/java/org/molgenis/test/AbstractMockitoSpringContextTests.java
+++ molgenis-test/src/main/java/org/molgenis/test/AbstractMockitoSpringContextTests.java
@@ -1,36 +1,7 @@
 package org.molgenis.test;
 
-import static java.util.Objects.requireNonNull;
-import static org.mockito.Mockito.mockitoSession;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoSession;
-import org.mockito.quality.Strictness;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class AbstractMockitoSpringContextTests extends AbstractMockitoTest {
-  private final Strictness strictness;
-
-  private MockitoSession mockitoSession;
-
-  public AbstractMockitoSpringContextTests() {
-    this(Strictness.STRICT_STUBS);
-  }
-
-  public AbstractMockitoSpringContextTests(Strictness strictness) {
-    this.strictness = requireNonNull(strictness);
-  }
-
-  @BeforeEach
-  public void initMocks() {
-    mockitoSession = mockitoSession().initMocks(this).strictness(strictness).startMocking();
-  }
-
-  @AfterEach
-  public void tearDownAfterMethod() {
-    mockitoSession.finishMocking();
-  }
-}
+public class AbstractMockitoSpringContextTests extends AbstractMockitoTest {}
