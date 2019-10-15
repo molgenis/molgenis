4360056fe2c5e1dd0664783733befba36cad30ad
Bart Charbon <b.charbon@umcg.nl>
2019-10-15 14:05:26 +0200

    revert number 'fix' + centralize page object creation

--- molgenis-api/src/main/java/org/molgenis/api/PageUtils.java
+++ molgenis-api/src/main/java/org/molgenis/api/PageUtils.java
@@ -7,13 +7,7 @@ public class PageUtils {
   private PageUtils() {}
 
   public static PageResponse getPageResponse(int size, int offset, int total, int pageSize) {
-    PageResponse pageResponse = null;
-    pageResponse =
-        PageResponse.create(
-            size,
-            total,
-            total > 0 ? (int) Math.ceil(total / (double) pageSize) : 0,
-            total > 0 ? (offset / pageSize) + 1 : 0);
-    return pageResponse;
+    return PageResponse.create(
+        size, total, total > 0 ? (int) Math.ceil(total / (double) pageSize) : 0, offset / pageSize);
   }
 }
