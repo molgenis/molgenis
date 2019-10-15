e147ffa5d7a53c2dd2ef5c04b277958b1e099bb2
tommydeboer <tommydeboer4@gmail.com>
2019-10-15 14:19:27 +0200

    Remove unused code in molgenis-util

--- molgenis-util/src/main/java/org/molgenis/util/stream/MultimapCollectors.java
+++ molgenis-util/src/main/java/org/molgenis/util/stream/MultimapCollectors.java
@@ -1,9 +1,7 @@
 package org.molgenis.util.stream;
 
 import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
-import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
@@ -33,16 +31,5 @@ public class MultimapCollectors {
           map1.putAll(map2);
           return map1;
         });
-  }
-
-  public static <K, V, A extends Multimap<K, V>> Collector<Entry<K, V>, A, A> toMultimap(
-      Supplier<A> supplier) {
-    return toMultimap(supplier, Entry::getKey, Entry::getValue);
-  }
-
-  public static <K, V>
-      Collector<Entry<K, V>, LinkedHashMultimap<K, V>, LinkedHashMultimap<K, V>>
-          toLinkedHashMultimap() {
-    return toMultimap(LinkedHashMultimap::<K, V>create);
   }
 }
