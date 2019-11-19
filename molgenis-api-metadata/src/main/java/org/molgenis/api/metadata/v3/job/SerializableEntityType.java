5437d2dee2b8a8676a3b43d54a4d5adf92ff6c99
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-11-19 07:10:21 +0100

    Remove unused create method with too many params Suppress squid:S1610: Abstract classes without fields should be converted to interfaces

--- molgenis-api-metadata/src/main/java/org/molgenis/api/metadata/v3/job/SerializableEntityType.java
+++ molgenis-api-metadata/src/main/java/org/molgenis/api/metadata/v3/job/SerializableEntityType.java
@@ -3,8 +3,6 @@ package org.molgenis.api.metadata.v3.job;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import java.util.List;
-import java.util.Map;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import org.molgenis.util.AutoGson;
@@ -42,39 +40,12 @@ abstract class SerializableEntityType {
 
   abstract int getIndexingDepth();
 
-  public static SerializableEntityType create(
-      String newId,
-      String newPackageId,
-      String newLabel,
-      Map<String, String> newLabelI18n,
-      String newDescription,
-      Map<String, String> newDescriptionI18n,
-      List<SerializableAttribute> newAttributes,
-      boolean newAbstract,
-      String newExtendsId,
-      List<String> newTagIds,
-      String newBackend,
-      int newIndexingDepth) {
-    return builder()
-        .setId(newId)
-        .setPackageId(newPackageId)
-        .setLabel(newLabel)
-        .setLabelI18n(ImmutableMap.copyOf(newLabelI18n))
-        .setDescription(newDescription)
-        .setDescriptionI18n(ImmutableMap.copyOf(newDescriptionI18n))
-        .setAttributes(ImmutableList.copyOf(newAttributes))
-        .setAbstract(newAbstract)
-        .setExtendsId(newExtendsId)
-        .setTagIds(ImmutableList.copyOf(newTagIds))
-        .setBackend(newBackend)
-        .setIndexingDepth(newIndexingDepth)
-        .build();
-  }
-
   public static Builder builder() {
     return new AutoValue_SerializableEntityType.Builder();
   }
 
+  @SuppressWarnings(
+      "squid:S1610") // Abstract classes without fields should be converted to interfaces
   @AutoValue.Builder
   public abstract static class Builder {
 
