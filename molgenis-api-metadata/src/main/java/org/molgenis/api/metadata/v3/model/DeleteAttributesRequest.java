fba34c0ded22a2fd82257ef188100af32e0c53fd
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-11-20 09:02:11 +0100

    Use @PathVariable where possible

--- molgenis-api-metadata/src/main/java/org/molgenis/api/metadata/v3/model/DeleteAttributesRequest.java
+++ molgenis-api-metadata/src/main/java/org/molgenis/api/metadata/v3/model/DeleteAttributesRequest.java
@@ -4,16 +4,7 @@ import javax.validation.constraints.NotNull;
 import org.molgenis.api.model.Query;
 
 public class DeleteAttributesRequest {
-  @NotNull private String entityTypeId;
   @NotNull private Query q;
-
-  public String getEntityTypeId() {
-    return entityTypeId;
-  }
-
-  public void setEntityTypeId(String entityTypeId) {
-    this.entityTypeId = entityTypeId;
-  }
 
   public Query getQ() {
     return q;
