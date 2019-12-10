d2f226235839c67fdd326fde749b3d2be40e4761
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-12-10 16:25:09 +0100

    Improve L1 cache usage

--- molgenis-data-cache/src/main/java/org/molgenis/data/cache/l1/L1CacheRepositoryDecorator.java
+++ molgenis-data-cache/src/main/java/org/molgenis/data/cache/l1/L1CacheRepositoryDecorator.java
@@ -33,18 +33,22 @@ public class L1CacheRepositoryDecorator extends AbstractRepositoryDecorator<Enti
   private static final int BATCH_SIZE = 1000;
 
   private final L1Cache l1Cache;
+  private final L1CacheJanitor l1CacheJanitor;
   private final boolean cacheable;
 
-  public L1CacheRepositoryDecorator(Repository<Entity> delegateRepository, L1Cache l1Cache) {
+  public L1CacheRepositoryDecorator(
+      Repository<Entity> delegateRepository, L1Cache l1Cache, L1CacheJanitor l1CacheJanitor) {
     super(delegateRepository);
     this.l1Cache = requireNonNull(l1Cache);
+    this.l1CacheJanitor = requireNonNull(l1CacheJanitor);
     this.cacheable = delegateRepository.getCapabilities().contains(CACHEABLE);
   }
 
   @Override
   public void add(Entity entity) {
     if (cacheable) {
-      upsertCache(entity);
+      l1CacheJanitor.cleanCacheBeforeAdd(entity);
+      putCache(entity);
     }
     delegate().add(entity);
   }
@@ -52,7 +56,8 @@ public class L1CacheRepositoryDecorator extends AbstractRepositoryDecorator<Enti
   @Override
   public Integer add(Stream<Entity> entities) {
     if (cacheable) {
-      entities = entities.filter(this::upsertCache);
+      entities =
+          l1CacheJanitor.cleanCacheBeforeAdd(getEntityType(), entities).filter(this::putCache);
     }
     return delegate().add(entities);
   }
@@ -67,6 +72,23 @@ public class L1CacheRepositoryDecorator extends AbstractRepositoryDecorator<Enti
     return cacheable ? findOneByIdWithCache(id, fetch) : delegate().findOneById(id, fetch);
   }
 
+  private Entity findOneByIdWithCache(Object id, @Nullable @CheckForNull Fetch fetch) {
+    EntityType entityType = getEntityType();
+    Optional<CacheHit<Entity>> cacheHit = l1Cache.get(entityType, id, fetch);
+    if (cacheHit.isPresent()) {
+      return cacheHit.get().getValue();
+    } else {
+      // TODO retrieve with fetch and cache with fetch (also see findAllWithCache)
+      Entity entity = delegate().findOneById(id);
+      if (entity != null) {
+        l1Cache.put(entity);
+      } else {
+        l1Cache.putDeletion(entityType, id);
+      }
+      return entity;
+    }
+  }
+
   @Override
   public Stream<Entity> findAll(Stream<Object> ids) {
     return cacheable ? findAllWithCache(ids, null) : delegate().findAll(ids);
@@ -75,87 +97,6 @@ public class L1CacheRepositoryDecorator extends AbstractRepositoryDecorator<Enti
   @Override
   public Stream<Entity> findAll(Stream<Object> ids, Fetch fetch) {
     return cacheable ? findAllWithCache(ids, fetch) : delegate().findAll(ids, fetch);
-  }
-
-  @Override
-  public void update(Entity entity) {
-    if (cacheable) {
-      upsertCache(entity);
-    }
-    delegate().update(entity);
-  }
-
-  @Override
-  public void update(Stream<Entity> entities) {
-    if (cacheable) {
-      entities = entities.filter(this::upsertCache);
-    }
-    delegate().update(entities);
-  }
-
-  @Override
-  public void delete(Entity entity) {
-    if (cacheable) {
-      deleteCache(entity);
-    }
-    delegate().delete(entity);
-  }
-
-  @Override
-  public void delete(Stream<Entity> entities) {
-    if (cacheable) {
-      entities = entities.filter(this::deleteCache);
-    }
-    delegate().delete(entities);
-  }
-
-  @Override
-  public void deleteById(Object id) {
-    if (cacheable) {
-      deleteCache(id);
-    }
-    delegate().deleteById(id);
-  }
-
-  @Override
-  public void deleteAll(Stream<Object> ids) {
-    if (cacheable) {
-      ids = ids.filter(this::deleteCache);
-    }
-    delegate().deleteAll(ids);
-  }
-
-  @Override
-  public void deleteAll() {
-    if (cacheable) {
-      l1Cache.evictAll(getEntityType());
-    }
-    delegate().deleteAll();
-  }
-
-  private boolean upsertCache(Entity entity) {
-    l1Cache.put(entity);
-    return true;
-  }
-
-  private boolean deleteCache(Entity entity) {
-    l1Cache.putDeletion(entity);
-    return true;
-  }
-
-  private boolean deleteCache(Object entityId) {
-    l1Cache.putDeletion(getEntityType(), entityId);
-    return true;
-  }
-
-  private Entity findOneByIdWithCache(Object id, @Nullable @CheckForNull Fetch fetch) {
-    EntityType entityType = getEntityType();
-    Optional<CacheHit<Entity>> cacheHit = l1Cache.get(entityType, id, fetch);
-    if (cacheHit.isPresent()) {
-      return cacheHit.get().getValue();
-    } else {
-      return fetch != null ? delegate().findOneById(id, fetch) : delegate().findOneById(id);
-    }
   }
 
   @SuppressWarnings("UnstableApiUsage")
@@ -197,15 +138,104 @@ public class L1CacheRepositoryDecorator extends AbstractRepositoryDecorator<Enti
     }
 
     if (missingEntityIds != null && !missingEntityIds.isEmpty()) {
-      Stream<Entity> entityStream =
-          fetch != null
-              ? delegate().findAll(missingEntityIds.stream(), fetch)
-              : delegate().findAll(missingEntityIds.stream());
+      // TODO retrieve with fetch and cache with fetch (also see findOneByIdWithCache)
+      Stream<Entity> entityStream = delegate().findAll(missingEntityIds.stream());
 
       // replace placeholder values with actual values
-      entityStream.forEach(entity -> entityMap.put(entity.getIdValue(), entity));
+      entityStream.forEach(
+          entity -> {
+            entityMap.put(entity.getIdValue(), entity);
+            // TODO cache nulls for ids for which no entity exists
+            l1Cache.put(entity);
+          });
     }
 
     return entityMap.values();
+  }
+
+  @Override
+  public void update(Entity entity) {
+    if (cacheable) {
+      l1CacheJanitor.cleanCacheBeforeUpdate(entity);
+      putCache(entity);
+    }
+    delegate().update(entity);
+  }
+
+  @Override
+  public void update(Stream<Entity> entities) {
+    if (cacheable) {
+      entities =
+          l1CacheJanitor.cleanCacheBeforeUpdate(getEntityType(), entities).filter(this::putCache);
+    }
+    delegate().update(entities);
+  }
+
+  @Override
+  public void delete(Entity entity) {
+    if (cacheable) {
+      l1CacheJanitor.cleanCacheBeforeDelete(entity);
+      delCache(entity);
+    }
+    delegate().delete(entity);
+  }
+
+  @Override
+  public void delete(Stream<Entity> entities) {
+    if (cacheable) {
+      entities =
+          l1CacheJanitor.cleanCacheBeforeDelete(getEntityType(), entities).filter(this::delCache);
+    }
+    delegate().delete(entities);
+  }
+
+  @Override
+  public void deleteById(Object id) {
+    if (cacheable) {
+      EntityType entityType = getEntityType();
+      l1CacheJanitor.cleanCacheBeforeDeleteById(entityType, id);
+      delCache(entityType, id);
+    }
+    delegate().deleteById(id);
+  }
+
+  @Override
+  public void deleteAll(Stream<Object> ids) {
+    if (cacheable) {
+      EntityType entityType = getEntityType();
+      ids =
+          l1CacheJanitor
+              .cleanCacheBeforeDeleteById(entityType, ids)
+              .filter(id -> this.delCache(entityType, id));
+    }
+    delegate().deleteAll(ids);
+  }
+
+  @Override
+  public void deleteAll() {
+    if (cacheable) {
+      EntityType entityType = getEntityType();
+      l1CacheJanitor.cleanCacheBeforeDeleteAll(entityType);
+      l1Cache.evictAll(entityType);
+    }
+    delegate().deleteAll();
+  }
+
+  /** Convenience method for use in stream filter. */
+  private boolean putCache(Entity entity) {
+    l1Cache.put(entity);
+    return true;
+  }
+
+  /** Convenience method for use in stream filter. */
+  private boolean delCache(Entity entity) {
+    l1Cache.putDeletion(entity);
+    return true;
+  }
+
+  /** Convenience method for use in stream filter. */
+  private boolean delCache(EntityType entityType, Object entityId) {
+    l1Cache.putDeletion(entityType, entityId);
+    return true;
   }
 }
