69ead50978a2d9c8ffbdb3227be6a3e2a4b81278
tommydeboer <tommydeboer4@gmail.com>
2019-12-13 09:36:43 +0100

    Fix #8811 Missing exception message + escaping issue in l10n

---
+++ molgenis-app-manager/src/test/java/org/molgenis/app/manager/exception/CouldNotUploadAppExceptionTest.java
@@ -0,0 +1,35 @@
+package org.molgenis.app.manager.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.molgenis.util.exception.CodedRuntimeException;
+import org.molgenis.util.exception.ExceptionMessageTest;
+
+public class CouldNotUploadAppExceptionTest extends ExceptionMessageTest {
+  @BeforeEach
+  void setUp() {
+    messageSource.addMolgenisNamespaces("app-manager");
+  }
+
+  @ParameterizedTest
+  @MethodSource("languageMessageProvider")
+  @Override
+  protected void testGetLocalizedMessage(String lang, String message) {
+    ExceptionMessageTest.assertExceptionMessageEquals(
+        new CouldNotUploadAppException("app.zip"), lang, message);
+  }
+
+  @Test
+  void testGetMessage() {
+    CodedRuntimeException ex = new CouldNotUploadAppException("app.zip");
+    assertEquals("app.zip", ex.getMessage());
+  }
+
+  public static Object[][] languageMessageProvider() {
+    return new Object[][] {new Object[] {"en", "Could not upload file 'app.zip'"}};
+  }
+}
