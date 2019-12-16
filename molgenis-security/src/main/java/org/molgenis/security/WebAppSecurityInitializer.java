1c3a33a1273b1ba145c167c44eea383c2fc29a5f
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-16 07:48:48 +0100

    Fix #8797: OpenID connect redirect_uri sometimes doesn't take X-Forwarded-* headers into account (#8796)
    * fix: put forwardedHeaderFilter before springSecurityFilterChain
    
    Fixes #8797
    
    * fix(security): update dispatcher types
    
    remove forward, add error to match spring boot's
    ServletWebServerFactoryAutoConfiguration.java

--- molgenis-security/src/main/java/org/molgenis/security/WebAppSecurityInitializer.java
+++ molgenis-security/src/main/java/org/molgenis/security/WebAppSecurityInitializer.java
@@ -1,5 +1,23 @@
 package org.molgenis.security;
 
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import static javax.servlet.DispatcherType.ASYNC;
+import static javax.servlet.DispatcherType.ERROR;
+import static javax.servlet.DispatcherType.REQUEST;
 
-public class WebAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
+import java.util.EnumSet;
+import javax.servlet.FilterRegistration.Dynamic;
+import javax.servlet.ServletContext;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+public class WebAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+  @Override
+  protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+    // add filters
+    Dynamic forwardedHeaderFilter =
+        servletContext.addFilter("forwardedHeaderFilter", ForwardedHeaderFilter.class);
+    forwardedHeaderFilter.setAsyncSupported(true);
+    forwardedHeaderFilter.addMappingForUrlPatterns(EnumSet.of(REQUEST, ERROR, ASYNC), false, "*");
+  }
+}
