5a266add14b252244cebf42897f6f21145f8bdab
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-02-13 14:14:48 +0100

    Unit tests

--- molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/generator/BaseQueryClauseGeneratorTest.java
+++ molgenis-data-elasticsearch/src/test/java/org/molgenis/data/elasticsearch/generator/BaseQueryClauseGeneratorTest.java
@@ -3,16 +3,32 @@ package org.molgenis.data.elasticsearch.generator;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.molgenis.data.meta.AttributeType.BOOL;
+import static org.molgenis.data.meta.AttributeType.COMPOUND;
+import static org.molgenis.data.meta.AttributeType.DATE;
+import static org.molgenis.data.meta.AttributeType.DATE_TIME;
+import static org.molgenis.data.meta.AttributeType.INT;
+import static org.molgenis.data.meta.AttributeType.STRING;
+import static org.molgenis.data.meta.AttributeType.XREF;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.molgenis.data.Entity;
+import org.molgenis.data.MolgenisQueryException;
 import org.molgenis.data.QueryRule;
 import org.molgenis.data.QueryRule.Operator;
 import org.molgenis.data.meta.model.Attribute;
@@ -32,6 +48,128 @@ class BaseQueryClauseGeneratorTest extends AbstractMockitoTest {
             return null;
           }
         };
+  }
+
+  @Test
+  void testGetOperator() {
+    assertEquals(Operator.EQUALS, queryGenerator.getOperator());
+  }
+
+  @Test
+  void testGetQueryFieldName() {
+    String fieldName = "MyFieldId";
+    Attribute attribute = mock(Attribute.class);
+    when(documentIdGenerator.generateId(attribute)).thenReturn(fieldName);
+    assertEquals(fieldName, queryGenerator.getQueryFieldName(attribute));
+  }
+
+  @Test
+  void testGetQueryFieldNameList() {
+    String fieldName0 = "MyFieldId1";
+    Attribute attribute0 = mock(Attribute.class);
+    doReturn(fieldName0).when(documentIdGenerator).generateId(attribute0);
+    String fieldName1 = "MyFieldId0";
+    Attribute attribute1 = mock(Attribute.class);
+    doReturn(fieldName1).when(documentIdGenerator).generateId(attribute1);
+    assertEquals(
+        "MyFieldId1.MyFieldId0", queryGenerator.getQueryFieldName(asList(attribute0, attribute1)));
+  }
+
+  @Test
+  void testUseNotAnalyzedFieldFalse() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(BOOL).getMock();
+    assertFalse(queryGenerator.useNotAnalyzedField(attribute));
+  }
+
+  @Test
+  void testUseNotAnalyzedFieldTrue() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(STRING).getMock();
+    assertTrue(queryGenerator.useNotAnalyzedField(attribute));
+  }
+
+  @Test
+  void testUseNotAnalyzedFieldXrefFalse() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(XREF).getMock();
+    EntityType refEntityType = mock(EntityType.class);
+    Attribute refIdAttribute = when(mock(Attribute.class).getDataType()).thenReturn(BOOL).getMock();
+    when(refEntityType.getIdAttribute()).thenReturn(refIdAttribute);
+    when(attribute.getRefEntity()).thenReturn(refEntityType);
+    assertFalse(queryGenerator.useNotAnalyzedField(attribute));
+  }
+
+  @Test
+  void testUseNotAnalyzedFieldXrefTrue() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(XREF).getMock();
+    EntityType refEntityType = mock(EntityType.class);
+    Attribute refIdAttribute =
+        when(mock(Attribute.class).getDataType()).thenReturn(STRING).getMock();
+    when(refEntityType.getIdAttribute()).thenReturn(refIdAttribute);
+    when(attribute.getRefEntity()).thenReturn(refEntityType);
+    assertTrue(queryGenerator.useNotAnalyzedField(attribute));
+  }
+
+  @SuppressWarnings("deprecation")
+  @Test
+  void testUseNotAnalyzedFieldCompound() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(COMPOUND).getMock();
+    assertThrows(MolgenisQueryException.class, () -> queryGenerator.useNotAnalyzedField(attribute));
+  }
+
+  @Test
+  void testQueryValue() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(STRING).getMock();
+    assertEquals("string", queryGenerator.getQueryValue(attribute, "string"));
+  }
+
+  @Test
+  void testQueryValueXref() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(XREF).getMock();
+    Entity entity = mock(Entity.class);
+    when(entity.getIdValue()).thenReturn("MyId");
+    assertEquals("MyId", queryGenerator.getQueryValue(attribute, entity));
+  }
+
+  @Test
+  void testQueryValueDate() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(DATE).getMock();
+    LocalDate localDate = LocalDate.now();
+    assertEquals(localDate.toString(), queryGenerator.getQueryValue(attribute, localDate));
+  }
+
+  @Test
+  void testQueryValueDateTime() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(DATE_TIME).getMock();
+    Instant instant = Instant.now();
+    assertEquals(instant.toString(), queryGenerator.getQueryValue(attribute, instant));
+  }
+
+  @SuppressWarnings("deprecation")
+  @Test
+  void testQueryValueCompound() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(COMPOUND).getMock();
+    assertThrows(MolgenisQueryException.class, () -> queryGenerator.getQueryValue(attribute, "x"));
+  }
+
+  @Test
+  void testQueryValues() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(STRING).getMock();
+    assertEquals(
+        asList("string0", "string1"),
+        queryGenerator.getQueryValues(attribute, asList("string0", "string1")));
+  }
+
+  @Test
+  void testValidateNumericalQueryFieldInt() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(INT).getMock();
+    assertDoesNotThrow(() -> queryGenerator.validateNumericalQueryField(attribute));
+  }
+
+  @SuppressWarnings("deprecation")
+  @Test
+  void testValidateNumericalQueryFieldBool() {
+    Attribute attribute = when(mock(Attribute.class).getDataType()).thenReturn(BOOL).getMock();
+    assertThrows(
+        MolgenisQueryException.class, () -> queryGenerator.validateNumericalQueryField(attribute));
   }
 
   @Test
