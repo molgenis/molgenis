e6bde383b6c3dd8ca56b45db91820e0bd0889f8b
Fleur Kelpin <f.kelpin@umcg.nl>
2020-02-25 14:00:21 +0100

    Fix Travis due to image update (#8941)
    Cleanup travis files
    
    Co-authored-by: Dennis Hendriksen <d.hendriksen@umcg.nl>

--- .ci/jobs/integration-tests.sh
+++ .ci/jobs/integration-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-travis_wait
+export MAVEN_SKIP_RC=true
+export MAVEN_OPTS="-XX:TieredStopAtLevel=1 -noverify"
 
 # run the integration tests
 mvn verify -pl molgenis-platform-integration-tests --batch-mode --quiet \
   -Dmaven.test.redirectTestOutputToFile=true \
   -Dit_db_user=postgres \
   -Dit_db_password=molgenis \
-  -Dit_db_port=5433
+  -Dit_db_port=5432
