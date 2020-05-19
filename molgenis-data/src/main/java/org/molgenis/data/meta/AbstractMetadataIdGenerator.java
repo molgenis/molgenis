55af6fd53c7db120f00e1fe67ec26c863f676088
Fleur Kelpin <f.kelpin@umcg.nl>
2020-05-19 11:33:41 +0200

    fix: cache generated metadata IDs
    Fixes #8989

--- molgenis-data/src/main/java/org/molgenis/data/meta/AbstractMetadataIdGenerator.java
+++ molgenis-data/src/main/java/org/molgenis/data/meta/AbstractMetadataIdGenerator.java
@@ -2,9 +2,23 @@ package org.molgenis.data.meta;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.guava.CaffeinatedGuava;
+import com.google.common.cache.LoadingCache;
 import com.google.common.hash.Hashing;
 
 public abstract class AbstractMetadataIdGenerator implements MetadataIdGenerator {
+
+  private static final int MAXIMUM_CACHE_SIZE = 10000;
+  private static final LoadingCache<String, String> CACHE =
+      CaffeinatedGuava.build(
+          Caffeine.newBuilder().maximumSize(MAXIMUM_CACHE_SIZE),
+          AbstractMetadataIdGenerator::generateHashcodeInternal);
+
+  private static String generateHashcodeInternal(String id) {
+    return Hashing.crc32().hashString(id, UTF_8).toString();
+  }
+
   /**
    * Generates a eight character [a-z0-9] system unique identifier.
    *
@@ -12,6 +26,6 @@ public abstract class AbstractMetadataIdGenerator implements MetadataIdGenerator
    * @return hashcode
    */
   protected String generateHashcode(Object id) {
-    return Hashing.crc32().hashString(id.toString(), UTF_8).toString();
+    return CACHE.getUnchecked(id.toString());
   }
 }
--- molgenis-data/src/test/java/org/molgenis/data/meta/AbstractMetadataIdGeneratorTest.java
+++ molgenis-data/src/test/java/org/molgenis/data/meta/AbstractMetadataIdGeneratorTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mock;
 
+import com.google.common.base.Stopwatch;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class AbstractMetadataIdGeneratorTest {
@@ -19,5 +21,25 @@ class AbstractMetadataIdGeneratorTest {
   void testGenerateHashcode() {
     String id = "0123456789";
     assertEquals("c6c784a6", abstractMetadataIdGenerator.generateHashcode(id));
+  }
+
+  @Test
+  @Disabled
+  public void demoSpeed() {
+    String hash = "seed";
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    for (var i = 0; i < 1000; i++) {
+      hash = abstractMetadataIdGenerator.generateHashcode("attributeName" + hash);
+    }
+    var uncached = stopwatch.elapsed();
+
+    stopwatch.reset().start();
+    for (var i = 0; i < 1000; i++) {
+      abstractMetadataIdGenerator.generateHashcode("attributeNameSame");
+    }
+    var cached = stopwatch.elapsed();
+
+    System.out.println(uncached);
+    System.out.println(cached);
   }
 }
