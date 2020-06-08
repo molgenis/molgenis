b128615bf5c5850ab91b0c3814ad505164c824d6
Fleur Kelpin <f.kelpin@umcg.nl>
2020-06-08 18:16:32 +0200

    feat(data): scale down connections when idle

--- molgenis-data/src/main/java/org/molgenis/data/config/DataSourceConfig.java
+++ molgenis-data/src/main/java/org/molgenis/data/config/DataSourceConfig.java
@@ -1,7 +1,6 @@
 package org.molgenis.data.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +8,15 @@ import org.springframework.context.annotation.Configuration;
 /** Datasource configuration */
 @Configuration
 public class DataSourceConfig {
+
   /**
    * Max pool size must be <= the maximum number of connections of configured in the DBMS (e.g.
    * PostgreSQL). The magic number is based on PostgreSQL default max connections = 100 minus 5
    * connections for admin tools communicating with the DBMS.
    */
   private static final int MAX_POOL_SIZE = 95;
+  /** When the connections are not in use, keep 10 idle connections around. */
+  public static final int MIN_IDLE = 10;
 
   @Value("${db_driver:org.postgresql.Driver}")
   private String dbDriverClass;
@@ -29,7 +31,7 @@ public class DataSourceConfig {
   private String dbPassword;
 
   @Bean
-  public DataSource dataSource() {
+  public HikariDataSource dataSource() {
     if (dbDriverClass == null) throw new IllegalArgumentException("db_driver is null");
     if (dbJdbcUri == null) throw new IllegalArgumentException("db_uri is null");
     if (dbUser == null)
@@ -45,6 +47,7 @@ public class DataSourceConfig {
     dataSource.setUsername(dbUser);
     dataSource.setPassword(dbPassword);
     dataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+    dataSource.setMinimumIdle(MIN_IDLE);
 
     dataSource.addDataSourceProperty("reWriteBatchedInserts", "true");
     dataSource.addDataSourceProperty("autosave", "CONSERVATIVE");
