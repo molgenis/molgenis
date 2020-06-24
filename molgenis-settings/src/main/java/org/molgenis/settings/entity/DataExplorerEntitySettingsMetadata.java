eb87259928cfceb67b4a640eb7263b2d4d5bb815
connoratrug <c.stroomberg@rug.nl>
2020-06-24 12:31:14 +0200

    fix: replace star import with full path

--- molgenis-settings/src/main/java/org/molgenis/settings/entity/DataExplorerEntitySettingsMetadata.java
+++ molgenis-settings/src/main/java/org/molgenis/settings/entity/DataExplorerEntitySettingsMetadata.java
@@ -1,7 +1,10 @@
 package org.molgenis.settings.entity;
 
 import static java.util.Objects.requireNonNull;
-import static org.molgenis.data.meta.AttributeType.*;
+import static org.molgenis.data.meta.AttributeType.STRING;
+import static org.molgenis.data.meta.AttributeType.XREF;
+import static org.molgenis.data.meta.AttributeType.SCRIPT;
+import static org.molgenis.data.meta.AttributeType.BOOL;
 import static org.molgenis.data.meta.model.EntityType.AttributeRole.ROLE_ID;
 import static org.molgenis.data.meta.model.Package.PACKAGE_SEPARATOR;
 import static org.molgenis.settings.entity.EntitySettingsPackage.PACKAGE_ENTITY_SETTINGS;
