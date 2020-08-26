6ed6d13688866c713e062266706e4a8c51058c7c
connoratrug <47183404+connoratrug@users.noreply.github.com>
2020-08-26 12:05:16 +0200

    Update step42-replaceFormsUrlMsg.sql
    remove unneeded insert ( already done by bootstrapper)

--- molgenis-data-migrate/src/main/resources/step42-replaceFormsUrlMsg.sql
+++ molgenis-data-migrate/src/main/resources/step42-replaceFormsUrlMsg.sql
@@ -1,9 +1,4 @@
--- rename url to hyperlink
--- first add new row
-INSERT INTO public."sys_L10nString#95a21e09" (id, msgid, namespace, description, en, nl, de, es, it, pt, fr, xx)
-VALUES ('form_not_a_valid_hyperlink', 'form_not_a_valid_hyperlink', 'ui-form', null, 'Not a valid Hyperlink',
-        'Geen geldige Hyperlink', null, null, null, null, null, null);
--- then remove old row
+-- remove unused row
 DELETE
 FROM public."sys_L10nString#95a21e09"
 WHERE id LIKE 'form_not_a_valid_url' ESCAPE '#';
