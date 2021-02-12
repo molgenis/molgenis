f300b09bea438423c9a678a3d5496bf90b73fd9a
Fleur Kelpin <f.kelpin@umcg.nl>
2021-02-12 17:18:54 +0100

    test(security): use concrete security events and check details map

--- molgenis-security/src/test/java/org/molgenis/security/audit/AuthorizationEventListenerTest.java
+++ molgenis-security/src/test/java/org/molgenis/security/audit/AuthorizationEventListenerTest.java
@@ -1,22 +1,23 @@
 package org.molgenis.security.audit;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.molgenis.security.audit.AuthenticationEventListener.AUTHENTICATION_FAILURE;
 import static org.molgenis.security.audit.AuthorizationEventListener.AUTHORIZATION_FAILURE;
 
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.molgenis.audit.AuditEventPublisher;
 import org.molgenis.test.AbstractMockitoTest;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.access.event.AuthenticationCredentialsNotFoundEvent;
 import org.springframework.security.access.event.AuthorizationFailureEvent;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 
 class AuthorizationEventListenerTest extends AbstractMockitoTest {
 
@@ -29,27 +30,49 @@ class AuthorizationEventListenerTest extends AbstractMockitoTest {
     authorizationEventListener = new AuthorizationEventListener(auditEventPublisher);
   }
 
-  @SuppressWarnings("unchecked")
   @Test
   void onAuthenticationCredentialsNotFoundEvent() {
     AuthenticationCredentialsNotFoundEvent event =
-        mock(AuthenticationCredentialsNotFoundEvent.class, RETURNS_DEEP_STUBS);
+        new AuthenticationCredentialsNotFoundEvent(
+            getClass(),
+            List.of(new SecurityConfig("isAuthenticated()")),
+            new AuthenticationCredentialsNotFoundException("Not authenticated."));
 
     authorizationEventListener.onAuthenticationCredentialsNotFoundEvent(event);
 
     verify(auditEventPublisher)
-        .publish(eq("<unknown>"), eq(AUTHENTICATION_FAILURE), any(Map.class));
+        .publish(
+            "<unknown>",
+            AUTHENTICATION_FAILURE,
+            Map.of(
+                "type",
+                "org.springframework.security.authentication.AuthenticationCredentialsNotFoundException",
+                "message",
+                "Not authenticated."));
   }
 
-  @SuppressWarnings("unchecked")
   @Test
   void onAuthorizationFailureEvent() {
-    AuthorizationFailureEvent event = mock(AuthorizationFailureEvent.class, RETURNS_DEEP_STUBS);
-    when(event.getAuthentication().getName()).thenReturn("henk");
-    when(event.getSource()).thenReturn(new Object());
+    Authentication authentication = new UsernamePasswordAuthenticationToken("henk", "secret");
+    AuthorizationFailureEvent event =
+        new AuthorizationFailureEvent(
+            getClass(),
+            List.of(new SecurityConfig("hasRole('user')")),
+            authentication,
+            new AccessDeniedException("Access denied"));
 
     authorizationEventListener.onAuthorizationFailureEvent(event);
 
-    verify(auditEventPublisher).publish(eq("henk"), eq(AUTHORIZATION_FAILURE), any(Map.class));
+    verify(auditEventPublisher)
+        .publish(
+            "henk",
+            AUTHORIZATION_FAILURE,
+            Map.of(
+                "type",
+                "org.springframework.security.access.AccessDeniedException",
+                "message",
+                "Access denied",
+                "object",
+                "class org.molgenis.security.audit.AuthorizationEventListenerTest"));
   }
 }
