5b90ba85ba0db1376e23c904227b0f1dae3a51b4
tommy <tommydeboer4@gmail.com>
2021-03-18 13:19:55 +0100

    fix formatting

--- molgenis-data-security/src/main/java/org/molgenis/data/security/audit/AuthenticationUtils.java
+++ molgenis-data-security/src/main/java/org/molgenis/data/security/audit/AuthenticationUtils.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 class AuthenticationUtils {
 
-  private AuthenticationUtils() { }
+  private AuthenticationUtils() {}
 
   static boolean isRunByUser() {
     var auth = SecurityContextHolder.getContext().getAuthentication();
