41d4822f46b48a942f68604780b76fe2287704b1
Jelmer Veen <j.p.veen@rug.nl>
2021-09-15 09:22:09 +0200

    chore: fix styling

--- molgenis-data-row-permissions/src/main/java/org/molgenis/data/row/permissions/DataRowPermissionsController.java
+++ molgenis-data-row-permissions/src/main/java/org/molgenis/data/row/permissions/DataRowPermissionsController.java
@@ -1,29 +1,28 @@
 package org.molgenis.data.row.permissions;
 
+import static org.molgenis.data.row.permissions.DataRowPermissionsController.URI;
+
 import org.molgenis.web.PluginController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.molgenis.data.row.permissions.DataRowPermissionsController.URI;
 
 @Controller
 @RequestMapping(URI)
 public class DataRowPermissionsController extends PluginController {
-    private static final Logger LOG = LoggerFactory.getLogger(DataRowPermissionsController.class);
+  private static final Logger LOG = LoggerFactory.getLogger(DataRowPermissionsController.class);
 
-    public static final String DATA_ROW_PERMISSIONS = "data-row-permissions";
-    public static final String URI = PLUGIN_URI_PREFIX + DATA_ROW_PERMISSIONS;
+  public static final String DATA_ROW_PERMISSIONS = "data-row-permissions";
+  public static final String URI = PLUGIN_URI_PREFIX + DATA_ROW_PERMISSIONS;
 
-    public DataRowPermissionsController() {
-        super(URI);
-    }
+  public DataRowPermissionsController() {
+    super(URI);
+  }
 
-    @GetMapping("/**")
-    public String init() {
-        return "view-data-row-permissions";
-    }
+  @GetMapping("/**")
+  public String init() {
+    return "view-data-row-permissions";
+  }
 }
