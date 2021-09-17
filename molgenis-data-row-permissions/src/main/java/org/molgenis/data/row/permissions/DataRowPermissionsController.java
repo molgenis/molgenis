e5e6811326b7a51b338f486300885d9dbb9ea1ac
Jelmer Veen <j.p.veen@rug.nl>
2021-09-17 16:19:37 +0200

    chore: added correct paths and files

--- molgenis-data-row-permissions/src/main/java/org/molgenis/data/row/permissions/DataRowPermissionsController.java
+++ molgenis-data-row-permissions/src/main/java/org/molgenis/data/row/permissions/DataRowPermissionsController.java
@@ -2,30 +2,34 @@ package org.molgenis.data.row.permissions;
 
 import static org.molgenis.data.row.permissions.DataRowPermissionsController.URI;
 
+import org.molgenis.settings.AppSettings;
 import org.molgenis.web.PluginController;
-import org.molgenis.web.menu.MenuReaderService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(URI)
 public class DataRowPermissionsController extends PluginController {
-  private static final Logger LOG = LoggerFactory.getLogger(DataRowPermissionsController.class);
 
   public static final String ID = "data-row-permissions";
   public static final String URI = PluginController.PLUGIN_URI_PREFIX + ID;
   public static final String VIEW_TEMPLATE = "view-data-row-permissions";
 
-  public DataRowPermissionsController() {
+  private final AppSettings appSettings;
+
+  public DataRowPermissionsController(AppSettings appSettings) {
     super(URI);
-    LOG.info("I'm getting accessed!");
+    this.appSettings = appSettings;
   }
 
   @GetMapping("/**")
-  public String init() {
+  public String init(Model model) {
+    model.addAttribute("baseUrl", URI);
+    model.addAttribute("lng", LocaleContextHolder.getLocale().getLanguage());
+    model.addAttribute("fallbackLng", appSettings.getLanguageCode());
     return VIEW_TEMPLATE;
   }
 }
--- molgenis-data-row-permissions/src/main/resources/templates/view-data-row-permissions.ftl
+++ molgenis-data-row-permissions/src/main/resources/templates/view-data-row-permissions.ftl
@@ -3,10 +3,14 @@
 <#include "molgenis-footer.ftl">
 
 <#assign js = []>
-<#assign css = ["/@molgenis-ui/data-row-permissions/dist/css/app.css"]>
+<#assign css = []>
 <#assign version = 2>
 
+<link rel="stylesheet" href="/@molgenis-ui/data-row-permissions/dist/css/chunk-vendors.css"/>
+<link rel="stylesheet" href="/@molgenis-ui/data-row-permissions/dist/css/app.css"/>
+
 <@header css js version/>
+
 <div id="app"></div>
 
 <script type="text/javascript">
@@ -16,10 +20,9 @@
             fallbackLng: '${fallbackLng}',
             isSuperUser: ${isSuperUser?c}
         }
-    </script>
+</script>
 
-<script type=text/javascript src="/@molgenis-ui/data-row-permissions/dist/js/manifest.js"></script>
-<script type=text/javascript src="/@molgenis-ui/data-row-permissions/dist/js/vendor.js"></script>
+<script type=text/javascript src="/@molgenis-ui/data-row-permissions/dist/js/chunk-vendors.js"></script>
 <script type=text/javascript src="/@molgenis-ui/data-row-permissions/dist/js/app.js"></script>
 
 <@footer version/>
