47cb44fa5d2113533af3fc0a2ff9162b6d37926d
Fleur Kelpin <f.kelpin@umcg.nl>
2021-10-31 22:54:39 +0100

    feat(metadata-manager): add a way to set the current value for a sequence
    When you move data from one server to the other, set current value for the sequence through a POST call

--- molgenis-metadata-manager/src/main/java/org/molgenis/metadata/manager/controller/MetadataManagerController.java
+++ molgenis-metadata-manager/src/main/java/org/molgenis/metadata/manager/controller/MetadataManagerController.java
@@ -1,9 +1,11 @@
 package org.molgenis.metadata.manager.controller;
 
 import static java.util.Objects.requireNonNull;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 import java.util.List;
+import org.molgenis.data.populate.Sequences;
 import org.molgenis.metadata.manager.model.EditorAttributeResponse;
 import org.molgenis.metadata.manager.model.EditorEntityType;
 import org.molgenis.metadata.manager.model.EditorEntityTypeResponse;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -31,18 +34,34 @@ public class MetadataManagerController extends PluginController {
 
   private MetadataManagerService metadataManagerService;
   private final MenuReaderService menuReaderService;
+  private final Sequences sequences;
 
   public MetadataManagerController(
-      MenuReaderService menuReaderService, MetadataManagerService metadataManagerService) {
+      MenuReaderService menuReaderService,
+      MetadataManagerService metadataManagerService,
+      Sequences sequences) {
     super(URI);
     this.metadataManagerService = requireNonNull(metadataManagerService);
     this.menuReaderService = requireNonNull(menuReaderService);
+    this.sequences = requireNonNull(sequences);
   }
 
   @GetMapping("/**")
   public String init(Model model) {
     model.addAttribute(KEY_BASE_URL, menuReaderService.findMenuItemPath(METADATA_MANAGER));
     return "view-metadata-manager";
+  }
+
+  @GetMapping(value = "/sequences", produces = "application/json")
+  @ResponseBody
+  public List<String> getSequences() {
+    return sequences.getSequences();
+  }
+
+  @PostMapping("/sequences/{sequenceName}")
+  @ResponseStatus(NO_CONTENT)
+  public void setValue(@PathVariable String sequenceName, @RequestParam long value) {
+    sequences.setValue(sequenceName, value);
   }
 
   @ResponseBody
