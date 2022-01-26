4ccbb00c22a4b8bb564bc3c927a8275c02b796f3
Joris de Keijser <chinook25@gmail.com>
2022-01-26 15:48:18 +0100

    chore: refactoring

--- molgenis-data/src/main/java/org/molgenis/data/support/TemplateExpressionEvaluator.java
+++ molgenis-data/src/main/java/org/molgenis/data/support/TemplateExpressionEvaluator.java
@@ -5,7 +5,6 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.singletonMap;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 import com.github.jknack.handlebars.Context;
 import com.github.jknack.handlebars.EscapingStrategy;
@@ -22,7 +21,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import org.molgenis.data.Entity;
 import org.molgenis.data.meta.AttributeType;
 import org.molgenis.data.meta.IllegalAttributeTypeException;
@@ -32,13 +30,12 @@ import org.molgenis.util.UnexpectedEnumException;
 
 public class TemplateExpressionEvaluator implements ExpressionEvaluator {
 
-  private static final Handlebars HANDLEBARS = setupHandlebars();
   public static final String MATH = "math";
-
+  private static final Handlebars HANDLEBARS = setupHandlebars();
   private final Attribute attribute;
   private final EntityType entityType;
   private Template template;
-  private List<List<String>> templateTags;
+  private List<List<String>> variables;
 
   TemplateExpressionEvaluator(Attribute attribute, EntityType entityType) {
     this.attribute = requireNonNull(attribute);
@@ -75,7 +72,7 @@ public class TemplateExpressionEvaluator implements ExpressionEvaluator {
   @Override
   public Object evaluate(Entity entity) {
     initTemplate();
-    Map<String, Object> tagValues = getTemplateTagValue(entity);
+    Map<String, Object> tagValues = getVarValues(entity);
 
     try {
       return template.apply(Context.newContext(tagValues));
@@ -85,90 +82,98 @@ public class TemplateExpressionEvaluator implements ExpressionEvaluator {
   }
 
   @SuppressWarnings("unchecked")
-  private Map<String, Object> getTemplateTagValue(Entity entity) {
-    Map<String, Object> tagValues = new HashMap<>();
-    templateTags.forEach(
-        tagParts -> {
-          Object value = getTemplateTagValueRecursive(entity, tagParts, 0);
+  private Map<String, Object> getVarValues(Entity entity) {
+    Map<String, Object> varValues = new HashMap<>();
+    variables.forEach(varParts -> getVarValue(entity, varValues, varParts));
+    return varValues;
+  }
 
-          String tagPart = tagParts.get(0);
-          if (tagValues.containsKey(tagPart)) {
-            Object existingValue = tagValues.get(tagPart);
-            if (existingValue != null
-                && !Objects.equals(value, existingValue)
-                && value instanceof Map
-                && existingValue instanceof Map) {
-              Map<String, Object> mergedValue = new HashMap<>();
-              mergedValue.putAll((Map) existingValue);
-              mergedValue.putAll((Map) value);
-              value = mergedValue;
-            }
-          }
-          tagValues.put(tagParts.get(0), value);
-        });
-    return tagValues;
+  private void getVarValue(Entity entity, Map<String, Object> varValues, List<String> varParts) {
+    Object value = getTemplateTagValueRecursive(entity, varParts, 0);
+
+    String variableName = varParts.get(0);
+    if (varValues.containsKey(variableName)) {
+      Object existingValue = varValues.get(variableName);
+      if (existingValue != null
+          && !Objects.equals(value, existingValue)
+          && value instanceof Map
+          && existingValue instanceof Map) {
+        Map<String, Object> mergedValue = new HashMap<>();
+        mergedValue.putAll((Map) existingValue);
+        mergedValue.putAll((Map) value);
+        value = mergedValue;
+      }
+    }
+    varValues.put(variableName, value);
   }
 
   private Object getTemplateTagValueRecursive(Entity entity, List<String> tagParts, int index) {
     String attributeName = tagParts.get(index);
 
-    Object value;
     Attribute tagAttribute = entity.getEntityType().getAttribute(attributeName);
     switch (tagAttribute.getDataType()) {
-      case BOOL -> value = entity.getBoolean(attributeName);
-      case CATEGORICAL, FILE, XREF -> {
-        Entity refEntity = entity.getEntity(attributeName);
-        Object refValue =
-            refEntity != null ? getTemplateTagValueRecursive(refEntity, tagParts, index + 1) : null;
-        String refTag = tagParts.get(index + 1);
-        value = singletonMap(refTag, refValue);
-      }
-      case CATEGORICAL_MREF, MREF, ONE_TO_MANY -> {
-        Object mrefValue =
-            stream(entity.getEntities(attributeName))
-                .map(mrefEntity -> getTemplateTagValueRecursive(mrefEntity, tagParts, index + 1))
-                .map(Object::toString)
-                .collect(joining(","));
-        String mrefTag = tagParts.get(index + 1);
-        value = singletonMap(mrefTag, mrefValue);
-      }
+      case BOOL -> { return entity.getBoolean(attributeName); }
+      case CATEGORICAL, FILE, XREF -> { return getXrefVarValue(entity, tagParts, index, attributeName); }
+      case CATEGORICAL_MREF, MREF, ONE_TO_MANY -> { return getMrefVarValue(entity, tagParts, index, attributeName); }
+      case DATE -> { return entity.getLocalDate(attributeName); }
+      case DATE_TIME -> { return entity.getInstant(attributeName); }
+      case DECIMAL -> { return entity.getDouble(attributeName); }
+      case EMAIL, ENUM, HTML, HYPERLINK, SCRIPT, STRING, TEXT -> { return entity.getString(attributeName); }
+      case INT -> { return entity.getInt(attributeName); }
+      case LONG ->{ return entity.getLong(attributeName); }
       case COMPOUND -> throw new IllegalAttributeTypeException(tagAttribute.getDataType());
-      case DATE -> value = entity.getLocalDate(attributeName);
-      case DATE_TIME -> value = entity.getInstant(attributeName);
-      case DECIMAL -> value = entity.getDouble(attributeName);
-      case EMAIL, ENUM, HTML, HYPERLINK, SCRIPT, STRING, TEXT -> value = entity.getString(
-          attributeName);
-      case INT -> value = entity.getInt(attributeName);
-      case LONG -> value = entity.getLong(attributeName);
       default -> throw new UnexpectedEnumException(tagAttribute.getDataType());
     }
-    return value;
+  }
+
+  private Map<String, Object> getXrefVarValue(Entity entity, List<String> tagParts, int index,
+      String attributeName) {
+    Entity refEntity = entity.getEntity(attributeName);
+    Object refValue =
+        refEntity != null ? getTemplateTagValueRecursive(refEntity, tagParts, index + 1) : null;
+    String refTag = tagParts.get(index + 1);
+    return singletonMap(refTag, refValue);
+  }
+
+  private Map<String, Object> getMrefVarValue(Entity entity, List<String> tagParts, int index,
+      String attributeName) {
+    Object mrefValue =
+        stream(entity.getEntities(attributeName))
+            .map(mrefEntity -> getTemplateTagValueRecursive(mrefEntity, tagParts, index + 1))
+            .map(Object::toString)
+            .collect(joining(","));
+    String mrefTag = tagParts.get(index + 1);
+    return singletonMap(mrefTag, mrefValue);
   }
 
   private synchronized void initTemplate() {
     if (template == null) {
       template = getTemplate(attribute);
-      templateTags = getTemplateVariables(template);
+      variables = getTemplateVariables(template);
     }
   }
 
   private List<List<String>> getTemplateVariables(Template template) {
-    List<String> tagNames = template.collectReferenceParameters();
-
-    List<String> foo = template.collect(TagType.VAR);
-    tagNames.addAll(foo);
-    List<String> collect = tagNames.stream().filter(s -> !s.equals(MATH)).collect(toList());
-
-    List<List<String>> composedTagNames =
-        collect.stream().map(tagName -> asList(tagName.split("\\."))).collect(toList());
-    composedTagNames.forEach(this::validateTemplateVariable);
-    return composedTagNames;
+    List<List<String>> varNames = getVarNames(template);
+    validateVars(varNames);
+    return varNames;
   }
 
-  private void validateTemplateVariable(List<String> composedTagName) {
+  private List<List<String>> getVarNames(Template template) {
+    List<String> varNames = template.collectReferenceParameters();
+    List<String> simpleVarNames = template.collect(TagType.VAR);
+    varNames.addAll(simpleVarNames);
+    return varNames.stream().filter(s -> !s.equals(MATH)).map(tagName -> asList(tagName.split("\\."))).toList();
+  }
+
+  private void validateVars(List<List<String>> varNames) {
+    varNames.forEach(this::validateVariable);
+  }
+
+  private void validateVariable(List<String> composedVarName) {
     EntityType variableEntityType = entityType;
-    for (Iterator<String> it = composedTagName.iterator(); it.hasNext(); ) {
-      String tagPartName = it.next();
+    for (Iterator<String> iterator = composedVarName.iterator(); iterator.hasNext(); ) {
+      String tagPartName = iterator.next();
 
       if (variableEntityType == null) {
         throw new TemplateExpressionInvalidTagException(attribute.getExpression(), tagPartName);
@@ -179,13 +184,14 @@ public class TemplateExpressionEvaluator implements ExpressionEvaluator {
         throw new TemplateExpressionUnknownAttributeException(
             attribute.getExpression(), tagPartName);
       }
+
       if (tagAttribute.getDataType() == AttributeType.COMPOUND) {
         throw new TemplateExpressionAttributeTypeException(
             attribute.getExpression(), tagPartName, tagAttribute);
       }
 
       variableEntityType = tagAttribute.hasRefEntity() ? tagAttribute.getRefEntity() : null;
-      if (variableEntityType != null && !it.hasNext()) {
+      if (variableEntityType != null && !iterator.hasNext()) {
         throw new TemplateExpressionMissingTagException(attribute.getExpression(), tagPartName);
       }
     }
